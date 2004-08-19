43ac3ed3ed2b7dfe80a104bbaeba09874005d2c4
Canonical.com Patch Queue Manager <pqm@canonical.com>
2004-08-19 19:54:22 +0000

    better command structure, better error handling, easier-to-read debugging data, added 'list-bugs' command Patches applied:
     * david.miller@canonical.com--2004/launchpad--devel--0--patch-3
       merge from rocketfuel
    
     * david.miller@canonical.com--2004/launchpad--devel--0--patch-4
       better command infrastructure
    
     * david.miller@canonical.com--2004/launchpad--devel--0--patch-5
       better error handling, added list-bugs command
    
     * david.miller@canonical.com--2004/launchpad--devel--0--patch-6
       make the debugging data easier to read
    
     * david.miller@canonical.com--2004/launchpad--devel--0--patch-7
       merge from rocketfuel
    
     * david.miller@canonical.com--2004/launchpad--devel--0--patch-8
       fix breakage from rocketfuel merge (malone's sql module moved)

--- lib/canonical/malone/rpcdaemon.py
+++ lib/canonical/malone/rpcdaemon.py
@@ -11,8 +11,8 @@ To run the server, just run this file ("rpcdaemon.py").
 from twisted.web import xmlrpc
 from sqlobject import connectionForURI
 from canonical.arch.sqlbase import SQLBase
-from canonical.malone.sql import Bug
-import xmlrpclib
+from canonical.database.malone import Bug
+import xmlrpclib, sqlobject, sre
 
 class RPCDaemon(xmlrpc.XMLRPC):
     """XMLRPC daemon handler.
@@ -24,13 +24,85 @@ class RPCDaemon(xmlrpc.XMLRPC):
         SQLBase.initZopeless(connectionForURI('postgres:///launchpad_test'))
 	self.debug=1
 
-    def xmlrpc_getBugField(self,field,id):
-        """Get bug title."""
+    def xmlrpc_runcommand(self, cmd, locale, packedargs):
+        """Run a command."""
 	if self.debug:
-	  print "getBugField(field='%s',id=%s) called" % (field, id)
-        id = int(id)
-        bug = Bug.get(id)
-	return getattr(bug,field)
+            print ">>> runcommand called with cmd=%s, locale=%s, args=%s" % (cmd,
+                locale, packedargs)
+        if len(packedargs) != 2:
+            if self.debug:
+                print "<<< *** Invalid argument format"
+            raise xmlrpc.Fault(-32000,"Invalid Argument Format")
+        cmd = cmd.lower().replace("-", "_")
+        options = packedargs[0]
+        args = packedargs[1]
+        try:
+            cmdmethod = getattr(self, "cmd_" + cmd);
+        except AttributeError:
+            if self.debug:
+                print "<<< *** Command not implemented"
+            raise xmlrpc.Fault(-32601,"Command not implemented")
+        # if we made it here, it exists. Call it.
+        result = cmdmethod(args)
+        if self.debug:
+            print "<<< returning with result = %s" % result
+        return result
+
+    def serialize_bug(self, bug):
+        data = {
+            'id': bug.id,
+            'datecreated': str(bug.datecreated),
+            'title': bug.title,
+            'description': bug.description,
+            #'duplicateof': bug.duplicateof,
+        }
+        return data
+
+    def serialize_buglist(self, buglist):
+        newbuglist = []
+        for bug in buglist:
+            newbuglist.append(self.serialize_bug(bug))
+        return newbuglist
+
+    def cmd_get_field(self, args):
+        if len(args) < 2:
+            return ["error", "Not enough arguments"]
+        if len(args) > 2:
+            return ["error", "Too many arguments"]
+        field = args[0]
+        if not args[1].isdigit():
+            return ["error", "The bug ID must be a number"]
+        id = int(args[1])
+        try:
+            bug = Bug.get(id)
+        except sqlobject.main.SQLObjectNotFound:
+            return ["error", "Bug not found"]
+	return ["result", getattr(bug,field)]
+
+    def cmd_show(self, args):
+        if len(args) < 1:
+            return ["error", "Not enough arguments"]
+        if len(args) > 1:
+            return ["error", "Too many arguments"]
+        if not sre.search(r'^\d+$',args[0]):
+            return ["error", "The bug ID must be a number"]
+        id = int(args[0])
+        try:
+            bug = Bug.get(id)
+        except sqlobject.main.SQLObjectNotFound:
+            return ["error", "Bug not found"]
+	return ["result", self.serialize_bug(bug)]
+
+    def cmd_list_bugs(self, args):
+        #if len(args) > 0:
+        #    return ["error", "Too many arguments"]
+        #id = int(args[0])
+        try:
+            buglist = list(Bug.select("lower(title) like '%%%s%%'" %
+                args[0].lower()))
+        except sqlobject.main.SQLObjectNotFound:
+            return ["error", "No bugs found"]
+	return ["result", self.serialize_buglist(buglist)]
 
 def main():
     from twisted.internet import reactor
