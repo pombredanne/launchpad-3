474bb385e34637c5b1b2aeb7640c20167aca4dfe
Canonical.com Patch Queue Manager <pqm@canonical.com>
2004-08-24 17:34:35 +0000

    make sure ftests reinitialize the database before running Patches applied:
     * lalo@canonical.com--canonical-work-2004/launchpad--devel--0--patch-70
       make sure ftests reinitialize the database before running
    
     * stuart.bishop@canonical.com/launchpad--devel--0--patch-67
       Merge from rocketfuel
    
     * stuart.bishop@canonical.com/launchpad--devel--0--patch-68
       Migrate some Malone sample data to sampledata.sql
    
     * stuart.bishop@canonical.com/launchpad--devel--0--patch-69
       EmailAddress interface and class
    
     * stuart.bishop@canonical.com/launchpad--devel--0--patch-70
       Allow specification of sourcepackage, product and/or binarypackage when creating Bugs

--- lib/canonical/rosetta/ftests/test_poimport.py
+++ lib/canonical/rosetta/ftests/test_poimport.py
@@ -9,7 +9,7 @@ from canonical.arch.sqlbase import SQLBase
 from canonical.rosetta.interfaces import ILanguages
 from canonical.rosetta.sql import RosettaPerson, RosettaPOTemplate, \
     RosettaProject, RosettaProduct, RosettaLanguages
-from sqlobject import connectionForURI
+from sqlobject.dbconnection import TheURIOpener as connectionFactory
 from canonical.rosetta.pofile_adapters import MessageProxy, TemplateImporter
 import os
 
@@ -21,12 +21,22 @@ here = os.path.dirname(os.path.abspath(__file__))
 class POImportTestCase(PlacelessSetup, unittest.TestCase):
 
     def setUp(self):
+        # these tests are going to be slow as hell...
+        os.system("make -C %s testdb > /dev/null 2>/dev/null" % os.path.dirname(here))
         super(POImportTestCase, self).setUp()
         utilityService = getService(servicenames.Utilities)
         utilityService.provideUtility(ILanguages, RosettaLanguages(), '')
-        SQLBase.initZopeless(connectionForURI('postgres:///launchpad_test'))
+        self.connection = connectionFactory.connectionForURI('postgres:///launchpad_test')
+        SQLBase.initZopeless(self.connection)
         self.pot = file(os.path.join(here, 'gnome-terminal.pot'))
         self.po = file(os.path.join(here, 'gnome-terminal-cy.po'))
+
+    def tearDown(self):
+        c = self.connection.getConnection()
+        self.connection.releaseConnection(c)
+        c.close()
+        connectionFactory.cachedURIs.clear()
+        super(POImportTestCase, self).tearDown()
 
     def testTemplateImporter(self):
         try:
@@ -124,7 +134,7 @@ class POImportTestCase(PlacelessSetup, unittest.TestCase):
 
 def test_suite():
     loader = unittest.TestLoader()
-    return loader.loadTestsFromTestCase(POImportTestCase)
+    return unittest.makeSuite(POImportTestCase)
 
 if __name__ == '__main__':
     unittest.main()
