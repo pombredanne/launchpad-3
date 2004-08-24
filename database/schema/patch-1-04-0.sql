56211e21fc06539e0af46c7cf880120584c97bb3
Canonical.com Patch Queue Manager <pqm@canonical.com>
2004-08-24 17:11:57 +0000

    Rosetta database schema fixes Patches applied:
     * stuart.bishop@canonical.com/launchpad--devel--0--patch-67
       Merge from rocketfuel
    
     * stuart.bishop@canonical.com/launchpad--devel--0--patch-68
       Migrate some Malone sample data to sampledata.sql
    
     * stuart.bishop@canonical.com/launchpad--devel--0--patch-69
       EmailAddress interface and class
    
     * stuart.bishop@canonical.com/launchpad--devel--0--patch-70
       Allow specification of sourcepackage, product and/or binarypackage when creating Bugs
    
     * stuart.bishop@canonical.com/launchpad--devel--0--patch-77
       Rosetta database schema fixes

---
+++ database/schema/patch-1-04-0.sql
@@ -0,0 +1,17 @@
+/*
+ * Rosetta fixes
+ *
+ * With the rename from deprecated to active in patch-1-1-0
+ * we forgot to change the default value.
+ */
+ALTER TABLE POTranslationSighting ALTER COLUMN active SET DEFAULT TRUE;
+
+/*
+ * A plural form could have the same translation for some numbers
+ */
+ALTER TABLE POTranslationSighting
+    DROP CONSTRAINT "potranslationsighting_pomsgset_key";
+ALTER TABLE POTranslationSighting
+    ADD CONSTRAINT "potranslationsighting_pomsgset_key"
+    UNIQUE(pomsgset, potranslation, license, person, pluralform)
+
