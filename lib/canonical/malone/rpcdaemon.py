1915441c90d8c31c501016cfdf7715c96bbcb197
Canonical.com Patch Queue Manager <pqm@canonical.com>
2004-08-25 15:56:09 +0000

    rpcdaemon: search with multiple params, server-side help, retrieve assignment info Patches applied:
     * david.miller@canonical.com--2004-laptop/launchpad--devel--0--base-0
       tag of david.miller@canonical.com--2004-MIRROR/launchpad--devel--0--patch-8
    
     * david.miller@canonical.com--2004-laptop/launchpad--devel--0--patch-1
       merge changes from rocketfuel
    
     * david.miller@canonical.com--2004-laptop/launchpad--devel--0--patch-2
       Merge from rocketfuel
    
     * david.miller@canonical.com--2004-laptop/launchpad--devel--0--patch-3
       list-bugs now works with multiple search params, can now get help from the server
    
     * david.miller@canonical.com--2004-laptop/launchpad--devel--0--patch-4
       make help return the entire usage message instead of just the first line
    
     * david.miller@canonical.com--2004-laptop/launchpad--devel--0--patch-5
       merge from rocketfuel
    
     * david.miller@canonical.com--2004-laptop/launchpad--devel--0--patch-6
       Retrieve assignment information on bugs
    
     * david.miller@canonical.com--2004-laptop/launchpad--devel--0--patch-7
       picking up Malone schema changes from rocketfuel
    
     * david.miller@canonical.com--2004-laptop/launchpad--devel--0--patch-8
       merge from rocketfuel
    
     * stuart.bishop@canonical.com/launchpad--devel--0--patch-67
       Merge from rocketfuel
    
     * stuart.bishop@canonical.com/launchpad--devel--0--patch-68
       Migrate some Malone sample data to sampledata.sql
    
     * stuart.bishop@canonical.com/launchpad--devel--0--patch-69
       EmailAddress interface and class
    
     * stuart.bishop@canonical.com/launchpad--devel--0--patch-70
       Allow specification of sourcepackage, product and/or binarypackage when creating Bugs

--- lib/canonical/malone/rpcdaemon.py
+++ lib/canonical/malone/rpcdaemon.py
@@ -5,14 +5,64 @@ To run the server, just run this file ("rpcdaemon.py").
 
 """
 
-#from twisted.manhole import gladereactor
-#gladereactor.install()
-
 from twisted.web import xmlrpc
 from sqlobject import connectionForURI
 from canonical.arch.sqlbase import SQLBase, quote
 from canonical.database.malone import Bug
-import xmlrpclib, sqlobject
+import xmlrpclib, sqlobject, optparse
+
+class OptionHelpException(optparse.OptParseError):
+    """
+    Raised in order to pass help back to the client without exiting the
+    program.
+    """
+
+class MyOptionParser(optparse.OptionParser):
+    """
+    Subclass OptionParser so we can return the output to the client instead of
+    printing it to the console on the server
+    """
+
+    def error (self, msg):
+        """error(msg : string)
+
+        Print a usage message incorporating 'msg' to stderr and exit.
+        If you override this in a subclass, it should not return -- it
+        should either exit or raise an exception.
+        """
+        raise OptionHelpException("%s: error: %s\n%s" % (self._get_prog_name(),
+            msg, self.get_usage()))
+
+    def print_usage (self, file=None):
+        """print_usage(file : file = stdout)
+
+        Print the usage message for the current program (self.usage).
+        Any occurence of the string "%prog" in
+        self.usage is replaced with the name of the current program
+        (basename of sys.argv[0]).  Does nothing if self.usage is empty
+        or not defined.
+        """
+        raise OptionHelpException(self.get_usage())
+
+    def print_version (self, file=None):
+        """print_version(file : file = stdout)
+
+        Print the version message for this program (self.version).
+        As with print_usage(), any occurence of "%prog" in
+        self.version is replaced by the current program's name.
+        Does nothing if self.version is empty or undefined.
+        """
+        raise OptionHelpException(self.get_version())
+
+    def print_help (self, file=None):
+        """print_help(file : file = stdout)
+
+        Print an extended help message, listing all options and any
+        help text provided with them.
+        """
+        raise OptionHelpException(self.format_help())
+
+# end class MyOptionParser
 
 class RPCDaemon(xmlrpc.XMLRPC):
     """XMLRPC daemon handler.
@@ -20,15 +70,15 @@ class RPCDaemon(xmlrpc.XMLRPC):
     """
     
     def __init__(self):
-	xmlrpc.XMLRPC.__init__(self)
+        xmlrpc.XMLRPC.__init__(self)
         SQLBase.initZopeless(connectionForURI('postgres:///launchpad_test'))
-	self.debug=1
+        self.debug=1
 
     def xmlrpc_runcommand(self, cmd, locale, packedargs):
         """Run a command."""
-	if self.debug:
-            print ">>> runcommand called with cmd=%s, locale=%s, args=%s" % (cmd,
-                locale, packedargs)
+        if self.debug:
+            print ">>> runcommand called with cmd=%s, locale=%s, args=%s" % (
+                cmd, locale, packedargs)
         if len(packedargs) != 2:
             if self.debug:
                 print "<<< *** Invalid argument format"
@@ -36,6 +86,8 @@ class RPCDaemon(xmlrpc.XMLRPC):
         cmd = cmd.lower().replace("-", "_")
         options = packedargs[0]
         args = packedargs[1]
+        # At this point, the user's authentication info will be in the
+        # options dictionary.  Once we care about auth, process it here.
         try:
             cmdmethod = getattr(self, "cmd_" + cmd);
         except AttributeError:
@@ -43,10 +95,25 @@ class RPCDaemon(xmlrpc.XMLRPC):
                 print "<<< *** Command not implemented"
             raise xmlrpc.Fault(-32601,"Command not implemented")
         # if we made it here, it exists. Call it.
-        result = cmdmethod(args)
+        try:
+            result = cmdmethod(args)
+        except OptionHelpException, val:
+            result = ("error", val)
+        except ValueError, val:
+            result = ("error", val)
         if self.debug:
-            print "<<< returning with result = %s" % result
+            print "<<< returning with result = %s" % (result,)
         return result
+
+    def serialize_assignments(self, bug):
+        newlist = []
+        for a in bug.sourceassignment:
+            newlist.append({
+                'product': a.product,
+                'status': a.status,
+                'severity': a.severity,
+                })
+        return newlist
 
     def serialize_bug(self, bug):
         data = {
@@ -54,8 +121,10 @@ class RPCDaemon(xmlrpc.XMLRPC):
             'datecreated': str(bug.datecreated),
             'title': bug.title,
             'description': bug.description,
-            'duplicateof': bug.duplicateof,
-        }
+            #'productassignment': list(bug.productassignment()),
+            'sourceassignment': self.serialize_assignments(bug),
+            #'duplicateof': bug.duplicateof,
+            }
         return data
 
     def serialize_buglist(self, buglist):
@@ -64,48 +133,77 @@ class RPCDaemon(xmlrpc.XMLRPC):
             newbuglist.append(self.serialize_bug(bug))
         return newbuglist
 
+    # Command definitions
+
+    # Add any new commands below this point.  They'll automatically be found
+    # if you create them.  Method name is "cmd_" followed by the command the
+    # end user will type, with any hyphens replaced by underscores.
+
     def cmd_get_field(self, args):
+        parser = MyOptionParser(prog="get-field",
+            usage="%prog FIELD BUG-ID")
+        (opts,args) = parser.parse_args(args)
         if len(args) < 2:
-            return ["error", "Not enough arguments"]
+            parser.error("Not enough arguments")
         if len(args) > 2:
-            return ["error", "Too many arguments"]
+            parser.error("Too many arguments")
         field = args[0]
         if not args[1].isdigit():
-            return ["error", "The bug ID must be a number"]
+            parser.error("The bug ID must be a number")
         id = int(args[1])
+        if self.debug:
+            print "--- id = %d" % id
         try:
             bug = Bug.get(id)
         except sqlobject.main.SQLObjectNotFound:
-            return ["error", "Bug not found"]
+            return ("error", "Bug not found")
         try:
             result = getattr(bug,field)
         except AttributeError:
-            return ["error", "Field not found"]
-	return ["result", result]
+            return ("error", "Field not found")
+        return ("result", result)
 
     def cmd_show(self, args):
+        parser = MyOptionParser(usage="%prog BUG-ID", prog="show")
+        (opts,args) = parser.parse_args(args)
         if len(args) < 1:
-            return ["error", "Too few arguments"]
+            parser.error("Not enough arguments")
         if len(args) > 1:
-            return ["error", "Too many arguments"]
+            parser.error("Too many arguments")
         if not args[0].isdigit():
-            return ["error", "The bug ID must be a number"]
-        return self.cmd_list_bugs(["id",args[0]])
+            parser.error("The bug ID must be a number")
+        return self.cmd_list_bugs(["--id",args[0]])
 
     def cmd_list_bugs(self, args):
-        #if len(args) > 0:
-        #    return ["error", "Too many arguments"]
-        #id = int(args[0])
-        field = args[0]
-        search = args[1]
-        try:
-            buglist = list(Bug.select("lower(%s) like %s" %
+        parser = MyOptionParser(prog="list-bugs",
+            usage="%prog [options]")
+        parser.add_option("--id", type="int", help="the bug ID")
+        parser.add_option("--title", type="string", help="the bug title")
+        (options,args) = parser.parse_args(args)
+        if len(args) > 0:
+            parser.error("Too many arguments")
+        fieldlist = []
+        for option in ("id","title"):
+            if getattr(options, option):
+                fieldlist.insert(-1,option)
+        if len(fieldlist) < 1:
+            parser.error("You must supply at least one search option")
+        andlist = []
+        for field in fieldlist:
+            andlist.insert(-1,"lower(%s) like %s" %
                 # lower(title) like '%foo%'
-                (field, quote("%%%s%%" % args[0].lower() ))
-                ))
+                (field, quote("%%%s%%" % getattr(options, field)))
+                )
+        wherepart = "(" + ") AND (".join(andlist) + ")"
+        try:
+            buglist = list(Bug.select(wherepart))
         except sqlobject.main.SQLObjectNotFound:
-            return ["error", "No bugs found"]
-	return ["result", self.serialize_buglist(buglist)]
+            return ("error", "No bugs found")
+        return ("result", self.serialize_buglist(buglist))
+
+        # end command definitions
+
+# end class RPCDaemon
 
 def main():
     from twisted.internet import reactor
