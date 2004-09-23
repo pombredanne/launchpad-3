e1588a080646d639fff5b568ad720034684cf32f
Canonical.com Patch Queue Manager <pqm@canonical.com>
2004-09-23 15:18:58 +0000

    fix #2025 (option to send emails from user creation script) Patches applied:
     * carlos.perello@canonical.com--2004/launchpad--devel--0--patch-183
       Merge from rocketfuel
    
     * carlos.perello@canonical.com--2004/launchpad--devel--0--patch-184
       First try to fix #1978 so the import script works
    
     * carlos.perello@canonical.com--2004/launchpad--devel--0--patch-185
       Merge from rocketfuel
    
     * carlos.perello@canonical.com--2004/launchpad--devel--0--patch-186
       Added Catalan plural forms information
    
     * daf@canonical.com--2004/launchpad--devel--0--patch-174
       add option to user creation script to send an email

--- lib/canonical/rosetta/scripts/createuser.py
+++ lib/canonical/rosetta/scripts/createuser.py
@@ -3,72 +3,110 @@
 # Copyright 2004 Canonical Ltd.  All rights reserved.
 # arch-tag: 6c618e88-b377-4ee6-8bfb-4d42fda1d378
 
-import os, popen2
+import os, popen2, smtplib
+from textwrap import wrap
 
-from zope.component.tests.placelesssetup import PlacelessSetup
-import canonical.lp
-from canonical.rosetta.sql import RosettaPerson, RosettaEmailAddress
+from canonical.lp import initZopeless
+from canonical.lp.dbschema import EmailAddressStatus
 from canonical.lp.placelessauth.encryption import SSHADigestEncryptor
+from canonical.rosetta.sql import RosettaPerson, RosettaEmailAddress
 from optparse import OptionParser
+from zope.component.tests.placelesssetup import PlacelessSetup
+
+def pwgen():
+    pwgen = popen2.Popen3('/usr/bin/pwgen -s -1', True)
+
+    # Now we wait until the command ends
+    status = pwgen.wait()
+
+    if os.WIFEXITED(status):
+        if os.WEXITSTATUS(status) == 0:
+            # The command worked
+            return pwgen.fromchild.read().strip('\n')
+        else:
+            raise RuntimeError("There was an error executing pwgen: " +
+                pwgen.childerr.read().strip('\n'))
+    else:
+        raise RuntimeError("There was an unknown error executing pwgen.")
+
+def createUser(givenName, familyName, displayName, email, password=None):
+    if password is None:
+        # If we don't get a password from command line, we generate one
+        # automaticaly.
+
+        password = pwgen()
+
+        encrypted_password = SSHADigestEncryptor().encrypt(password)
+
+    initZopeless()
+
+    # XXX: We don't check if the person already exists.
+    person = RosettaPerson(
+        givenName=givenName,
+        familyName=familyName,
+        displayName=displayName,
+        password=encrypted_password)
+
+    email = RosettaEmailAddress(
+        person=person,
+        email=email,
+        status=int(EmailAddressStatus.NEW))
+
+    return (person, password, encrypted_password)
+
+def send_email(person, address, password):
+    smtp = smtplib.SMTP('localhost')
+    paragraphs = (
+        "Hello %s," % person.displayName,
+        "A Rosetta account has been created for you. To log in, use the "
+        "email address this message was sent to as your username.",
+        "Your password is: %s" % password,
+        "Feel free to change it from within Rosetta using the preferences "
+        "page.")
+    body = "\n\n".join([ "\n".join(wrap(x)) for x in paragraphs ])
+    smtp.sendmail('launchpad@canonical.com', address, (
+        "From: Rosetta\n"
+        "To: %s <%s>\n"
+        "Subject: New Rosetta account\n"
+        "\n" % (person.displayName, address)) + body)
+    smtp.quit()
 
 if __name__ == '__main__':
     parser = OptionParser()
-    parser.add_option("-g", "--given-name", dest="given",
-                      help="Given name")
-    parser.add_option("-f", "--family-name", dest="family",
-                      help="Family name")
-    parser.add_option("-d", "--display-name", dest="display",
-                      help="Display name")
-    parser.add_option("-e", "--email", dest="email",
-                      help="Email address")
-    parser.add_option("-p", "--password", dest="password",
-                      help="Optional password")
+    parser.add_option(
+        "-g", "--given-name", dest="given", help="Given name")
+    parser.add_option(
+        "-f", "--family-name", dest="family", help="Family name")
+    parser.add_option(
+        "-d", "--display-name", dest="display", help="Display name")
+    parser.add_option(
+        "-e", "--email", dest="email", help="Email address")
+    parser.add_option(
+        "-p", "--password", dest="password", help="Optional password")
+    parser.add_option(
+        "-s", "--send-email", dest="send_email", help="Email the new user",
+        action="store_true", default=False)
     (options, args) = parser.parse_args()
 
     # If we get all needed options...
-    if options.given != None and options.family != None and \
-       options.display != None and options.email != None:
+    if not None in (options.given, options.family, options.display,
+            options.email):
+        person, password, encrypted_password = createUser(
+            givenName = options.given,
+            familyName = options.family,
+            displayName = options.display,
+            email = options.email,
+            password = options.password)
 
-        if options.password == None:
-            # If we don't get a password from command line, we generate one
-            # automaticaly.
-            pwgen = popen2.Popen3('/usr/bin/pwgen -s -1', True)
+        print "New user created."
+        print "ID: %d" % person.id
+        print "Password: " + password
+        print "Encrypted password: " + encrypted_password
 
-            # Now we wait until the command ends
-            status = pwgen.wait()
-
-            if os.WIFEXITED(status):
-                if os.WEXITSTATUS(status) == 0:
-                    # The command worked
-                    options.password = pwgen.fromchild.read()
-                    options.password = options.password.strip('\n')
-
-                else:
-                    print "There was an error executing pwgen: " + \
-                        pwgen.childerr.read()
-                    os.exit(1)
-            else:
-                print "There was an unknown error executing pwgen."
-                os.exit(1)
-
-        ssha = SSHADigestEncryptor()
-
-        passEncrypted = ssha.encrypt(options.password)
-
-        canonical.lp.initZopeless()
-
-        # XXX: We don't check if the person already exists
-        person = RosettaPerson(givenName=options.given,
-                               familyName=options.family,
-                               displayName=options.display,
-                               password=passEncrypted)
-        # XXX: Should be status=2? we know the people we add from here.
-        email = RosettaEmailAddress(person=person, email=options.email, status=1)
-
-        # XXX: Implement an email submit with all information filled so the
-        # user knows his/her password.
-        print "The password: " + options.password + " The encrypted: " + passEncrypted
-
+        if options.send_email:
+            print "Sending email to %s..." % options.email
+            send_email(person, options.email, password)
     else:
         # XXX: We should do this message more descriptive.
         print "Please, review the command line, we need more options..."
+
