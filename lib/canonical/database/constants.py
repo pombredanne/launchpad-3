1aa7459d425582ee56d313113aac319990d20a2c
Canonical.com Patch Queue Manager <pqm@canonical.com>
2004-09-30 10:36:26 +0000

    Allow SQLObject to initialize columns to the default value defined in the database Patches applied:
     * stuart.bishop@canonical.com/launchpad--devel--0--patch-199
       Allow SQLObject to initialize columns to the default value defined in the database

--- lib/canonical/database/constants.py
+++ lib/canonical/database/constants.py
@@ -2,8 +2,14 @@
 #
 # arch-tag: 10b21ab8-0009-4efd-af07-ba17c593c752
 
-class NowUTC:
+class NowUTC(object):
     def __sqlrepr__(self, dbName):
         return "CURRENT_TIMESTAMP AT TIME ZONE 'UTC'"
+UTCNOW = NowUTC() # All upper because this is the constants module
+nowUTC = UTCNOW
 
-nowUTC = NowUTC()
+class Default(object):
+    def __sqlrepr__(self, dbName):
+        return "DEFAULT"
+DEFAULT = Default()
+
