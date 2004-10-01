916620e88eb0b062eb930fac5e3479049a9d82ed
Canonical.com Patch Queue Manager <pqm@canonical.com>
2004-10-01 11:42:08 +0000

    Stub files for canonical.launchpad Patches applied:
     * andrew.bennetts@canonical.com/launchpad--big-rename--0--base-0
       tag of rocketfuel@canonical.com/launchpad--devel--0--patch-457
    
     * andrew.bennetts@canonical.com/launchpad--big-rename--0--patch-1
       Add stub files for canonical.launchpad

---
+++ lib/canonical/launchpad/database.py
@@ -0,0 +1,5 @@
+from canonical.launchpad.dandrew import *
+from canonical.launchpad.dmark import *
+from canonical.launchpad.dkiko import *
+from canonical.launchpad.dsteve import *
+from canonical.launchpad.dcelso import *
---
+++ lib/canonical/launchpad/interfaces.py
@@ -0,0 +1,5 @@
+from canonical.launchpad.iandrew import *
+from canonical.launchpad.imark import *
+from canonical.launchpad.ikiko import *
+from canonical.launchpad.isteve import *
+from canonical.launchpad.icelso import *
