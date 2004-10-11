57a3ce73610ca330b2758f82ff91feb0e4830aa6
Canonical.com Patch Queue Manager <pqm@canonical.com>
2004-10-11 22:18:05 +0000

    More lucille-related database stuff Patches applied:
     * daniel.silverstone@canonical.com--2004/launchpad--devel--0--patch-27
       status columns for the publishing tables
    
     * daniel.silverstone@canonical.com--2004/launchpad--devel--0--patch-28
       Merge from rocketfuel@canonical.com/launchpad--devel--0

--- database/schema/pending/dsilvers-patch-publishing+dates.sql
+++ database/schema/pending/dsilvers-patch-publishing+dates.sql
@@ -30,3 +30,29 @@ COMMENT ON COLUMN SourcepackagePublishing.scheduleddeletiondate IS 'This column 
 
 COMMENT ON COLUMN SourcepackagePublishing.datepublished IS 'This column contains the timestamp at which point the Build progressed from a pending publication to being published in the respective DistroRelease';
 COMMENT ON COLUMN SourcepackagePublishing.scheduleddeletiondate IS 'This column is only used when the the publishing record is PendingRemoval. It indicates the earliest time that this record can be removed. When a publishing record is removed, the files it embodies are made candidates for removal from the pool.';
+
+/*
+ * The 'uploadstatus' column name is crack in SourcepackagePublishing
+ */
+
+ALTER TABLE SourcepackagePublishing
+    RENAME COLUMN uploadstatus TO status;
+
+/*
+ * And PackagePublishing needs a similar status column
+ */
+
+ALTER TABLE PackagePublishing
+    ADD COLUMN status integer;
+
+ALTER TABLE PackagePublishing
+    ALTER COLUMN status SET NOT NULL;
+
+/*
+ * Provide comments about the status columns
+ */
+
+COMMENT ON COLUMN SourcepackagePublishing.status IS 'This column contains the status of the publishing record. The valid states are described in dbschema.py in PackagePublishingStatus. Example states are "Pending" and "Published"';
+
+COMMENT ON COLUMN PackagePublishing.status IS 'This column contains the status of the publishing record. The valid states are described in dbschema.py in PackagePublishingStatus. Example states are "Pending" and "Published"';
+
