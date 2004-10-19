abbf6687bddf59381f5f69fa18eb07fb44c9732b
Canonical.com Patch Queue Manager <pqm@canonical.com>
2004-10-19 20:14:26 +0000

    Make lucille tests run as part of the main launchpad unit tests.  Reformat lucille test code to pep8.  Change utf8 encoded file into ascii encoding. Patches applied:
     * steve.alexander@canonical.com/launchpad--devel--0--patch-104
       added __init__.py to lucille's tests directory that adds a function to get paths relative to the data directory.  Fixed tests in test_signedfiles to use this function.  Reformatted code to pep8.
    
     * steve.alexander@canonical.com/launchpad--devel--0--patch-105
       Fixed test_tagfiles of lucille to run with testrunner.  Also reformatted to pep8.
    
     * steve.alexander@canonical.com/launchpad--devel--0--patch-106
       make test_utils.py work from any working directory.  Recode the file as ascii, so that it is obvious to the naked what is being tested.

--- lib/canonical/lucille/tests/test_tagfiles.py
+++ lib/canonical/lucille/tests/test_tagfiles.py
@@ -1,89 +1,118 @@
 #!/usr/bin/env python
 
+# Copyright 2004 Canonical Ltd.  All rights reserved.
+#
 # arch-tag: 52e0c871-49a3-4186-beb8-9817d02d5465
 
 import unittest
 import sys
 import shutil
+from canonical.lucille.tests import datadir
 
 class TestTagFiles(unittest.TestCase):
+
     def testImport(self):
         """canonical.lucille.TagFiles should be importable"""
-        from canonical.lucille.TagFiles import TagFile, \
-             TagFileParseError, parse_tagfile
+        from canonical.lucille.TagFiles import TagFile
+        from canonical.lucille.TagFiles import TagFileParseError
+        from canonical.lucille.TagFiles import parse_tagfile
 
     def testTagFileOnSingular(self):
-        """canonical.lucille.TagFiles.TagFile should parse a singular stanza"""
+        """canonical.lucille.TagFiles.TagFile should parse a singular stanza
+        """
         from canonical.lucille.TagFiles import TagFile
-        f = TagFile( file("data/singular-stanza", "r") )
+        f = TagFile(file(datadir("singular-stanza"), "r"))
         seenone = False
         for stanza in f:
-            self.assertEquals( seenone, False )
+            self.assertEquals(seenone, False)
             seenone = True
-            self.assertEquals( "Format" in stanza, True )
-            self.assertEquals( "Source" in stanza, True )
-            self.assertEquals( "FooBar" in stanza, False )
+            self.assertEquals("Format" in stanza, True)
+            self.assertEquals("Source" in stanza, True)
+            self.assertEquals("FooBar" in stanza, False)
 
     def testTagFileOnSeveral(self):
         """canonical.lucille.TagFiles.TagFile should parse multiple stanzas"""
         from canonical.lucille.TagFiles import TagFile
-        f = TagFile( file("data/multiple-stanzas", "r") )
+        f = TagFile(file(datadir("multiple-stanzas"), "r"))
         seen = 0
         for stanza in f:
             seen += 1
-            self.assertEquals( "Format" in stanza, True )
-            self.assertEquals( "Source" in stanza, True )
-            self.assertEquals( "FooBar" in stanza, False )
-        self.assertEquals( seen > 1, True )
+            self.assertEquals("Format" in stanza, True)
+            self.assertEquals("Source" in stanza, True)
+            self.assertEquals("FooBar" in stanza, False)
+        self.assertEquals(seen > 1, True)
 
     def testCheckParseChangesOkay(self):
-        """canonical.lucille.TagFiles.parse_tagfile should work on a good changes file"""
+        """canonical.lucille.TagFiles.parse_tagfile should work on a good
+           changes file
+        """
         from canonical.lucille.TagFiles import parse_tagfile
-        p = parse_tagfile( "data/good-signed-changes" )
+        p = parse_tagfile(datadir("good-signed-changes"))
 
     def testCheckParseBadChangesRaises(self):
-        """canonical.lucille.TagFiles.parse_chantges should raise TagFileParseError on failure"""
-        from canonical.lucille.TagFiles import parse_tagfile, TagFileParseError
-        self.assertRaises( TagFileParseError,
-                           parse_tagfile, "data/badformat-changes", 1 )
-        
+        """canonical.lucille.TagFiles.parse_chantges should raise
+           TagFileParseError on failure
+        """
+        from canonical.lucille.TagFiles import parse_tagfile
+        from canonical.lucille.TagFiles import TagFileParseError
+        self.assertRaises(TagFileParseError,
+                          parse_tagfile, datadir("badformat-changes"), 1)
+
     def testCheckParseEmptyChangesRaises(self):
-        """canonical.lucille.TagFiles.parse_chantges should raise TagFileParseError on empty"""
-        from canonical.lucille.TagFiles import parse_tagfile, TagFileParseError
-        self.assertRaises( TagFileParseError,
-                           parse_tagfile, "data/empty-file", 1 )
-        
+        """canonical.lucille.TagFiles.parse_chantges should raise
+           TagFileParseError on empty
+        """
+        from canonical.lucille.TagFiles import parse_tagfile
+        from canonical.lucille.TagFiles import TagFileParseError
+        self.assertRaises(TagFileParseError,
+                          parse_tagfile, datadir("empty-file"), 1)
+
     def testCheckParseMalformedSigRaises(self):
-        """canonical.lucille.TagFiles.parse_chantges should raise TagFileParseError on malformed signatures"""
-        from canonical.lucille.TagFiles import parse_tagfile, TagFileParseError
-        self.assertRaises( TagFileParseError,
-                           parse_tagfile, "data/malformed-sig-changes", 1 )
-        
+        """canonical.lucille.TagFiles.parse_chantges should raise
+           TagFileParseError on malformed signatures
+        """
+        from canonical.lucille.TagFiles import parse_tagfile
+        from canonical.lucille.TagFiles import TagFileParseError
+        self.assertRaises(TagFileParseError,
+                          parse_tagfile, datadir("malformed-sig-changes"), 1)
+
     def testCheckParseMalformedMultilineRaises(self):
-        """canonical.lucille.TagFiles.parse_chantges should raise TagFileParseError on malformed continuation lines"""
-        from canonical.lucille.TagFiles import parse_tagfile, TagFileParseError
-        self.assertRaises( TagFileParseError,
-                           parse_tagfile, "data/bad-multiline-changes", 1 )
-        
+        """canonical.lucille.TagFiles.parse_chantges should raise
+           TagFileParseError on malformed continuation lines"""
+        from canonical.lucille.TagFiles import parse_tagfile
+        from canonical.lucille.TagFiles import TagFileParseError
+        self.assertRaises(TagFileParseError,
+                          parse_tagfile, datadir("bad-multiline-changes"), 1)
+
     def testCheckParseUnterminatedSigRaises(self):
-        """canonical.lucille.TagFiles.parse_chantges should raise TagFileParseError on unterminated signatures"""
-        from canonical.lucille.TagFiles import parse_tagfile, TagFileParseError
-        self.assertRaises( TagFileParseError,
-                           parse_tagfile, "data/unterminated-sig-changes", 1 )
+        """canonical.lucille.TagFiles.parse_chantges should raise
+           TagFileParseError on unterminated signatures
+        """
+        from canonical.lucille.TagFiles import parse_tagfile
+        from canonical.lucille.TagFiles import TagFileParseError
+        self.assertRaises(TagFileParseError,
+                          parse_tagfile,
+                          datadir("unterminated-sig-changes"),
+                          1)
 
     def testParseChangesNotVulnerableToArchExploit(self):
-        """canonical.lucille.TagFiles.parse_tagfile should not be vulnerable to tags outside of the signed portion"""
+        """canonical.lucille.TagFiles.parse_tagfile should not be vulnerable
+           to tags outside of the signed portion
+        """
         from canonical.lucille.TagFiles import parse_tagfile
-        tf = parse_tagfile( "data/changes-with-exploit-top" )
-        self.assertRaises( KeyError, tf.__getitem__, "you" )
-        tf = parse_tagfile( "data/changes-with-exploit-bottom" )
-        self.assertRaises( KeyError, tf.__getitem__, "you" )
-        
+        tf = parse_tagfile(datadir("changes-with-exploit-top"))
+        self.assertRaises(KeyError, tf.__getitem__, "you")
+        tf = parse_tagfile(datadir("changes-with-exploit-bottom"))
+        self.assertRaises(KeyError, tf.__getitem__, "you")
 
-def main(argv):
+def test_suite():
     suite = unittest.TestSuite()
     loader = unittest.TestLoader()
     suite.addTest(loader.loadTestsFromTestCase(TestTagFiles))
+    return suite
+
+def main(argv):
+    suite = test_suite()
     runner = unittest.TextTestRunner(verbosity = 2)
     if not runner.run(suite).wasSuccessful():
         return 1
--- lib/canonical/lucille/tests/test_utils.py
+++ lib/canonical/lucille/tests/test_utils.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+
+# Copyright 2004 Canonical Ltd.  All rights reserved.
+#
 # arch-tag: 90e6eb79-83a2-47e8-9f8b-3c687079c923
 
 import unittest
 import sys
 import shutil
+from canonical.lucille.tests import datadir
+
 
 class TestUtilities(unittest.TestCase):
+
     def testImport(self):
         """canonical.lucille.utils should be importable"""
         import canonical.lucille.utils
@@ -22,99 +27,108 @@ class TestUtilities(unittest.TestCase):
                           prefix_multi_line_string("foo\n\nbar", "A:", 1))
 
     def testExtractComponent(self):
-        """canonical.lucille.utils.extract_component_from_section should work"""
+        """canonical.lucille.utils.extract_component_from_section should work
+        """
         from canonical.lucille.utils import extract_component_from_section
 
-        (sect,comp) = extract_component_from_section( "libs" )
-        self.assertEquals( sect, "libs" )
-        self.assertEquals( comp, "main" )
-        
-        (sect,comp) = extract_component_from_section( "restricted/libs" )
-        self.assertEquals( sect, "libs" )
-        self.assertEquals( comp, "restricted" )
-        
-        (sect,comp) = extract_component_from_section( "libs", "multiverse" )
-        self.assertEquals( sect, "libs" )
-        self.assertEquals( comp, "multiverse" )
-        
-        (sect,comp) = extract_component_from_section( "restricted/libs", "multiverse" )
-        self.assertEquals( sect, "libs" )
-        self.assertEquals( comp, "restricted" )
+        (sect, comp) = extract_component_from_section("libs")
+        self.assertEquals(sect, "libs")
+        self.assertEquals(comp, "main")
+
+        (sect, comp) = extract_component_from_section("restricted/libs")
+        self.assertEquals(sect, "libs")
+        self.assertEquals(comp, "restricted")
+
+        (sect, comp) = extract_component_from_section("libs", "multiverse")
+        self.assertEquals(sect, "libs")
+        self.assertEquals(comp, "multiverse")
+
+        (sect, comp) = extract_component_from_section("restricted/libs",
+                                                      "multiverse")
+        self.assertEquals(sect, "libs")
+        self.assertEquals(comp, "restricted")
 
     def testBuildFileListFromChanges(self):
-        """canonical.lucille.utils.build_file_list should be capable of reading changes files"""
+        """canonical.lucille.utils.build_file_list should be capable of
+           reading changes files
+        """
         from canonical.lucille.utils import build_file_list
         from canonical.lucille.TagFiles import parse_tagfile
 
-        ch = parse_tagfile( "data/good-signed-changes" )
-        fl = build_file_list( ch )
-        self.assertEquals( "abiword_2.0.10-1.2_mips.deb" in fl, True )
+        ch = parse_tagfile(datadir("good-signed-changes"))
+        fl = build_file_list(ch)
+        self.assertEquals("abiword_2.0.10-1.2_mips.deb" in fl, True)
 
     def testFixMaintainerOkay(self):
-        """canonical.lucille.utils.fix_maintainer should parse correct values"""
+        """canonical.lucille.utils.fix_maintainer should parse correct values
+        """
         from canonical.lucille.utils import fix_maintainer
         cases = (
-            ( "Noèl Köthe <noel@debian.org>",
-              "Noèl Köthe <noel@debian.org>",
-              "=?utf-8?b?Tm/DqGwgS8O2dGhl?= <noel@debian.org>",
-              "Noèl Köthe",
-              "noel@debian.org" ),
-            
-            ( "No�l K�the <noel@debian.org>",
-              "Noèl Köthe <noel@debian.org>",
-              "=?iso-8859-1?q?No=E8l_K=F6the?= <noel@debian.org>",
-              "Noèl Köthe",
-              "noel@debian.org" ),
-            ( "James Troup <james@nocrew.org>",
-              "James Troup <james@nocrew.org>",
-              "James Troup <james@nocrew.org>",
-              "James Troup",
-              "james@nocrew.org" ),
+            ("No\xc3\xa8l K\xc3\xb6the <noel@debian.org>",
+             "No\xc3\xa8l K\xc3\xb6the <noel@debian.org>",
+             "=?utf-8?b?Tm/DqGwgS8O2dGhl?= <noel@debian.org>",
+             "No\xc3\xa8l K\xc3\xb6the",
+             "noel@debian.org"),
 
-            ( "James J. Troup <james@nocrew.org>",
-              "james@nocrew.org (James J. Troup)",
-              "james@nocrew.org (James J. Troup)",
-              "James J. Troup",
-              "james@nocrew.org" ),
+            ("No\xe8l K\xf6the <noel@debian.org>",
+             "No\xc3\xa8l K\xc3\xb6the <noel@debian.org>",
+             "=?iso-8859-1?q?No=E8l_K=F6the?= <noel@debian.org>",
+             "No\xc3\xa8l K\xc3\xb6the",
+             "noel@debian.org"),
 
-            ( "James J, Troup <james@nocrew.org>",
-              "james@nocrew.org (James J, Troup)",
-              "james@nocrew.org (James J, Troup)",
-              "James J, Troup",
-              "james@nocrew.org" ),
+            ("James Troup <james@nocrew.org>",
+             "James Troup <james@nocrew.org>",
+             "James Troup <james@nocrew.org>",
+             "James Troup",
+             "james@nocrew.org"),
 
-            ( "james@nocrew.org",
-              " <james@nocrew.org>",
-              " <james@nocrew.org>",
-              "",
-              "james@nocrew.org" ),
+            ("James J. Troup <james@nocrew.org>",
+             "james@nocrew.org (James J. Troup)",
+             "james@nocrew.org (James J. Troup)",
+             "James J. Troup",
+             "james@nocrew.org"),
 
-              ( "<james@nocrew.org>",
-                " <james@nocrew.org>",
-                " <james@nocrew.org>",
-                "",
-                "james@nocrew.org" ),
+            ("James J, Troup <james@nocrew.org>",
+             "james@nocrew.org (James J, Troup)",
+             "james@nocrew.org (James J, Troup)",
+             "James J, Troup",
+             "james@nocrew.org"),
 
-              ( "Cris van Pelt <\"Cris van Pelt\"@tribe.eu.org>",
-                "Cris van Pelt <\"Cris van Pelt\"@tribe.eu.org>",
-                "Cris van Pelt <\"Cris van Pelt\"@tribe.eu.org>",
-                "Cris van Pelt",
-                "\"Cris van Pelt\"@tribe.eu.org" ) )
+            ("james@nocrew.org",
+             " <james@nocrew.org>",
+             " <james@nocrew.org>",
+             "",
+             "james@nocrew.org"),
+
+            ("<james@nocrew.org>",
+             " <james@nocrew.org>",
+             " <james@nocrew.org>",
+             "",
+             "james@nocrew.org"),
+
+            ("Cris van Pelt <\"Cris van Pelt\"@tribe.eu.org>",
+             "Cris van Pelt <\"Cris van Pelt\"@tribe.eu.org>",
+             "Cris van Pelt <\"Cris van Pelt\"@tribe.eu.org>",
+             "Cris van Pelt",
+             "\"Cris van Pelt\"@tribe.eu.org")
+             )
 
         for case in cases:
             (a, b, c, d) = fix_maintainer(case[0])
-            self.assertEquals( case[1], a )
-            self.assertEquals( case[2], b )
-            self.assertEquals( case[3], c )
-            self.assertEquals( case[4], d )
+            self.assertEquals(case[1], a)
+            self.assertEquals(case[2], b)
+            self.assertEquals(case[3], c)
+            self.assertEquals(case[4], d)
 
     def testFixMaintainerRaises(self):
-        """canonical.lucille.utils.fix_maintainer should raise on incorrect values"""
+        """canonical.lucille.utils.fix_maintainer should raise on incorrect
+           values
+        """
         from canonical.lucille.utils import fix_maintainer, ParseMaintError
         cases = (
             "James Troup",
             "James Troup <james>",
-            "James Troup <james@nocrew.org" )
+            "James Troup <james@nocrew.org")
         for case in cases:
             try:
                 fix_maintainer(case)
@@ -122,10 +136,14 @@ class TestUtilities(unittest.TestCase):
             except ParseMaintError:
                 pass
 
-def main(argv):
+def test_suite():
     suite = unittest.TestSuite()
     loader = unittest.TestLoader()
     suite.addTest(loader.loadTestsFromTestCase(TestUtilities))
+    return suite
+
+def main(argv):
+    suite = test_suite()
     runner = unittest.TextTestRunner(verbosity = 2)
     if not runner.run(suite).wasSuccessful():
         return 1
