d305f09a07488c697e11e69245fbc0bc390c636f
Canonical.com Patch Queue Manager <pqm@canonical.com>
2004-10-25 23:53:58 +0000

    add docs for new add/edit form buffing Patches applied:
     * mark.shuttleworth@canonical.com/launchpad--devel--0--patch-319
       prettify add / edit forms
    
     * mark.shuttleworth@canonical.com/launchpad--devel--0--patch-320
       merge rocketfuel

--- lib/canonical/launchpad/validators/name.py
+++ lib/canonical/launchpad/validators/name.py
@@ -1,11 +1,11 @@
 
 def valid_name(name):
     import re
-    name = args[0]
     pat = r"^[a-z0-9][a-z0-9\\+\\.\\-]+$"
-    if name is None or re.match(pat, name):
+    if re.match(pat, name):
         return True
     return False
+    
 valid_name.sql_signature = [
     ('name', 'text'),
     ]
