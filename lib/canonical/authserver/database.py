1006b9cc10c424dd75e74518a9f2d0d9f08790f8
Canonical.com Patch Queue Manager <pqm@canonical.com>
2004-11-02 01:05:03 +0000

    [authserver] JOIN explicitly, and use lower(EmailAddress.email) Patches applied:
     * andrew.bennetts@canonical.com/launchpad--devel--0--patch-224
       Merge from rocketfuel
    
     * andrew.bennetts@canonical.com/launchpad--devel--0--patch-225
       JOIN explicitly, and use lower(EmailAddress.email)
    
     * andrew.bennetts@canonical.com/launchpad--devel--0--patch-226
       Minor cosmetic change to query assembly

--- lib/canonical/authserver/database.py
+++ lib/canonical/authserver/database.py
@@ -171,12 +171,12 @@ class DatabaseUserDetailsStorage(object):
         query = (
             "SELECT Person.id, Person.displayname, Person.password "
             "FROM Person "
+            "INNER JOIN EmailAddress ON EmailAddress.person = Person.id "
         )
         transaction.execute(
             query + 
-            "WHERE EmailAddress.email = '%s' "
-            "AND EmailAddress.person = Person.id"
-            % (str(loginID).replace("'", "''"),)
+            "WHERE lower(EmailAddress.email) = '%s' "
+            % (str(loginID).lower().replace("'", "''"),)
         )
         
         row = transaction.fetchone()
