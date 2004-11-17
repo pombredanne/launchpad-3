0852a0971466867254ca63c4ae89cefb07ed1123
Canonical.com Patch Queue Manager <pqm@canonical.com>
2004-11-17 16:28:41 +0000

    Malone polish Patches applied:
     * stuart.bishop@canonical.com/launchpad--devel--0--patch-408
       Add default view to productbugassignment and sourcepackagebugassignment to fix broken links in breadcrumbs
    
     * stuart.bishop@canonical.com/launchpad--devel--0--patch-409
       Switch of decoy hyperlinks in bugassignment listings
    
     * stuart.bishop@canonical.com/launchpad--devel--0--patch-410
       Add default values to ProductBugAssignment

--- lib/canonical/launchpad/interfaces/bugassignment.py
+++ lib/canonical/launchpad/interfaces/bugassignment.py
@@ -17,9 +17,12 @@ class IProductBugAssignment(Interface):
     id = Int(title=_('ID'), required=True, readonly=True)
     bug = Int(title=_('Bug ID'), required=True, readonly=True)
     product = Choice(title=_('Product'), required=True, vocabulary='Product')
-    bugstatus = Choice(title=_('Bug Status'), vocabulary='BugStatus')
-    priority = Choice(title=_('Priority'), vocabulary='BugPriority')
-    severity = Choice(title=_('Severity'), vocabulary='BugSeverity')
+    bugstatus = Choice(title=_('Bug Status'), vocabulary='BugStatus',
+            default=int(dbschema.BugAssignmentStatus.NEW))
+    priority = Choice(title=_('Priority'), vocabulary='BugPriority',
+            default=int(dbschema.BugPriority.MEDIUM))
+    severity = Choice(title=_('Severity'), vocabulary='BugSeverity',
+            default=int(dbschema.BugSeverity.NORMAL))
     assignee = Choice(title=_('Assignee'), required=False, vocabulary='Person')
     datecreated = Datetime(title=_('Date Created'), required=True,
                            readonly=True)
