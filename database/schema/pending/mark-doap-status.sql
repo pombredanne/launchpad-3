e56ba849067623616ece27b4b09664b953b769c1
Canonical.com Patch Queue Manager <pqm@canonical.com>
2004-11-24 10:45:32 +0000

    corrected doap status patch Patches applied:
     * mark.shuttleworth@canonical.com/launchpad--devel--0--patch-433
       merge rocketfuel
    
     * mark.shuttleworth@canonical.com/launchpad--devel--0--patch-434
       fix up doap status patch for active/reviewed flags
    
     * mark.shuttleworth@canonical.com/launchpad--devel--0--patch-435
       fix template link to baz branch details
    
     * mark.shuttleworth@canonical.com/launchpad--devel--0--patch-436
       fix name of field in doap status patch

--- database/schema/pending/mark-doap-status.sql
+++ database/schema/pending/mark-doap-status.sql
@@ -5,19 +5,35 @@
   results etc, but in time that may happen.
 */
 
-ALTER TABLE Project ADD COLUMN status integer;
-ALTER TABLE Project ALTER COLUMN status SET DEFAULT 1;
-UPDATE Project SET status = DEFAULT WHERE status IS NULL;
-ALTER TABLE Project ALTER COLUMN status SET NOT NULL;
+ALTER TABLE Project ADD COLUMN reviewed boolean;
+ALTER TABLE Project ALTER COLUMN reviewed SET DEFAULT False;
+UPDATE Project SET reviewed = False WHERE reviewed IS NULL;
+ALTER TABLE Project ALTER COLUMN reviewed SET NOT NULL;
 
-COMMENT ON COLUMN Project.status IS 'An indicator of the status of the Project, see dbschema.ProjectStatus';
+COMMENT ON COLUMN Project.reviewed IS 'Whether or not someone at Canonical has reviewed this project.';
 
 
-ALTER TABLE Product ADD COLUMN status integer;
-ALTER TABLE Product ALTER COLUMN status SET DEFAULT 1;
-UPDATE Product SET status = DEFAULT WHERE status IS NULL;
-ALTER TABLE Product ALTER COLUMN status SET NOT NULL;
+ALTER TABLE Project ADD COLUMN active boolean;
+ALTER TABLE Project ALTER COLUMN active SET DEFAULT True;
+UPDATE Project SET active = True WHERE active IS NULL;
+ALTER TABLE Project ALTER COLUMN active SET NOT NULL;
 
-COMMENT ON COLUMN Product.status IS 'An indicator of the status of the Product, see dbschema.ProductStatus';
+COMMENT ON COLUMN Project.active IS 'Whether or not this project should be considered active.';
+
+
+ALTER TABLE Product ADD COLUMN reviewed boolean;
+ALTER TABLE Product ALTER COLUMN reviewed SET DEFAULT False;
+UPDATE Product SET reviewed = False WHERE reviewed IS NULL;
+ALTER TABLE Product ALTER COLUMN reviewed SET NOT NULL;
+
+COMMENT ON COLUMN Product.reviewed IS 'Whether or not someone at Canonical has reviewed this product.';
+
+
+ALTER TABLE Product ADD COLUMN active boolean;
+ALTER TABLE Product ALTER COLUMN active SET DEFAULT True;
+UPDATE Product SET active = True WHERE active IS NULL;
+ALTER TABLE Product ALTER COLUMN active SET NOT NULL;
+
+COMMENT ON COLUMN Product.active IS 'Whether or not this product should be considered active.';
 
 
