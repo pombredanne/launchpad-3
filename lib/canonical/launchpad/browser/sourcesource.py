a597a04daf0cfd9368ff79b10fd6f2aff6c8f94a
Canonical.com Patch Queue Manager <pqm@canonical.com>
2004-12-06 20:09:28 +0000

    Some cleanup on launchpad.browser.sourcesource Patches applied:
     * guilherme.salgado@canonical.com/launchpad--devel--0--patch-28
       Some cleanup on launchpad.browser.sourcesource

--- lib/canonical/launchpad/browser/sourcesource.py
+++ lib/canonical/launchpad/browser/sourcesource.py
@@ -11,55 +11,43 @@ class SourceSourceView(object):
         self.form = request.form
 
     def edit(self):
-        if not self.request.form.get("Update", None)=="Update Upstream Source":
+        if not self.request.form.get("Update", None) == "Update Upstream Source":
             return
         if not self.request.method == "POST":
             return
-        formdata = {}
         #
         # Extract the form data
         #
-        title = self.form.get('title', None)
-        description = self.form.get('description', None)
-        cvsroot = self.form.get('cvsroot', None)
-        cvsmodule = self.form.get('cvsmodule', None)
-        cvstarfileurl = self.form.get('cvstarfileurl', None)
-        cvsbranch = self.form.get('cvsbranch', None)
-        svnrepository = self.form.get('svnrepository', None)
-        releaseroot = self.form.get('releaseroot', None)
-        releaseverstyle = self.form.get('releaseverstyle', None)
-        releasefileglob = self.form.get('releasefileglob', None)
-        newarchive = self.form.get('newarchive', None)
-        archversion = self.form.get('newbranchcategory', None)
-        newbranchcategory = self.form.get('newbranchcategory', None)
-        newbranchbranch = self.form.get('newbranchbranch', None)
-        newbranchversion = self.form.get('newbranchversion', None)
-        product = self.form.get('product', None)
-        if title: self.context.title = title
-        if description: self.context.description = description
-        if cvsroot: self.context.cvsroot = cvsroot
-        if cvsmodule: self.context.cvsmodule = cvsmodule
-        if cvstarfileurl: self.context.cvstarfileurl = cvstarfileurl
-        if cvsbranch: self.context.cvsbranch = cvsbranch
-        if svnrepository: self.context.svnrepository = svnrepository
-        if releaseroot: self.context.releaseroot = releaseroot
-        if releaseverstyle: self.context.releaseverstyle = releaseverstyle
-        if releasefileglob: self.context.releasefileglob = releasefileglob
-        if newarchive: self.context.newarchive = newarchive
-        if newbranchcategory: self.context.newbranchcategory = newbranchcategory
-        if newbranchbranch: self.context.newbranchbranch = newbranchbranch
-        if newbranchversion: self.context.newbranchversion = newbranchversion
+
+        fields = ["title"
+                  "description"
+                  "cvsroot"
+                  "cvsmodule"
+                  "cvstarfileurl"
+                  "cvsbranch"
+                  "svnrepository"
+                  "releaseroot"
+                  "releaseverstyle"
+                  "releasefileglob"
+                  "newarchive"
+                  "archversion"
+                  "newbranchcategory"
+                  "newbranchbranch"
+                  "newbranchversion"]
+        for f in fields:
+            v = self.form.get(f, None)
+            if v is not None:
+                setattr(self.context, f, v)
         if self.form.get('syncCertified', None):
             if not self.context.syncCertified():
                 self.context.certifyForSync()
         if self.form.get('autoSyncEnabled', None):
             if not self.context.autoSyncEnabled():
                 self.context.enableAutoSync()
-        newurl = None
+        product = self.form.get('product', None)
         if product and self.context.canChangeProduct():
             self.context.changeProduct(product)
             newurl='../../../../' + self.context.product.project.name + "/" + self.context.product.name
-        if newurl:
             self.request.response.redirect(newurl)
 
 
