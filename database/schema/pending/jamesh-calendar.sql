4382f31a91c24de3321b27914c74937a1b4c4810
James Henstridge <james.henstridge@canonical.com>
2004-12-13 20:25:05 +0000

    first parts of calendar db schema and sqlobjects * database/schema/pending/jamesh-calendar.sql: the SQL schema changes * lib/canonical/launchpad/database/calendar.py: the SQL objects, minus   any interfaces

---
+++ database/schema/pending/jamesh-calendar.sql
@@ -0,0 +1,52 @@
+
+SET client_min_messages TO error;
+
+CREATE TABLE Calendar (
+    id serial NOT NULL PRIMARY KEY,
+    owner integer NOT NULL,
+    title text NOT NULL,
+    revision integer NOT NULL
+);
+
+CREATE TABLE CalendarSubscription (
+    id serial NOT NULL PRIMARY KEY,
+    person integer NOT NULL,
+    calendar integer NOT NULL
+);
+
+CREATE TABLE CalendarEvent (
+    id serial NOT NULL PRIMARY KEY,
+    unique_id varchar(255) NOT NULL,
+    calendar integer NOT NULL,
+    dtstart timestamp without time zone NOT NULL,
+    duration interval NOT NULL,
+    location text NOT NULL,
+    recurrence varchar(10) NOT NULL,
+    count integer, /* if count is not positive, use until */
+    until timestamp without time zone, /* until is set if recurrence rule set */
+    exceptions text,
+    interval integer,
+    rec_list text,
+
+    CONSTRAINT until_set_if_recurs CHECK (recurrence='' OR until IS NOT NULL),
+    CONSTRAINT unique_unique_id UNIQUE (unique_id)
+);
+
+ALTER TABLE Calendar
+    ADD CONSTRAINT "$1" FOREIGN KEY (owner) REFERENCES Person(id);
+
+ALTER TABLE CalendarSubscription
+    ADD CONSTRAINT "$1" FOREIGN KEY (person) REFERENCES Person(id);
+ALTER TABLE CalendarSubscription
+    ADD CONSTRAINT "$2" FOREIGN KEY (calendar) REFERENCES Calendar(id);
+
+ALTER TABLE CalendarEvent
+    ADD CONSTRAINT "$1" FOREIGN KEY (calendar) REFERENCES Calendar(id);
+
+
+
+/* Add calendar column to Person table */
+
+ALTER TABLE Person ADD COLUMN calendar integer;
+ALTER TABLE Person
+    ADD CONSTRAINT "$2" FOREIGN KEY (calendar) REFERENCES Calendar(id);
---
+++ lib/canonical/launchpad/database/calendar.py
@@ -0,0 +1,32 @@
+from zope.interface import implements
+
+from sqlobject import DateTimeCol, ForeignKey, IntCol, StringCol, EnumCol
+
+from canonical.database.sqlbase import SQLBase
+
+import datetime
+
+class Calendar(SQLBase):
+    owner = ForeignKey(dbName='owner', notNull=True, foreignKey='Person')
+    title = StringCol(dbName='title', notNull=True)
+    revision = IntCol(dbName='revision', notNull=True)
+
+class CalendarSubscription(SQLBase):
+    person = ForeignKey(dbName='person', notNull=True, foreignKey='Person')
+    calendar = ForeignKey(dbName='calendar', notNull=True, foreignKey='Calendar')
+
+class CalendarEvent(SQLBase):
+    unique_id = StringCol(dbName='unique_id', notNull=True, length=255)
+    calendar = ForeignKey(dbName='calendar', notNull=True, foreignKey='Calendar')
+    dtstart = DateTimeCol(dbName='dtstart', notNull=True)
+    # actually an interval ...
+    duration = DateTimeCol(dbName='duration', notNull=True)
+    location = StringCol(dbName='location', notNull=True)
+    recurrence = EnumCol(dbName='recurrence', notNull=True,
+                         enumValues=['', 'SECONDLY', 'MINUTELY', 'HOURLY',
+                                     'DAILY', 'WEEKLY', 'MONTHLY', 'YEARLY'])
+    count = IntCol(dbName='count')
+    until = DateTimeCol(dbName='until')
+    exceptions = StringCol(dbName='exceptions')
+    interval = IntCol(dbName='interval')
+    rec_list = StringCol(dbName='rec_list')
