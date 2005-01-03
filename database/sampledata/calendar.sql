a2b2e7c61eff52671d7099b31eafe85624c9cdc5
James Henstridge <james.henstridge@canonical.com>
2005-01-03 04:11:03 +0000

    update sample data to add a few events

--- database/sampledata/calendar.sql
+++ database/sampledata/calendar.sql
@@ -2,9 +2,25 @@
 -- update pg_ts_cfg set locale='en_AU.UTF-8' where ts_name = 'default';
 
 INSERT INTO Calendar (owner, title, revision)
-    VALUES (16, 'Foo Bar', 0);
+    VALUES ((SELECT id from Person WHERE displayname = 'Sample Person'),
+            'Foo Bar', 0);
 
-UPDATE Person SET calendar = (SELECT id FROM Calendar WHERE title = 'Foo Bar') WHERE id = 16;
+UPDATE Person SET calendar = (SELECT id FROM Calendar WHERE title = 'Foo Bar')
+    WHERE id = (SELECT id from Person WHERE displayname = 'Sample Person');
 
 INSERT INTO CalendarSubscription (person, calendar)
-    VALUES (16, (SELECT id FROM Calendar WHERE title = 'Foo Bar'));
+    VALUES ((SELECT id from Person WHERE displayname = 'Sample Person'),
+            (SELECT id FROM Calendar WHERE title = 'Foo Bar'));
+
+INSERT INTO CalendarEvent (unique_id, calendar, dtstart, duration,
+                           title, location, recurrence)
+    VALUES ('sample-id-1', (SELECT id FROM Calendar WHERE title = 'Foo Bar'),
+            '2005-01-01 08:00:00', '01:00:00', 'Event 1', 'Location', '');
+INSERT INTO CalendarEvent (unique_id, calendar, dtstart, duration,
+                           title, location, recurrence)
+    VALUES ('sample-id-2', (SELECT id FROM Calendar WHERE title = 'Foo Bar'),
+            '2005-01-01 10:00:00', '01:00:00', 'Event 2', 'Location', '');
+INSERT INTO CalendarEvent (unique_id, calendar, dtstart, duration,
+                           title, location, recurrence)
+    VALUES ('sample-id-3', (SELECT id FROM Calendar WHERE title = 'Foo Bar'),
+            '2005-01-02 08:00:00', '01:00:00', 'Event 1', 'Location', '');
