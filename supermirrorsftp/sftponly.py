bc2fa4888ff6c759d42a8755a4ef1e1dd7524b1e
Andrew Bennetts <andrew.bennetts@canonical.com>
2005-01-07 09:02:27 +0000

    Working prototype -- authenticates from launchpad, SFTP works, shell access doesn't

--- supermirrorsftp/sftponly.py
+++ supermirrorsftp/sftponly.py
@@ -1,11 +1,12 @@
 from canonical.authserver.client.sshkeys import TwistedAuthServer
 
-from twisted.conch import avatar
+from twisted.conch import avatar, unix
 from twisted.conch.ssh import session, filetransfer
 from twisted.conch.ssh import factory, userauth, connection
 from twisted.conch.checkers import SSHPublicKeyDatabase
 from twisted.cred.checkers import ICredentialsChecker
 from twisted.cred.portal import IRealm
+from twisted.python import components
 
 from zope.interface import implements
 import binascii
@@ -17,17 +18,43 @@ class SubsystemOnlySession(session.SSHSession, object):
         # Get out the big hammer :)
         # (I'm too lazy to override all the different request_ methods
         # individually, or write an ISession adapter to give the same effect.)
-        if name.startswith('request') and name != 'request_subsystem':
+        if name.startswith('request_') and name != 'request_subsystem':
             raise AttributeError, name
         return object.__getattribute__(self, name)
 
 
 class SFTPOnlyAvatar(avatar.ConchUser):
-    # Set the only channel as a session that only allows requests for
-    # subsystems...
-    channelLookup = {'session': SubsystemOnlySession}
-    # ...and set the only subsystem to be SFTP.
-    subsystemLookup = {'sftp': filetransfer.FileTransferServer}
+    def __init__(self, avatarId):
+        self.avatarId = avatarId
+
+        # Set the only channel as a session that only allows requests for
+        # subsystems...
+        self.channelLookup = {'session': SubsystemOnlySession}
+        # ...and set the only subsystem to be SFTP.
+        self.subsystemLookup = {'sftp': filetransfer.FileTransferServer}
+
+    def _runAsUser(self, f, *args, **kwargs):
+        # Version of UnixConchUser._runAsUser with the setuid bits stripped out
+        # -- we don't need them.
+        try:
+            f = iter(f)
+        except TypeError:
+            f = [(f, args, kwargs)]
+        for i in f:
+            func = i[0]
+            args = len(i)>1 and i[1] or ()
+            kw = len(i)>2 and i[2] or {}
+            r = func(*args, **kw)
+        return r
+
+    def getHomeDir(self):
+        # XXX: the base should be configurable!
+        return '/tmp/' + self.avatarId
+
+
+# XXX: We need to customise unix.SFTPServerForUnixConchUser, we want to restrict
+#      it a little.
+components.registerAdapter(unix.SFTPServerForUnixConchUser, SFTPOnlyAvatar, filetransfer.ISFTPServer)
 
 
 class Realm:
