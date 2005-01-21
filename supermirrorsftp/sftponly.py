1951cae15b86f03a45f34f38293a42d2f0f5d979
Andrew Bennetts <andrew.bennetts@canonical.com>
2005-01-21 08:19:52 +0000

    Restricted file access for SFTP, as per the PushMirroring spec.

--- supermirrorsftp/sftponly.py
+++ supermirrorsftp/sftponly.py
@@ -9,10 +9,11 @@ from twisted.conch.checkers import SSHPublicKeyDatabase
 from twisted.cred.checkers import ICredentialsChecker
 from twisted.cred.portal import IRealm
 from twisted.python import components
+from twisted.python.filepath import FilePath, InsecurePath
 
 from zope.interface import implements
 import binascii
-import os.path
+import os, os.path
 
 
 class SubsystemOnlySession(session.SSHSession, object):
@@ -69,9 +70,111 @@ class SFTPOnlyAvatar(avatar.ConchUser):
         return os.path.join(self.homeDirsRoot, self.avatarId)
 
 
-# XXX: We need to customise unix.SFTPServerForUnixConchUser, we want to restrict
-#      it a little.
-components.registerAdapter(unix.SFTPServerForUnixConchUser, SFTPOnlyAvatar, filetransfer.ISFTPServer)
+class SFTPServerForPushMirrorUser:
+    """This is much like unix.SFTPServerForUnixConchUser, but:
+        - doesn't allow any traversal above the home directory
+        - uid/gid can't be set
+        - symlinks cannot be made
+    """
+    # TODO: This doesn't return friendly error messages to the client when
+    #       restricted operations are attempted (they generally are sent as
+    #       "Failure").
+    implements(filetransfer.ISFTPServer)
+    def __init__(self, avatar):
+        self.avatar = avatar
+        self.homedir = FilePath(self.avatar.getHomeDir())
+
+    def _childPath(self, path):
+        if path.startswith('/'):
+            path = '.' + path
+        return self.homedir.preauthChild(path)
+
+    def gotVersion(self, otherVersion, extData):
+        # we don't support anything extra beyond standard SFTP
+        return {}
+
+    def extendedRequest(self, extendedName, extendedData):
+        # We don't implement any extensions to SFTP.
+        raise NotImplementedError
+    
+    def openFile(self, filename, flags, attrs):
+        return unix.UnixSFTPFile(self, self._childPath(filename).path, flags,
+                attrs)
+
+    def removeFile(self, filename):
+        self._childPath(filename).remove()
+
+    def renameFile(self, oldpath, newpath):
+        old = self._childPath(oldpath)
+        new = self._childPath(newpath)
+        os.rename(old.path, new.path)
+
+    def makeDirectory(self, path, attrs):
+        path = self._childPath(path).path
+        os.mkdir(path)
+        # XXX: self._setattrs(path)
+
+    def removeDir(self, path):
+        os.rmdir(self._childPath(path).path)
+
+    def openDirectory(self, path):
+        return unix.UnixSFTPDirectory(self, self._childPath(path).path)
+
+    def _getAttrs(self, s):
+        """Convert the result of os.stat/os.lstat to an SFTP attributes dict
+        
+        Ideally this would be named something more like _statToAttrs, but this
+        is required by UnixSFTPDirectory.
+        """
+        # FIXME: We probably want to give fake uid/gid details.
+        # From twisted.conch.unix.SFTPServerForUnixConchUser._getAttrs
+        return {
+            "size" : s.st_size,
+            "uid" : s.st_uid,
+            "gid" : s.st_gid,
+            "permissions" : s.st_mode,
+            "atime" : s.st_atime,
+            "mtime" : s.st_mtime
+        }
+
+    def getAttrs(self, path, followLinks):
+        path = self._childPath(path).path
+        if followLinks:
+            statFunc = os.stat
+        else:
+            statFunc = os.lstat
+        return self._getAttrs(statFunc(path))
+    
+    def setAttrs(self, path, attrs):
+        path = self._childPath(path).path
+        # We ignore the uid and gid attributes!
+        # XXX: should we raise an error if they try to set them?
+        if 'permissions' in attrs:
+            os.chmod(path, attrs["permissions"])
+        if 'atime' in attrs and 'mtime' in attrs:
+            os.utime(path, (attrs["atime"], attrs["mtime"]))
+
+    def readLink(self, path):
+        path = self._childPath(path).path
+        return os.readlink(path)
+
+    def makeLink(self, linkPath, targetPath):
+        # We disallow symlinks entirely.
+        raise OSError, 'Permission denied'
+
+    def realPath(self, path):
+        path = self._childPath(path)
+        # Make sure it really exists
+        path.restat()
+
+        # If it exists, it must be a real path, because we've disallowed
+        # creating symlinks!  So, we can just return the path as-is (after
+        # prefixing it with "." rather than self.homedir).
+        return '.' + path.path[len(self.homedir.path):]
+
+
+components.registerAdapter(SFTPServerForPushMirrorUser, SFTPOnlyAvatar,
+                           filetransfer.ISFTPServer)
 
 
 class Realm:
