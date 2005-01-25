e9373ff92aa207c11a2c9237c394945c4120e92a
Canonical.com Patch Queue Manager <pqm@canonical.com>
2005-01-25 19:38:30 +0000

    Remember selected value for Team/People searches. DB patch to rename Membership to TeamMembership. Patches applied:
     * guilherme.salgado@canonical.com/launchpad--devel--0--patch-107
       Remember selected value for people/team searches
    
     * guilherme.salgado@canonical.com/launchpad--devel--0--patch-108
       Adding db patch to rename Membership table to TeamMembership.
    
     * guilherme.salgado@canonical.com/launchpad--devel--0--patch-109
       merge from rf

---
+++ database/schema/pending/salgado-rename-membership.sql
@@ -0,0 +1,14 @@
+
+/*
+  Renames the table Membership to TeamMembership, to be consistent with our
+  TeamParticipation table. Fixes Malone#96.
+*/
+
+ALTER TABLE Membership RENAME TO TeamMembership;
+
+ALTER TABLE membership_id_seq RENAME TO teammembership_id_seq;
+ALTER TABLE membership_pkey RENAME TO teammembership_pkey;
+ALTER TABLE membership_person_key RENAME TO teammembership_person_pkey;
+
+ALTER TABLE TeamMembership
+    ALTER COLUMN id SET DEFAULT nextval('teammembership_id_seq');
