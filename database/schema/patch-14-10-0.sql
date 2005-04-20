67e56b03a5216ce7438a4160abfda2228869894a
Canonical.com Patch Queue Manager <pqm@canonical.com>
2005-04-20 16:08:27 +0000

    [trivial] Bug.owner needs a foreign key constraint Patches applied:
     * stuart.bishop@canonical.com/launchpad--devel--1--patch-292
       Bug.owner needs a foreign key constraint

---
+++ database/schema/patch-14-10-0.sql
@@ -0,0 +1,11 @@
+SET client_min_messages=ERROR;
+
+ALTER TABLE Bug ADD CONSTRAINT bug_owner_fk
+    FOREIGN KEY(owner) REFERENCES Person;
+
+ALTER TABLE Bug DROP CONSTRAINT "$1";
+ALTER TABLE Bug ADD CONSTRAINT bug_duplicateof_fk
+    FOREIGN KEY(duplicateof) REFERENCES Bug;
+
+INSERT INTO LaunchpadDatabaseRevision VALUES (14, 10, 0);
+
