e2f39c32c4e96e878753ba52c3471e0c2000d893
Canonical.com Patch Queue Manager <pqm@canonical.com>
2005-04-25 10:20:53 +0000

    [trivial] Add primary key constraint to packaging table Patches applied:
     * stuart.bishop@canonical.com/launchpad--packaging-pkey--0--base-0
       tag of rocketfuel@canonical.com/launchpad--devel--0--patch-1637
    
     * stuart.bishop@canonical.com/launchpad--packaging-pkey--0--patch-1
       Add primary key constraint to packaging table

---
+++ database/schema/patch-14-12-0.sql
@@ -0,0 +1,7 @@
+SET client_min_messages=ERROR;
+
+ALTER TABLE Packaging ADD CONSTRAINT packaging_pkey primary key(id);
+UPDATE Packaging SET id=DEFAULT WHERE id IS NULL;
+
+INSERT INTO LaunchpadDatabaseRevision VALUES (14, 12, 0);
+
