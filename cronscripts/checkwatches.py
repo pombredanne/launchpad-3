68f4e00c4925b25572dd60252a7d2f5883bbf833
Canonical.com Patch Queue Manager <pqm@canonical.com>
2005-05-24 01:09:01 +0000

    [trivial] Typo in checkwatches.py Patches applied:
     * stuart.bishop@canonical.com/launchpad--devel--2--patch-9
       Merge from Rocketfuel
    
     * stuart.bishop@canonical.com/launchpad--devel--2--patch-10
       Add note about directory contents
    
     * stuart.bishop@canonical.com/launchpad--devel--2--patch-11
       Typo

--- cronscripts/checkwatches.py
+++ cronscripts/checkwatches.py
@@ -79,7 +79,7 @@ def check_one_watch(watch):
     except externalsystem.UnknownBugTrackerTypeError, val:
         logger.error("BugTrackerType '%s' is not known", val.bugtrackertypename)
     except externalsystem.BugTrackerConnectError:
-        logger.exepption("Got error trying to contact %s", bugtracker.name)
+        logger.exeption("Got error trying to contact %s", bugtracker.name)
     else:
         versioncache.update({ bugtracker.baseurl : remotesystem.version })
         remotestatus = remotesystem.get_bug_status(watch.remotebug)
