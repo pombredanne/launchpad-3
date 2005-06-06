bebf4457f202625c41bea0ff4a3ac55587d052d5
Canonical.com Patch Queue Manager <pqm@canonical.com>
2005-06-06 14:06:28 +0000

    New config machinery, database helpers and oddsnsods required for staging Patches applied:
     * stuart.bishop@canonical.com/launchpad--devel--2--patch-32
       Merge from stuart.bishop@canonical.com/launchpad--staging--0
    
     * stuart.bishop@canonical.com/launchpad--devel--2--patch-33
       Restore launchpad.conf
    
     * stuart.bishop@canonical.com/launchpad--devel--2--patch-34
       Merge from stuart.bishop@canonical.com/launchpad--staging--0
    
     * stuart.bishop@canonical.com/launchpad--devel--2--patch-35
       Merge from stuart.bishop@canonical.com/launchpad--staging--0
    
     * stuart.bishop@canonical.com/launchpad--devel--2--patch-36
       Add index to match production database
    
     * stuart.bishop@canonical.com/launchpad--devel--2--patch-37
       Push DB patch to production
    
     * stuart.bishop@canonical.com/launchpad--devel--2--patch-38
       Review feedback
    
     * stuart.bishop@canonical.com/launchpad--staging--0--base-0
       tag of stuart.bishop@canonical.com/launchpad--devel--2--patch-29
    
     * stuart.bishop@canonical.com/launchpad--staging--0--patch-1
       Don't build pygettextpo until it is available on asuka
    
     * stuart.bishop@canonical.com/launchpad--staging--0--patch-2
       Merge from stuart.bishop@canonical.com/launchpad--devel--2
    
     * stuart.bishop@canonical.com/launchpad--staging--0--patch-3
       Initial staging config
    
     * stuart.bishop@canonical.com/launchpad--staging--0--patch-4
       Set ports
    
     * stuart.bishop@canonical.com/launchpad--staging--0--patch-5
       Turn pygettext back on
    
     * stuart.bishop@canonical.com/launchpad--staging--0--patch-6
       Propagate LPCONFIG
    
     * stuart.bishop@canonical.com/launchpad--staging--0--patch-7
       Trash <canonical staging> and just use <canonical default>
    
     * stuart.bishop@canonical.com/launchpad--staging--0--patch-8
       Use localhost for librarian for time being - Librarian config needs work
    
     * stuart.bishop@canonical.com/launchpad--staging--0--patch-9
       Pass hostname and username on command like of database tools
    
     * stuart.bishop@canonical.com/launchpad--staging--0--patch-10
       Common script verbosity helpers and wire up database scripts with it
    
     * stuart.bishop@canonical.com/launchpad--staging--0--patch-11
       Add help and improve logger interface
    
     * stuart.bishop@canonical.com/launchpad--staging--0--patch-12
       Simplify usage for the trivial case
    
     * stuart.bishop@canonical.com/launchpad--staging--0--patch-13
       Common database connection details command line parsing
    
     * stuart.bishop@canonical.com/launchpad--staging--0--patch-14
       Database schema upgrader
    
     * stuart.bishop@canonical.com/launchpad--staging--0--patch-15
       Make upgrade invokoe trusted.sql and comments.sql
    
     * stuart.bishop@canonical.com/launchpad--staging--0--patch-16
       Make upgrade invokoe trusted.sql and comments.sql
    
     * stuart.bishop@canonical.com/launchpad--staging--0--patch-17
       Staging config works
    
     * stuart.bishop@canonical.com/launchpad--staging--0--patch-18
       Config
    
     * stuart.bishop@canonical.com/launchpad--staging--0--patch-19
       Merge from Rocketfuel
    
     * stuart.bishop@canonical.com/launchpad--staging--0--patch-20
       Nuke launchpad.pid using an atexit handler
    
     * stuart.bishop@canonical.com/launchpad--staging--0--patch-21
       Refactor configuration selection machinery
    
     * stuart.bishop@canonical.com/launchpad--staging--0--patch-22
       Silence build noise unless there is a problem
    
     * stuart.bishop@canonical.com/launchpad--staging--0--patch-23
       Add missing script

---
+++ database/schema/patch-17-14-1.sql
@@ -0,0 +1,4 @@
+CREATE INDEX potranslationsighting_person_idx ON POTranslationSighting(person);
+
+INSERT INTO LaunchpadDatabaseRevision VALUES (17, 14, 1);
+
---
+++ database/schema/upgrade.py
@@ -0,0 +1,124 @@
+# Copyright 2004-2005 Canonical Ltd.  All rights reserved.
+"""
+Apply all outstanding schema patches to an existing launchpad database
+
+TODO: Currently this gives no feedback on how violent the modifications
+of a patch are (eg. how many rows updated). If we split the patches into
+statements we could split out some statistics for manual eyeballing.
+-- StuartBishop 20050602
+"""
+
+__metaclass__ = type
+
+import _pythonpath # Sort PYTHONPATH
+
+import os.path, glob, re, sys
+from optparse import OptionParser
+
+from canonical.launchpad.scripts import db_options, logger_options, logger
+from canonical.database.sqlbase import connect
+
+def main():
+    schema_dir = os.path.dirname(__file__)
+
+    con = connect(options.dbuser)
+
+    # trusted.sql contains all our stored procedures, which may be required
+    # for patches to apply correctly so must be run first.
+    apply_other(con, 'trusted.sql')
+
+    dbpatches = applied_patches(con)
+
+    # Generate a list of all patches we might want to apply
+    patches = []
+    all_patch_files = glob.glob(os.path.join(schema_dir, 'patch-??-??-?.sql'))
+    all_patch_files.sort()
+    for patch_file in all_patch_files:
+        m = re.search('patch-(\d\d)-(\d\d)-(\d).sql$', patch_file)
+        if m is None:
+            log.fatal('Invalid patch filename %s' % repr(patch_file))
+            sys.exit(1)
+
+        major, minor, patch = [int(i) for i in m.groups()]
+        if (major, minor, patch) in dbpatches:
+            continue # This patch has already been applied
+        log.debug("Found patch %d.%d.%d -- %s" % (
+            major, minor, patch, patch_file
+            ))
+        patches.append(((major, minor, patch), patch_file))
+
+    # Apply the patches
+    for (major, minor, patch), patch_file in patches:
+        apply_patch(con, major, minor, patch, patch_file)
+
+    # Update comments.
+    apply_other(con, 'comments.sql')
+
+    # Commit changes
+    if options.commit:
+        log.debug("Committing changes")
+        con.commit()
+
+
+def applied_patches(con):
+    """Return a list of all patches that have been applied to the database.
+    """
+    cur = con.cursor()
+    cur.execute("SELECT major, minor, patch FROM LaunchpadDatabaseRevision")
+    return [tuple(row) for row in cur.fetchall()]
+
+
+def apply_patch(con, major, minor, patch, patch_file):
+    log.info("Applying %s" % patch_file)
+    cur = con.cursor()
+    sql = open(patch_file).read()
+
+    cur.execute(sql) # Will die on a bad patch.
+
+    # Ensure the patch updated LaunchpadDatabaseRevision. We could do this
+    # automatically and avoid the boilerplate, but then we would lose the
+    # ability to easily apply the patches manually.
+    if (major, minor, patch) not in applied_patches(con):
+        log.fatal("%s failed to update LaunchpadDatabaseRevision correctly"
+                % patch_file)
+        sys.exit(2)
+
+    # Commit changes if we allow partial updates.
+    if options.commit and options.partial:
+        log.debug("Committing changes")
+        con.commit()
+
+
+def apply_other(con, script):
+    log.info("Applying %s" % script)
+    cur = con.cursor()
+    path = os.path.join(os.path.dirname(__file__), script)
+    sql = open(path).read()
+    cur.execute(sql)
+
+    if options.commit and options.partial:
+        log.debug("Committing changes")
+        con.commit()
+    
+
+if __name__ == '__main__':
+    parser = OptionParser()
+    db_options(parser)
+    logger_options(parser)
+    parser.add_option(
+            "-n", "--dry-run", dest="commit", default=True,
+            action="store_false", help="Don't actually commit changes"
+            )
+    parser.add_option(
+            "-p", "--partial", dest="partial", default=False,
+            action="store_true",
+            help="Commit after applying each patch",
+            )
+    (options, args) = parser.parse_args()
+
+    if args:
+        parser.error("Too many arguments")
+
+    log = logger(options)
+    main()
+
