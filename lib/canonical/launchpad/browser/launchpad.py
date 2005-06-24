76074d8983a030bd9841fc894ae40b2359db559f
Canonical.com Patch Queue Manager <pqm@canonical.com>
2005-06-24 15:40:03 +0000

    [trivial] fixed virtual hosting regression for login links, with tests. Patches applied:
     * steve.alexander@canonical.com--z8/launchpad--BetterLogin--0--patch-8
       fixed regression by making login link aware of virtual hosting

--- lib/canonical/launchpad/browser/launchpad.py
+++ lib/canonical/launchpad/browser/launchpad.py
@@ -29,10 +29,27 @@ class LoginStatus:
         if query_string:
             query_string = '?' + query_string
         url = self.request.getURL()
+
+        # We're going to use PATH_INFO to remove any spurious '+index' at the
+        # end of the URL.  But, PATH_INFO will contain virtual hosting
+        # configuration, if there is any.
         path_info = self.request['PATH_INFO']
+        # Remove any virtual hosting segments.
+        path_steps = []
+        in_virtual_hosting_section = False
+        for step in path_info.split('/'):
+            if step.startswith('++vh++'):
+                in_virtual_hosting_section = True
+                continue
+            if step == '++':
+                in_virtual_hosting_section = False
+                continue
+            if not in_virtual_hosting_section:
+                path_steps.append(step)
+        path = '/'.join(path_steps)
         # Make the URL stop at the end of path_info so that we don't get
         # spurious '+index' at the end.
-        full_url = '%s%s' % (url[:url.find(path_info)], path_info)
+        full_url = '%s%s' % (url[:url.find(path)], path)
         if full_url.endswith('/'):
             full_url = full_url[:-1]
         logout_url_end = '/+logout'
