f89f8543d800985dab3cb66bad4b34f3a8979da7
Canonical.com Patch Queue Manager <pqm@canonical.com>
2005-06-27 13:28:46 +0000

    [trivial] Improve wikiname indexes Patches applied:
     * stuart.bishop@canonical.com/launchpad--dba--3--patch-3
       Improve wikiname indexes

--- database/schema/patch-17-32-0.sql
+++ database/schema/patch-17-32-0.sql
@@ -1,6 +1,8 @@
 SET client_min_messages=ERROR;
 
 CREATE INDEX wikiname_person_idx ON WikiName(person);
+ALTER TABLE WikiName DROP CONSTRAINT wikiname_wiki_key;
+ALTER TABLE WikiName ADD CONSTRAINT wikiname_wikiname_key UNIQUE(wikiname,wiki);
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (17, 32, 0);
 
