9bc2fa1f5bb9f943117281a02984095f2f971b24
Canonical.com Patch Queue Manager <pqm@canonical.com>
2005-07-08 13:39:05 +0000

    [r=BjornT] DisplayingParagraphsOfText Patches applied:
     * mpt@canonical.com/launchpad--DisplayingParagraphsOfText--0--base-0
       tag of steve.alexander@canonical.com--z8/launchpad--DisplayingParagraphsOfText--0--patch-3
    
     * mpt@canonical.com/launchpad--DisplayingParagraphsOfText--0--patch-1
       Add some failing tests
    
     * steve.alexander@canonical.com--z8/launchpad--DisplayingParagraphsOfText--0--base-0
       tag of rocketfuel@canonical.com/launchpad--devel--0--patch-1892
    
     * steve.alexander@canonical.com--z8/launchpad--DisplayingParagraphsOfText--0--patch-1
       implemented DisplayingParagraphsOfText
    
     * steve.alexander@canonical.com--z8/launchpad--DisplayingParagraphsOfText--0--patch-2
       added explanatory comments
    
     * steve.alexander@canonical.com--z8/launchpad--DisplayingParagraphsOfText--0--patch-3
       tidied up regular expression definitions
    
     * steve.alexander@canonical.com--z8/launchpad--DisplayingParagraphsOfText--0--patch-4
       updated to latest DisplayingParagraphisOfText spec, with new tests, and improved processing, and omre coments.
    
     * steve.alexander@canonical.com--z8/launchpad--DisplayingParagraphsOfText--0--patch-5
       merge from rf, resolving conflict in tales.txt
    
     * steve.alexander@canonical.com--z8/launchpad--DisplayingParagraphsOfText--0--patch-6
       removed tales-to-be-removed.txt
    
     * steve.alexander@canonical.com--z8/launchpad--DisplayingParagraphsOfText--0--patch-7
       merge from rf
    
     * steve.alexander@canonical.com--z8/launchpad--DisplayingParagraphsOfText--0--patch-8
       implemented suggestions from bjorn's review
    
     * steve.alexander@canonical.com--z8/launchpad--DisplayingParagraphsOfText--0--patch-9
       merge from rf

--- lib/canonical/launchpad/webapp/tales.py
+++ lib/canonical/launchpad/webapp/tales.py
@@ -312,7 +312,7 @@ class DurationFormatterAPI:
                 parts.append('%d minutes' % minutes)
         if parts or seconds > 0:
             parts.append('%0.1f seconds' % seconds)
-        
+
         return ', '.join(parts)
 
 
@@ -416,6 +416,137 @@ class FormattersAPI:
         """Quote HTML characters, then replace newlines with <br /> tags."""
         return cgi.escape(self._stringtoformat).replace('\n','<br />\n')
 
+    @staticmethod
+    def _substitute_matchgroup_for_spaces(matchobj):
+        """Return a string made up of '&nbsp;' for each character in the
+        first match group.
+
+        Used when replacing leading spaces with nbsps.
+
+        There must be only one match group.
+        """
+        groups = matchobj.groups()
+        assert len(groups) == 1
+        return '&nbsp;' * len(groups[0])
+
+    # Match <, >, & when they've been html-escaped, so that we can replace
+    # them with a single character to get the correct length of a line.
+    _re1 = re.compile('&lt;|&gt;|&amp;')
+
+    # Match for putting <div></div> around lines.
+    # Look back to check there was not a \n previously, match a single \n
+    # or the start of the text, some data we want, followed by a look-ahead
+    # of a single \n not followed by another \n, or the end of the text.
+    _re2 = re.compile('(?<!\n)(\n|^)([^\n]+)(?=\n[^\n]|\n$|$)')
+
+    # Match for putting <p></p> around paragraphs.
+    # Match two newlines, start of text and newline, or a </div>, then
+    # some data we want with no newlines or '<', then a look-ahead of
+    # a pair or newlines, or a single newline and the end of the string,
+    # or the end of the string, or a <div>.
+    _re3 = re.compile('(\n\n|^\n|^|</div>)([^\n<]+)(?=\n\n|\n$|$|<div>)')
+
+    # See if the output looks like a single div containing just text.
+    _re4 = re.compile('^<div>([^<>]*)</div>$')
+
+    # Whitespace following a <div>, so we can replace it with &nbsp;
+    # characters using _substitute_matchgroup_for_spaces().
+    _re5 = re.compile('(?<=<div>)( +)')
+
+    # Whitespace following a <p>, so we can replace it with &nbsp;
+    # characters using _substitute_matchgroup_for_spaces().
+    _re6 = re.compile('(?<=<p>)( +)')
+
+    # A </div> with any newlines after it, so we can ensure there's exactly
+    # one newline after a div.
+    _re7 = re.compile('</div>\n*')
+
+    # A <p> with any newlines after it, so we can ensure there's exactly
+    # one newline after a paragraph.
+    _re8 = re.compile('</p>\n*')
+
+    def text_to_html(self):
+        """Quote text according to DisplayingParagraphsOfText."""
+        text = cgi.escape(self._stringtoformat)
+
+        # This is based on the algorithm in the
+        # DisplayingParagraphsOfText spec, but is a little more
+        # complicated.
+
+        # 1. Trailing whitespace is removed from each line.
+        # 2. For each line in the text, if (when unescaped) it is between
+        #    60 and 80 characters long, and the next line exists and is
+        #    non-empty, replace the newline between them with a space
+        #    character. (We're assuming such line breaks are the result of
+        #    hard-wrapped text, such as an e-mail message.)
+
+        output = []
+        continuous_logical_paragraph = False
+        for line in text.splitlines():
+            line = line.rstrip()
+            output.append(line)
+            # Substitute the unescaped values for measuring the length of the
+            # line.  An 'X' is used to make it clearly show up if we use the
+            # output for anything else.
+            if 60 < len(self._re1.sub('X', line)) < 80:
+                if not continuous_logical_paragraph:
+                    output.insert(-1, '<parastartmarker>\n\n')
+                output.append(' ')
+                continuous_logical_paragraph = True
+            else:
+                if continuous_logical_paragraph:
+                    # Put a special marker in to separate the paragraph from
+                    # whatever is to come next.  This marker cannot occur in
+                    # the real text because the real text has been cgi escaped.
+                    # The marker effectively separates the set of newlines
+                    # that come immediately after the paragraph from the
+                    # newlines that come before the following text.
+                    output.append('\n\n<paraendmarker>')
+                    continuous_logical_paragraph = False
+                    if not line.strip():
+                        # Remove the trailing space, which is the second to
+                        # last item in output.
+                        output.pop(-3)
+                        # This was a deliberately blank line, so add another
+                        # \n.
+                        output.append('\n')
+                output.append('\n')
+        output = output[:-1]
+        text = ''.join(output)
+
+        # 3. Put <div>...</div> around non-whitespace lines that
+        #   - start with a single newline or the start of the entire text, and
+        #   - end with a single newline or the end of the entire text.
+
+        text = self._re2.sub(r'<div>\2</div>', text)
+
+        # 4. Put <p>...</p> around non-whitespace lines that:
+        #   - start with multiple newlines or the start of the entire text,
+        #     or where the previous line now ends in </div>, and
+        #   - end with multiple newlines or the end of the entire text,
+        #     or where the following line now starts with <div>.
+
+        text = self._re3.sub(r'<p>\2</p>', text)
+
+        # Need to move the <parastartmarker> and <paraendmarker> now.
+        text = text.replace('<parastartmarker>', '')
+        text = text.replace('<paraendmarker>', '')
+
+        # 5. If the entire text now consists of a single <div>...</div>
+        # element, change it to a <p>...</p> element.
+
+        text = self._re4.sub(r'<p>\1</p>', text)
+
+        # 6. Leading spaces on each line are converted to &nbsp;.
+        text = self._re5.sub(self._substitute_matchgroup_for_spaces, text)
+        text = self._re6.sub(self._substitute_matchgroup_for_spaces, text)
+
+        # Only one newline after closing tags, except at the end.
+        text = self._re7.sub('</div>\n', text)
+        text = self._re8.sub('</p>\n', text)
+        text = text.rstrip()
+        return text
+
     def nice_pre(self):
         """<pre>, except the browser knows it is allowed to break long lines
 
@@ -449,6 +580,8 @@ class FormattersAPI:
     def traverse(self, name, furtherPath):
         if name == 'nl_to_br':
             return self.nl_to_br()
+        elif name == 'text-to-html':
+            return self.text_to_html()
         elif name == 'nice_pre':
             return self.nice_pre()
         elif name == 'shorten':
