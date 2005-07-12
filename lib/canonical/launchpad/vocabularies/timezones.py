78be1bd64c0094870ccb44cffce9380e6ac4f7b5
James Henstridge <james.henstridge@canonical.com>
2005-07-12 07:45:16 +0000

    s/+edittz/+changetz/, and other changes suggested in BjornT's review

--- lib/canonical/launchpad/vocabularies/timezones.py
+++ lib/canonical/launchpad/vocabularies/timezones.py
@@ -1,4 +1,4 @@
-# Copyright 2005 Canonical Ltd
+# Copyright 2005 Canonical Ltd.  All rights reserved.
 
 __metaclass__ = type
 
@@ -9,12 +9,12 @@ from zope.schema.vocabulary import SimpleVocabulary
 __all__ = ['TimezoneNameVocabulary']
 
 # create a sorted list of the common time zone names, with UTC at the start
-_values = pytz.common_timezones[:]
-_values.sort()
+_values = sorted(pytz.common_timezones)
 _values.remove('UTC')
 _values.insert(0, 'UTC')
 
 _timezone_vocab = SimpleVocabulary.fromValues(_values)
+del _values
 
 def TimezoneNameVocabulary(context=None):
     return _timezone_vocab
