d24d8148d7e0e2b8a9bbf99c46a894432e0c32d6
James Henstridge <james.henstridge@canonical.com>
2005-07-13 02:36:05 +0000

    fix up database patch and regenerate sampledata

--- database/schema/patch-25-90-0.sql
+++ database/schema/patch-25-90-0.sql
@@ -4,4 +4,4 @@ UPDATE Person SET timezone = 'UTC' WHERE timezone IS NULL;
 ALTER TABLE Person ALTER COLUMN timezone SET DEFAULT 'UTC';
 ALTER TABLE Person ALTER COLUMN timezone SET NOT NULL;
 
-INSERT INTO LaunchpadDatabaseRevision VALUES (17, 90, 0);
+INSERT INTO LaunchpadDatabaseRevision VALUES (25, 90, 0);
