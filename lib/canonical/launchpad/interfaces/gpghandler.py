b927f450acba5a550c35254288d59a02234d2133
Canonical.com Patch Queue Manager <pqm@canonical.com>
2005-08-03 16:14:59 +0000

    [r=jamesh] fixing bugs #1496 and #1613, better encoding handling and subkey signatures support. Patches applied:
     * celso.providelo@canonical.com/launchpad--gpg-ng--0--patch-60
       Merge from Rf
    
     * celso.providelo@canonical.com/launchpad--gpg-ng--0--patch-61
       Attempt to handle unicode properly into gpghandler component.
    
     * celso.providelo@canonical.com/launchpad--gpg-ng--0--patch-62
       Merge from RF
    
     * celso.providelo@canonical.com/launchpad--gpg-ng--0--patch-63
       Review from salgado applied, better docstrings and type ensuring.
    
     * celso.providelo@canonical.com/launchpad--gpg-ng--0--patch-64
       Fixing unicode handling for gpghandler, with stub and bjorn
    
     * celso.providelo@canonical.com/launchpad--gpg-ng--0--patch-65
       Attemptin for GPG subkeys signatures.
    
     * celso.providelo@canonical.com/launchpad--gpg-ng--0--patch-66
       Merge from RF
    
     * celso.providelo@canonical.com/launchpad--gpg-ng--0--patch-67
       Fixing acknowledgeCoC() method, add a test and fix some tests indentation.

--- lib/canonical/launchpad/interfaces/gpghandler.py
+++ lib/canonical/launchpad/interfaces/gpghandler.py
@@ -5,60 +5,60 @@ __all__ = ['IGPGHandler', 'IPymeSignature', 'IPymeKey']
 class IGPGHandler(Interface):
     """Handler to perform GPG operations."""
 
-    def verifySignature(content, signature=None, key=None):
-        """Return whether or not the content is correctly signed.
+    def verifySignature(content, signature=None):
+        """Returns a PymeSignature objet if content is correctly signed
+        or None. 
         
         If signature is None, we assume content is clearsigned. Otherwise
         it stores the detached signature and content should contain the
         plain text in question.
-        
-        If key is None, we search launchpad's gpg key tables to find
-        the key which might have signed it and attempt to verify it
-        that way.
-        
-        :param content: The content to be verified
-        :param signature: The signature (or None if content is clearsigned)
-        :param key: The key to verify against (or None to search launchpad)
-        
-        :returns: a PymeSignature object containing the signature information
-        See IPymeSignature for further info.
+
+        content and signature must traditional strings. It's up to the caller
+        to encode or decode properly.
+    
+        :content: The content to be verified
+        :signature: The signature (or None if content is clearsigned)
         """
 
     def importKey(content):
-        """Import the given public or secret key. We assume it as the
-        default keyring.
+        """Returns a PymeKey object refering to an just-import GPG
+        public or secret key.
 
-        :param content: public or secret key content ASCII armored
-        
-        :returns: a PymeKey instance
+        content must be a traditional string. It's up to the caller to
+        encode or decode properly.
+
+        :content: public or secret key content ASCII armored
         """
 
     def encryptContent(content, fingerprint):
-        """Encrypt content for a given GPG key.
+        """Return the encrypted content or None if failed.
 
-        :param content: data content
-        :param fingerprint: key fingerprint
-
-        :returns: encrypted data or None if failed
+        content must be a traditional string. It's up to the caller to
+        encode or decode properly. Fingerprint must be hexadecimal string. 
+        
+        :content: the unicode content to be encrypted.
+        :fingerprint: the GPG Key's fingerprint.
         """
 
     def decryptContent(content, password):
-        """Decrypt content with the given password.
+        """Return the decrypted content or None if failed
 
-        :param content: encrypted data content
-        :param password: raw passwrod to unlock the secret key in question 
+        content and password must be traditional strings. It's up to
+        the caller to encode or decode properly. 
 
-        :returns: decrypted data or None if failed
+        :content: encrypted data content
+        :password: unicode password to unlock the secret key in question 
         """
 
     def retrieveKey(fingerprint):
-        """Retrieve key information from the local keyring, if key isn't
-        present, import it from the key server before.
+        """Returns a PymeKey containing the just-retrieved key information
+        from the local keyring, if key isn't present, import it from the
+        key server before. If the process fails, it returns debug information
+        about the process.
 
-        :param fingerprint: key fingerprint
+        Fingerprint must be hexadecimal string.
 
-        :returns: operation result, PymeKey instance or Debug Info if result
-        is False
+        :fingerprint: key fingerprint
         """
 
 
