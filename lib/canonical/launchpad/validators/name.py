187987a3270c18d6d643d343b1d54e17d267947a
Canonical.com Patch Queue Manager <pqm@canonical.com>
2005-09-04 06:17:23 +0000

    [r=SteveA,BjornT] Zope3FormErrorMessages Patches applied:
     * stuart.bishop@canonical.com/launchpad--Zope3FormErrorMessages--0--base-0
       tag of stuart.bishop@canonical.com/launchpad--trivial--0--patch-44
    
     * stuart.bishop@canonical.com/launchpad--Zope3FormErrorMessages--0--patch-1
       Refactor validators and allow them to raise rich text error messages
    
     * stuart.bishop@canonical.com/launchpad--Zope3FormErrorMessages--0--patch-2
       Nother test
    
     * stuart.bishop@canonical.com/launchpad--Zope3FormErrorMessages--0--patch-3
       Merge from Rocketfuel
    
     * stuart.bishop@canonical.com/launchpad--Zope3FormErrorMessages--0--patch-4
       Tweaks post review
    
     * stuart.bishop@canonical.com/launchpad--Zope3FormErrorMessages--0--patch-5
       Fix assumption about WidgetInputErrors always having an errors attribute.
    
     * stuart.bishop@canonical.com/launchpad--Zope3FormErrorMessages--0--patch-6
       Merge from Rocketfuel

--- lib/canonical/launchpad/validators/name.py
+++ lib/canonical/launchpad/validators/name.py
@@ -5,28 +5,40 @@
 __metaclass__ = type
 
 import re
+from textwrap import dedent
+
+from zope.schema import ValidationError
+
+from canonical.launchpad import _
+from canonical.launchpad.validators import LaunchpadValidationError
 
 from zope.schema import ValidationError
 
 from canonical.launchpad import _
 
 def valid_name(name):
-    pat = r"^[a-z0-9][a-z0-9\\+\\.\\-]+$"
+    """Return True if the name is valid, otherwise False.
+
+    Lauchpad `name` attributes are designed for use as url components
+    and short unique identifiers to things.
+
+    The default name constraints may be too strict for some objects,
+    such as binary packages or arch branches where naming conventions already
+    exists, so they may use their own specialized name validators
+    """
+    pat = r"^[a-z0-9][a-z0-9\+\.\-]+$"
     if re.match(pat, name):
         return True
     return False
 
-class InvalidName(ValidationError):
-    __doc__ = _(
-            "Invalid input. names must start with a letter or "
-            "number and be lowercase. The characers '+', '-' and '.' "
-            "are also allowed after the first character."
-            )
-
 def name_validator(name):
+    """Return True if the name is valid, or raise a LaunchpadValidationError"""
     if not valid_name(name):
-        raise InvalidName(name)
+        raise LaunchpadValidationError(_(dedent("""
+            Invalid name '%s'. names must start with a letter or
+            number and be lowercase. The characers <kbd>+</kbd>,
+            <kbd>-</kbd> and <kbd>.</kbd> are also allowed after the
+            first character.
+            """)), name)
     return True
-
-valid_name.sql_signature = [('name', 'text')]
 
