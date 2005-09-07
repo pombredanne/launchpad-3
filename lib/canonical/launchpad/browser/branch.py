9fb66c08f29577b31a593ab79b58a5711b323cb8
David Allouche <david.allouche@canonical.com>
2005-09-07 11:21:31 +0000

    wrap branch urls in the page index page

--- lib/canonical/launchpad/browser/branch.py
+++ lib/canonical/launchpad/browser/branch.py
@@ -12,8 +12,22 @@ from canonical.launchpad.browser.editview import SQLObjectEditView
 from canonical.launchpad.webapp import canonical_url
 
 __all__ = [
+    'BranchView',
     'BranchEditView',
     ]
+
+
+class BranchView:
+
+    __used_for__ = IBranch
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def url(self):
+        components = self.context.url.split('/')
+        return '/&#x200B;'.join(components)
 
 
 class BranchEditView(SQLObjectEditView):
