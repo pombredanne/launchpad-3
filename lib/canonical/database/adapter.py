3d3d2878e24b718e6a1eb4a3ae1cd2515acf5c5e
James Henstridge <james.henstridge@canonical.com>
2005-09-15 09:18:14 +0000

    add API to handle request timeouts in the database adapter

--- lib/canonical/database/adapter.py
+++ lib/canonical/database/adapter.py
@@ -2,6 +2,9 @@
 
 __metaclass__ = type
 
+import threading
+import time
+
 from psycopgda.adapter import PsycopgAdapter
 
 from canonical.config import config
@@ -16,10 +19,73 @@ class LaunchpadDatabaseAdapter(PsycopgAdapter):
         connection = PsycopgAdapter._connection_factory(self)
 
         if config.launchpad.db_statement_timeout is not None:
-            print 'setting timeout to %d' % config.launchpad.db_statement_timeout
             cursor = connection.cursor()
             cursor.execute('SET statement_timeout TO %d' %
                            config.launchpad.db_statement_timeout)
             connection.commit()
 
-        return connection
+        return ConnectionWrapper(connection)
+
+
+_local = threading.local()
+
+def set_request_started(starttime=None):
+    '''Set the start time for the request being served by the current
+    thread.
+
+    If the argument is given, it is used as the start time for the
+    request, as returned by time.time().  If it is not given, the
+    current time is used.'''
+    if starttime is None:
+        starttime = time.time()
+    _local.request_start_time = starttime
+
+def clear_request_started():
+    '''Clear the request timer.  This function should be called when
+    the request completes.'''
+    _local.request_start_time = None
+
+def _request_expired():
+    '''Checks whether the current request has expired.'''
+    if config.launchpad.db_statement_timeout is None:
+        return False # no timeout configured
+
+    starttime = getattr(_local, 'request_start_time', None)
+    if starttime is None:
+        return False # no current request
+
+    requesttime = (time.time() - starttime) * 1000
+    return requesttime > config.launchpad.db_statement_timeout * 1000
+
+
+class RequestExpired(RuntimeError):
+    '''Request has timed out'''
+
+
+class ConnectionWrapper:
+    def __init__(self, connection):
+        self.__dict__['_conn'] = connection
+
+    def cursor(self):
+        return CursorWrapper(self._conn.cursor())
+
+    def __getattr__(self, attr):
+        return getattr(self._conn, attr)
+
+    def __setattr__(self, attr, value):
+        setattr(self._conn, attr, value)
+
+class CursorWrapper:
+    def __init__(self, cursor):
+        self.__dict__['_cur'] = cursor
+
+    def execute(self, *args, **kwargs):
+        if _request_expired():
+            raise RequestExpired('The current request has expired')
+        return self._cur.execute(*args, **kwargs)
+
+    def __getattr__(self, attr):
+        return getattr(self._cur, attr)
+
+    def __setattr__(self, attr, value):
+        setattr(self._cur, attr, value)
