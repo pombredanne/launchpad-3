3ba40e29c412bd9143ab9e9b27127efbf6e3325d
Canonical.com Patch Queue Manager <pqm@canonical.com>
2005-09-22 11:47:41 +0000

    r=stub add 'launchpad (or shipit) is going down for maintenance' facility. Patches applied:
     * steve.alexander@canonical.com/launchpad--devel--0--patch-408
       first crack at a 'good evening mister launchpad, going down?' message
    
     * steve.alexander@canonical.com/launchpad--devel--0--patch-409
       made the maintenance notice work on shipit too

--- lib/canonical/launchpad/browser/launchpad.py
+++ lib/canonical/launchpad/browser/launchpad.py
@@ -2,16 +2,73 @@
 """Browser code for the launchpad application."""
 
 __metaclass__ = type
-__all__ = ['LoginStatus']
+__all__ = ['LoginStatus', 'MaintenanceMessage']
 
 import cgi
 import urllib
+import os.path
+from datetime import timedelta, datetime
 
+from zope.app.datetimeutils import parseDatetimetz, tzinfo, DateTimeError
+from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
 from zope.component import getUtility
 from canonical.launchpad.interfaces import (
-    ILaunchBag, ILaunchpadRoot, IRosettaApplication)
+    ILaunchBag, ILaunchpadRoot, IRosettaApplication, IAging)
 from canonical.launchpad.webapp import (
     StandardLaunchpadFacets, ApplicationMenu, Link)
+# XXX SteveAlexander, 2005-09-22, this is imported here because there is no
+#     general timedelta to duration format adapter available.  This should
+#     be factored out into a generally available adapter for both this
+#     code and for TALES namespace code to use.
+from canonical.launchpad.webapp.tales import DurationFormatterAPI
+
+
+class MaintenanceMessage:
+    """Display a maintenance message if the control file is present and
+    it contains a valid iso format time.
+
+    The maintenance message shows the approximate time before launchpad will
+    be taken offline for maintenance.
+
+    The control file is +maintenancetime.txt in the launchpad root.
+
+    If there is no maintenance message, an empty string is returned.
+
+    If the maintenance time is too far in the future, then an empty string
+    is returned.
+
+    If the maintenance time is in the past, then the maintenance message says
+    that Launchpad will go offline "very very soon".
+
+    If the text in the maintenance message is poorly formatted, then an
+    empty string is returned, and a warning should be logged.
+    """
+
+    timelefttext = None
+
+    notmuchtime = timedelta(seconds=30)
+    toomuchtime = timedelta(seconds=1800)  # 30 minutes
+
+    def __call__(self):
+        if os.path.exists('+maintenancetime.txt'):
+            message = file('+maintenancetime.txt').read()
+            try:
+                maintenancetime = parseDatetimetz(message)
+            except DateTimeError:
+                # XXX log a warning here.
+                #     SteveAlexander, 2005-09-22
+                return ''
+            nowtz = datetime.utcnow().replace(tzinfo=tzinfo(0))
+            timeleft = maintenancetime - nowtz
+            if timeleft > self.toomuchtime:
+                return ''
+            elif timeleft < self.notmuchtime:
+                self.timelefttext = 'very very soon'
+            else:
+                self.timelefttext = 'in %s' % (
+                    DurationFormatterAPI(timeleft).approximateduration())
+            return self.index()
+        return ''
 
 
 class LaunchpadRootFacets(StandardLaunchpadFacets):
