3f4751cc8c2fdb2ddcd5036935d93b9cadd56165
Canonical.com Patch Queue Manager <pqm@canonical.com>
2005-09-28 23:25:06 +0000

    [r=BjornT] fix a regression in the Malone menus where the menus had  magically disappeared from many pages,  likely during the conversion to the menu framework Patches applied:
     * brad.bollenbach@canonical.com/launchpad--malone-form-error-messages--0--base-0
       tag of rocketfuel@canonical.com/launchpad--devel--0--patch-2511
    
     * brad.bollenbach@canonical.com/launchpad--malone-form-error-messages--0--patch-1
       merge from rf
    
     * brad.bollenbach@canonical.com/launchpad--malone-form-error-messages--0--patch-2
       fix a nasty regression in the Malone menus, that seemed to have happened during the conversion to the menu framework
    
     * brad.bollenbach@canonical.com/launchpad--malone-form-error-messages--0--patch-3
       fixed based on code review

--- lib/canonical/launchpad/browser/bugtask.py
+++ lib/canonical/launchpad/browser/bugtask.py
@@ -23,8 +23,7 @@ from zope.app.form.utility import setUpWidgets, getWidgetsData
 from zope.app.form.interfaces import IInputWidget
 
 from canonical.lp import dbschema
-from canonical.launchpad.webapp import (
-    canonical_url, ContextMenu, Link, structured)
+from canonical.launchpad.webapp import canonical_url, Link
 from canonical.lp.z3batching import Batch
 from canonical.lp.batching import BatchNavigator
 from canonical.launchpad.interfaces import (
@@ -36,6 +35,7 @@ from canonical.launchpad.interfaces import IBugTaskSearchListingView
 from canonical.launchpad.searchbuilder import any, NULL
 from canonical.launchpad import helpers
 from canonical.launchpad.browser.editview import SQLObjectEditView
+from canonical.launchpad.browser.bug import BugContextMenu
 from canonical.launchpad.interfaces.bug import BugDistroReleaseTargetDetails
 
 # This shortcut constant indicates what we consider "open"
@@ -45,83 +45,8 @@ STATUS_OPEN = any(dbschema.BugTaskStatus.NEW,
                   dbschema.BugTaskStatus.ACCEPTED)
 
 
-class BugTaskContextMenu(ContextMenu):
+class BugTaskContextMenu(BugContextMenu):
     usedfor = IBugTask
-    links = ['editdescription', 'targetfix', 'secrecy', 'markduplicate',
-             'subscription', 'addsubscriber', 'addattachment', 'linktocve',
-             'addurl', 'addwatch', 'filebug', 'searchbugs', 'activitylog']
-
-    def editdescription(self):
-        text = 'Edit Description'
-        return Link('+edit', text, icon='edit')
-
-    def targetfix(self):
-        enabled = (
-            IDistroBugTask.providedBy(self.context) or
-            IDistroReleaseBugTask.providedBy(self.context))
-        text = 'Target Fix to Releases'
-        return Link('+target', text, icon='edit', enabled=enabled)
-
-    def secrecy(self):
-        text = 'Bug Secrecy'
-        return Link('+secrecy', text, icon='edit')
-
-    def markduplicate(self):
-        text = 'Mark as Duplicate'
-        return Link('+duplicate', text, icon='edit')
-
-    def subscription(self):
-        user = getUtility(ILaunchBag).user
-        if user is not None and self.context.bug.isSubscribed(user):
-            text = 'Unsubscribe'
-        else:
-            text = 'Subscribe'
-        return Link('+subscribe', text, icon='add')
-
-    def addsubscriber(self):
-        text = 'Subscribe Someone Else'
-        return Link('+addsubscriber', text, icon='add')
-
-    def addattachment(self):
-        text = 'Add Attachment'
-        return Link('+addattachment', text, icon='add')
-
-    def linktocve(self):
-        text = structured(
-            'Link to '
-            '<abbr title="Common Vulnerabilities and Exposures Index">'
-            'CVE'
-            '</abbr>')
-        return Link('+linkcve', text, icon='add')
-
-    def unlinkcve(self):
-        enabled = bool(self.context.bug.cves)
-        text = 'Remove CVE link'
-        return Link('+unlinkcve', text, icon='edit', enabled=enabled)
-
-    def addurl(self):
-        text = 'Link to Web Page'
-        return Link('+addurl', text, icon='add')
-
-    def addwatch(self):
-        text = 'Link To Other Bugtracker'
-        return Link('+addwatch', text, icon='add')
-
-    def filebug(self):
-        bugtarget = self.context.target
-        linktarget = '%s/%s' % (canonical_url(bugtarget), '+filebug')
-        text = 'Report a Bug in %s' % bugtarget.displayname
-        return Link(linktarget, text, icon='add')
-
-    def searchbugs(self):
-        bugtarget = self.context.target
-        linktarget = '%s/%s' % (canonical_url(bugtarget), '+bugs')
-        text = 'Search %s Bugs' % bugtarget.displayname
-        return Link(linktarget, text, icon='bugs')
-
-    def activitylog(self):
-        text = 'Activity Log'
-        return Link('+activity', text, icon='list')
 
 
 class BugTasksReportView:
