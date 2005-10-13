b9511a43fccfdfa92283dc29014323f6bd03ea93
Canonical.com Patch Queue Manager <pqm@canonical.com>
2005-10-13 19:18:32 +0000

    [r=SteveA] allow admins to edit any bug,  public or private. also add a Launchpad Developers team,  make them a celebrity and allow them to see error messages. Patches applied:
     * brad.bollenbach@canonical.com/launchpad--malone-bug-3059--0--base-0
       tag of rocketfuel@canonical.com/launchpad--devel--0--patch-2626
    
     * brad.bollenbach@canonical.com/launchpad--malone-bug-3059--0--patch-1
       checkpoint
    
     * brad.bollenbach@canonical.com/launchpad--malone-bug-3059--0--patch-2
       merge from rf
    
     * brad.bollenbach@canonical.com/launchpad--malone-bug-3059--0--patch-3
       allow admins to edit any bug, private or public. code review fixups.
    
     * brad.bollenbach@canonical.com/launchpad--malone-bug-3059--0--patch-4
       fix broken tests and make some unrelated additions as requested by SteveA
    
     * brad.bollenbach@canonical.com/launchpad--malone-bug-3059--0--patch-5
       test fix

--- lib/canonical/launchpad/interfaces/launchpad.py
+++ lib/canonical/launchpad/interfaces/launchpad.py
@@ -55,6 +55,7 @@ class ILaunchpadCelebrities(Interface):
     rosetta_expert = Attribute("The Rosetta Experts team.")
     debbugs = Attribute("The Debian Bug Tracker")
     shipit_admin = Attribute("The ShipIt Administrators.")
+    launchpad_developers = Attribute("The Launchpad development team.")
 
 
 class ICrowd(Interface):
