bfb05f32f799e576857fd4b2ce94acb7b0e043fb
Andrew Bennetts <andrew.bennetts@canonical.com>
2005-11-29 23:32:19 +1100

    Another test.

--- supermirrorsftp/tests/test_fs_restrictions.py
+++ supermirrorsftp/tests/test_fs_restrictions.py
@@ -8,7 +8,7 @@ from supermirrorsftp.sftponly import SFTPOnlyAvatar
 from supermirrorsftp.bazaarfs import SFTPServerRoot
 
 class TestTopLevelDir(unittest.TestCase):
-    def testListDir(self):
+    def testListDirNoTeams(self):
         # list only user dir + team dirs
         tmpdir = self.mktemp()
         os.mkdir(tmpdir)
@@ -18,6 +18,23 @@ class TestTopLevelDir(unittest.TestCase):
         self.assertEqual(
             [name for name, child in root.children()], 
             ['.', '..', '~alice'])
+        shutil.rmtree(tmpdir)
+
+    def testListDirTeams(self):
+        # list only user dir + team dirs
+        tmpdir = self.mktemp()
+        os.mkdir(tmpdir)
+        userDict = {
+            'id': 1, 
+            'name': 'alice', 
+            'teams': [{'id': '1', 'name': 'alice'},
+                      {'id': '2', 'name': 'test-team'}]
+        }
+        avatar = SFTPOnlyAvatar('alice', tmpdir, '/dev/null', userDict)
+        root = SFTPServerRoot(avatar)
+        self.assertEqual(
+            [name for name, child in root.children()], 
+            ['.', '..', '~alice', '~test-team'])
         shutil.rmtree(tmpdir)
 
     def testAllWriteOpsForbidden(self):
