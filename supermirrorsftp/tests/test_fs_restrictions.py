aa51a0763584edb2ac22eed54a1dfe497471e977
Andrew Bennetts <andrew.bennetts@canonical.com>
2005-11-30 17:06:58 +1100

    Refactor tests slightly.

--- supermirrorsftp/tests/test_fs_restrictions.py
+++ supermirrorsftp/tests/test_fs_restrictions.py
@@ -8,34 +8,40 @@ from supermirrorsftp.sftponly import SFTPOnlyAvatar
 from supermirrorsftp.bazaarfs import SFTPServerRoot
 
 class TestTopLevelDir(unittest.TestCase):
+    def setUp(self):
+        self.tmpdir = self.mktemp()
+        os.mkdir(self.tmpdir)
+        # A basic user dict, a member of no teams (aside from the user
+        # themself).
+        self.aliceUserDict = {
+            'id': 1, 
+            'name': 'alice', 
+            'teams': [{'id': 1, 'name': 'alice'}],
+        }
+
+    def tearDown(self):
+        shutil.rmtree(self.tmpdir)
+
     def testListDirNoTeams(self):
         # list only user dir + team dirs
-        tmpdir = self.mktemp()
-        os.mkdir(tmpdir)
-        userDict = {'id': 1, 'name': 'alice', 'teams': []}
-        avatar = SFTPOnlyAvatar('alice', tmpdir, '/dev/null', userDict)
+        avatar = SFTPOnlyAvatar('alice', self.tmpdir, '/dev/null',
+                                self.aliceUserDict)
         root = SFTPServerRoot(avatar)
         self.assertEqual(
             [name for name, child in root.children()], 
             ['.', '..', '~alice'])
-        shutil.rmtree(tmpdir)
 
     def testListDirTeams(self):
         # list only user dir + team dirs
-        tmpdir = self.mktemp()
-        os.mkdir(tmpdir)
-        userDict = {
-            'id': 1, 
-            'name': 'alice', 
-            'teams': [{'id': '1', 'name': 'alice'},
-                      {'id': '2', 'name': 'test-team'}]
-        }
-        avatar = SFTPOnlyAvatar('alice', tmpdir, '/dev/null', userDict)
+        
+        # Add a team to Alice's user dict
+        userDict = self.aliceUserDict.copy()
+        userDict['teams'].append({'id': '2', 'name': 'test-team'})
+        avatar = SFTPOnlyAvatar('alice', self.tmpdir, '/dev/null', userDict)
         root = SFTPServerRoot(avatar)
         self.assertEqual(
             [name for name, child in root.children()], 
             ['.', '..', '~alice', '~test-team'])
-        shutil.rmtree(tmpdir)
 
     def testAllWriteOpsForbidden(self):
         # mkdir
