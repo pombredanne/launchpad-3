68a7c1816f04ef96f192f97e5da112f4a7f873cb
Bjorn Tillenius <bjorn.tillenius@canonical.com>
2005-12-19 07:31:24 +0200

    review comments

--- lib/canonical/launchpad/mail/ftests/helpers.py
+++ lib/canonical/launchpad/mail/ftests/helpers.py
@@ -10,8 +10,7 @@ import os.path
 
 from canonical.launchpad.mail.signedmessage import SignedMessage
 
-testmails_path = os.path.join(
-                    os.path.dirname(__file__), 'emails') + os.path.sep
+testmails_path = os.path.join(os.path.dirname(__file__), 'emails')
 
 def read_test_message(filename):
     """Reads a test message and returns it as ISignedMessage.
@@ -19,4 +18,4 @@ def read_test_message(filename):
     The test messages are located in canonical/launchpad/mail/ftests/emails
     """
     return email.message_from_file(
-        open(testmails_path + filename), _class=SignedMessage)
+        open(os.path.join(testmails_path, filename)), _class=SignedMessage)
