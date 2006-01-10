af898430e45a0b1a8d7fdc09c3c1a9d4024c1e4b
Brad Bollenbach <brad.bollenbach@canonical.com>
2006-01-10 11:18:05 -0600

    remove references to status explanation in the UI

--- lib/canonical/launchpad/database/bugtask.py
+++ lib/canonical/launchpad/database/bugtask.py
@@ -414,17 +414,6 @@ class BugTaskSet:
                 " (BugTask.targetnamecache ILIKE '%%' || %s || '%%'))" % (
                 searchtext_quoted, searchtext_quoted, searchtext_like_quoted))
 
-        if params.statusexplanation:
-            # XXX: This clause relies on the fact that the Bugtask's fti is
-            # generated using only the values of the statusexplanation column,
-            # which is not true. Unfortunately, there's no way to fix this
-            # right now, and as this doesn't seem to be a big deal, we'll
-            # leave it as is for now. More info:
-            # https://launchpad.net/products/launchpad/+bug/4066
-            # -- Guilherme Salgado, 2005-11-09
-            extra_clauses.append("BugTask.fti @@ ftq(%s)" %
-                                 sqlvalues(params.statusexplanation))
-        
         if params.subscriber is not None:
             clauseTables = ['Bug', 'BugSubscription']
             extra_clauses.append("""Bug.id = BugSubscription.bug AND
