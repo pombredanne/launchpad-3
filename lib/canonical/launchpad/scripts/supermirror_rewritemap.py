75803b6ffc59535d9af24ff94fffbda5db522ca2
Andrew Bennetts <andrew.bennetts@canonical.com>
2006-01-11 16:29:17 +1100

    Small refactor of supermirror_rewritemap.

--- lib/canonical/launchpad/scripts/supermirror_rewritemap.py
+++ lib/canonical/launchpad/scripts/supermirror_rewritemap.py
@@ -12,17 +12,34 @@ from canonical.launchpad.interfaces import IBranchSet
 def write_map(outfile):
     branches = getUtility(IBranchSet)
     for branch in branches:
-        person_name = branch.owner.name
-        product = branch.product
-        if product is None:
-            product_name = '+junk'
-        else:
-            product_name = branch.product.name
-        branch_name = branch.name
+        line = generate_mapping_for_branch(branch)
+        outfile.write(line)
 
-        h = "%08x" % int(branch.id)
-        branch_location = '%s/%s/%s/%s' % (h[:2], h[2:4], h[4:6], h[6:])
 
-        outfile.write('~%s/%s/%s\t%s\n' % 
-            (person_name, product_name, branch_name, branch_location))
+def generate_mapping_for_branch(branch):
+    person_name = branch.owner.name
+    product = branch.product
+    if product is None:
+        product_name = '+junk'
+    else:
+        product_name = branch.product.name
+    branch_name = branch.name
+    
+    branch_location = split_branch_id(branch.id)
+    
+    return ('~%s/%s/%s\t%s\n' % 
+        (person_name, product_name, branch_name, branch_location))
+
+
+def split_branch_id(branch_id):
+    """Split a branch ID over multiple directories.
+
+    e.g.:
+    
+        >>> split_branch_id(0xabcdef12)
+        'ab/cd/ef/12'
+    """
+    h = "%08x" % int(branch_id)
+    branch_location = '%s/%s/%s/%s' % (h[:2], h[2:4], h[4:6], h[6:])
+    return branch_location
 
