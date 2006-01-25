ca2caa9aecce54b2188051d707dddffcd5af633f
Celso Providelo <celso.providelo@canonical.com>
2006-01-25 13:39:10 +0000

    Small fix on patch-40-10-0 and soyuz-upload.txt

--- database/schema/patch-40-10-0.sql
+++ database/schema/patch-40-10-0.sql
@@ -1,6 +1,5 @@
 SET client_min_messages=ERROR;
 
-ALTER TABLE DistroReleaseQueue DROP COLUMN changesfile;
 ALTER TABLE DistroReleaseQueue ADD COLUMN changesfile INTEGER;
 ALTER TABLE DistroReleaseQueue ALTER COLUMN changesfile SET NOT NULL;
 ALTER TABLE DistroReleaseQueue ADD CONSTRAINT
