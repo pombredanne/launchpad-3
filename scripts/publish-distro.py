5ad2360f215e9e4f7e71bcc89296c0f80964bb79
Daniel Silverstone <daniel.silverstone@canonical.com>
2006-01-26 09:24:38 +0000

    Flush caches between sources and binary publishing

--- scripts/publish-distro.py
+++ scripts/publish-distro.py
@@ -156,6 +156,8 @@ try:
                            sqlvalues(PackagePublishingStatus.PENDING))
     spps = SourcePackageFilePublishing.select(clause, orderBy=['componentname', 'sourcepackagename', 'libraryfilealiasfilename'])
     pub.publish(spps, isSource=True)
+    debug("Flushing caches.")
+    clear_cache()
     debug("Attempting to publish pending binaries.")
     pps = BinaryPackageFilePublishing.select(clause, orderBy=['componentname', 'sourcepackagename', 'libraryfilealiasfilename'])
     pub.publish(pps, isSource=False)
