9e9afce2ada6f152aaada45cbd659572b624eff9
Celso Providelo <celso.providelo@canonical.com>
2006-01-28 15:26:25 +0000

    Add 'update' cmdline options to build-slave-chroot-tool, resign bits of code.

--- lib/canonical/buildd/slave_chroot_tool.py
+++ lib/canonical/buildd/slave_chroot_tool.py
@@ -9,7 +9,7 @@ Build and manage chroots for launchpad (slave tool)
 __metaclass__ = type
 
 import sys, os
-
+from optparse import OptionParser
 from ConfigParser import SafeConfigParser
 import tempfile
 
@@ -24,7 +24,7 @@ def execute(cmd):
         raise OSError("Command %s failed" % cmd)
 
 def affirmed(response):
-    return response in ('y', 'yes', 'happy', 'gay', '')
+    return response in ('y', 'yes', '')
 
 class SlaveChrootBuilder:
     """Manage the process of building a chroot."""
@@ -56,7 +56,7 @@ class SlaveChrootBuilder:
                     raise ChrootError("Expected key %s in section %s" %
                                       (key,section))
                 self.config[section][key] = self.conf.get(section, key)
-                
+
     def makeTreeRoot(self):
         self.treeroot = tempfile.mkdtemp(dir=os.environ["HOME"],
                                          prefix="build-")
@@ -70,14 +70,16 @@ class SlaveChrootBuilder:
             print "Intervening at stage %s" % stage
             os.system("sudo chroot %s/chroot-autobuild /bin/su -" %
                       self.treeroot)
+
     def debootstrap(self):
         """Run debootstrap with the arguments from the config."""
         chrconf = self.config["chroot"]
-        
+
         if chrconf["architecture"] == "host":
             print "Obtaining host architecture..."
-            chrconf["architecture"] = \
-                os.popen("dpkg --print-installation-architecture").read().strip()
+            dpkg_proc = os.popen("dpkg --print-installation-architecture")
+            chrconf["architecture"] = dpkg_proc.read().strip()
+
         print "Debootstrapping %s/%s/%s (%s) from %s ..." % (
             chrconf["distribution"],
             chrconf["distrorelease"],
@@ -85,17 +87,18 @@ class SlaveChrootBuilder:
             chrconf["variant"],
             chrconf["debootstraparchive"])
 
-        execute("sudo debootstrap --resolve-deps --arch %s --variant=%s %s %s %s" % (
-            chrconf["architecture"],
-            chrconf["variant"],
-            chrconf["distrorelease"],
-            self.treeroot+"/chroot-autobuild",
-            chrconf["debootstraparchive"]))
+        execute("sudo debootstrap --resolve-deps --arch %s "
+                "--variant=%s %s %s %s" % (
+                chrconf["architecture"],
+                chrconf["variant"],
+                chrconf["distrorelease"],
+                self.treeroot+"/chroot-autobuild",
+                chrconf["debootstraparchive"]))
 
         # Make the user, paths and chown them...
         self.makeUserAndPaths()
         self.setupApt(True)
-        
+
         print "Mouting chroot..."
         execute(self.bin+"/mount-chroot %s" % self.buildid)
         self.intervene("postbootstrap")
@@ -125,7 +128,7 @@ class SlaveChrootBuilder:
                 self.config['buildd']['user'],
                 self.config['buildd']['group'],
                 self.treeroot, path))
-            
+
         print "Touching files..."
         for file in self.config['buildd']['touchfiles'].split():
             execute("sudo touch %s/chroot-autobuild/%s" % (
@@ -159,7 +162,8 @@ class SlaveChrootBuilder:
             root = self.config['chroot']['debootstraparchive']
         else:
             print "Configuring production archive apt sources"
-        pockets = ["-"+pocket for pocket in
+
+        pockets = ["-" + pocket for pocket in
                    self.config['apt']['pockets'].split()]
         pockets.append('')
 
@@ -184,7 +188,7 @@ class SlaveChrootBuilder:
 
         execute("sudo rm -f %s/chroot-autobuild/etc/apt/sources.list" %(
             self.treeroot))
-        
+
         execute("sudo ln -s sources.list.%s "
                 "%s/chroot-autobuild/etc/apt/sources.list" %(
             components[0], self.treeroot))
@@ -201,11 +205,10 @@ class SlaveChrootBuilder:
 
     def umount(self):
         """Unmount the chroot ready for tarring up."""
-
         self.intervene("pretarup")
-        execute(self.bin+"/scan-for-processes %s || true" % self.buildid)
-        execute(self.bin+"/umount-chroot %s" % self.buildid)
-        
+        execute(self.bin + "/scan-for-processes %s || true" % self.buildid)
+        execute(self.bin + "/umount-chroot %s" % self.buildid)
+
     def run(self):
         self.loadConfig()
         # Okay, we know all the configuration we're expecting has been
@@ -214,7 +217,7 @@ class SlaveChrootBuilder:
         self.debootstrap()
         # The chroot is made and currently configured for pre-buildd
         # package installation, so update it...
-        execute(self.bin+"/update-debian-chroot %s" % self.buildid)
+        execute(self.bin + "/update-debian-chroot %s" % self.buildid)
         # And install anything asked for in the config
         execute("sudo chroot %s/chroot-autobuild apt-get install %s" % (
             self.treeroot, self.config['buildd']['extrapkgs']))
@@ -222,96 +225,113 @@ class SlaveChrootBuilder:
         # Set us up for the internal apt config
         self.setupApt()
         self.intervene('postfinalapt')
-        
+
         self.umount()
         # And tar it up...
 
         print "Happy to repack? [Y]"
         yesno = sys.stdin.readline().strip().lower()
+
         if affirmed(yesno):
             print "Preparing chroot tarball..."
-            execute("sudo tar -C %s -cjf "
-                    "chroot-%s-%s-%s.tar.bz2 chroot-autobuild" %(
-                self.treeroot, self.config['chroot']['distribution'],
-                self.config['chroot']['distrorelease'],
-                self.config['chroot']['architecture']))
-            execute("sudo chown $USER %s" % chroottar)
+            chroottarname = ("chroot-%s-%s-%s.tar.bz2"
+                             %(self.config['chroot']['distribution'],
+                               self.config['chroot']['distrorelease'],
+                               self.config['chroot']['architecture']))
+            execute("sudo tar -C %s -cjf %s chroot-autobuild"
+                    %(self.treeroot, chroottarname))
+            execute("sudo chown $USER %s" % chroottarname)
 
         print "Cleaning up..."
-        execute(self.bin+"/remove-build %s" % self.buildid)
+        execute(self.bin + "/remove-build %s" % self.buildid)
+
         if affirmed(yesno):
             print "Constructed chroot-%s-%s-%s.tar.bz2" % (
                 self.config['chroot']['distribution'],
                 self.config['chroot']['distrorelease'],
                 self.config['chroot']['architecture'])
 
-def do_generate(conffile):
-    """Generate a chroot based on the configuration file supplied"""
-    # Simple override which helps a lot during bootstrapping
-    os.environ["LC_ALL"] = "C"
-    SlaveChrootBuilder(conffile, slavebin).run()
-
-def do_intervene(chroottar):
+def proccess(chroottar, slavebin, cmd=None):
     """Unpack, mount, chroot in, umount, repack, the chroot"""
     # We use the slavebin for the buildds to do some tasks
     execute("mkdir $HOME/build-chroot-tool")
     print "Unpacking..."
-    execute(slavebin+"/unpack-chroot chroot-tool "+chroottar)
+    execute(slavebin + "/unpack-chroot chroot-tool " + chroottar)
     print "Mounting..."
-    execute(slavebin+"/mount-chroot chroot-tool")
-    print "Chrooting in..."
-    execute("sudo chroot $HOME/build-chroot-tool/chroot-autobuild /bin/su -")
+    execute(slavebin + "/mount-chroot chroot-tool")
+    # execute the desired comand within the chroot
+    if cmd:
+        print "Chrooting in..."
+        execute(cmd)
     # We eat the result code of scanning for processes
     print "Scanning for processes to kill..."
-    os.system(slavebin+"/scan-for-processes chroot-tool")
+    os.system(slavebin + "/scan-for-processes chroot-tool")
     print "Unmounting..."
-    execute(slavebin+"/umount-chroot chroot-tool")
+    execute(slavebin + "/umount-chroot chroot-tool")
     print "Happy to repack? [Y]"
     yesno = sys.stdin.readline().strip().lower()
     if affirmed(yesno):
         print "Re-packing..."
-        execute("sudo tar -C $HOME/build-chroot-tool -cjf %s chroot-autobuild" %
-                chroottar)
+        execute("sudo tar -C $HOME/build-chroot-tool -cjf %s chroot-autobuild"
+                % chroottar)
         execute("sudo chown $USER %s" % chroottar)
     print "Cleaning up..."
-    execute(slavebin+"/remove-build chroot-tool")
+    execute(slavebin + "/remove-build chroot-tool")
     if affirmed(yesno):
         print "Done, updated %s" % chroottar
     else:
         print "Done, did not update %s" % chroottar
 
-if __name__ == "__main__":
-    from optparse import OptionParser
-    oparser = OptionParser()
-    oparser.add_option("-r", "--binaryroot",
-                       dest="binaryroot",
-                       help="root of slave binaries",
-                       metavar="BINARYROOT",
-                       default="/usr/share/launchpad-buildd/slavebin")
-    
-    oparser.add_option( "-g", "--generate",
-                        dest="generator",
-                        help="configuration file for a chroot to generate",
-                        metavar="CONFIG",
-                        default=None)
+def do_generate(conffile, slavebin):
+    """Generate a chroot based on the configuration file supplied"""
+    # Simple override which helps a lot during bootstrapping
+    os.environ["LC_ALL"] = "C"
+    SlaveChrootBuilder(conffile, slavebin).run()
 
-    oparser.add_option("-i", "--intervene",
-                       dest="intervention",
-                       help="chroot to unpack and chroot into",
-                       metavar="CHROOT",
-                       default=None)
+def do_intervene(chroottar, slavebin):
+    chroot_in = ("sudo chroot $HOME/build-chroot-tool/chroot-autobuild "
+                 "/bin/su -")
+    proccess(chroottar, slavebin, cmd=chroot_in)
+
+def do_upgrade(chroottar, slavebin):
+    chroot_upgrade = slavebin + "/update-debian-chroot chroot-tool"
+    proccess(chroottar, slavebin, cmd=chroot_upgrade)
+
+def main():
+    oparser = OptionParser()
+
+    oparser.add_option("-r", "--binaryroot", dest="binaryroot",
+                       metavar="BINARYROOT",
+                       default="/usr/share/launchpad-buildd/slavebin",
+                       help="root of slave binaries")
+
+    oparser.add_option( "-g", "--generate", dest="generate",
+                        metavar="CONFIG", default=None,
+                        help="configuration file for a chroot to generate")
+
+    oparser.add_option("-i", "--intervene", dest="intervene",
+                       metavar="CHROOT", default=None,
+                       help="chroot to unpack and chroot into")
+
+    oparser.add_option("-u", "--upgrade", dest="upgrade",
+                       metavar="CHROOT", default=None,
+                       help="chroot to unpack and upgrade")
 
     (opts,args) = oparser.parse_args()
 
-    # xnor
-    if not ((opts.generator is None) ^  (opts.intervention is None)):
-        oparser.print_help(sys.stderr)
-        sys.exit(1)
-
     slavebin = opts.binaryroot
 
-    if opts.generator:
-        do_generate(opts.generator)
-    else:
-        do_intervene(opts.intervention)
+    actions = {
+        opts.generate: do_generate,
+        opts.intervene: do_intervene,
+        opts.upgrade: do_upgrade,
+        }
 
+    for option, function in actions.items():
+        if option is not None:
+            opt_path = os.path.abspath(option)
+            function(opt_path, slavebin)
+            return
+
+if __name__ == "__main__":
+    main()
