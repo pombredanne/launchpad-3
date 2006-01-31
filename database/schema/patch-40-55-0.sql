617fb104a623fed7dccad49348ae93329b21b89e
Guilherme Salgado <guilherme.salgado@canonical.com>
2006-01-31 09:32:26 -0200

    Forgot to add the DB patch.

---
+++ database/schema/patch-40-55-0.sql
@@ -0,0 +1,12 @@
+SET client_min_messages=ERROR;
+
+CREATE VIEW BinaryAndSourcePackageNameView AS
+    SELECT name FROM BinaryPackageName
+    
+    UNION
+
+    SELECT name FROM SourcePackageName
+;
+
+INSERT INTO LaunchpadDatabaseRevision VALUES (40, 55, 0);
+
