d0daeface552f21ed8544a40bc462b147388de11
Stuart Bishop <stuart.bishop@canonical.com>
2006-02-22 09:01:00 +0700

    Update test.py for Zope3.2

--- test.py
+++ test.py
@@ -16,7 +16,7 @@
 
 $Id: test.py 25177 2004-06-02 13:17:31Z jim $
 """
-import sys, os, psycopg, time
+import sys, os, psycopg, time, logging, warnings
 
 os.setpgrp() # So test_on_merge.py can reap its children
 
@@ -57,16 +57,22 @@ except KeyError:
     pass
 
 # Silence spurious warnings or turn them into errors
-import warnings
+#
+warnings.filterwarnings(
+        'ignore', 'PyCrypto', RuntimeWarning, 'twisted[.]conch[.]ssh'
+        )
+warnings.filterwarnings(
+        'ignore', 'publisherhttpserver', DeprecationWarning
+        )
 # Our Z3 is still using whrandom
-warnings.filterwarnings(
-        "ignore",
-        "the whrandom module is deprecated; please use the random module"
-        )
+#warnings.filterwarnings(
+#        "ignore",
+#        "the whrandom module is deprecated; please use the random module"
+#        )
 # Some stuff got deprecated in 2.4 that we can clean up
-warnings.filterwarnings(
-        "error", category=DeprecationWarning, module="email"
-        )
+#warnings.filterwarnings(
+#        "error", category=DeprecationWarning, module="email"
+#        )
 
 from canonical.ftests import pgsql
 # If this is removed, make sure canonical.ftests.pgsql is updated
@@ -76,47 +82,22 @@ pgsql.installFakeConnect()
 
 # This is a terrible hack to divorce the FunctionalTestSetup from
 # its assumptions about the ZODB.
-from zope.app.tests.functional import FunctionalTestSetup
-FunctionalTestSetup.__init__ = lambda *x: None
+# XXX: Still needed under Z3.2?
+#from zope.app.testing.functional import FunctionalTestSetup
+#FunctionalTestSetup.__init__ = lambda *x: None
 
-# Install our own test runner to to pre/post sanity checks
-import zope.app.tests.test
-from canonical.database.sqlbase import SQLBase, ZopelessTransactionManager
-class LaunchpadTestRunner(zope.app.tests.test.ImmediateTestRunner):
-    def precheck(self, test):
-        pass
+from zope.testing import testrunner
 
-    def postcheck(self, test):
-        '''Tests run at the conclusion of every top level test suite'''
-        # Confirm Zopeless teardown has been called if necessary
-        assert ZopelessTransactionManager._installed is None, \
-                'Test used Zopeless but failed to tearDown correctly'
-
-        # Confirm all database connections have been dropped
-        assert len(pgsql.PgTestSetup.connections) == 0, \
-                'Not all PostgreSQL connections closed'
-
-        # Disabled this check - we now optimize by only dropping the
-        # db if necessary
-        #
-        #con = psycopg.connect('dbname=template1')
-        #try:
-        #    cur = con.cursor()
-        #    cur.execute("""
-        #        SELECT count(*) FROM pg_database
-        #        WHERE datname='launchpad_ftest'
-        #        """)
-        #    r = cur.fetchone()[0]
-        #    assert r == 0, 'launchpad_ftest database not dropped'
-        #finally:
-        #    con.close()
-
-    def run(self, test):
-        self.precheck(test)
-        rv = super(LaunchpadTestRunner, self).run(test)
-        self.postcheck(test)
-        return rv
-zope.app.tests.test.ImmediateTestRunner = LaunchpadTestRunner
+defaults = [
+    # Find tests in the tests and ftests directories
+    '--tests-pattern=^f?tests$',
+    '--test-path=%s' % os.path.join(here, 'lib'),
+    '--package=canonical',
+    ]
 
 if __name__ == '__main__':
-    zope.app.tests.test.process_args()
+    result = testrunner.run(defaults)
+    # Cribbed from sourcecode/zope/test.py - avoid spurious error during exit.
+    logging.disable(999999999)
+    sys.exit(result)
+
