6427db0655a1b831a625b74ca77c27e46f45a619
Dafydd Harries <daf@rhydd.org>
2006-02-28 18:24:40 +0000

    don't allow setting Branch.url when Branch.url is None

--- lib/canonical/launchpad/browser/branch.py
+++ lib/canonical/launchpad/browser/branch.py
@@ -137,6 +137,16 @@ class BranchView(LaunchpadView):
 
 
 class BranchEditView(SQLObjectEditView):
+    def __init__(self, context, request):
+        # If the context URL is none, Make a copy of the field names list and
+        # remove 'url' from it. This is to prevent users from converting
+        # push/import branches to pull branches.
+
+        if context.url is None:
+            self.fieldNames = list(self.fieldNames)
+            self.fieldNames.remove('url')
+
+        SQLObjectEditView.__init__(self, context, request)
 
     def changed(self):
         self.request.response.redirect(canonical_url(self.context))
