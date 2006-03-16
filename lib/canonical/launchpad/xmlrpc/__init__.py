5f3d1477234ba88ba0df96812f09ff4fab57f3a6
Steve Alexander <steve.alexander@canonical.com>
2006-03-16 18:34:37 +0000

    Add some doctests of the xmlrpc self-test and infrastructural base-class.

--- lib/canonical/launchpad/xmlrpc/__init__.py
+++ lib/canonical/launchpad/xmlrpc/__init__.py
@@ -1,4 +1,5 @@
-# XMLRPC views and APIs on Launchpad objects.
+# Copyright 2006 Canonical Ltd., all rights reserved.
+"""XMLRPC views and APIs on Launchpad objects."""
 
 from canonical.launchpad.xmlrpc.application import *
 
