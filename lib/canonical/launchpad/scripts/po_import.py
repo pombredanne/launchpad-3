419b4c826859edb0a0e4dc63de7ba1be8c50a0da
Carlos Perello Marin <carlos.perello@canonical.com>
2006-03-17 15:04:19 +0000

    Commit the transaction after removing obsolete entries from the queue.

--- lib/canonical/launchpad/scripts/po_import.py
+++ lib/canonical/launchpad/scripts/po_import.py
@@ -46,7 +46,7 @@ class ImportProcess:
                 if removed_entries > 0:
                     self.logger.info('Removed %d entries from the queue.' %
                         removed_entries)
-                    zmt.commit()
+                    self.ztm.commit()
                 # Exit the loop.
                 break
 
