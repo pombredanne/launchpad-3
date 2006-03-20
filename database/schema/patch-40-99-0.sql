eeac5fd357dcd22e9c8cf9c14b83eacdcc6d089c
Matthew Paul Thomas <mpt@canonical.com>
2006-03-20 10:38:34 +0000

    Changes from Stuart's approval of the schema change.

--- database/schema/patch-40-99-0.sql
+++
@@ -1,4 +0,0 @@
-update Bug set description = summary || '\n\n' || description where summary is not null;
-alter table Bug drop column summary;
-
-INSERT INTO LaunchpadDatabaseRevision VALUES (40, 99, 0);
