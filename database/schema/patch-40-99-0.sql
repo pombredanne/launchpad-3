9e8ca58e88c8f10d2b96921bd0270afbd2f80bd2
Celso Providelo <celso.providelo@canonical.com>
2006-03-22 05:46:23 -0300

    Reviewed DB patch

--- database/schema/patch-40-99-0.sql
+++
@@ -1,6 +0,0 @@
-SET client_min_messages=ERROR;
-
-ALTER TABLE BuildQueue ADD COLUMN manual boolean;
-ALTER TABLE BuildQueue ALTER COLUMN manual SET DEFAULT FALSE;
-
-INSERT INTO LaunchpadDatabaseRevision VALUES (40, 99, 0);
