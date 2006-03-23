75297177bc6c9583238994ee74615255952732cc
Brad Bollenbach <brad.bollenbach@canonical.com>
2006-03-23 16:43:59 +0000

    rename BugBranch.fixed_in_revision to BugBranch.revision_hint

--- database/schema/comments.sql
+++ database/schema/comments.sql
@@ -14,7 +14,7 @@ COMMENT ON COLUMN Bug.description IS 'A detailed description of the bug. Initial
 COMMENT ON TABLE BugBranch IS 'A branch related to a bug, most likely a branch for fixing the bug.';
 COMMENT ON COLUMN BugBranch.bug IS 'The bug associated with this branch.';
 COMMENT ON COLUMN BugBranch.branch IS 'The branch associated to the bug.';
-COMMENT ON COLUMN BugBranch.fixed_in_revision IS 'The revision ID of the branch that fixes this bug.';
+COMMENT ON COLUMN BugBranch.revision_hint IS 'An optional revision at which this branch became interesting to this bug, and/or may contain a fix for the bug.';
 COMMENT ON COLUMN BugBranch.status IS 'The status of the bugfix in this branch.';
 COMMENT ON COLUMN BugBranch.whiteboard IS 'Additional information about the status of the bugfix in this branch.';
 
