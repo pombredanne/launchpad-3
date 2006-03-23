3fe3542c41328cb179ad40447d11fc654de42ec4
Andrew Bennetts <andrew.bennetts@canonical.com>
2006-03-23 23:32:26 +1100

    DBA-approved SQL (patch 40-35-0, added comment).

--- database/schema/patch-40-35-0.sql
+++ database/schema/patch-40-35-0.sql
@@ -3,5 +3,4 @@ SET client_min_messages=ERROR;
 
 ALTER TABLE LibraryFileContent ADD COLUMN md5 character(32);
 
--- XXX: need real revision number
-INSERT INTO LaunchpadDatabaseRevision VALUES (40, 99, 0);
+INSERT INTO LaunchpadDatabaseRevision VALUES (40, 35, 0);
