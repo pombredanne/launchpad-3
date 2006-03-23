4c00c5c5560abc39338461f888cbc0f6156b7f55
Matthew Paul Thomas <mpt@canonical.com>
2006-03-23 12:27:34 +0000

    Fixes from jamesh's review.

--- lib/canonical/launchpad/browser/product.py
+++ lib/canonical/launchpad/browser/product.py
@@ -512,8 +512,6 @@ class ProductSetView:
 
     __used_for__ = IProductSet
 
-    header = "Products registered in Launchpad"
-
     def __init__(self, context, request):
         self.context = context
         self.request = request
