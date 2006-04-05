8935b37988554b6566cb1b01287b3c8198e58728
David Allouche <david.allouche@canonical.com>
2006-04-05 18:04:50 +0200

    baz2bzr.py: quiet is a local variable

--- lib/importd/baz2bzr.py
+++ lib/importd/baz2bzr.py
@@ -47,21 +47,38 @@ class BatchProgress(DummyProgress):
             print '%d/%d %s' % (current, total, msg)
 
 
-def make_progress_bar():
+def make_progress_bar(quiet):
     if quiet:
         return DummyProgress()
     else:
         return BatchProgress()
 
 
-def make_printer():
+def make_printer(quiet):
     if quiet:
         return silent_printer
     else:
         return stdout_printer
 
 
-def main(quiet, series_id, blacklist_path, push_prefix=None):
+def parse_arguments(args):
+    if args[0] == '-q':
+        quiet = True
+        del args[0]
+    else:
+        quiet = False
+    series_id = args.pop(0)
+    blacklist_path = args.pop(0)
+    if args:
+        push_prefix = args.pop(0)
+    else:
+        push_prefix = None
+    assert not args, 'extraneous arguments: %r' % (args,)
+    return quiet, series_id, blacklist_path, push_prefix
+
+
+def main(args):
+    quiet, series_id, blacklist_path, push_prefix = parse_arguments(args)
     to_location = 'bzrworking'
     begin()
     series = ProductSeries.get(series_id)
@@ -73,8 +90,8 @@ def main(quiet, series_id, blacklist_path, push_prefix=None):
         print "Not exporting to bzr"
         return 0
     from_branch = pybaz.Version(from_branch)
-    progress_bar = make_progress_bar()
-    printer = make_printer()
+    progress_bar = make_progress_bar(quiet)
+    printer = make_printer(quiet)
     baz_import.import_version(
         to_location, from_branch, printer, 
         max_count=None, reuse_history_from=[],
@@ -85,10 +102,10 @@ def main(quiet, series_id, blacklist_path, push_prefix=None):
     branch = branch_from_series(series)
     commit()
     push_to = push_prefix + ('%08x' % branch.id)
-    bzr_push(to_location, push_to)
+    bzr_push(to_location, push_to, quiet)
     return 0
 
-def bzr_push(from_location, to_location):
+def bzr_push(from_location, to_location, quiet):
     # Duplicate code from bzrlib so we can use our custom ProgressBar.
     from bzrlib.transport import get_transport
     from bzrlib.branch import Branch
@@ -110,7 +127,7 @@ def bzr_push(from_location, to_location):
         stop_revision = br_from.last_revision()
         if stop_revision in br_to.revision_history():
             return
-        pb = make_progress_bar()
+        pb = make_progress_bar(quiet)
         br_to.fetch(br_from, stop_revision, pb)
         pullable_revs = br_to.pullable_revisions(br_from, stop_revision)
         if len(pullable_revs) > 0:
@@ -166,11 +183,6 @@ def initialize_zopeless():
 
 if __name__ == '__main__':
     args = sys.argv[1:]
-    if args[0] == '-q':
-        quiet = True
-        del args[0]
-    else:
-        quiet = False
     initialize_zopeless()
-    status = main(quiet, *args)
+    status = main(args)
     sys.exit(status)
