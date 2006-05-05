090e0b2093c47acf27c06f04010aed69e9b2b18c
Bjorn Tillenius <bjorn.tillenius@canonical.com>
2006-05-05 14:55:25 +0300

    cleanup.

--- lib/canonical/launchpad/xmlrpc/faults.py
+++ lib/canonical/launchpad/xmlrpc/faults.py
@@ -18,9 +18,14 @@ class LaunchpadFault(xmlrpclib.Fault):
     """Base class for a Launchpad XMLRPC fault."""
 
     error_code = None
+    msg_template = None
 
-    def __init__(self, msg):
-        assert self.error_code is not None, "Subclasses must define error_code."
+    def __init__(self, **kw):
+        assert self.error_code is not None, (
+            "Subclasses must define error_code.")
+        assert self.msg_template is not None, (
+            "Subclasses must define msg_template.")
+        msg = self.msg_template % kw
         xmlrpclib.Fault.__init__(self, self.error_code, msg)
 
 
@@ -28,44 +33,48 @@ class NoSuchProduct(LaunchpadFault):
     """There's no such product registered in Launchpad."""
 
     error_code = 10
+    msg_template = "No such product: %(product_name)s"
 
     def __init__(self, product_name):
-        LaunchpadFault.__init__(self, "No such product: %s." % product_name)
+        LaunchpadFault.__init__(self, product_name=product_name)
 
 
 class NoSuchPerson(LaunchpadFault):
     """There's no Person with the specified email registered in Launchpad."""
 
     error_code = 20
+    msg_template = (
+        "No such email is registered in Launchpad: %(email_address)s")
 
     def __init__(self, email_address):
-        LaunchpadFault.__init__(
-            self,
-            "No such email is registered in Launchpad: %s." % email_address)
+        LaunchpadFault.__init__(self, email_address=email_address)
 
 
 class NoSuchBranch(LaunchpadFault):
     """There's no Branch with the specified URL registered in Launchpad."""
 
     error_code = 30
+    msg_template = "No such branch: %(branch_url)s"
 
     def __init__(self, branch_url):
-        LaunchpadFault.__init__(self, "No such branch: %s" % branch_url)
+        LaunchpadFault.__init__(self, branch_url=branch_url)
 
 
 class NoSuchBug(LaunchpadFault):
     """There's no Bug with the specified id registered in Launchpad."""
 
     error_code = 40
+    msg_template = "No such bug: %(bug_id)s"
 
     def __init__(self, bug_id):
-        LaunchpadFault.__init__(self, "No such bug: %s" % bug_id)
+        LaunchpadFault.__init__(self, bug_id=bug_id)
 
 
 class BranchAlreadyRegistered(LaunchpadFault):
     """A branch with the same URL is already registered in Launchpad."""
 
     error_code = 50
+    msg_template = "%(branch_url)s is already registered."
 
     def __init__(self, branch_url):
-        LaunchpadFault.__init__(self, "%s is already registered." % branch_url)
+        LaunchpadFault.__init__(self, branch_url=branch_url)
