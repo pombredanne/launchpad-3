e7c9606c09b707ff0be1f8366c16d7031d931f27
Andrew Bennetts <andrew.bennetts@canonical.com>
2006-05-08 15:53:04 +1000

    Disable hard-coded test failure, so that bzr can be updating without needing a simultaneous update to launchpad.

--- lib/canonical/launchpad/scripts/supermirror/ftests/test_branchtomirror.py
+++ lib/canonical/launchpad/scripts/supermirror/ftests/test_branchtomirror.py
@@ -125,9 +125,11 @@ class TestBranchToMirror_SourceProblems(TestCaseInTempDir):
             pass
         else:
             self.assertEqual(1, branch.mirror_failures)
-            self.fail(
-                "Bug 39884 appears to be fixed. Close and remove this assert"
-                )
+            # XXX Andrew Bennetts 2006-05-08: disabled failure to make merging
+            # bzr 0.8 easier.
+            #self.fail(
+            #    "Bug 39884 appears to be fixed. Close and remove this assert"
+            #    )
 
 
 
