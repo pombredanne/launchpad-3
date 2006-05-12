f2dd0b6a6199f7b6221236c97a67c554bf55cf22
Carlos Perelló Marín <carlos.perello@canonical.com>
2006-05-12 11:30:15 +0200

    The DB patch is approved

--- database/schema/patch-40-53-0.sql
+++ database/schema/patch-40-53-0.sql
@@ -2,4 +2,4 @@ SET client_min_messages=ERROR;
 
 ALTER TABLE POSubmission ALTER COLUMN person SET NOT NULL;
 
-INSERT INTO LaunchpadDatabaseRevision VALUES (40, 99, 0);
+INSERT INTO LaunchpadDatabaseRevision VALUES (40, 53, 0);
