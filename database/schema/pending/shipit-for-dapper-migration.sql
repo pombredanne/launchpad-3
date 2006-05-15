e92a3d92e36e76e27d75937b509e169eb17a2505
Guilherme Salgado <guilherme.salgado@canonical.com>
2006-05-15 09:20:02 -0300

    Move data migration patch together with the schema patch.

--- database/schema/pending/shipit-for-dapper-migration.sql
+++
@@ -1,8 +0,0 @@
-
-UPDATE RequestedCDs SET quantityapproved = 0 WHERE quantityapproved IS NULL;
-ALTER TABLE RequestedCDs ALTER COLUMN quantityapproved SET NOT NULL;
-
--- All existing standard requests are for the Ubuntu flavour.
-UPDATE StandardShipItRequest SET flavour = 1;
-
-ALTER TABLE StandardShipItRequest ALTER COLUMN flavour SET NOT NULL;
