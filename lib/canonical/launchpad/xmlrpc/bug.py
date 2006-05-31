cc850ed5cefb679cb7d83a939cff2b015be34598
Brad Bollenbach <brad.bollenbach@canonical.com>
2006-05-31 15:12:15 -0400

    checkpoint

--- lib/canonical/launchpad/xmlrpc/bug.py
+++ lib/canonical/launchpad/xmlrpc/bug.py
@@ -5,9 +5,22 @@
 __metaclass__ = type
 __all__ = ["FileBugAPI"]
 
-from canonical.launchpad.webapp import LaunchpadXMLRPCView
+from zope.component import getUtility
+
+from canonical.launchpad.interfaces import IProductSet, IDistributionSet
+from canonical.launchpad.webapp import canonical_url, LaunchpadXMLRPCView
 
 class FileBugAPI(LaunchpadXMLRPCView):
+    """The XML-RPC API for filing bugs in Malone."""
 
-    def report_bug(self, title):
-        return title
+    def report_bug(self, product, distro, package, title, comment, status,
+                   assignee, security_related, private, subscribers):
+        if product:
+            target = getUtility(IProductSet).getByName(product)
+        elif distro:
+            target = getUtility(IDistributionSet).getByName(distro)
+
+        bug = target.createBug(
+            owner=self.user, title=title, comment=comment)
+
+        return canonical_url(bug)
