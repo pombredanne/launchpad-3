9f1a806afdafd4cf597a395e62ecf3bebdea2685
Andrew Bennetts <andrew.bennetts@canonical.com>
2006-06-07 16:40:44 +1000

    Update test_sftp.py to catch PermissionDenied rather than IOError (because newer Twisted on the server triggers better errors in bzrlib).

--- lib/canonical/supermirrorsftp/tests/test_sftp.py
+++ lib/canonical/supermirrorsftp/tests/test_sftp.py
@@ -1,6 +1,6 @@
 # Copyright 2004-2006 Canonical Ltd.  All rights reserved.
 
-"""Acceptance tests for Supermirror SFTP server's bzr support.
+"""Tests for Supermirror SFTP server's bzr support.
 """
 
 __metaclass__ = type
@@ -11,11 +11,13 @@ import os
 import shutil
 import gc
 import stat
+import sys
+import traceback
 
 from bzrlib.bzrdir import ScratchDir
 import bzrlib.branch
 from bzrlib.tests import TestCase as BzrTestCase
-from bzrlib.errors import NoSuchFile, NotBranchError
+from bzrlib.errors import NoSuchFile, NotBranchError, PermissionDenied
 from bzrlib.transport import get_transport
 from bzrlib.transport import sftp
 from bzrlib.tests import TestCase as BzrTestCase
@@ -30,6 +32,37 @@ from canonical.authserver.ftests.harness import AuthserverTacTestSetup
 
 class SFTPTests(SFTPTestCase):
 
+    # XXX: AndrewBennetts 2006-06-07:
+    # failUnlessRaises from twisted/trial/unittest.py, unlike pyunit's
+    # failUnlessRaises it returns the caught exception, making it possible to
+    # assert things about the attributes of the exception, not just the type of
+    # the exception.
+    def failUnlessRaises(self, exception, f, *args, **kwargs):
+        """fails the test unless calling the function C{f} with the given C{args}
+        and C{kwargs} does not raise C{exception}. The failure will report the
+        traceback and call stack of the unexpected exception.
+        
+        @param exception: exception type that is to be expected
+        @param f: the function to call
+    
+        @return: The raised exception instance, if it is of the given type.
+        @raise self.failureException: Raised if the function call does not raise
+            an exception or if it raises an exception of a different type.
+        """
+        try:
+            result = f(*args, **kwargs)
+        except exception, inst:
+            return inst
+        except:
+            raise self.failureException('%s raised instead of %s:\n %s'
+                                        % (sys.exc_info()[0],
+                                           exception.__name__,
+                                           ''.join(traceback.format_stack())))
+        else:
+            raise self.failureException('%s not raised (%r returned)'
+                                        % (exception.__name__, result))
+    assertRaises = failUnlessRaises
+
     def test_rmdir_branch(self):
         # Make some directories under ~testuser/+junk (i.e. create some empty
         # branches)
@@ -40,12 +73,9 @@ class SFTPTests(SFTPTestCase):
         self.failUnless(stat.S_ISDIR(transport.stat('bar').st_mode))
 
         # Remove a directory.
-        # XXX Andrew Bennetts 2006-03-30: 
-        #    bzrlib currently throws an IOError with no way to distinguish
-        #    "permission denied" errors from other kinds.  I expect bzrlib will
-        #    probably change its behaviour here at some point, causing a trivial
-        #    failure here.
-        self.assertRaises(IOError, transport.rmdir, 'foo')
+        e = self.assertRaises(PermissionDenied, transport.rmdir, 'foo')
+        self.failUnless(
+            "removing branch directory 'foo' is not allowed." in e.extra)
 
 
 def test_suite():
