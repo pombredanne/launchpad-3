10e8fba6bd45f361e20ad0469df1ee55d37078a7
Celso Providelo <celso.providelo@canonical.com>
2006-06-16 22:06:22 -0300

    tests splited and fixed, we have native publication workflow now

--- lib/canonical/archivepublisher/tests/test_publisher.py
+++ lib/canonical/archivepublisher/tests/test_publisher.py
@@ -11,8 +11,6 @@ from StringIO import StringIO
 
 from zope.component import getUtility
 
-from canonical.database.constants import UTC_NOW
-
 from canonical.archivepublisher.config import Config
 from canonical.archivepublisher.pool import (
     DiskPool, Poolifier)
@@ -23,18 +21,11 @@ from canonical.functional import ZopelessLayer
 
 from canonical.launchpad.ftests.harness import (
     LaunchpadZopelessTestCase, LaunchpadZopelessTestSetup)
-from canonical.launchpad.database.publishing import (
-    SourcePackagePublishing, SecureSourcePackagePublishingHistory,
-    BinaryPackagePublishing, SecureBinaryPackagePublishingHistory)
 from canonical.launchpad.interfaces import (
-    ILibraryFileAliasSet, IDistributionSet, IPersonSet, ISectionSet,
-    IComponentSet, ISourcePackageNameSet)
+    ILibraryFileAliasSet, IDistributionSet)
 
 from canonical.librarian.ftests.harness import LibrarianTestSetup
 from canonical.librarian.client import LibrarianClient
-
-from canonical.lp.dbschema import (
-    PackagePublishingStatus, PackagePublishingPocket, SourcePackageUrgency)
 
 
 class TestPublisher(LaunchpadZopelessTestCase):
@@ -46,7 +37,6 @@ class TestPublisher(LaunchpadZopelessTestCase):
         LaunchpadZopelessTestCase.setUp(self)
         self.library = LibrarianClient()
         self._distribution = getUtility(IDistributionSet)['ubuntutest']
-        self._distrorelease = self._distribution['breezy-autotest']
         self._config = Config(self._distribution)
         self._config.setupArchiveDirs()
 
@@ -68,50 +58,6 @@ class TestPublisher(LaunchpadZopelessTestCase):
             filename, len(content), StringIO(content), 'application/text')
         LaunchpadZopelessTestSetup.txn.commit()
         return getUtility(ILibraryFileAliasSet)[alias_id]
-
-    def getPubSource(self, sourcename, component, filename,
-                     filecontent="I do not care about sources."):
-        """Return a mock source publishing record."""
-
-        alias = self.addMockFile(filename, filecontent)
-
-        spn = getUtility(ISourcePackageNameSet).getOrCreateByName(sourcename)
-        section = getUtility(ISectionSet)['base']
-        component = getUtility(IComponentSet)['main']
-        person = getUtility(IPersonSet).get(1)
-
-        spr = self._distrorelease.createUploadedSourcePackageRelease(
-            sourcepackagename=spn.id,
-            maintainer=person.id,
-            creator=person.id,
-            component=component.id,
-            section=section.id,
-            urgency=SourcePackageUrgency.LOW,
-            dateuploaded=UTC_NOW,
-            version='666',
-            builddepends='',
-            builddependsindep='',
-            architecturehintlist='',
-            changelog='',
-            dsc='',
-            dscsigningkey=1,
-            manifest=None
-            )
-
-        spr.addFile(alias)
-
-        sspph = SecureSourcePackagePublishingHistory(
-            distrorelease=self._distrorelease.id,
-            sourcepackagerelease=spr.id,
-            component=spr.component.id,
-            section=spr.section.id,
-            status=PackagePublishingStatus.PENDING,
-            datecreated=UTC_NOW,
-            pocket=PackagePublishingPocket.RELEASE,
-            embargo=False
-            )
-
-        return SourcePackagePublishing.get(sspph.id)
 
     def getFakePubSource(self, sourcename, component, leafname,
                          section='', dr='',
@@ -151,117 +97,6 @@ class TestPublisher(LaunchpadZopelessTestCase):
             )
         for case in cases:
             self.assertEqual( case[3], p._pathfor(case[0], case[1], case[2]) )
-
-    def testPublish(self):
-        """Test publishOne in normal conditions (new file)."""
-        from canonical.archivepublisher import Publisher
-        pub_source = self.getPubSource( "foo", "main", "foo.dsc",
-                                        filecontent='Hello world')
-        pub_source.publish(self._dp, self._logger)
-        self.assertEqual(pub_source.status, PackagePublishingStatus.PUBLISHED)
-        foo_name = "%s/main/f/foo/foo.dsc" % self._pooldir
-        self.assertEqual(open(foo_name).read().strip(), 'Hello world')
-
-
-    def testPublishingOverwriteFileInPool(self):
-        """Test if publishOne refuses to overwrite a file in pool.
-
-        Check if it also keeps the original file content.
-        """
-        from canonical.archivepublisher import Publisher
-
-        # publish 'foo' by-hand and ensure it has a special content
-        foo_name = "%s/main/f/foo/foo.dsc" % self._pooldir
-        os.mkdir(os.path.join(self._pooldir, 'main'))
-        os.mkdir(os.path.join(self._pooldir, 'main', 'f'))
-        os.mkdir(os.path.join(self._pooldir, 'main', 'f', 'foo'))
-        open(foo_name, 'w').write('Hello world')
-
-        # try to publish 'foo' again, via publisher, and check the content
-        self._dp.scan()
-        pub_source = self.getPubSource("foo", "main", "foo.dsc",
-                                       filecontent="Something")
-        pub_source.publish(self._dp, self._logger)
-        self.assertEqual(pub_source.status,
-                         PackagePublishingStatus.PENDING)
-        self.assertEqual(open(foo_name).read().strip(), 'Hello world')
-
-    def testPublishingDiferentContents(self):
-        """Test if publishOne refuses to overwrite its own publication."""
-        from canonical.archivepublisher import Publisher
-
-        pub_source = self.getPubSource("foo", "main", "foo.dsc",
-                                           filecontent='foo is happy')
-        pub_source.publish(self._dp, self._logger)
-
-        foo_name = "%s/main/f/foo/foo.dsc" % self._pooldir
-        self.assertEqual(pub_source.status,
-                         PackagePublishingStatus.PUBLISHED)
-        self.assertEqual(open(foo_name).read().strip(), 'foo is happy')
-
-        # try to publish 'foo' again with a different content, it
-        # raises and keep the files with the original content.
-        pub_source2 = self.getPubSource("foo", "main", "foo.dsc",
-                                            'foo is depressing')
-        pub_source2.publish(self._dp, self._logger)
-        self.assertEqual(pub_source2.status,
-                         PackagePublishingStatus.PENDING)
-        self.assertEqual(open(foo_name).read().strip(), 'foo is happy')
-
-    def testPublishingAlreadyInPool(self):
-        """Test if publishOne works if file is already in Pool.
-
-        It should identify that the file has the same content and
-        mark it as PUBLISHED.
-        """
-        from canonical.archivepublisher import Publisher
-
-        pub_source = self.getPubSource("bar", "main", "bar.dsc",
-                                           filecontent='bar is good')
-        pub_source.publish(self._dp, self._logger)
-        bar_name = "%s/main/b/bar/bar.dsc" % self._pooldir
-        self.assertEqual(open(bar_name).read().strip(), 'bar is good')
-        self.assertEqual(pub_source.status,
-                         PackagePublishingStatus.PUBLISHED)
-
-        pub_source2 = self.getPubSource("bar", "main", "bar.dsc",
-                                            filecontent='bar is good')
-        pub_source2.publish(self._dp, self._logger)
-        self.assertEqual(pub_source2.status,
-                         PackagePublishingStatus.PUBLISHED)
-
-    def testPublishingSymlink(self):
-        """Test if publishOne moving publication between components.
-
-        After check if the pool file contents as the same, it should
-        create a symlink in the new pointing to the original file.
-        """
-        from canonical.archivepublisher import Publisher
-
-        content = 'am I a file or a symbolic link ?'
-        # publish sim.dsc in main and re-publish in universe
-        pub_source = self.getPubSource( "sim", "main", "sim.dsc",
-                                            filecontent=content)
-        pub_source2 = self.getPubSource( "sim", "universe", "sim.dsc",
-                                            filecontent=content)
-        pub_source.publish(self._dp, self._logger)
-        pub_source2.publish(self._dp, self._logger)
-        self.assertEqual(pub_source.status,
-                         PackagePublishingStatus.PUBLISHED)
-        self.assertEqual(pub_source2.status,
-                         PackagePublishingStatus.PUBLISHED)
-
-        # check the resulted symbolic link
-        sim_universe = "%s/universe/s/sim/sim.dsc" % self._pooldir
-        self.assertEqual(os.readlink(sim_universe),
-                         '../../../main/s/sim/sim.dsc')
-
-        # if the contests don't match it raises.
-        pub_source3 = self.getPubSource("sim", "restricted", "sim.dsc",
-                                            filecontent='It is all my fault')
-        pub_source3.publish(self._dp, self._logger)
-        self.assertEqual(pub_source3.sourcepackagepublishing.status,
-                         PackagePublishingStatus.PENDING)
 
     def testPublishOverrides(self):
         """canonical.archivepublisher.Publisher.publishOverrides should work"""
