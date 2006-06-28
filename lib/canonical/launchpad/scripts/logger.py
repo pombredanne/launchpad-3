333bc661560436b2e404a3766ff33b4496f27d52
Stuart Bishop <stuart.bishop@canonical.com>
2006-06-28 14:08:45 +0700

    Fix logger tests

--- lib/canonical/launchpad/scripts/logger.py
+++ lib/canonical/launchpad/scripts/logger.py
@@ -112,7 +112,8 @@ def logger_options(parser, default=logging.INFO):
     True
 
     Cleanup:
-    >>> reset_root_logger()
+    >>> from canonical.testing import reset_logging
+    >>> reset_logging()
 
     As part of the options parsing, the 'log' global variable is updated.
     This can be used by code too lazy to pass it around as a variable.
