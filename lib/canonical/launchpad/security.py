640affdff78ce74ce77d25e8f8f6b3317338b959
Matthew Paul Thomas <mpt@canonical.com>
2006-06-30 20:52:41 +1200

    Restricts translation uploads to owners, admins, and RosettaExperts. Also tweaks the Translations page to suggest linking the product to packages, rather than seeing if any linked packages are translatable (since we already know they're not).

--- lib/canonical/launchpad/security.py
+++ lib/canonical/launchpad/security.py
@@ -414,6 +414,23 @@ class ProductSeriesDrivers(AuthorizationBase):
         return user.inTeam(admins)
 
 
+class EditProductSeries(EditByOwnersOrAdmins):
+    usedfor = IProductSeries
+    
+    def checkAuthenticated(self, user):
+        """Allow product owner, Rosetta Experts, or admins.
+        """
+        if (user.inTeam(self.obj.product.owner)):
+            # The user is the owner of the product.
+            return True
+        
+        # Rosetta experts need to be able to upload translations.
+        rosetta_experts = getUtility(ILaunchpadCelebrities).rosetta_expert
+        
+        return (EditByOwnersOrAdmins.checkAuthenticated(self, user) or
+            user.inTeam(rosetta_experts))
+
+
 class EditBugTask(AuthorizationBase):
     """Permission checker for editing objects linked to a bug.
 
