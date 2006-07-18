9f09203b865b492720e6085e619eb1bed1ea25af
Stuart Bishop <stuart.bishop@canonical.com>
2006-07-18 18:43:42 +0700

    Comment

--- database/schema/patch-67-03-1.sql
+++ database/schema/patch-67-03-1.sql
@@ -1,5 +1,12 @@
 SET client_min_messages=ERROR;
 
+/*
+Daniel Silverstone (Kinnison): I'm not so convinced about the new constraints
+Daniel Silverstone (Kinnison): In particular, if the queue is to be used to model packages getting into derivatives (which makes sense) then we can't have the constraint
+stub: Can we keep it until then?
+Daniel Silverstone (Kinnison): stub: sure, constraints can always be relaxed
+*/
+
 ALTER TABLE DistroReleaseQueueBuild
     ADD CONSTRAINT distroreleasequeuebuild__distroreleasequeue__build__unique
     UNIQUE (distroreleasequeue, build);
