22723aed5bc7523f3e9c9d8ff980c534d6848d80
Brad Bollenbach <brad.bollenbach@canonical.com>
2006-07-18 17:34:12 -0400

    move pagetitles to the browser directory, and fix a glitch in the heading of the comment/attach file form

--- lib/canonical/launchpad/webapp/tales.py
+++ lib/canonical/launchpad/webapp/tales.py
@@ -26,7 +26,7 @@ from canonical.launchpad.interfaces import (
     NoCanonicalUrl, IBugSet, NotFoundError
     )
 from canonical.lp import dbschema
-import canonical.launchpad.pagetitles
+from canonical.launchpad.browser import pagetitles
 from canonical.launchpad.webapp import canonical_url, nearest_menu
 from canonical.launchpad.webapp.url import Url
 from canonical.launchpad.webapp.publisher import get_current_browser_request
@@ -570,23 +570,23 @@ class PageTemplateContextsAPI:
 
         Take the simple filename without extension from
         self.contextdict['template'].filename, replace any hyphens with
-        underscores, and use this to look up a string, unicode or function in
-        the module canonical.launchpad.pagetitles.
+        underscores, and use this to look up a string, unicode or
+        function in the module canonical.launchpad.browser.pagetitles.
 
-        If no suitable object is found in canonical.launchpad.pagetitles,
-        emit a warning that this page has no title, and return the default
-        page title.
+        If no suitable object is found in
+        canonical.launchpad.browser.pagetitles, emit a warning that this
+        page has no title, and return the default page title.
         """
         template = self.contextdict['template']
         filename = os.path.basename(template.filename)
         name, ext = os.path.splitext(filename)
         name = name.replace('-', '_')
-        titleobj = getattr(canonical.launchpad.pagetitles, name, None)
+        titleobj = getattr(pagetitles, name, None)
         if titleobj is None:
             # sabdfl 25/0805 page titles are now mandatory hence the assert
             raise AssertionError(
-                 "No page title in canonical.launchpad.pagetitles for %s"
-                 % name)
+                 "No page title in canonical.launchpad.browser.pagetitles "
+                 "for %s" % name)
         elif isinstance(titleobj, basestring):
             return titleobj
         else:
@@ -594,7 +594,7 @@ class PageTemplateContextsAPI:
             view = self.contextdict['view']
             title = titleobj(context, view)
             if title is None:
-                return canonical.launchpad.pagetitles.DEFAULT_LAUNCHPAD_TITLE
+                return pagetitles.DEFAULT_LAUNCHPAD_TITLE
             else:
                 return title
 
