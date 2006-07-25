23f0dc60b58c83420b8e1563f217c7060e77c55b
Diogo Matsubara <matsubara@async.com.br>
2006-07-25 20:18:38 -0300

    fix bug 45788, 50816 and some SprintAttendanceView refactoring.

--- lib/canonical/launchpad/browser/sprintattendance.py
+++ lib/canonical/launchpad/browser/sprintattendance.py
@@ -9,14 +9,34 @@ __all__ = [
 
 import datetime
 
-from zope.component import getUtility
-
-from canonical.launchpad.interfaces import ISprintAttendance, ILaunchBag
+from canonical.launchpad.interfaces import validate_date_interval
 
 from canonical.launchpad.webapp import canonical_url, GeneralFormView
 
+from canonical.launchpad import _
+
 
 class SprintAttendanceAddView(GeneralFormView):
+
+    def validate(self, form_values):
+        """Verify if the entered dates are sane.
+
+        sane dates mean:
+        - time_starts precedes time_ends;
+        - time_starts precedes self.context.time_ends;
+        - self.context.time_starts precedes time_ends.
+
+        """
+        time_starts = form_values['time_starts']
+        time_ends = form_values['time_ends']
+        msg = _("Attendee should come before going!")
+        validate_date_interval(time_starts, time_ends, error_msg=msg)
+        msg = _("Choose an arrival time before the end of the meeting.")
+        validate_date_interval(
+            time_starts, self.context.time_ends, error_msg=msg)
+        msg = _("Choose an ending date after the start of the meeting.")
+        validate_date_interval(
+            self.context.time_starts, time_ends, error_msg=msg)
 
     def process(self, attendee, time_starts, time_ends):
         if time_ends.hour == 0 and time_ends.minute == 0 and \
@@ -25,16 +45,13 @@ class SprintAttendanceAddView(GeneralFormView):
             # midnight in the morning of that day, when they probably want
             # the end of the day
             time_ends = time_ends + datetime.timedelta(0, 0, 0, 0, 59, 23)
-        if time_starts >= time_ends:
-            return 'Attendee should come before going!'
         if time_starts < self.context.time_starts:
             # Can't arrive before the conference starts, we assume that you
             # meant to say you will get there at the beginning
             time_starts = self.context.time_starts
-        if time_starts > self.context.time_ends:
-            # You do need to show up before the meeting ends
-            return 'Choose an arrival time before the end of the meeting'
         if time_ends > self.context.time_ends:
+            # Can't stay after the conference ends, we assume that you meant
+            # to say you will leave at the end.
             time_ends = self.context.time_ends
         self._nextURL = canonical_url(self.context)
         return self.context.attend(attendee, time_starts, time_ends)
