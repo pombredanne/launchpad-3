a32ae175424b243716b3ce048ca4d8b65ebcccef
Carlos Perello Marin <carlos.perello@canonical.com>
2006-08-10 08:23:35 +0200

    Small additions to the logging code

--- scripts/copy-missing-translations-from-parent.py
+++ scripts/copy-missing-translations-from-parent.py
@@ -40,11 +40,14 @@ def main(argv):
     distribution = getUtility(IDistributionSet)[options.distro]
     release = distribution[options.release]
 
+    logger_object.info('Starting...')
     release.copyMissingTranslationsFromParent()
 
     # Finally, we changed many things related with cached statistics,
     # let's update it.
+    logger_object.info('Updating DistroRelease statistics...')
     release.updateStatistics(ztm)
+    logger_object.info('Done...')
 
     # Commit the transaction.
     ztm.commit()
