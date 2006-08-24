e1f69e31d6e92da331fa382721546927cdb81e69
Christian Reis <christian.reis@canonical.com>
2006-08-24 09:30:31 -0300

    Factor DAR release file generation into a separate method; update some comments and debug output

--- lib/canonical/archivepublisher/publishing.py
+++ lib/canonical/archivepublisher/publishing.py
@@ -16,7 +16,8 @@ from canonical.launchpad.interfaces import pocketsuffix
 
 suffixpocket = dict((v, k) for (k, v) in pocketsuffix.items())
 # XXX: if people actually start seriously using ComponentSelections this
-# will need to be revisited. -- kiko, 2006-08-23
+# will need to be revisited. For instance, adding new components will
+# break places which use this list. -- kiko, 2006-08-23
 HARDCODED_COMPONENT_ORDER = ['main', 'restricted', 'universe', 'multiverse']
 
 DISTRORELEASE_STANZA = """Origin: %s
@@ -99,7 +100,7 @@ class Publisher(object):
         publishing records to be updated, and files to be placed on disk
         where necessary.
         """
-        self.log.debug("Step A: Publishing packages")
+        self.log.debug("* Step A: Publishing packages")
         for distrorelease in self.distro:
             more_dirt = distrorelease.publish(self._diskpool, self.log,
                                               is_careful=force_publishing)
@@ -107,7 +108,7 @@ class Publisher(object):
 
     def B_dominate(self, force_domination):
         """Second step in publishing: domination."""
-        self.log.debug("Step B: dominating packages")
+        self.log.debug("* Step B: dominating packages")
         judgejudy = Dominator(self.log)
         for distrorelease in self.distro:
             for pocket in PackagePublishingPocket.items:
@@ -125,6 +126,7 @@ class Publisher(object):
                 judgejudy.judgeAndDominate(distrorelease, pocket, self._config)
 
     def C_doFTPArchive(self, is_careful):
+        self.log.debug("* Step C: Set apt-ftparchive up and run it")
         self.apt_handler.run(is_careful)
 
     def D_writeReleaseFiles(self, is_careful):
@@ -134,7 +136,7 @@ class Publisher(object):
 
         Otherwise we include only pockets flagged as true in dirty_pockets.
         """
-        self.log.debug("Generating Release files.")
+        self.log.debug("* Step D: Generating Release files.")
         for distrorelease in self.distro:
             for pocket, suffix in pocketsuffix.items():
 
@@ -150,7 +152,7 @@ class Publisher(object):
                 self._writeDistroRelease(distrorelease, pocket)
 
     def E_sanitiseLinks(self):
-        self.log.debug("Sanitising links in the pool.")
+        self.log.debug("* Step E: Sanitising links in the pool.")
         self._diskpool.sanitiseLinks(HARDCODED_COMPONENT_ORDER)
 
     def isDirty(self, distrorelease, pocket):
@@ -160,6 +162,8 @@ class Publisher(object):
 
     def _writeDistroRelease(self, distrorelease, pocket):
         """Write out the Release files for the provided distrorelease."""
+        # XXX: untested method -- kiko, 2006-08-24
+
         # As we generate file lists for apt-ftparchive we record which
         # distroreleases and so on we need to generate Release files for.
         # We store this in release_files_needed and consume the information
@@ -174,64 +178,32 @@ class Publisher(object):
         all_components = set()
         all_architectures = set()
         all_files = set()
-
         for component, architectures in release_files_needed[full_name].items():
             all_components.add(component)
             for architecture in architectures:
-                self.log.debug("Writing Release file for %s/%s/%s" % (
-                    full_name, component, architecture))
-                if architecture != "source":
-                    # Strip "binary-" off the front of the architecture before
-                    # noting it in all_architectures
-                    clean_architecture = architecture[7:]
-                    all_architectures.add(clean_architecture)
-                    file_stub = "Packages"
-
-                    # Set up the debian-installer paths, which are nested
-                    # inside the component
-                    di_path = os.path.join(component, "debian-installer",
-                                           architecture)
-                    di_file_stub = os.path.join(di_path, file_stub)
-                    for suffix in ('', '.gz', '.bz2'):
-                        all_files.add(di_file_stub + suffix)
-                else:
-                    file_stub = "Sources"
-                    clean_architecture = architecture
-
-                # Now, grab the actual (non-di) files inside each of
-                # the suite's architectures
-                file_stub = os.path.join(component, architecture, file_stub)
-
-                for suffix in ('', '.gz', '.bz2'):
-                    all_files.add(file_stub + suffix)
-
-                all_files.add(os.path.join(component, architecture, "Release"))
-
-                f = open(os.path.join(self._config.distsroot, full_name,
-                                      component, architecture, "Release"), "w")
-
-                contents = DISTROARCHRELEASE_STANZA % (full_name, 
-                    distrorelease.version, component,
-                    self.distro.displayname, self.distro.displayname,
-                    clean_architecture)
-                f.write(contents)
-                f.close()
+                self._writeDistroArchRelease(
+                    distrorelease, pocket, component, architecture,
+                    all_files, all_architectures)
 
         drsummary = "%s %s " % (self.distro.displayname,
                                 distrorelease.displayname)
-
         if pocket == PackagePublishingPocket.RELEASE:
             drsummary += distrorelease.version
         else:
             drsummary += pocket.name.capitalize()
 
         f = open(os.path.join(self._config.distsroot, full_name, "Release"), "w")
-        f.write(DISTRORELEASE_STANZA % (
-            self.distro.displayname, self.distro.displayname,
-            full_name, distrorelease.version, distrorelease.name,
-            datetime.utcnow().strftime("%a, %d %b %Y %k:%M:%S UTC"), 
-            " ".join(all_architectures), 
-            " ".join(reorder_components(all_components)), drsummary))
+        stanza = DISTRORELEASE_STANZA % (
+                    self.distro.displayname,
+                    self.distro.displayname,
+                    full_name,
+                    distrorelease.version,
+                    distrorelease.name,
+                    datetime.utcnow().strftime("%a, %d %b %Y %k:%M:%S UTC"),
+                    " ".join(all_architectures),
+                    " ".join(reorder_components(all_components)), drsummary)
+        f.write(stanza)
+
         f.write("MD5Sum:\n")
         all_files = sorted(list(all_files), key=os.path.dirname)
         for file_name in all_files:
@@ -239,11 +211,62 @@ class Publisher(object):
         f.write("SHA1:\n")
         for file_name in all_files:
             self._writeSumLine(full_name, f, file_name, sha)
+
         f.close()
 
+    def _writeDistroArchRelease(self, distrorelease, pocket, component,
+        # XXX: untested method -- kiko, 2006-08-24
+
+        architecture, all_files, all_architectures):
+        full_name = distrorelease.name + pocketsuffix[pocket]
+
+        self.log.debug("Writing Release file for %s/%s/%s" % (
+            full_name, component, architecture))
+        if architecture != "source":
+            file_stub = "Packages"
+
+            # Set up the debian-installer paths, which are nested
+            # inside the component
+            di_path = os.path.join(component, "debian-installer",
+                                   architecture)
+            di_file_stub = os.path.join(di_path, file_stub)
+            for suffix in ('', '.gz', '.bz2'):
+                all_files.add(di_file_stub + suffix)
+            # Strip "binary-" off the front of the architecture before
+            # noting it in all_architectures
+            clean_architecture = architecture[7:]
+        else:
+            file_stub = "Sources"
+            clean_architecture = architecture
+
+        all_architectures.add(clean_architecture)
+
+        # Now, grab the actual (non-di) files inside each of
+        # the suite's architectures
+        file_stub = os.path.join(component, architecture, file_stub)
+        for suffix in ('', '.gz', '.bz2'):
+            all_files.add(file_stub + suffix)
+        all_files.add(os.path.join(component, architecture, "Release"))
+
+        f = open(os.path.join(self._config.distsroot, full_name,
+                              component, architecture, "Release"), "w")
+        stanza = DISTROARCHRELEASE_STANZA % (
+                full_name,
+                distrorelease.version,
+                component,
+                self.distro.displayname,
+                self.distro.displayname,
+                clean_architecture)
+        f.write(stanza)
+        f.close()
+
+        return clean_architecture
+
     def _writeSumLine(self, distrorelease_name, out_file, file_name, sum_form):
-        """Write out a checksum line to the given file for the given
-        filename in the given form.
+        """Write out a checksum line.
+
+        Writes a checksum to the given file for the given filename in
+        the given form.
         """
         full_name = os.path.join(self._config.distsroot,
                                  distrorelease_name, file_name)
