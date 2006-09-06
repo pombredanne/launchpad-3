5c1bcfbcb632f551ccc85ed6eb1e5c437f5742aa
James Henstridge <james.henstridge@canonical.com>
2006-09-06 13:06:24 +0800

    fix up database constraints

--- database/schema/patch-67-95-0.sql
+++ database/schema/patch-67-95-0.sql
@@ -9,9 +9,26 @@ ALTER TABLE ProductSeries
   RENAME COLUMN branch TO import_branch;
 ALTER TABLE ProductSeries
   ADD COLUMN user_branch integer;
+
+/* Split the productseries_branch_fk constraint */
+ALTER TABLE ProductSeries
+  ADD CONSTRAINT productseries_import_branch_fk
+  FOREIGN KEY (import_branch) REFERENCES Branch(id);
+ALTER TABLE ProductSeries
+  ADD CONSTRAINT productseries_user_branch_fk
+  FOREIGN KEY (user_branch) REFERENCES Branch(id);
+ALTER TABLE ProductSeries
+  DROP CONSTRAINT productseries_branch_fk;
+
+/* Split the productseries_branch_key constraint */
+ALTER TABLE ProductSeries
+  ADD CONSTRAINT productseries_import_branch_key
+  UNIQUE (import_branch);
 ALTER TABLE ProductSeries
   ADD CONSTRAINT productseries_user_branch_key
-  FOREIGN KEY (user_branch) REFERENCES Branch(id);
+  UNIQUE (user_branch);
+ALTER TABLE ProductSeries
+  DROP CONSTRAINT productseries_branch_key;
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (67, 95, 0);
 
