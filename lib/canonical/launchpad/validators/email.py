2bb4f7096d3dc75830c1d2b92d9d3f8a0387dc9a
kiko <kiko@beetle>
2006-09-16 09:48:45 -0300

    Fix the validator to deal with some more corner cases, and extend tests.

--- lib/canonical/launchpad/validators/email.py
+++ lib/canonical/launchpad/validators/email.py
@@ -9,7 +9,19 @@ import re
 def valid_email(emailaddr):
     """Validate an email address.
 
+    >>> valid_email('kiko.async@hotmail.com')
+    True
+    >>> valid_email('kiko+async@hotmail.com')
+    True
+    >>> valid_email('kiko-async@hotmail.com')
+    True
+    >>> valid_email('kiko_async@hotmail.com')
+    True
+    >>> valid_email('kiko@async.com.br')
+    True
     >>> valid_email('kiko@canonical.com')
+    True
+    >>> valid_email('i@tv')
     True
 
     As per OOPS-256D762:
@@ -21,9 +33,15 @@ def valid_email(emailaddr):
     >>> valid_email('keith@-risby-family.co.uk')
     False
     """
-    if re.match(r"^[_\.0-9a-zA-Z-+]+@[^-]([0-9a-zA-Z-]{1,}[^-]\.)*[a-zA-Z]{2,}$",
-                emailaddr):
-        return True
-    else:
+    email_re = r"^[_\.0-9a-zA-Z-+]+@(([0-9a-zA-Z-]{1,}\.)*)[a-zA-Z]{2,}$"
+    email_match = re.match(email_re, emailaddr)
+    if not email_match:
         return False
+    host_minus_tld = email_match.group(1)
+    if not host_minus_tld:
+        return True
+    for part in host_minus_tld.split("."):
+        if part.startswith("-") or part.endswith("-"):
+            return False
+    return True
 
