f832e88bdf47a4fa4955496e0edb28f59328f4b5
Bjorn Tillenius <bjorn.tillenius@canonical.com>
2006-09-19 14:25:50 +0300

    provide a better error message than 'wrong contained type'.

--- lib/canonical/widgets/bug.py
+++ lib/canonical/widgets/bug.py
@@ -3,10 +3,13 @@
 __metaclass__ = type
 
 from zope.app.form.browser.textwidgets import IntWidget, TextWidget
+from zope.app.form.interfaces import ConversionError, WidgetInputError
 from zope.component import getUtility
-from zope.app.form.interfaces import ConversionError
+from zope.schema.interfaces import ConstraintNotSatisfied
 
 from canonical.launchpad.interfaces import IBugSet, NotFoundError
+from canonical.launchpad.validators import LaunchpadValidationError
+
 
 class BugWidget(IntWidget):
     """A widget for displaying a field that is bound to an IBug."""
@@ -50,3 +53,22 @@ class BugTagsWidget(TextWidget):
         else:
             return sorted(tag.lower() for tag in input.split())
 
+    def getInputValue(self):
+        try:
+            return TextWidget.getInputValue(self)
+        except WidgetInputError, input_error:
+            # The standard error message isn't useful at all. We look to
+            # see if it's a ConstraintNotSatisfied error and change it
+            # to a better one. For simplicity, we care only about the
+            # first error.
+            validation_errors = input_error.args[2]
+            for validatation_error in validation_errors.args[0]:
+                if isinstance(validatation_error, ConstraintNotSatisfied):
+                    self._error = WidgetInputError(
+                        input_error.args[0], input_error.args[1],
+                        LaunchpadValidationError(
+                            "'%s' isn't a valid tag name",
+                            validatation_error.args[0]))
+                raise self._error
+            else:
+                raise
