a51e5937be2b54d1dcdf0af294cc35202d448a18
David Allouche <david.allouche@canonical.com>
2006-09-21 16:34:23 +0200

    db patch to remove unused ProductSeries fields

---
+++ database/schema/pending/ddaa-cleanup-productseries.sql
@@ -0,0 +1,16 @@
+
+-- Remove arch-related fields from ProductSeries
+
+alter table productseries drop column targetarcharchive;
+alter table productseries drop column targetarchcategory;
+alter table productseries drop column targetarchbranch;
+alter table productseries drop column targetarchversion;
+
+-- Remove the WHUI bkrepository field too
+
+alter table productseries drop column bkrepository;
+
+
+-- Reinstate the no_empty_string constraint that referred to the deleted fields
+
+alter table productseries add constraint no_empty_strings check (cvsroot <> ''::text AND cvsmodule <> ''::text AND cvsbranch <> ''::text AND svnrepository <> ''::text);
