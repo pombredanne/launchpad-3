36f673bd25297d21d9b64957144ffd27852cf40e
David Allouche <david.allouche@canonical.com>
2006-09-29 15:15:02 +0200

    database patch got DBA approved

--- database/schema/patch-67-22-0.sql
+++ database/schema/patch-67-22-0.sql
@@ -1,3 +1,4 @@
+SET client_min_messages=ERROR;
 
 -- Remove arch-related fields from ProductSeries
 
@@ -14,3 +15,5 @@ alter table productseries drop column bkrepository;
 -- Reinstate the no_empty_string constraint that referred to the deleted fields
 
 alter table productseries add constraint no_empty_strings check (cvsroot <> ''::text AND cvsmodule <> ''::text AND cvsbranch <> ''::text AND svnrepository <> ''::text);
+
+INSERT INTO LaunchpadDatabaseRevision VALUES (67, 22, 0);
