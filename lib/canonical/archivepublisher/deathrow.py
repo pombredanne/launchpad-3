56984631894959a345bfcb97ee42cd32d150c5c1
Malcolm Cleaton <malcolm.cleaton@canonical.com>
2006-10-06 16:34:35 +0100

    Fix bug 62584 (over-zealous file deletion) and quieten some logging noise.

--- lib/canonical/archivepublisher/deathrow.py
+++ lib/canonical/archivepublisher/deathrow.py
@@ -94,24 +94,34 @@ class DeathRow:
         condemned_records = set()
         details = {}
 
-        # XXX: these two queries need to check
-        # SourcePackagePublishing.scheduleddeletiondate or else they
-        # will risk deleting stuff which has just been moved into
-        # PENDINGREMOVAL without going through the mandatory stay of
-        # execution. -- kiko, 2006-08-23
         live_source_files = SourcePackageFilePublishing.select(
-            "publishingstatus NOT IN (%s, %s) AND distribution = %s" %
-            sqlvalues(PackagePublishingStatus.PENDINGREMOVAL,
-                      PackagePublishingStatus.REMOVED,
-                      self.distribution),
-                      orderBy="id")
-
+            """
+            distribution = %s AND 
+            publishingstatus != %s AND
+            SourcePackagePublishingHistory.id =
+            SourcePackageFilePublishing.sourcepackagepublishing AND
+            (publishingstatus != %s OR
+             SourcePackagePublishingHistory.scheduleddeletiondate > %s)
+            """ % sqlvalues(self.distribution,
+                            PackagePublishingStatus.REMOVED,
+                            PackagePublishingStatus.PENDINGREMOVAL,
+                            UTC_NOW),
+            clauseTables = ["SourcePackagePublishingHistory"],
+            orderBy="id")
         live_binary_files = BinaryPackageFilePublishing.select(
-            "publishingstatus NOT IN (%s, %s) AND distribution = %s" %
-            sqlvalues(PackagePublishingStatus.PENDINGREMOVAL,
-                      PackagePublishingStatus.REMOVED,
-                      self.distribution),
-                      orderBy="id")
+            """
+            distribution = %s AND 
+            publishingstatus != %s AND
+            BinaryPackagePublishingHistory.id =
+            BinaryPackageFilePublishing.binarypackagepublishing AND
+            (publishingstatus != %s OR
+             BinaryPackagePublishingHistory.scheduleddeletiondate > %s)
+             """ % sqlvalues(self.distribution,
+                            PackagePublishingStatus.REMOVED,
+                            PackagePublishingStatus.PENDINGREMOVAL,
+                            UTC_NOW),
+            clauseTables = ["BinaryPackagePublishingHistory"],
+            orderBy="id")
 
         for p in live_source_files:
             filename = updateDetails(p, details)
