f884be5ecb4d70d8c7894b82bb1b114ada12a346
Bjorn Tillenius <bjorn.tillenius@canonical.com>
2006-10-11 13:11:22 +0300

    add XXX and clarify a comment.

--- lib/canonical/launchpad/database/bugwatch.py
+++ lib/canonical/launchpad/database/bugwatch.py
@@ -145,6 +145,11 @@ class BugWatchSet(BugSetBase):
                 continue
 
             for bugwatch in bug.watches:
+                #XXX: This matching is a bit fragile, since
+                #     bugwatch.remotebug is a user editable text string.
+                #     We should improve the matching so that for example
+                #     '#42' matches '42' and so on.
+                #     -- Bjorn Tillenius, 2006-10-11
                 if (bugwatch.bugtracker == bugtracker and
                     bugwatch.remotebug == remotebug):
                     break
