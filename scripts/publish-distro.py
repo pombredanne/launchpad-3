d3d7d56d9d14442441024c5a32eeba126960ba25
Celso Providelo <celso.providelo@canonical.com>
2006-10-20 09:50:03 -0300

    remove populate-sources prototype & the changes in the current publish-distro in order to make the branch ready for review.

--- scripts/publish-distro.py
+++ scripts/publish-distro.py
@@ -129,8 +129,6 @@ def main():
                    options.careful or options.careful_domination)
     try_and_commit("doing apt-ftparchive", publisher.C_doFTPArchive,
                    options.careful or options.careful_apt)
-    try_and_commit("generating Indexes", publisher.C_writeIndexes,
-                   options.careful or options.careful_apt)
     try_and_commit("doing release files", publisher.D_writeReleaseFiles,
                    options.careful)
     try_and_commit("santising links", publisher.E_sanitiseLinks)
