730313a8536a74e87f0c3d4ff7333eadccb7b038
Bjorn Tillenius <bjorn.tillenius@canonical.com>
2006-10-26 13:13:52 +0300

    doctweaks.

--- lib/canonical/launchpad/interfaces/bugtask.py
+++ lib/canonical/launchpad/interfaces/bugtask.py
@@ -515,8 +515,8 @@ class IBugTaskSet(Interface):
         If the bug is public, bug contacts will be automatically
         subscribed.
 
-        If the bug as any release nominations for a supplied
-        distribution, release tasks will be created for accepted ones.
+        If the bug has any release nominations for a supplied
+        distribution, release tasks will be created for the accepted ones.
 
         Exactly one of product, distribution or distrorelease must be provided.
         """
