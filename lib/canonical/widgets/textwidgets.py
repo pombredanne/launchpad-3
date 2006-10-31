44a581073d6b81155e554dcbf0c88bcf15090edc
James Henstridge <james.henstridge@canonical.com>
2006-10-31 17:32:57 +0800

    add LocalisedDateTimeWidget

--- lib/canonical/widgets/textwidgets.py
+++ lib/canonical/widgets/textwidgets.py
@@ -1,6 +1,11 @@
 # Copyright 2004-2006 Canonical Ltd.  All rights reserved.
 
+import datetime
+import pytz
+
+from zope.app.datetimeutils import parse, DateTimeError
 from zope.app.form.browser.textwidgets import TextWidget
+from zope.app.form.interfaces import ConversionError
 #XXX matsubara 2006-05-10: Should I move our NewLineToSpacesWidget to 
 # this module?
 
@@ -10,3 +15,28 @@ class StrippedTextWidget(TextWidget):
 
     def _toFieldValue(self, input):
         return TextWidget._toFieldValue(self, input.strip())
+
+
+class LocalisedDateTimeWidget(TextWidget):
+
+    timeZoneName = 'UTC'
+
+    def _toFieldValue(self, input):
+        if input == self._missing:
+            return self.context.missing_value
+        try:
+            year, month, day, hour, minute, second = parse(input)
+            second, micro = divmod(second, 1.0)
+            micro = round(micro * 1000000)
+            dt = datetime.datetime(year, month, day,
+                                   hour, minute, int(second), int(micro))
+        except (DateTimeError, ValueError, IndexError), v:
+            raise ConversionError('Invalid datetime data', v)
+        tz = pytz.timezone(self.timeZoneName)
+        return tz.localize(dt)
+        
+    def _toFormValue(self, value):
+        if value == self.context.missing_value:
+            return self._missing
+        tz = pytz.timezone(self.timeZoneName)
+        return value.astimezone(tz).strftime('%Y-%m-%d %H:%M:%S')
