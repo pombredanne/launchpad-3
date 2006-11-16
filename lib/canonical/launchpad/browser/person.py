132fb9c5c600e301a3ae6fe998ba8d085ac2d978
Guilherme Salgado <salgado@frida>
2006-11-16 12:56:43 -0800

    Create a new page under Launchpad's root to redirect non-logged in users to their +editlanguages page, after they log in. Also redirect from /rosetta/prefs to the new page, to keep old bookmarks working

--- lib/canonical/launchpad/browser/person.py
+++ lib/canonical/launchpad/browser/person.py
@@ -43,6 +43,8 @@ __all__ = [
     'ObjectReassignmentView',
     'TeamReassignmentView',
     'RedirectToAssignedBugsView',
+    'PersonLanguagesView',
+    'RedirectToEditLanguagesView',
     'PersonLatestTicketsView',
     'PersonSearchTicketsView',
     'PersonSupportMenu',
@@ -51,7 +53,6 @@ __all__ = [
     'SearchCommentedTicketsView',
     'SearchCreatedTicketsView',
     'SearchSubscribedTicketsView',
-    'PersonLanguagesView',
     ]
 
 import cgi
@@ -699,6 +700,21 @@ class PersonClaimView(LaunchpadFormView):
             "An email message was sent to '%(email)s'. Follow the "
             "instructions in that message to finish claiming this "
             "profile."), email=email)
+
+
+class RedirectToEditLanguagesView(LaunchpadView):
+    """Redirect the logged in user to his +editlanguages page.
+
+    This view should always be registered with a launchpad.AnyPerson
+    permission, to make sure the user is logged in. It exists so that
+    we can keep the /rosetta/prefs link working and also provide a link
+    for non logged in users that will require them to login and them send
+    them straight to the page they want to go.
+    """
+    
+    def initialize(self):
+        self.request.response.redirect(
+            '%s/+editlanguages' % canonical_url(self.user))
 
 
 class PersonRdfView:
@@ -2484,6 +2500,7 @@ class SearchSubscribedTicketsView(SearchTicketsView):
         return _('No support requests subscribed to by $name found with the '
                  'requested statuses.',
                  mapping=dict(name=self.context.displayname))
+
 
 class PersonSupportMenu(ApplicationMenu):
 
