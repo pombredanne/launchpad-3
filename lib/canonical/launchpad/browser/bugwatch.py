8b08b2718732dc18c437ea856c872e628d3c916a
Bjorn Tillenius <bjorn.tillenius@canonical.com>
2007-01-05 13:29:02 +0200

    convert bugwatch edit view to LaunchpadEditFormView.

--- lib/canonical/launchpad/browser/bugwatch.py
+++ lib/canonical/launchpad/browser/bugwatch.py
@@ -3,13 +3,33 @@
 """IBugWatch-related browser views."""
 
 __metaclass__ = type
-__all__ = ['BugWatchSetNavigation']
+__all__ = [
+    'BugWatchSetNavigation',
+    'BugWatchEditView']
 
-from canonical.launchpad.interfaces import IBugWatchSet
-from canonical.launchpad.webapp import GetitemNavigation
+
+from zope.component import getUtility
+
+from canonical.launchpad.interfaces import IBugWatch, IBugWatchSet, ILaunchBag
+from canonical.launchpad.webapp import (
+    action, canonical_url, GetitemNavigation, LaunchpadEditFormView)
 
 
 class BugWatchSetNavigation(GetitemNavigation):
 
     usedfor = IBugWatchSet
 
+
+class BugWatchEditView(LaunchpadEditFormView):
+    """View for editing a bug watch."""
+
+    schema = IBugWatch
+    field_names = ['bugtracker', 'remotebug']
+
+    @action('Change', name='change')
+    def change_action(self, action, data):
+        self.updateContextFromData(data)
+
+    @property
+    def next_url(self):
+        return canonical_url(getUtility(ILaunchBag).bugtask)
