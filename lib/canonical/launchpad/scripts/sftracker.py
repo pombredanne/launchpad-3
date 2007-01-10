eb6591092ed4cb8533a3b34c85ea469bcdcc9e6c
James Henstridge <james.henstridge@canonical.com>
2007-01-10 16:47:42 +0900

    add an XXX comment to the sftracker code noting that it would be good to make it generate XML for bug-import.py

--- lib/canonical/launchpad/scripts/sftracker.py
+++ lib/canonical/launchpad/scripts/sftracker.py
@@ -8,6 +8,11 @@ screen-scraping tools:
   http://effbot.org/zone/sandbox-sourceforge.htm
 """
 
+# XXX: 20070110 jamesh
+# It would be good to change this code so that it generates an XML
+# dump suitable for use with the bug-import.py script.  This would
+# reduce the number of bug importers we need to manage.
+
 __metaclass__ = type
 
 __all__ = [
