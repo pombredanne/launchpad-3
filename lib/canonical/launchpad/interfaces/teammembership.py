fc9f7c8cc69dcddbe8606211b9327e01005cd4ba
Guilherme Salgado <guilherme.salgado@canonical.com>
2007-01-17 16:00:55 -0200

    Fix https://launchpad.net/launchpad/+bug/70518: Notify team members when their membership is going to expire

--- lib/canonical/launchpad/interfaces/teammembership.py
+++ lib/canonical/launchpad/interfaces/teammembership.py
@@ -36,6 +36,11 @@ class ITeamMembership(Interface):
     def isExpired():
         """Return True if this membership's status is EXPIRED."""
 
+    def sendExpirationWarningEmail():
+        """Send an email to the member warning him that this membership will
+        expire soon.
+        """
+
     def setStatus(status, reviewer, reviewercomment=None):
         """Set the status of this membership.
         
@@ -49,11 +54,13 @@ class ITeamMembership(Interface):
 class ITeamMembershipSet(Interface):
     """A Set for TeamMembership objects."""
 
-    def getMembershipsToExpire():
+    def getMembershipsToExpire(when=None):
         """Return all TeamMemberships that should be expired.
 
+        If when is None, we use datetime.now().
+
         A TeamMembership should be expired when its expiry date is prior or
-        equal to today and its status is either ADMIN or APPROVED.
+        equal to :when: and its status is either ADMIN or APPROVED.
         """
 
     def new(person, team, status, dateexpires=None, reviewer=None,
