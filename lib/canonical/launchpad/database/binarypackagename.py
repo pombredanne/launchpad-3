6017b5be9d6564b573267e3025bdb93a2ff7f999
kiko <kiko@beetle>
2007-01-18 15:55:40 -0200

    Reorganize BinaryAndSourcePackage* into its own module, and kill PackageNameIterator in favor of just using two functions to accomplish what's needed cleanly.

--- lib/canonical/launchpad/database/binarypackagename.py
+++ lib/canonical/launchpad/database/binarypackagename.py
@@ -4,9 +4,8 @@ __metaclass__ = type
 __all__ = [
     'BinaryPackageName', 
     'BinaryPackageNameSet',
-    'BinaryAndSourcePackageName',
-    'BinaryAndSourcePackageNameVocabulary',
-    'BinaryPackageNameVocabulary'
+    'BinaryPackageNameVocabulary',
+    'getBinaryPackageDescriptions'
 ]
 
 # Zope imports
@@ -17,13 +16,11 @@ from zope.schema.vocabulary import SimpleTerm
 from sqlobject import (
     SQLObjectNotFound, StringCol, SQLMultipleJoin, CONTAINSSTRING)
 
-from canonical.database.sqlbase import SQLBase, sqlvalues, cursor
+from canonical.database.sqlbase import SQLBase, sqlvalues
 from canonical.launchpad.webapp.vocabulary import (
-    NamedSQLObjectHugeVocabulary, IHugeVocabulary, BatchedCountableIterator)
+    NamedSQLObjectHugeVocabulary, BatchedCountableIterator)
 from canonical.launchpad.interfaces import (
-    IBinaryPackageName, IBinaryPackageNameSet, NotFoundError,
-    IBinaryAndSourcePackageName)
-
+    IBinaryPackageName, IBinaryPackageNameSet, NotFoundError)
 from canonical.launchpad.database.binarypackagerelease import (
     BinaryPackageRelease)
 
@@ -86,158 +83,7 @@ class BinaryPackageNameSet:
             return BinaryPackageName(name=name)
 
 
-class BinaryAndSourcePackageName(SQLBase):
-    """See IBinaryAndSourcePackageName"""
-
-    implements(IBinaryAndSourcePackageName)
-
-    _table = 'BinaryAndSourcePackageNameView'
-    _idName = 'name'
-    _idType = str
-    _defaultOrder = 'name'
-
-    name = StringCol(dbName='name', notNull=True, unique=True,
-                     alternateID=True)
-
-
-class PackageNameIterator(BatchedCountableIterator):
-    """Base class for package name vocabulary iteration.
-
-    Includes convenience methods to build descriptions for source and
-    binary packages based on guesswork using the binary package releases
-    [loosely] associated to them.
-
-    Note that the reason we use special iterators is to ensure that we
-    only do the search for descriptions across source package names that
-    we actually are attempting to list, taking advantage of the
-    resultset slicing that BatchNavigator does.
-
-    WARNING: the methods in this class assume that there is little
-    overlap and much coherence in how package names are used, in
-    particular across distributions if derivation is implemented. IOW,
-    it does not make a promise to provide The Correct Description, but a
-    pretty good guess at what the description should be.
-    """
-    MAX_TITLE_LENGTH = 50
-    # XXX: this could live in a separate file to avoid needing to be
-    # imported from sourcepackagename.py, but where?
-    def getSourcePackageDescriptions(self, results, use_names=False):
-        """Return a dictionary with descriptions keyed on source package names.
-
-        Takes an ISelectResults of a *PackageName query. The use_names
-        flag is a hack that allows this method to work for the
-        BinaryAndSourcePackageName view, which lacks IDs.
-        """
-        # XXX: use_names could be removed if we instead added IDs to the
-        # BinaryAndSourcePackageName view, but we'd still need to find
-        # out how to specify the attribute, since it would be
-        # sourcepackagename_id and binarypackagename_id depending on
-        # whether the row represented one or both of those cases.
-        #   -- kiko, 2007-01-17
-        if use_names:
-           clause = ("SourcePackageName.name in %s" %
-                     sqlvalues([pn.name for pn in results]))
-        else:
-           clause = ("SourcePackageName.id in %s" %
-                     sqlvalues([spn.id for spn in results]))
-
-        cur = cursor()
-        cur.execute("""SELECT DISTINCT BinaryPackageName.name,
-                              SourcePackageName.name
-                         FROM BinaryPackageRelease, SourcePackageName, Build,
-                              SourcePackageRelease, BinaryPackageName
-                        WHERE
-                           BinaryPackageName.id = BinaryPackageRelease.id AND
-                           BinaryPackageRelease.build = Build.ID AND
-                           SourcePackageRelease.sourcepackagename =
-                               SourcePackageName.id AND
-                           Build.sourcepackagerelease =
-                               SourcePackageRelease.id AND
-                           %s
-                       ORDER BY BinaryPackageName.name,
-                                SourcePackageName.name"""
-                        % clause)
-
-        descriptions = {}
-        for binarypackagename, sourcepackagename in cur.fetchall():
-            if not descriptions.has_key(sourcepackagename):
-                descriptions[sourcepackagename] = (
-                    "Source of: %s" % binarypackagename)
-            else:
-                if len(descriptions[sourcepackagename]) > self.MAX_TITLE_LENGTH:
-                    description = "..."
-                else:
-                    description = ", %s" % binarypackagename
-                descriptions[sourcepackagename] += description
-        return descriptions
-
-    def getBinaryPackageDescriptions(self, results, use_names=False):
-        """See getSourcePackageDescriptions, which is analogous to this."""
-        if use_names:
-           clause = ("BinaryPackageName.name in %s" %
-                     sqlvalues([pn.name for pn in results]))
-        else:
-           clause = ("BinaryPackageName.id in %s" %
-                     sqlvalues([bpn.id for bpn in results]))
-
-        descriptions = {}
-        releases = BinaryPackageRelease.select(
-            """BinaryPackageRelease.binarypackagename =
-                BinaryPackageName.id AND
-               %s""" % clause,
-            clauseTables=["BinaryPackageRelease", "BinaryPackageName"],
-            orderBy=["-BinaryPackageRelease.datecreated"])
-
-        for release in releases:
-            binarypackagename = release.binarypackagename.name
-            if not descriptions.has_key(binarypackagename):
-                description = release.description.strip().replace("\n", " ")
-                if len(description) > self.MAX_TITLE_LENGTH:
-                    description = (release.description[:self.MAX_TITLE_LENGTH]
-                                  + "...")
-                descriptions[binarypackagename] = description
-        return descriptions
-
-
-class BinaryAndSourcePackageNameIterator(PackageNameIterator):
-    """Iterator for BinaryAndSourcePackageNameVocabulary.
-
-    Builds descriptions from source and binary descriptions it can
-    identify based on the names returned when queried.
-    """
-    def getTermsWithDescriptions(self, results):
-        # Note that we grab first source package descriptions and then
-        # binary package descriptions, giving preference to the latter,
-        # via the update() call.
-        descriptions = self.getSourcePackageDescriptions(results,
-                                                         use_names=True)
-        binary_descriptions = self.getBinaryPackageDescriptions(results,
-                                                                use_names=True)
-        descriptions.update(binary_descriptions)
-        return [SimpleTerm(obj, obj.name,
-                    descriptions.get(obj.name, "Not uploaded"))
-                for obj in results]
-
-
-class BinaryAndSourcePackageNameVocabulary(NamedSQLObjectHugeVocabulary):
-    """A vocabulary for searching for binary and sourcepackage names.
-
-    This is useful for, e.g., reporting a bug on a 'package' when a reporter
-    often has no idea about whether they mean a 'binary package' or a 'source
-    package'.
-
-    The value returned by a widget using this vocabulary will be either an
-    ISourcePackageName or an IBinaryPackageName.
-    """
-    implements(IHugeVocabulary)
-
-    _table = BinaryAndSourcePackageName
-    displayname = 'Select a Package'
-    _orderBy = 'name'
-    iterator = BinaryAndSourcePackageNameIterator
-
-
-class BinaryPackageNameIterator(PackageNameIterator):
+class BinaryPackageNameIterator(BatchedCountableIterator):
     """An iterator for BinaryPackageNameVocabulary.
 
     Builds descriptions based on releases of that binary package name.
@@ -245,7 +91,7 @@ class BinaryPackageNameIterator(PackageNameIterator):
     def getTermsWithDescriptions(self, results):
         # Prefill the descriptions dictionary with the latest
         # description uploaded for that package name.
-        descriptions = self.getBinaryPackageDescriptions(results)
+        descriptions = getBinaryPackageDescriptions(results)
         return [SimpleTerm(obj, obj.name,
                     descriptions.get(obj.name, "Not uploaded"))
                 for obj in results]
@@ -257,4 +103,35 @@ class BinaryPackageNameVocabulary(NamedSQLObjectHugeVocabulary):
     _orderBy = 'name'
     displayname = 'Select a Binary Package'
     iterator = BinaryPackageNameIterator
+
+
+def getBinaryPackageDescriptions(results, use_names=False, max_title_length=50):
+    """Return a dict of descriptions keyed by package name.
+
+    See sourcepackage.py:getSourcePackageDescriptions, which is analogous.
+    """
+    if use_names:
+       clause = ("BinaryPackageName.name in %s" %
+                 sqlvalues([pn.name for pn in results]))
+    else:
+       clause = ("BinaryPackageName.id in %s" %
+                 sqlvalues([bpn.id for bpn in results]))
+
+    descriptions = {}
+    releases = BinaryPackageRelease.select(
+        """BinaryPackageRelease.binarypackagename =
+            BinaryPackageName.id AND
+           %s""" % clause,
+        clauseTables=["BinaryPackageRelease", "BinaryPackageName"],
+        orderBy=["-BinaryPackageRelease.datecreated"])
+
+    for release in releases:
+        binarypackagename = release.binarypackagename.name
+        if not descriptions.has_key(binarypackagename):
+            description = release.description.strip().replace("\n", " ")
+            if len(description) > max_title_length:
+                description = (release.description[:max_title_length]
+                              + "...")
+            descriptions[binarypackagename] = description
+    return descriptions
 
--- lib/canonical/launchpad/database/sourcepackagename.py
+++ lib/canonical/launchpad/database/sourcepackagename.py
@@ -4,7 +4,8 @@ __metaclass__ = type
 __all__ = [
     'SourcePackageName',
     'SourcePackageNameSet',
-    'SourcePackageNameVocabulary'
+    'SourcePackageNameVocabulary',
+    'getSourcePackageDescriptions'
 ]
 
 from zope.interface import implements
@@ -13,15 +14,15 @@ from zope.schema.vocabulary import SimpleTerm
 from sqlobject import SQLObjectNotFound
 from sqlobject import StringCol, SQLMultipleJoin
 
-from canonical.database.sqlbase import SQLBase, quote_like
+from canonical.database.sqlbase import SQLBase, quote_like, cursor, sqlvalues
 
 from canonical.launchpad.webapp.vocabulary import (
     NamedSQLObjectHugeVocabulary)
 from canonical.launchpad.interfaces import (
     ISourcePackageName, ISourcePackageNameSet, NotFoundError)
 
-from canonical.launchpad.database.binarypackagename import (
-    PackageNameIterator)
+from canonical.launchpad.webapp.vocabulary import (
+    BatchedCountableIterator)
 
 
 class SourcePackageName(SQLBase):
@@ -85,14 +86,19 @@ class SourcePackageNameSet:
             return self.new(name)
 
 
-class SourcePackageNameIterator(PackageNameIterator):
+class SourcePackageNameIterator(BatchedCountableIterator):
     """A custom iterator for SourcePackageNameVocabulary.
 
     Used to iterate over vocabulary items and provide full
     descriptions.
+
+    Note that the reason we use special iterators is to ensure that we
+    only do the search for descriptions across source package names that
+    we actually are attempting to list, taking advantage of the
+    resultset slicing that BatchNavigator does.
     """
     def getTermsWithDescriptions(self, results):
-        descriptions = self.getSourcePackageDescriptions(results)
+        descriptions = getSourcePackageDescriptions(results)
         return [SimpleTerm(obj, obj.name,
                     descriptions.get(obj.name, "Not yet built"))
                 for obj in results]
@@ -104,4 +110,61 @@ class SourcePackageNameVocabulary(NamedSQLObjectHugeVocabulary):
     _table = SourcePackageName
     _orderBy = 'name'
     iterator = SourcePackageNameIterator
+
+
+def getSourcePackageDescriptions(results, use_names=False, max_title_length=50):
+    """Return a dictionary with descriptions keyed on source package names.
+
+    Takes an ISelectResults of a *PackageName query. The use_names
+    flag is a hack that allows this method to work for the
+    BinaryAndSourcePackageName view, which lacks IDs.
+
+    WARNING: this function assumes that there is little overlap and much
+    coherence in how package names are used, in particular across
+    distributions if derivation is implemented. IOW, it does not make a
+    promise to provide The Correct Description, but a pretty good guess
+    at what the description should be.
+    """
+    # XXX: use_names could be removed if we instead added IDs to the
+    # BinaryAndSourcePackageName view, but we'd still need to find
+    # out how to specify the attribute, since it would be
+    # sourcepackagename_id and binarypackagename_id depending on
+    # whether the row represented one or both of those cases.
+    #   -- kiko, 2007-01-17
+    if use_names:
+       clause = ("SourcePackageName.name in %s" %
+                 sqlvalues([pn.name for pn in results]))
+    else:
+       clause = ("SourcePackageName.id in %s" %
+                 sqlvalues([spn.id for spn in results]))
+
+    cur = cursor()
+    cur.execute("""SELECT DISTINCT BinaryPackageName.name,
+                          SourcePackageName.name
+                     FROM BinaryPackageRelease, SourcePackageName, Build,
+                          SourcePackageRelease, BinaryPackageName
+                    WHERE
+                       BinaryPackageName.id = BinaryPackageRelease.id AND
+                       BinaryPackageRelease.build = Build.ID AND
+                       SourcePackageRelease.sourcepackagename =
+                           SourcePackageName.id AND
+                       Build.sourcepackagerelease =
+                           SourcePackageRelease.id AND
+                       %s
+                   ORDER BY BinaryPackageName.name,
+                            SourcePackageName.name"""
+                    % clause)
+
+    descriptions = {}
+    for binarypackagename, sourcepackagename in cur.fetchall():
+        if not descriptions.has_key(sourcepackagename):
+            descriptions[sourcepackagename] = (
+                "Source of: %s" % binarypackagename)
+        else:
+            if len(descriptions[sourcepackagename]) > max_title_length:
+                description = "..."
+            else:
+                description = ", %s" % binarypackagename
+            descriptions[sourcepackagename] += description
+    return descriptions
 
