72ef26f338aff7b9fe92218a1479eeb1272d208d
Guilherme Salgado <guilherme.salgado@canonical.com>
2007-01-25 12:51:26 -0200

    add a comment to the db patch

--- database/schema/patch-67-77-0.sql
+++ database/schema/patch-67-77-0.sql
@@ -2,6 +2,7 @@ SET client_min_messages=ERROR;
 
 DELETE FROM KarmaCache;
 
+-- Prevent multiple rows with the same context for a given person and category
 CREATE UNIQUE INDEX karmacache__unique_context_and_category_by_person
     ON karmacache(person, category, coalesce(product, -1), 
                   coalesce(distribution, -1), coalesce(sourcepackagename, -1));
