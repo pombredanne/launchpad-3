f85681d638a3d639261a40092dfdbdc8cb21f113
Guilherme Salgado <guilherme.salgado@canonical.com>
2007-01-25 11:11:13 -0200

    fix the db patch

--- database/schema/patch-67-77-0.sql
+++ database/schema/patch-67-77-0.sql
@@ -3,7 +3,8 @@ SET client_min_messages=ERROR;
 DELETE FROM KarmaCache;
 
 CREATE UNIQUE INDEX karmacache__unique_context_and_category_by_person
-    ON karmacache(person, category, product, distribution, sourcepackagename);
+    ON karmacache(person, category, coalesce(product, -1), 
+                  coalesce(distribution, -1), coalesce(sourcepackagename, -1));
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (67, 77, 0);
 
