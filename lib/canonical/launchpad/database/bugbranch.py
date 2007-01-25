5d7a5376266b3f52f698b9ba70dbb214bda3f475
Tim Penhey <tim.penhey@canonical.com>
2007-01-25 13:55:44 +1300

    Basics working for IBugBranchSet

--- lib/canonical/launchpad/database/bugbranch.py
+++ lib/canonical/launchpad/database/bugbranch.py
@@ -4,7 +4,8 @@
 
 __metaclass__ = type
 
-__all__ = ["BugBranch"]
+__all__ = ["BugBranch",
+           "BugBranchSet"]
 
 from sqlobject import ForeignKey, StringCol
 
@@ -13,7 +14,7 @@ from zope.interface import implements
 from canonical.database.constants import UTC_NOW
 from canonical.database.datetimecol import UtcDateTimeCol
 from canonical.database.sqlbase import SQLBase
-from canonical.launchpad.interfaces import IBugBranch
+from canonical.launchpad.interfaces import IBugBranch, IBugBranchSet
 from canonical.lp.dbschema import EnumCol, BugBranchStatus
 
 
@@ -29,3 +30,15 @@ class BugBranch(SQLBase):
         dbName="status", schema=BugBranchStatus, notNull=False,
         default=BugBranchStatus.INPROGRESS)
     whiteboard = StringCol(notNull=False, default=None)
+
+
+class BugBranchSet:
+
+    implements(IBugBranchSet)
+
+    def getBugBranchesForBranches(self, branches):
+        "See IBugBranchSet."
+
+        branch_ids = [branch.id for branch in branches]
+        bugbranches = BugBranch.select(BugBranch.q.branchID in branch_ids)
+        return bugbranches.prejoin(['bug'])
