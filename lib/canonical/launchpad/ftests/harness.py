d893aed534e46fb3311c77422f22f8d6dc3398b0
Stuart Bishop <stuart.bishop@canonical.com>
2007-02-05 15:17:33 +0700

    Fix tests

--- lib/canonical/launchpad/ftests/harness.py
+++ lib/canonical/launchpad/ftests/harness.py
@@ -76,14 +76,15 @@ def _reconnect_sqlos(dbuser=None):
         dbuser = config.launchpad.dbuser
     da.connect(dbuser)
 
-    # Confirm that the database adapter *really is* connected and connected
-    # to the right database
+    # Confirm that the database adapter *really is* connected.
     assert da.isConnected(), 'Failed to reconnect'
-    confirm_dbrevision()
 
     # Confirm that the SQLOS connection cache has been emptied, so access
     # to SQLBase._connection will get a fresh Tranaction
     assert len(connCache.keys()) == 0, 'SQLOS appears to have kept connections'
+
+    # Confirm the database has the right patchlevel
+    confirm_dbrevision()
 
     # Confirm that SQLOS is again talking to the database (it connects
     # as soon as SQLBase._connection is accessed
