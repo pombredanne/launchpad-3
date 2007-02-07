5df12dc9fa0dc82d146b1a034dad6b73da4e30f6
Danilo Šegan <danilo.segan@canonical.com>
2007-02-07 16:52:02 +0100

    Split DB patch.

--- database/schema/pending/danilo-add-firefox-language-identifiers.sql
+++ database/schema/pending/danilo-add-firefox-language-identifiers.sql
@@ -1,38 +1,4 @@
-SET client_min_messages=ERROR;
-
-/* Rosetta Firefox/OpenOffice.org updates
-   Extension of Rosetta data model to allow native
-   Firefox and OpenOffice import/export.
-*/
-
-  -- Column which holds alternative message IDs for Firefox
-ALTER TABLE potmsgset
-    ADD COLUMN alternative_msgid TEXT DEFAULT NULL;
-UPDATE potmsgset SET alternative_msgid=NULL;
-
-CREATE INDEX potmsgset_alternative_msgid_idx ON potmsgset USING btree (alternative_msgid);
-
-ALTER TABLE ONLY potmsgset
-    DROP CONSTRAINT potmsgset_potemplate_key;
-ALTER TABLE ONLY potmsgset
-    ADD CONSTRAINT potmsgset_potemplate_key UNIQUE (potemplate, primemsgid, alternative_msgid);
-
-ALTER TABLE translationimportqueueentry
-    ADD COLUMN format INTEGER DEFAULT 1 NOT NULL;
-  -- Set format for existing imports to PO
-UPDATE translationimportqueueentry SET format=1;
-
-ALTER TABLE potemplate
-    ADD COLUMN source_file INTEGER;
-ALTER TABLE potemplate
-    ADD COLUMN source_file_format INTEGER DEFAULT 1 NOT NULL;
-
-ALTER TABLE ONLY potemplate
-    ADD CONSTRAINT "$1" FOREIGN KEY (source_file) REFERENCES libraryfilealias(id);
-
--- Firefox special UUID
-ALTER TABLE language
-    ADD COLUMN uuid TEXT;
+-- Firefox special UUID list
 UPDATE language SET uuid='{95a05dab-bf44-4804-bb97-be2a3ee83acd}'
     WHERE code='af';
 UPDATE language SET uuid='{b5cfaf65-895d-4d69-ba92-99438d6003e9}'
@@ -103,7 +69,4 @@ UPDATE language SET uuid='{4CD2763D-5532-4ddc-84D9-2E094695A680}'
     WHERE code='nb';
 UPDATE language SET uuid='{f68df430-4534-4473-8ca4-d5de32268a8d}'
     WHERE code='uk';
-
-
-INSERT INTO LaunchpadDatabaseRevision VALUES (67, 73, 0);
 
