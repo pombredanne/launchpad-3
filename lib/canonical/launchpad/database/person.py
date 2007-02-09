d26a6234c4df27330a61bdba3828e0422e19b221
Christian Reis <christian.reis@canonical.com>
2007-02-09 17:29:46 -0200

    Add missing linking table, because Storm wants one.

--- lib/canonical/launchpad/database/person.py
+++ lib/canonical/launchpad/database/person.py
@@ -1925,6 +1925,14 @@ class PersonSet:
         flush_database_caches()
 
 
+class PersonLanguage(SQLBase):
+    _table = 'PersonLanguage'
+
+    person = ForeignKey(foreignKey='Person', dbName='person', notNull=True)
+    language = ForeignKey(foreignKey='Language', dbName='language',
+                          notNull=True)
+
+
 class SSHKey(SQLBase):
     implements(ISSHKey)
 
