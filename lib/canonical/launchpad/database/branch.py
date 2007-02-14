655ff3b060f2f89c0b79883a3052ba0a3ced2f54
Tim Penhey <tim.penhey@canonical.com>
2007-02-15 10:32:18 +1300

    diff reduction due to stale code

--- lib/canonical/launchpad/database/branch.py
+++ lib/canonical/launchpad/database/branch.py
@@ -1,8 +1,7 @@
 # Copyright 2004-2005 Canonical Ltd.  All rights reserved.
 
 __metaclass__ = type
-__all__ = ['Branch', 'BranchSet',
-           'BranchRelationship', 'BranchLabel']
+__all__ = ['Branch', 'BranchSet', 'BranchRelationship', 'BranchLabel']
 
 import re
 
