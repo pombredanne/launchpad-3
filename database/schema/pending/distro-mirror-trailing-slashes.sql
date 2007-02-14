68a04e936a88e5d0bc95103eb886a5112a10249f
Guilherme Salgado <guilherme.salgado@canonical.com>
2007-02-14 12:42:52 -0200

    Remove data migration patch since it'll be added to the wiki

--- database/schema/pending/distro-mirror-trailing-slashes.sql
+++
@@ -1,9 +0,0 @@
-UPDATE DistributionMirror SET http_base_url = http_base_url || '/'
-WHERE http_base_url IS NOT NULL AND http_base_url NOT LIKE '%/';
-
-UPDATE DistributionMirror SET ftp_base_url = ftp_base_url || '/'
-WHERE ftp_base_url IS NOT NULL AND ftp_base_url NOT LIKE '%/';
-
-UPDATE DistributionMirror SET rsync_base_url = rsync_base_url || '/'
-WHERE rsync_base_url IS NOT NULL AND rsync_base_url NOT LIKE '%/';
-
