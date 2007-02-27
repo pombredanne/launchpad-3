acdd9643ad3ef2c13c539fc046c883509a3b56c3
Stuart Bishop <stuart.bishop@canonical.com>
2007-02-27 17:52:16 +0700

    Work in progress

--- lib/canonical/launchpad/browser/openidserver.py
+++ lib/canonical/launchpad/browser/openidserver.py
@@ -6,6 +6,8 @@ __metaclass__ = type
 __all__ = []
 
 from tempfile import mkdtemp
+from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
+from zope.interface import Interface, Attribute, implements
 
 from openid.server.server import ProtocolError, Server, ENCODE_URL
 # XXX: Temporary - switch to SQL -- StuartBishop 20070214
@@ -15,9 +17,17 @@ from canonical.launchpad.webapp import LaunchpadView
 from canonical.launchpad.webapp.vhosts import allvhosts
 
 
+class IOpenIdView(Interface):
+    foo = Attribute('Foo')
+
+
 class OpenIdView(LaunchpadView):
+    implements(IOpenIdView)
 
     openid_server = Server(FileOpenIDStore(mkdtemp('openid')))
+
+    default_template = ViewPageTemplateFile("../templates/openid-index.pt")
+    decide_template = ViewPageTemplateFile("../templates/openid-decide.pt")
 
     def render(self):
         try:
@@ -28,7 +38,7 @@ class OpenIdView(LaunchpadView):
         # Not an OpenID request, so display a message explaining what this
         # is to nosy users.
         if openid_request is None:
-            return self.template()
+            return self.default_template()
 
         if openid_request.mode in ['checkid_immediate', 'checkid_setup']:
             if self.isAuthorized(
@@ -39,8 +49,7 @@ class OpenIdView(LaunchpadView):
                         False, allvhosts.configs['openid'].rooturl
                         )
             else:
-                self.showDecidePage(openid_request)
-                return
+                return self.showDecidePage(openid_request)
         else:
             openid_response = self.openid_server.handleRequest(openid_request)
 
@@ -52,10 +61,26 @@ class OpenIdView(LaunchpadView):
             response.setHeader(header, value)
         return webresponse.body
 
+    def showDecidePage(self, openid_request):
+        """Render the 'do you want to authenticate' page.
+
+        An OpenID consumer has redirected the user here to be authenticated.
+        We need to explain what they are doing here and ask them if they
+        want to allow Launchpad to authenticate them with the OpenID consumer.
+
+        If the user is not authenticated with Launchpad, they will be
+        presented with a login form. Otherwise, just "Login", and
+        "Don't Login" buttons.
+        """
+        self.authenticated = False
+        self.login = 'Fixme'
+        return self.decide_template(foo='foobar')
+
     def isAuthorized(self, identity, trust_root):
         return False
 
     def renderProtocolError(self, exception):
+        # XXX: Is this tested? -- StuartBishop 20070226
         response = self.request.response
         if exception.whichEncoding() == ENCODE_URL:
             url = excecption.encodeToURL()
