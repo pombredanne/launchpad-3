bc9617e8450fd4bd3504de91374471671cb171dc
Diogo Matsubara <matsubara@async.com.br>
2007-02-27 19:47:44 -0300

    Fixes 86966 (OpenPGP keys link in LaunchPad account not working)

--- lib/canonical/launchpad/browser/person.py
+++ lib/canonical/launchpad/browser/person.py
@@ -1722,10 +1722,8 @@ class PersonGPGView(LaunchpadView):
 
     def keyserver_url(self):
         assert self.fingerprint
-        url = getUtility(IGPGHandler).getURLForKeyInServer(self.fingerprint)
-        # Our servers use an internal keyserver which users can't access.
-        # We must point them to the ubuntu keyserver. See bug 81269
-        return url.replace('keyserver.internal', 'keyserver.ubuntu.com', 1)
+        return getUtility(
+            IGPGHandler).getURLForKeyInServer(self.fingerprint, public=True)
 
     def form_action(self):
         permitted_actions = [
