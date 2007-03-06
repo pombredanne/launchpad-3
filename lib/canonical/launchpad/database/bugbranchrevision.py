f4a86ade845f0b68257871a4e98521034cdce3dd
jml@canonical.com <jml@canonical.com>
2007-03-06 16:24:23 +1100

    Add a BugBranchRevisionSet

--- lib/canonical/launchpad/database/bugbranchrevision.py
+++ lib/canonical/launchpad/database/bugbranchrevision.py
@@ -1,14 +1,15 @@
 # Copyright 2007 Canonical Ltd.  All rights reserved.
 
 __metaclass__ = type
-__all__ = ['BugBranchRevision']
+__all__ = ['BugBranchRevision', 'BugBranchRevisionSet']
 
 from zope.interface import implements
 
 from sqlobject import ForeignKey, IntCol
 
 from canonical.database.sqlbase import SQLBase
-from canonical.launchpad.interfaces import IBugBranchRevision
+from canonical.launchpad.interfaces import (
+    IBugBranchRevision, IBugBranchRevisionSet)
 
 
 class BugBranchRevision(SQLBase):
@@ -23,3 +24,12 @@ class BugBranchRevision(SQLBase):
     revision = ForeignKey(
         dbName='revision', foreignKey='Revision', notNull=True)
 
+
+class BugBranchRevisionSet:
+    """The set of all associations between a bug and a revision."""
+
+    implements(IBugBranchRevisionSet)
+
+    def new(self, bug, branch, revision):
+        """See IBugBranchRevisionSet."""
+        return BugBranchRevision(bug=bug, branch=branch, revision=revision)
--- lib/canonical/launchpad/interfaces/bugbranchrevision.py
+++ lib/canonical/launchpad/interfaces/bugbranchrevision.py
@@ -3,7 +3,7 @@
 """BugBranchRevision interfaces."""
 
 __metaclass__ = type
-__all__ = ['IBugBranchRevision']
+__all__ = ['IBugBranchRevision', 'IBugBranchRevisionSet']
 
 from zope.interface import Interface, Attribute
 from zope.schema import Int
@@ -12,11 +12,19 @@ from canonical.launchpad import _
 
 
 class IBugBranchRevision(Interface):
-    """The association between a bug and a revision of a branch.
-    """
+    """The association between a bug and a revision of a branch."""
 
     id = Int(title=_('The database ID'))
 
     bug = Attribute("The bug that relates to the revision.")
     branch = Attribute("The branch that the revision occurs in.")
     revision = Attribute("The revision that relates to the bug.")
+
+
+class IBugBranchRevisionSet(Interface):
+    """The set of all bug-revision associations."""
+
+    def new(bug, branch, revision):
+        """Create a new BugBranchRevision for the specified bug, branch and
+        revision."""
+
