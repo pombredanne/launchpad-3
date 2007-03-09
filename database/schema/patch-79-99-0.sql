c6a297d854ee253a3de2ba10ddc1d6a710293909
jml@canonical.com <jml@canonical.com>
2007-03-09 16:55:12 +1100

    Add a RevisionProperty table and set BugBranchRevision fields to not null.

--- database/schema/patch-79-99-0.sql
+++ database/schema/patch-79-99-0.sql
@@ -2,9 +2,15 @@ ALTER TABLE BugBranch DROP COLUMN revision_hint;
 
 CREATE TABLE BugBranchRevision (
   id serial,
-  bug integer CONSTRAINT bugbranchrevision_bug_fk REFERENCES Bug(id),
+  bug integer NOT NULL CONSTRAINT bugbranchrevision_bug_fk REFERENCES Bug(id),
   branch integer CONSTRAINT bugbranchrevision_branch_fk REFERENCES Branch(id),
-  revision integer CONSTRAINT bugbranchrevision_revision_fk REFERENCES Revision(id),
+  revision integer NOT NULL CONSTRAINT bugbranchrevision_revision_fk REFERENCES Revision(id),
   status integer);
+
+CREATE TABLE RevisionProperty (
+  id serial,
+  revision integer CONSTRAINT revisionproperty_fk REFERENCES Revision(id),
+  name text NOT NULL,
+  value text NOT NULL);
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (79, 99, 0);
