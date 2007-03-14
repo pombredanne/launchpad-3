63f4d6b1871ed778f5f3b5188734e85a1a741647
jml@canonical.com <jml@canonical.com>
2007-03-14 13:42:54 +1100

    Uniqueness constraints for BugBranchRevision and RevisionProperty

--- database/schema/patch-79-99-0.sql
+++ database/schema/patch-79-99-0.sql
@@ -7,10 +7,18 @@ CREATE TABLE BugBranchRevision (
   revision integer NOT NULL CONSTRAINT bugbranchrevision_revision_fk REFERENCES Revision(id),
   status integer NOT NULL);
 
+ALTER TABLE ONLY BugBranchRevision
+  ADD CONSTRAINT bugbranchrevision_bug_revision_uniq UNIQUE (bug, revision);
+
+
 CREATE TABLE RevisionProperty (
   id serial,
   revision integer NOT NULL CONSTRAINT revisionproperty_fk REFERENCES Revision(id),
   name text NOT NULL,
   value text NOT NULL);
+
+ALTER TABLE ONLY RevisionProperty
+  ADD CONSTRAINT revisionproperty_revision_name_uniq UNIQUE (revision, name);
+
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (79, 99, 0);
