f5faf07a63c0af8d8b7e29f18f4358a2124137b0
jml@canonical.com <jml@canonical.com>
2007-03-14 13:35:22 +1100

    Add tables to security and proper constraints to BugBranchRevision and RevisionProperty.

--- database/schema/patch-79-99-0.sql
+++ database/schema/patch-79-99-0.sql
@@ -5,11 +5,11 @@ CREATE TABLE BugBranchRevision (
   bug integer NOT NULL CONSTRAINT bugbranchrevision_bug_fk REFERENCES Bug(id),
   branch integer CONSTRAINT bugbranchrevision_branch_fk REFERENCES Branch(id),
   revision integer NOT NULL CONSTRAINT bugbranchrevision_revision_fk REFERENCES Revision(id),
-  status integer);
+  status integer NOT NULL);
 
 CREATE TABLE RevisionProperty (
   id serial,
-  revision integer CONSTRAINT revisionproperty_fk REFERENCES Revision(id),
+  revision integer NOT NULL CONSTRAINT revisionproperty_fk REFERENCES Revision(id),
   name text NOT NULL,
   value text NOT NULL);
 
