75b11783c07c3f8cfa004f19eaa1f3feda42c063
jml@canonical.com <jml@canonical.com>
2007-03-21 12:54:50 +1100

    Move BzrlibZopelessLayer to testing/layers.py

--- lib/canonical/testing/layers.py
+++ lib/canonical/testing/layers.py
@@ -20,9 +20,12 @@ __all__ = [
     'BaseLayer', 'DatabaseLayer', 'LibrarianLayer', 'FunctionalLayer',
     'LaunchpadLayer', 'ZopelessLayer', 'LaunchpadFunctionalLayer',
     'LaunchpadZopelessLayer', 'PageTestLayer',
-    'LayerConsistencyError', 'LayerIsolationError', 'TwistedLayer'
+    'LayerConsistencyError', 'LayerIsolationError', 'TwistedLayer',
+    'BzrlibZopelessLayer'
     ]
 
+import shutil
+import sys
 import time
 from urllib import urlopen
 
@@ -32,6 +35,8 @@ from zope.component import getUtility, getGlobalSiteManager
 from zope.component.interfaces import ComponentLookupError
 from zope.security.management import getSecurityPolicy
 from zope.security.simplepolicies import PermissiveSecurityPolicy
+
+from bzrlib.tests import TestCaseInTempDir
 
 from twisted.trial.runner import TrialSuite
 
@@ -611,3 +616,31 @@ class TwistedLayer(LaunchpadZopelessLayer):
     @classmethod
     def testTearDown(cls):
         pass
+
+
+class BzrlibZopelessLayer(LaunchpadZopelessLayer):
+    """Clean up the test directory created by TestCaseInTempDir tests."""
+
+    @classmethod
+    def setUp(cls):
+        pass
+
+    @classmethod
+    def tearDown(cls):
+        # Remove the test directory created by TestCaseInTempDir.
+        # Copied from bzrlib.tests.TextTestRunner.run.
+        test_root = TestCaseInTempDir.TEST_ROOT
+        if test_root is not None:
+            test_root = test_root.encode(sys.getfilesystemencoding())
+            shutil.rmtree(test_root)
+
+
+    @classmethod
+    def testSetUp(cls):
+        pass
+
+    @classmethod
+    def testTearDown(cls):
+        pass
+
+
