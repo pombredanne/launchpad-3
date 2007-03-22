5d6c4e392feb7f9e7b88f8397012b85027ae8985
Curtis Hovey <curtis.hovey@canonical.com>
2007-03-22 18:11:11 -0400

    Added asterisks to supported languages during the questions from a project scenario. answer-tracker-pages is broken because QuestionLanguageVocabularyFactory contract changed.

--- lib/canonical/launchpad/browser/question.py
+++ lib/canonical/launchpad/browser/question.py
@@ -46,7 +46,7 @@ from canonical.launchpad.interfaces import (
     ILaunchpadStatisticSet,
     IQuestion,
     IQuestionAddMessageForm, IQuestionChangeStatusForm, IQuestionSet,
-    IQuestionTarget, UnexpectedFormData)
+    IQuestionTarget, IProject, UnexpectedFormData)
 
 from canonical.launchpad.webapp import (
     ContextMenu, Link, canonical_url, enabled_with_permission, Navigation,
@@ -182,17 +182,19 @@ class QuestionLanguageVocabularyFactory:
 
     implements(IContextSourceBinder)
 
-    def __init__(self, request):
+    def __init__(self, view):
         """Create a QuestionLanguageVocabularyFactory.
 
-        :param request: The request in which the vocabulary will be used. This
-        will be used to determine the user languages.
+        :param view: The view provides the request used to determine the user 
+        languages. The view will provide the widgets that contains the
+        Product selected by the user in the case where a question is asked in 
+        the context of a Project 
         """
-        self.request = request
+        self.view = view
 
     def __call__(self, context):
         languages = set()
-        for lang in request_languages(self.request):
+        for lang in request_languages(self.view.request):
             if not is_english_variant(lang):
                 languages.add(lang)
         if (context is not None and IQuestion.providedBy(context) and
@@ -204,8 +206,15 @@ class QuestionLanguageVocabularyFactory:
         languages.insert(0, getUtility(ILanguageSet)['en'])
         
         # The vocabulary indicates which languages are supported.
-        if context is not None:
+        if context is not None and not IProject.providedBy(context):
             question_target = IQuestionTarget(context)
+            supported_languages = question_target.getSupportedLanguages()
+        elif (IProject.providedBy(context) and 
+            self.view.continue_action.submitted()):
+            
+            # projects do not implement IQuestionTarget--the user must
+            # choose a product while asking a question
+            question_target = IQuestionTarget(self.view.question_target)
             supported_languages = question_target.getSupportedLanguages()
         else:
             supported_languages = set([getUtility(ILanguageSet)['en']])
@@ -260,7 +269,7 @@ class QuestionSupportLanguageMixin:
             key=attrgetter('englishname'))
 
     def createLanguageField(self):
-        """Create a field to edit a question language using a special vocabulary.
+        """Create a field to edit a question language using a vocabulary.
 
         :param the_form: The form that will use this field.
         :return: A form.Fields instance containing the language field.
@@ -268,7 +277,7 @@ class QuestionSupportLanguageMixin:
         return form.Fields(
                 Choice(
                     __name__='language',
-                    source=QuestionLanguageVocabularyFactory(self.request),
+                    source=QuestionLanguageVocabularyFactory(view=self),
                     title=_('Language'),
                     description=_(
                         "The language in which this question is written. "
@@ -310,7 +319,8 @@ class QuestionAddView(QuestionSupportLanguageMixin, LaunchpadFormView):
 
     custom_widget('title', TextWidget, displayWidth=40)
 
-    search_template = ViewPageTemplateFile('../templates/question-add-search.pt')
+    search_template = ViewPageTemplateFile(
+        '../templates/question-add-search.pt')
 
     add_template = ViewPageTemplateFile('../templates/question-add.pt')
 
@@ -344,7 +354,7 @@ class QuestionAddView(QuestionSupportLanguageMixin, LaunchpadFormView):
         """
         if 'title' not in data:
             self.setFieldError(
-                'title',_('You must enter a summary of your problem.'))
+                'title', _('You must enter a summary of your problem.'))
         if self.widgets.get('description'):
             if 'description' not in data:
                 self.setFieldError(
@@ -659,7 +669,7 @@ class QuestionWorkflowView(LaunchpadFormView):
         if msgid is None:
             raise UnexpectedFormData('missing answer_id')
         try:
-            data['answer']= self.context.messages[int(msgid)]
+            data['answer'] = self.context.messages[int(msgid)]
         except ValueError:
             raise UnexpectedFormData('invalid answer_id: %s' % msgid)
         except IndexError:
