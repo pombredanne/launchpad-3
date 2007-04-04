bb7c70f5b93dc71aa479545ca56dfbc7d3b2c32c
Francis J. Lacoste <francis.lacoste@contre.com>
2007-04-04 14:01:37 -0400

    Got official patch number from stub.

--- database/schema/patch-79-15-0.sql
+++ database/schema/patch-79-15-0.sql
@@ -10,6 +10,9 @@ ALTER TABLE TicketSubscription RENAME TO QuestionSubscription;
 
 ALTER TABLE SupportContact RENAME TO AnswerContact;
 
+-- Update the fticache table so the full text indexes don't get rebuilt
+UPDATE FtiCache SET tablename='question' WHERE tablename='ticket';
+
 -- Rename sequences.
 
 ALTER TABLE ticket_id_seq RENAME TO question_id_seq;
@@ -19,6 +22,9 @@ ALTER TABLE ticketreopening_id_seq RENAME TO questionreopening_id_seq;
 ALTER TABLE ticketsubscription_id_seq RENAME TO questionsubscription_id_seq;
 
 ALTER TABLE supportcontact_id_seq RENAME TO answercontact_id_seq;
+
+-- And ensure SERIAL columns use the renamed sequences
+-- Yay - PostgreSQL 8.2 now does this automatically for us.
 
 -- Rename indexes.
 
@@ -210,4 +216,100 @@ ALTER TABLE AnswerContact
     ADD CONSTRAINT answercontact__sourcepackagename__fkey
         FOREIGN KEY (sourcepackagename) REFERENCES sourcepackagename(id);
 
-INSERT INTO LaunchpadDatabaseRevision VALUES (79, 50, 0);
+-- Rename the janitor.
+
+UPDATE Person SET name = 'answer-tracker-janitor',
+    displayname = 'Launchpad Answers Janitor'
+    WHERE name = 'support-tracker-janitor';
+
+
+-- Rename KarmaAction.
+
+UPDATE KarmaAction SET
+    name = 'questioncommentadded',
+    title = 'Comment made on a question',
+    summary = 'User made a comment on a question in the Answer Tracker'
+    WHERE name = 'ticketcommentadded';
+
+UPDATE KarmaAction SET
+    name = 'questiontitlechanged',
+    title = 'Question title changed',
+    summary = 'User changed the title of a question in the Answer Tracker'
+    WHERE name = 'tickettitlechanged';
+
+UPDATE KarmaAction SET
+    name = 'questiondescriptionchanged',
+    title = 'Question description changed',
+    summary = 'User changed the description of a question in the Answer Tracker'
+    WHERE name = 'ticketdescriptionchanged';
+
+UPDATE KarmaAction SET
+    name = 'questionlinkedtobug',
+    title = 'Question linked to a bug',
+    summary = 'User linked a question in the Answer Tracker to a bug.'
+    WHERE name = 'ticketlinkedtobug';
+
+UPDATE KarmaAction SET
+    name = 'questionansweraccepted',
+    title = 'Question owner accepted answer',
+    summary = 'User accepted one of the message as the actual answer to his question.'
+    WHERE name = 'ticketansweraccepted';
+
+UPDATE KarmaAction SET
+    name = 'questionanswered',
+    title = 'Answered question',
+    summary = 'User posed a message that was accepted by the question owner as answering the question.'
+    WHERE name = 'ticketanswered';
+
+UPDATE KarmaAction SET
+    name = 'questionrequestedinfo',
+    title = 'Requested for information on a question',
+    summary = 'User post a message requesting for more information from a question owner in the Answer Tracker.'
+    WHERE name = 'ticketrequestedinfo';
+
+UPDATE KarmaAction SET
+    name = 'questiongaveinfo',
+    title = 'Gave more information on a question',
+    summary = 'User replied to a message asking for more information on a question in the Answer Tracker.'
+    WHERE name = 'ticketgaveinfo';
+
+UPDATE KarmaAction SET
+    name = 'questiongaveanswer',
+    title = 'Gave answer on a question',
+    summary = 'User post a message containing an answer to a question in the Answer Tracker. This is distinct from having that message confirmed as solving the problem.'
+    WHERE name = 'ticketgaveanswer';
+
+UPDATE KarmaAction SET
+    name = 'questionrejected',
+    title = 'Rejected question',
+    summary = 'User rejected a question in the Answer Tracker.'
+    WHERE name = 'ticketrejected';
+
+UPDATE KarmaAction SET
+    name = 'questionownersolved',
+    title = 'Solved own question',
+    summary = 'User post a message explaining how he solved his own problem.'
+    WHERE name = 'ticketownersolved';
+
+UPDATE KarmaAction SET
+    name = 'questionreopened',
+    title = 'Reopened question',
+    summary = 'User posed a message to reopen his question in the Answer Tracker.'
+    WHERE name = 'ticketreopened';
+
+UPDATE KarmaAction SET
+    name = 'questionasked',
+    title = 'Asked question',
+    summary = 'User asked a question in the Answer Tracker.'
+    WHERE name = 'ticketcreated';
+
+-- Rename category.
+
+UPDATE KarmaCategory SET
+    name = 'answers',
+    title = 'Answer Tracker',
+    summary = 'This is the category for all karma associated with helping with users questions in the Launchpad Answer Tracker. Help solve users problems to earn this karma.'
+    WHERE name = 'support';
+
+INSERT INTO LaunchpadDatabaseRevision VALUES (79, 15, 0);
+
