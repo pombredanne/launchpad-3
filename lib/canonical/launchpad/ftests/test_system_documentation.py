00b18cc88efbaf4ba72981b9e5866e8b8195b7d2
Barry Warsaw <barry@canonical.com>
2007-04-13 16:57:57 -0400

    Special-case the distroreleasequeue-debian-installer.txt doctest so that the correct and expected umask (of 022) is set for the duration of this test. This is what the test expects and needs, and it is what the production environment will provide.  Without this, the test's success is based on the whim of the local user's umask.

--- lib/canonical/launchpad/ftests/test_system_documentation.py
+++ lib/canonical/launchpad/ftests/test_system_documentation.py
@@ -141,6 +141,21 @@ def statisticianTearDown(test):
     logout()
     LaunchpadZopelessTestSetup().tearDown()
 
+def distroreleasequeueSetUp(test):
+    setUp(test)
+    # The test requires that the umask be set to 022, and in fact this comment
+    # was made in irc on 13-Apr-2007:
+    #
+    # (04:29:18 PM) kiko: barry, cprov says that the local umask is controlled
+    # enough for us to rely on it
+    #
+    # Setting it here reproduces the environment that the doctest expects.
+    # Save the old umask so we can reset it in the tearDown().
+    test.old_umask = os.umask(022)
+
+def distroreleasequeueTearDown(test):
+    os.umask(test.old_umask)
+
 def LayeredDocFileSuite(*args, **kw):
     '''Create a DocFileSuite with a layer.'''
     layer = kw.pop('layer')
@@ -283,6 +298,12 @@ special = {
             '../doc/package-cache.txt',
             setUp=statisticianSetUp, tearDown=statisticianTearDown,
             optionflags=default_optionflags, layer=ZopelessLayer
+            ),
+    'distroreleasequeue-debian-installer.txt': FunctionalDocFileSuite(
+            '../doc/distroreleasequeue-debian-installer.txt',
+            setUp=distroreleasequeueSetUp, tearDown=distroreleasequeueTearDown,
+            optionflags=default_optionflags,
+            layer=LaunchpadFunctionalLayer
             ),
     }
 
