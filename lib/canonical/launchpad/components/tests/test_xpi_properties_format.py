c3568cd230479b476c3926b83273c5f58ced2d33
Carlos Perello Marin <carlos.perello@canonical.com>
2007-04-13 21:07:53 +0200

    Split tests by file format

--- lib/canonical/launchpad/components/tests/test_xpi_properties_format.py
+++ lib/canonical/launchpad/components/tests/test_xpi_properties_format.py
@@ -6,7 +6,7 @@ import unittest
 from textwrap import dedent
 
 from canonical.launchpad.components.rosettaformats.mozilla_xpi import (
-    DtdFile, PropertyFile, UnsupportedEncoding)
+    PropertyFile, UnsupportedEncoding)
 
 class BaseEncodingPropertyFileTest(unittest.TestCase):
     """Test class for property file format.
@@ -87,56 +87,11 @@ class Latin1PropertyFileTest(unittest.TestCase):
         self.assertEqual(detected, True)
 
 
-class UTF8DtdFileTest(unittest.TestCase):
-    """Test class for dtd file format.
-
-    This test makes sure that we handle UTF-8 encoding files.
-    """
-
-    content = '<!ENTITY utf8.message "\xc2\xbfQuieres? \xc2\xa1S\xc3\xad!">'
-
-
-    def runTest(self):
-        dtd_file = DtdFile('test.dtd', self.content)
-
-        count = 0
-        for message in dtd_file._data:
-            if message['msgid'] == u'utf8.message':
-                self.assertEqual(
-                    message['content'], u'\xbfQuieres? \xa1S\xed!')
-                count += 1
-
-        # Validate that we actually found the string so the test is really
-        # passing.
-        self.assertEqual(count, 1)
-
-
-class Latin1DtdFileTest(unittest.TestCase):
-    """Test class for dtd file format.
-
-    This test makes sure that we detect bad encodings.
-    """
-
-    content = '<!ENTITY latin1.message "\xbfQuieres? \xa1S\xed!">\n'
-
-    def runTest(self):
-        detected = False
-        try:
-            dtd_file = DtdFile('test.dtd', self.content)
-        except UnsupportedEncoding:
-            detected = True
-
-        # Whether the unsupported encoding was detected.
-        self.assertEqual(detected, True)
-
-
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTest(UTF8PropertyFileTest())
     suite.addTest(UnicodeEscapedPropertyFileTest())
     suite.addTest(Latin1PropertyFileTest())
-    suite.addTest(UTF8DtdFileTest())
-    suite.addTest(Latin1DtdFileTest())
     return suite
 
 if __name__ == '__main__':
