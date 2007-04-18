19b550cae96e657829055ad922c59d19ecfdbed5
Danilo Šegan <danilo@gnome.org>
2007-04-18 17:56:22 +0200

    Improve docstring.

--- lib/canonical/launchpad/browser/person.py
+++ lib/canonical/launchpad/browser/person.py
@@ -1807,9 +1807,9 @@ class PersonTranslationView(LaunchpadView):
     def should_display_message(self, pomsgset):
         """Should a certain POMsgSet be displayed.
 
-        It's used only on the view to check if a user is not logged in,
-        and if she isn't, hide messages which may contain sensitive data
-        such as email addresses.
+        It's used only to check if a user is not logged in, and if she isn't,
+        returns False for messages which may contain sensitive data such as
+        email addresses.
         """
         # Show all messages for logged-in users
         if self.user:
