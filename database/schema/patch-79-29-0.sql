78aed556233d87dd4955094dbe28c3165a67e00a
Guilherme Salgado <guilherme.salgado@canonical.com>
2007-04-20 15:05:24 -0300

    Schema changes necessary for fixing bug #70519

---
+++ database/schema/patch-79-29-0.sql
@@ -0,0 +1,7 @@
+SET client_min_messages=ERROR;
+
+ALTER TABLE Person ADD COLUMN renewal_policy integer;
+UPDATE Person SET renewal_policy = 10;
+ALTER TABLE Person ALTER COLUMN renewal_policy SET NOT NULL;
+
+INSERT INTO LaunchpadDatabaseRevision VALUES (79, 29, 0);
