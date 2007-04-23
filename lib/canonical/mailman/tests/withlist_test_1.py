f0969e391c29a024db77c6850c113600eb10ce86
Barry Warsaw <barry@canonical.com>
2007-04-23 18:10:14 -0400

    Add a test that verifies that Mailman can import the libmailman package.  Also rename some tests (though I'm going to rename them once more momentarily).

--- lib/canonical/mailman/tests/withlist_test_1.py
+++ lib/canonical/mailman/tests/withlist_test_1.py
@@ -1,13 +1,13 @@
 # Copyright 2007 Canonical Ltd.  All rights reserved.
 
-# This is a bin/withlist script for testing that Mailman can import and access
-# Launchpad's modules.  It works by side effect; on success it exits with a
-# return code of 99, which would be difficult to false positive, but very easy
-# to check by the parent process.  See test-monkeypatch.txt for details.
+# This is a bin/withlist script for testing that Mailman's site list exists.
+# It works by side effect; on success it exits with a return code of 99, which
+# would be difficult to false positive, but very easy to check by the parent
+# process.  See test-monkeypatch.txt for details.
 #
 # This script must be called like so:
 #
-# bin/withlist -r canonical.mailman.tests.launchpad.launchpad mailman
+# bin/withlist -r canonical.mailman.tests.withlist_test_1.test_site_list mailman
 #
 # where the final argument is the name of the site list, which must exist.
 
@@ -15,7 +15,7 @@ import sys
 from Mailman.mm_cfg import MAILMAN_SITE_LIST
 
 
-def launchpad(mlist):
+def test_site_list(mlist):
     # The very fact that this function got called proves that Launchpad's
     # package namespace is accessible.  Do one more check for sanity.
     if mlist.internal_name() == MAILMAN_SITE_LIST:
