5431510d2099c884ea717f3414f2c2c1b858dd5f
Guilherme Salgado <guilherme.salgado@canonical.com>
2007-05-16 09:14:34 -0300

    Rename db patch and update sampledata

--- database/schema/patch-87-03-0.sql
+++ database/schema/patch-87-03-0.sql
@@ -4,4 +4,4 @@ ALTER TABLE Person ADD COLUMN renewal_policy integer;
 UPDATE Person SET renewal_policy = 10;
 ALTER TABLE Person ALTER COLUMN renewal_policy SET NOT NULL;
 
-INSERT INTO LaunchpadDatabaseRevision VALUES (79, 22, 0);
+INSERT INTO LaunchpadDatabaseRevision VALUES (87, 03, 0);
