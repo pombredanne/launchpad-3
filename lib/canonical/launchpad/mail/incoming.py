ff369edd175dab2b492676372477a19c42a2fc55
Bjorn Tillenius <bjorn.tillenius@canonical.com>
2007-05-16 11:22:17 +0300

    drop multipart/report messages.

--- lib/canonical/launchpad/mail/incoming.py
+++ lib/canonical/launchpad/mail/incoming.py
@@ -175,6 +175,13 @@ def handleMail(trans=transaction):
                         file_alias_url, notify=False
                         )
                     continue
+                if mail.get_content_type() == 'multipart/report':
+                    # Mails with a content type of multipart/report are
+                    # generall DSN messages and should be ignored.
+                    _handle_error(
+                        "Got a multipart/report message.",
+                        file_alias_url, notify=False)
+                    continue
 
                 try:
                     principal = authenticateEmail(mail)
