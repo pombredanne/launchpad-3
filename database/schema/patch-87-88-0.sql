ab9d60496ade15a24c64527e7cefd631c0bef72d
David Allouche <david.allouche@canonical.com>
2007-05-25 22:39:12 +0200

    start doctest, fixes to db patch

---
+++ database/schema/patch-87-88-0.sql
@@ -0,0 +1,38 @@
+SET client_min_messages=ERROR;
+
+CREATE TABLE CodeImport (
+    id SERIAL PRIMARY KEY,
+    date_created TIMESTAMP WITHOUT TIME ZONE
+        DEFAULT timezone('UTC', now()) NOT NULL,
+    name text NOT NULL UNIQUE,
+    product integer REFERENCES Product NOT NULL,
+    series integer REFERENCES ProductSeries UNIQUE NOT NULL,
+    branch integer REFERENCES Branch,
+
+    rcs_type integer NOT NULL,
+    svn_branch_url text UNIQUE,
+    cvs_root text,
+    cvs_module text,
+
+    UNIQUE (cvs_root, cvs_module),
+
+    CONSTRAINT valid_name CHECK (valid_name(name)),
+    CONSTRAINT valid_cvs CHECK ((rcs_type <> 1) OR (
+        (cvs_root IS NOT NULL) AND (cvs_root <> '') AND
+        (cvs_module IS NOT NULL) AND (cvs_module <> ''))),
+    CONSTRAINT null_cvs CHECK ((rcs_type = 1) OR (
+        (cvs_root IS NULL) AND (cvs_module IS NULL))),
+    CONSTRAINT valid_svn CHECK ((rcs_type <> 2) OR (
+        (svn_branch_url IS NOT NULL) AND (svn_branch_url <> ''))),
+    CONSTRAINT null_svn CHECK ((rcs_type = 2) OR (svn_branch_url IS NULL))
+
+    -- XXX: product must be consistent across the codeimport, the branch and
+    -- the productseries. What is the best way to enforce that?
+    -- -- David Allouche 2007-05-18
+    -- FOREIGN KEY (branch, product) REFERENCES Branch (id, product),
+    -- FOREIGN KEY (series, product) REFERENCES ProductSeries (id, product)
+);
+
+-- XXX: This should be fixed once we get a real patch number:
+-- MichaelHudson, 2007-05-15
+INSERT INTO LaunchpadDatabaseRevision VALUES (87, 88, 0);
