f72ea728f8be9267c0f73019dc983d55a5475695
Francis J. Lacoste <francis.lacoste@contre.com>
2007-06-06 14:21:32 -0400

    Respect the 78 width.

--- lib/canonical/launchpad/interfaces/faqtarget.py
+++ lib/canonical/launchpad/interfaces/faqtarget.py
@@ -21,12 +21,16 @@ class IFAQTarget(Interface):
 
         :param owner: The `IPerson` creating the FAQ document.
         :param title: The document's title.
-        :param summary: A short description of the issue answered in the document.
-        :param content: The document's content. Alternatively a URL can be provided.
+        :param summary: A short description of the issue answered in the
+            document.
+        :param content: The document's content. Alternatively a URL can be
+            provided.
         :param url: The URL to the document containing the answer.
-        :param date_created: The creation time of the document. Defaults to now.
+        :param date_created: The creation time of the document.
+            Defaults to now.
 
-        Either content or url must be prrovided. It is an error to specify both.
+        Either content or url must be prrovided. It is an error to specify
+        both.
         """
 
     def findSimilarFAQs(summary):
