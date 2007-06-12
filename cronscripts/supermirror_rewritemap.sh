1a90fc1d6ac44dc821d7d93dfcb1765d347ea1a0
Tom Haddon <tom.haddon@canonical.com>
2007-06-12 11:48:53 -0700

    Use $MAP variable for readability and include comment explaining what we are importing

--- cronscripts/supermirror_rewritemap.sh
+++ cronscripts/supermirror_rewritemap.sh
@@ -4,11 +4,16 @@ RSYNC_FILE=/srv/launchpad.net/etc/supermirror_rewritemap.conf
 
 if [ -f "$RSYNC_FILE" ]
 then
+    # This simply exports for us the RSYNC_PASSWORD
+    # variable. We don't want it here because we don't
+    # want that password exposed within RF
     . $RSYNC_FILE
 else
     echo `date` "Supermirror config file not found, exiting"
     exit 1
 fi
+
+export LPCONFIG=lpnet1
 
 cd  /srv/launchpad.net/production/launchpad/cronscripts
 
@@ -17,7 +22,7 @@ MAP=/tmp/new-sm-map
 
 lockfile -l 600 ${LOCK}
 
-python supermirror_rewritemap.py -q /tmp/new-sm-map && rsync ${MAP} \
+python supermirror_rewritemap.py -q ${MAP} && rsync ${MAP} \
         launchpad@bazaar.launchpad.net::config/launchpad-lookup.txt
 
 rm -f ${LOCK}
