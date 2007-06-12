5e82f58182e5c763565c284d6d45eac4b4746e77
Tom Haddon <tom.haddon@canonical.com>
2007-06-12 14:58:53 -0700

    Refactored runlaunchpad to use config.root and now we can call make-static in a much cleaner way (thx stub)

--- scripts/make-static.py
+++ scripts/make-static.py
@@ -3,14 +3,6 @@
 Create static files such as +style-slimmer.css
 """
 
-import os
-import contrib.slimmer
+from canonical.launchpad.scripts.runlaunchpad import make_css_slimmer, make_abspath
 
-inputfile = os.path.join(os.path.abspath(''), 
-    'lib/canonical/launchpad/icing/style.css')
-outputfile = os.path.join(os.path.abspath(''), 
-    'lib/canonical/launchpad/icing/+style-slimmer.css')
-
-cssdata = open(inputfile, 'rb').read()
-slimmed = contrib.slimmer.slimmer(cssdata, 'css')
-open(outputfile, 'w').write(slimmed)
+make_css_slimmer()
