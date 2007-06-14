add0a749712e8b17dd1059efd73e5207ae6c6740
Tim Penhey <tim.penhey@canonical.com>
2007-06-14 14:55:13 +1200

    cleaning up following review

--- lib/canonical/database/tests/enumeration.py
+++ lib/canonical/database/tests/enumeration.py
@@ -1,7 +1,8 @@
+# Copyright 2007 Canonical Ltd.  All rights reserved.
+
 """Test enumerations used for enumcol doctests."""
 
-from canonical.launchpad.webapp.enum import DBEnumeratedType, DBItem, extends
-
+__metaclass__ = type
 __all__ = [
     'DBTestEnumeration',
     'InheritedTestEnumeration',
@@ -9,16 +10,23 @@ __all__ = [
     'UnrelatedTestEnumeration',
     ]
 
+
+from canonical.launchpad.webapp.enum import DBEnumeratedType, DBItem, extends
+
+
 class DBTestEnumeration(DBEnumeratedType):
     VALUE1 = DBItem(1, 'Some value')
     VALUE2 = DBItem(2, 'Some other value')
 
+
 class InheritedTestEnumeration(DBTestEnumeration):
     VALUE3 = DBItem(3, 'Yet another item')
+
 
 class ExtendedTestEnumeration(DBEnumeratedType):
     extends(DBTestEnumeration)
     VALUE3 = DBItem(3, 'Yet another item')
+
 
 class UnrelatedTestEnumeration(DBEnumeratedType):
     VALUE1 = DBItem(1, 'Some value')
--- lib/canonical/database/tests/test_enumcol.py
+++ lib/canonical/database/tests/test_enumcol.py
@@ -1,11 +1,19 @@
-# Copyright 2005 Canonical Ltd.  All rights reserved.
+# Copyright 2007 Canonical Ltd.  All rights reserved.
+
+"""Module docstring goes here."""
+
+__metaclass__ = type
+__all__ = []
+
 
 import unittest
 from zope.testing import doctest
 import canonical.database.enumcol
 
+
 def test_suite():
     return doctest.DocTestSuite(canonical.database.enumcol)
+
 
 if __name__ == '__main__':
     runner = unittest.TextTestRunner()
