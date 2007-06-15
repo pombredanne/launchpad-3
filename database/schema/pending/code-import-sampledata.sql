ed56d8ea34a9d5c06c51bfe31488f2db18710def
Michael Hudson <michael.hudson@canonical.com>
2007-06-15 10:10:39 +0100

    rewrite sampledata creation script

--- database/schema/pending/code-import-sampledata.sql
+++ database/schema/pending/code-import-sampledata.sql
@@ -1,13 +1,15 @@
+DELETE FROM CodeImport;
 
-INSERT INTO CodeImport (name, product, rcs_type, svn_branch_url, review_status, branch)
-       (SELECT 'gnome-terminal-trunk-1', product.id, 2,
-               'http://svn.example.org/svnroot/gnome-terminal/trunk', 20, branch.id
+INSERT INTO CodeImport (branch, registrant, rcs_type, svn_branch_url, review_status)
+       (SELECT branch.id, person.id, 2, 'http://svn.example.org/svnroot/gnome-terminal/trunk', 20
         FROM product
         JOIN branch on branch.product = product.id
+        JOIN person on person.name='no-priv'
         WHERE product.name='gnome-terminal' and branch.url like '%main');
 
-INSERT INTO CodeImport (name, product, series, rcs_type, cvs_root, cvs_module)
-       (SELECT 'a52dec-trunk-3', series.product, series.id, 1, 'cvs.example.org/cvsroot', 'a52dec'
+INSERT INTO CodeImport (branch, registrant, rcs_type, cvs_root, cvs_module)
+       (SELECT series.import_branch, person.id, 1, series.cvsroot, series.cvsmodule
         FROM productseries as series
+        JOIN person on person.name='no-priv'
         JOIN product on series.product=product.id
-        WHERE product.name='a52dec' AND series.name='trunk');
+        WHERE product.name='evolution' AND series.name='trunk');
