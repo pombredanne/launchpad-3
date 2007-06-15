33adca54d672ad5b1b926072ee4c966e21c68f3b
Stuart Bishop <stuart@stuartbishop.net>
2007-06-15 20:25:50 +0700

    Update patch-87-15-0.sql to not take 6 hours. Will require post-rollout updates to set openid_identifiers on remaining users, drop temp trigger and set NOT NULL constraint

--- database/schema/patch-87-15-0.sql
+++ database/schema/patch-87-15-0.sql
@@ -2,14 +2,22 @@ SET client_min_messages=ERROR;
 
 ALTER TABLE Person ADD COLUMN openid_identifier text UNIQUE;
 
--- Give every existing user an openid_identifier
+-- Give users in the openid-testers team an openid_identifier
+-- (Ideally we would give everyone one right now, but that takes over 5 hours)
 CREATE TRIGGER temp_t_set_openid_identifier BEFORE UPDATE ON Person
 FOR EACH ROW EXECUTE PROCEDURE set_openid_identifier();
-UPDATE Person SET openid_identifier=NULL WHERE openid_identifier IS NULL;
-DROP TRIGGER temp_t_set_openid_identifier ON Person;
+UPDATE Person SET openid_identifier=NULL
+    FROM TeamParticipation, Person AS Team
+    WHERE Person.id = TeamParticipation.person
+        AND TeamParticipation.team = Team.id
+        AND Team.name = 'openid-testers'
+        AND Person.openid_identifier IS NULL;
+-- Drop this trigger when we have populated everyone's openid_identifier
+-- and set the column to NOT NULL.
+--DROP TRIGGER temp_t_set_openid_identifier ON Person;
 
--- Make the openid_identifier column required
-ALTER TABLE Person ALTER COLUMN openid_identifier SET NOT NULL;
+-- We should make the openid_identifier column required, but we can't yet.
+-- ALTER TABLE Person ALTER COLUMN openid_identifier SET NOT NULL;
 
 -- Add a trigger so the new column is always filled in on creation
 CREATE TRIGGER set_openid_identifier_t BEFORE INSERT ON Person
