493f1df8e55156635c8e4ed37152b2db37560e44
Michael Hudson <michael.hudson@canonical.com>
2007-06-20 13:27:43 +0100

    respond to review comments:  - add assertions to CodeImportSet.new  - test rewrites

--- lib/canonical/launchpad/database/codeimport.py
+++ lib/canonical/launchpad/database/codeimport.py
@@ -19,7 +19,8 @@ from canonical.database.constants import DEFAULT
 from canonical.database.datetimecol import UtcDateTimeCol
 from canonical.database.enumcol import EnumCol
 from canonical.database.sqlbase import SQLBase
-from canonical.launchpad.interfaces import ICodeImport, ICodeImportSet
+from canonical.launchpad.interfaces import (
+    ICodeImport, ICodeImportSet, ILaunchpadCelebrities)
 from canonical.lp.dbschema import (
     CodeImportReviewStatus, RevisionControlSystems)
 
@@ -51,26 +52,36 @@ class CodeImport(SQLBase):
 
 
 class CodeImportSet:
-    """See ICodeImportSet."""
+    """See `ICodeImportSet`."""
 
     implements(ICodeImportSet)
 
     def new(self, registrant, branch, rcs_type, svn_branch_url=None,
             cvs_root=None, cvs_module=None):
-        """See ICodeImportSet."""
+        """See `ICodeImportSet`."""
         assert branch.owner == getUtility(ILaunchpadCelebrities).vcs_imports
+        if rcs_type == RevisionControlSystems.CVS:
+            assert cvs_root is not None and cvs_module is not None
+            assert svn_branch_url is None
+        elif rcs_type == RevisionControlSystems.SVN:
+            assert cvs_root is None and cvs_module is None
+            assert svn_branch_url is not None
+        else:
+            raise AssertionError(
+                "Don't know how to sanity check source details for unknown "
+                "rcs_type %s"%rcs_type)
         return CodeImport(registrant=registrant, branch=branch,
             rcs_type=rcs_type, svn_branch_url=svn_branch_url,
             cvs_root=cvs_root, cvs_module=cvs_module)
 
     def getAll(self):
-        """See ICodeImportSet."""
+        """See `ICodeImportSet`."""
         return CodeImport.select()
 
     def get(self, id):
-        """See ICodeImportSet."""
+        """See `ICodeImportSet`."""
         return CodeImport.get(id)
 
     def getByBranch(self, branch):
-        """See ICodeImportSet."""
+        """See `ICodeImportSet`."""
         return CodeImport.selectOneBy(branch=branch)
