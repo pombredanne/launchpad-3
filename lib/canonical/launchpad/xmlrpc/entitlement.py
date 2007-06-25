2f5f2d452356e21091e4f404caa030828e5d0b9b
Brad Crittenden <bac@canonical.com>
2007-06-25 08:02:02 -0400

    checkpoint

---
+++ lib/canonical/launchpad/xmlrpc/entitlement.py
@@ -0,0 +1,94 @@
+# Copyright 2007 Canonical Ltd.  All rights reserved.
+
+"""Entitlement XMLRPC API."""
+
+__metaclass__ = type
+__all__ = ['IEntitlementSetAPI', 'EntitlementSetAPI']
+
+import xmlrpclib
+
+from zope.component import getUtility
+from zope.interface import Interface, implements
+
+from canonical.launchpad.interfaces import (
+    IEntitlement, ILaunchBag, IProductSet, IPersonSet, NotFoundError)
+from canonical.launchpad.webapp import LaunchpadXMLRPCView, canonical_url
+from canonical.launchpad.xmlrpc import faults
+from canonical.lp.dbschema import EntitlementStatus
+
+
+class IEntitlementAPI(Interface):
+    """An XMLRPC interface for dealing with entitlements."""
+
+    def get_entitlements_for_person(
+    def register_branch(branch_url, branch_name, branch_title,
+                        branch_description, author_email, product_name):
+        """Register a new branch in Launchpad."""
+
+    def link_branch_to_bug(branch_url, bug_id, whiteboard):
+        """Link the branch to the bug."""
+
+
+class BranchSetAPI(LaunchpadXMLRPCView):
+
+    implements(IBranchSetAPI)
+
+    def register_branch(self, branch_url, branch_name, branch_title,
+                        branch_description, author_email, product_name):
+        """See IBranchSetAPI."""
+        owner = getUtility(ILaunchBag).user
+        assert owner is not None, (
+            "register_branch shouldn't be accessible to unauthenicated"
+            " requests.")
+        if product_name:
+            product = getUtility(IProductSet).getByName(product_name)
+            if product is None:
+                return faults.NoSuchProduct(product_name)
+        else:
+            product = None
+
+        # Branch URLs in Launchpad do not end in a slash, so strip any
+        # slashes from the end of the URL.
+        branch_url = branch_url.rstrip('/')
+
+        existing_branch = getUtility(IBranchSet).getByUrl(branch_url)
+        if existing_branch is not None:
+            return faults.BranchAlreadyRegistered(branch_url)
+
+        # We want it to be None in the database, not ''.
+        if not branch_description:
+            branch_description = None
+        if not branch_title:
+            branch_title = None
+
+        if not branch_name:
+            branch_name = branch_url.split('/')[-1]
+
+        if author_email:
+            author = getUtility(IPersonSet).getByEmail(author_email)
+        else:
+            author = owner
+        if author is None:
+            return faults.NoSuchPerson(
+                type="author", email_address=author_email)
+
+        branch = getUtility(IBranchSet).new(
+            name=branch_name, owner=owner, product=product, url=branch_url,
+            title=branch_title, summary=branch_description, author=author)
+
+        return canonical_url(branch)
+
+    def link_branch_to_bug(self, branch_url, bug_id, whiteboard):
+        """See IBranchSetAPI."""
+        branch = getUtility(IBranchSet).getByUrl(url=branch_url)
+        if branch is None:
+            return faults.NoSuchBranch(branch_url)
+        try:
+            bug = getUtility(IBugSet).get(bug_id)
+        except NotFoundError:
+            return faults.NoSuchBug(bug_id)
+        if not whiteboard:
+            whiteboard = None
+
+        bug.addBranch(branch, whiteboard=whiteboard)
+        return canonical_url(bug)
