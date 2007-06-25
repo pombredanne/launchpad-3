654e2c2daa41cbf25eb6b0a2d08f77e02576c9c7
Michael Hudson <michael.hudson@canonical.com>
2007-06-25 21:51:34 +0100

    after waaaay too much fighting with sampledata, both the doctest and the pagetests pass without bumping into db constraints.

--- database/schema/pending/code-import-sampledata.sql
+++ database/schema/pending/code-import-sampledata.sql
@@ -1,15 +1,30 @@
 DELETE FROM CodeImport;
 
-INSERT INTO CodeImport (branch, registrant, rcs_type, svn_branch_url, review_status)
-       (SELECT branch.id, person.id, 2, 'http://svn.example.org/svnroot/gnome-terminal/trunk', 20
-        FROM product
-        JOIN branch on branch.product = product.id
-        JOIN person on person.name='no-priv'
-        WHERE product.name='gnome-terminal' and branch.url like '%main');
+DELETE FROM Branch where name = 'import';
 
-INSERT INTO CodeImport (branch, registrant, rcs_type, cvs_root, cvs_module)
-       (SELECT series.import_branch, person.id, 1, series.cvsroot, series.cvsmodule
+INSERT INTO Branch (name, owner, product, url, title)
+       (SELECT 'import', person.id, product.id, NULL, 'GNOME Terminal Import Branch'
+       FROM person, product
+       WHERE person.name = 'vcs-imports' AND product.name='gnome-terminal');
+
+INSERT INTO Branch (name, owner, product, url, title)
+       (SELECT 'import', person.id, product.id, NULL, 'Evolution Import Branch'
+       FROM person, product
+       WHERE person.name = 'vcs-imports' AND product.name='evolution');
+
+INSERT INTO CodeImport (id, branch, registrant, rcs_type, svn_branch_url, review_status)
+       (SELECT 1, branch.id, np.id, 2, 'http://svn.example.org/svnroot/gnome-terminal/trunk', 20
+        FROM product
+        JOIN person np on np.name='no-priv'
+        JOIN person vcs on vcs.name='vcs-imports'
+        JOIN branch on branch.product = product.id and branch.owner = vcs.id and branch.name='import'
+        WHERE product.name='gnome-terminal');
+
+INSERT INTO CodeImport (id, branch, registrant, rcs_type, cvs_root, cvs_module)
+       (SELECT 2, branch.id, np.id, 1, ':pserver:anonymous@anoncvs.example.org:/cvs/gnome', 'evolution'
         FROM productseries as series
-        JOIN person on person.name='no-priv'
-        JOIN product on series.product=product.id
-        WHERE product.name='evolution' AND series.name='trunk');
+        JOIN person as np on np.name='no-priv'
+        JOIN person as vcs on vcs.name='vcs-imports'
+        JOIN product on product.name='evolution'
+        JOIN branch on branch.product = product.id and branch.owner = vcs.id and branch.name='import'
+        WHERE series.product=product.id and series.name='trunk');
