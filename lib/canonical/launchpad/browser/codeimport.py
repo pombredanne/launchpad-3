7c5d55e30848071d46eb246ef55897789ba244f6
Michael Hudson <michael.hudson@canonical.com>
2007-06-27 14:47:08 +0100

    after what felt like heroic Z3 hacking, a fairly small amount of code to allow filtering the imports by review_status on the code imports listing page.

--- lib/canonical/launchpad/browser/codeimport.py
+++ lib/canonical/launchpad/browser/codeimport.py
@@ -11,10 +11,14 @@ __all__ = [
     ]
 
 
+from canonical.launchpad import _
 from canonical.launchpad.interfaces import ICodeImportSet
 from canonical.launchpad.webapp import LaunchpadView, Navigation
 from canonical.launchpad.webapp.batching import BatchNavigator
+from canonical.widgets import LaunchpadDropdownWidget
 
+from zope.app.form import CustomWidgetFactory
+from zope.schema import Choice
 
 class CodeImportSetNavigation(Navigation):
 
@@ -31,8 +35,26 @@ class CodeImportSetNavigation(Navigation):
 
 class CodeImportSetView(LaunchpadView):
     def initialize(self):
-        self.batchnav = BatchNavigator(
-            self.context.getAll(), self.request, size=50)
+        # ICodeImport['review_status'].required is True, which means the
+        # generated <select> widget lacks a 'no choice' option.
+        field = Choice(
+            __name__='status', title=_("Review Status"),
+            vocabulary='CodeImportReviewStatus', required=False)
+        factory = CustomWidgetFactory(LaunchpadDropdownWidget)
+        self.status_widget = factory(field.bind(self.context), self.request)
+        self.status_widget._messageNoValue = 'Any'
+
+        status = None
+        if self.status_widget.hasValidInput():
+            status = self.status_widget.getInputValue()
+
+        if status is not None:
+            imports = self.context.search(
+                review_status=status)
+        else:
+            imports = self.context.getAll()
+
+        self.batchnav = BatchNavigator(imports, self.request, size=50)
 
 class CodeImportView(LaunchpadView):
     def initialize(self):
