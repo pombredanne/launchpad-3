231e8e445b41ce2cfe03db11c2f6fc8997bbd3c7
Jeroen Vermeulen <jeroen.vermeulen@canonical.com>
2007-07-04 13:15:32 +0700

    Double underscores in index name, like other indexes

--- database/schema/patch-87-99-0.sql
+++ database/schema/patch-87-99-0.sql
@@ -9,7 +9,7 @@ ALTER TABLE POMsgSet ADD COLUMN language integer REFERENCES Language(id);
 ANALYZE POMsgSet(language);
 
 -- We'll probably want to partition POMsgSet on this column in the future
-CREATE INDEX pomsgset_language_idx ON POMsgSet(language);
+CREATE INDEX pomsgset__language__idx ON POMsgSet(language);
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (87, 99, 0);
 
