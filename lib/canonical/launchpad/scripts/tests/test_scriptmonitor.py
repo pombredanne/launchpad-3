bc2e2788ad0f236ccb9c29af7016c2adce3e90d0
Tom Haddon <tom.haddon@canonical.com>
2007-07-05 15:20:48 -0700

    Working tests

--- lib/canonical/launchpad/scripts/tests/test_scriptmonitor.py
+++ lib/canonical/launchpad/scripts/tests/test_scriptmonitor.py
@@ -1,29 +1,43 @@
 import unittest
-import getpass
 
 from canonical import lp
 from canonical.database.sqlbase import connect
 from canonical.launchpad.scripts.scriptmonitor import check_script
+from canonical.launchpad.scripts import logger
+from canonical.testing import DatabaseLayer
 
 
 class CheckScriptTestCase(unittest.TestCase):
     """Test script activity."""
-    con = connect('dbuser=%s, dbname=%s' % (lp.dbuser, lp.dbname))
+    layer = DatabaseLayer
+
+    def setUp(self):
+        self.con = connect(lp.dbuser)
+        self.log = logger()
+
+    def tearDown(self):
+        self.con.close()
 
     def test_scriptfound(self):
         self.assertEqual(
-            check_script(con, log, 'localhost', 'test-script',
+            check_script(self.con, self.log, 'localhost', 'test-script',
                 '2007-05-23 00:30:00', '2007-05-23 01:30:00'), None)
 
     def test_scriptnotfound_timing(self):
-        self.assertRaises(
-            check_script(con, log, 'localhost', 'test-script',
-                '2007-05-23 01:30:00', '2007-05-23 02:30:00'))
+        output = ("The script 'test-script' didn't run on "
+            "'localhost' between 2007-05-23 01:30:00 and "
+            "2007-05-23 02:30:00 (last seen 2007-05-23 01:00:00)")
+        self.assertEqual(
+            check_script(self.con, self.log, 'localhost', 'test-script',
+                '2007-05-23 01:30:00', '2007-05-23 02:30:00'), output)
 
     def test_scriptnotfound_hostname(self):
-        self.assertRaises(
-            check_script(con, log, 'notlocalhost', 'test-script',
-                '2007-05-23 00:30:00', '2007-05-23 01:30:00'))
+        output = ("The script 'test-script' didn't run on "
+            "'notlocalhost' between 2007-05-23 00:30:00 and "
+            "2007-05-23 01:30:00")
+        self.assertEqual(
+            check_script(self.con, self.log, 'notlocalhost', 'test-script',
+                '2007-05-23 00:30:00', '2007-05-23 01:30:00'), output)
 
 
 def test_suite():
