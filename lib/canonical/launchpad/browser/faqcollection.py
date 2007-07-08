6eeac9f95f65f4da35af55854e6edc9584d0ede9
Francis J. Lacoste <francis.lacoste@contre.com>
2007-07-08 13:24:01 -0400

    Add search to FAQs listing.

--- lib/canonical/launchpad/browser/faqcollection.py
+++ lib/canonical/launchpad/browser/faqcollection.py
@@ -9,11 +9,15 @@ __all__ = [
     'SearchFAQsBaseView',
     ]
 
+from urllib import urlencode
+
 from canonical.launchpad import _
-from canonical.launchpad.interfaces import IFAQCollection, ISearchFAQsForm
+from canonical.launchpad.interfaces import (
+    IFAQCollection, ISearchFAQsForm, QUESTION_STATUS_DEFAULT_SEARCH)
 from canonical.launchpad.webapp import (
-    canonical_url, ContextMenu, LaunchpadFormView, Link)
+    action, canonical_url, ContextMenu, LaunchpadFormView, Link, safe_action)
 from canonical.launchpad.webapp.batching import BatchNavigator
+from canonical.lp.dbschema import QuestionSort
 
 
 class FAQCollectionMenu(ContextMenu):
@@ -38,12 +42,60 @@ class SearchFAQsBaseView(LaunchpadFormView):
 
     schema = ISearchFAQsForm
 
+    # This attribute contains the search_text to use.
+    search_text = None
+
+    # This attribute is updated to the number of matching questions when
+    # the user does a search.
+    matching_questions_count = 0
+
     @property
     def heading(self):
         """Return the heading that should be used for the listing."""
-        return _('FAQs for $displayname',
-                 mapping=dict(displayname=self.context.displayname))
+        replacements=dict(
+            displayname=self.context.displayname,
+            search_text=self.search_text)
+        if self.search_text:
+            return _(u'FAQs matching \u201c${search_text}\u201d for '
+                     u'$displayname', mapping=replacements)
+        else:
+            return _('FAQs for $displayname', mapping=replacements)
+
+    @property
+    def empty_listing_message(self):
+        """Return the message to render when there are no FAQs to display."""
+        replacements=dict(
+            displayname=self.context.displayname,
+            search_text=self.search_text)        
+        if self.search_text:
+            return _(u'There are no FAQs for $displayname matching '
+                     u'\u201c${search_text}\u201d.', mapping=replacements)
+        else:
+            return _('There are no FAQs for $displayname.',
+                     mapping=replacements)
 
     def getMatchingFAQs(self):
         """Return a BatchNavigator of the matching FAQs."""
-        return BatchNavigator(self.context.searchFAQs(), self.request)
+        faqs = self.context.searchFAQs(search_text=self.search_text)
+        return BatchNavigator(faqs, self.request)
+
+    @safe_action
+    @action(_('Search'), name='search')
+    def search_action(self, action, data):
+        """Filter the search results by keywords."""
+        self.search_text = data.get('search_text', None)
+        if self.search_text:
+            matching_questions = self.context.searchQuestions(
+                search_text=self.search_text)
+            self.matching_questions_count = matching_questions.count()
+
+    @property
+    def matching_questions_url(self):
+        """Return the URL to the questions matching the same keywords."""
+        return canonical_url(self.context) + '/+questions?' + urlencode(
+            {'field.status': [
+                status.title for status in QUESTION_STATUS_DEFAULT_SEARCH],
+             'field.search_text': self.search_text,
+             'field.actions.search': 'Search',
+             'field.sort' : QuestionSort.RELEVANCY.title,
+             'field.language-empty-marker': 1}, doseq=True)
