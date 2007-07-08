f9d912382d49903a0c00b29803cc1a26d9a098d9
Francis J. Lacoste <francis.lacoste@contre.com>
2007-07-08 17:23:57 -0400

    Add info about related FAQ to notification.

--- lib/canonical/launchpad/mailnotification.py
+++ lib/canonical/launchpad/mailnotification.py
@@ -1373,6 +1373,15 @@ class QuestionModifiedDefaultNotification(QuestionNotification):
                 indent + '%s\n' % canonical_url(unlinked_bug) +
                 indent + '"%s"' % unlinked_bug.title)
 
+        if question.faq != old_question.faq:
+            if question.faq is None:
+                info_fields.append(indent + 'Related FAQ was removed.')
+            else:
+                info_fields.append(
+                    indent + 'Related FAQ set to:\n' +
+                    indent + question.faq.title + '\n' +
+                    indent + canonical_url(question.faq))
+
         if question.title != old_question.title:
             info_fields.append('Summary changed to:\n%s' % question.title)
         if question.description != old_question.description:
