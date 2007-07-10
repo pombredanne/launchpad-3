a4ac320336e9977b409060ad185669e308496334
Stuart Bishop <stuart@stuartbishop.net>
2007-07-10 20:28:57 +0700

    Give codeimport.date_last_successful db patch an official patch number

--- database/schema/patch-87-29-0.sql
+++ database/schema/patch-87-29-0.sql
@@ -4,5 +4,5 @@ ALTER TABLE CodeImport ADD COLUMN
     date_last_successful TIMESTAMP WITHOUT TIME ZONE;
 
 /* DBA set a real db patch number on review. -- David Allouche 2007-07-03. */
-INSERT INTO LaunchpadDatabaseRevision VALUES (87, 89, 0);
+INSERT INTO LaunchpadDatabaseRevision VALUES (87, 29, 0);
 
