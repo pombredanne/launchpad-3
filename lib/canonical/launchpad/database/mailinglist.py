5a357669aa0b2adcc2dd46c109fb7b0572296b2f
Barry Warsaw <barry@canonical.com>
2007-07-13 09:31:28 -0400

    More fleshing out of the mailing list database classes and doctest.

--- lib/canonical/launchpad/database/mailinglist.py
+++ lib/canonical/launchpad/database/mailinglist.py
@@ -2,22 +2,38 @@
 
 __metaclass__ = type
 
+__all__ = [
+    'MailingList',
+    'MailingListRegistry',
+    ]
+
 import datetime
+import pytz
 
 from sqlobject import (
     BoolCol, ForeignKey, IntCol, MultipleJoin, SQLMultipleJoin,
     SQLObjectNotFound, SQLRelatedJoin, StringCol)
 from sqlobject.sqlbuilder import AND, OR, SQLConstant
+from zope.component import getUtility
 from zope.interface import implements
 
+from canonical.database.sqlbase import (
+    cursor, flush_database_caches, flush_database_updates, quote, quote_like,
+    sqlvalues, SQLBase)
 from canonical.database.constants import UTC_NOW, DEFAULT
 from canonical.database.enumcol import EnumCol
-from canonical.launchpad.interfaces import IMailingList, IMailingListRegistry
+from canonical.launchpad.interfaces import (
+    ILaunchpadCelebrities, IMailingList, IMailingListRegistry)
 from canonical.lp.dbschema import MailingListStatus
+from canonical.database.datetimecol import UtcDateTimeCol
 
 
 class MailingList(SQLBase):
     implements(IMailingList)
+
+    def __repr__(self):
+        return '<MailingList for team "%s"; status=%s at %#x>' % (
+            self.team.name, self.status.name, id(self))
 
     team = ForeignKey(dbName='team', foreignKey='Person')
 
@@ -25,36 +41,43 @@ class MailingList(SQLBase):
     date_registered = UtcDateTimeCol(notNull=True, default=UTC_NOW)
 
     reviewer = ForeignKey(dbName='reviewer', foreignKey='Person', default=None)
-    date_reviewed = UtcDateTimeCol(notNull=True, default=UTC_NOW)
 
-    date_activated = UtcDateTimeCol(notNull=True, default=UTC_NOW)
+    date_reviewed = UtcDateTimeCol(notNull=True, default=None)
 
-    status = EnumCol(schema=MailingListStatus, default=None)
+    date_activated = UtcDateTimeCol(notNull=True, default=None)
+
+    status = EnumCol(schema=MailingListStatus,
+                     default=MailingListStatus.REGISTERED)
 
     welcome_message_text = StringCol(default=None)
 
     def review(self, reviewer, status):
+        """See `IMailingList`"""
         # Only mailing lists which are in the REGISTERED state may be
         # reviewed.  This is the state for newly requested mailing lists.
         assert self.status == MailingListStatus.REGISTERED, (
-            'Only registered mailing lists may be reviewed.')
+            'Only unreviewed mailing lists may be reviewed')
         # A registered mailing list may only transition to either APPROVED or
         # DECLINED state.
         assert status in (MailingListStatus.APPROVED,
                           MailingListStatus.DECLINED), (
             'Reviewed lists may only be approved or declined')
         # The reviewer must be a Launchpad administrator.
-        # XXX
+        assert reviewer is not None and reviewer.inTeam(
+            getUtility(ILaunchpadCelebrities).admin), (
+            'Reviewer must be a Launchpad administrator')
         self.reviewer = reviewer
         self.status = status
-        self.date_reviewed = datetime.datetime.now()
+        self.date_reviewed = datetime.datetime.now(pytz.timezone('UTC'))
 
     def construct(self):
+        """See `IMailingList`"""
         assert self.status == MailingListStatus.APPROVED, (
             'Only approved mailing lists may be constructed')
         self.status = MailingListStatus.CONSTRUCTING
 
     def reportConstructionResult(self, status):
+        """See `IMailingList`"""
         assert self.status == MailingListStatus.CONSTRUCTING, (
             'The mailing list is not under construction')
         assert status in (MailingListStatus.ACTIVE,
@@ -63,23 +86,40 @@ class MailingList(SQLBase):
         self.status = status
 
     def deactivate(self):
+        """See `IMailingList`"""
         assert self.status == MailingListStatus.ACTIVE, (
             'Only active mailing lists may be deactivated')
+        self.status = MailingListStatus.INACTIVE
 
 
 class MailingListRegistry:
     implements(IMailingListRegistry)
 
     def register(self, team):
-        # XXX
+        """See `IMailingListRegistry`"""
+        assert team.isTeam(), (
+            'Cannot register a list for a person who is not a team')
+        assert self.getTeamMailingList(team) is None, (
+            'Mailing list for team "%s" already exists' % team.name)
+        return MailingList(team=team, registrant=team.teamowner)
 
     def getTeamMailingList(self, team):
-        # XXX
+        """See `IMailingListRegistry`"""
+        results = list(MailingList.selectBy(team=team))
+        if len(results) == 0:
+            return None
+        elif len(results) == 1:
+            return results[0]
+        else:
+            assert len(results) <= 1, (
+                'Too many MailingLists registered for team "%s"' % team.name)
 
     @property
     def registered_lists(self):
-        # XXX
+        """See `IMailingListRegistry`"""
+        return MailingList.selectBy(status=MailingListStatus.REGISTERED)
 
     @property
     def approved_lists(self):
-        # XXX
+        """See `IMailingListRegistry`"""
+        return MailingList.selectBy(status=MailingListStatus.APPROVED)
