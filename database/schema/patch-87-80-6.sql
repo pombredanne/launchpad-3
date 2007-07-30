52d4c7c61fbb537d36502be7fdff8f6e87296812
David Allouche <david.allouche@canonical.com>
2007-07-30 19:28:02 +0200

    Tweak database patch comments and layout.

--- database/schema/patch-87-80-6.sql
+++ database/schema/patch-87-80-6.sql
@@ -7,17 +7,20 @@ ALTER TABLE CodeImport ADD COLUMN update_interval interval;
 ALTER TABLE CodeImportMachine ADD COLUMN
     heartbeat TIMESTAMP WITHOUT TIME ZONE;
 
+/* Turn the CodeImportMachine.online bool into a CodeImportMachine.state enum,
+with three states OFFLINE (10), ONLINE (20) and QUIESCING (30).
+
+Also implement data migration, this is not terribly useful since we should not
+have any data to migrate yet either in the sampledata or in production. But
+it would be the correct thing to do if we had some. */
+
 ALTER TABLE CodeImportMachine ADD COLUMN state integer;
-
-/* This is not terribly useful since we should not have any row in this table
-yet either in the sampledata or in production. But it would be the correct
-thing to do should we have data to migrate. */
 UPDATE CodeImportMachine SET state=(
-    CASE WHEN online=FALSE THEN 10
-         WHEN online=TRUE THEN 20
+    CASE WHEN online=FALSE THEN 10 -- OFFLINE
+         WHEN online=TRUE THEN 20  -- ONLINE
     END);
-
 ALTER TABLE CodeImportMachine ALTER state SET NOT NULL;
+ALTER TABLE CodeImportMachine ALTER state SET DEFAULT 10; -- OFFLINE
 ALTER TABLE CodeImportMachine DROP COLUMN online;
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (87, 80, 6);
