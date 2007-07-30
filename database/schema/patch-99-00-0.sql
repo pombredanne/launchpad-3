0211a07dc0ffb4f6db3abe2daab2aa1f5b945f94
Tom Berger <tom.berger@gmail.com>
2007-07-30 11:48:30 +0100

    move db patch so that it can be applied by make schema

--- database/schema/patch-99-00-0.sql
+++ database/schema/patch-99-00-0.sql
@@ -4,4 +4,4 @@ ALTER TABLE BugTracker ALTER COLUMN title DROP NOT NULL;
 ALTER TABLE BugTracker ALTER COLUMN summary DROP NOT NULL;
 ALTER TABLE BugTracker ALTER COLUMN contactdetails DROP NOT NULL;
 
--- INSERT INTO LaunchpadDatabaseRevision VALUES (??, ??, ??);
+INSERT INTO LaunchpadDatabaseRevision VALUES (99, 0, 0);
