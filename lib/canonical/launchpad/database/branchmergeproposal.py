f731955de974223f3cd7f1638fc34c89ec1e4568
Tim Penhey <tim.penhey@canonical.com>
2007-08-01 12:09:32 +1200

    Forgotten renames.

--- lib/canonical/launchpad/database/branchmergeproposal.py
+++ lib/canonical/launchpad/database/branchmergeproposal.py
@@ -9,7 +9,7 @@ __all__ = [
 
 from zope.interface import implements
 
-from sqlobject import ForeignKey
+from sqlobject import ForeignKey, StringCol
 
 from canonical.database.constants import DEFAULT
 from canonical.database.datetimecol import UtcDateTimeCol
@@ -43,39 +43,39 @@ class BranchMergeProposal(SQLBase):
     date_created = UtcDateTimeCol(notNull=True, default=DEFAULT)
 
 
-class BranchLandingTargetSet:
+class BranchMergeProposalSet:
     """The set of defined landing targets."""
 
-    implements(IBranchLandingTargetSet)
+    implements(IBranchMergeProposalSet)
 
     def new(self, registrant, source_branch, target_branch):
         """See IBranchLandingTargetSet."""
         if source_branch == target_branch:
-            raise InvalidBranchLandingTarget(
+            raise InvalidBranchMergeProposal(
                 'Source and target branches must be different.')
 
         if source_branch.product is None:
-            raise InvalidBranchLandingTarget(
+            raise InvalidBranchMergeProposal(
                 'Junk branches cannot be used as source branches.')
 
         if target_branch.product is None:
-            raise InvalidBranchLandingTarget(
+            raise InvalidBranchMergeProposal(
                 'Junk branches cannot be used as target branches.')
 
         if source_branch.product != target_branch.product:
-            raise InvalidBranchLandingTarget(
+            raise InvalidBranchMergeProposal(
                 'The source branch and target branch must be branches of the '
                 'same project.')
 
-        target = BranchLandingTarget.selectOneBy(
+        target = BranchMergeProposal.selectOneBy(
             registrant=registrant, source_branch=source_branch,
             target_branch=target_branch)
         if target is not None:
-            raise InvalidBranchLandingTarget(
+            raise InvalidBranchMergeProposal(
                 'There is already a landing target registered for '
                 'branch %s to land on %s'
                 % (source_branch.unique_name, target_branch.unique_name))
 
-        return BranchLandingTarget(
+        return BranchMergeProposal(
             registrant=registrant, source_branch=source_branch,
             target_branch=target_branch)
