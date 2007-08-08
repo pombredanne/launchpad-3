a08ff703e228c5a42035c8a842a28401c61c019b
Stuart Bishop <stuart@stuartbishop.net>
2007-08-08 10:33:42 +0700

    Add DEFAULT and NOT NULL to Person.account_status

--- database/schema/patch-87-32-2.sql
+++ database/schema/patch-87-32-2.sql
@@ -1,11 +1,5 @@
 SET client_min_messages=ERROR;
 
--- No point adding this yet, as this column is not being maintained
--- properly until salgado's bug-2773 branch lands.
--- UPDATE Person SET account_status = 10 WHERE account_status IS NULL;
--- ALTER TABLE Person ALTER COLUMN account_status SET DEFAULT 10;
--- ALTER TABLE Person ALTER COLUMN account_status SET NOT NULL;
-
 ALTER TABLE Person
 ALTER COLUMN personal_standing SET DEFAULT 0,
 ALTER COLUMN mailing_list_receive_duplicates SET DEFAULT TRUE,
@@ -14,20 +8,24 @@ ALTER COLUMN mailing_list_auto_subscribe_policy SET DEFAULT 1;
 UPDATE Person SET
     personal_standing = DEFAULT,
     mailing_list_receive_duplicates = DEFAULT,
-    mailing_list_auto_subscribe_policy = DEFAULT
+    mailing_list_auto_subscribe_policy = DEFAULT,
+    account_status = 10
 WHERE
     personal_standing IS NULL
     AND mailing_list_receive_duplicates IS NULL
-    AND mailing_list_auto_subscribe_policy IS NULL;
+    AND mailing_list_auto_subscribe_policy IS NULL
+    AND account_status IS NULL;
 
 ALTER TABLE Person
 ALTER COLUMN personal_standing SET NOT NULL,
 ALTER COLUMN mailing_list_receive_duplicates SET NOT NULL,
 ALTER COLUMN mailing_list_auto_subscribe_policy SET NOT NULL,
+ALTER COLUMN account_status SET DEFAULT 10,
+ALTER COLUMN account_status SET NOT NULL,
 -- Populated by trigger
 ALTER COLUMN openid_identifier SET NOT NULL;
 
--- Too slow - do during downtime
+-- Too slow - do during next rollout
 -- ALTER TABLE POMsgSet ALTER COLUMN language SET NOT NULL;
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (87, 32, 2);
