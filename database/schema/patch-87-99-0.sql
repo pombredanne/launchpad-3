7278d0ebcaa8cfd35bce896c34c605d0a6868628
Celso Providelo <celso.providelo@canonical.com>
2007-08-14 16:46:03 -0300

    remove obsolete DB patch.

--- database/schema/patch-87-99-0.sql
+++
@@ -1,9 +0,0 @@
-/*
- * Adds a index to speed up queries on LibraryFileAlias by filename
- */
-
-SET client_min_messages=ERROR;
-
-CREATE INDEX libraryfilealias_filename_idx on libraryfilealias using btree (filename);
-
-INSERT INTO LaunchpadDatabaseRevision VALUES (87, 99, 0);
