6d96af529186cdc47a6ae091a5f2374bf29cec95
Stuart Bishop <stuart@stuartbishop.net>
2007-08-24 14:51:48 +0700

    Work in progress

--- database/schema/pending/patch-87-21-0.sql
+++ database/schema/pending/patch-87-21-0.sql
@@ -2,7 +2,7 @@ BEGIN;
 
 SET client_min_messages=ERROR;
 
--- Rename DistroRelease table
+-- DistroRelease table -> DistroSeries
 ALTER TABLE DistroRelease RENAME TO DistroSeries;
 
 ALTER TABLE distrorelease_id_seq RENAME TO distroseries_id_seq;
@@ -39,12 +39,110 @@ ALTER TABLE DistroSeries RENAME COLUMN parentrelease TO parent_series;
 ALTER TABLE DistroSeries ADD CONSTRAINT distroseries__parent_series__fk
     FOREIGN KEY (parent_series) REFERENCES DistroSeries;
 
-\d distroseries
-        
+-- DistroReleaseLanguage table -> DistroSeriesLanguage
 ALTER TABLE DistroReleaseLanguage RENAME TO DistroSeriesLanguage;
+
+ALTER TABLE distroreleaselanguage_id_seq RENAME TO distroserieslanguage_id_seq;
+ALTER TABLE DistroSeriesLanguage ALTER COLUMN id
+    SET DEFAULT nextval('distroserieslanguage_id_seq');
+
 ALTER TABLE DistroSeriesLanguage RENAME COLUMN distrorelease TO distroseries;
 
+ALTER TABLE distroreleaselanguage_pkey RENAME TO distroserieslanguage_pkey;
+ALTER TABLE distroreleaselanguage_distrorelease_language_uniq
+    RENAME TO distroserieslanguage__distrorelease__language__key;
+ALTER TABLE DistroSeriesLanguage
+    ADD CONSTRAINT distroserieslanguage__distroseries__fk
+    FOREIGN KEY (distroseries) REFERENCES DistroSeries;
+ALTER TABLE DistroSeriesLanguage
+    DROP CONSTRAINT distroreleaselanguage_distrorelease_fk;
+ALTER TABLE DistroSeriesLanguage
+    ADD CONSTRAINT distroserieslanguage__language__fk
+    FOREIGN KEY (language) REFERENCES Language;
+
+-- DistroArchRelease -> DistroArchSeries
+ALTER TABLE DistroArchRelease RENAME TO DistroArchSeries;
+
+ALTER TABLE distroarchrelease_id_seq RENAME TO distroarchseries_id_seq;
+ALTER TABLE DistroArchSeries ALTER COLUMN id
+    SET DEFAULT nextval('distroarchseries_id_seq');
+
+ALTER TABLE DistroArchSeries RENAME COLUMN distrorelease TO distroseries;
+
+ALTER TABLE distroarchrelease_pkey RENAME TO distroarchseries_pkey;
+ALTER TABLE DistroArchSeries
+    ADD CONSTRAINT distroarchseries__architecturetag__distroseries__key
+    UNIQUE (architecturetag, distroseries);
+ALTER TABLE DistroArchSeries DROP CONSTRAINT
+    distroarchrelease_distrorelease_architecturetag_unique;
+DROP INDEX distroarchrelease_architecturetag_idx;
+ALTER TABLE distroarchrelease_distrorelease_idx
+    RENAME TO distroarchseries__distroseries__idx;
+ALTER TABLE DistroArchSeries
+    DROP CONSTRAINT distroarchrelease_distrorelease_processorfamily_unique;
+ALTER TABLE DistroArchSeries
+    ADD CONSTRAINT distroarchseries__processorfamily__distroseries__key
+    UNIQUE (processorfamily, distroseries);
+DROP INDEX distroarchrelease_processorfamily_idx;
+
+ALTER TABLE DistroArchSeries ADD CONSTRAINT distroarchseries__distroseries__fk
+    FOREIGN KEY (distroseries) REFERENCES DistroSeries;
+ALTER TABLE DistroArchSeries DROP CONSTRAINT "$1";
+ALTER TABLE DistroArchSeries
+    ADD CONSTRAINT distroarchseries__processorfamily__fk
+    FOREIGN KEY (processorfamily) REFERENCES ProcessorFamily;
+ALTER TABLE DistroArchSeries DROP CONSTRAINT "$2";
+ALTER TABLE DistroArchSeries ADD CONSTRAINT distroarchseries__owner__fk
+    FOREIGN KEY (owner) REFERENCES Person;
+CREATE INDEX distroarchseries__owner__idx ON DistroArchSeries(owner);
+ALTER TABLE DistroArchSeries DROP CONSTRAINT "$3";
+
+-- DistroReleasePackageCache -> DistroSeriesPackageCache
+ALTER TABLE DistroReleasePackageCache RENAME TO DistroSeriesPackageCache;
+
+ALTER TABLE distroreleasepackagecache_id_seq
+    RENAME TO distroseriespackagecache_id_seq;
+ALTER TABLE DistroSeriesPackageCache ALTER COLUMN id
+    SET DEFAULT nextval('distroseriespackagecache');
+
+ALTER TABLE DistroSeriesPackageCache
+    RENAME COLUMN distrorelease TO distroseries;
+
+ALTER TABLE distroreleasepackagecache_pkey
+    RENAME TO distroseriespackagecache_pkey;
+ALTER TABLE distroreleasepackagecache_fti
+    RENAME TO distroseriespackagecache_fti;
+
+ALTER TABLE DistroSeriesPackageCache
+    ADD CONSTRAINT
+    distroseriespackagecache__binarypackagename_distroseries__key
+    UNIQUE (binarypackagename, distroseries),
+
+    DROP CONSTRAINT
+    distroreleasepackagecache_distrorelease_binarypackagename_uniq,
+
+    DROP CONSTRAINT distroreleasepackagecache_binarypackagename_fk,
+
+    ADD CONSTRAINT distroseriespackagecache__binarypackagename__fk
+    FOREIGN KEY (binarypackagename) REFERENCES BinaryPackageName,
+
+    DROP CONSTRAINT distroreleasepackagecache_distrorelease_fk,
+
+    ADD CONSTRAINT distroseriespackagecache__distroseries__fk
+    FOREIGN KEY (distroseries) REFERENCES DistroSeries;
+
+CREATE INDEX distroseriespackagecache__distroseries__idx
+    ON DistroSeriesPackageCache(distroseries);
+
+
+-- BugNomination
+ALTER TABLE BugNomination RENAME COLUMN distrorelease TO distroseries;
+ALTER TABLE BugNomination
+    DROP CONSTRAINT bugnomination__distrorelease__fk,
+    ADD CONSTRAINT bugnomination__distroseries__fk
+        FOREIGN KEY (distroseries) REFERENCES DistroSeries;
+
+\d BugNomination
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (87, 21, 0);
-
 ABORT;
