c50c2a74e8319917d8e757659a21a530d17d64b6
Graham Binns <graham.binns@canonical.com>
2007-08-25 20:53:38 +0100

    Removed pdb code, which would have made life interesting...

--- lib/canonical/launchpad/components/externalbugtracker.py
+++ lib/canonical/launchpad/components/externalbugtracker.py
@@ -795,7 +795,6 @@ class Trac(ExternalBugTracker):
             # (correctly) return an HTTP 500 if CSV exports are not available,
             # but we check the returned data's content type if the server
             # gives us an HTTP 200, just in case.
-            import pdb; pdb.set_trace()
             http_data = self.urlopen(
                 "%s/%s" % (self.baseurl, self.ticket_url % 1))
             return http_data.headers.subtype == 'csv'
