031456dbadac0c1e77f4cfcb308783dcc5d83a46
Carlos Perello Marin <carlos.perello@canonical.com>
2007-08-31 12:35:47 +0200

    Fixed the db constraints to cope with the new code

---
+++ database/schema/pending/bug-133611.sql
@@ -0,0 +1,14 @@
+SET client_min_messages=ERROR;
+
+DROP INDEX unique_entry_per_importer;
+
+CREATE UNIQUE INDEX unique_entry_per_importer ON TranslationImportQueueEntry (
+                        importer,
+                        path,
+                        (COALESCE(potemplate, -1)),
+                        (COALESCE(distrorelease, -1)),
+                        (COALESCE(sourcepackagename, -1)),
+                        (COALESCE(productseries, -1))
+                        );
+
+INSERT INTO LaunchpadDatabaseRevision VALUES (87, 99, 0);
