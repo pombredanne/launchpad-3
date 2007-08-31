d68955e7cd9d8e5dfb335d072c7d15e398eaaf23
David Allouche <david.allouche@canonical.com>
2007-08-31 15:46:14 +0200

    Combine ftests and tests modules in c.l.scripts.supermirror.

--- lib/canonical/launchpad/scripts/supermirror/tests/test_jobmanager.py
+++ lib/canonical/launchpad/scripts/supermirror/tests/test_jobmanager.py
@@ -11,7 +11,7 @@ from canonical.launchpad.interfaces import BranchType
 from canonical.launchpad.scripts.supermirror.branchtomirror import (
     BranchToMirror)
 from canonical.launchpad.scripts.supermirror.branchtargeter import branchtarget
-from canonical.launchpad.scripts.supermirror.ftests import createbranch
+from canonical.launchpad.scripts.supermirror.tests import createbranch
 from canonical.launchpad.scripts.supermirror import jobmanager
 from canonical.authserver.client.branchstatus import BranchStatusClient
 from canonical.authserver.tests.harness import AuthserverTacTestSetup
