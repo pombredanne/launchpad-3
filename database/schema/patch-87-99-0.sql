7d27e457b318b401bffb14fe8f37047961f7572e
Tim Penhey <tim.penhey@canonical.com>
2007-09-03 15:27:14 +1200

    First cut removal.

---
+++ database/schema/patch-87-99-0.sql
@@ -0,0 +1,9 @@
+SET client_min_messages=ERROR;
+
+-- Not used any more (if ever).
+DROP TABLE DevelopmentManifest;
+DROP TABLE ManifestAncestry;
+DROP TABLE ManifestEntry;
+DROP TABLE Manifest;
+
+INSERT INTO LaunchpadDatabaseRevision VALUES (87, 99, 0);
