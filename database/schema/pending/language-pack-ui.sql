f48b4f7f5d7b132b760f0af6b124b7aadac2ede8
Carlos Perello Marin <carlos.perello@canonical.com>
2007-09-03 12:33:47 +0200

    Small improvements after starting with the implementation

--- database/schema/pending/language-pack-ui.sql
+++ database/schema/pending/language-pack-ui.sql
@@ -3,7 +3,10 @@ SET client_min_messages=ERROR;
 CREATE TABLE LanguagePack (
     id serial NOT NULL PRIMARY KEY,
     file integer NOT NULL REFERENCES libraryfilealias(id),
-    date_exported timestamp without time zone NOT NULL,
+    date_exported timestamp without time zone NOT NULL
+        DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC'),
+    date_last_use timestamp without time zone
+        DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC'),
     distro_release integer NOT NULL REFERENCES DistroRelease(id),
     type integer NOT NULL DEFAULT 0,
     language_pack_that_updates integer REFERENCES LanguagePack(id)
@@ -20,6 +23,9 @@ COMMENT ON COLUMN LanguagePack.file IS 'Librarian file where the language pack
 is stored.';
 COMMENT ON COLUMN LanguagePack.date_exported IS 'When was exported the
 language pack.';
+COMMENT ON COLUMN LanguagePack.date_last_use IS 'When did we stop using the
+language pack. It\'s used to decide whether we can remove it completely from
+the system. When it\'s being used, its value is NULL';
 COMMENT ON COLUMN LanguagePack.distro_release IS 'The distribution release
 from where this language pack was exported.';
 COMMENT ON COLUMN LanguagePack.type IS 'Type of language pack. There are two
@@ -37,10 +43,11 @@ ALTER TABLE DistroRelease
 ALTER TABLE DistroRelease
     ADD COLUMN language_pack_full_export_requested BOOLEAN DEFAULT FALSE;
 ALTER TABLE DistroRelease
+    ALTER COLUMN language_pack_full_export_requested SET NOT NULL;
+ALTER TABLE DistroRelease
     ADD CONSTRAINT distrorelease_has_language_pack_base
-    CHECK (language_pack_base IS NOT NULL OR
-           (language_pack_delta IS NULL AND
-            language_pack_proposed IS NULL));
+    CHECK (language_pack_base IS NOT NULL OR language_pack_delta IS NULL);
+
 COMMENT ON COLUMN DistroRelease.language_pack_base IS 'Current full export
 language pack for this distribution release.';
 COMMENT ON COLUMN DistroRelease.language_pack_delta IS 'Current language pack
