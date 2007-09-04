9b6678e3aa09d5871e2ed9e7d03cc9a57094d703
Graham Binns <graham.binns@canonical.com>
2007-09-04 12:44:58 +0100

    Added remote bug ids / statuses and a huge honking wodge of test data.

--- lib/canonical/launchpad/ftests/externalbugtracker.py
+++ lib/canonical/launchpad/ftests/externalbugtracker.py
@@ -260,4 +260,4 @@ class TestPython(Python):
 
         file_path = os.path.join(os.path.dirname(__file__), 'testfiles')
         return open(
-            file_path + '/' + 'roundup_example_ticket_export.csv', 'r')
+            file_path + '/' + 'python_example_ticket_export.csv', 'r')
