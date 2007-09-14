74824cab34caf7552ac4005eaa3c750ac54bb101
Jeroen Vermeulen <jeroen.vermeulen@canonical.com>
2007-09-14 17:47:52 +0700

    Send notification emails if errors encountered.

--- lib/canonical/launchpad/scripts/verify_pofile_stats.py
+++ lib/canonical/launchpad/scripts/verify_pofile_stats.py
@@ -11,13 +11,20 @@ import logging
 from zope.component import getUtility
 from zope.interface import implements
 
+from canonical.config import config
 from canonical.launchpad.interfaces.looptuner import ITunableLoop
 from canonical.launchpad.interfaces import IPOFileSet
+from canonical.launchpad.mail.sendmail import simple_sendmail
+from canonical.launchpad.mailnotification import MailWrapper
 from canonical.launchpad.utilities.looptuner import LoopTuner
 
 class Verifier:
     """`ITunableLoop` that recomputes & checks all `POFile`s' statistics."""
     implements(ITunableLoop)
+
+    total_checked = 0
+    total_incorrect = 0
+    total_exceptions = 0
 
     def __init__(self, transaction, logger, start_at_id=0):
         self.transaction = transaction
@@ -42,6 +49,7 @@ class Verifier:
 
         self.start_id = None
         for pofile in pofiles:
+            self.total_checked += 1
             # Set starting point of next batch to right after the POFile we're
             # looking at.  If we don't get any POFiles, this loop iterates
             # zero times and start_id will remain set to None.
@@ -52,6 +60,7 @@ class Verifier:
                 # Verification failed for this POFile.  Don't bail out: if
                 # there's a pattern of failure, we'll want to report that and
                 # not just the first problem we encounter.
+                self.total_exceptions += 1
                 self.logger.warning(
                     "Error %s while recomputing stats for POFile %d: %s"
                     % (type(error), pofile.id, error))
@@ -69,6 +78,7 @@ class Verifier:
         old_stats = pofile.getStatistics()
         new_stats = pofile.updateStatistics()
         if new_stats != old_stats:
+            self.total_incorrect += 1
             self.logger.warning(
                 "POFile %d: cached stats were %s, recomputed as %s"
                 % (pofile.id, str(old_stats), str(new_stats)))
@@ -95,5 +105,27 @@ class VerifyPOFileStatsProcess:
         # make relatively long, low-overhead iterations without disrupting
         # application response times.
         LoopTuner(loop, 4).run()
+
+        if loop.total_incorrect > 0 or loop.total_exceptions > 0:
+            # Not all statistics were correct, or there were failures while
+            # checking them.  Email the admins.
+            message = (
+                "The POFile statistics verifier encountered errors while "
+                "checking cached statistics in the database:\n"
+                "\n"
+                "Exceptions: %d\n"
+                "POFiles with incorrect statistics: %d\n"
+                "Total POFiles checked: %d\n"
+                "\n"
+                "See the pofile-stats log for detailed information.\n"
+                % (loop.total_exceptions, loop.total_incorrect,
+                    loop.total_checked))
+            simple_sendmail(
+                from_addr=config.rosetta.rosettaadmin.email,
+                to_addrs=[config.rosetta.rosettaadmin.email],
+                subject="POFile statistics errors",
+                body=MailWrapper().format(message))
+            self.transaction.commit()
+
         self.logger.info("Done.")
 
