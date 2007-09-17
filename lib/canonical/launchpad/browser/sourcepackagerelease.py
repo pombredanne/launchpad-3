d2fda782dbdc5ad6a15c8693cd4c0bbad6fc224e
Julian Edwards <ed@julian-edwards.com>
2007-09-17 14:12:02 +0100

    Expand tests to include functional testing for linkifying bugs in changelogs.

--- lib/canonical/launchpad/browser/sourcepackagerelease.py
+++ lib/canonical/launchpad/browser/sourcepackagerelease.py
@@ -23,25 +23,9 @@ class SourcePackageReleaseView(LaunchpadView):
         return self._linkify_changelog(
             self.context.changelog, self.context.name)
 
-    def _linkify_changelog(self, changelog, sourcepkgnametxt):
-        if changelog is None:
-            return ''
-        changelog = cgi.escape(changelog)
-
-        # Linkify the package name.
-        escaped_name = re.escape(sourcepkgnametxt)
-        matches = re.findall(r'%s (\(([^)]+)\) (\w+));' % escaped_name,
-            changelog)
-        for match_text, version, distroseries in matches:
-            # Rather ugly to construct this URL, but it avoids the need to
-            # look up database objects for each matching line of text here.
-            url = '../../../%s/+source/%s/%s' % (
-                distroseries, sourcepkgnametxt, version)
-            changelog = changelog.replace(match_text,
-                '(<a href="%s">%s</a>) %s' % (url, version, distroseries))
-
-        # Linkify bug numbers of the format "LP: #<number>"
-        matches = re.findall(r'(LP: #(\d+))', changelog)
+    def _linkify_bug_numbers(self, changelog):
+        """Linkify to a bug if LP: #number appears in the changelog text."""
+        matches = re.findall(r'(LP:\s*#(\d+))', changelog)
         bug_set = getUtility(IBugSet)
         for match_text, bug_id in matches:
             try:
@@ -55,4 +39,27 @@ class SourcePackageReleaseView(LaunchpadView):
 
         return changelog
 
+    def _linkify_packagename(self, changelog, sourcepkgnametxt):
+        """Linkify a package name and its version in changelog text."""
+        escaped_name = re.escape(sourcepkgnametxt)
+        matches = re.findall(r'%s (\(([^)]+)\) (\w+));' % escaped_name,
+            changelog)
+        for match_text, version, distroseries in matches:
+            # Rather ugly to construct this URL, but it avoids the need to
+            # look up database objects for each matching line of text here.
+            url = '../../../%s/+source/%s/%s' % (
+                distroseries, sourcepkgnametxt, version)
+            changelog = changelog.replace(match_text,
+                '(<a href="%s">%s</a>) %s' % (url, version, distroseries))
+
+        return changelog
+
+    def _linkify_changelog(self, changelog, sourcepkgnametxt):
+        """Linkify source packages and bug numbers in changelogs."""
+        if changelog is None:
+            return ''
+        changelog = cgi.escape(changelog)
+        changelog = self._linkify_packagename(changelog, sourcepkgnametxt)
+        changelog = self._linkify_bug_numbers(changelog)
+        return changelog
 
