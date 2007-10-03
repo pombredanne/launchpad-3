ca55352bba88d0ccb0a3e8723e24197a641a6318
Michael Hudson <michael.hudson@canonical.com>
2007-10-03 17:46:23 +0100

    make all the attributes of codeimportjob read-only

--- lib/canonical/launchpad/interfaces/codeimportjob.py
+++ lib/canonical/launchpad/interfaces/codeimportjob.py
@@ -46,6 +46,10 @@ class ICodeImportJob(Interface):
     There is always such a row for any active import, but it will not
     run until date_due is in the past."""
 
+    # Some of these attributes are not conceptually read-only but are
+    # set to be read-only here to force client code to use methods
+    # that update the audit trail appropriately.
+
     id = Int(required=True, readonly=True)
 
     date_created = Datetime(required=True, readonly=True)
@@ -60,38 +64,38 @@ class ICodeImportJob(Interface):
                       "where the job is currently running."))
 
     date_due = Datetime(
-        required=True, readonly=False,
+        required=True, readonly=True,
         description=_("When the import should happen."))
 
     state = Choice(
-        vocabulary=CodeImportJobState, required=True, readonly=False,
+        vocabulary=CodeImportJobState, required=True, readonly=True,
         description=_("The current state of the job."))
 
     requesting_user = Object(
-        schema=IPerson, required=False, readonly=False,
+        schema=IPerson, required=False, readonly=True,
         description=_("The user who requested the import, if any."))
 
     ordering = Int(
-        required=False, readonly=False,
+        required=False, readonly=True,
         description=_("A measure of how urgent the job is -- queue entries "
                       "with lower 'ordering' should be processed first, or "
                       "in other words 'ORDER BY ordering' returns the most "
                       "import jobs first."))
 
     heartbeat = Datetime(
-        required=False, readonly=False,
+        required=False, readonly=True,
         description=_("While the job is running, this field should be "
                       "updated frequently to indicate that the import job "
                       "hasn't crashed."))
 
     logtail = Text(
-        required=False, readonly=False,
+        required=False, readonly=True,
         description=_("The last few lines of output produced by the running "
                       "job. It should be updated at the same time as the "
                       "heartbeat."))
 
     date_started = Datetime(
-        required=False, readonly=False,
+        required=False, readonly=True,
         description=_("When the import began to be processed."))
 
 
