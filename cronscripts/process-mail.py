6730a65c1226be9950366e04a5c18e046a1e16f9
Francis J. Lacoste <francis.lacoste@contre.com>
2007-10-05 16:53:08 -0400

    Make process-mail.py dbuser configurable.

--- cronscripts/process-mail.py
+++ cronscripts/process-mail.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python2.4
-# Copyright 2004 Canonical Ltd.  All rights reserved.
+# Copyright 2004-2007 Canonical Ltd.  All rights reserved.
 """Fetches mail from the mail box and feeds them to the handlers."""
 
 import _pythonpath
 
 from zope.component.exceptions import ComponentLookupError
 
+from canonical.config import config
 from canonical.launchpad.scripts.base import (
     LaunchpadCronScript, LaunchpadScriptFailure)
 from canonical.launchpad.mail.incoming import handleMail
 from canonical.launchpad.interfaces import IMailBox
+
 
 class ProcessMail(LaunchpadCronScript):
     usage = """%prog [options]
@@ -27,6 +29,5 @@ class ProcessMail(LaunchpadCronScript):
 
 
 if __name__ == '__main__':
-    script = ProcessMail('process-mail')
+    script = ProcessMail('process-mail', dbuser=config.processmail.dbuser)
     script.lock_and_run(use_web_security=True)
-
