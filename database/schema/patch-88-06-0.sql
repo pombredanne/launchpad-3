b933d0bcea7f42c5d5fd634d3a026442125ee82d
Stuart Bishop <stuart@stuartbishop.net>
2007-10-11 11:16:32 +0700

    Approve Milestone.description

--- database/schema/patch-88-06-0.sql
+++ database/schema/patch-88-06-0.sql
@@ -2,4 +2,4 @@ SET client_min_messages=ERROR;
 
 ALTER TABLE Milestone ADD COLUMN description text;
 
-INSERT INTO LaunchpadDatabaseRevision VALUES (87, 98, 8);
+INSERT INTO LaunchpadDatabaseRevision VALUES (88, 06, 0);
