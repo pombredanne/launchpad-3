9ceb0ca31ff1f907c44c0934eb13fb52bdd86293
Elliot Murphy <elliot@canonical.com>
2007-10-11 21:01:11 -0400

    Handle bug searches. Reorder logic based more closely on LaunchpadRootNavigation. Add missing imports.

--- lib/canonical/launchpad/browser/feeds.py
+++ lib/canonical/launchpad/browser/feeds.py
@@ -7,12 +7,17 @@ __all__ = [
     ]
 
 from zope.component import getUtility
-from zope.publisher.interfaces import NotFound
 
 from canonical.launchpad.interfaces import (
-    IFeedsApplication, IPillarNameSet, NotFoundError)
+    IFeedsApplication,
+    IMaloneApplication,
+    IPersonSet,
+    IPillarNameSet,
+    NotFoundError,
+    )
 from canonical.launchpad.layers import FeedsLayer
-from canonical.launchpad.webapp import Navigation
+from canonical.launchpad.webapp import (
+    canonical_url, Navigation)
 
 class FeedsNavigation(Navigation):
 
@@ -20,21 +25,27 @@ class FeedsNavigation(Navigation):
 
     newlayer = FeedsLayer
 
+    stepto_utilities = {
+        'bugs': IMaloneApplication,
+        }
+
     def traverse(self, name):
+        # XXX: statik 2007-10-09 bug=150941
+        # Need to block pages not registered on the FeedsLayer
+
+        if name in self.stepto_utilities:
+            return getUtility(self.stepto_utilities[name])
+
+        if name.startswith('~'):
+            # redirect to the lower() version before doing the lookup
+            if name.lower() != name:
+                return self.redirectSubTree(
+                    canonical_url(self.context) + name.lower(), status=301)
+            else:
+                person = getUtility(IPersonSet).getByName(name[1:])
+                return person
+
         try:
-            if name.startswith('~'):
-                # redirect to the lower() version before doing the lookup
-                if name.lower() != name:
-                    return self.redirectSubTree(
-                        canonical_url(self.context) + name.lower(), status=301)
-                else:
-                    personset = getUtility(IPersonSet)
-                    person = personset.getByName(name[1:])
-                    return person
-
-            # XXX: statik 2007-10-09 bug=150941
-            # Need to block pages not registered on the FeedsLayer
-
             # redirect to the lower() version before doing the lookup
             if name.lower() != name:
                 return self.redirectSubTree(
