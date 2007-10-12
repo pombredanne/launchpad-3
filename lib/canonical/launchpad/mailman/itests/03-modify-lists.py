73384fc5ecdaabd570938fe91410fcf6ca389082
Barry Warsaw <barry@canonical.com>
2007-10-12 10:43:11 -0400

    Adding files I missed in the last commit.

---
+++ lib/canonical/launchpad/mailman/itests/03-modify-lists.py
@@ -0,0 +1,29 @@
+# Copyright 2007 Canonical Ltd.  All rights reserved.
+
+"""Launchpad-Mailman integration test #3
+
+Check that Mailman properly modifies a list's welcome message.
+"""
+
+import xmlrpclib
+import itest_helper
+
+
+def compare_message(greeting):
+    def poll_function():
+        stdout = itest_helper.run_mailman(
+            './withlist', '-q', '-r', 'mmhelper.welcome', 'team-one')
+        return stdout.splitlines()[0] == greeting
+    return poll_function
+
+
+def main():
+    """End-to-end testing of mailing list modification."""
+    # This test can't currently be done through the web.
+    proxy = xmlrpclib.ServerProxy(itest_helper.XMLRPC_URL)
+    proxy.testStep('03-modify-lists-A')
+    # Now wait a little while for Mailman to modify the mailing list.
+    itest_helper.poll_mailman(compare_message('Greetings team one members!'))
+    # Set a new welcome message and poll until Mailman has done its work.
+    proxy.testStep('03-modify-lists-B')
+    itest_helper.poll_mailman(compare_message('Saluations team one members!'))
---
+++ lib/canonical/launchpad/mailman/itests/itest_helper.py
@@ -0,0 +1,71 @@
+# Copyright 2007 Canonical Ltd.  All rights reserved.
+
+"""Various helper functions and constants for use in test scripts."""
+
+import os
+import sys
+import base64
+import datetime
+
+from subprocess import Popen, PIPE
+
+
+HERE = os.path.abspath(os.path.dirname(sys.argv[0]))
+TOP = os.path.normpath(os.path.join(HERE, '../../../..'))
+MAILMAN_BIN = os.path.normpath(os.path.join(
+    os.path.dirname(sys.argv[0]), '../../../../', 'mailman', 'bin'))
+
+XMLRPC_URL = 'http://xmlrpc.launchpad.dev:8087/mailinglists'
+
+
+class IntegrationTestFailure(Exception):
+    """An integration test failed."""
+
+
+def auth(user, password):
+    """Create a Base64 encoded Basic Auth string."""
+    return 'Basic ' + base64.encodestring('%s:%s' % (user, password))
+
+
+def poll_mailman(function):
+    """Standard loop for checking something for a while."""
+    # Now wait a little while for Mailman to do some operation.  Using the
+    # default Mailman polling frequency, the list should get created in under
+    # 20 seconds.
+    until = datetime.datetime.now() + datetime.timedelta(seconds=20)
+    while datetime.datetime.now() < until:
+        if function():
+            return
+    raise IntegrationTestFailure('Timed out before success')
+
+
+def run_mailman(*args):
+    """Run a Mailman script."""
+    proc = Popen(args, stdout=PIPE, stderr=PIPE, cwd=MAILMAN_BIN)
+    stdout, stderr = proc.communicate()
+    if stderr:
+        raise IntegrationTestFailure(stderr)
+    return stdout
+
+
+transactionmgr = None
+def create_transaction_manager():
+    """Create the global transaction manager for this side of the tests."""
+    global transactionmgr
+    # Import this here because our paths are not set up correctly in the
+    # global module scope.
+    from canonical.lp import initZopeless
+    # Set up the connection to the database.  We use the 'testadmin' uses
+    # because it has rights to do nasty things like delete Person entries.
+    transactionmgr = initZopeless(dbuser='testadmin')
+
+
+def make_browser():
+    # Import this here because our paths are not set up correctly in the
+    # global module scope.  This is like the setupBrowser for page tests, but
+    # with the base64 hack needed for authentication from the outside.
+    from zope.testbrowser.browser import Browser
+    browser = Browser()
+    browser.handleErrors = False
+    browser.addHeader('Authorization', auth('no-priv@canonical.com', 'test'))
+    return browser
