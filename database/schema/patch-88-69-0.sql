e75fedbca24852990af31a4d4700dd0e7b60a710
Francis J. Lacoste <francis.lacoste@contre.com>
2007-10-22 17:06:02 -0400

    Add DB patch setting the constraint.

---
+++ database/schema/patch-88-69-0.sql
@@ -0,0 +1,6 @@
+SET client_min_messages=ERROR;
+
+-- All pre-existing rows should have been updated off-line.
+ALTER TABLE LibraryFileAlias ALTER COLUMN date_created SET NOT NULL;
+
+INSERT INTO LaunchpadDatabaseRevision VALUES (88, 69, 0);
