d0b1ef9776718c8cd95fcb414b59d079019b967a
Barry Warsaw <barry@canonical.com>
2007-10-24 16:10:32 -0400

    Remove the caching; it's premature optimization.

--- lib/canonical/launchpad/mailman/monkeypatches/lphandler.py
+++ lib/canonical/launchpad/mailman/monkeypatches/lphandler.py
@@ -3,18 +3,13 @@
 """A global pipeline handler for determining Launchpad membership."""
 
 
+import socket
 import datetime
 import xmlrpclib
 
 from Mailman import Errors
 from Mailman import mm_cfg
 from Mailman.Logging.Syslog import syslog
-
-# Keep a cache of known Launchpad members.  Check back with Launchpad once per
-# day per poster.
-_CACHE = {}
-_MEMBER = object()
-_NOT_A_MEMBER = object()
 
 
 
@@ -25,31 +20,21 @@ def process(mlist, msg, msgdata):
     # Ask Launchpad whether the sender is a Launchpad member.  If not, discard
     # the message with extreme prejudice, but log this.
     sender = msg.get_sender()
-    # Is this sender information in the cache and has the cache entry expired?
-    last_date, status = _CACHE.get(sender, (None, _NOT_A_MEMBER))
-    if last_date is not None and last_date == datetime.date.today():
-        # The cache entry is good, so discard the message if the status makes
-        # the sender not a member.  Otherwise return, letting the rest of the
-        # process continue.
-        if status is _NOT_A_MEMBER:
-            raise Errors.DiscardMessage
-        return
-    # The cache entry was missing or out of date, so check with Launchpad.
+    # Check with Launchpad about whether the sender is a member or not.  If we
+    # can't talk to Launchpad, I believe it's better to let the message get
+    # posted to the list than to discard or hold it.
+    is_member = True
     proxy = xmlrpclib.ServerProxy(mm_cfg.XMLRPC_URL)
     try:
-        if proxy.isRegisteredInLaunchpad(sender):
-            status =_MEMBER
-        else:
-            status = _NOT_A_MEMBER
-    except xmlrpclib.ProtocolError, error:
+        is_member = proxy.isRegisteredInLaunchpad(sender)
+    except (xmlrpclib.ProtocolError, socket.error), error:
         syslog('xmlrpc', 'Cannot talk to Launchpad:\n%s', error)
-        # If we can't talk to Launchpad, I believe it's better to let the
-        # message get posted to the list than to discard or hold it.
-        return
-    _CACHE[sender] = (datetime.date.today(), status)
-    if status is _MEMBER:
+    except xmlrpclib.Fault, error:
+        syslog('xmlrpc', 'Launchpad exception: %s', error)
+    # This handler can just return if the sender is a member of Launchpad.
+    if is_member:
         return
     # IncomingRunner already posts the Message-ID to the logs/vette for
-    # discarded messages.
+    # discarded messages, so we only need to add a little more detail here.
     syslog('vette', 'Sender is not a Launchpad member: %s', sender)
     raise Errors.DiscardMessage
