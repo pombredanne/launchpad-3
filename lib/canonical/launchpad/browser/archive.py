bb11a42981f08f368c2078502a910a4c8c4d94c3
Celso Providelo <celso.providelo@canonical.com>
2007-10-26 19:49:22 -0200

    Status selector UI for PPA package browser.

--- lib/canonical/launchpad/browser/archive.py
+++ lib/canonical/launchpad/browser/archive.py
@@ -25,8 +25,8 @@ from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 from canonical.launchpad import _
 from canonical.launchpad.browser.build import BuildRecordsView
 from canonical.launchpad.interfaces import (
-    ArchivePurpose, IArchive, IPPAActivateForm, IArchiveSet, IBuildSet, IHasBuildRecords,
-    NotFoundError)
+    ArchivePurpose, IArchive, IPPAActivateForm, IArchiveSet, IBuildSet,
+    IHasBuildRecords, NotFoundError, PackagePublishingStatus)
 from canonical.launchpad.webapp import (
     action, canonical_url, custom_widget, enabled_with_permission,
     stepthrough, ApplicationMenu, LaunchpadEditFormView, LaunchpadFormView,
@@ -103,16 +103,67 @@ class ArchiveView(LaunchpadView):
         setUpWidget(self, 'series',  field, IInputWidget)
         self.series_widget.extra = "onChange='updateSeries(this);'"
 
-        self.name_filter = self.request.get('name_filter', None)
+        self.name_filter = self.request.get('field.name_filter')
+        status_filter = self.request.get('field.status_filter', 'published')
+        self.setupStatusFilterWidget(status_filter)
+
         publishing = self.context.getPublishedSources(
-            name=self.name_filter)
+            name=self.name_filter,
+            status=self.selected_status_filter.value.collection)
+
         self.batchnav = BatchNavigator(publishing, self.request)
         self.search_results = self.batchnav.currentBatch()
+
+    def setupStatusFilterWidget(self, status_filter):
+        """Setup a customized publishing status select widget.
+
+        Receives the one of the established field values:
+
+        ('published', 'superseded', 'any').
+
+        Allow user to select between:
+
+         * Published:  PENDING and PUBLISHED records,
+         * Superseded: SUPERSEDED and DELETED records,
+         * Any Status
+
+        """
+        class StatusCollection:
+            def __init__(self, collection=None):
+                self.collection = collection
+
+        published_status = [PackagePublishingStatus.PENDING,
+                            PackagePublishingStatus.PUBLISHED]
+        superseded_status = [PackagePublishingStatus.SUPERSEDED,
+                             PackagePublishingStatus.DELETED]
+
+        status_terms = [
+            SimpleTerm(StatusCollection(published_status),
+                       'published','Published'),
+            SimpleTerm(StatusCollection(superseded_status),
+                       'superseded','Superseded'),
+            SimpleTerm(StatusCollection(), 'any', 'Any Status')
+            ]
+        status_vocabulary = SimpleVocabulary(status_terms)
+
+        self.selected_status_filter = status_vocabulary.getTermByToken(
+            status_filter)
+
+        field = Choice(
+            __name__='status_filter', title=_("Status Filter"),
+            vocabulary=status_vocabulary, required=True)
+        setUpWidget(self, 'status_filter',  field, IInputWidget)
 
     @property
     def plain_series_widget(self):
         """Render a <select> control with no <div>s around it."""
         return self.series_widget.renderValue(None)
+
+    @property
+    def plain_status_filter_widget(self):
+        """Render a <select> control with no <div>s around it."""
+        return self.status_filter_widget.renderValue(
+            self.selected_status_filter.value)
 
     @property
     def sources_in_more_than_one_series(self):
