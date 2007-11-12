cf48d71754ed75b265e7c23d81ea37ffc4bb901c
Francis J. Lacoste <francis.lacoste@contre.com>
2007-11-12 15:39:18 -0500

    Rename to official patch number.

--- database/schema/patch-88-15-0.sql
+++ database/schema/patch-88-15-0.sql
@@ -3,4 +3,4 @@ SET client_min_messages=ERROR;
 -- All pre-existing rows should have been updated off-line.
 ALTER TABLE LibraryFileAlias ALTER COLUMN date_created SET NOT NULL;
 
-INSERT INTO LaunchpadDatabaseRevision VALUES (88, 69, 0);
+INSERT INTO LaunchpadDatabaseRevision VALUES (88, 15, 0);
