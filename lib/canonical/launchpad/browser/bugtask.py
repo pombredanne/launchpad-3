ab0ed1f49f64bd1ab5d2015125205a046fc0a8ae
Jonathan Knowles <jonathan.knowles@canonical.com>
2007-11-12 15:59:51 +0000

    Adding the skeleton 'BugTaskTextView' class: a view for a simple text page displaying information about a bug task.

--- lib/canonical/launchpad/browser/bugtask.py
+++ lib/canonical/launchpad/browser/bugtask.py
@@ -21,6 +21,7 @@ __all__ = [
     'BugTaskTableRowView',
     'BugTargetView',
     'BugTasksAndNominationsView',
+    'BugTaskTextView',
     'BugTaskView',
     'get_sortorder_from_request',
     'get_buglisting_search_filter_url',
@@ -80,7 +81,7 @@ from canonical.launchpad import helpers
 
 from canonical.launchpad.event.sqlobjectevent import SQLObjectModifiedEvent
 
-from canonical.launchpad.browser.bug import BugContextMenu
+from canonical.launchpad.browser.bug import BugContextMenu, BugTextView
 from canonical.launchpad.browser.bugcomment import build_comments_from_chunks
 from canonical.launchpad.browser.mentoringoffer import CanBeMentoredView
 from canonical.launchpad.browser.launchpad import StructuralObjectPresentation
@@ -340,6 +341,11 @@ class BugTaskSetNavigation(GetitemNavigation):
 class BugTaskContextMenu(BugContextMenu):
     """Context menu of actions that can be performed upon an `IBugTask`."""
     usedfor = IBugTask
+
+
+class BugTaskTextView(BugTextView):
+    """View for a simple text page displaying information about a bug task."""
+    pass
 
 
 class BugTaskView(LaunchpadView, CanBeMentoredView):
