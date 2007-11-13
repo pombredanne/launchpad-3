ce63c6a6eebd1cbed32ffea01807c7132f27d6bb
Tim Penhey <tim.penhey@canonical.com>
2007-11-14 10:21:04 +1300

    Patch renamed and slightly rearranged to avoid duplicate work.

--- database/schema/patch-88-21-0.sql
+++ database/schema/patch-88-21-0.sql
@@ -9,17 +9,18 @@ date_created of the tip revision or the date the branch was created.
 */
 
 UPDATE Branch
-SET date_last_modified = date_created;
-
-UPDATE Branch
 SET date_last_modified = Revision.date_created
 FROM Revision
 WHERE Branch.last_scanned_id = Revision.revision_id
 AND Revision.date_created > Branch.date_last_modified;
 
+UPDATE Branch
+SET date_last_modified = date_created
+WHERE date_last_modified IS NULL;
+
 ALTER TABLE Branch
   ALTER COLUMN date_last_modified SET NOT NULL,
-  ALTER COLUMN date_last_modified SET DEFAULT timezone('UTC'::text, now());
+  ALTER COLUMN date_last_modified SET DEFAULT
+    CURRENT_TIMESTAMP AT TIME ZONE 'UTC');
 
-INSERT INTO LaunchpadDatabaseRevision VALUES (87, 93, 0);
-
+INSERT INTO LaunchpadDatabaseRevision VALUES (88, 21, 0);
