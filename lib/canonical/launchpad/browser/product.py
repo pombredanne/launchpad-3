146291876f6685828ba2f93f5923af44df44fc25
Edwin Grubbs <edwin.grubbs@canonical.com>
2007-11-14 18:03:02 -0600

    Added test to verify that an email is sent when OTHER_OPEN_SOURCE is selected as the license. Added the user name to the Reply-To header.

--- lib/canonical/launchpad/browser/product.py
+++ lib/canonical/launchpad/browser/product.py
@@ -200,10 +200,12 @@ class ProductLicenseMixin:
                 license_titles=indent(license_titles),
                 license_info=indent(self.product.license_info))
 
+            reply_to = format_address(user.displayname, 
+                                      user.preferredemail.email)
             simple_sendmail(fromaddress,
                             'feedback@launchpad.net',
                             subject, message,
-                            headers={'Reply-To': user.preferredemail.email})
+                            headers={'Reply-To': reply_to})
 
             self.request.response.addInfoNotification(_(
                 "Launchpad is free to use for software under approved "
