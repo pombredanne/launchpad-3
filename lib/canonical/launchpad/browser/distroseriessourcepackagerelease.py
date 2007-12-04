520ce01603244559e479bf12420c7ed4c44ad465
Christian Robottom Reis <kiko@gasolinux>
2007-12-04 10:54:27 -0200

    First cut of fix for bug #130158, Launchpad should provide dgettable URLs. Implements a virtual +files hierarchy under IDistroSeriesSourcePackageRelease that redirects to the appropriate librarian URLs, and updates the package pages to use it.

--- lib/canonical/launchpad/browser/distroseriessourcepackagerelease.py
+++ lib/canonical/launchpad/browser/distroseriessourcepackagerelease.py
@@ -8,14 +8,13 @@ __all__ = [
     'DistroSeriesSourcePackageReleaseView',
     ]
 
-from zope.component import getUtility
-
 from canonical.launchpad.interfaces import (
-    IDistroSeriesSourcePackageRelease, ILaunchBag)
+    IDistroSeriesSourcePackageRelease)
 
 
 from canonical.launchpad.webapp import (
-    StandardLaunchpadFacets, Link, ContextMenu, ApplicationMenu, Navigation)
+    StandardLaunchpadFacets, ApplicationMenu, Navigation, stepthrough)
+from canonical.launchpad.webapp.publisher import RedirectionView
 
 
 class DistroSeriesSourcePackageReleaseFacets(StandardLaunchpadFacets):
@@ -37,9 +36,24 @@ class DistroSeriesSourcePackageReleaseNavigation(Navigation):
     usedfor = IDistroSeriesSourcePackageRelease
 
 
+    @stepthrough('+files')
+    def traverse_files(self, name):
+        # If you are like me you'll ask yourself how it can be that we're
+        # putting this traversal on IDistroSeriesSourcePackageRelease and
+        # using it with sourcepackagerelease-files.pt. The reason is
+        # that the canonical_url for SourcePackageRelease is actually an
+        # IDistroSeriesSourcePackageRelease page. Weird.
+        for file in self.context.files:
+            if file.libraryfile.filename == name:
+                return RedirectionView(
+                    file.libraryfile.http_url, self.request, 301)
+        return None
+
+
 class DistroSeriesSourcePackageReleaseView:
 
     def __init__(self, context, request):
         self.context = context
         self.request = request
+
 
