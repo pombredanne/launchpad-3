062b696ea37c2804514a71a044e4e6c1d921929d
Graham Binns <graham.binns@canonical.com>
2007-12-05 16:25:29 +0000

    More test addiness.

--- lib/canonical/launchpad/scripts/ftests/test_checkwatches.py
+++ lib/canonical/launchpad/scripts/ftests/test_checkwatches.py
@@ -7,12 +7,25 @@ __all__ = []
 
 from unittest import TestLoader
 
+from zope.component import getUtility
+
 from canonical.config import config
 from canonical.database.sqlbase import commit
 from canonical.launchpad.ftests.harness import LaunchpadZopelessTestCase
+from canonical.launchpad.interfaces import IBugSet
 
 class TestCheckwatches(LaunchpadZopelessTestCase):
     """Tests for the bugwatch updating system."""
+
+    dbuser = config.malone.bugnotification_dbuser
+
+    def setUp(self):
+        """Set up bugs, watches and questions to test with."""
+        super(TestCheckwatches, self).setUp()
+
+        self.bug = getUtility(IBugSet).get(10)
+        self.bugwatch = self.bug
+
 
 def test_suite():
     return TestLoader().loadTestsFromName(__name__)
