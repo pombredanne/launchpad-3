6b44c2649e4b836fc8dde19984f74513ad6eac19
Brad Crittenden <bac@canonical.com>
2007-12-05 09:49:57 -0500

    Add encoding if the filename is 'txt.gz' to accomodate in-browser decompression of Ubuntu build logs

--- lib/canonical/librarian/web.py
+++ lib/canonical/librarian/web.py
@@ -101,7 +101,35 @@ class LibraryFileAliasResource(resource.Resource):
         if dbfilename.encode('utf-8') != filename:
             return fourOhFour
         if self.storage.hasFile(dbcontentID) or self.upstreamHost is None:
+            # This fix is in response to Bug 173096.  The Ubuntu team wants their
+            # log files to be automatically unzipped.  Previously this was done by
+            # having Apache add an encoding for all content that was .gz or .tgz.
+            # Doing so violates the intent of the Content-Encoding header and
+            # caused other gzipped files served from the Librarian to be treated
+            # incorrectly by browsers.  The fix shown here is to still support the
+            # encoding of Ubuntu log files while allowing others to pass with no
+            # encoding.  Apache will be changed to remove the Content-Encoding
+            # header for gzip.
+            if filename.endswith(".txt.gz"):
+                # We want to set the encoding to a variation of gzip.  We must
+                # see what if the browser has advertised an expected encoding.
+                accept_encoding_header = request.getHeader('accept-encoding')
+                if accept_encoding_header is not None:
+                    accept_encodings = accept_encoding_header.split(',')
+                else:
+                    accept_encodings = []
+                # Set the default encoding to "gzip" in case one is not
+                # advertised.  Otherwise, take the first one that looks good.
+                encoding = "gzip"
+                for enc in accept_encodings:
+                    if 'gzip' in enc:
+                        encoding = enc
+                        break
+                mimetype = "text/plain"
+            else:
+                encoding = None
             return File(mimetype.encode('ascii'),
+                        encoding,
                         self.storage._fileLocation(dbcontentID))
         else:
             return proxy.ReverseProxyResource(self.upstreamHost,
@@ -113,10 +141,10 @@ class LibraryFileAliasResource(resource.Resource):
 
 class File(static.File):
     isLeaf = True
-    def __init__(self, contentType, *args, **kwargs):
+    def __init__(self, contentType, encoding=None, *args, **kwargs):
         static.File.__init__(self, *args, **kwargs)
         self.type = contentType
-        self.encoding = None
+        self.encoding = encoding
 
 
 class DigestSearchResource(resource.Resource):
@@ -163,4 +191,3 @@ Disallow: /
 def _eb(failure, request):
     """Generic errback for failures during a render_GET."""
     request.processingFailed(failure)
-
