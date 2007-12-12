fa08ed18f7d98307fc4633256c7caf8fcd41a201
Michael Hudson <michael.hudson@canonical.com>
2007-12-12 19:33:21 +0000

    oops, add the files!

---
+++ lib/canonical/launchpad/xmlrpc/codeimportscheduler.py
@@ -0,0 +1,27 @@
+# Copyright 2007 Canonical Ltd.  All rights reserved.
+
+"""The code import scheduler XML-RPC API."""
+
+__metaclass__ = type
+__all__ = [
+    'CodeImportSchedulerAPI',
+    ]
+
+
+from canonical.launchpad.webapp import LaunchpadXMLRPCView
+from canonical.launchpad.interfaces import ICodeImportScheduler
+
+from zope.interface import implements
+
+
+class CodeImportSchedulerAPI(LaunchpadXMLRPCView):
+    """See `ICodeImportScheduler`."""
+
+    implements(ICodeImportScheduler)
+
+    def getJobForMachine(self, hostname):
+        """See `ICodeImportScheduler`.
+
+        Currently hard coded to return 4 until more of the code import
+        machinery is implemented."""
+        return 4
