406a4dfe3f18757aa6328d5d2680abfe3488a921
Mark Shuttleworth <mark@canonical.com>
2007-12-19 19:29:16 +0000

    Rename db patch for [dbar=stub]

--- database/schema/patch-88-42-0.sql
+++ database/schema/patch-88-42-0.sql
@@ -4,4 +4,4 @@ ALTER TABLE Project ADD COLUMN reviewer_whiteboard text;
 ALTER TABLE Product ADD COLUMN reviewer_whiteboard text;
 ALTER TABLE Distribution ADD COLUMN reviewer_whiteboard text;
 
-INSERT INTO LaunchpadDatabaseRevision VALUES (88, 57, 0);
+INSERT INTO LaunchpadDatabaseRevision VALUES (88, 42, 0);
