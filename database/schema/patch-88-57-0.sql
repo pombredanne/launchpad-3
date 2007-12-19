cf79abf982672aacb156dde06e96fcf88071d804
Mark Shuttleworth <mark@canonical.com>
2007-12-19 16:56:06 +0000

    Add reviewer whiteboards for pillars

---
+++ database/schema/patch-88-57-0.sql
@@ -0,0 +1,7 @@
+SET client_min_messages=ERROR;
+
+ALTER TABLE Project ADD COLUMN reviewer_whiteboard text;
+ALTER TABLE Product ADD COLUMN reviewer_whiteboard text;
+ALTER TABLE Distribution ADD COLUMN reviewer_whiteboard text;
+
+INSERT INTO LaunchpadDatabaseRevision VALUES (88, 57, 0);
