0a8cff69f46c0ffb53ffb6fe157a29cb1e897242
Leonard Richardson <leonard.richardson@canonical.com>
2008-01-04 15:03:37 -0500

    Added an 'api' vhost that responds to all major HTTP methods.

--- lib/canonical/launchpad/webapp/servers.py
+++ lib/canonical/launchpad/webapp/servers.py
@@ -177,7 +177,7 @@ class VirtualHostRequestPublicationFactory:
         self.request_factory = request_factory
         self.publication_factory = publication_factory
         self.port = port
-        if methods  is None:
+        if methods is None:
             methods = ['GET', 'HEAD', 'POST']
         self.methods = methods
         self.handle_default_host = handle_default_host
@@ -321,6 +321,51 @@ class XMLRPCRequestPublicationFactory(VirtualHostRequestPublicationFactory):
                 # 415 - Unsupported Media Type
                 publication_factory = ProtocolErrorPublicationFactory(415)
         return request_factory, publication_factory
+
+
+class WebServiceRequestPublicationFactory(VirtualHostRequestPublicationFactory):
+    """A VirtualHostRequestPublicationFactory for requests against
+    resources published through a web service.
+    """
+
+    def __init__(self, vhost_name, request_factory, publication_factory,
+                 port=None):
+        """This factory accepts requests that use all five major HTTP methods.
+        """
+        super(WebServiceRequestPublicationFactory, self).__init__(
+            vhost_name, request_factory, publication_factory, port,
+            ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'])
+
+    def checkRequest(self, environment):
+        """See `VirtualHostRequestPublicationFactory`.
+
+        Accept only requests where the MIME type is application/json
+        """
+        request_factory, publication_factory = (
+            super(WebServiceRequestPublicationFactory, self).checkRequest(
+                environment))
+        if request_factory is None:
+            mime_type = environment.get('CONTENT_TYPE')
+            method = environment.get('REQUEST_METHOD')
+            if (method not in ['GET', 'HEAD', 'DELETE', 'OPTIONS'] and
+                mime_type != 'application/json'):
+                request_factory = ProtocolErrorRequest
+                # 415 - Unsupported Media Type
+                publication_factory = ProtocolErrorPublicationFactory(415)
+        return request_factory, publication_factory
+
+
+    def canHandle(self, environment):
+        """See `IRequestPublicationFactory`.
+
+        XXX: This factory only accepts calls if the web service is
+        exposed. Once we launch the web service this method will be
+        removed.-- Leonard Richardson 2008-01-04
+        (https://launchpad.net/launchpad/+spec/api-bugs-remote)
+        """
+        result = super(WebServiceRequestPublicationFactory, self).canHandle(
+            environment)
+        return result and config.webservice.exposed
 
 
 class NotFoundRequestPublicationFactory:
@@ -826,6 +871,14 @@ class FeedsBrowserRequest(LaunchpadBrowserRequest):
     """Request type for a launchpad feed."""
     implements(canonical.launchpad.layers.FeedsLayer)
 
+# ---- web service
+
+class WebServicePublication(LaunchpadBrowserPublication):
+    """The publication used for Launchpad feed requests."""
+
+class WebServiceClientRequest(LaunchpadBrowserRequest):
+    """Request type for a resource published through the web service."""
+
 # ---- openid
 
 class OpenIdPublication(LaunchpadBrowserPublication):
@@ -988,6 +1041,8 @@ def register_launchpad_request_publication_factories():
         VHRP('shipitedubuntu', EdubuntuShipItBrowserRequest,
              ShipItPublication),
         VHRP('feeds', FeedsBrowserRequest, FeedsPublication),
+        WebServiceRequestPublicationFactory('api', WebServiceClientRequest,
+                                            WebServicePublication),
         XMLRPCRequestPublicationFactory('xmlrpc', PublicXMLRPCRequest,
                                         PublicXMLRPCPublication)
         ]
