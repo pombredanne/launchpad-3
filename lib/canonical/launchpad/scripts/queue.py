8f993e28eb02cab75d38c3da848a0fcd7498bc6b
Julian Edwards <julian.edwards@canonical.com>
2008-01-07 12:47:15 +0000

    Add/fix tests.

--- lib/canonical/launchpad/scripts/queue.py
+++ lib/canonical/launchpad/scripts/queue.py
@@ -30,6 +30,8 @@ from canonical.launchpad.interfaces import (
 
 from canonical.cachedproperty import cachedproperty
 from canonical.config import config
+from canonical.launchpad.scripts.processaccepted import (
+    close_bugs_for_queue_item)
 from canonical.launchpad.webapp.tales import DurationFormatterAPI
 from canonical.librarian.utils import filechunks
 
@@ -480,8 +482,24 @@ class QueueActionAccept(QueueAction):
                     announce_list=self.announcelist, logger=self.log,
                     dry_run=self.no_mail)
             except QueueInconsistentStateError, info:
-                self.display('** %s could not be accepted due %s'
+                self.display('** %s could not be accepted due to %s'
                              % (queue_item.displayname, info))
+                continue
+
+            # If this is a single source upload we can create the
+            # publishing records now so that the user doesn't have to
+            # wait for a publisher cycle (which calls process-accepted
+            # to do this).
+            if ((queue_item.sources.count() == 1) and
+                (queue_item.builds.count() == 0) and
+                (queue_item.customfiles.count() == 0)):
+                self.display("Creating PENDING publishing record for "
+                    "%s." % queue_item.displayname)
+                queue_item.realiseUpload()
+
+            # When accepting packages, we must also check the changes file
+            # for bugs to close automatically.
+            close_bugs_for_queue_item(queue_item)
 
         self.displayRule()
         self.displayBottom()
@@ -490,7 +508,8 @@ class QueueActionAccept(QueueAction):
 class QueueActionOverride(QueueAction):
     """Override information in a queue item content.
 
-    queue override [-c|--component] [-x|--section] [-p|--priority] <override_stanza> <filter>
+    queue override [-c|--component] [-x|--section] [-p|--priority]
+        <override_stanza> <filter>
 
     Where override_stanza is one of:
     source
