691d35b74be97b037c6685107d5fe5ee9169e701
Tom Berger <tom.berger@gmail.com>
2008-01-08 20:18:57 +0000

    add a comment explaining the optimization

--- lib/canonical/launchpad/browser/specificationtarget.py
+++ lib/canonical/launchpad/browser/specificationtarget.py
@@ -362,6 +362,9 @@ class HasSpecificationsView(LaunchpadView):
             for spec in specs:
                 if spec in plan:
                     continue
+                # Check whether the spec has
+                # any dependencies by looking
+                # at the cached dependencies list
                 spec_has_dependencies = False
                 for spec_id, dep_id in dependencies:
                     if spec_id == spec.id:
