216043d8fd43e73ac279514702b166b1f7759c6b
Celso Providelo <celso.providelo@canonical.com>
2008-01-10 09:05:07 -0200

    s/visible/active as per Mark's bug comment, see further information on bug 180236.

--- database/schema/patch-88-99-0.sql
+++ database/schema/patch-88-99-0.sql
@@ -1,11 +1,11 @@
 SET client_min_messages=ERROR;
 
 -- Add a new column as per bug #180236 request.
-ALTER TABLE Builder ADD COLUMN visible boolean;
-ALTER TABLE Builder ALTER COLUMN visible SET DEFAULT true;
+ALTER TABLE Builder ADD COLUMN active boolean;
+ALTER TABLE Builder ALTER COLUMN active SET DEFAULT true;
 
--- Make all builders 'visible'.
-UPDATE Builder set visible=true;
+-- Make all builders 'active'.
+UPDATE Builder set active=true;
 
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (88, 99, 0);
