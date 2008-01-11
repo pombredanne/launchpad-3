05abf387c2266b1eaced19af38b6925fa3361a4e
Jeroen Vermeulen <jeroen.vermeulen@canonical.com>
2008-01-11 21:08:34 +0700

    Assigned patch number: 88-50-0.

--- database/schema/patch-88-50-0.sql
+++ database/schema/patch-88-50-0.sql
@@ -4,5 +4,5 @@ UPDATE POTemplate SET header='' WHERE header IS NULL;
 
 ALTER TABLE POTemplate ALTER header SET NOT NULL;
 
-INSERT INTO LaunchpadDatabaseRevision VALUES (88, 99, 0);
+INSERT INTO LaunchpadDatabaseRevision VALUES (88, 50, 0);
 
