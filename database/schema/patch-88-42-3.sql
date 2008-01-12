06cdd147ee6f1ea478405f9355df5188ea6bfb57
Stuart Bishop <stuart@stuartbishop.net>
2008-01-12 21:32:48 +0700

    Renumber patch-88-49-0.sql as it has already been applied to production

--- database/schema/patch-88-42-3.sql
+++ database/schema/patch-88-42-3.sql
@@ -4,5 +4,5 @@ CREATE INDEX translationimportqueueentry__context__path__idx
     ON TranslationImportQueueEntry(
         distroseries, sourcepackagename, productseries, path);
 
-INSERT INTO LaunchpadDatabaseRevision VALUES (88, 49, 0);
+INSERT INTO LaunchpadDatabaseRevision VALUES (88, 42, 3);
 
