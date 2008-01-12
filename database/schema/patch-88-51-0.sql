536362f2cd4e4f0f6c43809c4dff7392f92168de
Stuart Bishop <stuart@stuartbishop.net>
2008-01-12 21:29:29 +0700

    Tweak Person.verbose_bugnotifications and assign db patch number

---
+++ database/schema/patch-88-51-0.sql
@@ -0,0 +1,6 @@
+SET client_min_messages=ERROR;
+
+-- Make NOT NULL DEFAULT=FALSE when fully populated
+ALTER TABLE Person ADD COLUMN verbose_bugnotifications boolean;
+
+INSERT INTO LaunchpadDatabaseRevision VALUES (88, 51, 0);
--- database/schema/patch-88-99-0.sql
+++
@@ -1,4 +0,0 @@
-SET client_min_messages=ERROR;
-
-ALTER TABLE Person ADD COLUMN bugnotification_include_description bool NOT NULL DEFAULT true;
-INSERT INTO LaunchpadDatabaseRevision VALUES (88, 99, 0);
