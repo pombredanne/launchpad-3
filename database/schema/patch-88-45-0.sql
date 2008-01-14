df9229a5961d870a06b3d5af1b6af6d16471601d
Julian Edwards <julian.edwards@canonical.com>
2008-01-14 12:36:56 +0000

    Rename patch file for correct version.

--- database/schema/patch-88-45-0.sql
+++ database/schema/patch-88-45-0.sql
@@ -3,4 +3,4 @@ SET client_min_messages=ERROR;
 ALTER TABLE SourcePackageRelease
     RENAME COLUMN changelog to changelog_entry;
 
-INSERT INTO LaunchpadDatabaseRevision VALUES (88, 99, 0);
+INSERT INTO LaunchpadDatabaseRevision VALUES (88, 45, 0);
