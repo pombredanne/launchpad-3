f19cce70150d09f468f015055836f7b7861eee7e
Aaron Bentley <aaron@canonical.com>
2008-01-22 13:39:19 +1100

    Update schema to only mark projects with hosted branches

--- database/schema/patch-88-99-0.sql
+++ database/schema/patch-88-99-0.sql
@@ -3,13 +3,14 @@ SET client_min_messages=ERROR;
 ALTER TABLE Product
     ADD COLUMN official_codehosting BOOLEAN;
 /* Set official_codhosting value to True if the development focus series of
-   the product has a user branch specified */
+   the product has a hosted branch specified */
 UPDATE Product
 SET official_codehosting = TRUE
 WHERE id IN (
-    SELECT product.id FROM product, productseries
+    SELECT product.id FROM product, productseries, branch
     WHERE product.development_focus = productseries.id
-    AND productseries.user_branch IS NOT NULL
+    AND productseries.user_branch = branch.id
+    AND branch.branch_type = 1
 );
 
 UPDATE Product
