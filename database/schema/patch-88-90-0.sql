dae01ecae5e59a4e8d967cb60d4e9b3ca60ff38d
Aaron Bentley <aaron@canonical.com>
2008-01-28 14:20:57 -0500

    Add support for BranchMergeProposalMessage to database schema

---
+++ database/schema/patch-88-90-0.sql
@@ -0,0 +1,25 @@
+SET client_min_messages=ERROR;
+
+BEGIN TRANSACTION;
+
+ALTER TABLE BranchMergeProposal ADD COLUMN conversation INTEGER;
+
+CREATE TABLE BranchMergeProposalSubscription (
+    id SERIAL PRIMARY KEY,
+    branch_merge_proposal INTEGER NOT NULL REFERENCES BranchMergeProposal(id),
+    person integer NOT NULL REFERENCES Person(id),
+    date_created TIMESTAMP WITHOUT TIME ZONE NOT NULL
+                 DEFAULT timezone('UTC'::text, now()),
+    registrant INTEGER REFERENCES Person(id)
+    );
+
+CREATE TABLE BranchMergeProposalMessage (
+    id SERIAL PRIMARY KEY,
+    branch_merge_proposal INTEGER NOT NULL REFERENCES BranchMergeProposal(id),
+    message INTEGER NOT NULL UNIQUE REFERENCES Message(id),
+    vote INTEGER NOT NULL
+    );
+
+INSERT INTO LaunchpadDatabaseRevision VALUES (88, 90, 0);
+
+COMMIT TRANSACTION;
