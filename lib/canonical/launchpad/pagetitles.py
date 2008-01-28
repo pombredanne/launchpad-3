ffc89f23055fd459352407fd758173e9de13715b
Barry Warsaw <barry.warsaw@canonical.com>
2008-01-28 12:51:43 -0500

    Add basic infrastructure for Launchpad administrative approval of outstanding mailing list requests.  This adds a pagetest, page title, traversal glue, root glue.

--- lib/canonical/launchpad/pagetitles.py
+++ lib/canonical/launchpad/pagetitles.py
@@ -698,6 +698,8 @@ logintoken_validatesignonlygpg = 'Confirm sign-only OpenPGP key'
 
 logintoken_validateteamemail = 'Confirm e-mail address'
 
+mailinglists_review = 'Pending mailing lists requests'
+
 # main_template has the code to insert one of these titles.
 
 malone_about = 'About Launchpad Bugs'
