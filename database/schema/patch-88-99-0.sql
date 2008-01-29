0050348c4da8ef3d4254d0a2cbf902e6686a48f8
Aaron Bentley <aaron@canonical.com>
2008-01-29 10:27:23 -0500

    Simplify database patch

--- database/schema/patch-88-99-0.sql
+++ database/schema/patch-88-99-0.sql
@@ -1,7 +1,7 @@
 SET client_min_messages=ERROR;
 
 ALTER TABLE Product
-    ADD COLUMN official_codehosting BOOLEAN;
+    ADD COLUMN official_codehosting BOOLEAN NOT NULL DEFAULT FALSE;
 
 /* Set official_codehosting value to True if the development focus series of
    the product has a hosted branch specified */
@@ -13,15 +13,5 @@ WHERE id IN (
     AND productseries.user_branch = branch.id
     AND branch.branch_type = 1
 );
-
-UPDATE Product
-SET official_codehosting = FALSE
-WHERE official_codehosting IS NULL;
-
-ALTER TABLE Product ALTER COLUMN official_codehosting
-    SET DEFAULT FALSE;
-
-ALTER TABLE Product ALTER COLUMN official_codehosting
-    SET NOT NULL;
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (88, 99, 0);
