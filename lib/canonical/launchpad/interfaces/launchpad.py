2433135404cabd0a780c195ecfe68b99ecab4a8b
Brad Crittenden <bac@canonical.com>
2008-01-29 16:08:42 -0500

    Added ignore of pylint W0611 for an interfaces file.

--- lib/canonical/launchpad/interfaces/launchpad.py
+++ lib/canonical/launchpad/interfaces/launchpad.py
@@ -1,5 +1,5 @@
 # Copyright 2004 Canonical Ltd.  All rights reserved.
-# pylint: disable-msg=E0211,E0213
+# pylint: disable-msg=E0211,E0213,W0611
 
 """Interfaces pertaining to the launchpad application.
 
