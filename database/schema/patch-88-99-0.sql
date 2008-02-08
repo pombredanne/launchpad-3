c7681576ecea6c86f10d76cc6e530f2d12d4455d
Celso Providelo <celso.providelo@canonical.com>
2008-02-08 10:17:06 -0200

    Addressing review comments.

--- database/schema/patch-88-99-0.sql
+++ database/schema/patch-88-99-0.sql
@@ -2,7 +2,7 @@ SET client_min_messages=ERROR;
 
 CREATE TABLE PackageDiff (
     id serial PRIMARY KEY,
-    date_created timestamp without time zone
+    date_requested timestamp without time zone
         DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC') NOT NULL,
     requester integer NOT NULL REFERENCES Person(id),
     from_source integer NOT NULL REFERENCES SourcePackageRelease(id),
