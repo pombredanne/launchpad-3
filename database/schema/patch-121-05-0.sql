8730a1d7313ff310268d101fceb2166426465122
Carlos Perello Marin <carlos.perello@canonical.com>
2008-02-11 18:35:57 +0100

    Stub approved it

--- database/schema/patch-121-05-0.sql
+++ database/schema/patch-121-05-0.sql
@@ -3,4 +3,4 @@ SET client_min_messages=ERROR;
 ALTER TABLE POFile DROP COLUMN exportfile;
 ALTER TABLE POFile DROP COLUMN exporttime;
 
-INSERT INTO LaunchpadDatabaseRevision VALUES (88, 99, 0);
+INSERT INTO LaunchpadDatabaseRevision VALUES (121, 05, 0);
