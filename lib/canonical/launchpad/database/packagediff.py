39e6bcd7daf8abccbb008d177eafd0c8bd23f610
Celso Providelo <celso.providelo@canonical.com>
2008-02-11 17:13:22 -0200

    Adding PackageDiff content classes and utilities.

---
+++ lib/canonical/launchpad/database/packagediff.py
@@ -0,0 +1,57 @@
+# Copyright 2004-2008 Canonical Ltd.  All rights reserved.
+
+__metaclass__ = type
+__all__ = [
+    'PackageDiff',
+    'PackageDiffSet',
+    ]
+
+from zope.interface import implements
+from sqlobject import ForeignKey
+
+from canonical.database.constants import UTC_NOW
+from canonical.database.datetimecol import UtcDateTimeCol
+from canonical.database.sqlbase import SQLBase
+from canonical.launchpad.interfaces import (
+    IHasOwner, IPackageDiff, IPackageDiffSet)
+
+
+class PackageDiff(SQLBase):
+    """A Queue item for Lucille."""
+    implements(IHasOwner, IPackageDiff)
+
+    _defaultOrder = ['id']
+
+    date_requested = UtcDateTimeCol(notNull=False, default=UTC_NOW)
+
+    requester = ForeignKey(
+        dbName='requester', foreignKey='Person', notNull=True)
+
+    from_source = ForeignKey(
+        dbName="from_source", foreignKey='SourcePackageRelease', notNull=True)
+
+    to_source = ForeignKey(
+        dbName="to_source", foreignKey='SourcePackageRelease', notNull=False)
+
+    date_fulfilled = UtcDateTimeCol(notNull=False)
+
+    diff_content = ForeignKey(
+        dbName="diff_content", foreignKey='LibraryFileAlias', notNull=False)
+
+    def performDiff(self):
+        """See `IPackageDiff`."""
+        pass
+
+class PackageDiffSet:
+    """This class is to deal with Distribution related stuff"""
+
+    implements(IPackageDiffSet)
+
+    def __iter__(self):
+        """Return all `PackageDiff`s sorted by date_requested."""
+        diffset = PackageDiff.select(orderBy=['-date_requested'])
+        return iter(diffset)
+
+    def get(self, diff_id):
+        """See `IPackageDiffSet`."""
+        return PackageDiff.get(diff_id)
