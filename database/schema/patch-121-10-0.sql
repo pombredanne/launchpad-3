4173cf52e23493ccc50eb3012f5260b40845eb3b
Stuart Bishop <stuart@stuartbishop.net>
2008-02-12 16:06:06 +0700

    Number and tweak BranchMergeProposal supersedition

--- database/schema/patch-121-10-0.sql
+++ database/schema/patch-121-10-0.sql
@@ -1,14 +1,14 @@
 SET client_min_messages=ERROR;
 
 ALTER TABLE BranchMergeProposal
-  ADD COLUMN superceded_proposal INT REFERENCES BranchMergeProposal;
+  ADD COLUMN superseded_by INT REFERENCES BranchMergeProposal;
 
 -- This constraint is no longer correct and a database level
 -- constraint isn't going to help now due to model changes.
 ALTER TABLE BranchMergeProposal
   DROP CONSTRAINT branchmergeproposal_source_branch_key;
 
-CREATE INDEX branchmergeproposal_superceded_proposal
-  ON BranchMergeProposal(superceded_proposal);
+CREATE INDEX branchmergeproposal__superseded_by__idx
+  ON BranchMergeProposal(superseded_by) WHERE superseded_by IS NOT NULL;
 
-INSERT INTO LaunchpadDatabaseRevision VALUES (88, 95, 0);
+INSERT INTO LaunchpadDatabaseRevision VALUES (121, 10, 0);
