aa0bd6e50b65d48487f9329e0a6d4a1b82a25288
Barry Warsaw <barry@canonical.com>
2008-02-14 13:24:55 -0500

    Changes in response to Francis's review:
    - Use LaunchpadCronScript
    - Use a unique dbuser
    - Refactor some methods
    - Do the entire rsync in one command
    - Other minor updates

--- scripts/mlist-sync.py
+++ scripts/mlist-sync.py
@@ -4,7 +4,7 @@
 """Sync Mailman data from one Launchpad to another."""
 
 # XXX BarryWarsaw 12-Feb-2008
-# Things this script does NOT do correctly
+# Things this script does NOT do correctly.
 #
 # - Fix up the deactivated lists.  This isn't done because that data lives in
 #   the backed up tar file, so handling this would mean untar'ing, tricking
@@ -20,8 +20,9 @@
 #   and update is quite painful in Python 2.4, but would be easier with Python
 #   2.5's new mailbox module.  /Then/ we'd have to regenerate the archives.
 #   Not doing this means that some of the links in staging's MHonArc archive
-#   will be broken.
+#   will point to production archives.
 
+import os
 import sys
 import logging
 import textwrap
@@ -34,14 +35,16 @@ from zope.component import getUtility
 
 from canonical.config import config
 from canonical.launchpad.interfaces import IEmailAddressSet, IMailingListSet
-from canonical.launchpad.scripts.base import LaunchpadScript
+from canonical.launchpad.scripts.base import LaunchpadCronScript
 
 
 RSYNC_OPTIONS = ('-avz', '--delete')
 RSYNC_COMMAND = '/usr/bin/rsync'
+RSYNC_SUBDIRECTORIES = ('archives', 'backups', 'lists', 'mhonarc')
+SPACE = ' '
 
 
-class MailingListSyncScript(LaunchpadScript):
+class MailingListSyncScript(LaunchpadCronScript):
     """
     %prog [options] source_url
 
@@ -59,42 +62,45 @@ class MailingListSyncScript(LaunchpadScript):
     loglevel = logging.INFO
     description = 'Sync the Mailman data structures with the database.'
 
-    def __init__(self):
+    def __init__(self, name, dbuser=None):
         self.usage = textwrap.dedent(self.__doc__)
-        super(MailingListSyncScript, self).__init__('scripts.mlist_sync')
+        super(MailingListSyncScript, self).__init__(name, dbuser)
 
-    def main(self):
-        """See `LaunchpadScript`."""
-        source_url = None
-        if len(self.args) == 0:
-            self.parser.error('Missing source_url')
-        elif len(self.args) > 1:
-            self.parser.error('Too many arguments')
-        else:
-            source_url = self.args[0]
+    def syncMailmanDirectories(self, source_url):
+        """Synchronize the Mailman directories.
 
+        :param source_url: the base url of the source
+        """
+        # This can't be done at module global scope.
+        from Mailman import mm_cfg
         # Start by rsync'ing over the entire $vardir/lists, $vardir/archives,
         # $vardir/backups, and $vardir/mhonarc directories.  We specifically
         # do not rsync the data, locks, logs, qfiles, or spam directories.
         destination_url = config.mailman.build.var_dir
+        # Do one rsync for all subdirectories.
+        rsync_command = [RSYNC_COMMAND]
+        rsync_command.extend(RSYNC_OPTIONS)
+        rsync_command.append('--exclude=%s' % mm_cfg.MAILMAN_SITE_LIST)
+        rsync_command.append('--exclude=%s.mbox' % mm_cfg.MAILMAN_SITE_LIST)
+        rsync_command.extend(os.path.join(source_url, subdirectory)
+                             for subdirectory in RSYNC_SUBDIRECTORIES)
+        rsync_command.append(destination_url)
+        self.logger.info('executing: %s', SPACE.join(rsync_command))
+        process = subprocess.Popen(rsync_command,
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+        if process.returncode == 0:
+            self.logger.info('%s', stdout)
+        else:
+            self.logger.error('rsync command failed with exit status: %s',
+                              process.returncode)
+            self.logger.error('STDOUT:\n%s\nSTDERR:\n%s', stdout, stderr)
+        return process.returncode
 
-        # Do the rsync's for each subdirectory.
-        for subdirectory in ('archives', 'backups', 'lists', 'mhonarc'):
-            rsync_command = [RSYNC_COMMAND]
-            rsync_command.extend(RSYNC_OPTIONS)
-            rsync_command.extend((source_url + '/' + subdirectory,
-                                  destination_url))
-
-            print 'rsyncing', source_url, '-->', destination_url
-            retcode = subprocess.call(rsync_command)
-            if retcode:
-                print >> sys.stderr, 'rsync failed!'
-                return retcode
-
-        # We need to get to the Mailman API.  Set up the paths so that Mailman
-        # can be imported.  This can't be done at module global scope.
-        mailman_path = config.mailman.build.prefix
-        sys.path.append(mailman_path)
+    def fixHostnames(self):
+        """Fix up the host names in Mailman and the LP database."""
+        # These can't be done at module global scope.
         from Mailman import Utils
         from Mailman import mm_cfg
         from Mailman.MailList import MailList
@@ -105,6 +111,10 @@ class MailingListSyncScript(LaunchpadScript):
 
         # Clean things up per mailing list.
         for list_name in Utils.list_names():
+            # Skip the site list.
+            if list_name == mm_cfg.MAILMAN_SITE_LIST:
+                continue
+
             # The first thing to clean up is the mailing list pickles.  There
             # are things like host names in some attributes that need to be
             # converted.  The following opens a locked list.
@@ -119,13 +129,39 @@ class MailingListSyncScript(LaunchpadScript):
 
             # Patch up the email address for the list in the Launchpad
             # database.
-            mailing_list = mailing_list_set.get(list_name)
-            email_address = email_address_set.getByEmail(mailing_list.address)
-            if mailing_list is None or email_address is None:
-                print >> sys.stderr, (
-                    'Missing db objects for list:', list_name)
+            lp_mailing_list = mailing_list_set.get(list_name)
+            if lp_mailing_list is None:
+                self.logger.error('No LP mailing list for: %s', list_name)
+                continue
+
+            email_address = email_address_set.getByEmail(
+                lp_mailing_list.address)
+
+            if email_address is None:
+                self.logger.error('No LP email address for: %s', list_name)
             else:
-                email_address.email = mailing_list.address
+                email_address.email = lp_mailing_list.address
+
+    def main(self):
+        """See `LaunchpadCronScript`."""
+        source_url = None
+        if len(self.args) == 0:
+            self.parser.error('Missing source_url')
+        elif len(self.args) > 1:
+            self.parser.error('Too many arguments')
+        else:
+            source_url = self.args[0]
+
+        # We need to get to the Mailman API.  Set up the paths so that Mailman
+        # can be imported.  This can't be done at module global scope.
+        mailman_path = config.mailman.build.prefix
+        sys.path.append(mailman_path)
+
+        retcode = self.syncMailmanDirectories(source_url)
+        if retcode != 0:
+            return retcode
+
+        self.fixHostnames()
 
         # All done; commit the database changes.
         self.txn.commit()
@@ -133,6 +169,6 @@ class MailingListSyncScript(LaunchpadScript):
 
 
 if __name__ == '__main__':
-    script = MailingListSyncScript()
-    status = script.run()
+    script = MailingListSyncScript('scripts.mlist-sync', 'mlist-sync')
+    status = script.lock_and_run()
     sys.exit(status)
