0974e983a311555f1bfa0339d6a0751e34e07064
Aaron Bentley <aaron@canonical.com>
2008-02-22 11:16:04 -0500

    Implement dumb view_page param to canonical_path

--- lib/canonical/launchpad/webapp/publisher.py
+++ lib/canonical/launchpad/webapp/publisher.py
@@ -283,7 +283,8 @@ def canonical_url_iterator(obj):
 
 
 def canonical_url(
-    obj, request=None, rootsite=None, path_only_if_possible=False):
+    obj, request=None, rootsite=None, path_only_if_possible=False,
+    view_page=None):
     """Return the canonical URL string for the object.
 
     If the canonical url configuration for the given object binds it to a
@@ -305,10 +306,16 @@ def canonical_url(
     from the root_url given in launchpad.conf.
 
     Raises NoCanonicalUrl if a canonical url is not available.
+    :param path_only_if_possible: If the protocol and hostname can be omitted
+        in the current context, return a url containing only the path.
+    :param view_page: Generate the canonical url for the specified view page,
+        rather than the default view page.
     """
     urlparts = [urldata.path
                 for urldata in canonical_urldata_iterator(obj)
                 if urldata.path]
+    if view_page is not None:
+        urlparts.insert(0, view_page)
 
     if rootsite is None:
         obj_urldata = ICanonicalUrlData(obj, None)
