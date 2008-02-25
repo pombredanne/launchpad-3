01a517635dc83206b9b9269ba689b8cb20eb3806
Aaron Bentley <aaron@canonical.com>
2008-02-25 15:58:06 -0500

    Explain why Attribute, not Object for IBranch.code_import

--- lib/canonical/launchpad/interfaces/branch.py
+++ lib/canonical/launchpad/interfaces/branch.py
@@ -515,6 +515,7 @@ class IBranch(IHasOwner):
     code_is_browseable = Attribute(
         "Is the code in this branch accessable through codebrowse?")
 
+    # Don't use Object-- that would cause an import loop with ICodeImport
     code_import = Attribute("The associated CodeImport, if any.")
 
     def getBzrUploadURL(person=None):
