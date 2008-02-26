482b8750de04711cb87654d8aceb1851303ff6d9
Barry Warsaw <barry@canonical.com>
2008-02-26 18:24:49 -0500

    Address Brad's reviewer comments.

--- lib/canonical/launchpad/mailman/itests/itest_helper.py
+++ lib/canonical/launchpad/mailman/itests/itest_helper.py
@@ -330,7 +330,7 @@ def subscribe(first_name, team_name):
         person = new_person(first_name)
     team = getUtility(IPersonSet).getByName(team_name)
     person.join(team)
-    # Subscriber her to the list.
+    # Subscribe her to the list.
     mailing_list = getUtility(IMailingListSet).get(team_name)
     mailing_list.subscribe(person)
     logout()
@@ -340,19 +340,40 @@ def subscribe(first_name, team_name):
 
 
 def prepare_for_sync():
-    """Prepare a sync'd directory for mlist-sync.py."""
+    """Prepare a sync'd directory for mlist-sync.py.
+
+    This simulates what happens in the real-world: the production Launchpad
+    database is copied to staging, and then the Mailman data is copied to a
+    temporary local directory on staging.  It is from this temporary location
+    that the actual staging Mailman data is sync'd.
+
+    Because of this, it's possible that a mailing list will exist in Mailman
+    but not in Launchpad's database.  We simulate this by creating fake-team
+    in Mailman only.
+
+    Also, the Mailman data will have some incorrect hostnames that reflect
+    production hostnames instead of staging hostnames.  We simulate this by
+    hacking those production names into the Mailman lists.
+
+    The Launchpad database will also have production hostnames in the mailing
+    list data it knows about.
+
+    Finally, after all this hackery, we copy the current Mailman tree to a
+    temporary location.  Thus this temporary copy will look like production's
+    Mailman database, and thus the sync will be more realistic.
+    """
     from canonical.config import config
     from canonical.database.sqlbase import commit
     from canonical.launchpad.ftests import login, logout
     from canonical.launchpad.interfaces import IEmailAddressSet
     from zope.component import getUtility
+    from Mailman import mm_cfg
+    from Mailman.MailList import MailList
+    from Mailman.Utils import list_names
     # Tweak each of the mailing lists by essentially breaking their host_name
     # and web_page_urls.  These will get repaired by the sync script.  Do this
     # before we copy so that the production copy will have the busted values.
     # pylint: disable-msg=F0401
-    from Mailman import mm_cfg
-    from Mailman.MailList import MailList
-    from Mailman.Utils import list_names
     team_names = list_names()
     for list_name in team_names:
         if list_name == mm_cfg.MAILMAN_SITE_LIST:
