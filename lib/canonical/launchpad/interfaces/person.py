bfd0bf454501e5cbab5ef72c84a76dd28e343370
Francis J. Lacoste <francis.lacoste@canonical.com>
2008-02-27 18:06:59 -0500

    Make lint happy.

--- lib/canonical/launchpad/interfaces/person.py
+++ lib/canonical/launchpad/interfaces/person.py
@@ -35,15 +35,13 @@ __all__ = [
 
 
 from zope.formlib.form import NoInputData
-from zope.schema import Bool, Choice, Datetime, Int, Object, Text, TextLine
+from zope.schema import Bool, Choice, Datetime, Int, Text, TextLine
 from zope.interface import Attribute, Interface
 from zope.interface.exceptions import Invalid
 from zope.interface.interface import invariant
 from zope.component import getUtility
 
 from canonical.lazr import DBEnumeratedType, DBItem, EnumeratedType, Item
-from canonical.lazr.interfaces import IEntry
-from canonical.lazr.rest.schema import CollectionField
 
 from canonical.launchpad import _
 
