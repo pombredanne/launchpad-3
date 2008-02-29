d7ed33fb5d7025697417274b1de6e20792a26632
Tim Penhey <tim.penhey@canonical.com>
2008-02-29 16:04:35 +1300

    Further review comment updates.

--- lib/canonical/launchpad/browser/person.py
+++ lib/canonical/launchpad/browser/person.py
@@ -249,11 +249,11 @@ class BranchTraversalMixin:
             if branch is not None and branch.product is not None:
                 # The launch bag contains "stuff of interest" related to where
                 # the user is traversing to.  When a user traverses over a
-                # product, the product gets added to the launchpad by the
+                # product, the product gets added to the launch bag by the
                 # traversal machinery, however when traversing to a branch, we
                 # short circuit it somewhat by looking for a two part key (in
                 # addition to the user) to identify the branch, and as such
-                # the product wasn't being added to the bag by the internals.
+                # the product isnt't being added to the bag by the internals.
                 getUtility(IOpenLaunchBag).add(branch.product)
             return branch
         else:
