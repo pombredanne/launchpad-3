3c2e4612d272c96eebef1873b87946977683d9be
Guilherme Salgado <salgado@canonical.com>
2008-03-04 12:47:03 -0300

    Automatically register consumers in the +request-token page

--- lib/canonical/launchpad/browser/oauth.py
+++ lib/canonical/launchpad/browser/oauth.py
@@ -20,21 +20,32 @@ class OAuthRequestTokenView(LaunchpadView):
     def __call__(self):
         """Create a request token and include its key/secret in the response.
 
-        First checks that the consumer is registered and, if not, respond
-        with a 401.
+        If the consumer key is empty or the signature doesn't match, respond
+        with a 401 status.  If the key is not empty but there's no consumer
+        with it, we register a new consumer.
         """
         form = self.request.form
-        consumer = getUtility(IOAuthConsumerSet).getByKey(
-            form.get('oauth_consumer_key'))
-
-        if consumer is None:
+        consumer_key = form.get('oauth_consumer_key')
+        if not consumer_key:
             self.request.unauthorized(CHALLENGE)
             return u''
-        # XXX: 2008-02-27, salgado: We should verify the signature here, but
-        # for now it doesn't make much sense since in the beginning all our
-        # consumer secrets will be empty.
-        assert consumer.secret == '', (
-            'We should not have any consumers with non-empty keys.')
+
+        consumer_set = getUtility(IOAuthConsumerSet)
+        consumer = consumer_set.getByKey(consumer_key)
+        if consumer is None:
+            consumer = consumer_set.new(key=consumer_key)
+
+        if form.get('oauth_signature_method') != 'PLAINTEXT':
+            # XXX: 2008-03-04, salgado: Only the PLAINTEXT method is supported
+            # now. Others will be implemented later.
+            self.request.response.setStatus(400)
+            return u''
+
+        expected_signature = "&%s" % consumer.secret
+        if expected_signature != form.get('oauth_signature'):
+            self.request.unauthorized(CHALLENGE)
+            return u''
+
         token = consumer.newRequestToken()
         body = u'oauth_token=%s&oauth_token_secret=%s' % (
             token.key, token.secret)
