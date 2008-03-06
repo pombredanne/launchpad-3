7545925e03a324ec7c15d476ef028bcbba5e1700
Curtis Hovey <curtis.hovey@canonical.com>
2008-03-06 12:28:18 -0500

    Revisions per review.

--- lib/canonical/launchpad/webapp/vhosts.py
+++ lib/canonical/launchpad/webapp/vhosts.py
@@ -89,7 +89,7 @@ class AllVirtualHostsConfiguration:
         for section in config.getByCategory('vhost'):
             if section.hostname is None:
                 continue
-            category, vhost = section.name.split('.')
+            category, vhost = section.category_specific_names
             self._configs[vhost] = config = VirtualHostConfig(
                 section.hostname,
                 section.althostnames,
