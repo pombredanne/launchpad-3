98e483b525bb3784df642bc1a938410da07bdf05
Francis J. Lacoste <francis.lacoste@canonical.com>
2008-03-17 17:16:25 -0400

    Remove code related to having our resources provide IPublishTraverse and ICanonicalUrlData. Regular Nanvigation and browser:url will be used to setup traversal/canonical_url.

--- lib/canonical/lazr/rest/resource.py
+++ lib/canonical/lazr/rest/resource.py
@@ -19,14 +19,12 @@ __all__ = [
 
 from datetime import datetime
 import simplejson
-import urllib
 
-from zope.component import adapts, getMultiAdapter
-from zope.interface import implements, directlyProvides
+from zope.component import adapts
+from zope.interface import implements
 from zope.proxy import isProxy
-from zope.publisher.interfaces import NotFound
-from zope.schema import ValidationError
-from zope.schema.interfaces import IField, IObject
+from zope.schema import getFields, ValidationError
+from zope.schema.interfaces import IObject
 from zope.security.proxy import removeSecurityProxy
 
 from canonical.lazr.enum import BaseItem
@@ -34,6 +32,7 @@ from canonical.lazr.enum import BaseItem
 # XXX leonardr 2008-01-25 bug=185958:
 # canonical_url code should be moved into lazr.
 from canonical.launchpad.webapp import canonical_url
+from canonical.launchpad.webapp.interfaces import ICanonicalUrlData
 from canonical.lazr.interfaces import (
     ICollection, ICollectionField, ICollectionResource, IEntry,
     IEntryResource, IHTTPResource, IJSONPublishable, IScopedCollection,
@@ -94,13 +93,6 @@ class HTTPResource:
         """See `IHTTPResource`."""
         pass
 
-    @property
-    def root_resource(self):
-        try:
-            return self.request.publication.getApplication(self.request)
-        except NotFound:
-            return None
-
 
 class ReadOnlyResource(HTTPResource):
     """A resource that serves a string in response to GET."""
@@ -129,76 +121,14 @@ class ReadWriteResource(HTTPResource):
             self.request.response.setHeader("Allow", "GET PATCH")
 
 
-class CollectionEntryDummy:
-    """An empty object providing the interface of the items in the collection.
-
-    This is to work around the fact that getMultiAdapter() and other
-    zope.component lookup methods don't accept a bare interface and only
-    works with objects.
-    """
-    def __init__(self, collection_field):
-        directlyProvides(self, collection_field.value_type.schema)
-
-
 class EntryResource(ReadWriteResource):
     """An individual object, published to the web."""
     implements(IEntryResource, IJSONPublishable)
 
-    rootsite = None
-    @property
-    def inside(self):
-        """See `ICanonicalUrlData`."""
-        return self.parent_collection
-
-    def __init__(self, context, request, parent_collection = None):
+    def __init__(self, context, request):
         """Associate this resource with a specific object and request."""
         super(EntryResource, self).__init__(IEntry(context), request)
-        if parent_collection:
-            self.parent_collection = parent_collection
-        else:
-            resource = self.root_resource
-            for fragment in self.context._parent_collection_path:
-                if callable(fragment):
-                    # Ask the context to do the traversal from a
-                    # collection to a specific item in that
-                    # collection.
-                    resource = resource.makeEntryResource(
-                        fragment(self.context), self.request)
-                else:
-                    # Traverse from an entry to one of the entry's
-                    # collections, by name.
-                    resource = resource.publishTraverse(self.request,
-                                                        fragment)
-            self.parent_collection = resource
-
-    @property
-    def path(self):
-        """See `IEntryResource`."""
-        path = self.parent_collection.getEntryPath(self.context)
-        return urllib.quote(path)
-
-    def publishTraverse(self, request, name):
-        """Fetch a scoped collection resource by name."""
-        field = self.context.schema.get(name)
-        if not ICollectionField.providedBy(field):
-            raise NotFound(self, name)
-        collection = getattr(self.context, name, None)
-        if collection is None:
-            raise NotFound(self, name)
-        # Create a dummy object that implements the field's interface.
-        # This is neccessary because we can't pass the interface itself
-        # into getMultiAdapter.
-        example_entry = CollectionEntryDummy(field)
-        scoped_collection = getMultiAdapter((self.context, example_entry),
-                                             IScopedCollection)
-
-        # Tell the IScopedCollection object what collection it's managing,
-        # and what the collection's relationship is to the entry it's
-        # scoped to.
-        scoped_collection.collection = collection
-        scoped_collection.relationship = field
-        return ScopedCollectionResource(scoped_collection, self.request, name)
-
+        self.original_context = context
 
     def toDataForJSON(self):
         """Turn the object into a simple data structure.
@@ -206,41 +136,27 @@ class EntryResource(ReadWriteResource):
         In this case, a dictionary containing all fields defined by
         the resource interface.
         """
-        dict = {}
-        dict['self_link'] = canonical_url(self, request=self.request)
-        schema = self.context.schema
-        for name in schema.names(True):
-            element = schema.get(name)
-            if ICollectionField.providedBy(element):
+        data = {}
+        data['self_link'] = canonical_url(self.original_context)
+        for name, field in getFields(self.context.schema).items():
+            value = getattr(self.context, name)
+            if ICollectionField.providedBy(field):
                 # The field is a collection; include a link to the
                 # collection resource.
-                try:
-                    related_resource = self.publishTraverse(
-                        self.request, name)
+                if value is not None:
                     key = name + '_collection_link'
-                    dict[key] = canonical_url(related_resource,
-                                              request=self.request)
-                except NotFound:
-                    pass
-            elif IObject.providedBy(element):
+                    data[key] = "%s/%s" % (data['self_link'], name)
+            elif IObject.providedBy(field):
                 # The field is an entry; include a link to the
                 # entry resource.
-                related_entry = getattr(self.context, name)
-                if related_entry is not None:
-                    related_resource = EntryResource(related_entry,
-                                                     self.request)
+                if value is not None:
                     key = name + '_link'
-                    dict[key] = canonical_url(related_resource,
-                                              request=self.request)
-            elif IField.providedBy(element) and not name.startswith('_'):
+                    data[key] = canonical_url(value)
+            else:
                 # It's a data field; display it as part of the
                 # representation.
-                dict[name] = getattr(self.context, name)
-            else:
-                # It's a method or some other part of an interface.
-                # Ignore it.
-                pass
-        return dict
+                data[name] = value
+        return data
 
     def do_GET(self):
         """Render the entry as JSON."""
@@ -309,14 +225,14 @@ class EntryResource(ReadWriteResource):
                 return str(e)
             validated_changeset[name] = value
 
-        original_url = canonical_url(self, request=self.request)
+        original_url = canonical_url(self.original_context)
         # Make the changes.
         for name, value in validated_changeset.items():
             setattr(self.context, name, value)
 
         # If the modification caused the entry's URL to change, tell
         # the client about the new URL.
-        new_url = canonical_url(self, request=self.request)
+        new_url = canonical_url(self.original_context)
         if new_url == original_url:
             return ''
         else:
@@ -328,84 +244,29 @@ class CollectionResource(ReadOnlyResource):
     """A resource that serves a list of entry resources."""
     implements(ICollectionResource)
 
-    # A top-level collection resource is inside the root resource.
-    inside = None
-    rootsite = None
-
-    def __init__(self, context, request, collection_name):
-        """Initialize a resource for a given collection."""
-        super(CollectionResource, self).__init__(
-            ICollection(context), request)
-        self.collection_name = collection_name
-
-    @property
-    def path(self):
-        """See `ICollectionResource`."""
-        return self.collection_name
-
-    def getEntryPath(self, entry):
-        """See `ICollectionResource`."""
-        return self.context.getEntryPath(entry)
-
-    def makeEntryResource(self, entry, request):
-        """See `ICollectionResource`."""
-        return EntryResource(entry, request)
-
-    def publishTraverse(self, request, name):
-        """Fetch an entry resource by name."""
-        entry = self.context.lookupEntry(name)
-        if entry is None:
-            raise NotFound(self, name)
-        return self.makeEntryResource(entry, self.request)
-
     def do_GET(self):
         """Fetch a collection and render it as JSON."""
-        entries = self.context.find()
+        entries = ICollection(self.context).find()
         if entries is None:
-            raise NotFound(self, self.collection_name)
-        entry_resources = [self.makeEntryResource(entry, self.request)
+            entries = []
+        entry_resources = [EntryResource(entry, self.request)
                            for entry in entries]
         self.request.response.setHeader('Content-type', 'application/json')
         return simplejson.dumps(entry_resources, cls=ResourceJSONEncoder)
 
 
 class ScopedCollectionResource(CollectionResource):
-    """A resource for a collection scoped to some entry."""
-
-    @property
-    def inside(self):
-        """See `ICanonicalUrlData`.
-
-        The object to which the collection is scoped.
-        """
-        return EntryResource(self.context.context, self.request)
-
-    def makeEntryResource(self, entry, request):
-        """Construct an entry resource, possibly scoped to this collection.
-
-        If this is the sort of scoped collection that contains the
-        actual entries (as opposed to containing references to entries
-        that 'really' live in a top-level collection), the entry resource
-        will be created knowing who its parent is.
-        """
-        if self.context.relationship.is_entry_container:
-            parent_collection = self
-        else:
-            parent_collection = None
-        return EntryResource(entry, request, parent_collection)
+    """Obsolete. Provides no functionality over CollectionResource, will be
+    removed."""
 
 
 class ServiceRootResource:
     """A resource that responds to GET by describing the service."""
-    implements(IServiceRootResource)
+    implements(IServiceRootResource, ICanonicalUrlData)
 
     inside = None
     path = ''
     rootsite = None
-
-    @property
-    def top_level_collections(self):
-        return {}
 
     def __call__(self, REQUEST=None):
         """Handle a GET request."""
@@ -414,13 +275,6 @@ class ServiceRootResource:
         else:
             REQUEST.response.setStatus(405)
             REQUEST.response.setHeader("Allow", "GET")
-
-    def publishTraverse(self, request, name):
-        if name in self.top_level_collections:
-            return CollectionResource(
-                self.top_level_collections[name], request, name)
-        else:
-            raise NotFound(self, name)
 
 
 class Entry:
@@ -453,6 +307,8 @@ class ScopedCollection:
         """
         self.context = context
         self.collection = collection
+        # Unknown at this time. Should be set by our call-site.
+        self.relationship = None
 
     def lookupEntry(self, name):
         """See `ICollection`"""
@@ -471,14 +327,6 @@ class OrderBasedScopedCollection(ScopedCollection):
     their URLs look like /collection/1, /collection/2, etc. The
     numbers start from 1.
     """
-
-    def getEntryPath(self, child):
-        """See `ICollection`."""
-        for i, entry in enumerate(self.collection):
-            if child.context == entry:
-                return str(i+1)
-        else:
-            return None
 
     def lookupEntry(self, number):
         """Find a message by its order number."""
