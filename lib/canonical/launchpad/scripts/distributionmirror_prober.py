d46c1d40b6c7fe50368285bdd3c5dac5d9b9482a
Guilherme Salgado <salgado@canonical.com>
2008-04-01 18:40:31 -0300

    Change ProberFactory.connect() to cancel the existing timeout call if it exists and is active.

--- lib/canonical/launchpad/scripts/distributionmirror_prober.py
+++ lib/canonical/launchpad/scripts/distributionmirror_prober.py
@@ -208,6 +208,8 @@ class ProberFactory(protocol.ClientFactory):
     def connect(self):
         host_requests[self.request_host] += 1
         reactor.connectTCP(self.connect_host, self.connect_port, self)
+        if hasattr(self, 'timeoutCall') and self.timeoutCall.active():
+            self._cancelTimeout(None)
         self.timeoutCall = reactor.callLater(
             self.timeout, self.failWithTimeoutError)
         self._deferred.addBoth(self._cancelTimeout)
