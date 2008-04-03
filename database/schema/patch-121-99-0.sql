c913d4458f2cef49a9e2bbfa5abce426f0aa5219
Bjorn Tillenius <bjorn@canonical.com>
2008-04-03 19:34:25 +0300

    s/remote_id/remote_comment_id/

--- database/schema/patch-121-99-0.sql
+++ database/schema/patch-121-99-0.sql
@@ -1,16 +1,16 @@
 SET client_min_messages=ERROR;
 
-ALTER TABLE BugMessage ADD COLUMN remote_id TEXT;
+ALTER TABLE BugMessage ADD COLUMN remote_comment_id TEXT;
 
--- Only allow remote_id to be set if it's an imported comment, i.e.
--- bugwatch is not NULL.
+-- Allow remote_comment_id_id to be set only if it's an imported
+-- comment, i.e.  bugwatch is not NULL.
 ALTER TABLE BugMessage ADD CONSTRAINT imported_comment
-    CHECK (remote_id IS NULL OR bugwatch IS NOT NULL);
+    CHECK (remote_comment_id IS NULL OR bugwatch IS NOT NULL);
 
 -- Make sure the same comment isn't imported twice. There can be
--- multiple comments having a NULL remote_id associated with the same
--- bug watch.
+-- multiple comments having a NULL remote_comment_id associated with the
+-- same bug watch.
 ALTER TABLE BugMessage ADD CONSTRAINT imported_comment_key
-    UNIQUE (remote_id, bugwatch);
+    UNIQUE (remote_comment_id, bugwatch);
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (121, 99, 0);
