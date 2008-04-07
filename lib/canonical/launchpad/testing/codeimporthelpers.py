ac396a9d6c60db50f37e92be9607ff30058b0f5c
Tim Penhey <tim.penhey@canonical.com>
2008-04-07 13:51:34 +1200

    Show the last successful import date on the branch page.

--- lib/canonical/launchpad/testing/codeimporthelpers.py
+++ lib/canonical/launchpad/testing/codeimporthelpers.py
@@ -5,6 +5,7 @@
 __metaclass__ = type
 __all__ = [
     'get_import_for_branch_name',
+    'make_finished_import',
     'make_running_import',
     ]
 
@@ -14,7 +15,8 @@ from zope.security.proxy import removeSecurityProxy
 
 from canonical.launchpad.database.codeimportjob import CodeImportJobWorkflow
 from canonical.launchpad.interfaces import (
-    CodeImportJobState, CodeImportReviewStatus, IBranchSet
+    CodeImportJobState, CodeImportResultStatus, CodeImportReviewStatus,
+    IBranchSet
     )
 from canonical.launchpad.testing import LaunchpadObjectFactory
 
@@ -59,5 +61,43 @@ def make_running_import(code_import=None, machine=None, date_started=None,
         # Override the job date_started.
         naked_job = removeSecurityProxy(code_import.import_job)
         naked_job.date_started = date_started
+
+    return code_import
+
+
+def make_finished_import(code_import=None, status=None, date_finished=None,
+                         factory=None):
+    """Return a code import with a new finished job.
+
+    :param code_import: The code import to create the job for.  If None, an
+        anonymous CodeImport is created.
+    :param status: The result status.  If not specified it is set to
+        SUCCESSFUL.
+    :param date_finished: If specified this overrides the date_last_successful
+        attribute of the code_import if the state is SUCCESSFUL.
+    :param factory: The LaunchpadObjectFactory to use for the creation of
+        the objects.  If None, one is created.
+    """
+    if factory is None:
+        factory = LaunchpadObjectFactory()
+    if code_import is None:
+        code_import = factory.makeCodeImport()
+    if status is None:
+        status = CodeImportResultStatus.SUCCESS
+    # The code import must be in a reviewed state.
+    if code_import.review_status != CodeImportReviewStatus.REVIEWED:
+        code_import.approve({}, code_import.registrant)
+
+    # If the job isn't running, make it run.
+    if code_import.import_job.state != CodeImportJobState.RUNNING:
+        machine = factory.makeCodeImportMachine(set_online=True)
+        CodeImportJobWorkflow().startJob(code_import.import_job, machine)
+
+    CodeImportJobWorkflow().finishJob(code_import.import_job, status, None)
+
+    if date_finished is not None and status == CodeImportResultStatus.SUCCESS:
+        # Override the code import date last successful.
+        naked_import = removeSecurityProxy(code_import)
+        naked_import.date_last_successful = date_finished
 
     return code_import
