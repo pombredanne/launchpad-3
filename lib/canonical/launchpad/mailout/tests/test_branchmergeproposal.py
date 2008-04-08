677caf4f460b7ce2fd9021f2da4b3c0cc5152567
Aaron Bentley <aaron@canonical.com>
2008-04-08 15:57:13 -0400

    Ensure the send_merge_proposal_modified_notifications actually works

--- lib/canonical/launchpad/mailout/tests/test_branchmergeproposal.py
+++ lib/canonical/launchpad/mailout/tests/test_branchmergeproposal.py
@@ -7,11 +7,14 @@ from unittest import TestLoader, TestCase
 from canonical.testing import LaunchpadFunctionalLayer
 
 from canonical.launchpad.components.branch import BranchMergeProposalDelta
+from canonical.launchpad.event import (SQLObjectModifiedEvent)
 from canonical.launchpad.ftests import login
 from canonical.launchpad.interfaces import (
     BranchSubscriptionNotificationLevel, CodeReviewNotificationLevel,
     EmailAddressStatus)
-from canonical.launchpad.mailout.branchmergeproposal import BMPMailer
+from canonical.launchpad.mailout.branchmergeproposal import (BMPMailer,
+    send_merge_proposal_modified_notifications)
+from canonical.launchpad.tests.mail_helpers import pop_notifications
 from canonical.launchpad.webapp import canonical_url
 from canonical.launchpad.testing import LaunchpadObjectFactory
 
@@ -92,11 +95,11 @@ Baz Qux has proposed merging foo into bar.
             self.fail('Did not detect bogus team recipient.')
 
     def test_forModificationNoModification(self):
-        """Ensure an assertion is raised if no change has been made."""
+        """Ensure None is returned if no change has been made."""
         merge_proposal, person = self.makeProposalWithSubscriber()
         old_merge_proposal = BranchMergeProposalDelta.snapshot(merge_proposal)
-        self.assertRaises(AssertionError, BMPMailer.forModification,
-            old_merge_proposal, merge_proposal, merge_proposal.registrant)
+        self.assertEqual(None, BMPMailer.forModification(
+            old_merge_proposal, merge_proposal, merge_proposal.registrant))
 
     def makeMergeProposalMailerModification(self):
         """Fixture method providing a mailer for a modified merge proposal"""
@@ -136,6 +139,27 @@ The proposal to merge foo into bar has been updated.
 
 %s
 """ % (url, reason), body)
+
+    def test_send_merge_proposal_modified_notifications(self):
+        """Should send emails when invoked with correct parameters."""
+        merge_proposal, subscriber = self.makeProposalWithSubscriber()
+        snapshot = BranchMergeProposalDelta.snapshot(merge_proposal)
+        merge_proposal.commit_message = 'new message'
+        event = SQLObjectModifiedEvent(merge_proposal, snapshot, None)
+        pop_notifications()
+        send_merge_proposal_modified_notifications(merge_proposal, event)
+        emails = pop_notifications()
+        self.assertEqual(1, len(emails))
+
+    def test_send_merge_proposal_modified_notifications_no_delta(self):
+        """Should not send emails if no delta."""
+        merge_proposal, subscriber = self.makeProposalWithSubscriber()
+        snapshot = BranchMergeProposalDelta.snapshot(merge_proposal)
+        event = SQLObjectModifiedEvent(merge_proposal, snapshot, None)
+        pop_notifications()
+        send_merge_proposal_modified_notifications(merge_proposal, event)
+        emails = pop_notifications()
+        self.assertEqual([], emails)
 
 
 def test_suite():
