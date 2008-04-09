3f241589d9b7ad03377028153412634ce4ea67f8
Francis J. Lacoste <francis.lacoste@canonical.com>
2008-04-09 19:23:21 -0400

    Add export_collection and @collection_default_content.

--- lib/canonical/lazr/rest/declarations.py
+++ lib/canonical/lazr/rest/declarations.py
@@ -4,40 +4,67 @@
 
 __metaclass__ = type
 __all__ = [
+    'COLLECTION_TYPE',
     'ENTRY_TYPE',
     'FIELD_TYPE',
     'LAZR_WEBSERVICE_EXPORTED',
+    'LAZR_WEBSERVICE_NS',
+    'collection_default_content',
+    'export_collection',
     'export_entry',
     'export_field',
     ]
 
 import sys
 
-from zope.interface.interfaces import IInterface
 from zope.interface.advice import addClassAdvisor
+from zope.interface.interface import TAGGED_DATA
+from zope.interface.interfaces import IInterface
 from zope.schema.interfaces import IField
 
-LAZR_WEBSERVICE_EXPORTED = 'lazr.webservice.exported'
+LAZR_WEBSERVICE_NS = 'lazr.webservice'
+LAZR_WEBSERVICE_EXPORTED = '%s.exported' % LAZR_WEBSERVICE_NS
+COLLECTION_TYPE = 'collection'
 ENTRY_TYPE = 'entry'
 FIELD_TYPE = 'field'
+
+
+def _check_called_from_interface_def(name):
+    """Make sure that the declaration was used from within a class definition.
+    """
+    # 2 is our caller's caller.
+    frame = sys._getframe(2)
+    f_locals = frame.f_locals
+
+    # Try to make sure we were called from a class def.
+    if (f_locals is frame.f_globals) or ('__module__' not in f_locals):
+        raise TypeError(
+            "%s can only be used from within an interface definition." % name)
+
+
+def _check_interface(name, interface):
+    """Check that interface provides IInterface or raise a TypeError."""
+    if not IInterface.providedBy(interface):
+        raise TypeError("%s can only be used on an interface." % name)
+
+
+def _get_interface_tags():
+    """Retrieve the dictionary containing tagged values for the interface.
+
+    This will create it, if it hasn't been defined yet.
+    """
+    # Our caller is contained within the interface definition.
+    f_locals = sys._getframe(2).f_locals
+    return f_locals.setdefault(TAGGED_DATA, {})
 
 
 def export_entry():
     """Mark the content interface as exported on the web service as an entry.
     """
-    frame = sys._getframe(1)
-    locals = frame.f_locals
-
-    # Try to make sure we were called from a class def.
-    if (locals is frame.f_globals) or ('__module__' not in locals):
-        raise TypeError(
-            "export_entry() can only be used from within an interface "
-            "definition.")
-    def tagInterface(interface):
+    _check_called_from_interface_def('export_entry()')
+    def mark_entry(interface):
         """Class advisor that tags the interface once its created."""
-        if not IInterface.providedBy(interface):
-            raise TypeError(
-                "export_entry() can only be used on an interface.")
+        _check_interface('export_entry()', interface)
         interface.setTaggedValue(
             LAZR_WEBSERVICE_EXPORTED, dict(type=ENTRY_TYPE))
 
@@ -53,7 +80,7 @@ def export_entry():
                 tag['as'] = name
 
         return interface
-    addClassAdvisor(tagInterface)
+    addClassAdvisor(mark_entry)
 
 
 def export_field(field, as=None):
@@ -66,3 +93,47 @@ def export_field(field, as=None):
         raise TypeError("export_field() can only be used on IFields.")
     field.setTaggedValue(
         LAZR_WEBSERVICE_EXPORTED, dict(type=FIELD_TYPE, as=as))
+
+
+def export_collection():
+    """Mark the interface as exported on the web service as a collection."""
+    _check_called_from_interface_def('export_collection()')
+
+    # Set the tag at this point, so that future declarations can
+    # check it.
+    tags = _get_interface_tags()
+    tags[LAZR_WEBSERVICE_EXPORTED] = dict(type=COLLECTION_TYPE)
+
+    def mark_collection(interface):
+        """Class advisor verifying the collection export requirements."""
+        _check_interface('export_collection()', interface)
+
+        tag = interface.getTaggedValue(LAZR_WEBSERVICE_EXPORTED)
+        if 'collection_default_content' not in tag:
+            raise TypeError(
+                "export_collection() is missing a method tagged with "
+                "@collection_default_content.")
+        return interface
+
+    addClassAdvisor(mark_collection)
+
+
+def collection_default_content(f):
+    """Function decorator that tags the method as providing the content."""
+    _check_called_from_interface_def('@collection_default_content')
+
+    tags = _get_interface_tags()
+    tag = tags.get(LAZR_WEBSERVICE_EXPORTED)
+    if tag is None or tag['type'] != COLLECTION_TYPE:
+        raise TypeError(
+            "@collection_default_content can only be used from within an "
+            "interface exported as a collection.")
+
+    if 'collection_default_content' in tag:
+        raise TypeError(
+            "only one method should be marked with "
+            "@collection_default_content.")
+
+    tag['collection_default_content'] = f.__name__
+
+    return f
