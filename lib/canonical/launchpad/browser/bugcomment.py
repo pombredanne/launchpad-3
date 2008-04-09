34ab1fc420679fc54e167d2f392a21c527733bb7
Abel Deuring <abel.deuring@canonical.com>
2008-04-09 15:59:00 +0200

    implemented reviewer's comments

--- lib/canonical/launchpad/browser/bugcomment.py
+++ lib/canonical/launchpad/browser/bugcomment.py
@@ -89,6 +89,10 @@ class BugComment:
         """
         comment_limit = config.malone.max_comment_size
 
+        # MessageChunks can contain either text (in MessageChunk.content)
+        # or arbitrary content (in MessageChunk.blob). The unused attribute
+        # of a MessageChunk is None. To build the text content, we want
+        # only the chunks that contain text.
         bits = [unicode(chunk.content)
                 for chunk in self.chunks if chunk.content]
         text = self.text_contents = '\n\n'.join(bits)
