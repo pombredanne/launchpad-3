4f8cc2f451ce51187d0617c265cf0ece60f5d4bc
Guilherme Salgado <salgado@canonical.com>
2008-04-11 11:12:37 -0300

    Ask users to answer a survey when they come in to request server CDs.

--- lib/canonical/launchpad/pagetitles.py
+++ lib/canonical/launchpad/pagetitles.py
@@ -1199,6 +1199,8 @@ shipit_reports = 'ShipIt reports'
 
 shipit_requestcds = 'Your ShipIt Request'
 
+shipit_survey = 'ShipIt Survey'
+
 shipitrequests_index = 'ShipIt requests'
 
 shipitrequests_search = 'Search ShipIt requests'
