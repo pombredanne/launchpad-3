b576c5476a925be45073c19e7c940a455ebde1da
Michael Hudson <michael.hudson@canonical.com>
2008-04-11 17:37:08 +1200

    unit tests for most of CodeImportWorkerMonitor

--- lib/canonical/codehosting/codeimport/tests/test_worker_monitor.py
+++ lib/canonical/codehosting/codeimport/tests/test_worker_monitor.py
@@ -8,16 +8,26 @@ __metaclass__ = type
 import StringIO
 import unittest
 
+from twisted.internet import defer
 from twisted.trial.unittest import TestCase
 
 from zope.component import getUtility
+from zope.security.proxy import removeSecurityProxy
 
 from canonical.codehosting.codeimport.worker_monitor import (
-    CodeImportWorkerMonitorProtocol)
-from canonical.launchpad.interfaces import ICodeImportJobSet
-from canonical.testing.layers import TwistedLayer
+    CodeImportWorkerMonitor, CodeImportWorkerMonitorProtocol, ExitQuietly,
+    read_only_transaction)
+from canonical.database.constants import UTC_NOW
+from canonical.database.sqlbase import commit
+from canonical.launchpad.database import CodeImportJob
+from canonical.launchpad.interfaces import (
+    CodeImportResultStatus, ICodeImportJobSet, ICodeImportJobWorkflow,
+    ICodeImportResultSet, ICodeImportSet)
+from canonical.launchpad.testing import LaunchpadObjectFactory
+from canonical.testing.layers import (
+    TwistedLayer, TwistedLaunchpadZopelessLayer)
 from canonical.twistedsupport.tests.test_processmonitor import (
-    ProcessTestsMixin)
+    makeFailure, ProcessTestsMixin)
 
 class TestWorkerMonitorProtocol(ProcessTestsMixin, TestCase):
 
@@ -90,7 +100,139 @@ class TestWorkerMonitorProtocol(ProcessTestsMixin, TestCase):
         self.assertEqual(self.protocol._tail, 'a'*50 + 'b'*50)
 
 
-class TestWorkerMonitorIntegration(TestCase):
+class TestWorkerMonitor(TestCase):
+    """Unit tests for most of the `CodeImportWorkerMonitor` class."""
+
+    layer = TwistedLaunchpadZopelessLayer
+
+    def getResultsForOurCodeImport(self):
+        code_import = getUtility(ICodeImportSet).get(self.code_import_id)
+        return getUtility(ICodeImportResultSet).getResultsForImport(
+            code_import)
+
+    def getOneResultForOurCodeImport(self):
+        results = list(self.getResultsForOurCodeImport())
+        self.failUnlessEqual(len(results), 1)
+        return results[0]
+
+    def setUp(self):
+        self.factory = LaunchpadObjectFactory()
+        job = self.factory.makeCodeImportJob()
+        self.code_import_id = job.code_import.id
+        getUtility(ICodeImportJobWorkflow).startJob(
+            job, self.factory.makeCodeImportMachine(set_online=True))
+        self.job_id = job.id
+        self.worker_monitor = CodeImportWorkerMonitor(job.id)
+        commit()
+
+    def test_getJob(self):
+        # getJob() returns the job whose id we passed to the constructor.
+        return self.assertEqual(
+            self.worker_monitor.getJob().id, self.job_id)
+
+    def test_getJobWhenJobDeleted(self):
+        # If the job has been deleted, getJob sets _call_finish_job to False
+        # and raises ExitQuietly.
+        job = self.worker_monitor.getJob()
+        removeSecurityProxy(job).destroySelf()
+        self.assertRaises(ExitQuietly, self.worker_monitor.getJob)
+        self.assertNot(self.worker_monitor._call_finish_job)
+
+    def test_getSourceDetails(self):
+        # getSourceDetails extracts the details from the CodeImport database
+        # object.
+        @read_only_transaction
+        def check_source_details(details):
+            job = self.worker_monitor.getJob()
+            self.assertEqual(
+                details.svn_branch_url, job.code_import.svn_branch_url)
+            self.assertEqual(
+                details.cvs_root, job.code_import.cvs_root)
+            self.assertEqual(
+                details.cvs_module, job.code_import.cvs_module)
+        return self.worker_monitor.getSourceDetails().addCallback(
+            check_source_details)
+
+    def test_updateHeartbeat(self):
+        # The worker monitor's updateHeartbeat method calls the
+        # updateHeartbeat job workflow method.
+        @read_only_transaction
+        def check_updated_details(result):
+            job = self.worker_monitor.getJob()
+            self.assertEqual(job.logtail, 'log tail')
+        return self.worker_monitor.updateHeartbeat('log tail').addCallback(
+            check_updated_details)
+
+    def test_finishJobCallsFinishJob(self):
+        # The worker monitor's finishJob method calls the
+        # finishJob job workflow method.
+        @read_only_transaction
+        def check_finishJob_called(result):
+            # We take as indication that finishJob was called that a
+            # CodeImportResult was created.
+            self.assertEqual(
+                len(list(self.getResultsForOurCodeImport())), 1)
+        return self.worker_monitor.finishJob(
+            CodeImportResultStatus.SUCCESS).addCallback(
+            check_finishJob_called)
+
+    def test_finishJobDoesntUploadEmptyFileToLibrarian(self):
+        # The worker monitor's finishJob method does not try to upload an
+        # empty log file to the librarian.
+        self.assertEqual(self.worker_monitor._log_file.tell(), 0)
+        @read_only_transaction
+        def check_no_file_uploaded(result):
+            result = self.getOneResultForOurCodeImport()
+            self.assert_(result.log_file is None)
+        return self.worker_monitor.finishJob(
+            CodeImportResultStatus.SUCCESS).addCallback(
+            check_no_file_uploaded)
+
+    def test_finishJobUploadsNonEmptyFileToLibrarian(self):
+        # The worker monitor's finishJob method uploads the log file to the
+        # librarian.
+        self.worker_monitor._log_file.write('some text')
+        @read_only_transaction
+        def check_file_uploaded(result):
+            result = self.getOneResultForOurCodeImport()
+            self.assert_(result.log_file is not None)
+            self.assertEqual(result.log_file.read(), 'some text')
+        return self.worker_monitor.finishJob(
+            CodeImportResultStatus.SUCCESS).addCallback(
+            check_file_uploaded)
+
+    def patchOutFinishJob(self):
+        calls = []
+        def finishJob(status):
+            calls.append(status)
+            return defer.succeed(None)
+        self.worker_monitor.finishJob = finishJob
+        return calls
+
+    def test_callFinishJobCallsFinishJobSuccess(self):
+        # callFinishJob calls finishJob with CodeImportResultStatus.SUCCESS if
+        # its argument is not a Failure.
+        calls = self.patchOutFinishJob()
+        self.worker_monitor.callFinishJob(None)
+        self.assertEqual(calls, [CodeImportResultStatus.SUCCESS])
+
+    def test_callFinishJobCallsFinishJobFailure(self):
+        # callFinishJob calls finishJob with CodeImportResultStatus.FAILURE if
+        # its argument is a Failure.
+        calls = self.patchOutFinishJob()
+        ret = self.worker_monitor.callFinishJob(makeFailure(RuntimeError))
+        self.assertEqual(calls, [CodeImportResultStatus.FAILURE])
+        return self.assertFailure(ret, RuntimeError)
+
+    def test_callFinishJobRespects_call_finish_job(self):
+        # callFinishJob does not call finishJob if _call_finish_job is False.
+        calls = self.patchOutFinishJob()
+        self.worker_monitor._call_finish_job = False
+        self.worker_monitor.callFinishJob(None)
+        self.assertEqual(calls, [])
+
+
+class TestXXXWorkerMonitorIntegration(TestCase):
 
     def setUp(self):
         self.nukeCodeImportSampleData()
--- lib/canonical/codehosting/codeimport/worker_monitor.py
+++ lib/canonical/codehosting/codeimport/worker_monitor.py
@@ -29,79 +29,6 @@ from canonical.twistedsupport import defer_to_thread
 from canonical.twistedsupport.processmonitor import (
     ProcessMonitorProtocolWithTimeout)
 
-def read_only_transaction(function):
-    """Wrap 'function' in a transaction and Zope session.
-
-    The transaction is always aborted."""
-    def transacted(*args, **kwargs):
-        begin()
-        login(ANONYMOUS)
-        try:
-            return function(*args, **kwargs)
-        finally:
-            logout()
-            rollback()
-    return mergeFunctionMetadata(function, transacted)
-
-
-def writing_transaction(function):
-    """Wrap 'function' in a transaction and Zope session.
-
-    The transaction is committed if 'function' returns normally and
-    aborted if it raises."""
-    def transacted(*args, **kwargs):
-        begin()
-        login(ANONYMOUS)
-        try:
-            ret = function(*args, **kwargs)
-        except:
-            logout()
-            rollback()
-            raise
-        logout()
-        commit()
-        return ret
-    return mergeFunctionMetadata(function, transacted)
-
-
-class ExitWithoutCallingFinishJob(Exception):
-    """A special exception to be raised when we should exit quietly.
-
-    """
-    pass
-
-
-@defer_to_thread
-@writing_transaction
-def finish_job(job_id, status, log_file_object):
-    """Call `finishJob` for the job of id 'job_id'."""
-    job = getUtility(ICodeImportJobSet).getById(job_id)
-    if job is None:
-        return
-    log_file_size = log_file_object.tell()
-    if log_file_size > 0:
-        log_file_object.seek(0)
-        branch = job.code_import.branch
-        log_file_name = '%s-%s-log.txt' % (branch.product.name, branch.name)
-        # Watch out for this failing!!
-        log_file_alias = getUtility(ILibraryFileAliasSet).create(
-            log_file_name, log_file_size, log_file_object,
-            'text/plain')
-    else:
-        log_file_alias = None
-    getUtility(ICodeImportJobWorkflow).finishJob(job, status, log_file_alias)
-
-
-@defer_to_thread
-@read_only_transaction
-def get_source_details(job_id):
-    """Get a `CodeImportSourceDetails` object for the job of id 'job_id'."""
-    job = getUtility(ICodeImportJobSet).getById(job_id)
-    if job is None:
-        raise ExitWithoutCallingFinishJob
-    return CodeImportSourceDetails.fromCodeImport(
-        job.code_import)
-
 
 class CodeImportWorkerMonitorProtocol(ProcessMonitorProtocolWithTimeout):
 
@@ -133,6 +60,39 @@ class CodeImportWorkerMonitorProtocol(ProcessMonitorProtocolWithTimeout):
         ProcessMonitorProtocolWithTimeout.processEnded(self, reason)
         self._looping_call.stop()
 
+def read_only_transaction(function):
+    """XXX."""
+    def transacted(*args, **kwargs):
+        begin()
+        login(ANONYMOUS)
+        try:
+            return function(*args, **kwargs)
+        finally:
+            logout()
+            rollback()
+    return mergeFunctionMetadata(function, transacted)
+
+
+def writing_transaction(function):
+    """XXX."""
+    def transacted(*args, **kwargs):
+        begin()
+        login(ANONYMOUS)
+        try:
+            ret = function(*args, **kwargs)
+        except:
+            logout()
+            rollback()
+            raise
+        logout()
+        commit()
+        return ret
+    return mergeFunctionMetadata(function, transacted)
+
+
+class ExitQuietly(Exception):
+    pass
+
 
 class CodeImportWorkerMonitor:
 
@@ -140,50 +100,92 @@ class CodeImportWorkerMonitor:
         get_rocketfuel_root(),
         'scripts', 'code-import-worker.py')
 
-    def __init__(self, job_id, logger):
-        self.job_id = job_id
-        self.call_finish_job = True
-        self.logger = logger
+    def __init__(self, job_id):
+        self._job_id = job_id
+        self._call_finish_job = True
+        self._log_file = tempfile.TemporaryFile()
+
+    def begin(self):
+        begin()
+        login(ANONYMOUS)
+
+    def rollback(self):
+        logout()
+        rollback()
+
+    def commit(self):
+        logout()
+        commit()
+
+    def getJob(self):
+        """Only call this from defer_to_thread-ed methods!"""
+        job = getUtility(ICodeImportJobSet).getById(self._job_id)
+        if job is None:
+            self._call_finish_job = False
+            raise ExitQuietly
+        else:
+            return job
+
+    @defer_to_thread
+    @read_only_transaction
+    def getSourceDetails(self):
+        """XXX."""
+        return CodeImportSourceDetails.fromCodeImport(
+            self.getJob().code_import)
 
     @defer_to_thread
     @writing_transaction
     def updateHeartbeat(self, tail):
-        """Call `updateHeartbeat` for the job of id 'job_id'."""
-        job = getUtility(ICodeImportJobSet).getById(self.job_id)
-        if job is None:
-            self.call_finish_job = False
-            raise ExitWithoutCallingFinishJob
-        getUtility(ICodeImportJobWorkflow).updateHeartbeat(job, tail)
+        """XXX."""
+        getUtility(ICodeImportJobWorkflow).updateHeartbeat(
+            self.getJob(), tail)
+
+    @defer_to_thread
+    @writing_transaction
+    def finishJob(self, status):
+        """XXX."""
+        job = self.getJob()
+        log_file_size = self._log_file.tell()
+        if log_file_size > 0:
+            self._log_file.seek(0)
+            branch = job.code_import.branch
+            log_file_name = '%s-%s-log.txt' % (branch.product.name, branch.name)
+            # Watch out for this failing!!
+            log_file_alias = getUtility(ILibraryFileAliasSet).create(
+                log_file_name, log_file_size, self._log_file,
+                'text/plain')
+        else:
+            log_file_alias = None
+        getUtility(ICodeImportJobWorkflow).finishJob(
+            job, status, log_file_alias)
 
     def _launchProcess(self, source_details):
         deferred = defer.Deferred()
-        self.tmpfile = tempfile.TemporaryFile()
         protocol = CodeImportWorkerMonitorProtocol(
-            deferred, self.job_id, self.tmpfile)
+            deferred, self._job_id, self._log_file)
         command = [sys.executable, self.path_to_script]
         command.extend(source_details.asArguments())
-        print command
         reactor.spawnProcess(
             protocol, sys.executable, command, env=os.environ, usePTY=True)
-        deferred.addErrback(self._silenceQuietDeath)
-        deferred.addBoth(self.callFinishJob)
         return deferred
 
     def run(self):
-        return get_source_details(self.job_id).addCallback(
-            self._launchProcess)
+        return self.getSourceDetails().addCallback(
+            self._launchProcess).addBoth(
+            self.callFinishJob).addErrback(
+            self._silenceQuietExit)
 
-    def _silenceQuietDeath(self, failure):
-        failure.trap(ExitWithoutCallingFinishJob)
-        self.call_finish_job = False
+    def _silenceQuietExit(self, failure):
+        failure.trap(ExitQuietly)
         return None
 
     def callFinishJob(self, reason):
-        if not self.call_finish_job:
-            return
+        if not self._call_finish_job:
+            return reason
         if isinstance(reason, failure.Failure):
             status = CodeImportResultStatus.FAILURE
         else:
             status = CodeImportResultStatus.SUCCESS
-        return finish_job(self.job_id, status, self.tmpfile)
+        return self.finishJob(status).addCallback(
+            lambda ignored: reason)
 
