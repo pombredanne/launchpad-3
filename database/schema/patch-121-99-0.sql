74ee637ad833f33f7264caa7e1a3e41e0901f17f
Tom Berger <tom.berger@canonical.com>
2008-04-14 17:37:24 +0100

    remove a blank line to test bzr

--- database/schema/patch-121-99-0.sql
+++ database/schema/patch-121-99-0.sql
@@ -6,5 +6,4 @@ ALTER TABLE BugTask
   ADD COLUMN date_fix_committed timestamp without time zone,
   ADD COLUMN date_fix_released timestamp without time zone;
 
-
 INSERT INTO LaunchpadDatabaseRevision VALUES (121, 99, 0);
