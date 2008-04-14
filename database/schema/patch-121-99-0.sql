d8322f8ae0423120a2fda434f062fbb4ee35cc2a
Brad Crittenden <bac@canonical.com>
2008-04-14 15:42:34 -0400

    Added subscription table patch.  Updated permissions.

---
+++ database/schema/patch-121-99-0.sql
@@ -0,0 +1,23 @@
+SET client_min_messages=ERROR;
+
+CREATE TABLE Subscription (
+    id serial PRIMARY KEY,
+    date_created timestamp without time zone DEFAULT timezone('UTC'::text, now()) NOT NULL,
+    date_last_modified timestamp without time zone DEFAULT timezone('UTC'::text, now()) NOT NULL,
+    date_starts timestamp without time zone NOT NULL,
+    date_expires timestamp without time zone NOT NULL,
+    status integer NOT NULL DEFAULT 10, -- UNSUBSCRIBED
+    product integer NOT NULL,
+    registrant integer,
+    authorized_by integer,
+    whiteboard text,
+    sales_system integer,
+    sales_system_id text,
+    sales_method integer
+);
+
+-- Need indexes for people merge
+CREATE INDEX subscription__product__idx
+  ON Subscription(product);
+
+INSERT INTO LaunchpadDatabaseRevision VALUES (121, 99, 0);
