f0df94ce40c545165c391541b6ec81b7975c1a29
Tom Berger <tom.berger@canonical.com>
2008-04-14 17:34:22 +0100

    add a blank line to test bzr

--- lib/canonical/launchpad/interfaces/productseries.py
+++ lib/canonical/launchpad/interfaces/productseries.py
@@ -38,6 +38,7 @@ from canonical.launchpad import _
 from canonical.lazr.enum import DBEnumeratedType, DBItem
 
 
+
 class ImportStatus(DBEnumeratedType):
     """This schema describes the states that a SourceSource record can take
     on."""
