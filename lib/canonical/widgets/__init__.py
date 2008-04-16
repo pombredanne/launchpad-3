52d6b2a726e72ead4842e175e77ccabf33ed49db
Jonathan Lange <jml@canonical.com>
2008-04-16 12:05:56 +1000

    Sort imports.

--- lib/canonical/widgets/__init__.py
+++ lib/canonical/widgets/__init__.py
@@ -6,18 +6,18 @@
 These may be fed back into Zope3 at some point.
 """
 
+from canonical.widgets.announcementdate import (
+    IAnnouncementDateWidget, AnnouncementDateWidget)
+from canonical.widgets.context import IContextWidget, ContextWidget
 from canonical.widgets.date import (
     DateWidget, DateTimeWidget, DatetimeDisplayWidget)
 from canonical.widgets.image import (
     GotchiTiedWithHeadingWidget, ImageChangeWidget)
+from canonical.widgets.itemswidgets import *
 from canonical.widgets.owner import IUserWidget, HiddenUserWidget
 from canonical.widgets.popup import (
     ISinglePopupWidget, SearchForUpstreamPopupWidget, SinglePopupWidget,
     BranchPopupWidget)
-from canonical.widgets.announcementdate import (
-    IAnnouncementDateWidget, AnnouncementDateWidget)
-from canonical.widgets.context import IContextWidget, ContextWidget
-from canonical.widgets.itemswidgets import *
 from canonical.widgets.password import PasswordChangeWidget
 from canonical.widgets.textwidgets import (
     DelimitedListWidget, LocalDateTimeWidget, LowerCaseTextWidget,
