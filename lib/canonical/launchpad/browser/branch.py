4fec14f47ffe496e61ccc05c9bf41736fcde5cc0
Jonathan Lange <jml@canonical.com>
2008-04-17 11:47:13 +1000

    Uses of the creator kwarg that I missed.

--- lib/canonical/launchpad/browser/branch.py
+++ lib/canonical/launchpad/browser/branch.py
@@ -759,7 +759,7 @@ class BranchAddView(LaunchpadFormView, BranchNameValidationMixin):
             self.branch = getUtility(IBranchSet).new(
                 branch_type=BranchType.items[ui_branch_type.name],
                 name=data['name'],
-                creator=self.user,
+                registrant=self.user,
                 owner=data['owner'],
                 author=self.getAuthor(data),
                 product=data['product'],
