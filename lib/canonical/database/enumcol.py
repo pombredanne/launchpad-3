6426e8df7e5ea52504b108eaef3e4c6d1a7e35fd
James Henstridge <james.henstridge@canonical.com>
2008-04-21 14:01:27 +0800

    Update EnumCol to build on top of a proper DBEnumeratedType-based storm property/variable.

--- lib/canonical/database/enumcol.py
+++ lib/canonical/database/enumcol.py
@@ -3,28 +3,60 @@
 __metaclass__ = type
 
 from storm import sqlobject
-from storm.properties import Enum
+from storm.properties import SimpleProperty
+from storm.variables import Variable
 
-from canonical.lazr import DBEnumeratedType
+from zope.security.proxy import isinstance as zope_isinstance
+
+from canonical.lazr import DBEnumeratedType, DBItem
 
 __all__ = [
+'DBEnum',
 'EnumCol',
     ]
 
-class DBSchemaEnumCol(sqlobject.PropertyAdapter, Enum):
-    def __init__(self, **kw):
-        try:
-            self.enum = kw.pop('enum')
-            source = 'enum'
-        except KeyError:
-            self.enum = kw.pop('schema')
-            source = 'schema'
-        if not issubclass(self.enum, DBEnumeratedType):
+
+class DBEnumVariable(Variable):
+    """A Storm variable class representing a DBEnumeratedType."""
+
+    def __init__(self, *args, **kwargs):
+        self._enum = kwargs.pop("enum")
+        if not issubclass(self._enum, DBEnumeratedType):
             raise TypeError(
                 '%s must be a DBEnumeratedType: %r'
-                % (source, type(self.enum)))
+                % (source, type(self._enum)))
+        super(DBEnumVariable, self).__init__(*args, **kwargs)
+
+    def parse_set(self, value, from_db):
+        if from_db:
+            return self._enum.items[value]
+        else:
+            if not zope_isinstance(value, DBItem):
+                raise TypeError("Not a DBItem: %r" % type(value))
+            if self._enum != value.enum:
+                raise TypeError("DBItem from wrong type, %r != %r" % (
+                        self._enum.name, value.enum.name))
+            return value
+
+    def parse_get(self, value, to_db):
+        if to_db:
+            return value.value
+        else:
+            return value
+
+
+class DBEnum(SimpleProperty):
+    variable_class = DBEnumVariable
+
+
+class DBSchemaEnumCol(sqlobject.PropertyAdapter, DBEnum):
+    def __init__(self, **kw):
+        try:
+            enum = kw.pop('enum')
+        except KeyError:
+            enum = kw.pop('schema')
         self._kwargs = {
-            'map': dict((item, item.value) for item in self.enum.items)
+            'enum': enum
             }
         super(DBSchemaEnumCol, self).__init__(**kw)
 
