7eb4657efacff6be42e65d13cb0709e840bfd6b7
Stuart Bishop <stuart.bishop@canonical.com>
2008-04-22 11:41:43 +0700

    Revert 5967.3.5 as it prevents us from ever removing Person records

--- database/schema/patch-121-30-0.sql
+++
@@ -1,7 +0,0 @@
-SET client_min_messages=ERROR;
-
-CREATE TRIGGER t_assert_self_membership
-    AFTER DELETE ON TeamParticipation
-    FOR EACH ROW EXECUTE PROCEDURE assert_self_membership();
-
-INSERT INTO LaunchpadDatabaseRevision VALUES (121, 30, 0);
