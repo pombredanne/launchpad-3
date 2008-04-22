e379f687c26c5b51031bc74d984434b1945579b9
Guilherme Salgado <salgado@canonical.com>
2008-04-22 12:49:15 -0300

    Grant permission on some other tables to the shipit db user so that we can run shipit-exports.py again.

--- database/sampledata/current.sql
+++ database/sampledata/current.sql
@@ -7445,6 +7445,12 @@ INSERT INTO requestedcds (id, request, quantity, flavour, distroseries, architec
 INSERT INTO requestedcds (id, request, quantity, flavour, distroseries, architecture, quantityapproved) VALUES (159, 19, 0, 2, 3, 2, 0);
 INSERT INTO requestedcds (id, request, quantity, flavour, distroseries, architecture, quantityapproved) VALUES (160, 19, 50, 3, 3, 1, 50);
 INSERT INTO requestedcds (id, request, quantity, flavour, distroseries, architecture, quantityapproved) VALUES (161, 20, 8, 1, 4, 1, 0);
+INSERT INTO requestedcds (id, request, quantity, flavour, distroseries, architecture, quantityapproved) VALUES (166, 21, 1, 1, 6, 1, 1);
+INSERT INTO requestedcds (id, request, quantity, flavour, distroseries, architecture, quantityapproved) VALUES (167, 21, 0, 1, 6, 2, 0);
+INSERT INTO requestedcds (id, request, quantity, flavour, distroseries, architecture, quantityapproved) VALUES (168, 21, 0, 1, 6, 3, 0);
+INSERT INTO requestedcds (id, request, quantity, flavour, distroseries, architecture, quantityapproved) VALUES (169, 21, 5, 2, 6, 1, 5);
+INSERT INTO requestedcds (id, request, quantity, flavour, distroseries, architecture, quantityapproved) VALUES (170, 21, 0, 2, 6, 2, 0);
+INSERT INTO requestedcds (id, request, quantity, flavour, distroseries, architecture, quantityapproved) VALUES (171, 21, 0, 2, 6, 3, 0);
 
 
 ALTER TABLE requestedcds ENABLE TRIGGER ALL;
@@ -7774,6 +7780,7 @@ INSERT INTO shippingrequest (id, recipient, whoapproved, whocancelled, datereque
 INSERT INTO shippingrequest (id, recipient, whoapproved, whocancelled, daterequested, shockandawe, reason, highpriority, recipientdisplayname, addressline1, addressline2, organization, city, province, country, postcode, phone, fti, shipment, status, normalized_address, "type") VALUES (18, 23, 55, NULL, '2006-03-01 18:38:24.526648', NULL, NULL, false, 'David Allouche', 'Velvet Zephyr Woods', '5423', NULL, 'whatever', 'not mandatory', 75, '999432423', '+55 16 3374-2027', NULL, 4, 4, 'velvetzephyrwoods5423whatever', NULL);
 INSERT INTO shippingrequest (id, recipient, whoapproved, whocancelled, daterequested, shockandawe, reason, highpriority, recipientdisplayname, addressline1, addressline2, organization, city, province, country, postcode, phone, fti, shipment, status, normalized_address, "type") VALUES (19, 243601, NULL, NULL, '2006-10-17 20:53:11.714301', NULL, NULL, false, 'Marilize Coetzee', 'Somewhere around here', NULL, NULL, 'fdas fd as fa', NULL, 9, '43243242', '432432', NULL, NULL, 1, 'somewherearoundherefdasfdasfa', NULL);
 INSERT INTO shippingrequest (id, recipient, whoapproved, whocancelled, daterequested, shockandawe, reason, highpriority, recipientdisplayname, addressline1, addressline2, organization, city, province, country, postcode, phone, fti, shipment, status, normalized_address, "type") VALUES (20, 1, NULL, NULL, '2006-02-01 18:38:24.526648', NULL, NULL, false, 'Somebody Else', 'Rue de Rivoli', '1345', NULL, 'Paris', NULL, 25, '999432423', '+55 16 3374-2027', NULL, NULL, 6, 'ruederivoli1345paris', NULL);
+INSERT INTO shippingrequest (id, recipient, whoapproved, whocancelled, daterequested, shockandawe, reason, highpriority, recipientdisplayname, addressline1, addressline2, organization, city, province, country, postcode, phone, fti, shipment, status, normalized_address, "type") VALUES (21, 63, NULL, NULL, '2008-04-22 14:57:32.037998', NULL, NULL, false, 'fdsafdsa fdsafdsa', 'fdsafdsaf', NULL, 'fdsafdfda', 'fdsafdsafd', 'fdsafdsa', 16, '43214321421', '43124321', NULL, NULL, 1, 'fdsafdsaffdsafdsafd', 2);
 
 
 ALTER TABLE shippingrequest ENABLE TRIGGER ALL;
