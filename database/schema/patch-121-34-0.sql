1f671a63f248a1c8aabb6960d4364f065d162b39
Muharem Hrnjadovic <muharem@canonical.com>
2008-04-23 10:33:29 +0200

    renamed database patch file

--- database/schema/patch-121-34-0.sql
+++ database/schema/patch-121-34-0.sql
@@ -2,4 +2,4 @@ SET client_min_messages=ERROR;
 
 ALTER TABLE distroarchseries RENAME COLUMN ppa_supported TO supports_virtualized;
 
-INSERT INTO LaunchpadDatabaseRevision VALUES (121, 99, 0);
+INSERT INTO LaunchpadDatabaseRevision VALUES (121, 34, 0);
