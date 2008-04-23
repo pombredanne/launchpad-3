165175d682d844f1a958737a4a0458d8694cf5a8
Stuart Bishop <stuart.bishop@canonical.com>
2008-04-23 16:24:28 +0700

    Renumber block comment push db patch

--- database/schema/patch-121-37-0.sql
+++ database/schema/patch-121-37-0.sql
@@ -3,4 +3,4 @@ SET client_min_messages=ERROR;
 ALTER TABLE BugTracker ADD COLUMN block_comment_pushing BOOLEAN NOT NULL
     DEFAULT false;
 
-INSERT INTO LaunchpadDatabaseRevision VALUES (121, 99, 0);
+INSERT INTO LaunchpadDatabaseRevision VALUES (121, 37, 0);
