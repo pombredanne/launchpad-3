191487777838a04a336f989beaa04c793035a9f9
Stuart Bishop <stuart.bishop@canonical.com>
2008-04-24 12:57:04 +0700

    Renumber shock-and-awe dropping db patch

--- database/schema/patch-121-41-0.sql
+++ database/schema/patch-121-41-0.sql
@@ -4,4 +4,4 @@ ALTER TABLE ShippingRequest DROP COLUMN shockandawe;
 
 DROP TABLE shockandawe;
 
-INSERT INTO LaunchpadDatabaseRevision VALUES (121, 39, 0);
+INSERT INTO LaunchpadDatabaseRevision VALUES (121, 41, 0);
