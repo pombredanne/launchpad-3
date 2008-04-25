ba7e298433201db979255179bc58c7708cf526e8
Bjorn Tillenius <bjorn@canonical.com>
2008-04-25 11:48:45 +0300

    fix XX message.

--- lib/canonical/codehosting/tests/test_acceptance.py
+++ lib/canonical/codehosting/tests/test_acceptance.py
@@ -623,8 +623,8 @@ def test_suite():
     base_suite = unittest.makeSuite(AcceptanceTests)
     suite = unittest.TestSuite()
 
-    # XXX: Disabling the SFTP tests due to them failing intermittently.
-    #      Bug 221762. -- Bjorn Tillenius, 2008-04-25
+    # XXX: Disabling the AcceptanceTests tests due to them failing
+    #      intermittently. Bug 221762. -- Bjorn Tillenius, 2008-04-25
     #suite.addTest(make_server_tests(
     #        base_suite, [make_sftp_server, make_bzr_ssh_server]))
 
