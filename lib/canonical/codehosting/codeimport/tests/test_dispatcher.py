5845bd5343afe2a40f2509beff2f2fa555bb50d9
Michael Hudson <michael.hudson@canonical.com>
2008-04-29 13:24:31 +1200

    Write the two tests I've been putting off writing for days.

--- lib/canonical/codehosting/codeimport/tests/test_dispatcher.py
+++ lib/canonical/codehosting/codeimport/tests/test_dispatcher.py
@@ -5,11 +5,18 @@
 __metaclass__ = type
 
 
+import os
+import shutil
 import socket
+import sys
+import tempfile
 from textwrap import dedent
 from unittest import TestLoader
 
+from twisted.internet import reactor
 from twisted.trial.unittest import TestCase
+from twisted.web.server import Site
+from twisted.web.xmlrpc import XMLRPC
 
 from zope.component import getUtility
 
@@ -19,16 +26,19 @@ from canonical.codehosting.codeimport.tests.test_foreigntree import (
     _make_silent_logger)
 from canonical.codehosting.codeimport.tests.test_worker_monitor import (
     nuke_codeimport_sample_data)
+from canonical.codehosting.codeimport.worker_monitor import (
+    writing_transaction)
 from canonical.database.sqlbase import flush_database_updates
 from canonical.launchpad.interfaces import (
     CodeImportMachineOfflineReason, CodeImportMachineState,
     ICodeImportJobWorkflow)
 from canonical.launchpad.testing import LaunchpadObjectFactory
-from canonical.testing.layers import LaunchpadZopelessLayer
+from canonical.testing.layers import TwistedLaunchpadZopelessLayer
+from canonical.twistedsupport import defer_to_thread
 
 class CodeImportDispatcherTestMixin:
 
-    layer = LaunchpadZopelessLayer
+    layer = TwistedLaunchpadZopelessLayer
 
     def pushConfig(self, **args):
         """XXX."""
@@ -46,8 +56,34 @@ class CodeImportDispatcherTestMixin:
         self.config_count = 0
         self.pushConfig(forced_hostname='none')
         self.dispatcher = self.Dispatcher(
-            LaunchpadZopelessLayer.txn, _make_silent_logger())
+            TwistedLaunchpadZopelessLayer.txn, _make_silent_logger())
         self.factory = LaunchpadObjectFactory()
+
+
+class FakeScheduler(XMLRPC):
+
+    def __init__(self, id_to_return):
+        XMLRPC.__init__(self)
+        self.id_to_return = id_to_return
+
+    def xmlrpc_getJobForMachine(self, machine_name):
+        return self.id_to_return
+
+class FakeSchedulerFixture:
+
+    def __init__(self, id_to_return):
+        self.id_to_return = id_to_return
+
+    def setUp(self):
+        self.port = reactor.listenTCP(
+            0, Site(FakeScheduler(self.id_to_return)))
+
+    def tearDown(self):
+        return self.port.stopListening()
+
+    def get_url(self):
+        tcp_port = self.port.getHost().port
+        return 'http://localhost:%s/' % tcp_port
 
 
 class TestCodeImportDispatcherUnit(CodeImportDispatcherTestMixin, TestCase):
@@ -68,24 +104,59 @@ class TestCodeImportDispatcherUnit(CodeImportDispatcherTestMixin, TestCase):
             self.dispatcher.getHostname(),
             'test-value')
 
+    def writePythonScript(self, script_path, script_body):
+        script = open(script_path, 'w')
+        script.write("#!%s\n" % sys.executable)
+        for script_line in script_body:
+            script.write(script_line + '\n')
+        os.chmod(script_path, 0700)
+
+    def filterOutLoggingOptions(self, arglist):
+        from canonical.launchpad import scripts
+        from optparse import OptionParser
+        parser = OptionParser()
+        scripts.logger_options(parser)
+        options, args = parser.parse_args(arglist)
+        return args
+
     def test_dispatchJob(self):
-        # dispatchJob launches a process described by worker_script
-        # with a given job id as an argument.
-        # create script that writes command line arguments to some tmpfile
-        # assign this to worker_script
+        # dispatchJob launches a process described by its
+        # worker_script attribute with a given job id as an argument.
+
+        # We create a script that writes its command line arguments to
+        # some a temporary file and examine that.
+        tmpdir = tempfile.mkdtemp()
+        self.addCleanup(shutil.rmtree, tmpdir)
+        script_path = os.path.join(tmpdir, 'script.py')
+        output_path = os.path.join(tmpdir, 'output.txt')
+        self.writePythonScript(
+            script_path,
+            ['import sys',
+             'open(%r, "w").write(str(sys.argv[1:]))' % output_path])
+        self.dispatcher.worker_script = script_path
         self.dispatcher.dispatchJob(10)
-        # assert stuff about contents of tmpfile.  I guess we'll need
-        # to wait a while or call os.wait or stash the child pid
-        # somewhere to know that the test is done...
+        # It's a little bit dodgy to call os.wait() here: we don't
+        # know for certain that it will be the child launched in
+        # dispatchJob that will be reaped.  It's pretty gosh-darned
+        # likely though.
+        os.wait()
+        arglist = self.filterOutOptions(eval(open(output_path).read()))
+        self.assertEqual(arglist, ['10'])
 
     def test_getJobForMachine(self):
         # getJobForMachine calls getJobForMachine on the endpoint
         # described by the codeimportscheduler_url config entry.
-        # Fire up an xml-rpc server that always answers 42
-        self.pushConfig(codeimportscheduler_url='...')
-        job_id = self.dispatcher.getJobForMachine(
-            self.factory.makeCodeImportMachine(set_online=True))
-        self.assertEqual(job_id, 42)
+        fake_scheduler_fixture = FakeSchedulerFixture(42)
+        fake_scheduler_fixture.setUp()
+        self.addCleanup(fake_scheduler_fixture.tearDown)
+        self.pushConfig(
+            codeimportscheduler_url=fake_scheduler_fixture.get_url())
+        @defer_to_thread
+        @writing_transaction
+        def get_job_id():
+            return self.dispatcher.getJobForMachine(
+                self.factory.makeCodeImportMachine(set_online=True))
+        return get_job_id().addCallback(self.assertEqual, 42)
 
 
 class TestCodeImportDispatcherDispatchJobs(
