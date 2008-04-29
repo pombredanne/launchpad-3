46d28fd12af0ad8075e1df2ff7268fdc80b2520f
Julian Edwards <julian.edwards@canonical.com>
2008-04-29 19:33:21 +0100

    SteveA's suggestions on removing all the librarian code for buildd as well.

--- lib/canonical/librarian/client.py
+++ lib/canonical/librarian/client.py
@@ -289,10 +289,7 @@ class FileDownloadClient:
             return None
         return '/%d/%s' % (aliasID, quote(lfa.filename.encode('utf-8')))
 
-    # XXX Julian 2008-04-29 Bug 224301
-    # The is_buildd hack can be removed once the Soyuz dependency on it
-    # has gone after 1.2.4.
-    def getURLForAlias(self, aliasID, is_buildd=False):
+    def getURLForAlias(self, aliasID):
         """Returns the url for talking to the librarian about the given
         alias.
 
@@ -304,8 +301,6 @@ class FileDownloadClient:
         if path is None:
             return None
         base = self.download_url
-        if is_buildd:
-            base = config.librarian.buildd_download_url
         return urljoin(base, path)
 
     def getFileByAlias(self, aliasID):
