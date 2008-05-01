0945a1175745c3d31b178bb13c15dd986868561b
Graham Binns <graham@canonical.com>
2008-05-01 12:56:39 +0100

    Added a method _extractErrorMessage to the SourceForge ExternalBugTracker. SourceForge.initializeRemoteBugDB() will no longer raise an error for private bugs.

--- lib/canonical/launchpad/components/externalbugtracker/sourceforge.py
+++ lib/canonical/launchpad/components/externalbugtracker/sourceforge.py
@@ -37,24 +37,33 @@ class SourceForge(ExternalBugTracker):
 
         for bug_id in bug_ids:
             query_url = self.export_url % bug_id
-            page_data = self._getPage(query_url)
+            page_data = self._getPage(query_url).read()
 
             soup = BeautifulSoup(page_data)
             status_tag = soup.find(text=re.compile('Status:'))
 
-            # If we can't find a status line in the output from
-            # SourceForge there's little point in continuing.
-            if not status_tag:
-                raise UnparseableBugData(
-                    'Remote bug %s does not define a status.' % bug_id)
+            status = None
+            private = False
+            if status_tag:
+                # We can extract the status by finding the grandparent tag.
+                # Happily, BeautifulSoup will turn the contents of this tag
+                # into a newline-delimited list from which we can then
+                # extract the requisite data.
+                status_row = status_tag.findParent().findParent()
+                status = status_row.contents[-1]
+                status = status.strip()
+            else:
+                error_message = self._extractErrorMessage(page_data)
 
-            # We can extract the status by finding the grandparent tag.
-            # Happily, BeautifulSoup will turn the contents of this tag
-            # into a newline-delimited list from which we can then
-            # extract the requisite data.
-            status_row = status_tag.findParent().findParent()
-            status = status_row.contents[-1]
-            status = status.strip()
+                # If the error message suggests that the bug is private,
+                # set the bug's private field to True.
+                if error_message and 'private' in error_message.lower():
+                    private = True
+                else:
+                    # If we can't find a status line in the output from
+                    # SourceForge there's little point in continuing.
+                    raise UnparseableBugData(
+                        'Remote bug %s does not define a status.' % bug_id)
 
             # We need to do the same for Resolution, though if we can't
             # find it it's not critical.
@@ -68,8 +77,30 @@ class SourceForge(ExternalBugTracker):
 
             self.bugs[int(bug_id)] = {
                 'id': int(bug_id),
+                'private': private,
                 'status': status,
-                'resolution': resolution}
+                'resolution': resolution,
+                }
+
+    def _extractErrorMessage(self, page_data):
+        """Extract an error message from a SourceForge page and return it."""
+        soup = BeautifulSoup(page_data)
+        error_frame = soup.find(attrs={'class': 'error'})
+
+        if not error_frame:
+            return None
+
+        # We find the error message by going via the somewhat shakey
+        # method of looking for the only paragraph inside the
+        # error_frame div.
+        error_message = error_frame.find(name='p')
+        if error_message:
+            # Strip out any leading or trailing whitespace and return the
+            # error message.
+            return error_message.string.strip()
+        else:
+            # We know there was an error, but we can't tell what it was.
+            return 'Unspecified error.'
 
     def getRemoteImportance(self, bug_id):
         """See `ExternalBugTracker`.
