6c0c982167050a70a0d31fed4da4f45baecb7f4a
Tim Penhey <tim.penhey@canonical.com>
2008-05-12 13:00:39 +1200

    Removed unused import.

--- lib/canonical/launchpad/browser/project.py
+++ lib/canonical/launchpad/browser/project.py
@@ -42,8 +42,7 @@ from zope.security.interfaces import Unauthorized
 
 from canonical.launchpad import _
 from canonical.launchpad.interfaces import (
-    IBranchSet, IProductSet, IProject, IProjectSeries, IProjectSet,
-    NotFoundError)
+    IProductSet, IProject, IProjectSeries, IProjectSet, NotFoundError)
 from canonical.launchpad.browser.announcement import HasAnnouncementsView
 from canonical.launchpad.browser.product import ProductAddViewBase
 from canonical.launchpad.browser.branchlisting import BranchListingView
