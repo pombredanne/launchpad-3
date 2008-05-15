81754f5cc4822c4efa85a16f5fbe15b0a6822ec2
Curtis Hovey <curtis.hovey@canonical.com>
2008-05-15 16:06:00 -0400

    Added googlesearches with good sample data to test GSP XML parsing.

--- lib/canonical/launchpad/utilities/searchservice.py
+++ lib/canonical/launchpad/utilities/searchservice.py
@@ -145,7 +145,7 @@ class GoogleSearchService:
     implements(ISearchService)
 
     _default_values = {
-        'as_rq' : 'launchpad.net',
+        'as_sitesearch' : 'launchpad.net',
         'client' : 'google-csbe',
         'cx' : None,
         'ie' : 'utf8',
@@ -189,20 +189,17 @@ class GoogleSearchService:
         # XXX sinzui 2008-05-14: finish this.
         #response = urlopen(search_url)
         #gsp_xml = response.read()
-        gsp_xml = ("""<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
-            <GSP VER="3.2">
-              <TM>0.123456</TM>
-              <Q>svg +bugs</Q>
-              <RES SN="1" EN="1">
-                <M>1</M>
-                <R N="1">
-                    <U>http://bugs.launchpad.dev/firefox/+bug/1</U>
-                    <T>SVG is broken in Firefox</T>
-                    <S>This matches because <b>svg</b> is in the title.</S>
-                </R>
-              </RES>
-            </GSP>
-            """.encode('utf8'))
+        from os import path
+        if start == 0:
+            file_name = 'googlesearchservice-bugs-1.xml'
+        else:
+            file_name = 'googlesearchservice-bugs-2.xml'
+        gsp_xml_file_name_1 = path.normpath(path.join(
+            path.dirname(__file__), '..', 'ftests', 'googlesearches',
+            file_name))
+        gsp_xml_file = open(gsp_xml_file_name_1, 'r')
+        gsp_xml = gsp_xml_file.read()
+        gsp_xml_file.close()
         page_matches = self._parse_google_search_protocol(gsp_xml)
         return page_matches
 
@@ -227,6 +224,31 @@ class GoogleSearchService:
         query_string = '&'.join(search_param_list)
         return config.google.site + '?' + query_string
 
+    def _getElementsByAttributeValue(self, doc, path, name, value):
+        """Return a list of elements whose named attribute matches the value.
+
+        The cElementTree implementation does not support attribute selection
+        (@) or conditional expressions (./PARAM[@name = 'start']).
+
+        :param doc: An ElementTree of an XML document.
+        :param path: A string path to match one or more elements.
+        :param name: The attribute name to check.
+        :param value: the string value of the named attribute.
+        """
+        elements = doc.findall(path)
+        return [element for element in elements if element.get(name) == value]
+
+    def _getElementByAttributeValue(self, doc, path, name, value):
+        """Return the first element whose named attribute matches the value.
+
+        :param doc: An ElementTree of an XML document.
+        :param path: A string path to match an element.
+        :param name: The attribute name to check.
+        :param value: The string value of the named attribute.
+        """
+        return self._getElementsByAttributeValue(doc, path, name, value)[0]
+
+
     def _parse_google_search_protocol(self, gsp_xml):
         """Return a `PageMatches` object.
 
@@ -238,10 +260,12 @@ class GoogleSearchService:
         """
         error_message =  "The xml is not Google Search Protocol version 3.2."
         gsp_doc = ET.fromstring(gsp_xml)
+        start_param = self._getElementByAttributeValue(
+            gsp_doc, './PARAM', 'name', 'start')
         results = gsp_doc.find('RES')
         try:
-            start = int(results.get('SN'))
             total = int(results.find('M').text)
+            start = int(start_param.get('value'))
         except (AttributeError, ValueError):
             # The datatype is not what PageMatches requires.
             raise GoogleWrongGSPVersion(error_message)
