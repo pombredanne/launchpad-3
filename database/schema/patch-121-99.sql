48f435fec5543e9cf587837abd8f55ce40cd8310
Michael Hudson <michael.hudson@canonical.com>
2008-05-15 17:54:12 +1200

    the patch

---
+++ database/schema/patch-121-99.sql
@@ -0,0 +1,9 @@
+SET client_min_messages=ERROR;
+
+CREATE TABLE ProductSeriesCodeImportMapping (
+    id serial PRIMARY KEY,
+    productseries integer NOT NULL UNIQUE REFERENCES ProductSeries(id));
+    codeimport integer NOT NULL UNIQUE REFERENCES CodeImport (id));
+);
+
+INSERT INTO LaunchpadDatabaseRevision VALUES (121, 99, 0);
