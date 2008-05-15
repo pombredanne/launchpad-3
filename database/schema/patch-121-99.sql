e09ca159a5eda09f5da417d69022acbb5bcd4e73
Michael Hudson <michael.hudson@canonical.com>
2008-05-15 18:07:25 +1200

    syntax, comments, permissions

--- database/schema/patch-121-99.sql
+++ database/schema/patch-121-99.sql
@@ -2,7 +2,7 @@ SET client_min_messages=ERROR;
 
 CREATE TABLE ProductSeriesCodeImportMapping (
     id serial PRIMARY KEY,
-    productseries integer NOT NULL UNIQUE REFERENCES ProductSeries(id));
+    productseries integer NOT NULL UNIQUE REFERENCES ProductSeries (id),
     codeimport integer NOT NULL UNIQUE REFERENCES CodeImport (id));
 );
 
