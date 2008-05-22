bec72bde16021582b418269cbd6833345620227e
Aaron Bentley <aaron@canonical.com>
2008-05-22 16:19:30 -0400

    Initial work on a handler

--- lib/canonical/launchpad/mail/tests/test_handlers.py
+++ lib/canonical/launchpad/mail/tests/test_handlers.py
@@ -1,13 +1,25 @@
-# Copyright 2005 Canonical Ltd.  All rights reserved.
+# Copyright 2005, 2008 Canonical Ltd.  All rights reserved.
+
+__metaclass__ = type
 
 import unittest
 
 from zope.testing.doctest import DocTestSuite
 
+from canonical.config import config
+
+
+class TestCodeHandler
+
+    def test_get():
+
+        handlers.get(config.launchpad.code_domain)
+
 
 def test_suite():
     suite = unittest.TestSuite()
-    suite.addTest(DocTestSuite('canonical.launchpad.mail.handlers'))
+    suite.addTests(DocTestSuite('canonical.launchpad.mail.handlers')
+    suite.addTests(unittest.TestLoader().loadTestsFromName(__name__))
     return suite
 
 
