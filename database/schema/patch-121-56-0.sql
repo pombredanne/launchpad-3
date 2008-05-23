8e664e53e47f0d6de70d126e894e42d22161a661
Danilo Šegan <danilo@gnome.org>
2008-05-23 13:04:03 +0200

    Rename DB patch and remove msgid_plural from NULL indexes.

--- database/schema/patch-121-56-0.sql
+++ database/schema/patch-121-56-0.sql
@@ -5,10 +5,10 @@ DROP INDEX potmsgset__potemplate__no_context__primemsgid__key;
 
 CREATE UNIQUE INDEX potmsgset__potemplate__context__msgid_singular__msgid_plural__key ON potmsgset USING btree (potemplate, context, msgid_singular, msgid_plural) WHERE (context IS NOT NULL AND msgid_plural IS NOT NULL);
 
-CREATE UNIQUE INDEX potmsgset__potemplate__context__msgid_singular__no_msgid_plural__key ON potmsgset USING btree (potemplate, context, msgid_singular, msgid_plural) WHERE (context IS NOT NULL AND msgid_plural IS NULL);
+CREATE UNIQUE INDEX potmsgset__potemplate__context__msgid_singular__no_msgid_plural__key ON potmsgset USING btree (potemplate, context, msgid_singular) WHERE (context IS NOT NULL AND msgid_plural IS NULL);
 
 CREATE UNIQUE INDEX potmsgset__potemplate__no_context__msgid_singular__msgid_plural__key ON potmsgset USING btree (potemplate, msgid_singular, msgid_plural) WHERE (context IS NULL AND msgid_plural IS NOT NULL);
 
-CREATE UNIQUE INDEX potmsgset__potemplate__no_context__msgid_singular__no_msgid_plural__key ON potmsgset USING btree (potemplate, msgid_singular, msgid_plural) WHERE (context IS NULL AND msgid_plural IS NULL);
+CREATE UNIQUE INDEX potmsgset__potemplate__no_context__msgid_singular__no_msgid_plural__key ON potmsgset USING btree (potemplate, msgid_singular) WHERE (context IS NULL AND msgid_plural IS NULL);
 
-INSERT INTO LaunchpadDatabaseRevision VALUES (121, 99, 0);
+INSERT INTO LaunchpadDatabaseRevision VALUES (121, 56, 0);
