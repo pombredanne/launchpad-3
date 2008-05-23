7d513fdad5e7d0a8c905f0c6e77f5af621392809
Danilo Šegan <danilo@gnome.org>
2008-05-23 12:56:27 +0200

    Update DB patch per stub suggestion.

--- database/schema/patch-121-99-0.sql
+++ database/schema/patch-121-99-0.sql
@@ -3,10 +3,12 @@ SET client_min_messages=ERROR;
 DROP INDEX potmsgset__potemplate__context__primemsgid__key;
 DROP INDEX potmsgset__potemplate__no_context__primemsgid__key;
 
--- XXX (Danilo, 2008-05-21): should we split these indexes into two each
--- by `msgid_plural IS NULL` as well?
-CREATE UNIQUE INDEX potmsgset__potemplate__context__msgid_singular__msgid_plural__key ON potmsgset USING btree (potemplate, context, msgid_singular, msgid_plural) WHERE (context IS NOT NULL);
+CREATE UNIQUE INDEX potmsgset__potemplate__context__msgid_singular__msgid_plural__key ON potmsgset USING btree (potemplate, context, msgid_singular, msgid_plural) WHERE (context IS NOT NULL AND msgid_plural IS NOT NULL);
 
-CREATE UNIQUE INDEX potmsgset__potemplate__no_context__msgid_singular__msgid_plural__key ON potmsgset USING btree (potemplate, msgid_singular, msgid_plural) WHERE (context IS NULL);
+CREATE UNIQUE INDEX potmsgset__potemplate__context__msgid_singular__no_msgid_plural__key ON potmsgset USING btree (potemplate, context, msgid_singular, msgid_plural) WHERE (context IS NOT NULL AND msgid_plural IS NULL);
+
+CREATE UNIQUE INDEX potmsgset__potemplate__no_context__msgid_singular__msgid_plural__key ON potmsgset USING btree (potemplate, msgid_singular, msgid_plural) WHERE (context IS NULL AND msgid_plural IS NOT NULL);
+
+CREATE UNIQUE INDEX potmsgset__potemplate__no_context__msgid_singular__no_msgid_plural__key ON potmsgset USING btree (potemplate, msgid_singular, msgid_plural) WHERE (context IS NULL AND msgid_plural IS NULL);
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (121, 99, 0);
