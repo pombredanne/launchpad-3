631a22678cc418224960a7f4a7b880aa3260f790
Jonathan Lange <jml@canonical.com>
2008-05-23 11:25:30 +0200

    Give the patch a number.

--- database/schema/patch-121-54-0.sql
+++ database/schema/patch-121-54-0.sql
@@ -6,4 +6,4 @@ ADD COLUMN stacked_on_branch integer REFERENCES Branch (id);
 CREATE INDEX branch__stacked_on_branch__idx ON Branch(stacked_on_branch)
 WHERE stacked_on_branch IS NOT NULL;
 
-INSERT INTO LaunchpadDatabaseRevision VALUES (121, 99, 0);
+INSERT INTO LaunchpadDatabaseRevision VALUES (121, 54, 0);
