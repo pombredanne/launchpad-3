fb138fc89f6fc286652ad21aa64e67ffa6137ce6
Maris Fogels <maris.fogels@canonical.com>
2008-05-23 14:38:10 -0400

    Another small tweak to the layout.

--- lib/canonical/launchpad/webapp/tales.py
+++ lib/canonical/launchpad/webapp/tales.py
@@ -2504,6 +2504,7 @@ class PageMacroDispatcher:
                 portlets=True),
         'search':
             LayoutElements(
+                actionsmenu=False,
                 applicationborder=True,
                 applicationtabs=True,
                 globalsearch=False,
