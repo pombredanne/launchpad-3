6ede5992dd3759001d99f9737239f5cee56e4afa
Muharem Hrnjadovic <muharem@canonical.com>
2008-05-23 12:58:54 +0200

    review changes

--- database/schema/patch-121-55-0.sql
+++ database/schema/patch-121-55-0.sql
@@ -1,12 +1,31 @@
 SET client_min_messages=ERROR;
 
--- This (new) link table ties a "rebuild archive" to a DistroSeries.
+-- This (new) link table ties a "rebuild archive" to a DistroSeries,
+-- captures the rebild life cycle data and statistics.
 CREATE TABLE archiverebuild (
     id serial PRIMARY KEY,
-    -- This is the "rebuild archive" in question.
+    -- This is the archive to be used for the rebuild.
     archive integer NOT NULL,
     -- This is the DistroSeries in question.
-    distroseries integer NOT NULL
+    distroseries integer NOT NULL,
+    -- The person who requested/started the rebuild.
+    registrant integer NOT NULL,
+    -- The rebuild status (in-progress, complete, cancelled, obsolete).
+    status integer NOT NULL,
+    -- The reason why this rebuild was started (one-liner).
+    reason text,
+    -- When were the rebuild statistics last updated?
+    date_updated timestamp without time zone DEFAULT timezone('UTC'::text, ('now'::text)::timestamp(6) with time zone) NOT NULL,
+    -- How many source packages are in the rebuild archive altogether?
+    total_count integer NOT NULL,
+    -- How many packages still need building?
+    pending_count integer NOT NULL,
+    -- How many source packages were build sucessfully?
+    succeeded_count integer NOT NULL,
+    -- How many packages failed to build?
+    failed_count integer NOT NULL,
+    -- How many packages are building at present?
+    building_count integer NOT NULL
 );
 
 -- A "rebuild archive" may be used only once i.e. for a single DistroSeries.
@@ -18,5 +37,10 @@ ALTER TABLE ONLY archiverebuild
 
 ALTER TABLE ONLY archiverebuild
     ADD CONSTRAINT archiverebuild__distroseries__fk FOREIGN KEY (distroseries) REFERENCES distroseries(id);
+
+ALTER TABLE ONLY archiverebuild
+    ADD CONSTRAINT archiverebuild__requestor__fk FOREIGN KEY (registrant) REFERENCES person(id);
+
+CREATE INDEX archiverebuild__registrant__idx ON archiverebuild USING btree (registrant);
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (121, 55, 0);
