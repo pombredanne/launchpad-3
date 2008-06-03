9bdc67f3772d0dcc93c022f5990b36ec1067575f
Michael Hudson <michael.hudson@canonical.com>
2008-06-04 11:54:16 +1200

    fix + bad tests

--- lib/canonical/launchpad/database/tests/test_codeimportmachine.py
+++ lib/canonical/launchpad/database/tests/test_codeimportmachine.py
@@ -10,28 +10,24 @@ from zope.component import getUtility
 
 from canonical.config import config
 from canonical.database.sqlbase import flush_database_updates
-from canonical.launchpad.ftests import logout
 from canonical.launchpad.interfaces import (
     CodeImportMachineOfflineReason, CodeImportMachineState,
     ICodeImportJobWorkflow)
-from canonical.launchpad.testing import LaunchpadObjectFactory
+from canonical.launchpad.testing import TestCaseWithFactory
 from canonical.launchpad.tests.test_codeimportjob import (
     login_for_code_imports)
 from canonical.testing import LaunchpadFunctionalLayer
 
 
-class TestCodeImportMachineShouldLookForJob(unittest.TestCase):
+class TestCodeImportMachineShouldLookForJob(TestCaseWithFactory):
     """Tests for  `CodeImportMachine.shouldLookForJob`."""
 
     layer = LaunchpadFunctionalLayer
 
     def setUp(self):
+        super(TestCodeImportMachineShouldLookForJob, self).setUp()
         login_for_code_imports()
-        self.factory = LaunchpadObjectFactory()
         self.machine = self.factory.makeCodeImportMachine(set_online=True)
-
-    def tearDown(self):
-        logout()
 
     def createJobRunningOnMachine(self, machine):
         """Create a job in the database and mark it as running on `machine`.
@@ -48,7 +44,7 @@ class TestCodeImportMachineShouldLookForJob(unittest.TestCase):
     def test_machineIsQuiescingNoJobsRunning(self):
         # When the machine is quiescing and no jobs are running on this
         # machine, we should set the machine to OFFLINE and not look for jobs.
-        self.machine.setQuiescing(self.factory.makePerson(), "reason")
+        self.machine.setQuiescing(self.factory.makePerson())
         self.assertFalse(self.machine.shouldLookForJob())
         self.assertEqual(self.machine.state, CodeImportMachineState.OFFLINE)
 
@@ -56,7 +52,7 @@ class TestCodeImportMachineShouldLookForJob(unittest.TestCase):
         # When the machine is quiescing and there are jobs running on this
         # machine, we shouldn't look for any more jobs.
         self.createJobRunningOnMachine(self.machine)
-        self.machine.setQuiescing(self.factory.makePerson(), "reason")
+        self.machine.setQuiescing(self.factory.makePerson())
         self.assertFalse(self.machine.shouldLookForJob())
         self.assertEqual(self.machine.state, CodeImportMachineState.QUIESCING)
 
@@ -71,6 +67,28 @@ class TestCodeImportMachineShouldLookForJob(unittest.TestCase):
         # If the machine is online and there are not already
         # max_jobs_per_machine jobs running, then we should look for a job.
         self.assertTrue(self.machine.shouldLookForJob())
+
+    def test_noHeartbeatUpdateWhenOffline(self):
+        # When the machine is offline, the heartbeat is not updated.
+        # Guardedly check that the machine was created with a NULL heartbeat.
+        self.assertTrue(self.machine.heartbeat is None)
+        self.machine.setOffline(CodeImportMachineOfflineReason.STOPPED)
+        self.machine.shouldLookForJob()
+
+    def test_heartbeatUpdateWhenQuiescing(self):
+        # When the machine is quiescing, the heartbeat is updated.
+        # Guardedly check that the machine was created with a NULL heartbeat.
+        self.assertTrue(self.machine.heartbeat is None)
+        self.machine.setQuiescing(self.factory.makePerson())
+        self.machine.shouldLookForJob()
+        self.assertTrue(self.machine.heartbeat is not None)
+
+    def test_heartbeatUpdateWhenOnline(self):
+        # When the machine is online, the heartbeat is updated.
+        # Guardedly check that the machine was created with a NULL heartbeat.
+        self.assertTrue(self.machine.heartbeat is None)
+        self.machine.shouldLookForJob()
+        self.assertTrue(self.machine.heartbeat is not None)
 
 
 def test_suite():
