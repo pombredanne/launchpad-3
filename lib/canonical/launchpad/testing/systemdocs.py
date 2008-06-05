7fc868a28ff7d48631027836e87544332d1841f0
James Henstridge <james.henstridge@canonical.com>
2008-06-05 10:34:22 +0800

    Don't put get_transaction_timestamp() in the default globals -- we already have too many of them.
    Add a few transaction.commit/abort calls to doc/distroseries.txt to fix 
    synchronisation errors when running external scripts.

--- lib/canonical/launchpad/testing/systemdocs.py
+++ lib/canonical/launchpad/testing/systemdocs.py
@@ -23,8 +23,7 @@ from zope.testing.loggingsupport import Handler
 
 from canonical.chunkydiff import elided_source
 from canonical.config import config
-from canonical.database.sqlbase import (
-    flush_database_updates, get_transaction_timestamp)
+from canonical.database.sqlbase import flush_database_updates
 from canonical.launchpad.ftests import ANONYMOUS, login, logout
 from canonical.launchpad.interfaces import ILaunchBag
 from canonical.launchpad.layers import setFirstLayer
@@ -176,7 +175,6 @@ def setGlobs(test):
     test.globs['flush_database_updates'] = flush_database_updates
     test.globs['create_view'] = create_view
     test.globs['LaunchpadObjectFactory'] = LaunchpadObjectFactory
-    test.globs['get_transaction_timestamp'] = get_transaction_timestamp
 
 
 def setUp(test):
