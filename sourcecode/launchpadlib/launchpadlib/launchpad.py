462833839cc32a2ff2e309861ba18de5d440a28d
Barry Warsaw <barry@canonical.com>
2008-06-06 17:32:59 -0400

    Merge to RF, requiring moving create_team() to newTeam() and placing this on the People collection class.

--- sourcecode/launchpadlib/launchpadlib/launchpad.py
+++ sourcecode/launchpadlib/launchpadlib/launchpad.py
@@ -60,27 +60,3 @@ class Launchpad:
         if self._bug_set_link is None:
             return None
         return _FakeBugCollection(self._browser, URI(self._bug_set_link))
-
-    def create_team(self, name, display_name):
-        """Create a new team.
-
-        :param name: The name of the team
-        :type name: string
-        :param display_name: The 'display name' of the team
-        :type display_name: string
-        :return: the new team
-        :rtype: `Entry`
-        :raises ResponseError: when an unexpected response occurred.
-        """
-        url = self._root.append('people')
-        # If the team got created, a 201 status will be returned.  When that
-        # happens, we dig the 'Location' header out of the response and create
-        # a new Person instance with that base url.
-        response, content = self._browser.post(
-            url, 'create_team', name=name, display_name=display_name)
-        if response.status == 201:
-            # We know this has to be a person, so create and return the
-            # appropriate instance.
-            data = self._browser.get(URI(response['location']))
-            return Entry(data)
-        raise UnexpectedResponseError(response, content)
--- sourcecode/launchpadlib/launchpadlib/person.py
+++ sourcecode/launchpadlib/launchpadlib/person.py
@@ -8,6 +8,7 @@ __all__ = [
     ]
 
 
+from launchpadlib._utils.uri import URI
 from launchpadlib.collection import Collection, Entry
 from launchpadlib.errors import HTTPError
 
@@ -33,3 +34,26 @@ class People(Collection):
                 return default
             raise
         return Entry(data)
+
+    def newTeam(self, name, display_name):
+        """Create a new team.
+
+        :param name: The name of the team
+        :type name: string
+        :param display_name: The 'display name' of the team
+        :type display_name: string
+        :return: the new team
+        :rtype: `Entry`
+        :raises ResponseError: when an unexpected response occurred.
+        """
+        # If the team got created, a 201 status will be returned.  When that
+        # happens, we dig the 'Location' header out of the response and create
+        # a new Person instance with that base url.
+        response, content = self._browser.post(
+            self._base_url, 'newTeam', name=name, display_name=display_name)
+        if response.status == 201:
+            # We know this has to be a person, so create and return the
+            # appropriate instance.
+            data = self._browser.get(URI(response['location']))
+            return Entry(data)
+        raise UnexpectedResponseError(response, content)
