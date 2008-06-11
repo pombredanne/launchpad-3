5f26c16cc250fd40d5671d9159a76a1ddb3422a7
Curtis Hovey <curtis.hovey@canonical.com>
2008-06-11 18:33:51 -0400

    Simplified the OpenIDRPSummary schema change to a single data to reduce the complexity of the update.

--- database/schema/patch-121-97-0.sql
+++ database/schema/patch-121-97-0.sql
@@ -3,76 +3,32 @@ SET client_min_messages=ERROR;
 -- Update the schema to store information about the Relying Parties (RP)
 -- used by Persons.
 
-
--- Create a table to represent the basic RP.
--- It becomes the base for OpenIDRelyingParty, so the trust_root is
--- copied into the new table.
-CREATE TABLE OpenIDRelyingParty (
-    id SERIAL PRIMARY KEY,
-    trust_root TEXT NOT NULL,
-    date_created TIMESTAMP WITHOUT TIME ZONE
-        DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC') NOT NULL
-);
-
-INSERT INTO OpenIDRelyingParty (date_created, trust_root)
-SELECT CURRENT_TIMESTAMP AT TIME ZONE 'UTC', trust_root
-FROM OpenIDRPConfig;
-
-
--- Add a column to create a relationship between the basic table and
--- the config table. update the OpenIDRelyingParty id and set the
--- column to not null.
--- XXX sinzui 2008-06-11: We should drop the trust_root column, but I
--- chose to defer that until after the Launchpad has adopted the new
--- OpenID identifier.
-ALTER TABLE OpenIDRPConfig
-    ADD COLUMN relying_party INTEGER
-        REFERENCES OpenIDRelyingParty(id);
-
-UPDATE OpenIDRPConfig
-SET relying_party = OpenIDRelyingParty.id
-FROM OpenIDRelyingParty
-WHERE OpenIDRPConfig.trust_root = OpenIDRelyingParty.trust_root;
-
--- XXX sinzui 2008-06-11: relying_party should never be null, but
--- updating the schema without a concurrent code change is fatal.
---ALTER TABLE OpenIDRPConfig
---    ALTER COLUMN relying_party SET NOT NULL;
-
-
--- Add a column to OpenIDAuthorization to link the current authorizations
--- to the base RP. This table is not used at this time so no other migration
--- is need to the DB.
-ALTER TABLE OpenIDAuthorization
-    ADD COLUMN relying_party INTEGER
-        REFERENCES OpenIDRelyingParty(id);
-
--- XXX sinzui 2008-06-11: relying_party should never be null, but
--- updating the schema without a concurrent code change is fatal.
---ALTER TABLE OpenIDRPConfig
---    ALTER COLUMN relying_party SET NOT NULL;
-
+-- XXX sinzui 2008-06-11: I considered replacing the trust_root column
+-- in OpenIDAuthorizations and OpenIDRPConfig with a FK to a new RP entity.
+-- I chose to defer that until after the Launchpad has adopted the new
+-- OpenID identifier. Normalising the data entails more work than I think
+-- we want to undertake for Launchpad 2.0. I'm not certain an RP object
+-- viable either.
 
 -- Create a table to log person-relying party relationships.
--- XXX sinzui 2008-06-11: OpenIDAuthorization overlaps this table.
--- The date_last_used and login_count might be a method (query)
--- or a view.
-CREATE TABLE OpenIDRPLog (
+-- This table overlaps OpenIDAuthorization, where it is unlikely to
+-- contain a transient authorizations, this table will.
+CREATE TABLE OpenIDRPSummary (
     id SERIAL PRIMARY KEY,
-    relying_party INTEGER NOT NULL
-        REFERENCES OpenIDRelyingParty(id),
     person INTEGER NOT NULL
         REFERENCES Person(id),
+    trust_root text NOT NULL,
     date_created TIMESTAMP without time zone
         DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC') NOT NULL,
     date_last_used TIMESTAMP without time zone
         DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC') NOT NULL,
-    login_count INTEGER NOT NULL
+    last_client_id text,
+    total_logins INTEGER NOT NULL
         DEFAULT 1
 );
 
-CREATE UNIQUE INDEX openidrplog__relying_party__person__unq
-  ON openidrplog USING btree (relying_party, person);
+CREATE UNIQUE INDEX openidrpsummary__relying_party__person__unq
+  ON openidrpsummary USING btree (person, trust_root);
 
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (121, 97, 0);
