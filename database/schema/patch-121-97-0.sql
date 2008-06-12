046780409321cc56016ba639cc02e42a92f5f44e
Curtis Hovey <curtis.hovey@canonical.com>
2008-06-12 10:55:28 -0400

    Revised SQL patch per comment from Stuart.

--- database/schema/patch-121-97-0.sql
+++ database/schema/patch-121-97-0.sql
@@ -3,32 +3,30 @@ SET client_min_messages=ERROR;
 -- Update the schema to store information about the Relying Parties (RP)
 -- used by Persons.
 
--- XXX sinzui 2008-06-11: I considered replacing the trust_root column
--- in OpenIDAuthorizations and OpenIDRPConfig with a FK to a new RP entity.
--- I chose to defer that until after the Launchpad has adopted the new
--- OpenID identifier. Normalising the data entails more work than I think
--- we want to undertake for Launchpad 2.0. I'm not certain an RP object
--- viable either.
-
--- Create a table to log person-relying party relationships.
--- This table overlaps OpenIDAuthorization, where it is unlikely to
--- contain a transient authorizations, this table will.
+-- Create a table to record person-relying party relationships.
 CREATE TABLE OpenIDRPSummary (
     id SERIAL PRIMARY KEY,
     person INTEGER NOT NULL
         REFERENCES Person(id),
     trust_root text NOT NULL,
+    identifier text NOT NULL,
     date_created TIMESTAMP without time zone
         DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC') NOT NULL,
     date_last_used TIMESTAMP without time zone
         DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC') NOT NULL,
-    last_client_id text,
     total_logins INTEGER NOT NULL
-        DEFAULT 1
+        DEFAULT 1,
+    CONSTRAINT openidrpsummary__person__trust_root__identifier__key
+        UNIQUE (person, trust_root, identifier)
 );
 
-CREATE UNIQUE INDEX openidrpsummary__relying_party__person__unq
-  ON openidrpsummary USING btree (person, trust_root);
+CREATE UNIQUE INDEX openidrpsummary__person__idx
+    ON openidrpsummary USING btree (person);
 
+CREATE UNIQUE INDEX openidrpsummary__trust_root__idx
+    ON openidrpsummary USING btree (trust_root);
+
+CREATE UNIQUE INDEX openidrpsummary__identifier__idx
+    ON openidrpsummary USING btree (identifier);
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (121, 97, 0);
