f70ff0cd1112e8fd432219a30f902c133c4b1571
Abel Deuring <abel.deuring@canonical.com>
2008-06-16 16:14:07 +0200

    fixed DB version in patch-121-60-0.sql

--- database/schema/patch-121-60-0.sql
+++ database/schema/patch-121-60-0.sql
@@ -4,4 +4,4 @@ SET client_min_messages=ERROR;
 
 ALTER TABLE HWSubmissionDevice DROP CONSTRAINT hwsubmissiondevice__devicer_driver_link__submission__key;
 
-INSERT INTO LaunchpadDatabaseRevision VALUES (121, 99, 0);
+INSERT INTO LaunchpadDatabaseRevision VALUES (121, 60, 0);
