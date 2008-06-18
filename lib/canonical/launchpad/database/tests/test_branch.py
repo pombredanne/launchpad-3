6aa5394dcabed58c240e43d35cdc0f193d3f453d
Tim Penhey <tim.penhey@canonical.com>
2008-06-19 11:15:25 +1200

    Do some renames of CodeReviewMessage -> CodeReviewComment.

--- lib/canonical/launchpad/database/tests/test_branch.py
+++ lib/canonical/launchpad/database/tests/test_branch.py
@@ -29,7 +29,7 @@ from canonical.launchpad.database.branchmergeproposal import (
     )
 from canonical.launchpad.database.bugbranch import BugBranch
 from canonical.launchpad.database.codeimport import CodeImport, CodeImportSet
-from canonical.launchpad.database.codereviewmessage import CodeReviewMessage
+from canonical.launchpad.database.codereviewcomment import CodeReviewComment
 from canonical.launchpad.database.product import ProductSet
 from canonical.launchpad.database.revision import RevisionSet
 from canonical.launchpad.database.specificationbranch import (
@@ -306,23 +306,23 @@ class TestBranchDeletionConsequences(TestCase):
         merge_proposal1.dependent_branch.destroySelf(break_references=True)
         self.assertEqual(None, merge_proposal1.dependent_branch)
 
-    def test_deleteSourceCodeReviewMessage(self):
-        """Deletion of branches that have CodeReviewMessages works."""
-        comment = self.factory.makeCodeReviewMessage()
+    def test_deleteSourceCodeReviewComment(self):
+        """Deletion of branches that have CodeReviewComments works."""
+        comment = self.factory.makeCodeReviewComment()
         comment_id = comment.id
         branch = comment.branch_merge_proposal.source_branch
         branch.destroySelf(break_references=True)
         self.assertRaises(
-            SQLObjectNotFound, CodeReviewMessage.get, comment_id)
+            SQLObjectNotFound, CodeReviewComment.get, comment_id)
 
-    def test_deleteTargetCodeReviewMessage(self):
-        """Deletion of branches that have CodeReviewMessages works."""
-        comment = self.factory.makeCodeReviewMessage()
+    def test_deleteTargetCodeReviewComment(self):
+        """Deletion of branches that have CodeReviewComments works."""
+        comment = self.factory.makeCodeReviewComment()
         comment_id = comment.id
         branch = comment.branch_merge_proposal.target_branch
         branch.destroySelf(break_references=True)
         self.assertRaises(
-            SQLObjectNotFound, CodeReviewMessage.get, comment_id)
+            SQLObjectNotFound, CodeReviewComment.get, comment_id)
 
     def test_branchWithSubscriptionReqirements(self):
         """Deletion requirements for a branch with subscription are right."""
