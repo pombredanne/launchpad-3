5465127256cda16ebfbd663f82efb947b895087b
Guilherme Salgado <salgado@canonical.com>
2008-06-30 16:28:09 -0300

    Fix bug 244282 by creating a new method used as the collection's default content.

--- lib/canonical/launchpad/interfaces/person.py
+++ lib/canonical/launchpad/interfaces/person.py
@@ -1653,6 +1653,10 @@ class IPersonSet(Interface):
         default ordering specified in Person._defaultOrder.
         """
 
+    @collection_default_content()
+    def getAllValidPersonsAndTeams():
+        """Return all valid persons and teams."""
+
     def updateStatistics(ztm):
         """Update statistics caches and commit."""
 
@@ -1666,7 +1670,6 @@ class IPersonSet(Interface):
            statistics update.
         """
 
-    @collection_default_content()
     @operation_parameters(
         text=TextLine(title=_("Search text"), default=u""))
     @export_read_operation()
