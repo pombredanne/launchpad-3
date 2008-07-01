4a67387489bb79ec595fabcaa0d8658509c24185
Brad Crittenden <bac@canonical.com>
2008-07-01 17:36:29 -0400

    Add decorated classes for ProductSeries to view classes to reduce database accesses.

--- lib/canonical/launchpad/database/productrelease.py
+++ lib/canonical/launchpad/database/productrelease.py
@@ -8,7 +8,7 @@ from zope.interface import implements
 
 from sqlobject import ForeignKey, StringCol, SQLMultipleJoin, AND
 
-from canonical.database.sqlbase import SQLBase
+from canonical.database.sqlbase import SQLBase, sqlvalues
 from canonical.database.constants import UTC_NOW
 from canonical.database.datetimecol import UtcDateTimeCol
 from canonical.database.enumcol import EnumCol
@@ -141,3 +141,21 @@ class ProductReleaseSet(object):
         if productrelease is None:
             return default
         return productrelease
+
+    def getReleasesForSerieses(self, serieses):
+        """See `IProductReleaseSet`."""
+        if len(list(serieses)) == 0:
+            return ProductRelease.select('1 = 2')
+        return ProductRelease.select("""
+            ProductRelease.productseries IN %s
+            """ % sqlvalues([series.id for series in serieses]),
+            orderBy='-datereleased')
+
+    def getFilesForReleases(self, releases):
+        """See `IProductReleaseSet`."""
+        if len(list(releases)) == 0:
+            return ProductReleaseFile.select('1 = 2')
+        return ProductReleaseFile.select("""
+            ProductReleaseFile.productrelease IN %s
+            """ % sqlvalues([release.id for release in releases]),
+            orderBy='-date_uploaded')
--- lib/canonical/launchpad/interfaces/productrelease.py
+++ lib/canonical/launchpad/interfaces/productrelease.py
@@ -149,6 +149,7 @@ class IProductRelease(Interface):
 
 class IProductReleaseFile(Interface):
 
+    id = Int(title=_('ID'), required=True, readonly=True)
     productrelease = Choice(title=_('Project release'), required=True,
                             vocabulary='ProductRelease')
     libraryfile = Object(schema=ILibraryFileAlias, title=_("File"),
@@ -193,3 +194,9 @@ class IProductReleaseSet(Interface):
 
         If no release is found, default will be returned.
         """
+
+    def getReleasesForSerieses(serieses):
+        """Get all releases for the serieses."""
+
+    def getFilesForReleases(releases):
+        """Get all files for the releases."""
