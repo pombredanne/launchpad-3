217224f0fb955d67fe3af2e8c7a6b1a573a32a91
Francis J. Lacoste <francis.lacoste@canonical.com>
2008-07-14 17:45:13 -0400

    Start/stop mailman in the MailmanLayer.

--- lib/canonical/launchpad/mailman/testing/layers.py
+++ lib/canonical/launchpad/mailman/testing/layers.py
@@ -7,22 +7,23 @@ __all__ = [
     'MailmanLayer',
     ]
 
+import atexit
 
 from canonical.testing.layers import AppServerLayer
-
+from canonical.launchpad.mailman.runmailman import start_mailman, stop_mailman
 
 class MailmanLayer(AppServerLayer):
-    """A marker layer for the Mailman integration tests."""
-
-    # Make sure the base class methods are not called.
+    """A layer for the Mailman integration tests."""
 
     @classmethod
     def setUp(cls):
-        pass
+        start_mailman(quiet=True, config=AppServerLayer.appserver_config)
+        # Make sure that mailman is killed even if tearDown() is skipped.
+        atexit.register(cls.tearDown)
 
     @classmethod
     def tearDown(cls):
-        pass
+        stop_mailman(quiet=True, config=AppServerLayer.appserver_config)
 
     @classmethod
     def testSetUp(cls):
