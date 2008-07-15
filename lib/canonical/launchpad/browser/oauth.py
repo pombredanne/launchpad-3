3f81ae271a3243560873eab91f89283357b9b520
Guilherme Salgado <salgado@canonical.com>
2008-07-15 16:25:20 -0300

    Make +oauth-authorize honor the context specified by the consumer and display the context when managing a person's tokens.

--- lib/canonical/launchpad/browser/oauth.py
+++ lib/canonical/launchpad/browser/oauth.py
@@ -10,13 +10,17 @@ __all__ = [
 from zope.component import getUtility
 from zope.formlib.form import Action, Actions
 
-from canonical.launchpad.interfaces import (
+from canonical.launchpad.interfaces.distribution import IDistributionSet
+from canonical.launchpad.interfaces.oauth import (
     IOAuthConsumerSet, IOAuthRequestToken, IOAuthRequestTokenSet,
     OAUTH_CHALLENGE)
+from canonical.launchpad.interfaces.product import IProductSet
+from canonical.launchpad.interfaces.project import IProjectSet
 from canonical.launchpad.webapp import LaunchpadFormView, LaunchpadView
 from canonical.launchpad.webapp.authentication import (
     check_oauth_signature, get_oauth_authorization)
-from canonical.launchpad.webapp.interfaces import OAuthPermission
+from canonical.launchpad.webapp.interfaces import (
+    OAuthPermission, UnexpectedFormData)
 
 
 class OAuthRequestTokenView(LaunchpadView):
@@ -77,13 +81,56 @@ class OAuthAuthorizeTokenView(LaunchpadFormView):
     token = None
 
     def initialize(self):
-        key = self.request.form.get('oauth_token')
+        form = self.request.form
+        product = form.get('lp.product')
+        project = form.get('lp.project')
+        distro = form.get('lp.distribution')
+        package = form.get('lp.sourcepackagename')
+        if product:
+            if project or distro or package:
+                raise UnexpectedFormData("More than one context specified.")
+            self.token_context = getUtility(IProductSet)[product]
+            self.token_context_params = dict(product=self.token_context)
+        elif project:
+            if product or distro or package:
+                raise UnexpectedFormData("More than one context specified.")
+            self.token_context = getUtility(IProjectSet)[project]
+            self.token_context_params = dict(project=self.token_context)
+        elif package:
+            if product or project:
+                raise UnexpectedFormData("More than one context specified.")
+            elif not distro:
+                raise UnexpectedFormData(
+                    "Must specify the package's distribution.")
+            else:
+                distro = getUtility(IDistributionSet)[distro]
+                distro_source_package = distro.getSourcePackage(package)
+                self.token_context = distro_source_package
+                self.token_context_params = dict(
+                    distribution=distro,
+                    sourcepackagename=distro_source_package.sourcepackagename)
+        elif distro:
+            if product or project:
+                raise UnexpectedFormData("More than one context specified.")
+            self.token_context = getUtility(IDistributionSet)[distro]
+            self.token_context_params = dict(distribution=self.token_context)
+        else:
+            # No context specified.
+            self.token_context = None
+            self.token_context_params = {}
+
+        # XXX: Just like in other OAuth requests, the parameters here may be
+        # in the Authorization header, so we must use
+        # get_oauth_authorization() here instead of getting the token from the
+        # request's form.  I'm not sure it'd make sense to have the context in
+        # the Authorization header, though.
+        key = form.get('oauth_token')
         if key:
             self.token = getUtility(IOAuthRequestTokenSet).getByKey(key)
         super(OAuthAuthorizeTokenView, self).initialize()
 
     def reviewToken(self, permission):
-        self.token.review(self.user, permission)
+        self.token.review(self.user, permission, **self.token_context_params)
         callback = self.request.form.get('oauth_callback')
         if callback:
             self.next_url = callback
