e1bc5a4fcbb79fcb7588f6db72c5576b48fddb8f
Jeroen Vermeulen <jeroen.vermeulen@canonical.com>
2008-07-16 19:35:07 +0700

    Made options-printing test helper a bit more flexible.

--- lib/canonical/launchpad/browser/potemplate.py
+++ lib/canonical/launchpad/browser/potemplate.py
@@ -198,6 +198,7 @@ class POTemplateAppMenus(ApplicationMenu):
         text = 'Download translations'
         return Link('+export', text, icon='download')
 
+    @enabled_with_permission('launchpad.Edit')
     def edit(self):
         text = 'Change details'
         return Link('+edit', text, icon='edit')
