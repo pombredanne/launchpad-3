b0ae5fb26f02dd15a381137277782f2e3fdb5207
Guilherme Salgado <salgado@canonical.com>
2008-07-16 12:27:11 -0300

    A few more tests as Francis suggested.

--- lib/canonical/launchpad/tests/test_webapp_authorization.py
+++ lib/canonical/launchpad/tests/test_webapp_authorization.py
@@ -14,7 +14,7 @@ from canonical.launchpad.webapp.authorization import LaunchpadSecurityPolicy
 from canonical.launchpad.webapp.interfaces import AccessLevel
 
 
-class TestWebappAuthorization_getPrincipalsAccessLevel(unittest.TestCase):
+class TestLaunchpadSecurityPolicy_getPrincipalsAccessLevel(unittest.TestCase):
     layer = ZopelessLayer
 
     def setUp(self):
@@ -23,6 +23,7 @@ class TestWebappAuthorization_getPrincipalsAccessLevel(unittest.TestCase):
         self.security = LaunchpadSecurityPolicy()
 
     def test_no_scope(self):
+        """Principal's access level is used when no scope is given."""
         self.principal.access_level = AccessLevel.WRITE_PUBLIC
         self.principal.scope = None
         self.failUnlessEqual(
@@ -31,6 +32,7 @@ class TestWebappAuthorization_getPrincipalsAccessLevel(unittest.TestCase):
             self.principal.access_level)
 
     def test_object_within_scope(self):
+        """Principal's access level is used when object is within scope."""
         obj = LoneObject()
         self.principal.access_level = AccessLevel.WRITE_PUBLIC
         self.principal.scope = obj
@@ -39,10 +41,22 @@ class TestWebappAuthorization_getPrincipalsAccessLevel(unittest.TestCase):
             self.principal.access_level)
 
     def test_object_not_within_scope(self):
+        """READ_PUBLIC is used when object is /not/ within scope."""
         obj = LoneObject()
-        obj2 = LoneObject()
-        self.principal.access_level = AccessLevel.WRITE_PUBLIC
+        obj2 = LoneObject()  # This is out of obj's scope.
         self.principal.scope = obj
+
+        self.principal.access_level = AccessLevel.WRITE_PUBLIC
+        self.failUnlessEqual(
+            self.security._getPrincipalsAccessLevel(self.principal, obj2),
+            AccessLevel.READ_PUBLIC)
+
+        self.principal.access_level = AccessLevel.READ_PRIVATE
+        self.failUnlessEqual(
+            self.security._getPrincipalsAccessLevel(self.principal, obj2),
+            AccessLevel.READ_PUBLIC)
+
+        self.principal.access_level = AccessLevel.WRITE_PRIVATE
         self.failUnlessEqual(
             self.security._getPrincipalsAccessLevel(self.principal, obj2),
             AccessLevel.READ_PUBLIC)
