fce7e02d7210f4b90e3adb962f88adbedd54883f
Stuart Bishop <stuart.bishop@canonical.com>
2008-07-24 19:07:22 +0700

    Initial load balancing dbpolicy

--- lib/canonical/launchpad/webapp/dbpolicy.py
+++ lib/canonical/launchpad/webapp/dbpolicy.py
@@ -1,16 +1,15 @@
 # Copyright 2008 Canonical Ltd.  All rights reserved.
 
-"""Launchpad IDatabaseInteractionPolicy.
-
-The policy connects our Storm stores to either master or replica
-databases based on the type of request or if read only mode is in operation.
-"""
+"""Launchpad IDatabaseInteractionPolicy."""
 
 __metaclass__ = type
 __all__ = [
         'LaunchpadDatabasePolicy',
         ]
 
+from datetime import datetime, timedelta
+
+from zope.app.session.interfaces import ISession
 from zope.interface import implements
 
 import canonical.launchpad.webapp.adapter as da
@@ -30,17 +29,27 @@ class LaunchpadDatabasePolicy:
         This method is invoked by
         LaunchpadBrowserPublication.beforeTraversal()
         
-        The policy connects our Storm stores to either master or replica
-        databases based on the type of request or if read only mode is in
-        operation.
+        The policy connects our Storm stores to either master or
+        replica databases.
         """
+        # Detect if this is a read only request or not.
         self.read_only = self.request.method in ['GET', 'HEAD']
 
-        # Select the default Store. Talking directly to the implementation
-        # rather than looking up via the IStoreSelector interface as
-        # setDefaultFlavor isn't part of that interface.
+        # Select if the DEFAULT_FLAVOR Store will be the master or a
+        # slave. We select slave if this is a readonly request, and
+        # only readonly requests have been made by this user recently.
+        # This ensures that a user will see any changes they just made
+        # on the master, despite the fact it might take a while for
+        # those changes to propagate to the slave databases.
         if self.read_only:
-            da.StoreSelector.setDefaultFlavor(SLAVE_FLAVOR)
+            session_data = ISession(self.request)['lp.dbpolicy']
+            last_write = session_data.get('last_write', None)
+            now = datetime.utcnow()
+            # 'recently' is hardcoded at 5 minutes.
+            if last_write is None or last_write < now - timedelta(minutes=5):
+                da.StoreSelector.setDefaultFlavor(SLAVE_FLAVOR)
+            else:
+                da.StoreSelector.setDefaultFlavor(MASTER_FLAVOR)
         else:
             da.StoreSelector.setDefaultFlavor(MASTER_FLAVOR)
 
@@ -49,5 +58,12 @@ class LaunchpadDatabasePolicy:
         
         This method is invoked by LaunchpadBrowserPublication.endRequest.
         """
-        da.StoreSelector.setDefaultFlavor(MASTER_FLAVOR)
+        if not self.read_only:
+            # A non-readonly request has been made. Store this fact
+            # in the session. Precision is hard coded at 1 minute.
+            session_data = ISession(self.request)['lp.dbpolicy']
+            last_write = session_data.get('last_write', None)
+            now = datetime.utcnow()
+            if last_write is None or last_write < now - timedelta(minutes=1):
+                session_data['last_write'] = now
 
