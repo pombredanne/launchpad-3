80b567ae0eca92e4252eb4ee67348ff142662faf
Michael Hudson <michael.hudson@canonical.com>
2008-08-05 11:47:33 +1200

    add IHostedBranchStorage to canonical.launchpad.interfaces.codehosting

--- lib/canonical/launchpad/interfaces/codehosting.py
+++ lib/canonical/launchpad/interfaces/codehosting.py
@@ -81,3 +81,82 @@ class IBranchDetailsStorage(Interface):
             tuple.
         :returns: True if the ScriptActivity record was successfully inserted.
         """
+
+
+class IHostedBranchStorage(Interface):
+    """An interface for dealing with hosted branches in Launchpad.
+
+    Published at `...`.
+
+    The code hosting service uses this to register branches, to retrieve
+    information about a user's branches, and to update their status.
+    """
+
+    def getBranchesForUser(personID):
+        """Return all branches owned by a particular user, grouped by product.
+
+        :returns: a list like::
+            [(product id, product name, [(branch id, branch name), ...]), ...]
+        """
+
+    def getBranchInformation(loginID, personName, productName, branchName):
+        """Return the database ID and permissions for a branch.
+
+        :param loginID: The login ID for the person asking for the branch
+            information. This is used for branch privacy checks.
+        :param personName: The owner of the branch.
+        :param productName: The product that the branch belongs to. '+junk' is
+            allowed.
+        :param branchName: The name of the branch.
+
+        :returns: (branch_id, permissions), where 'permissions' is 'w' if the
+            user represented by 'loginID' can write to the branch, and 'r' if
+            they cannot. If the branch doesn't exist, return ('', '').
+        """
+
+    def getDefaultStackedOnBranch(login_id, product_name):
+        """Return the URL for the default stacked-on branch of a product.
+
+        :param login_id: The login ID for the person asking for the branch
+            information. This is used for branch privacy checks.
+        :param product_name: The name of a `Product`.
+        :return: An absolute path to a branch on Launchpad. If there is no
+            default stacked-on branch configured, return the empty string.
+        """
+
+    def fetchProductID(productName):
+        """Return the database ID for a product name.
+
+        :returns: a product ID.
+        """
+
+    def createBranch(loginID, personName, productName, branchName):
+        """Register a new hosted branch in Launchpad.
+
+        This is called by the bazaar.launchpad.net server when a user pushes a
+        new branch to it.  See also
+        https://launchpad.canonical.com/SupermirrorFilesystemHierarchy.
+
+        Note that this function raises instances of exactly
+        twisted.web.xmlrpc.Fault; while raising subclasses would perhaps be
+        clearer, the client side would only see a Fault, so we do that on the
+        server side too for consistency.
+
+        :param loginID: the person ID of the user creating the branch.
+        :param personName: the unique name of the owner of the branch.
+        :param productName: the unique name of the product that the branch
+            belongs to.
+        :param branchName: the name for this branch, to be used in URLs.
+        :returns: the ID for the new branch.
+        :raises twisted.web.xmlrpc.Fault: If the branch cannot be created.
+            The faultCode will be PERMISSION_DENIED_FAULT_CODE or
+            NOT_FOUND_FAULT_CODE and the faultString will be a description
+            suitable to display to the user.
+        """
+
+    def requestMirror(loginID, branchID):
+        """Mark a branch as needing to be mirrored.
+
+        :param loginID: the person ID of the user requesting the mirror.
+        :param branchID: a branch ID.
+        """
