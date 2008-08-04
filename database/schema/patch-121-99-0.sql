c07089d3b125a9a82b99cc076b8f24372381554f
Brad Crittenden <bac@canonical.com>
2008-08-04 09:11:16 -0400

    Added registrant to project table.

--- database/schema/patch-121-99-0.sql
+++ database/schema/patch-121-99-0.sql
@@ -16,4 +16,20 @@ ALTER TABLE Product
 CREATE INDEX product__registrant__idx
   ON Product(registrant);
 
+ALTER TABLE Project
+    ADD COLUMN registrant int REFERENCES Person;
+
+-- Unfortunately we can't recreate who originally registered the
+-- project, so defaulting to the owner will have to
+-- do.
+UPDATE Project
+SET registrant = owner;
+
+ALTER TABLE Project
+  ALTER COLUMN registrant SET NOT NULL;
+
+-- Create an index.
+CREATE INDEX project__registrant__idx
+  ON Project(registrant);
+
 INSERT INTO LaunchpadDatabaseRevision VALUES (121, 99, 0);
