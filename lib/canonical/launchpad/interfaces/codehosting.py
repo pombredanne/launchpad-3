90abd11481dc9a1903a4aa7370e1d77e1f08194d
Michael Hudson <michael.hudson@canonical.com>
2008-08-05 11:45:13 +1200

    uncomment the branch details methods in the interface

--- lib/canonical/launchpad/interfaces/codehosting.py
+++ lib/canonical/launchpad/interfaces/codehosting.py
@@ -35,49 +35,49 @@ class IBranchDetailsStorage(Interface):
         is '+junk' if there is no product associated with the branch.
         """
 
-    ## def startMirroring(branchID):
-    ##     """Notify Launchpad that the given branch has started mirroring.
+    def startMirroring(branchID):
+        """Notify Launchpad that the given branch has started mirroring.
 
-    ##     The last_mirror_attempt field of the given branch record will be
-    ##     updated appropriately.
+        The last_mirror_attempt field of the given branch record will be
+        updated appropriately.
 
-    ##     :param branchID: The database ID of the given branch.
-    ##     :returns: True if the branch status was successfully updated.
-    ##     """
+        :param branchID: The database ID of the given branch.
+        :returns: True if the branch status was successfully updated.
+        """
 
-    ## def mirrorComplete(branchID, lastRevisionID):
-    ##     """Notify Launchpad that the branch has been successfully mirrored.
+    def mirrorComplete(branchID, lastRevisionID):
+        """Notify Launchpad that the branch has been successfully mirrored.
 
-    ##     In the Launchpad database, the last_mirrored field will be updated to
-    ##     match the last_mirror_attempt value, the mirror_failures counter will
-    ##     be reset to zero and the next_mirror_time will be set to NULL.
+        In the Launchpad database, the last_mirrored field will be updated to
+        match the last_mirror_attempt value, the mirror_failures counter will
+        be reset to zero and the next_mirror_time will be set to NULL.
 
-    ##     :param branchID: The database ID of the given branch.
-    ##     :param lastRevisionID: The last revision ID mirrored.
-    ##     :returns: True if the branch status was successfully updated.
-    ##     """
+        :param branchID: The database ID of the given branch.
+        :param lastRevisionID: The last revision ID mirrored.
+        :returns: True if the branch status was successfully updated.
+        """
 
-    ## def mirrorFailed(branchID, reason):
-    ##     """Notify Launchpad that the branch could not be mirrored.
+    def mirrorFailed(branchID, reason):
+        """Notify Launchpad that the branch could not be mirrored.
 
-    ##     The mirror_failures counter for the given branch record will be
-    ##     incremented and the next_mirror_time will be set to NULL.
+        The mirror_failures counter for the given branch record will be
+        incremented and the next_mirror_time will be set to NULL.
 
-    ##     :param branchID: The database ID of the given branch.
-    ##     :param reason: A string giving the reason for the failure.
-    ##     :returns: True if the branch status was successfully updated.
-    ##     """
+        :param branchID: The database ID of the given branch.
+        :param reason: A string giving the reason for the failure.
+        :returns: True if the branch status was successfully updated.
+        """
 
-    ## def recordSuccess(name, hostname, date_started, date_completed):
-    ##     """Notify Launchpad that a mirror script has successfully completed.
+    def recordSuccess(name, hostname, date_started, date_completed):
+        """Notify Launchpad that a mirror script has successfully completed.
 
-    ##     Create an entry in the ScriptActivity table with the provided data.
+        Create an entry in the ScriptActivity table with the provided data.
 
-    ##     :param name: Name of the script.
-    ##     :param hostname: Where the script was running.
+        :param name: Name of the script.
+        :param hostname: Where the script was running.
 
-    ##     :param date_started: When the script started, as an UTC time tuple.
-    ##     :param date_completed: When the script completed (now), as an UTC time
-    ##         tuple.
-    ##     :returns: True if the ScriptActivity record was successfully inserted.
-    ##     """
+        :param date_started: When the script started, as an UTC time tuple.
+        :param date_completed: When the script completed (now), as an UTC time
+            tuple.
+        :returns: True if the ScriptActivity record was successfully inserted.
+        """
