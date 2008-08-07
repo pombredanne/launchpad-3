837b4e655c5a95fa5de3b18be45891b0df7471b6
Stuart Bishop <stuart.bishop@canonical.com>
2008-08-07 15:12:35 +0700

    Extend db patch with Revision indexes created on production

--- database/schema/patch-121-70-1.sql
+++ database/schema/patch-121-70-1.sql
@@ -8,4 +8,9 @@ CREATE OR REPLACE VIEW ValidPersonOrTeamCache AS
     WHERE teamowner IS NOT NULL
         OR (account.status = 20 AND EmailAddress.status = 4);
 
+CREATE INDEX revision__revision_author__idx ON Revision(revision_author);
+CREATE INDEX revision__revision_date__idx ON Revision(revision_date);
+CREATE INDEX revision__gpgkey__idx ON Revision(gpgkey)
+    WHERE gpgkey IS NOT NULL;
+
 INSERT INTO LaunchpadDatabaseRevision VALUES (121, 70, 1);
