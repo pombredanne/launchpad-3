321a78da129a3e83114afb3f9ea1a1181a535aab
Aaron Bentley <aaron@canonical.com>
2008-08-07 16:22:28 -0400

    Fill out and clean up schema patch

--- database/schema/patch-121-90-0.sql
+++ database/schema/patch-121-90-0.sql
@@ -1,26 +1,47 @@
-BEGIN;
+SET client_min_messages=ERROR;
+
 
 CREATE TABLE Diff (
   id serial PRIMARY KEY,
-  bytes integer NOT NULL
-    CONSTRAINT diff__bytes__fk REFERENCES LibraryFileAlias,
-  conflicts text,
-  from_revision integer
-    CONSTRAINT from_revision__revision__fk REFERENCES Revision,
-  to_revision integer
-    CONSTRAINT to_revision__revision__fk REFERENCES Revision
+  bytes integer NOT NULL REFERENCES LibraryFileAlias,
+  diff_lines integer,
+  diffstat text,
+  added_lines integer,
+  removed_lines integer,
+  conflicts text
 );
 
 
 CREATE TABLE StaticDiffReference (
   id serial PRIMARY KEY,
-  branch integer NOT NULL
-    CONSTRAINT staticdiffreference__branch__fk REFERENCES Branch,
+  diff integer REFERENCES Diff,
+  branch integer NOT NULL REFERENCES Branch,
   from_revision_spec text,
   to_revision_spec text,
-  diff integer
-    CONSTRAINT staticdiffreference__diff__fk REFERENCES Diff
+  lease timestamp without time zone
 );
 
+
+CREATE TABLE PendingCodeMail (
+  id serial PRIMARY KEY,
+  from_address text NOT NULL,
+  to_address text NOT NULL,
+  subject text NOT NULL,
+  body text NOT NULL,
+  footer text NOT NULL,
+  rationale text NOT NULL,
+  branch_url text NOT NULL,
+  date_created timestamp without time zone NOT NULL
+      DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC'),
+  rfc822msgid text NOT NULL,
+  in_reply_to text NOT NULL, -- A Message-Id
+  static_diff integer REFERENCES StaticDiffReference
+  );
+
+
+ALTER TABLE BranchMergeProposal
+  ADD COLUMN review_diff integer REFERENCES StaticDiffReference;
+
+
+
 INSERT INTO LaunchpadDatabaseRevision VALUES (121, 90, 0);
-COMMIT;
