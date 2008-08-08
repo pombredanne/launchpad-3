80981d5fe0612324453e4c9fa6bf45e6a50d0322
Aaron Bentley <aaron@canonical.com>
2008-08-08 09:29:11 -0400

    Update from Gobby session

--- database/schema/patch-121-90-0.sql
+++ database/schema/patch-121-90-0.sql
@@ -3,22 +3,33 @@ SET client_min_messages=ERROR;
 
 CREATE TABLE Diff (
   id serial PRIMARY KEY,
-  bytes integer NOT NULL REFERENCES LibraryFileAlias,
+  diff_text integer NOT NULL REFERENCES LibraryFileAlias,
   diff_lines_count integer,
   diffstat text,
   added_lines_count integer,
   removed_lines_count integer,
-  conflicts text
+  conflicts text -- perhpas BYTES, store serialised bzrlib obj or not?
 );
 
 
 CREATE TABLE StaticDiffReference (
   id serial PRIMARY KEY,
-  diff integer REFERENCES Diff,
   branch integer NOT NULL REFERENCES Branch,
   from_revision_spec text,
   to_revision_spec text,
+  diff integer REFERENCES Diff, -- this is populated by the thing that takes the lease
   lease timestamp without time zone
+);
+
+
+CREATE TABLE PreviewDiffReference (
+  id serial PRIMARY KEY,
+  branch_merge_proposal integer NOT NULL REFERENCES BranchMergeProposal, -- UNIQUE?
+  last_source_revision integer NOT NULL REFERENCES Revision,
+  last_target_revision integer NOT NULL REFERENCES Revision,
+  last_dependent_revision integer REFERENCES Revision,
+  diff integer REFERENCES Diff,
+  lease timestamp without time zone,
 );
 
 
@@ -41,7 +52,6 @@ CREATE TABLE PendingCodeMail (
 
 ALTER TABLE BranchMergeProposal
   ADD COLUMN review_diff integer REFERENCES StaticDiffReference;
-
 
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (121, 90, 0);
