b8fceffe5007547b5628e141641ffcdea4d0a04f
Michael Hudson <michael.hudson@canonical.com>
2008-08-08 12:49:46 +1200

    cherry pick addition of TwistedAppServerLayer from the move-codehosting thread

--- lib/canonical/testing/__init__.py
+++ lib/canonical/testing/__init__.py
@@ -17,6 +17,7 @@ __all__ = [
     'LibrarianLayer',
     'PageTestLayer',
     'reset_logging',
+    'TwistedAppServerLayer',
     'TwistedLaunchpadZopelessLayer',
     'TwistedLayer',
     'ZopelessLayer',
