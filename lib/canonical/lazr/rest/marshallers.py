2952cf73c6c0486f482056c3352b3bb546b3f1a1
Leonard Richardson <leonard.richardson@canonical.com>
2008-08-08 09:13:58 -0400

    Initial implementation.

--- lib/canonical/lazr/rest/marshallers.py
+++ lib/canonical/lazr/rest/marshallers.py
@@ -66,11 +66,15 @@ class URLDereferencingMixin:
         request_host = self.request.get('HTTP_HOST')
         if config.vhosts.use_https:
             site_protocol = 'https'
+            default_port = '443'
         else:
             site_protocol = 'http'
+            default_port = '80'
 
-        if (host != request_host or protocol != site_protocol or
-            query != '' or fragment != ''):
+        if (protocol != site_protocol or query != '' or fragment != ''
+            or (host != request_host
+                and host + ':' + default_port != request_host
+                and host != request_host + ':' + default_port)):
             raise NotFound(self, url, self.request)
 
         path_parts = [urllib.unquote(part) for part in path.split('/')]
