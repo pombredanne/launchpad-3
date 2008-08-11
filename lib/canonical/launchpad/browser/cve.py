40b162df71b89bb1392ebc9b8e4b3be0d8970a48
Maris Fogels <maris.fogels@canonical.com>
2008-08-11 17:14:15 -0400

    Migrated the Cve breadcrumb to the new system interface.

--- lib/canonical/launchpad/browser/cve.py
+++ lib/canonical/launchpad/browser/cve.py
@@ -5,12 +5,13 @@
 __metaclass__ = type
 
 __all__ = [
-    'CveSetNavigation',
+    'CveBreadcrumbBuilder',
     'CveContextMenu',
-    'CveSetContextMenu',
     'CveLinkView',
-    'CveUnlinkView',
+    'CveSetContextMenu',
+    'CveSetNavigation',
     'CveSetView',
+    'CveUnlinkView',
     ]
 
 from zope.component import getUtility
@@ -22,6 +23,7 @@ from canonical.launchpad.validators.cve import valid_cve
 
 from canonical.launchpad.webapp import (
     canonical_url, ContextMenu, Link, GetitemNavigation)
+from canonical.launchpad.webapp.breadcrumb import BreadcrumbBuilder
 from canonical.launchpad.webapp.generalform import GeneralFormView
 
 
@@ -29,8 +31,11 @@ class CveSetNavigation(GetitemNavigation):
 
     usedfor = ICveSet
 
-    def breadcrumb(self):
-        return "CVE reports"
+
+class CveBreadcrumbBuilder(BreadcrumbBuilder):
+    """Returns a breadcrumb for an `ICve`."""
+    text = "CVE reports"
+
 
 class CveContextMenu(ContextMenu):
 
