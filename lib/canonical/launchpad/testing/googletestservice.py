3b93c5ac4a902276a7851cb31dea82846c37f3ff
Stuart Bishop <stuart.bishop@canonical.com>
2008-08-11 19:45:04 +0700

    Comment

--- lib/canonical/launchpad/testing/googletestservice.py
+++ lib/canonical/launchpad/testing/googletestservice.py
@@ -190,11 +190,12 @@ def start_as_process():
         # Make sure we run the .py file, not the .pyc.
         head, _ = os.path.splitext(script)
         script = head + '.py'
+    # Make sure we aren't using the parent stdin and stdout to avoid spam
+    # and have fewer things that can go wrong shutting down the process.
     proc = subprocess.Popen(script,
         stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     proc.stdin.close()
     return proc
-
 
 
 def kill_running_process():
