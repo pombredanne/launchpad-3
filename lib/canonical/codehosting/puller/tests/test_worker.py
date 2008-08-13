2a27978b2ac65ab7957fa09d67c3e261c630f3a1
Michael Hudson <michael.hudson@canonical.com>
2008-08-13 14:09:18 +1200

    oops, move PullerWorkerMixin back

--- lib/canonical/codehosting/puller/tests/test_worker.py
+++ lib/canonical/codehosting/puller/tests/test_worker.py
@@ -19,41 +19,13 @@ from canonical.codehosting.bzrutils import ensure_base
 from canonical.codehosting.puller.worker import (
     BadUrl, BadUrlFile, BadUrlLaunchpad, BadUrlSsh, BranchOpener,
     BranchReferenceForbidden, BranchReferenceLoopError, MirroredBranchOpener,
-    PullerWorker, PullerWorkerProtocol, get_canonical_url_for_branch_name,
+    PullerWorkerProtocol, get_canonical_url_for_branch_name,
     install_worker_ui_factory)
+from canonical.codehosting.puller.tests import PullerWorkerMixin
 from canonical.launchpad.database import Branch
 from canonical.launchpad.testing import LaunchpadObjectFactory
 from canonical.launchpad.webapp import canonical_url
 from canonical.testing import LaunchpadScriptLayer, reset_logging
-
-
-class PullerWorkerMixin:
-    """Mixin for tests that want to make PullerWorker objects.
-
-    Assumes that it is mixed into a class that runs in a temporary directory,
-    such as `TestCaseInTempDir` and that `get_transport` is provided as a
-    method.
-    """
-
-    def makePullerWorker(self, src_dir=None, dest_dir=None, branch_type=None,
-                         protocol=None, oops_prefix=None):
-        """Anonymous creation method for PullerWorker."""
-        if src_dir is None:
-            src_dir = self.get_transport('source-branch').base
-        if dest_dir is None:
-            dest_dir = './dest-branch'
-        if protocol is None:
-            protocol = PullerWorkerProtocol(StringIO())
-        if oops_prefix is None:
-            oops_prefix = ''
-        class _AcceptAnythingOpener(BranchOpener):
-            def checkOneURL(self, url):
-                pass
-        opener = _AcceptAnythingOpener()
-        return PullerWorker(
-            src_dir, dest_dir, branch_id=1, unique_name='foo/bar/baz',
-            branch_type=branch_type, protocol=protocol, branch_opener=opener,
-            oops_prefix=oops_prefix)
 
 
 class TestPullerWorker(TestCaseWithTransport, PullerWorkerMixin):
@@ -309,7 +281,6 @@ class TestWorkerProtocol(TestCaseInTempDir, PullerWorkerMixin):
         TestCaseInTempDir.setUp(self)
         self.output = StringIO()
         self.protocol = PullerWorkerProtocol(self.output)
-        self.branch_to_mirror = self.makePullerWorker()
 
     def assertSentNetstrings(self, expected_netstrings):
         """Assert that the protocol sent the given netstrings (in order)."""
@@ -337,6 +308,7 @@ class TestWorkerProtocol(TestCaseInTempDir, PullerWorkerMixin):
 
     def test_nothingSentOnConstruction(self):
         # The protocol sends nothing until it receives an event.
+        self.branch_to_mirror = self.makePullerWorker(protocol=self.protocol)
         self.assertSentNetstrings([])
 
     def test_startMirror(self):
