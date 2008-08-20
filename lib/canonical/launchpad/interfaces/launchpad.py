1e5d6fb84d6cc4271497c9722448e61e14966c95
Maris Fogels <maris.fogels@canonical.com>
2008-08-20 15:38:03 -0400

    Removed an unused export.

--- lib/canonical/launchpad/interfaces/launchpad.py
+++ lib/canonical/launchpad/interfaces/launchpad.py
@@ -32,7 +32,6 @@ __all__ = [
     'IAuthServerApplication',
     'IBasicLaunchpadRequest',
     'IBazaarApplication',
-    'IBreadcrumb',
     'ICrowd',
     'IFeedsApplication',
     'IHWDBApplication',
