c2fc96c89580394447527faa8dd74414daa27dcc
Aaron Bentley <aaron@canonical.com>
2008-09-09 15:16:09 -0400

    Capitalize types, add BMP.review_diff_job

--- database/schema/patch-121-90-0.sql
+++ database/schema/patch-121-90-0.sql
@@ -1,7 +1,7 @@
 SET client_min_messages=ERROR;
 
 CREATE TABLE Job (
-  id serial PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   lease_expires TIMESTAMP WITHOUT TIME ZONE
 );
 
@@ -14,22 +14,22 @@ CREATE TABLE JobDependency ( -- Rows in this table describe dependencies between
 
 
 CREATE TABLE StaticDiffJob ( -- Rows in this table are directions for producing a diff.
-  id serial PRIMARY KEY,
-  job integer REFERENCES Job,
-  branch integer NOT NULL REFERENCES Branch,
-  from_revision_spec text,
-  to_revision_spec text,
+  id SERIAL PRIMARY KEY,
+  job INTEGER REFERENCES Job,
+  branch INTEGER NOT NULL REFERENCES Branch,
+  from_revision_spec TEXT,
+  to_revision_spec TEXT,
   UNIQUE (branch, from_revision_spec, to_revision_spec)
 );
 
 
 CREATE TABLE Diff ( -- Contains information about static and preview diffs
   id serial PRIMARY KEY,
-  diff_text integer NOT NULL REFERENCES LibraryFileAlias,
-  diff_lines_count integer,
-  diffstat text,
-  added_lines_count integer,
-  removed_lines_count integer
+  diff_text INTEGER NOT NULL REFERENCES LibraryFileAlias,
+  diff_lines_count INTEGER,
+  diffstat TEXT,
+  added_lines_count INTEGER,
+  removed_lines_count INTEGER
 );
 
 
@@ -37,44 +37,48 @@ CREATE TABLE StaticDiff ( -- Contains information about static diffs
   id serial PRIMARY KEY,
   from_revision_id TEXT, -- a revision-id
   to_revision_id TEXT, -- a revision-id
-  diff integer REFERENCES Diff,
+  diff INTEGER REFERENCES Diff,
   UNIQUE (from_revision_id, to_revision_id)
 );
 
 
 CREATE TABLE PreviewDiffReference ( -- Contains information about preview diffs, but does not duplicate information with BranchMergeProposal
-  id serial PRIMARY KEY,
-  branch_merge_proposal integer UNIQUE NOT NULL REFERENCES BranchMergeProposal,
-  last_source_revision integer NOT NULL REFERENCES Revision,
-  last_target_revision integer NOT NULL REFERENCES Revision,
-  last_dependent_revision integer REFERENCES Revision,
-  diff integer REFERENCES Diff,
-  conflicts text -- perhaps BYTES, store serialised bzrlib obj or not?
+  id SERIAL PRIMARY KEY,
+  branch_merge_proposal INTEGER UNIQUE NOT NULL REFERENCES BranchMergeProposal,
+  last_source_revision INTEGER NOT NULL REFERENCES Revision,
+  last_target_revision INTEGER NOT NULL REFERENCES Revision,
+  last_dependent_revision INTEGER REFERENCES Revision,
+  diff INTEGER REFERENCES Diff,
+  conflicts TEXT -- perhaps BYTES, store serialised bzrlib obj or not?
 );
 
 
 CREATE TABLE CodeMailJob (
-  id serial PRIMARY KEY,
-  job integer REFERENCES Job,
-  static_diff integer REFERENCES StaticDiff,
-  from_address text NOT NULL,
-  reply_to_address text,
-  to_address text NOT NULL,
-  subject text NOT NULL,
-  body text NOT NULL,
-  footer text NOT NULL,
-  rationale text NOT NULL,
-  branch_url text NOT NULL,
-  branch_project_name text,
-  date_created timestamp without time zone NOT NULL
+  id SERIAL PRIMARY KEY,
+  job INTEGER REFERENCES Job,
+  static_diff INTEGER REFERENCES StaticDiff,
+  max_diff_lines INTEGER,
+  from_address TEXT NOT NULL,
+  reply_to_address TEXT,
+  to_address TEXT NOT NULL,
+  subject TEXT NOT NULL,
+  body TEXT NOT NULL,
+  footer TEXT NOT NULL,
+  rationale TEXT NOT NULL,
+  branch_url TEXT NOT NULL,
+  branch_project_name TEXT,
+  date_created TIMESTAMP WITHOUT TIME ZONE NOT NULL
       DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC'),
-  rfc822msgid text NOT NULL,
-  in_reply_to text -- A Message-Id
+  rfc822msgid TEXT NOT NULL,
+  in_reply_to TEXT -- A Message-Id
 );
 
 
 ALTER TABLE BranchMergeProposal
-  ADD COLUMN review_diff integer REFERENCES StaticDiff;
+  ADD COLUMN review_diff INTEGER REFERENCES StaticDiff;
+
+ALTER TABLE BranchMergeProposal
+  ADD COLUMN review_diff_job INTEGER REFERENCES StaticDiffJob;
 
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (121, 90, 0);
