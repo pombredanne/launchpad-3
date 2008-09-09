f408b5154f2ad8156bb3dbc768736eb9bf4ee10f
Jeroen Vermeulen <jeroen.vermeulen@canonical.com>
2008-09-09 09:48:08 +0200

    Disabled spuriously failing test, see bug 268034.

--- lib/canonical/codehosting/codeimport/tests/test_workermonitor.py
+++ lib/canonical/codehosting/codeimport/tests/test_workermonitor.py
@@ -316,7 +316,10 @@ class TestWorkerMonitorUnit(TestCase):
         # callFinishJob did not swallow the error, this will fail the test.
         return ret
 
-    def test_callFinishJobLogsTracebackOnFailure(self):
+    # XXX JeroenVermeulen 2008-09-09 bug=268034: Disabled this after
+    # spurious (and probably rare) PQM failure.  Fix race condition and
+    # re-enable.
+    def disabled_test_callFinishJobLogsTracebackOnFailure(self):
         # When callFinishJob is called with a failure, it dumps the traceback
         # of the failure into the log file.
         ret = self.worker_monitor.callFinishJob(makeFailure(RuntimeError))
