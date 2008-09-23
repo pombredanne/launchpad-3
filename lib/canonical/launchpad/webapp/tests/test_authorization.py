e05c3688d60cb06e324da646dd92b43d21514772
Michael Hudson <michael.hudson@canonical.com>
2008-09-23 17:57:22 +1200

    more tests!

--- lib/canonical/launchpad/webapp/tests/test_authorization.py
+++ lib/canonical/launchpad/webapp/tests/test_authorization.py
@@ -8,10 +8,12 @@ import StringIO
 import unittest
 
 from zope.app.testing import ztapi
+from zope.component import getUtility
 
 from canonical.launchpad.security import AuthorizationBase
+from canonical.launchpad.testing import TestCaseWithFactory
 from canonical.launchpad.webapp.authorization import LaunchpadSecurityPolicy
-from canonical.launchpad.webapp.interfaces import IAuthorization
+from canonical.launchpad.webapp.interfaces import IAuthorization, IPlacelessLoginSource
 from canonical.launchpad.webapp.servers import LaunchpadBrowserRequest
 from canonical.testing.layers import DatabaseFunctionalLayer
 
@@ -26,6 +28,10 @@ class Checker(AuthorizationBase):
         self.calls.append('checkUnauthenticated')
         return False
 
+    def checkAuthenticated(self, user):
+        self.calls.append(('checkAuthenticated', user))
+        return True
+
 
 class CheckerFactory:
 
@@ -36,7 +42,7 @@ class CheckerFactory:
         return Checker(obj, self.calls)
 
 
-class TestCheckPermissionCaching(unittest.TestCase):
+class TestCheckPermissionCaching(TestCaseWithFactory):
 
     layer = DatabaseFunctionalLayer
 
@@ -60,8 +66,55 @@ class TestCheckPermissionCaching(unittest.TestCase):
             pass
         checker_factory = self.registerCheckerFactoryForClass(C)
         objecttoauthorize = C()
-        policy.checkPermission(self.permission, objecttoauthorize)
-        self.assertEqual(['checkUnauthenticated'], checker_factory.calls)
+        self.assertEqual(
+            False, policy.checkPermission(self.permission, objecttoauthorize))
+        self.assertEqual(
+            ['checkUnauthenticated'], checker_factory.calls)
+        self.assertEqual(
+            False, policy.checkPermission(self.permission, objecttoauthorize))
+        self.assertEqual(
+            ['checkUnauthenticated'], checker_factory.calls)
+
+    def test_checkPermission_cache_authenticated(self):
+        person = self.factory.makePerson()
+        login_src = getUtility(IPlacelessLoginSource)
+        principal = login_src.getPrincipal(person.id)
+        request = self.makeRequest()
+        request.setPrincipal(principal)
+        policy = LaunchpadSecurityPolicy(request)
+        class C:
+            pass
+        checker_factory = self.registerCheckerFactoryForClass(C)
+        objecttoauthorize = C()
+        self.assertEqual(
+            True, policy.checkPermission(self.permission, objecttoauthorize))
+        self.assertEqual(
+            [('checkAuthenticated', person)], checker_factory.calls)
+        self.assertEqual(
+            True, policy.checkPermission(self.permission, objecttoauthorize))
+        self.assertEqual(
+            [('checkAuthenticated', person)], checker_factory.calls)
+
+    def test_checkPermission_setPrincipal_resets_cache(self):
+        person = self.factory.makePerson()
+        login_src = getUtility(IPlacelessLoginSource)
+        principal = login_src.getPrincipal(person.id)
+        request = self.makeRequest()
+        policy = LaunchpadSecurityPolicy(request)
+        class C:
+            pass
+        checker_factory = self.registerCheckerFactoryForClass(C)
+        objecttoauthorize = C()
+        self.assertEqual(
+            False, policy.checkPermission(self.permission, objecttoauthorize))
+        self.assertEqual(
+            ['checkUnauthenticated'], checker_factory.calls)
+        request.setPrincipal(principal)
+        self.assertEqual(
+            True, policy.checkPermission(self.permission, objecttoauthorize))
+        self.assertEqual(
+            ['checkUnauthenticated', ('checkAuthenticated', person)],
+            checker_factory.calls)
 
 
 def test_suite():
