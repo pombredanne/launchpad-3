5a199d209dde6dc352b5d89306b73a67d863f111
Bjorn Tillenius <bjorn@canonical.com>
2008-09-30 22:39:04 +0300

    re-enable bugs-emailinterface.txt

--- lib/canonical/launchpad/ftests/test_system_documentation.py
+++ lib/canonical/launchpad/ftests/test_system_documentation.py
@@ -891,8 +891,7 @@ class ProcessMailLayer(LaunchpadZopelessLayer):
 
     doctests_without_logging = [
         'answer-tracker-emailinterface.txt',
-        #XXX: Commenting out spurious failing test
-        #'bugs-emailinterface.txt',
+        'bugs-emailinterface.txt',
         'bugs-email-affects-path.txt',
         'emailauthentication.txt',
         ]
