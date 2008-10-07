cbc9e5391acf7c6c53bdb1bcf33bfaa945e68134
Celso Providelo <celso.providelo@canonical.com>
2008-10-07 11:05:36 -0300

    improved db patch.

--- database/schema/patch-2109-99-0.sql
+++ database/schema/patch-2109-99-0.sql
@@ -1,6 +1,13 @@
 SET client_min_messages=ERROR;
 
+-- Adding a 'status' column for better handling failed request.
 ALTER TABLE PackageDiff ADD COLUMN status INTEGER;
+
+-- Set the default value so the NOT NULL constraint can be added.
+-- A proper data migration will be done when the new code gets released.
+UPDATE PackageDiff SET status = 0;
+
+-- Column contraints.
 ALTER TABLE PackageDiff ALTER COLUMN status SET NOT NULL;
 ALTER TABLE PackageDiff ALTER COLUMN status SET DEFAULT 0;
 
