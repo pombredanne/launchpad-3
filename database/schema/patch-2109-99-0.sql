1b5d44690fdce34fafbdb5f7206a3b992039f6b4
Barry Warsaw <barry@canonical.com>
2008-10-07 09:47:15 -0400

    Fix the db patch to conform to Stuart's review comments.

--- database/schema/patch-2109-99-0.sql
+++ database/schema/patch-2109-99-0.sql
@@ -26,10 +26,10 @@ SET DEFAULT nextval('usertouseremail_id_seq'::regclass);
 
 ALTER TABLE ONLY usertouseremail
 ADD CONSTRAINT usertouseremail_sender_fkey
-FOREIGN KEY (sender_id) REFERENCES person(id);
+FOREIGN KEY (sender) REFERENCES person(id);
 
 ALTER TABLE ONLY usertouseremail
 ADD CONSTRAINT usertouseremail_recipient_fkey
-FOREIGN KEY (recipient_id) REFERENCES person(id);
+FOREIGN KEY (recipient) REFERENCES person(id);
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2109, 99,0);
