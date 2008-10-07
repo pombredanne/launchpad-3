329f3c7439c2b18f7841fee26468b7488ac96878
Maris Fogels <maris.fogels@canonical.com>
2008-10-07 12:54:09 -0400

    Tweaked the test harness a bit.

--- windmill.sh
+++ windmill.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-windmill $@ firefox http://launchpad.dev:8085
+js_testdir=lib/canonical/launchpad/icing/
+
+# We need to set the PYTHONPATH because windmill will look for tests under
+# the canonical.launchpad module, which imports some stuff from Zope.
+PYTHONPATH=lib/
+
+windmill \
+    $@ \
+    jsdir=$js_testdir \
+    firefox \
+    http://launchpad.dev:8085
