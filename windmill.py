8981a8d8df667e64fe03dd9f45fea804886bb00e
Francis J. Lacoste <francis.lacoste@canonical.com>
2008-10-07 20:49:42 -0400

    Trim down windmill.py.

--- windmill.py
+++ windmill.py
@@ -1,23 +1,10 @@
 #!/usr/bin/python2.5
 """Windmill wrapper script.
 
-Windmill requires Python 2.5
+Note: Windmill requires Python 2.5
 """
 import os
 import sys
-
-if os.getsid(0) == os.getsid(os.getppid()):
-    # We need to become the process group leader so test_on_merge.py
-    # can reap its children.
-    #
-    # Note that if setpgrp() is used to move a process from one
-    # process group to another (as is done by some shells when
-    # creating pipelines), then both process groups must be part of
-    # the same session.
-    os.setpgrp()
-
-# Enable Storm's C extensions
-os.environ['STORM_CEXTENSIONS'] = '1'
 
 here = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, os.path.join(here, 'lib'))
