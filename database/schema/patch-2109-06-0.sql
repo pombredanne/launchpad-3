524d01dd0231fa3e5916dd704faf6d45d143ca64
Stuart Bishop <stuart.bishop@canonical.com>
2008-10-10 18:52:06 +0700

    Tighten ArchiveDependency DB contraints

--- database/schema/patch-2109-06-0.sql
+++ database/schema/patch-2109-06-0.sql
@@ -7,4 +7,10 @@ ALTER TABLE PackageDiff ADD COLUMN status INTEGER DEFAULT 0 NOT NULL;
 
 CREATE INDEX packagediff__status__idx ON PackageDiff(status);
 
+DROP INDEX archivedependency__unique__temp;
+ALTER TABLE ArchiveDependency
+    DROP CONSTRAINT archivedependency_unique,
+    ADD CONSTRAINT archivedependency__unique UNIQUE (archive, dependency),
+    ALTER COLUMN pocket SET NOT NULL;
+
 INSERT INTO LaunchpadDatabaseRevision VALUES (2109, 6, 0);
