c0160eec93eb6ddd1c1d51f4349668f1609cde16
Tom Haddon <tom.haddon@canonical.com>
2008-10-14 09:40:04 -0700

    Filename is bzr-version-info not bzr_version_info

--- scripts/update-bzr-version-info.sh
+++ scripts/update-bzr-version-info.sh
@@ -9,24 +9,24 @@ if ! which bzr > /dev/null || !  test -x $(which bzr); then
     exit 1
 fi
 
-newfile=bzr_version_info_${RANDOM}.py
+newfile=bzr-version-info-${RANDOM}.py
 PYTHONPATH= bzr version-info --format=python > $newfile 2>/dev/null;
 # There's a leading space here that I don't care to trim.. 
 revno=$(python $newfile | grep revision: | cut -d: -f2)
-if ! [ -f bzr_version_info.py ]; then
-    echo "Creating bzr_version_info.py at revno$revno"
-    mv ${newfile} bzr_version_info.py
+if ! [ -f bzr-version-info.py ]; then
+    echo "Creating bzr-version-info.py at revno$revno"
+    mv ${newfile} bzr-version-info.py
 else
     # Here we compare the actual output instead of the contents of the
     # file because bzr includes a build-date that is actually updated
     # every time you run bzr version-info.
     newcontents=$(python $newfile)
-    oldcontents=$(python bzr_version_info.py)
+    oldcontents=$(python bzr-version-info.py)
     if [ "$newcontents" != "$oldcontents" ]; then
-        echo "Updating bzr_version_info.py to revno$revno"
-        mv ${newfile} bzr_version_info.py
+        echo "Updating bzr-version-info.py to revno$revno"
+        mv ${newfile} bzr-version-info.py
     else
-        echo "Skipping bzr_version_info.py update; already at revno$revno"
+        echo "Skipping bzr-version-info.py update; already at revno$revno"
         rm ${newfile}
     fi
 fi
