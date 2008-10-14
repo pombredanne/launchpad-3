180d8282e9c5de7317a7c2ad32a41535f407044d
Stuart Bishop <stuart.bishop@canonical.com>
2008-10-14 16:35:28 +0700

    Fix command line argument handling

--- database/replication/repair-restored-db.py
+++ database/replication/repair-restored-db.py
@@ -24,8 +24,9 @@ import sys
 import replication.helpers
 
 from canonical.config import config
+from canonical.database.postgresql import ConnectionString
 from canonical.database.sqlbase import connect, sqlvalues
-from canonical.launchpad.scripts import db_options, logger_options
+from canonical.launchpad.scripts import db_options, logger_options, logger
 
 def main():
     parser = OptionParser()
@@ -33,24 +34,41 @@ def main():
     logger_options(parser)
     options, args = parser.parse_args()
 
+    log = logger(options)
+
     con = connect(options.dbuser)
+
+    if not replication.helpers.slony_installed(con):
+        log.info("Slony-I not installed. Nothing to do.")
+        return 0
+
     cur = con.cursor()
 
     # Determine the node id the database thinks it is.
     cur.execute("SELECT _sl.getlocalnodeid('_sl')")
     node_id = cur.fetchone()[0]
+    log.debug("Node Id is %d" % node_id)
 
     # Get a list of set ids in the database.
     cur.execute("SELECT DISTINCT set_id FROM _sl.sl_set")
     set_ids = set(row[0] for row in cur.fetchall())
+    log.debug("Set Ids are %s" % repr(set_ids))
 
     # Close so we don't block slonik(1)
     del cur
     con.close()
 
+    connection_string = ConnectionString(config.database.main_master)
+    if options.dbname:
+        connection_string.dbname = options.dbname
+    if options.dbuser:
+        connection_string.user = options.dbuser
+    if options.dbhost:
+        connection_string.host = options.dbhost
+
     script = [
         "cluster name = sl;",
-        "node 1 admin conninfo = '%s';" % config.database.main_master,
+        "node 1 admin conninfo = '%s';" % connection_string,
         ]
     for set_id in set_ids:
         script.append(
