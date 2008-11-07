4f1aae8d5d2fd4829f6c26733715e7047d2b43ec
Tom Berger <tom.berger@canonical.com>
2008-11-07 12:13:37 +0000

    without timezone

--- database/schema/patch-2109-99-0.sql
+++ database/schema/patch-2109-99-0.sql
@@ -1,6 +1,6 @@
 SET client_min_messages = ERROR;
 
 ALTER TABLE BugTask
-ADD COLUMN date_left_closed TIMESTAMP NULL;
+ADD COLUMN date_left_closed TIMESTAMP WITHOUT TIME ZONE NULL;
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2109, 99, 0);
