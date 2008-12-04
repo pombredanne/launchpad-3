d8192c0cd9df05bb3f6fd0240a6acc6e5c44ab64
Danilo Šegan <danilo@canonical.com>
2008-12-04 15:01:42 +0100

    And bzr add the actual tests.

---
+++ lib/canonical/launchpad/tests/test_translation_shared_potemplate.py
@@ -0,0 +1,178 @@
+# Copyright 2008 Canonical Ltd.  All rights reserved.
+
+__metaclass__ = type
+
+from datetime import datetime, timedelta
+from pytz import timezone
+import unittest
+
+import transaction
+
+from zope.component import getUtility
+from zope.security.proxy import removeSecurityProxy
+
+from canonical.launchpad.database.translationtemplateitem import (
+    TranslationTemplateItem)
+from canonical.launchpad.interfaces import (
+    ILanguageSet, TranslationFileFormat)
+from canonical.launchpad.testing.factory import LaunchpadObjectFactory
+from canonical.testing import LaunchpadZopelessLayer
+
+
+class TestTranslationSharedPOTemplate(unittest.TestCase):
+    """Test behaviour of "shared" PO templates."""
+
+    layer = LaunchpadZopelessLayer
+
+    def setUp(self):
+        """Set up context to test in."""
+        # Create a product with two series and a shared POTemplate
+        # in different series ('devel' and 'stable').
+        factory = LaunchpadObjectFactory()
+        self.factory = factory
+        self.foo = factory.makeProduct()
+        self.foo_devel = factory.makeProductSeries(
+            name='devel', product=self.foo)
+        self.foo_stable = factory.makeProductSeries(
+            name='stable', product=self.foo)
+        self.foo.official_rosetta = True
+
+        # POTemplate is 'shared' if it has the same name ('messages').
+        self.devel_potemplate = factory.makePOTemplate(
+            productseries=self.foo_devel, name="messages")
+        self.stable_potemplate = factory.makePOTemplate(self.foo_stable,
+                                                        name="messages")
+
+        # Create a single POTMsgSet that is used across all tests,
+        # and add it to only one of the POTemplates.
+        self.potmsgset = self.factory.makePOTMsgSet(self.devel_potemplate)
+        self.potmsgset.setSequence(self.devel_potemplate, 1)
+
+    def test_getPOTMsgSets(self):
+        self.potmsgset.setSequence(self.stable_potemplate, 1)
+
+        devel_potmsgsets = list(self.devel_potemplate.getPOTMsgSets())
+        stable_potmsgsets = list(self.stable_potemplate.getPOTMsgSets())
+
+        self.assertEquals(devel_potmsgsets, [self.potmsgset])
+        self.assertEquals(devel_potmsgsets, stable_potmsgsets)
+
+    def test_getPOTMsgSetByMsgIDText(self):
+        potmsgset = self.factory.makePOTMsgSet(self.devel_potemplate,
+                                               singular="Open file")
+
+        # It's still not present in the PO template.
+        read_potmsgset = self.devel_potemplate.getPOTMsgSetByMsgIDText(
+            "Open file")
+        self.assertEquals(read_potmsgset, None)
+
+        # To actually insert it into a POTemplate, it needs
+        # a sequence to be set.
+        potmsgset.setSequence(self.devel_potemplate, 2)
+        read_potmsgset = self.devel_potemplate.getPOTMsgSetByMsgIDText(
+            "Open file")
+        self.assertEquals(potmsgset, read_potmsgset)
+
+    def test_getPOTMsgSetBySequence(self):
+        potmsgset = self.factory.makePOTMsgSet(self.devel_potemplate)
+        sequence = self.factory.getUniqueInteger()
+
+        # It's still not present in the PO template.
+        read_potmsgset = self.devel_potemplate.getPOTMsgSetBySequence(
+            sequence)
+        self.assertEquals(read_potmsgset, None)
+
+        # Now we set the appropriate sequence in a potemplate and see that
+        # it works.
+        potmsgset.setSequence(self.devel_potemplate, sequence)
+        read_potmsgset = self.devel_potemplate.getPOTMsgSetBySequence(
+            sequence)
+        self.assertEquals(potmsgset, read_potmsgset)
+
+        # It's still not present in different shared PO template.
+        read_potmsgset = self.stable_potemplate.getPOTMsgSetBySequence(
+            sequence)
+        self.assertEquals(read_potmsgset, None)
+
+    def test_getPOTMsgSetByID(self):
+        potmsgset = self.factory.makePOTMsgSet(self.devel_potemplate)
+        id = potmsgset.id
+
+        # It's still not present in the PO template.
+        read_potmsgset = self.devel_potemplate.getPOTMsgSetByID(id)
+        self.assertEquals(read_potmsgset, None)
+
+        # Now we set the appropriate sequence in a potemplate and see that
+        # we can get it by ID.
+        potmsgset.setSequence(self.devel_potemplate, 3)
+        read_potmsgset = self.devel_potemplate.getPOTMsgSetByID(id)
+        self.assertEquals(potmsgset, read_potmsgset)
+
+        # Getting this one in a different template doesn't work.
+        read_potmsgset = self.stable_potemplate.getPOTMsgSetByID(id)
+        self.assertEquals(read_potmsgset, None)
+
+        # Nor can you get an entry with a made up ID.
+        random_id = 100000 + self.factory.getUniqueInteger()
+        read_potmsgset = self.devel_potemplate.getPOTMsgSetByID(random_id)
+        self.assertEquals(read_potmsgset, None)
+
+    def test_hasMessageID(self):
+        naked_potemplate = removeSecurityProxy(self.devel_potemplate)
+        # Let's get details we need for a POTMsgSet that is
+        # already in the POTemplate.
+        present_msgid_singular = self.potmsgset.msgid_singular
+        present_msgid_plural = self.potmsgset.msgid_plural
+        present_context = self.potmsgset.context
+        has_message_id = naked_potemplate.hasMessageID(
+            present_msgid_singular, present_msgid_plural, present_context)
+        self.assertEquals(has_message_id, True)
+
+        # A new POTMsgSet that is not part of the POTemplate cannot
+        # be gotten using hasMessageID on a POTemplate.
+        absent_potmsgset = self.factory.makePOTMsgSet(self.devel_potemplate)
+        absent_msgid_singular = absent_potmsgset.msgid_singular
+        absent_msgid_plural = absent_potmsgset.msgid_plural
+        absent_context = absent_potmsgset.msgid_plural
+        has_message_id = naked_potemplate.hasMessageID(
+            absent_msgid_singular, absent_msgid_plural, absent_context)
+        self.assertEquals(has_message_id, False)
+
+    def test_hasPluralMessage(self):
+        naked_potemplate = removeSecurityProxy(self.devel_potemplate)
+
+        # At the moment, a POTemplate has no plural form messages.
+        self.assertEquals(self.devel_potemplate.hasPluralMessage(), False)
+
+        # Let's add a POTMsgSet with plural forms.
+        plural_potmsgset = self.factory.makePOTMsgSet(self.devel_potemplate,
+                                                      singular="singular",
+                                                      plural="plural")
+        plural_potmsgset.setSequence(self.devel_potemplate, 4)
+
+        # Now, template contains a plural form message.
+        self.assertEquals(self.devel_potemplate.hasPluralMessage(), True)
+
+        # And as a clean-up, let's remove it from the template.
+        plural_potmsgset.setSequence(self.devel_potemplate, 0)
+
+    def test_expireAllMessages(self):
+        devel_potmsgsets = list(self.devel_potemplate.getPOTMsgSets())
+        self.assertEquals(len(devel_potmsgsets) > 0, True)
+
+        # Expiring all messages brings the count back to zero.
+        self.devel_potemplate.expireAllMessages()
+        devel_potmsgsets = list(self.devel_potemplate.getPOTMsgSets())
+        self.assertEquals(len(devel_potmsgsets), 0)
+
+        # Expiring all messages even when all are already expired still works.
+        self.devel_potemplate.expireAllMessages()
+        devel_potmsgsets = list(self.devel_potemplate.getPOTMsgSets())
+        self.assertEquals(len(devel_potmsgsets), 0)
+
+        # Let's restore the POTMsgSet's participation in the template.
+        self.potmsgset.setSequence(self.devel_potemplate, 1)
+
+
+def test_suite():
+    return unittest.TestLoader().loadTestsFromName(__name__)
