4a6862898061af2e641be9fae47596db41874a7e
Stuart Bishop <stuart.bishop@canonical.com>
2008-12-04 10:48:28 +0700

    Indexes on Revision and BranchRevision to improve BranchRevision traversal

--- database/schema/patch-2109-14-0.sql
+++ database/schema/patch-2109-14-0.sql
@@ -13,4 +13,22 @@ ALTER TABLE BranchRevision
     ON DELETE CASCADE
     DEFERRABLE INITIALLY DEFERRED;
 
+-- Index to allow quick access to MIN(id) WHERE revision_date > ....
+-- We use this to roughly filter BranchRevision on date to make the
+-- real join with Revision faster.
+ALTER TABLE Revision
+   ADD CONSTRAINT revision__id__revision_date__key
+   UNIQUE (id, revision_date);
+
+-- Index to allow quick filtering of BranchRevision by branch and revision.
+-- Note that existing UNIQUE (revision, branch) isn't used for this (we
+-- generally have a very wide filter on revision, such as all revisions in the
+-- last 30 days).
+ALTER TABLE BranchRevision
+   -- Branch -> Revision traversal
+   ADD CONSTRAINT revision__branch__revision__key UNIQUE (branch, revision),
+   -- Revision -> Branch traversal
+   DROP CONSTRAINT revisionnumber_revision_branch_unique,
+   ADD CONSTRAINT revision__revision__branch__key UNIQUE (revision, branch);
+
 INSERT INTO LaunchpadDatabaseRevision VALUES (2109, 14, 0);
