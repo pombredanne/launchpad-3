8f2e7aacac1b8e10c9de59ca47535d72a2ce8815
Jonathan Lange <jml@canonical.com>
2008-12-12 13:42:01 +1100

    Apply review comments.

--- lib/canonical/launchpad/interfaces/branchcontainer.py
+++ lib/canonical/launchpad/interfaces/branchcontainer.py
@@ -14,20 +14,7 @@ class IBranchContainer(Interface):
     """A container of branches.
 
     A product contains branches, a source package on a distroseries contains
-    branches, and a person contains branches in two different ways ('branches
-    owned' and 'junk branches').
+    branches, and a person contains 'junk' branches.
     """
 
     name = Attribute("The name of the container.")
-
-    def getBranches():
-        """Return the branches in this container."""
-
-    # TODO: add lifecycle_statuses filter
-    # TODO: add visible_by_user filter
-    # TODO: add sort_by option
-    # TODO: add quantity / limit option
-    # TODO: add getTargetBranchesForUsersMergeProposals, whatever that is.
-    # TODO: add subscribed-by-user filter
-    # TODO: add owned-by-user filter
-    # TODO: add registered-by-user filter
