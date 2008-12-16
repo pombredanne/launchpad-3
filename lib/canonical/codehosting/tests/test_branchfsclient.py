0d2ab3a8e970340878fe6706d0242dbb083616ec
Michael Hudson <michael.hudson@canonical.com>
2008-12-17 12:10:22 +1300

    move client creation out of setUp

--- lib/canonical/codehosting/tests/test_branchfsclient.py
+++ lib/canonical/codehosting/tests/test_branchfsclient.py
@@ -25,12 +25,15 @@ class TestBranchFileSystemClient(TestCase):
         self.factory = frontend.getLaunchpadObjectFactory()
         self.user = self.factory.makePerson()
         self._xmlrpc_client = XMLRPCWrapper(frontend.getFilesystemEndpoint())
-        self.client = BranchFileSystemClient(
+
+    def makeClient(self):
+        return BranchFileSystemClient(
             self._xmlrpc_client, self.user.id)
 
     def test_translatePath(self):
         branch = self.factory.makeBranch()
-        deferred = self.client.translatePath('/' + branch.unique_name)
+        client = self.makeClient()
+        deferred = client.translatePath('/' + branch.unique_name)
         deferred.addCallback(
             self.assertEqual,
             (BRANCH_TRANSPORT, dict(id=branch.id, writable=False), ''))
@@ -41,8 +44,9 @@ class TestBranchFileSystemClient(TestCase):
         # matched. _getMatchedPart returns that part, based on the path given
         # and the returned data.
         branch = self.factory.makeBranch()
+        client = self.makeClient()
         requested_path = '/%s/a/b' % branch.unique_name
-        matched_part = self.client._getMatchedPart(
+        matched_part = client._getMatchedPart(
             requested_path,
             (BRANCH_TRANSPORT, {'id': branch.id, 'writable': False}, 'a/b'))
         self.assertEqual('/%s' % branch.unique_name, matched_part)
@@ -54,8 +58,9 @@ class TestBranchFileSystemClient(TestCase):
         #
         # This test is added to exercise a corner case.
         branch = self.factory.makeBranch()
+        client = self.makeClient()
         requested_path = '/%s' % branch.unique_name
-        matched_part = self.client._getMatchedPart(
+        matched_part = client._getMatchedPart(
             requested_path,
             (BRANCH_TRANSPORT, {'id': branch.id, 'writable': False}, ''))
         self.assertEqual('/%s' % branch.unique_name, matched_part)
@@ -67,8 +72,9 @@ class TestBranchFileSystemClient(TestCase):
         #
         # This test is added to exercise a corner case.
         branch = self.factory.makeBranch()
+        client = self.makeClient()
         requested_path = '/%s/' % branch.unique_name
-        matched_part = self.client._getMatchedPart(
+        matched_part = client._getMatchedPart(
             requested_path,
             (BRANCH_TRANSPORT, {'id': branch.id, 'writable': False}, ''))
         self.assertEqual('/%s' % branch.unique_name, matched_part)
@@ -77,10 +83,11 @@ class TestBranchFileSystemClient(TestCase):
         # We can retrieve data that we've added to the cache. The data we
         # retrieve looks an awful lot like the data that the endpoint sends.
         branch = self.factory.makeBranch()
+        client = self.makeClient()
         fake_data = self.factory.getUniqueString()
-        self.client._addToCache(
+        client._addToCache(
             (BRANCH_TRANSPORT, fake_data, ''), '/%s' % branch.unique_name)
-        result = self.client._getFromCache('/%s/foo/bar' % branch.unique_name)
+        result = client._getFromCache('/%s/foo/bar' % branch.unique_name)
         self.assertEqual(
             (BRANCH_TRANSPORT, fake_data, 'foo/bar'), result)
 
@@ -89,30 +96,33 @@ class TestBranchFileSystemClient(TestCase):
         # the requested path. Simple string prefixing is not enough. Added to
         # trap bug 308077.
         branch = self.factory.makeBranch()
+        client = self.makeClient()
         fake_data = self.factory.getUniqueString()
-        self.client._addToCache(
+        client._addToCache(
             (BRANCH_TRANSPORT, fake_data, ''), '/%s' % branch.unique_name)
         self.assertRaises(
             NotInCache,
-            self.client._getFromCache, '/%s-suffix' % branch.unique_name)
+            client._getFromCache, '/%s-suffix' % branch.unique_name)
 
     def test_not_in_cache(self):
         # _getFromCache raises an error when the given path isn't in the
         # cache.
+        client = self.makeClient()
         self.assertRaises(
-            NotInCache, self.client._getFromCache, "foo")
+            NotInCache, client._getFromCache, "foo")
 
     def test_translatePath_retrieves_from_cache(self):
         # If the path already has a prefix in the cache, we use that prefix to
         # translate the path.
         branch = self.factory.makeBranch()
+        client = self.makeClient()
         # We'll store fake data in the cache to show that we get data from
         # the cache if it's present.
         fake_data = self.factory.getUniqueString()
-        self.client._addToCache(
+        client._addToCache(
             (BRANCH_TRANSPORT, fake_data, ''), '/%s' % branch.unique_name)
         requested_path = '/%s/foo/bar' % branch.unique_name
-        deferred = self.client.translatePath(requested_path)
+        deferred = client.translatePath(requested_path)
         def path_translated((transport_type, data, trailing_path)):
             self.assertEqual(BRANCH_TRANSPORT, transport_type)
             self.assertEqual(fake_data, data)
@@ -123,22 +133,24 @@ class TestBranchFileSystemClient(TestCase):
         # translatePath adds successful path translations to the cache, thus
         # allowing for future translations to be retrieved from the cache.
         branch = self.factory.makeBranch()
-        deferred = self.client.translatePath('/' + branch.unique_name)
+        client = self.makeClient()
+        deferred = client.translatePath('/' + branch.unique_name)
         deferred.addCallback(
             self.assertEqual,
-            self.client._getFromCache('/' + branch.unique_name))
+            client._getFromCache('/' + branch.unique_name))
         return deferred
 
     def test_translatePath_control_branch_cache_interaction(self):
         # We don't want the caching to make us mis-interpret paths in the
         # branch as paths into the control transport.
         branch = self.factory.makeBranch()
+        client = self.makeClient()
         self.factory.enableDefaultStackingForProduct(branch.product)
-        deferred = self.client.translatePath(
+        deferred = client.translatePath(
             '/~' + branch.owner.name + '/' + branch.product.name +
             '/.bzr/format')
         def call_translatePath_again(ignored):
-            return self.client.translatePath('/' + branch.unique_name)
+            return client.translatePath('/' + branch.unique_name)
         def check_results((transport_type, data, trailing_path)):
             self.assertEqual(BRANCH_TRANSPORT, transport_type)
         deferred.addCallback(call_translatePath_again)
@@ -147,13 +159,14 @@ class TestBranchFileSystemClient(TestCase):
 
     def test_errors_not_cached(self):
         # Don't cache failed translations. What would be the point?
-        deferred = self.client.translatePath('/foo/bar/baz')
+        client = self.makeClient()
+        deferred = client.translatePath('/foo/bar/baz')
         def translated_successfully(result):
             self.fail(
                 "Translated successfully. Expected error, got %r" % result)
         def failed_translation(failure):
             self.assertRaises(
-                NotInCache, self.client._getFromCache, '/foo/bar/baz')
+                NotInCache, client._getFromCache, '/foo/bar/baz')
         return deferred.addCallbacks(
             translated_successfully, failed_translation)
 
