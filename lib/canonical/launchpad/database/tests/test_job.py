f43a0841ec7eb0d03304f05e430a42980f631c37
Aaron Bentley <aaron@canonical.com>
2008-12-16 12:49:05 -0500

    Update test docs

--- lib/canonical/launchpad/database/tests/test_job.py
+++ lib/canonical/launchpad/database/tests/test_job.py
@@ -18,13 +18,19 @@ class TestJob(TestCase):
     layer = LaunchpadZopelessLayer
 
     def test_implements_IJob(self):
+        """Job should implement IJob."""
         verifyObject(IJob, Job())
 
     def test_default_status(self):
+        """The default status should be WAITING."""
         job = Job()
         self.assertEqual(job.status, JobStatus.WAITING)
 
     def test_start(self):
+        """Job.start should update the object appropriately.
+
+        It should set date_started, clear date_finished, and set the status to
+        RUNNING."""
         job = Job(date_finished=UTC_NOW)
         self.assertEqual(None, job.date_started)
         self.assertNotEqual(None, job.date_finished)
@@ -34,6 +40,7 @@ class TestJob(TestCase):
         self.assertEqual(job.status, JobStatus.RUNNING)
 
     def test_start_increments_attempt_count(self):
+        """Job.start should increment the attempt count."""
         job = Job(date_finished=UTC_NOW)
         self.assertEqual(0, job.attempt_count)
         job.start()
@@ -43,18 +50,25 @@ class TestJob(TestCase):
         self.assertEqual(2, job.attempt_count)
 
     def test_start_when_completed(self):
+        """When a job is completed, attempting to start is invalid."""
         job = Job(_status=JobStatus.COMPLETED)
         self.assertRaises(InvalidTransition, job.start)
 
     def test_start_when_failed(self):
+        """When a job is failed, attempting to start is invalid."""
         job = Job(_status=JobStatus.FAILED)
         self.assertRaises(InvalidTransition, job.start)
 
     def test_start_when_running(self):
+        """When a job is running, attempting to start is invalid."""
         job = Job(_status=JobStatus.FAILED)
         self.assertRaises(InvalidTransition, job.start)
 
     def test_complete(self):
+        """Job.complete should update the Job appropriately.
+
+        It should set date_finished and set the job status to COMPLETED.
+        """
         job = Job(_status=JobStatus.RUNNING)
         self.assertEqual(None, job.date_finished)
         job.complete()
@@ -62,18 +76,25 @@ class TestJob(TestCase):
         self.assertEqual(job.status, JobStatus.COMPLETED)
 
     def test_complete_waiting(self):
+        """When a job is waiting, attempting to complete is invalid."""
         job = Job(_status=JobStatus.WAITING)
         self.assertRaises(InvalidTransition, job.complete)
 
     def test_complete_completed(self):
+        """When a job is completed, attempting to complete is invalid."""
         job = Job(_status=JobStatus.COMPLETED)
         self.assertRaises(InvalidTransition, job.complete)
 
     def test_complete_failed(self):
+        """When a job is failed, attempting to complete is invalid."""
         job = Job(_status=JobStatus.FAILED)
         self.assertRaises(InvalidTransition, job.complete)
 
     def test_fail(self):
+        """Job.fail should update the Job appropriately.
+
+        It should set date_finished and set the job status to FAILED.
+        """
         job = Job(_status=JobStatus.RUNNING)
         self.assertEqual(None, job.date_finished)
         job.fail()
@@ -81,18 +102,25 @@ class TestJob(TestCase):
         self.assertEqual(job.status, JobStatus.FAILED)
 
     def test_fail_waiting(self):
+        """When a job is waiting, attempting to fail is invalid."""
         job = Job(_status=JobStatus.WAITING)
         self.assertRaises(InvalidTransition, job.fail)
 
     def test_fail_completed(self):
+        """When a job is completed, attempting to fail is invalid."""
         job = Job(_status=JobStatus.COMPLETED)
         self.assertRaises(InvalidTransition, job.fail)
 
     def test_fail_failed(self):
+        """When a job is failed, attempting to fail is invalid."""
         job = Job(_status=JobStatus.FAILED)
         self.assertRaises(InvalidTransition, job.fail)
 
     def test_queue(self):
+        """Job.queue should update the job appropriately.
+
+        It should set date_finished, and set status to WAITING.
+        """
         job = Job(_status=JobStatus.RUNNING)
         self.assertEqual(None, job.date_finished)
         job.queue()
@@ -100,14 +128,17 @@ class TestJob(TestCase):
         self.assertEqual(job.status, JobStatus.WAITING)
 
     def test_queue_completed(self):
+        """When a job is completed, attempting to queue is invalid."""
         job = Job(_status=JobStatus.COMPLETED)
         self.assertRaises(InvalidTransition, job.queue)
 
     def test_queue_waiting(self):
+        """When a job is waiting, attempting to queue is invalid."""
         job = Job(_status=JobStatus.WAITING)
         self.assertRaises(InvalidTransition, job.queue)
 
     def test_queue_failed(self):
+        """When a job is failed, attempting to queue is invalid."""
         job = Job(_status=JobStatus.FAILED)
         self.assertRaises(InvalidTransition, job.queue)
 
