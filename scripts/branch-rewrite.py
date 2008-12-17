a3ada33ec96c6b2f88eefcac05ae32a7157eb411
Michael Hudson <michael.hudson@canonical.com>
2008-12-17 14:08:55 +1300

    start using usual script infrastructure

--- scripts/branch-rewrite.py
+++ scripts/branch-rewrite.py
@@ -9,6 +9,7 @@ import xmlrpclib
 
 from canonical.codehosting import branch_id_to_path
 from canonical.config import config
+from canonical.launchpad.scripts.base import LaunchpadScript
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(levelname)s %(message)s',
@@ -16,27 +17,42 @@ logging.basicConfig(level=logging.DEBUG,
 
 log = logging.getLogger('BranchRewrite')
 
-log.debug("Starting up...")
-
 s = xmlrpclib.ServerProxy(config.codehosting.branchfs_endpoint)
 
-while True:
-    try:
-        line = sys.stdin.readline().strip()
-        T = time.time()
-        trailingSlash = line.endswith('/')
-        log.info(repr(line))
-        transport_type, info, trailing = s.translatePath(
-            "+launchpad-services", line)
-        if transport_type == 'BRANCH_TRANSPORT':
-            if trailing.startswith('.bzr'):
-                r = '/' + branch_id_to_path(info['id']) + '/' + trailing
-                if trailingSlash:
-                    r += '/'
-            else:
-                r = 'http://localhost:8080' + line
-        log.debug("%r -> %r (%fs)", line, r, time.time() - T)
-        print r
-    except:
-        log.exception('oops')
-        print 'notfound'
+class BranchRewriter(LaunchpadScript):
+
+    def run(self, use_web_security=False, implicit_begin=True,
+            isolation=None):
+        """See `LaunchpadScript.run`.
+
+        We override to avoid all of the setting up all of the component
+        architecture and connecting to the database.
+        """
+        self.main()
+
+    def main(self):
+        log.debug("Starting up...")
+        while True:
+            try:
+                line = sys.stdin.readline().strip()
+                T = time.time()
+                trailingSlash = line.endswith('/')
+                log.info(repr(line))
+                transport_type, info, trailing = s.translatePath(
+                    "+launchpad-services", line)
+                if transport_type == 'BRANCH_TRANSPORT':
+                    if trailing.startswith('.bzr'):
+                        r = '/' + branch_id_to_path(info['id']) + '/' + trailing
+                        if trailingSlash:
+                            r += '/'
+                    else:
+                        r = 'http://localhost:8080' + line
+                log.debug("%r -> %r (%fs)", line, r, time.time() - T)
+                print r
+            except:
+                log.exception('oops')
+                print 'notfound'
+
+
+if __name__ == '__main__':
+    BranchRewriter("branch-rewrite").run()
