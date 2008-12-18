2c4ffb8a6049dd6cc16166edb97737e0f3e9d508
Francis J. Lacoste <francis.lacoste@canonical.com>
2008-12-18 18:02:09 -0500

    Rename icing/src to ../icing-sources

--- lib/canonical/launchpad/browser/launchpad.py
+++ lib/canonical/launchpad/browser/launchpad.py
@@ -759,16 +759,6 @@ class IcingFolder(ExportedFolder):
     folder = os.path.join(
         os.path.dirname(os.path.realpath(__file__)), '../icing/')
 
-    def publishTraverse(self, request, name):
-        """Hide the source directory.
-
-        The source directory contains source material that we don't want
-        published over the web.
-        """
-        if name == 'src':
-            raise NotFound(request, name)
-        return super(IcingFolder, self).publishTraverse(request, name)
-
 
 class LaunchpadImageFolder(ExportedImageFolder):
     """Export the Launchpad images - supporting retrieval without extension.
