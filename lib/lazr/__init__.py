02b792eaf66e524c462591d863c5feaec64ab721
Gary Poster <gary.poster@canonical.com>
2008-12-19 15:41:31 -0500

    adjust based on reviews

--- lib/lazr/__init__.py
+++ lib/lazr/__init__.py
@@ -1,2 +1,2 @@
-# this is a namespace package for zc.* packages.  We should absolutely get rid
+# This is a namespace package for lazr.* packages.  We should absolutely get rid
 # of this once we are using eggs.
