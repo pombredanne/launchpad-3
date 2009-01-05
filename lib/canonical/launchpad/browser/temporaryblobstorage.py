942cf8bebd99f0cbe82cc65d2a89ad107b0f4096
Guilherme Salgado <salgado@canonical.com>
2009-01-05 12:13:53 -0200

    Convert TemporaryBlobStorageAddView into a LFV

--- lib/canonical/launchpad/browser/temporaryblobstorage.py
+++ lib/canonical/launchpad/browser/temporaryblobstorage.py
@@ -9,21 +9,38 @@ __all__ = [
 
 from zope.component import getUtility
 
-from canonical.launchpad.interfaces import ITemporaryStorageManager
-from canonical.launchpad.webapp import GeneralFormView
-from canonical.launchpad.interfaces import BlobTooLarge
+from canonical.launchpad.webapp.launchpadform import action, LaunchpadFormView
+
+from canonical.launchpad.interfaces.temporaryblobstorage import (
+    BlobTooLarge, ITemporaryBlobStorage, ITemporaryStorageManager)
 from canonical.librarian.interfaces import UploadFailed
 
 
-class TemporaryBlobStorageAddView(GeneralFormView):
+class TemporaryBlobStorageAddView(LaunchpadFormView):
+    schema = ITemporaryBlobStorage
+    label = 'Store BLOB'
+    field_names = ['blob']
+    for_input = True
 
-    def process(self, blob):
+    def initialize(self):
+        # Need this hack here to ensure Action.__get__ doesn't add the view's
+        # prefix to the action's __name__.  See note below to understand why
+        # we need the action's name to be FORM_SUBMIT.
+        self.actions = [action for action in self.actions]
+        self.actions[0].__name__ = 'FORM_SUBMIT'
+        super(TemporaryBlobStorageAddView, self).initialize()
+
+    # This action is named FORM_SUBMIT because apport depends on it being
+    # named like that.
+    @action('Continue', name='FORM_SUBMIT')
+    def continue_action(self, action, data):
         try:
-            uuid = getUtility(ITemporaryStorageManager).new(blob)
+            uuid = getUtility(ITemporaryStorageManager).new(data['blob'])
             self.request.response.setHeader('X-Launchpad-Blob-Token', uuid)
-            return 'Your ticket is "%s"' % uuid
+            self.request.response.addInfoNotification(
+                'Your ticket is "%s"' % uuid)
         except BlobTooLarge:
-            return 'Uploaded file was too large.'
+            self.addError('Uploaded file was too large.')
         except UploadFailed:
-            return 'File storage unavailable - try again later.'
+            self.addError('File storage unavailable - try again later.')
 
