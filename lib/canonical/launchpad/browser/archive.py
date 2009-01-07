016c15cc430ce4fcea299b5979553c7e2579ab30
Michael Nelson <michael.nelson@canonical.com>
2009-01-07 10:26:39 +0100

    Fixes bug 311889 by adding a filter for distro-series on the ppa/archive pages.

--- lib/canonical/launchpad/browser/archive.py
+++ lib/canonical/launchpad/browser/archive.py
@@ -353,6 +353,20 @@ class ArchiveViewBase:
             ]
         return SimpleVocabulary(status_terms)
 
+    @cachedproperty
+    def simplified_series_vocabulary(self):
+        """Return a simplified distro series vocabulary.
+
+        Allows users to select between a valid series for the archive
+        distribution, or 'Any Series'.
+        """
+        series_terms = [SimpleTerm(None, token='any', title='Any Series')]
+        for distroseries in self.context.series_with_sources:
+            series_terms.append(
+                SimpleTerm(distroseries, token=distroseries.name,
+                           title=distroseries.displayname))
+        return SimpleVocabulary(series_terms)
+
     @property
     def archive_url(self):
         """Return an archive_url where available, or None."""
@@ -396,6 +410,7 @@ class ArchiveView(ArchiveViewBase, LaunchpadView):
         """
         self.setupSourcesListEntries()
         self.setupStatusFilterWidget()
+        self.setupSeriesFilterWidget()
         self.setupPackageBatchResult()
 
     def setupStatusFilterWidget(self):
@@ -418,6 +433,26 @@ class ArchiveView(ArchiveViewBase, LaunchpadView):
         return self.status_filter_widget.renderValue(
             self.selected_status_filter.value)
 
+    def setupSeriesFilterWidget(self):
+        """Build a customized archive series select widget.
+
+        See `ArchiveViewBase.simplified_series_vocabulary`.
+        """
+        series_filter = self.request.get('field.series_filter', 'any')
+        self.selected_series_filter = (
+            self.simplified_series_vocabulary.getTermByToken(series_filter))
+
+        field = Choice(
+            __name__ = 'series_filter', title=_("Series Filter"),
+            vocabulary=self.simplified_series_vocabulary, required=True)
+        setUpWidget(self, 'series_filter', field, IInputWidget)
+
+    @property
+    def plain_series_filter_widget(self):
+        """Render a <select> control with no <div>s around it."""
+        return self.series_filter_widget.renderValue(
+            self.selected_series_filter.value)
+
     def setupSourcesListEntries(self):
         """Setup of the sources list entries widget."""
         entries = SourcesListEntries(
@@ -439,7 +474,8 @@ class ArchiveView(ArchiveViewBase, LaunchpadView):
         name_filter = self.request.get('field.name_filter')
         return self.context.getPublishedSources(
             name=name_filter,
-            status=self.selected_status_filter.value.collection)
+            status=self.selected_status_filter.value.collection,
+            distroseries=self.selected_series_filter.value)
 
     def setupPackageBatchResult(self):
         """Setup of the package search results."""
