33b808c77c12c05ac546c41579ca23616772308a
Michael Nelson <michael.nelson@canonical.com>
2009-01-09 12:54:12 +0100

    Added tests to ensure that getBuildCounters is not publically accessible, and updated IArchive to ensure the test succeeds.

--- lib/canonical/launchpad/interfaces/archive.py
+++ lib/canonical/launchpad/interfaces/archive.py
@@ -277,34 +277,6 @@ class IArchivePublic(IHasOwner):
         :return: SelectResults containing `IBinaryPackagePublishingHistory`.
         """
 
-    @operation_parameters(
-        include_needsbuild=Bool(
-            title=_("Include builds with state NEEDSBUILD"), required=False))
-    @export_read_operation()
-    def getBuildCounters(include_needsbuild=True):
-        """Return a dictionary containing the build counters for an archive.
-
-        This is necessary currently because the IArchive.failed_builds etc.
-        counters are not in use.
-
-        The returned dictionary contains the follwoing keys and values:
-
-         * 'total': total number of builds (includes SUPERSEDED);
-         * 'pending': number of builds in BUILDING or NEEDSBUILD state;
-         * 'failed': number of builds in FAILEDTOBUILD, MANUALDEPWAIT,
-           CHROOTWAIT and FAILEDTOUPLOAD state;
-         * 'succeeded': number of SUCCESSFULLYBUILT builds.
-         * 'superseded': number of SUPERSEDED builds.
-
-        :param include_needsbuild: Indicates whether to include builds with
-            the status NEEDSBUILD in the pending and total counts. This is
-            useful in situations where a build that hasn't started isn't
-            considered a build by the user.
-        :type include_needsbuild: ``bool``
-        :return: a dictionary with the 4 keys specified above.
-        :rtype: ``dict``.
-        """
-
     def allowUpdatesToReleasePocket():
         """Return whether the archive allows publishing to the release pocket.
 
@@ -650,6 +622,34 @@ class IArchiveView(Interface):
                              matching.
 
         :return: SelectResults containing `ISourcePackagePublishingHistory`.
+        """
+
+    @operation_parameters(
+        include_needsbuild=Bool(
+            title=_("Include builds with state NEEDSBUILD"), required=False))
+    @export_read_operation()
+    def getBuildCounters(include_needsbuild=True):
+        """Return a dictionary containing the build counters for an archive.
+
+        This is necessary currently because the IArchive.failed_builds etc.
+        counters are not in use.
+
+        The returned dictionary contains the follwoing keys and values:
+
+         * 'total': total number of builds (includes SUPERSEDED);
+         * 'pending': number of builds in BUILDING or NEEDSBUILD state;
+         * 'failed': number of builds in FAILEDTOBUILD, MANUALDEPWAIT,
+           CHROOTWAIT and FAILEDTOUPLOAD state;
+         * 'succeeded': number of SUCCESSFULLYBUILT builds.
+         * 'superseded': number of SUPERSEDED builds.
+
+        :param include_needsbuild: Indicates whether to include builds with
+            the status NEEDSBUILD in the pending and total counts. This is
+            useful in situations where a build that hasn't started isn't
+            considered a build by the user.
+        :type include_needsbuild: ``bool``
+        :return: a dictionary with the 4 keys specified above.
+        :rtype: ``dict``.
         """
 
 
