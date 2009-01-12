1f2518acb97111893c219e844bc357e2c3b6c656
Michael Hudson <michael.hudson@canonical.com>
2009-01-13 10:23:39 +1300

    revert that bit

--- lib/canonical/codehosting/__init__.py
+++ lib/canonical/codehosting/__init__.py
@@ -33,7 +33,8 @@ def get_bzr_plugins_path():
     """Find the path to the Bazaar plugins for this rocketfuel instance"""
     return get_rocketfuel_root() + '/bzrplugins'
 
+os.environ['BZR_PLUGIN_PATH'] = get_bzr_plugins_path()
 
 # We want to have full access to Launchpad's Bazaar plugins throughout the
 # codehosting package.
-load_plugins([get_bzr_plugins_path()])
+load_plugins()
