885142549a072dfb7367099fe045baf278a7ec0d
Jonathan Lange <jml@canonical.com>
2009-01-19 15:42:30 +1100

    Not part of this thread.

--- lib/canonical/launchpad/interfaces/branchcontainer.py
+++ lib/canonical/launchpad/interfaces/branchcontainer.py
@@ -10,25 +10,6 @@ __all__ = [
 from zope.interface import Interface, Attribute
 
 
-# XXX: Add display name.
-# XXX: Add 'branches' or __iter__ feature
-# XXX: filter by "visible to user"
-# XXX: Add 'revisions' property or 'getRecentRevisions' method
-# XXX: Add branch count
-# XXX: Add recent revision count
-# XXX: Add recent committer count
-# XXX: Add team_owner_count
-# XXX: Add person_owner_count
-# XXX: Possibly add a group-by-namespaces feature
-# XXX: filter by lifecyle
-# XXX: make sure we can get canonical_url of container
-# XXX: make adapters for IProduct, IPerson, ISourcePackage
-# XXX: somehow deal with sort
-# XXX: registered by person
-# XXX: owned by person
-# XXX: subscribed by person
-
-
 class IBranchContainer(Interface):
     """A container of branches.
 
