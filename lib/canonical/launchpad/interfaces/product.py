ead0d43d9eb6a11fe9714475e0651663bef1c52b
Graham Binns <graham@canonical.com>
2009-01-22 12:53:28 +0000

    Added tests and implementation of Product.upstream_bug_filing_url.

--- lib/canonical/launchpad/interfaces/product.py
+++ lib/canonical/launchpad/interfaces/product.py
@@ -479,8 +479,12 @@ class IProductPublic(
     remote_product = exported(
         TextLine(
             title=_('Remote product'),
+            readonly=True,
             description=_(
                 "The ID of this project on its remote bug tracker.")))
+
+    upstream_bug_filing_url = Attribute(
+        "The URL of bug filing form on this product's upstream bug tracker")
 
     def redeemSubscriptionVoucher(voucher, registrant, purchaser,
                                   subscription_months, whiteboard=None,
