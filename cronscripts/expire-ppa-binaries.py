aa71981307dd124648408331fe013fe8e108b201
Julian Edwards <julian.edwards@canonical.com>
2009-01-22 12:43:00 +0000

    Split cronscript out to a separate script file so it can be tested more easily.

--- cronscripts/expire-ppa-binaries.py
+++ cronscripts/expire-ppa-binaries.py
@@ -9,71 +9,9 @@
 
 import _pythonpath
 
-from zope.component import getUtility
-
 from canonical.config import config
-from canonical.launchpad.scripts.base import LaunchpadCronScript
-from canonical.launchpad.webapp.interfaces import (
-    IStoreSelector, MAIN_STORE, DEFAULT_FLAVOR)
+from canonical.launchpad.scripts.expire-ppa-binaries import PPABinaryExpirer
 
-
-class PPABinaryExpirer(LaunchpadCronScript):
-    """Helper class for expiring old PPA binaries.
-    
-    Any PPA binary older than 30 days that is superseded or deleted
-    will be marked for immediate expiry.  The associated publishing
-    record is also marked as OBSOLETE so that it is prevented from
-    being copied in the future.
-    """
-
-    def main(self):
-        self.logger.info('Starting the PPA binary expiration')
-
-        store = getUtility(IStoreSelector).get(MAIN_STORE, DEFAULT_FLAVOR)
-
-        # A temp table is used to collect the necessary row IDs for
-        # updating, since we need to update two tables at once and the
-        # selection criteria is time-based (which would change on the
-        # second query if done with two).
-
-        store.execute("""
-        CREATE TEMP TABLE tmp_lfa_expiration_data (
-            lfa_id integer,
-            bpph_id integer);
-
-        INSERT INTO tmp_lfa_expiration_data (lfa_id, bpph_id)
-        SELECT
-            lfa.id AS lfa_id, bpph.id AS bpph_id
-        FROM
-            libraryfilealias lfa,
-            binarypackagefile bpf,
-            binarypackagerelease bpr,
-            binarypackagepublishinghistory bpph,
-            archive
-        WHERE
-            lfa.id = bpf.libraryfile AND
-            bpr.id = bpf.binarypackagerelease AND
-            bpph.binarypackagerelease = bpr.id AND
-            bpph.status IN (3,4) AND
-            bpph.dateremoved < (now() - interval '30 days') AND
-            archive.id = bpph.archive AND
-            archive.purpose = 2;
-
-        UPDATE libraryfilealias
-        SET expires=now()
-        FROM tmp_lfa_expiration_data
-        WHERE libraryfilealias.id = tmp_lfa_expiration_data.lfa_id;
-
-        UPDATE securebinarypackagepublishinghistory
-        SET status = 5
-        FROM tmp_lfa_expiration_data
-        WHERE
-            securebinarypackagepublishinghistory.id = 
-                tmp_lfa_expiration_data.bpph_id;
-        """)
-
-        self.txn.commit()
-        self.logger.info('Finished the package cache update')
 
 if __name__ == '__main__':
     script = PPABinaryExpirer(
---
+++ lib/canonical/launchpad/scripts/expire_ppa_binaries.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python2.4
+
+# Copyright 2009 Canonical Ltd.  All rights reserved.
+# pylint: disable-msg=C0103,W0403
+
+# This script expires PPA binaries that are superseded or deleted, and
+# are older than 30 days.  It's done with pure SQL rather than Python
+# for speed reasons.
+
+from zope.component import getUtility
+
+from canonical.launchpad.scripts.base import LaunchpadCronScript
+from canonical.launchpad.webapp.interfaces import (
+    IStoreSelector, MAIN_STORE, DEFAULT_FLAVOR)
+
+
+class PPABinaryExpirer(LaunchpadCronScript):
+    """Helper class for expiring old PPA binaries.
+    
+    Any PPA binary older than 30 days that is superseded or deleted
+    will be marked for immediate expiry.  The associated publishing
+    record is also marked as OBSOLETE so that it is prevented from
+    being copied in the future.
+    """
+
+    def main(self):
+        self.logger.info('Starting the PPA binary expiration')
+
+        stay_of_execution = '30 days'
+
+        store = getUtility(IStoreSelector).get(MAIN_STORE, DEFAULT_FLAVOR)
+
+        store.execute("""
+        UPDATE libraryfilealias
+        SET expires=now()
+        FROM
+            libraryfilealias lfa,
+            binarypackagefile bpf,
+            binarypackagerelease bpr,
+            binarypackagepublishinghistory bpph,
+            archive
+        WHERE
+            lfa.id = bpf.libraryfile AND
+            bpr.id = bpf.binarypackagerelease AND
+            bpph.binarypackagerelease = bpr.id AND
+            bpph.dateremoved < (now() - interval '%s') AND
+            archive.id = bpph.archive AND
+            archive.purpose = %s AND
+            NOT EXISTS (
+                SELECT TRUE FROM binarypackagepublishinghistory as bpph2,
+                                 binarypackagerelease as bpr2
+                WHERE
+                    bpph2.binarypackagerelease = bpph.binarypackagerelease
+                    AND
+                    (now() - bpph2.dateremoved < interval '%s'
+                     OR
+                     dateremoved IS NULL
+                    )
+            );
+        """ % (stay_of_execution, ArchivePurpose.PPA, stay_of_execution)
+
+        self.txn.commit()
+        self.logger.info('Finished PPA binary expiration')
+
