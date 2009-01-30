7c6ee7276b0d4ab5f929a17bdd78b6051cd10810
Paul Hummer <paul.hummer@canonical.com>
2009-01-29 22:13:39 -0700

    Added a failing test

--- lib/canonical/launchpad/mail/tests/test_handlers.py
+++ lib/canonical/launchpad/mail/tests/test_handlers.py
@@ -505,6 +505,29 @@ class TestCodeHandler(TestCaseWithFactory):
         self.assertEqual(notification['to'],
             message['from'])
 
+    def test_processMissingMergeDirective(self):
+        """process sends an email if the original email lacks an attachment.
+        """
+        _unused, source, target = self.makeMergeDirectiveEmail()
+        message = self.factory.makeSignedMessage(body='A body',
+            subject='A subject', attachment_contents='')
+        self.switchDbUser(config.processmail.dbuser)
+        code_handler = CodeHandler()
+        bmp, comment = code_handler.processMergeProposal(message)
+        transaction.commit()
+        [notification] = pop_notifications()
+
+        self.assertEqual(
+            notification['Subject'], 'Error Creating Merge Proposal')
+        self.assertEqual(
+            notification.get_payload(),
+            'Your email did not contain a merge directive. Please resend '
+            'your email with the merge directive attached.'
+            )
+        self.assertEqual(notification['to'],
+            message['from'])
+
+
 
 class TestVoteEmailCommand(TestCase):
     """Test the vote and tag processing of the VoteEmailCommand."""
