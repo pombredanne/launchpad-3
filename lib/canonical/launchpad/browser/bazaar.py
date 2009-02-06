458b4b044467f2071b2005fa94a07d66dc963664
Jonathan Lange <jml@canonical.com>
2009-02-06 16:08:31 +1100

    Change the way we fetch the tag data so that:   - we only use one query   - we show the most active projects first, rather than the first     sorted alphabetically   - official_codehosting is used, rather than dev focus branch presence   - we don't include branches with no revisions in the count   - we don't include projects with no branches in the cloud

--- lib/canonical/launchpad/browser/bazaar.py
+++ lib/canonical/launchpad/browser/bazaar.py
@@ -24,6 +24,8 @@ from canonical.launchpad.interfaces.launchpad import IBazaarApplication
 from canonical.launchpad.interfaces.product import IProduct, IProductSet
 from canonical.launchpad.webapp import (
     ApplicationMenu, enabled_with_permission, LaunchpadView, Link)
+from canonical.launchpad.webapp.interfaces import (
+    IStoreSelector, MAIN_STORE, SLAVE_FLAVOR)
 
 from lazr.delegates import delegates
 
@@ -99,6 +101,57 @@ class ProductInfo:
         self.elapsed_since_commit = elapsed
         self.important = important
 
+    @classmethod
+    def _findProductInfo(cls, num_products):
+        """Get products with their branch activity information.
+
+        :return: a `ResultSet` of (product, num_branches, last_revision_date).
+        """
+        from canonical.launchpad.database import Branch, Product, Revision
+        from canonical.launchpad.interfaces.branch import BranchType
+        from storm.locals import Count, Max, Or
+        # It doesn't matter if this query is even a whole day out of date, so
+        # use the slave store.
+        store = getUtility(IStoreSelector).get(MAIN_STORE, SLAVE_FLAVOR)
+        # Get all products, the count of all hosted & mirrored branches and
+        # the last revision date.
+        result = store.find(
+            (Product, Count(Branch.id), Max(Revision.revision_date)),
+            Branch.product == Product.id,
+            Or(Branch.branch_type == BranchType.HOSTED,
+               Branch.branch_type == BranchType.MIRRORED),
+            Branch.last_scanned_id == Revision.revision_id).group_by(Product)
+        if num_products:
+            result.config(limit=num_products)
+        return result.order_by(Count(Branch.id))
+
+    @classmethod
+    def getProductInfo(cls, num_products=None):
+        product_info = sorted(
+            list(cls._findProductInfo(num_products)),
+            key=lambda data: data[0].name)
+        now = datetime.today()
+        counts = sorted(zip(*product_info)[1])
+        # Lowest half are small.
+        small_count = counts[len(counts)/2]
+        # Top 20% are big.
+        large_count = counts[-(len(counts)/5)]
+        for product, num_branches, last_revision_date in product_info:
+            # Projects with no branches are not interesting.
+            if num_branches == 0:
+                continue
+            if num_branches > large_count:
+                branch_size = 'large'
+            elif num_branches < small_count:
+                branch_size = 'small'
+            else:
+                branch_size = 'medium'
+            elapsed = now - last_revision_date
+            # We want to highlight products that actually _use_ Launchpad.
+            important = product.official_codehosting
+            yield cls(product, num_branches, branch_size, elapsed, important)
+
+
     @property
     def branch_class(self):
         return "cloud-size-%s" % self.branch_size
@@ -148,6 +201,7 @@ class BazaarProductView:
     """Browser class for products gettable with Bazaar."""
 
     def products(self, num_products=None):
+        return ProductInfo.getProductInfo(num_products)
         # XXX: TimPenhey 2007-02-26
         # sabdfl really wants a page that has all the products with code
         # on it.  I feel that at some stage it will just look too cumbersome,
