01fc9a2387c3ed981b2c7bc61b57f97cad662b48
Danilo Šegan <danilo@canonical.com>
2009-02-06 19:39:40 +0100

    Fix removal script to run.

--- lib/canonical/launchpad/scripts/remove_obsolete_translations.py
+++ lib/canonical/launchpad/scripts/remove_obsolete_translations.py
@@ -7,13 +7,15 @@ __all__ = ['RemoveObsoleteTranslations']
 from canonical.launchpad.webapp.interfaces import (
     IStoreSelector, AUTH_STORE, MASTER_FLAVOR)
 
+import time
+
 from zope.component import getUtility
 from zope.interface import implements
 from zope.security.proxy import removeSecurityProxy
 
 from canonical.launchpad.interfaces.looptuner import ITunableLoop
 from canonical.launchpad.scripts.base import LaunchpadScript
-from canonical.launchpad.utilities import LoopTuner
+from canonical.launchpad.utilities.looptuner import LoopTuner
 
 # Define SQL queries here to avoid cluttering up the code.
 # Query to collect all obsolete potemplates into a table
@@ -101,15 +103,37 @@ DeleteObsoletePOTemplatesSQL = """
         FROM obsolete_pots
         )"""
 
+
+def commit_transaction(transaction, logger, throttle=0.0, dry_run=False):
+    """Commit ongoing transaction, start a new one.
+
+    Pauses process execution to give the database slave a chance
+    to keep up."""
+    logger.debug("Commit point.")
+    if transaction is None:
+        return
+
+    if not dry_run:
+        transaction.commit()
+        transaction.begin()
+
+    time.sleep(float(throttle))
+
+
 class DeletionLoopRunner(object):
     implements(ITunableLoop)
 
-    def __init__(self, store, size, commitmethod):
+    def __init__(self, transaction, logger, store, size,
+                 throttle=0.0, dry_run=False):
         """Initialize the loop."""
+        self._txn = transaction
+        self._logger = logger
         self._store = store
-        self._do_commit = commitmethod
+        self._throttle = throttle
+        self._dry_run = dry_run
         self._iteration_end = size
         self._iterations_done = 0
+        self._commit_count = 0
 
     def isDone(self):
         """See ITunableLoop."""
@@ -117,11 +141,21 @@ class DeletionLoopRunner(object):
 
     def __call__(self, chunk_size):
         """See ITunableLoop."""
-        self._store.execute(
-            DeleteObsoleteTranslationmessagesSQL % (chunk_size,
-                                                    self._iterations_done))
+        query = DeleteObsoleteTranslationmessagesSQL % (chunk_size,
+                                                        self._iterations_done)
+        result = self._store.execute(query)
+        self._logger.info(
+            " * Removed another %d TranslationMessages (%d of %d)." % (
+                result.row_count(),
+                self._iterations_done + result.row_count(), 
+                self._iteration_end))
         self._iterations_done += chunk_size
-        self._do_commit()
+        commit_transaction(self._txn, self._logger, throttle=self._throttle,
+                           dry_run=self._dry_run)
+        self._commit_count += 1
+
+    def getTotalCommits(self):
+        return self._commit_count
 
 
 class RemoveObsoleteTranslations(LaunchpadScript):
@@ -133,7 +167,11 @@ class RemoveObsoleteTranslations(LaunchpadScript):
 
         self.parser.add_option(
             '-l', '--loop-timing', dest='loop_time',
-            default=5, help="Time in seconds for the loop to run and to sleep.")
+            default=5, help="Time in seconds for the loop to run.")
+
+        self.parser.add_option(
+            '-t', '--throttle', dest='throttle',
+            default=5, help="Time in seconds to sleep between commits.")
 
     def main(self):
         if self.options.dry_run:
@@ -146,35 +184,59 @@ class RemoveObsoleteTranslations(LaunchpadScript):
 
         # Working on the writable master store
         store = getUtility(IStoreSelector).get(AUTH_STORE, MASTER_FLAVOR)
+        self._store = store
 
         # Create temporary tables
         store.execute(CollectObsoletePOTemplatesSQL)
         store.execute(CollectObsoletePOFilesSQL)
         store.execute(CollectObsoleteTranslationmessagesSQL)
         self._do_commit()
-        num_tms = self._count_rows("obsolete_tms")
 
+        num_tms = self._count_rows("obsolete_tms")
+        self.logger.info(
+            "Removing %d TranslationMessage rows." % num_tms)
         # Delete these first because they reference Translationmessage
         # and POFiles
         num_pofiletranslators = store.execute(
-            DeleteObsoletePofiletranslatorsSQL).count()
+            DeleteObsoletePofiletranslatorsSQL).row_count()
         self._do_commit()
 
         # Delete the translation messages in batches because the deletion
         # is a long-running operation.
-        LoopTuner(DeletionLoopRunner(store, num_tms, self._do_commit),
-                  self.options.loop_time).run()
+        loop = DeletionLoopRunner(self.txn, self.logger, store, num_tms,
+                                  throttle=float(self.options.throttle),
+                                  dry_run=self.options.dry_run)
+        LoopTuner(loop, self.options.loop_time).run()
+        self._commit_count += loop.getTotalCommits()
 
-        self._do_commit()
         # Delete these now because they reference POTemplates and POTMsgSets
         num_templateitems = store.execute(
-            DeleteObsoleteTranslationTemplateItemsSQL).count()
+            DeleteObsoleteTranslationTemplateItemsSQL).row_count()
+        self.logger.info(
+            "Removed %d TranslationTemplateItem rows." % num_templateitems)
+        self._do_commit()
+
+        # XXX Danilo: we have to pause here for a bit to allow the transaction
+        # to catch up; if we don't do that, POTMsgSet removal might still fail.
+
         # Delete the remaining data
-        num_potmsgsets = store.execute(DeleteObsoletePOTMsgSetSQL).count()
+        num_potmsgsets = store.execute(DeleteObsoletePOTMsgSetSQL).row_count()
+        self.logger.info(
+            "Removed %d POTMsgSet rows." % num_potmsgsets)
         self._do_commit()
-        num_pofiles = store.execute(DeleteObsoletePofilesSQL).count()
-        num_pots = store.execute(DeleteObsoletePOTemplatesSQL).count()
+
+        num_pofiles = store.execute(DeleteObsoletePofilesSQL).row_count()
+        self.logger.info(
+            "Removed %d POFile rows." % num_pofiles)
         self._do_commit()
+
+        num_pots = store.execute(DeleteObsoletePOTemplatesSQL).row_count()
+        self.logger.info(
+            "Removed %d POTemplate rows." % num_pots)
+        self._do_commit()
+
+        if self.options.dry_run:
+            self.txn.abort()
 
         self.logger.info("Done.")
         self.logger.info("Deletion statistics:")
@@ -189,7 +251,7 @@ class RemoveObsoleteTranslations(LaunchpadScript):
     def _count_rows(self, tablename):
         """Helper to count all rows in a table."""
         result = self._store.execute(
-            CountRowsSQL % tablename).one()
+            CountRowsSQL % tablename).get_one()
         return result[0]
 
     def _do_commit(self):
@@ -197,17 +259,7 @@ class RemoveObsoleteTranslations(LaunchpadScript):
 
         Pauses process execution to give the database slave a chance
         to keep up."""
-        self.logger.debug("Commit point.")
+        commit_transaction(self.txn, self.logger,
+                           throttle=float(self.options.throttle),
+                           dry_run=self.options.dry_run)
         self._commit_count += 1
-
-        if self.txn is None:
-            return
-
-        if self.options.dry_run:
-            self.txn.abort()
-        else:
-            self.txn.commit()
-
-        time.sleep(self.options.loop_time)
-        self.txn.begin()
-
