7008cd1df20b844e0255186394dee540b46e0d77
Jonathan Lange <jml@canonical.com>
2009-02-10 17:07:09 +1100

    Call them collections, not subsets.

--- lib/canonical/launchpad/tests/test_branchcollection.py
+++ lib/canonical/launchpad/tests/test_branchcollection.py
@@ -35,44 +35,51 @@ class TestGenericBranchCollection(TestCaseWithFactory):
             GenericBranchCollection(self.store), IBranchCollection)
 
     def test_name(self):
-        subset = GenericBranchCollection(self.store, name="foo")
-        self.assertEqual('foo', subset.name)
+        collection = GenericBranchCollection(self.store, name="foo")
+        self.assertEqual('foo', collection.name)
 
     def test_displayname(self):
-        subset = GenericBranchCollection(self.store, displayname='Foo Bar')
-        self.assertEqual('Foo Bar', subset.displayname)
+        collection = GenericBranchCollection(
+            self.store, displayname='Foo Bar')
+        self.assertEqual('Foo Bar', collection.displayname)
 
     def test_getBranches_no_filter_no_branches(self):
         # If no filter is specified, then the collection is of all branches in
         # Launchpad. By default, there are no branches.
-        subset = GenericBranchCollection(self.store)
-        self.assertEqual([], list(subset.getBranches()))
+        collection = GenericBranchCollection(self.store)
+        self.assertEqual([], list(collection.getBranches()))
 
     def test_getBranches_no_filter(self):
         # If no filter is specified, then the collection is of all branches in
         # Launchpad.
-        subset = GenericBranchCollection(self.store)
+        collection = GenericBranchCollection(self.store)
         branch = self.factory.makeAnyBranch()
-        self.assertEqual([branch], list(subset.getBranches()))
+        self.assertEqual([branch], list(collection.getBranches()))
 
     def test_getBranches_product_filter(self):
         # If the specified filter is for the branches of a particular product,
         # then the collection contains only branches of that product.
         branch = self.factory.makeProductBranch()
         branch2 = self.factory.makeAnyBranch()
-        subset = GenericBranchCollection(
+        collection = GenericBranchCollection(
             self.store, Branch.product == branch.product)
-        all_branches = self.store.find(Branch)
-        self.assertEqual([branch], list(subset.getBranches()))
+        self.assertEqual([branch], list(collection.getBranches()))
 
     def test_count(self):
         # The 'count' property of a collection is the number of elements in
         # the collection.
-        subset = GenericBranchCollection(self.store)
-        self.assertEqual(0, subset.count)
+        collection = GenericBranchCollection(self.store)
+        self.assertEqual(0, collection.count)
         for i in range(3):
             self.factory.makeAnyBranch()
-        self.assertEqual(3, subset.count)
+        self.assertEqual(3, collection.count)
+
+    def test_count_respects_filter(self):
+        branch = self.factory.makeProductBranch()
+        branch2 = self.factory.makeAnyBranch()
+        collection = GenericBranchCollection(
+            self.store, Branch.product == branch.product)
+        self.assertEqual(1, collection.count)
 
 
 def test_suite():
