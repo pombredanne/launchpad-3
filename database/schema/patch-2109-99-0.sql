0015c094efa0df3c3033a38d2e71066b08787a23
Tom Berger <tom.berger@canonical.com>
2009-02-10 16:30:27 +0000

    rename _bin to _rank

--- database/schema/patch-2109-99-0.sql
+++ database/schema/patch-2109-99-0.sql
@@ -2,7 +2,7 @@ SET client_min_messages=ERROR;
 
 ALTER TABLE BugTask
 ADD COLUMN hotness INTEGER NOT NULL DEFAULT 0,
-ADD COLUMN hotness_bin INTEGER NOT NULL DEFAULT 0;
+ADD COLUMN hotness_rank INTEGER NOT NULL DEFAULT 0;
 
 CREATE INDEX bugtask__hotness__idx ON BugTask USING btree (hotness);
 
