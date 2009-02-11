55b3fd410d18614447947bd43d213a89045ed5cf
Paul Hummer <paul.hummer@canonical.com>
2009-02-11 14:03:56 -0700

    Cleaned up the lint

--- lib/canonical/launchpad/interfaces/codereviewcomment.py
+++ lib/canonical/launchpad/interfaces/codereviewcomment.py
@@ -18,7 +18,7 @@ from canonical.launchpad.interfaces.branchmergeproposal import (
     IBranchMergeProposal)
 from canonical.launchpad.interfaces.message import IMessage
 from canonical.lazr import DBEnumeratedType, DBItem
-from canonical.lazr.fields import Reference, ReferenceChoice
+from canonical.lazr.fields import Reference
 from canonical.lazr.rest.declarations import (
     export_as_webservice_entry, exported)
 
