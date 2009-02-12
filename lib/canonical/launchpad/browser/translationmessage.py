2ceafee2ec4672c05b968b242fa7fe893d03ae32
Michael Nelson <michael.nelson@canonical.com>
2009-02-12 20:07:36 +0100

    Found one more instance of batch-hacking on the request.form dict.

--- lib/canonical/launchpad/browser/translationmessage.py
+++ lib/canonical/launchpad/browser/translationmessage.py
@@ -248,6 +248,15 @@ class POTMsgSetBatchNavigator(BatchNavigator):
             del request.form['start']
         if 'batch' in request.form:
             del request.form['batch']
+        # Note: the BatchNavigator has now been updated so that it
+        # gets the parameters out of the request.query_string_params
+        # dict by default. Therefore, we'll remove the 'start' option
+        # from request.query_string_params as well.
+        if 'start' in request.query_string_params:
+            del request.query_string_params['start']
+        if 'batch' in request.query_string_params:
+            del request.query_string_params['batch']
+
         # 'path' will be like: 'POTURL/LANGCODE/POTSEQUENCE/+translate' and
         # we are interested on the POTSEQUENCE.
         self.start_path, pot_sequence, self.page = path.rsplit('/', 2)
