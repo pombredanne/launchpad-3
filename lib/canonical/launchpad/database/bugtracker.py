b9681764ae77f98f557519a714d5c11e3c1f8055
Graham Binns <graham@canonical.com>
2009-02-14 20:51:24 +0000

    Removed the (now unnecessary) test fixes that I had to put in when trunk broke.

--- lib/canonical/launchpad/database/bugtracker.py
+++ lib/canonical/launchpad/database/bugtracker.py
@@ -221,7 +221,7 @@ class BugTracker(SQLBase):
         if remote_product is None and self.multi_product:
             # Don't try to return anything if remote_product is required
             # for this BugTrackerType and one hasn't been passed.
-            return None
+            return bugtracker_urls
 
         if remote_product is None:
             # Turn the remote product into an empty string so that
