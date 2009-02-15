fac89200faafacfe51802b6847746b263076ac0b
Tim Penhey <tim.penhey@canonical.com>
2009-02-16 11:12:46 +1300

    Import directly from vfs.

--- cronscripts/mpcreationjobs.py
+++ cronscripts/mpcreationjobs.py
@@ -14,7 +14,7 @@ import _pythonpath
 from zope.component import getUtility
 
 from canonical.config import config
-from canonical.codehosting.vfs.branchfs import get_scanner_server
+from canonical.codehosting.vfs import get_scanner_server
 from canonical.codehosting.jobs import JobRunner
 from canonical.launchpad.interfaces.branchmergeproposal import (
     IMergeProposalCreatedJobSource,)
