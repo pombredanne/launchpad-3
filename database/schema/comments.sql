a0cda06f9b4fd5dbf7086666baf24f5d0c333770
Bjorn Tillenius <bjorn@canonical.com>
2009-02-20 13:54:40 +0200

    Add BugTask.date_milestone_set to the DB table.

--- database/schema/comments.sql
+++ database/schema/comments.sql
@@ -208,6 +208,7 @@ COMMENT ON COLUMN BugTask.date_triaged IS 'The date when this bug transitioned t
 COMMENT ON COLUMN BugTask.date_fix_committed IS 'The date when this bug transitioned to a status >= FIXCOMMITTED.';
 COMMENT ON COLUMN BugTask.date_fix_released IS 'The date when this bug transitioned to a FIXRELEASED status.';
 COMMENT ON COLUMN BugTask.date_left_closed IS 'The date when this bug last transitioned out of a CLOSED status.';
+COMMENT ON COLUMN BugTask.date_milestone_set IS 'The date when this bug was targed to the milestone that is currently set.';
 
 
 -- BugNotification
