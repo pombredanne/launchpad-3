fddbec858e7896bfc83e4324bea28e37db95166e
Jonathan Lange <jml@canonical.com>
2009-02-24 14:08:14 +1100

    These were supposed to be added ages ago. Tests & implementation of new methods for getRecent* support.

--- lib/canonical/launchpad/database/branchcollection.py
+++ lib/canonical/launchpad/database/branchcollection.py
@@ -109,6 +109,10 @@ class GenericBranchCollection:
                Branch.registrant == person,
                BranchSubscription.person == person))
 
+    def scanned(self):
+        """See `IBranchCollection`."""
+        return self._filterBy([], [Branch.last_scanned != None])
+
     def subscribedBy(self, person):
         """See `IBranchCollection`."""
         return self._filterBy(
@@ -147,6 +151,9 @@ class GenericBranchCollection:
                            TeamParticipation.person == person,
                            Branch.private == True)))
         return self._filterBy([], Branch.id.is_in(visible_branches))
+
+    def withBranchType(self, *branch_types):
+        return self._filterBy([], [Branch.branch_type.is_in(branch_types)])
 
     def withLifecycleStatus(self, *statuses):
         """See `IBranchCollection`."""
