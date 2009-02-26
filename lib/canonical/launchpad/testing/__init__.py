2b4c924127bc4bccedab0f6ac55e521855c270bf
Tim Penhey <tim.penhey@canonical.com>
2009-02-26 13:20:48 +1300

    Add a helper function to the harness.

--- lib/canonical/launchpad/testing/__init__.py
+++ lib/canonical/launchpad/testing/__init__.py
@@ -1,6 +1,8 @@
 # Copyright 2008 Canonical Ltd.  All rights reserved.
 # pylint: disable-msg=W0401,C0301
 
+__metaclass__ = type
+
 import os, shutil, tempfile, unittest
 
 from storm.store import Store
