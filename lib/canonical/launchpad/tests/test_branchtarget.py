20939ecf327122ca810c76a64ed43a08bfbc1303
Jonathan Lange <jml@canonical.com>
2009-02-27 15:49:56 +1100

    A round of golf

--- lib/canonical/launchpad/tests/test_branchtarget.py
+++ lib/canonical/launchpad/tests/test_branchtarget.py
@@ -14,110 +14,88 @@ from canonical.launchpad.webapp.interfaces import IPrimaryContext
 from canonical.testing import DatabaseFunctionalLayer
 
 
-class TestPackageBranchTarget(TestCaseWithFactory):
-
-    layer = DatabaseFunctionalLayer
-
-    def test_name(self):
-        # The name of a package context is distro/series/sourcepackage
-        sourcepackage = self.factory.makeSourcePackage()
-        target = PackageBranchTarget(sourcepackage)
-        self.assertEqual(sourcepackage.path, target.name)
+class BaseBranchTargetTests:
 
     def test_provides_IPrimaryContext(self):
-        sourcepackage = self.factory.makeSourcePackage()
-        target = PackageBranchTarget(sourcepackage)
-        self.assertProvides(target, IPrimaryContext)
-
-    def test_getNamespace(self):
-        """Get namespace produces the correct namespace."""
-        person = self.factory.makePerson()
-        sourcepackage = self.factory.makeSourcePackage()
-        target = PackageBranchTarget(sourcepackage)
-        namespace = target.getNamespace(person)
-        self.assertEqual(person, namespace.owner)
-        self.assertEqual(sourcepackage, namespace.sourcepackage)
-
-    def test_adapter(self):
-        package = self.factory.makeSourcePackage()
-        target = IBranchTarget(package)
-        self.assertIsInstance(target, PackageBranchTarget)
+        self.assertProvides(self.target, IPrimaryContext)
 
     def test_context(self):
         # IBranchTarget.context is the original object.
-        package = self.factory.makeSourcePackage()
-        target = PackageBranchTarget(package)
-        self.assertEqual(package, target.context)
+        self.assertEqual(self.original, self.target.context)
 
 
-class TestPersonBranchTarget(TestCaseWithFactory):
+class TestPackageBranchTarget(TestCaseWithFactory, BaseBranchTargetTests):
 
     layer = DatabaseFunctionalLayer
 
-    def test_name(self):
-        # The name of a junk context is '+junk'.
-        target = PersonBranchTarget(self.factory.makePerson())
-        self.assertEqual('+junk', target.name)
+    def setUp(self):
+        TestCaseWithFactory.setUp(self)
+        self.original = self.factory.makeSourcePackage()
+        self.target = PackageBranchTarget(self.original)
 
-    def test_provides_IPrimaryContext(self):
-        person = self.factory.makePerson()
-        target = PackageBranchTarget(person)
-        self.assertProvides(target, IPrimaryContext)
+    def test_name(self):
+        # The name of a package context is distro/series/sourcepackage
+        self.assertEqual(self.original.path, self.target.name)
 
     def test_getNamespace(self):
         """Get namespace produces the correct namespace."""
         person = self.factory.makePerson()
-        target = PersonBranchTarget(self.factory.makePerson())
-        namespace = target.getNamespace(person)
-        self.assertEqual(namespace.owner, person)
+        namespace = self.target.getNamespace(person)
+        self.assertEqual(person, namespace.owner)
+        self.assertEqual(self.original, namespace.sourcepackage)
+
+    def test_adapter(self):
+        target = IBranchTarget(self.original)
+        self.assertIsInstance(self.target, PackageBranchTarget)
+
+
+class TestPersonBranchTarget(TestCaseWithFactory, BaseBranchTargetTests):
+
+    layer = DatabaseFunctionalLayer
+
+    def setUp(self):
+        TestCaseWithFactory.setUp(self)
+        self.original = self.factory.makePerson()
+        self.target = PersonBranchTarget(self.original)
+
+    def test_name(self):
+        # The name of a junk context is '+junk'.
+        self.assertEqual('+junk', self.target.name)
+
+    def test_getNamespace(self):
+        """Get namespace produces the correct namespace."""
+        namespace = self.target.getNamespace(self.original)
+        self.assertEqual(namespace.owner, self.original)
         self.assertRaises(AttributeError, lambda: namespace.product)
         self.assertRaises(AttributeError, lambda: namespace.sourcepackage)
 
     def test_adapter(self):
-        person = self.factory.makePerson()
-        target = IBranchTarget(person)
+        target = IBranchTarget(self.original)
         self.assertIsInstance(target, PersonBranchTarget)
 
-    def test_context(self):
-        # IBranchTarget.context is the original object.
-        person = self.factory.makePerson()
-        target = PersonBranchTarget(person)
-        self.assertEqual(person, target.context)
 
-
-class TestProductBranchTarget(TestCaseWithFactory):
+class TestProductBranchTarget(TestCaseWithFactory, BaseBranchTargetTests):
 
     layer = DatabaseFunctionalLayer
 
-    def test_name(self):
-        product = self.factory.makeProduct()
-        target = ProductBranchTarget(product)
-        self.assertEqual(product.name, target.name)
+    def setUp(self):
+        TestCaseWithFactory.setUp(self)
+        self.original = self.factory.makeProduct()
+        self.target = ProductBranchTarget(self.original)
 
-    def test_provides_IPrimaryContext(self):
-        product = self.factory.makeProduct()
-        target = PackageBranchTarget(product)
-        self.assertProvides(target, IPrimaryContext)
+    def test_name(self):
+        self.assertEqual(self.original.name, self.target.name)
 
     def test_getNamespace(self):
         """Get namespace produces the correct namespace."""
-        product = self.factory.makeProduct()
         person = self.factory.makePerson()
-        target = ProductBranchTarget(product)
-        namespace = target.getNamespace(person)
-        self.assertEqual(namespace.product, product)
+        namespace = self.target.getNamespace(person)
+        self.assertEqual(namespace.product, self.original)
         self.assertEqual(namespace.owner, person)
 
     def test_adapter(self):
-        product = self.factory.makeProduct()
-        target = IBranchTarget(product)
+        target = IBranchTarget(self.original)
         self.assertIsInstance(target, ProductBranchTarget)
-
-    def test_context(self):
-        # IBranchTarget.context is the original object.
-        product = self.factory.makeProduct()
-        target = ProductBranchTarget(product)
-        self.assertEqual(product, target.context)
 
 
 class TestPrimaryContext(TestCaseWithFactory):
