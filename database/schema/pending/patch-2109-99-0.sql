f420b7bd0fc9892f39db0aab507c859ff1a7af5e
Paul Hummer <paul.hummer@canonical.com>
2009-03-03 09:55:44 +1300

    Fixed patch, I'm retarded

--- database/schema/pending/patch-2109-99-0.sql
+++ database/schema/pending/patch-2109-99-0.sql
@@ -1,4 +1,14 @@
 SET client_min_messages=ERROR;
 
+ALTER TABLE ProductSeries
+  ADD COLUMN branch INT REFERENCES Branch;
+
+UPDATE ProductSeries
+  SET branch=COALESCE (user_branch, import_branch);
+
+ALTER TABLE ProductSeries
+  DROP COLUMN user_branch,
+  DROP COLUMN import_branch;
+
 INSERT INTO LaunchpadDatabaseRevision VALUES (2109, 99, 0);
 
