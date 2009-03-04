cfba5b765f8086d0dfc4017fe4952cc051677168
Paul Hummer <paul.hummer@canonical.com>
2009-03-05 11:05:53 +1300

    Added an index

--- database/schema/patch-2109-99-0.sql
+++ database/schema/patch-2109-99-0.sql
@@ -10,5 +10,8 @@ ALTER TABLE ProductSeries
   DROP COLUMN user_branch,
   DROP COLUMN import_branch;
 
+CREATE INDEX productseries__branch__idx ON ProductSeries(branch)
+  WHERE branch IS NOT NULL;
+
 INSERT INTO LaunchpadDatabaseRevision VALUES (2109, 99, 0);
 
