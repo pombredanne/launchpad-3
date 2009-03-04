48d02b2bd2ee4feb12f98f323eb453bf687dfa28
Paul Hummer <paul.hummer@canonical.com>
2009-03-05 11:20:31 +1300

    Fixed the revision number

--- database/schema/patch-2109-37-0.sql
+++ database/schema/patch-2109-37-0.sql
@@ -2,6 +2,8 @@ SET client_min_messages=ERROR;
 
 ALTER TABLE ProductSeries
   ADD COLUMN branch INT REFERENCES Branch;
+CREATE INDEX productseries__branch__idx ON ProductSeries(branch)
+  WHERE branch IS NOT NULL;
 
 UPDATE ProductSeries
   SET branch=COALESCE (user_branch, import_branch);
@@ -10,8 +12,6 @@ ALTER TABLE ProductSeries
   DROP COLUMN user_branch,
   DROP COLUMN import_branch;
 
-CREATE INDEX productseries__branch__idx ON ProductSeries(branch)
-  WHERE branch IS NOT NULL;
 
-INSERT INTO LaunchpadDatabaseRevision VALUES (2109, 99, 0);
+INSERT INTO LaunchpadDatabaseRevision VALUES (2109, 37, 0);
 
