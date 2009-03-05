79f038999df60061bb0094dfc44dabb439b7ea5f
Michael Hudson <michael.hudson@canonical.com>
2009-03-05 16:16:15 +1300

    we got a patch number from stub

--- database/schema/patch-2109-38-0.sql
+++ database/schema/patch-2109-38-0.sql
@@ -21,4 +21,4 @@ END);
 
 CREATE UNIQUE INDEX codeimport__git_repo_url__idx ON CodeImport USING btree (git_repo_url) WHERE (git_repo_url is NOT NULL);
 
-INSERT INTO LaunchpadDatabaseRevision VALUES (2109, 87, 0);
+INSERT INTO LaunchpadDatabaseRevision VALUES (2109, 38, 0);
