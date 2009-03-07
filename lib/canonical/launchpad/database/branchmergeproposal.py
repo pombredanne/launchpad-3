a8e9ec6a5f5bb2570c322ce705b447f846a9e330
Tim Penhey <tim.penhey@canonical.com>
2009-03-07 20:15:50 +1300

    Fix the query for finding the merge proposals that a reviewer is reviewing.

--- lib/canonical/launchpad/database/branchmergeproposal.py
+++ lib/canonical/launchpad/database/branchmergeproposal.py
@@ -25,6 +25,7 @@ import transaction
 from zope.component import getUtility
 from zope.event import notify
 from zope.interface import classProvides, implements
+from zope.security.proxy import removeSecurityProxy
 
 from storm.expr import Desc, Join, LeftJoin
 from storm.locals import Int, Reference, Unicode
@@ -697,8 +698,15 @@ class BranchMergeProposalGetter:
                  BranchMergeProposal.id),
             LeftJoin(CodeReviewComment,
                  CodeReviewVoteReference.commentID == CodeReviewComment.id)]
-        visible_branches = set(getUtility(IAllBranches).visibleByUser(
-            visible_by_user).getBranches().values(Branch.id))
+
+        # XXX: JonathanLange 2009-03-04 bug=337494: getBranches() returns a
+        # decorated set, so we get at the underlying set so we can get at the
+        # private and juicy _get_select.
+        naked_query = removeSecurityProxy(
+            getUtility(IAllBranches).visibleByUser(
+                visible_by_user).getBranches())
+        visible_branches = naked_query.result_set._get_select()
+        visible_branches.columns = (Branch.id,)
         expression = And(
             CodeReviewVoteReference.reviewer == reviewer,
             BranchMergeProposal.target_branchID.is_in(visible_branches),
