52f78600f12811f50f8a56a7cc4e4d09f66ded96
Jonathan Lange <jml@canonical.com>
2009-03-09 10:40:41 +1000

    Move the email tests out.

--- lib/canonical/codehosting/scanner/tests/test_bzrsync.py
+++ lib/canonical/codehosting/scanner/tests/test_bzrsync.py
@@ -3,7 +3,6 @@
 # pylint: disable-msg=W0141
 
 import datetime
-import email
 import os
 import random
 import time
@@ -22,16 +21,9 @@ from zope.component import getUtility
 from canonical.config import config
 from canonical.launchpad.database import (
     BranchRevision, Revision, RevisionAuthor, RevisionParent)
-from canonical.launchpad.mail import stub
-from canonical.launchpad.interfaces import (
-    BranchSubscriptionDiffSize,
-    BranchSubscriptionNotificationLevel, CodeReviewNotificationLevel,
-    IPersonSet, IRevisionSet)
+from canonical.launchpad.interfaces import IRevisionSet
 from canonical.launchpad.interfaces.branch import IBranchSet
-from canonical.launchpad.interfaces.branchjob import (
-    IRevisionMailJobSource, IRevisionsAddedJobSource)
 from canonical.launchpad.testing import LaunchpadObjectFactory
-from canonical.codehosting.jobs import JobRunner
 from canonical.codehosting.scanner.bzrsync import (
     BzrSync, InvalidStackedBranchURL)
 from canonical.codehosting.bzrutils import ensure_base
@@ -590,161 +582,6 @@ class TestBzrSyncOneRevision(BzrSyncTestCase):
         revision = getUtility(IRevisionSet).getByRevisionId(
             fake_rev.revision_id)
         self.assertEqual(old_date, revision.revision_date)
-
-
-class TestBzrSyncEmail(BzrSyncTestCase):
-    """Tests BzrSync support for generating branch email notifications."""
-
-    def setUp(self):
-        BzrSyncTestCase.setUp(self)
-        stub.test_emails = []
-
-    def makeDatabaseBranch(self):
-        branch = BzrSyncTestCase.makeDatabaseBranch(self)
-        LaunchpadZopelessLayer.txn.begin()
-        test_user = getUtility(IPersonSet).getByEmail('test@canonical.com')
-        branch.subscribe(
-            test_user,
-            BranchSubscriptionNotificationLevel.FULL,
-            BranchSubscriptionDiffSize.FIVEKLINES,
-            CodeReviewNotificationLevel.NOEMAIL)
-        LaunchpadZopelessLayer.txn.commit()
-        return branch
-
-    def assertTextIn(self, expected, text):
-        """Assert that expected is in text.
-
-        Report expected and text in case of failure.
-        """
-        self.failUnless(expected in text, '%r not in %r' % (expected, text))
-
-    def test_empty_branch(self):
-        self.makeBzrSync(self.db_branch).syncBranchAndClose()
-        JobRunner.fromReady(getUtility(IRevisionMailJobSource)).runAll()
-        self.assertEqual(len(stub.test_emails), 1)
-        [initial_email] = stub.test_emails
-        expected = 'First scan of the branch detected 0 revisions'
-        email_body = email.message_from_string(initial_email[2]).get_payload()
-        self.assertTextIn(expected, email_body)
-
-    def test_import_revision(self):
-        self.commitRevision()
-        self.makeBzrSync(self.db_branch).syncBranchAndClose()
-        JobRunner.fromReady(getUtility(IRevisionMailJobSource)).runAll()
-        self.assertEqual(len(stub.test_emails), 1)
-        [initial_email] = stub.test_emails
-        expected = ('First scan of the branch detected 1 revision'
-                    ' in the revision history of the=\n branch.')
-        email_body = email.message_from_string(initial_email[2]).get_payload()
-        self.assertTextIn(expected, email_body)
-
-    def test_import_uncommit(self):
-        self.commitRevision()
-        self.makeBzrSync(self.db_branch).syncBranchAndClose()
-        JobRunner.fromReady(getUtility(IRevisionMailJobSource)).runAll()
-        stub.test_emails = []
-        self.uncommitRevision()
-        self.makeBzrSync(self.db_branch).syncBranchAndClose()
-        JobRunner.fromReady(getUtility(IRevisionMailJobSource)).runAll()
-        self.assertEqual(len(stub.test_emails), 1)
-        [uncommit_email] = stub.test_emails
-        expected = '1 revision was removed from the branch.'
-        email_body = email.message_from_string(
-            uncommit_email[2]).get_payload()
-        self.assertTextIn(expected, email_body)
-
-    def test_import_recommit(self):
-        # When scanning the uncommit and new commit
-        # there should be an email generated saying that
-        # 1 (in this case) revision has been removed,
-        # and another email with the diff and log message.
-        self.commitRevision('first')
-        self.makeBzrSync(self.db_branch).syncBranchAndClose()
-        JobRunner.fromReady(getUtility(IRevisionMailJobSource)).runAll()
-        stub.test_emails = []
-        self.uncommitRevision()
-        self.writeToFile(filename="hello.txt",
-                         contents="Hello World\n")
-        author = self.factory.getUniqueString()
-        self.commitRevision('second', committer=author)
-        self.makeBzrSync(self.db_branch).syncBranchAndClose()
-        JobRunner.fromReady(getUtility(IRevisionsAddedJobSource)).runAll()
-        JobRunner.fromReady(getUtility(IRevisionMailJobSource)).runAll()
-        self.assertEqual(len(stub.test_emails), 2)
-        [recommit_email, uncommit_email] = stub.test_emails
-        uncommit_email_body = uncommit_email[2]
-        expected = '1 revision was removed from the branch.'
-        self.assertTextIn(expected, uncommit_email_body)
-        subject = (
-            'Subject: [Branch %s] Test branch' % self.db_branch.unique_name)
-        self.assertTextIn(expected, uncommit_email_body)
-        recommit_email_body = recommit_email[2]
-        body_bits = [
-            'Subject: [Branch %s] Rev 1: second'
-            % self.db_branch.unique_name,
-            'revno: 1',
-            'committer: %s' % author,
-            'branch nick: %s'  % self.bzr_branch.nick,
-            'message:\n  second',
-            'added:\n  hello.txt',
-            "=3D=3D=3D added file 'hello.txt'",
-            ]
-        for bit in body_bits:
-            self.assertTextIn(bit, recommit_email_body)
-
-
-class TestBzrSyncNoEmail(BzrSyncTestCase):
-    """Tests BzrSync support for not generating branch email notifications
-    when no one is interested.
-    """
-
-    def setUp(self):
-        BzrSyncTestCase.setUp(self)
-        stub.test_emails = []
-
-    def assertNoPendingEmails(self, bzrsync):
-        self.assertEqual(
-            len(bzrsync._branch_mailer.pending_emails), 0,
-            "There should be no pending emails.")
-
-    def test_no_subscribers(self):
-        self.assertEqual(self.db_branch.subscribers.count(), 0,
-                         "There should be no subscribers to the branch.")
-
-    def test_empty_branch(self):
-        bzrsync = self.makeBzrSync(self.db_branch)
-        bzrsync.syncBranchAndClose()
-        self.assertNoPendingEmails(bzrsync)
-
-    def test_import_revision(self):
-        self.commitRevision()
-        bzrsync = self.makeBzrSync(self.db_branch)
-        bzrsync.syncBranchAndClose()
-        self.assertNoPendingEmails(bzrsync)
-
-    def test_import_uncommit(self):
-        self.commitRevision()
-        bzrsync = self.makeBzrSync(self.db_branch)
-        bzrsync.syncBranchAndClose()
-        stub.test_emails = []
-        self.uncommitRevision()
-        bzrsync = self.makeBzrSync(self.db_branch)
-        bzrsync.syncBranchAndClose()
-        self.assertNoPendingEmails(bzrsync)
-
-    def test_import_recommit(self):
-        # No emails should have been generated.
-        self.commitRevision('first')
-        bzrsync = self.makeBzrSync(self.db_branch)
-        bzrsync.syncBranchAndClose()
-        stub.test_emails = []
-        self.uncommitRevision()
-        self.writeToFile(filename="hello.txt",
-                         contents="Hello World\n")
-        self.commitRevision('second')
-        bzrsync = self.makeBzrSync(self.db_branch)
-        bzrsync.syncBranchAndClose()
-        self.assertNoPendingEmails(bzrsync)
 
 
 class TestRevisionProperty(BzrSyncTestCase):
