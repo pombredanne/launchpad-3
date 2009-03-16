a257d56ba61ae6698e113a5b0f4ede4bd740fd0e
Graham Binns <graham@canonical.com>
2009-03-16 18:18:24 +0200

    Added tests and implementation for TextualBugChange.

--- lib/canonical/launchpad/components/bugchange.py
+++ lib/canonical/launchpad/components/bugchange.py
@@ -8,10 +8,13 @@ __all__ = [
     'get_bug_change_class',
 ]
 
+from textwrap import dedent
+
 from zope.interface import implements
 
 from canonical.launchpad.interfaces.bugchange import (
     IBugChange)
+from canonical.launchpad.mailnotification import get_unified_diff
 
 
 def get_bug_change_class(obj, field_name):
@@ -27,9 +30,12 @@ class BugChangeBase:
 
     implements(IBugChange)
 
-    def __init__(self, when, person):
-        self.when = when
+    def __init__(self, when, person, what_changed, old_value, new_value):
+        self.new_value = new_value
+        self.old_value = old_value
         self.person = person
+        self.what_changed = what_changed
+        self.when = when
 
     def getBugActivity(self):
         """Return the `BugActivity` entry for this change."""
@@ -46,6 +52,31 @@ class BugChangeBase:
 
 class TextualBugChange(BugChangeBase):
     """Describes a textual attribute change to a bug."""
+
+    def getBugActivity(self):
+        """Return the BugActivity data for the textual change."""
+        return {
+            'newvalue': self.new_value,
+            'oldvalue': self.old_value,
+            'whatchanged': self.what_changed,
+            }
+
+    def getBugNotification(self):
+        if self.what_changed == 'title':
+            notification_text = dedent("""\
+                ** Summary changed:
+
+                - %s
+                + %s""" % (self.old_value, self.new_value))
+
+        if self.what_changed == 'description':
+            description_diff = get_unified_diff(
+                self.old_value, self.new_value, 72)
+
+            notification_text = (
+                u"** Description changed:\n\n%s" % description_diff)
+
+        return {'text': notification_text}
 
 
 BUG_CHANGE_LOOKUP = {
