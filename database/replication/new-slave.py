1cc0d91591cc1d7c54fc92502dee64fc4d4696f4
Stuart Bishop <stuart.bishop@canonical.com>
2009-03-17 18:47:31 +0700

    Harden new-slave.py and make it duplicate and repair database schema

--- database/replication/new-slave.py
+++ database/replication/new-slave.py
@@ -9,15 +9,19 @@ __all__ = []
 import _pythonpath
 
 from optparse import OptionParser
-import re
+import subprocess
 import sys
 import time
 from textwrap import dedent
 
-from canonical.database.sqlbase import connect
+import psycopg2
+
+from canonical.database.postgresql import ConnectionString
+from canonical.database.sqlbase import (
+    connect_string, ISOLATION_LEVEL_AUTOCOMMIT)
 from canonical.launchpad.scripts import db_options, logger_options, logger
 
-import helpers
+import replication.helpers
 
 def main():
     parser = OptionParser(
@@ -33,8 +37,28 @@ def main():
     if len(args) != 2:
         parser.error("Missing required arguments.")
 
-    node_id, connection_string = args
+    node_id, raw_target_connection_string = args
 
+    # Confirm we can connect to the source database.
+    # Keep the connection as we need it later.
+    source_connection_string = ConnectionString(connect_string('postgres'))
+    try:
+        log.debug(
+            "Opening source connection to '%s'" % source_connection_string)
+        source_connection = psycopg2.connect(str(source_connection_string))
+        source_connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    except psycopg2.Error, exception:
+        parser.error("Unable to connect as %s (%s)" % (
+            source_connection_string, str(exception).strip()))
+
+    # Confirm we are connected to a Slony-I node.
+    if not replication.helpers.slony_installed(source_connection):
+        parser.error(
+            "Database at %s is not a Slony-I node." % source_connection_string)
+
+    # Sanity check the given node_id.
+    existing_nodes = replication.helpers.get_all_cluster_nodes(
+        source_connection)
     try:
         node_id = int(node_id)
     except ValueError:
@@ -42,15 +66,72 @@ def main():
     if node_id <= 0:
         parser.error("node_id must be a positive integer.")
 
-    existing_nodes = helpers.get_all_cluster_nodes(connect('slony'))
-
     if node_id in [node.node_id for node in existing_nodes]:
         parser.error("Node %d already exists in the cluster." % node_id)
 
-    if re.search(r'\buser', connection_string, re.I):
+    # Sanity check the target connection string.
+    target_connection_string = ConnectionString(raw_target_connection_string)
+    if target_connection_string.user is not None:
         parser.error("Don't include username in connection string.")
 
-    slony_connection_string = "%s user=slony" % connection_string
+    target_slony_connection_string = ConnectionString(
+        raw_target_connection_string)
+    target_slony_connection_string.user = 'slony'
+
+    target_postgres_connection_string = ConnectionString(
+        raw_target_connection_string)
+    target_postgres_connection_string.user = 'postgres'
+
+    # Make sure we can connect as the required users to our target.
+    for connection_string in [
+        target_postgres_connection_string, target_slony_connection_string]:
+        try:
+            psycopg2.connect(str(connection_string))
+        except psycopg2.Error, exception:
+            parser.error("Failed to connect using '%s' (%s)" % (
+                connection_string, str(exception).strip()))
+
+    # Confirm the target database is sane. Check for common errors
+    # that people might make when bringing new replicas online at 4am.
+    target_con = psycopg2.connect(str(target_postgres_connection_string))
+    cur = target_con.cursor()
+    cur.execute("SHOW lc_collate")
+    collation = cur.fetchone()[0]
+    if collation != "C":
+        parser.error(
+            "Database at %s has incorrect collation (%s)" % (
+                target_postgres_connection_string, collation))
+    cur.execute("SHOW server_encoding")
+    encoding = cur.fetchone()[0]
+    if encoding != "UTF8":
+        parser.error(
+            "Database at %s has incorrect encoding (%s)" % (
+                target_postgres_connection_string, encoding))
+    cur.execute("""
+        SELECT COUNT(*) FROM information_schema.tables
+        WHERE table_schema NOT IN ('pg_catalog', 'information_schema')
+        """)
+    num_existing_objects = cur.fetchone()[0]
+    if num_existing_objects != 0:
+        parser.error(
+            "Database at %s is not empty." % target_postgres_connection_string)
+    target_con.rollback()
+
+    # Duplicate the schema.
+    log.info("Duplicating db schema from '%s' to '%s'" % (
+        source_connection_string, target_postgres_connection_string))
+    cmd = "pg_dump --schema-only --no-privileges %s | psql -1 -q %s" % (
+        source_connection_string.asPGCommandLineArgs(),
+        target_postgres_connection_string.asPGCommandLineArgs())
+    if subprocess.call(cmd, shell=True) != 0:
+        log.error("Failed to duplicate database schema.")
+        return 1
+
+    # Trash the broken Slony tables we just duplicated.
+    cur = target_con.cursor()
+    cur.execute("DROP SCHEMA _sl CASCADE")
+    target_con.commit()
+    del target_con
 
     comment = 'New node created %s' % time.ctime()
 
@@ -63,7 +144,7 @@ def main():
         try {
             store node (id=@new_node, comment='%s');
             echo 'Creating new node paths.';
-        """ % (node_id, slony_connection_string, comment))
+        """ % (node_id, target_slony_connection_string, comment))
 
     for node in existing_nodes:
         nickname = node.nickname
@@ -80,6 +161,7 @@ def main():
         } on error { echo 'Failed.'; exit 1; }
 
         echo 'Waiting for sync.';
+        echo 'This will hang if no slon daemon is running for the new slave';
         sync (id = @master_node);
         wait for event (
             origin = ALL, confirmed = ALL,
@@ -96,9 +178,9 @@ def main():
             wait on = @master_node, timeout = 0);
         """)
 
-    helpers.execute_slonik(script)
+    replication.helpers.execute_slonik(script)
 
-    helpers.validate_replication(connect('slony').cursor())
+    replication.helpers.validate_replication(source_connection.cursor())
 
     return 0
 
