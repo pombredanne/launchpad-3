5a4c4065b8bd296bb530d4fa2f3cfa456bda8c69
Gavin Panella <gavin.panella@canonical.com>
2009-03-17 11:00:15 +0200

    Neither bug.displayname nor bugtask.title are interesting fields.

--- lib/canonical/launchpad/subscribers/bugactivity.py
+++ lib/canonical/launchpad/subscribers/bugactivity.py
@@ -19,7 +19,6 @@ vocabulary_registry = getVocabularyRegistry()
 
 BUG_INTERESTING_FIELDS = [
     'description',
-    'displayname',
     'duplicateof',
     'name',
     'private',
@@ -36,7 +35,6 @@ BUGTASK_INTERESTING_FIELDS = [
     'milestone',
     'status',
     'target',
-    'title',
     ]
 
 
