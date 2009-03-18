c6b1ffd4200e252f04cd351f6de532691a423bde
Edwin Grubbs <edwin.grubbs@canonical.com>
2009-03-17 21:02:31 -0500

    Added test_product_edit_people.py windmill test.

--- lib/canonical/launchpad/windmill/tests/test_registry/test_product_edit_people.py
+++ lib/canonical/launchpad/windmill/tests/test_registry/test_product_edit_people.py
@@ -2,61 +2,54 @@ from canonical.launchpad.windmill.testing import lpuser
 
 from windmill.authoring import WindmillTestClient
 
-def test_change_assignee():
-    """Test inplace editing of the bugtask assignee.
-
-    This excercises the Activator and Picker widgets.
-    """
-    client = WindmillTestClient('BugTask assignee test')
+def test_product_edit_people():
+    client = WindmillTestClient('Project +edit-people Picker widget test')
 
     lpuser.FOO_BAR.ensure_login(client)
-    # Load bug page.
-    client.open(url='http://bugs.launchpad.dev:8085/bugs/1')
+    # Load +edit-people page.
+    client.open(url='http://launchpad.dev:8085/firefox/+edit-people')
     client.waits.forPageLoad(timeout=u'20000')
 
-    # Click on assignee edit button for first bugtask.
-    client.waits.forElement(
-        xpath=u"//span[@id='assignee-content-box-2']/button",
-        timeout=u'20000')
-    client.click(xpath=u"//span[@id='assignee-content-box-2']/button")
-    # Search for "admin" in picker widget.
+    # Click on "Choose" link to show picker for the "owner" field.
+    client.click(id=u'show-widget-field-owner')
+    # Search for "guadamen".
     client.type(
-        text=u'admin',
+        text=u'guadamen',
         xpath=u"//table[contains(@class, 'yui-picker') "
                "and not(contains(@class, 'yui-picker-hidden'))]"
                "//input[@class='yui-picker-search']")
+    # Click the search button.
     client.click(
         xpath=u"//table[contains(@class, 'yui-picker') "
                "and not(contains(@class, 'yui-picker-hidden'))]"
                "//div[@class='yui-picker-search-box']/button")
-    # Select first item in list (Commercial Admins).
+    # Choose the first item in the list.
     client.click(
         xpath=u"//table[contains(@class, 'yui-picker') "
                "and not(contains(@class, 'yui-picker-hidden'))]"
                "//ul[@class='yui-picker-results']/li[1]/span")
-    # Verify update.
-    client.asserts.assertText(
-        xpath=u"//span[@id='assignee-content-box-2']//a",
-        validator=u'Commercial Subscription Admins')
+    # Verify value.
+    client.asserts.assertProperty(
+        id=u'field.owner', validator=u"value|guadamen")
 
-    # Click on assignee edit button for another bugtask.
-    client.click(xpath=u"//span[@id='assignee-content-box-17']/button")
-    # Search for "admin" in picker widget.
+    # Click on "Choose" link to show picker for the "driver" field.
+    client.click(id=u'show-widget-field-driver')
+    # Search for "foo".
     client.type(
-        text=u'admin',
+        text=u'foo',
         xpath=u"//table[contains(@class, 'yui-picker') "
                "and not(contains(@class, 'yui-picker-hidden'))]"
                "//input[@class='yui-picker-search']")
+    # Click the search button.
     client.click(
         xpath=u"//table[contains(@class, 'yui-picker') "
                "and not(contains(@class, 'yui-picker-hidden'))]"
                "//div[@class='yui-picker-search-box']/button")
-    # Select second item in list (Foo Bar).
+    # Choose the first item in the list.
     client.click(
         xpath=u"//table[contains(@class, 'yui-picker') "
                "and not(contains(@class, 'yui-picker-hidden'))]"
-               "//ul[@class='yui-picker-results']/li[2]/span")
-    # Verify update.
-    client.asserts.assertText(
-        xpath=u"//span[@id='assignee-content-box-17']//a",
-        validator=u'Foo Bar')
+               "//ul[@class='yui-picker-results']/li[1]/span")
+    # Verify value.
+    client.asserts.assertProperty(
+        id=u'field.driver', validator=u"value|name16")
