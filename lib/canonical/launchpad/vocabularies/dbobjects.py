dc82bee11018a719297698651a8e505cbe92d029
Francis J. Lacoste <francis.lacoste@canonical.com>
2009-03-18 11:45:04 -0400

    Extracted out FAQVocabulary.

--- lib/canonical/launchpad/vocabularies/dbobjects.py
+++ lib/canonical/launchpad/vocabularies/dbobjects.py
@@ -25,7 +25,6 @@ __all__ = [
     'DistributionUsingMaloneVocabulary',
     'DistributionVocabulary',
     'DistroSeriesVocabulary',
-    'FAQVocabulary',
     'FeaturedProjectVocabulary',
     'FilteredDeltaLanguagePackVocabulary',
     'FilteredDistroArchSeriesVocabulary',
@@ -132,9 +131,6 @@ from canonical.launchpad.webapp.tales import (
 from canonical.launchpad.webapp.vocabulary import (
     CountableIterator, IHugeVocabulary, NamedSQLObjectHugeVocabulary,
     NamedSQLObjectVocabulary, SQLObjectVocabularyBase)
-
-from lp.answers.interfaces.faq import IFAQ
-from lp.answers.interfaces.faqtarget import IFAQTarget
 
 
 class BasePersonVocabulary:
@@ -291,62 +287,6 @@ class WebBugTrackerVocabulary(BugTrackerVocabulary):
     """All web-based bug tracker types."""
 
     _filter = BugTracker.q.bugtrackertype != BugTrackerType.EMAILADDRESS
-
-
-class FAQVocabulary:
-    """Vocabulary containing all the FAQs in an `IFAQTarget`."""
-
-    implements(IHugeVocabulary)
-
-    displayname = 'Select a FAQ'
-
-    def __init__(self, context):
-        """Create a new vocabulary for the context.
-
-        :param context: It should adaptable to `IFAQTarget`.
-        """
-        self.context = IFAQTarget(context)
-
-    def __len__(self):
-        """See `IIterableVocabulary`."""
-        return self.context.searchFAQs().count()
-
-    def __iter__(self):
-        """See `IIterableVocabulary`."""
-        for faq in self.context.searchFAQs():
-            yield self.toTerm(faq)
-
-    def __contains__(self, value):
-        """See `IVocabulary`."""
-        if not IFAQ.providedBy(value):
-            return False
-        return self.context.getFAQ(value.id) is not None
-
-    def getTerm(self, value):
-        """See `IVocabulary`."""
-        if value not in self:
-            raise LookupError(value)
-        return self.toTerm(value)
-
-    def getTermByToken(self, token):
-        """See `IVocabularyTokenized`."""
-        try:
-            faq_id = int(token)
-        except ValueError:
-            raise LookupError(token)
-        faq = self.context.getFAQ(token)
-        if faq is None:
-            raise LookupError(token)
-        return self.toTerm(faq)
-
-    def toTerm(self, faq):
-        """Return the term for a FAQ."""
-        return SimpleTerm(faq, faq.id, faq.title)
-
-    def searchForTerms(self, query=None):
-        """See `IHugeVocabulary`."""
-        results = self.context.findSimilarFAQs(query)
-        return CountableIterator(results.count(), results, self.toTerm)
 
 
 class LanguageVocabulary(SQLObjectVocabularyBase):
