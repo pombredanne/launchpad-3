514678a36d08fcd6009d1bc72dc92334022a2cee
Francis J. Lacoste <francis.lacoste@canonical.com>
2009-03-18 12:14:51 -0400

    Added docstrings.

--- lib/lp/services/__init__.py
+++ lib/lp/services/__init__.py
@@ -1,1 +1,12 @@
-# lp.servers namespace package
+# Copyright 2006-2009 Canonical Ltd.  All rights reserved.
+"""lp.services namespace package
+
+WARNING: This is a namespace package, it should only include other packages,
+but no actual code or modules.
+
+In this namespace live the packages providing system-level services to the
+rest of the Launchpad application. Things like languages, countries, geoip,
+GPG handling, email, etc.
+
+Packages in this namespace can only use general LAZR or library functionality.
+"""
