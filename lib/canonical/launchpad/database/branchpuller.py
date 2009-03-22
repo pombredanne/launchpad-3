3a5071f8157053d72cef78daa31d112a02f427d6
Jonathan Lange <jml@canonical.com>
2009-03-22 13:14:29 +1100

    Use storm.

--- lib/canonical/launchpad/database/branchpuller.py
+++ lib/canonical/launchpad/database/branchpuller.py
@@ -8,13 +8,17 @@ __all__ = []
 
 from datetime import timedelta
 
-from sqlobject.sqlbuilder import AND
-
+from storm.expr import LeftJoin, Join
+from zope.component import getUtility
 from zope.interface import implements
 
 from canonical.database.constants import UTC_NOW
 from canonical.launchpad.database.branch import Branch
+from canonical.launchpad.database.person import Owner
+from canonical.launchpad.database.product import Product
 from canonical.launchpad.interfaces.branchpuller import IBranchPuller
+from canonical.launchpad.webapp.interfaces import (
+    IStoreSelector, MAIN_STORE, DEFAULT_FLAVOR)
 
 
 class BranchPuller:
@@ -25,8 +29,17 @@ class BranchPuller:
     MIRROR_TIME_INCREMENT = timedelta(hours=6)
 
     def getPullQueue(self, branch_type):
-        """See `IBranchSet`."""
-        return Branch.select(
-            AND(Branch.q.branch_type == branch_type,
-                Branch.q.next_mirror_time <= UTC_NOW),
-            prejoins=['owner', 'product'], orderBy='next_mirror_time')
+        """See `IBranchPuller`."""
+        store = getUtility(IStoreSelector).get(MAIN_STORE, DEFAULT_FLAVOR)
+        # Prejoin on owner and product to preserve existing behaviour.
+        # XXX: JonathanLange 2009-03-22 spec=package-branches: This prejoin is
+        # inappropriate in the face of package branches.
+        prejoin = store.using(
+            Branch,
+            LeftJoin(Product, Branch.product == Product.id),
+            Join(Owner, Branch.owner == Owner.id))
+        return prejoin.find(
+            Branch,
+            Branch.branch_type == branch_type,
+            Branch.next_mirror_time <= UTC_NOW).order_by(
+                Branch.next_mirror_time)
