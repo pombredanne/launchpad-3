9424496622f6600ca4f75066a9b028c6a487b239
Leonard Richardson <leonard.richardson@canonical.com>
2009-03-23 12:50:20 -0400

    Initial implementation.

--- lib/canonical/lazr/rest/example/interfaces.py
+++ lib/canonical/lazr/rest/example/interfaces.py
@@ -4,7 +4,7 @@
 """Interface objects for the LAZR example web service."""
 
 __metaclass__ = type
-__all__ = ['AlreadyNouvelle',
+__all__ = ['AlreadyNew',
            'ICookbook',
            'ICookbookSet',
            'IDish',
@@ -15,8 +15,9 @@ __all__ = ['AlreadyNouvelle',
            'NameAlreadyTaken']
 
 from zope.interface import Attribute, Interface
-from zope.schema import Bool, Date, Int, TextLine, Text
+from zope.schema import Bool, Choice, Date, Int, TextLine, Text
 
+from lazr.enum import EnumeratedType, Item
 
 from canonical.lazr.fields import CollectionField, Reference
 from canonical.lazr.interfaces.rest import ITopLevelEntryLink
@@ -27,8 +28,8 @@ from canonical.lazr.rest.declarations import (
     operation_parameters, operation_returns_collection_of, webservice_error)
 
 
-class AlreadyNouvelle(Exception):
-    """A cookbook's cuisine prohibits the cheap 'Nouvelle' trick."""
+class AlreadyNew(Exception):
+    """A cookbook's name prohibits the cheap 'The New' trick."""
     webservice_error(400)
 
 
@@ -45,6 +46,17 @@ class WhitespaceStrippingTextLine(TextLine):
         if value is not None:
             value = value.strip()
         super(WhitespaceStrippingTextLine, self).set(object, value)
+
+
+class Cuisine(EnumeratedType):
+    """A vocabulary for cuisines."""
+
+    GENERAL = Item("General", "General cooking")
+    VEGETARIAN = Item("Vegetarian", "Vegetarian cooking")
+    AMERICAN = Item("American", "Traditional American cooking")
+    DESSERT = Item("Dessert", "Desserts")
+    FRANCAIS = Item("Fran\xc3\xa7ais".decode("utf-8"),
+                    "Cuisine fran\xc3\xa7ais".decode("utf-8"))
 
 
 class IHasGet(Interface):
@@ -78,12 +90,14 @@ class ICookbook(IHasGet):
     """A cookbook, annotated for export to the web service."""
     export_as_webservice_entry()
     name = exported(TextLine(title=u"Name", required=True))
+    description = exported(
+        WhitespaceStrippingTextLine(title=u"Description", required=False))
     copyright_date = exported(Date(title=u"Copyright Date", readonly=True))
     confirmed = exported(Bool(
             title=u"Whether this information has been confirmed",
             default=False))
-    cuisine = exported(WhitespaceStrippingTextLine(
-            title=u"Cuisine", required=False, default=None))
+    cuisine = exported(Choice(
+        vocabulary=Cuisine, title=u"Cuisine", required=False, default=None))
     recipes = exported(CollectionField(title=u"Recipes in this cookbook",
                                        value_type=Reference(schema=IRecipe)))
 
@@ -123,9 +137,9 @@ class ICookbookSet(IHasGet):
     def find_recipes(search):
         """Search for recipes across cookbooks."""
 
-    @export_factory_operation(ICookbook,
-                              ['name', 'cuisine', 'copyright_date'])
-    def create(name, cuisine, copyright_date):
+    @export_factory_operation(
+        ICookbook, ['name', 'description', 'cuisine', 'copyright_date'])
+    def create(name, description, cuisine, copyright_date):
         """Create a new cookbook."""
 
     featured = Attribute("The currently featured cookbook.")
