a082b274ed8eccda4b6811adbb9ed2c56fc46217
Julian Edwards <julian.edwards@canonical.com>
2009-03-24 16:14:07 +0000

    Export IDistroArchSeries on the webservice

--- lib/canonical/launchpad/interfaces/distroarchseries.py
+++ lib/canonical/launchpad/interfaces/distroarchseries.py
@@ -14,52 +14,97 @@ __all__ = [
 from zope.interface import Interface, Attribute
 from zope.schema import Bool, Choice, Int, TextLine
 
+from canonical.launchpad.interfaces.archive import IArchive
+from canonical.launchpad.interfaces.distroseries import IDistroSeries
+from canonical.launchpad.interfaces.person import IPerson
 from canonical.launchpad.interfaces import IHasOwner
 from canonical.launchpad import _
 
+from canonical.lazr.fields import Reference
+from canonical.lazr.rest.declarations import (
+    export_as_webservice_entry, exported)
+
+
 class IDistroArchSeries(IHasOwner):
     """DistroArchSeries Table Interface"""
+    export_as_webservice_entry()
+
     id = Attribute("Identifier")
-    distroseries = Attribute("DistroSeries")
-    processorfamily = Choice(title=_("Processor Family"),
+    distroseries = exported(
+        Reference(
+            IDistroSeries,
+            title=_("The context distroseries"),
+            required=False, readonly=False))
+    processorfamily = Choice(
+        title=_("Processor Family"),
         required=True, vocabulary='ProcessorFamily')
-    architecturetag = TextLine(title=_("Architecture Tag"),
-        description=_("The architecture tag, or short piece of text that "
-        "identifies this architecture. All binary packages in the archive "
-        "will use this tag in their filename. Please get it correct. It "
-        "should really never be changed!"), required=True)
-    official = Bool(title=_("Official Support"),
-        description=_("Indicate whether or not this port has official "
-        "support from the vendor of the distribution."), required=True)
-    owner = Int(title=_('The person who registered this port.'),
-        required=True)
-    package_count = Attribute('A cache of the number of packages published '
-        'in the RELEASE pocket of this port.')
-    supports_virtualized = Bool(
-        title=_("PPA support available"),
-        description=_("Indicate whether or not this port has support "
-                      "for building PPA packages."),
-        required=False)
+    architecturetag = exported(
+        TextLine(
+            title=_("Architecture Tag"),
+            description=_(
+                "The architecture tag, or short piece of text that "
+                "identifies this architecture. All binary packages in the "
+                "archive will use this tag in their filename. Please get it "
+                "correct. It should really never be changed!"),
+            required=True))
+    official = exported(
+        Bool(
+            title=_("Official Support"),
+            description=_(
+                "Indicate whether or not this port has official "
+                "support from the vendor of the distribution."),
+            required=True))
+    owner = exported(
+        Reference(
+            IPerson,
+            title=_('The person who registered this port.'),
+            required=True))
+    package_count = exported(
+        Int(
+            title=_("Package Count"),
+            description=_(
+                'A cache of the number of packages published '
+                'in the RELEASE pocket of this port.'),
+            readonly=False, required=False))
+    supports_virtualized = exported(
+        Bool(
+            title=_("PPA support available"),
+            description=_("Indicate whether or not this port has support "
+                          "for building PPA packages."),
+            required=False))
 
     # Joins.
     packages = Attribute('List of binary packages in this port.')
 
     # Page layouts helpers.
-    title = Attribute('Title')
-    displayname = Attribute('Display name')
+    title = exported(
+        TextLine(
+            title=_('Title'),
+            description=_("The title of this distroarchseries.")))
 
-    # Other usefult bits.
-    isNominatedArchIndep = Attribute(
-        'True if this distroarchseries is the NominatedArchIndep one.')
+    displayname = exported(
+        TextLine(
+            title=_("Display name"),
+            description=_("The display name of this distroarchseries.")))
 
-    distribution = Attribute("The distribution of the package.")
+    # Other useful bits.
+    isNominatedArchIndep = exported(
+        Bool(
+            title=_("Is Nominated Arch Independent"),
+            description=_(
+                'True if this distroarchseries is the NominatedArchIndep '
+                'one.')))
     default_processor = Attribute(
         "Return the DistroArchSeries default processor, by picking the "
         "first processor inside its processorfamily.")
     processors = Attribute(
         "The group of Processors for this DistroArchSeries.processorfamily."
         )
-    main_archive = Attribute('Main Archive')
+    main_archive = exported(
+        Reference(
+            IArchive,
+            title=_('Main Archive'),
+            description=_("The main archive of the distroarchseries.")))
 
     def updatePackageCount():
         """Update the cached binary package count for this distro arch
