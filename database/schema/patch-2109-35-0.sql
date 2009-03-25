05f67439446a9fbe699307b271da8d751123b416
Stuart Bishop <stuart.bishop@canonical.com>
2009-03-25 14:20:03 +0700

    Revert OpenIDNonce table changes to keep the openid consumer library happy.

--- database/schema/patch-2109-35-0.sql
+++ database/schema/patch-2109-35-0.sql
@@ -4,11 +4,10 @@ SET client_min_messages=ERROR;
 -- OpenIDAssociations.  This is what the python openid package expects the
 -- table to look like.
 CREATE TABLE OpenIDNonce (
-    id serial PRIMARY KEY,
     server_url VARCHAR(2047) NOT NULL,
     timestamp INTEGER NOT NULL,
     salt CHAR(40) NOT NULL,
-    UNIQUE (server_url, timestamp, salt)
+    PRIMARY KEY (server_url, timestamp, salt)
     );
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2109, 35, 0);
--- lib/canonical/launchpad/database/openidconsumer.py
+++ lib/canonical/launchpad/database/openidconsumer.py
@@ -11,11 +11,12 @@ class OpenIDNonce(Storm):
     """An OpenIDNonce.
 
     The table definition matches that required by the openid library,
-    so doesn't follow our standards.
+    so doesn't follow our standards. In particular, it doesn't have an
+    id column and the timestamp is an epoch time integer rather than a
+    datetime.
     """
     __storm_table__ = "OpenIDNonce"
-
-    id = Int(primary=True)
+    __storm_primary__ = "server_url", "timestamp", "salt"
 
     server_url = Unicode()
     timestamp = Int()
