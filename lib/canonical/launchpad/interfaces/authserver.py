80752ecb2c7261ddf9173df1f78f3333ad25d57b
Michael Hudson <michael.hudson@canonical.com>
2009-03-25 16:43:53 +1300

    fix docs

--- lib/canonical/launchpad/interfaces/authserver.py
+++ lib/canonical/launchpad/interfaces/authserver.py
@@ -14,24 +14,10 @@ from zope.interface import Interface
 class IAuthServer(Interface):
     """A storage for details about users.
 
-    Many of the methods defined here return *user dicts*.  A user dict is a
-    dictionary containing:
-        :id:             person id (integer, doesn't change ever)
-        :displayname:    full name, for display
-        :emailaddresses: list of email addresses, preferred email first, the
-                         rest alphabetically sorted.
-        :teams:          a list of team dicts for each team the user is a
-                         member of (including the user themself).
+    A user dict is a dictionary containing:
 
-    A *team dict* contains:
-        :id:            team id (integer, doesn't change ever)
-        :name:          nickname for the team
-        :displayname:   full name of the team, for display
-
-    Differences from version 1 (IUserDetailsStorage):
-        - no salts in user dicts
-        - no SSHA digests, just cleartext passwords
-        - teams in user dicts.
+        :id:   person id (integer, doesn't change ever)
+        :name: person nickname
     """
 
     def getUser(loginID):
@@ -43,10 +29,11 @@ class IAuthServer(Interface):
         :returns: user dict if loginID exists, otherwise empty dict
         """
 
-    def getSSHKeys(archiveName):
+    def getSSHKeys(loginID):
         """Retrieve SSH public keys for a given push mirror archive
 
-        :param archive: an archive name.
+        :param loginID: A login ID (an email address, nickname, or numeric
+            person ID from a user dict).
         :returns: list of 2-tuples of (key type, key text).  This list will be
             empty if the user has no keys or does not exist.
         """
