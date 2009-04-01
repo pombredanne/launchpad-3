75ca425ddb3355a15edd4748c1d13beb5e5e37a7
Leonard Richardson <leonard.richardson@canonical.com>
2009-04-01 15:43:59 -0400

    Added cleanup to complete a test request so it doesn't overstay its welcome.

--- lib/canonical/launchpad/fields/__init__.py
+++ lib/canonical/launchpad/fields/__init__.py
@@ -88,7 +88,7 @@ from canonical.launchpad.validators import LaunchpadValidationError
 from canonical.launchpad.validators.name import valid_name, name_validator
 
 from lazr.restful.fields import Reference
-from canonical.lazr.interfaces.fields import IReferenceChoice
+from lazr.restful.interfaces import IReferenceChoice
 
 
 # Marker object to tell BaseImageUpload to keep the existing image.
