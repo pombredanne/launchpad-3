799443480d013f87f5b9801095dca9a0921fdda8
Leonard Richardson <leonard.richardson@canonical.com>
2009-04-03 10:32:49 -0400

    Removed code that lives in lazr.restful, and added re-imports from lazr.restful because that's not its final destination.

--- lib/canonical/lazr/testing/layers.py
+++ lib/canonical/lazr/testing/layers.py
@@ -1,20 +1,6 @@
 # Copyright 2008-2009 Canonical Ltd.  All rights reserved.
 
-"""Miscellaneous testing helpers."""
-
-__metaclass__ = type
-__all__ = [
-    'MockRootFolder',
-]
-
-class MockRootFolder:
-    """Implement the minimum functionality required by Z3 ZODB dependencies
-
-    Installed as part of FunctionalLayer.testSetUp() to allow the http()
-    method (zope.app.testing.functional.HTTPCaller) to work.
-    """
-    @property
-    def _p_jar(self):
-        return self
-    def sync(self):
-        pass
+# Re-import code from lazr.restful until it can be refactored into a
+# utility module.
+from lazr.restful.testing.layers import *
+from lazr.restful.testing.layers import __all__ as __all__
--- lib/canonical/lazr/utils.py
+++ lib/canonical/lazr/utils.py
@@ -1,72 +1,13 @@
 # Copyright 2008 Canonical Ltd.  All rights reserved.
 
-"""Various utility functions."""
+# Re-import code from lazr.restful until it can be refactored into a
+# utility module.
 
-__metaclass__ = type
-__all__ = [
-    'camelcase_to_underscore_separated',
-    'get_current_browser_request',
-    'safe_js_escape',
-    'safe_hasattr',
-    'smartquote',
-    ]
-
-
-import cgi
-import re
-
-from simplejson import encoder
-
-
-missing = object()
-
-
-def camelcase_to_underscore_separated(name):
-    """Convert 'ACamelCaseString' to 'a_camel_case_string'"""
-    def prepend_underscore(match):
-        return '_' + match.group(1)
-    return re.sub('\B([A-Z])', prepend_underscore, name).lower()
-
-
-def safe_hasattr(ob, name):
-    """hasattr() that doesn't hide exceptions."""
-    return getattr(ob, name, missing) is not missing
-
-
-def smartquote(str):
-    """Return a copy of the string, with typographical quote marks applied."""
-    str = unicode(str)
-    str = re.compile(u'(^| )(")([^" ])').sub(u'\\1\u201c\\3', str)
-    str = re.compile(u'([^ "])(")($|[\s.,;:!?])').sub(u'\\1\u201d\\3', str)
-    return str
-
-
-def safe_js_escape(text):
-    """Return the given text escaped for use in Javascript code.
-
-    This will also perform a cgi.escape() on the given text.
-    """
-    return encoder.encode_basestring(cgi.escape(text, True))
-
-
-def get_current_browser_request():
-    """Return the current browser request, looked up from the interaction.
-
-    If there is no suitable request, then return None.
-
-    Returns only requests that provide IHTTPApplicationRequest.
-    """
-    from zope.security.management import queryInteraction
-    from zope.publisher.interfaces.http import IHTTPApplicationRequest
-    interaction = queryInteraction()
-    requests = [
-        participation
-        for participation in interaction.participations
-        if IHTTPApplicationRequest.providedBy(participation)
-        ]
-    if not requests:
-        return None
-    assert len(requests) == 1, (
-        "We expect only one IHTTPApplicationRequest in the interaction."
-        " Got %s." % len(requests))
-    return requests[0]
+# pylint: disable-msg=W0401
+try:
+    from lazr.restful.utils import *
+    from lazr.restful.utils import __all__ as _utils_all
+    __all__ = []
+    __all__.extend(_utils_all)
+except ImportError:
+    pass
