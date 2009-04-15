16e9bf8ba89f040067345005fae81da9f2df76df
Curtis Hovey <curtis.hovey@canonical.com>
2009-04-14 23:20:29 -0400

    Extracted registry vocabularies from vocabularies.dbobjects and the vocabularies.txt doctest.

--- lib/lp/registry/tests/test_commercialprojects_vocabularies.py
+++ lib/lp/registry/tests/test_commercialprojects_vocabularies.py
@@ -16,10 +16,9 @@ from canonical.launchpad.interfaces.emailaddress import (
 from lp.registry.interfaces.product import (
     IProductSet, License)
 from canonical.launchpad.testing import LaunchpadObjectFactory
-from canonical.launchpad.vocabularies.dbobjects import (
-    CommercialProjectsVocabulary)
 from canonical.testing import LaunchpadFunctionalLayer
 
+from lp.registry.vocabularies import CommercialProjectsVocabulary
 
 class TestCommProjVocabulary(TestCase):
     """Test that the CommercialProjectsVocabulary behaves as expected."""
