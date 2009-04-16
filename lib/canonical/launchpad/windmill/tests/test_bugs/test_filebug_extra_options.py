47128ae952a1e21150647cc74ca7ab6a06d2b4ef
Gavin Panella <gavin.panella@canonical.com>
2009-04-16 12:27:58 +0100

    A windmill test for the new collapsible area in +filebug.

---
+++ lib/canonical/launchpad/windmill/tests/test_bugs/test_filebug_extra_options.py
@@ -0,0 +1,52 @@
+from canonical.launchpad.windmill.testing import lpuser
+
+from windmill.authoring import WindmillTestClient
+
+
+def test_filebug_extra_options():
+    """Test the extra options area on +filebug pages.
+
+    This test ensures that, with Javascript enabled, the extra options
+    expander starts closed, and contains several fields when opened.
+    """
+    client = WindmillTestClient("File bug extra options test")
+    lpuser.SAMPLE_PERSON.ensure_login(client)
+
+    # Open a +filebug page and wait for it to finish loading.
+    client.open(url=u'http://bugs.launchpad.dev:8085/firefox/+filebug')
+    client.waits.forPageLoad(timeout=u'20000')
+
+    # Search for a possible duplicate.
+    client.type(text=u'Broken', id=u'field.title')
+    client.click(id=u'field.actions.search')
+    client.waits.forPageLoad(timeout=u'20000')
+
+    # No duplicates were found.
+    client.asserts.assertText(
+        xpath=u"//form[@name='launchpadform']//p",
+        validator=u'No similar bug reports were found.')
+
+    # The collapsible area is present and collapsed.
+    collapsible_area_xpath=(
+        u"//form[@name='launchpadform']"
+        u"//fieldset[contains(.//legend,'Include an attachment')]")
+    client.asserts.assertProperty(
+        xpath=collapsible_area_xpath,
+        validator="className|collapsible")
+    client.asserts.assertNode(
+        xpath=collapsible_area_xpath + u"/div[@class='collapsed']")
+
+    # Click on the legend and it expands.
+    client.click(
+        xpath=collapsible_area_xpath + u"/legend/a")
+    expanded_div_xpath = collapsible_area_xpath + u"/div[@class='expanded']"
+    client.waits.forElement(xpath=expanded_div_xpath)
+
+    # All users can see the attachment options.
+    client.asserts.assertNode(
+        xpath=expanded_div_xpath + u"//input[@name='field.filecontent']")
+    client.asserts.assertNode(
+        xpath=expanded_div_xpath + u"//input[@name='field.patch']")
+    client.asserts.assertNode(
+        xpath=(expanded_div_xpath +
+               u"//input[@name='field.attachment_description']"))
