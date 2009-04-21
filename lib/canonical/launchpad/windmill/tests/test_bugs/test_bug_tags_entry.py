eb31782a0234f171928aa0d38da3b70b23bef81e
Tom Berger <tom.berger@canonical.com>
2009-04-21 14:34:00 +0100

    fixes for no bugs and some more windmill testing

--- lib/canonical/launchpad/windmill/tests/test_bugs/test_bug_tags_entry.py
+++ lib/canonical/launchpad/windmill/tests/test_bugs/test_bug_tags_entry.py
@@ -16,11 +16,11 @@ def test_bug_tags_entry():
     # First, we add some official tags to test with
 
     client.open(url='http://bugs.launchpad.dev:8085/firefox')
-    client.waits.forPageLoad(timeout=u'20000')
+    client.waits.forPageLoad(timeout=u'100000')
 
     lpuser.FOO_BAR.ensure_login(client)
     client.click(link=u'Edit official tags')
-    client.waits.forPageLoad(timeout=u'20000')
+    client.waits.forPageLoad(timeout=u'100000')
 
     client.type(text=u'eenie', id=u'new-tag-text')
     client.click(id=u'new-tag-add')
@@ -31,14 +31,15 @@ def test_bug_tags_entry():
     client.type(text=u'moe', id=u'new-tag-text')
     client.click(id=u'new-tag-add')
     client.click(id=u'save-button')
-    client.waits.forPageLoad(timeout=u'20000')
+    client.waits.forPageLoad(timeout=u'100000')
 
     client.open(url='http://bugs.launchpad.dev:8085/firefox/+bug/5')
-    client.waits.forPageLoad(timeout=u'20000')
-    
+    client.waits.forPageLoad(timeout=u'100000')
+    client.waits.sleep(milliseconds=u'3000')
     client.click(id=u'edit-tags-trigger')
     client.asserts.assertNode(id=u'tag-input')
     client.type(text=u'e', id=u'tag-input')
     client.asserts.assertNode(classname=u'yui-autocomplete-list')
     client.click(id=u'item0')
+    client.asserts.assertProperty(validator=u'value|eenie ')
 
