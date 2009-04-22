d98472af8402218bb98b791d864b92bd4d0120ce
Tom Berger <tom.berger@canonical.com>
2009-04-22 14:35:42 +0100

    clarify the documentation a bit, as per suggestion by allenap

--- lib/canonical/launchpad/browser/bugcomment.py
+++ lib/canonical/launchpad/browser/bugcomment.py
@@ -73,8 +73,8 @@ def build_comments_from_chunks(chunks, bugtask, truncate=False):
         # XXX intellectronica 2009-04-22, bug=365092
         # Currently, there are some bug messages for which no chunks exist
         # in the DB, so we need to make sure that we skip them, since the
-        # corresponding message wont have been added to the dictionary in the
-        # section above.
+        # corresponding message wont have been added to the comments dictionary
+        # in the section above.
         if comment is not None:
             comment.visible = bug_message.visible
 
