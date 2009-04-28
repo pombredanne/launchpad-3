4e06ded5e14fbb3407cb48308bdfa1811d70d111
Jeroen Vermeulen <jeroen.vermeulen@canonical.com>
2009-04-28 20:55:42 +0700

    Supporting infrastructure: check iteration orders.

--- lib/canonical/launchpad/utilities/__init__.py
+++ lib/canonical/launchpad/utilities/__init__.py
@@ -2,5 +2,7 @@
 
 from canonical.launchpad.utilities.gpghandler import *
 from canonical.launchpad.utilities.celebrities import *
+from canonical.launchpad.utilities.looptuner import *
+from canonical.launchpad.utilities.orderingcheck import *
 from canonical.launchpad.utilities.searchservice import *
 from canonical.launchpad.utilities.salesforce import *
