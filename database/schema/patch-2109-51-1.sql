614faffa79920790aa20df420ddfecd6e77b4eac
Stuart Bishop <stuart.bishop@canonical.com>
2009-04-30 16:31:36 +0700

    More indexes for pofile deletion

--- database/schema/patch-2109-51-1.sql
+++ database/schema/patch-2109-51-1.sql
@@ -1,5 +1,6 @@
 SET client_min_messages=ERROR;
 
 create index translationmessage__pofile__idx on translationmessage(pofile);
+create index pofiletranslator__pofile__idx on pofiletranslator(pofile);
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2109, 51, 1);
