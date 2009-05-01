b56ba2315bcf6aa1cd75c7790bb7dc071018656f
James Henstridge <james.henstridge@canonical.com>
2009-05-01 16:40:30 +0800

    Garbo jobs to link people to RevisionAuthors and HWSubmissions as new email addresses get created or validated.

--- lib/canonical/launchpad/scripts/garbo.py
+++ lib/canonical/launchpad/scripts/garbo.py
@@ -14,9 +14,13 @@ from storm.locals import SQL, Max, Min
 
 from canonical.database.sqlbase import sqlvalues
 from lp.code.model.codeimportresult import CodeImportResult
+from lp.code.model.revision import RevisionAuthor
+from canonical.launchpad.database.emailaddress import EmailAddress
+from canonical.launchpad.database.hwdb import HWSubmission
 from canonical.launchpad.database.oauth import OAuthNonce
 from canonical.launchpad.database.openidconsumer import OpenIDConsumerNonce
 from canonical.launchpad.interfaces import IMasterStore
+from canonical.launchpad.interfaces.emailaddress import EmailAddressStatus
 from canonical.launchpad.interfaces.looptuner import ITunableLoop
 from canonical.launchpad.scripts.base import LaunchpadCronScript
 from canonical.launchpad.utilities.looptuner import LoopTuner
@@ -152,6 +156,125 @@ class CodeImportResultPruner(TunableLoop):
         transaction.commit()
 
 
+class RevisionAuthorLinker(TunableLoop):
+    """A TunableLoop that links `RevisionAuthor` objects to `Person` objects.
+
+    `EmailAddress` objects are looked up for `RevisionAuthor` objects
+    that have not yet been linked to a `Person`.  If the
+    `EmailAddress` is linked to a person, then the the
+    `RevisionAuthor` is linked to the same.
+    """
+
+    maximum_chunk_size = 100
+
+    def __init__(self):
+        self.author_store = IMasterStore(RevisionAuthor)
+        self.email_store = IMasterStore(EmailAddress)
+
+        (self.min_author_id,
+         self.max_author_id) = self.author_store.find(
+            (Min(RevisionAuthor.id), Max(RevisionAuthor.id))).one()
+
+        self.next_author_id = self.min_author_id
+
+    def isDone(self):
+        return (self.min_author_id is None or
+                self.next_author_id > self.max_author_id)
+
+    def __call__(self, chunk_size):
+        result = self.author_store.find(
+            RevisionAuthor,
+            RevisionAuthor.id >= self.next_author_id,
+            RevisionAuthor.personID == None,
+            RevisionAuthor.email != None)
+        result.order_by(RevisionAuthor.id)
+        authors = list(result[:chunk_size])
+
+        # No more authors found.
+        if len(authors) == 0:
+            self.next_author_id = self.max_author_id + 1
+            transaction.commit()
+            return
+
+        emails = dict(self.email_store.find(
+            (EmailAddress.email.lower(), EmailAddress.personID),
+            EmailAddress.email.lower().is_in(
+                    [author.email.lower() for author in authors]),
+            EmailAddress.status.is_in([EmailAddressStatus.PREFERRED,
+                                       EmailAddressStatus.VALIDATED]),
+            EmailAddress.personID != None))
+
+        if emails:
+            for author in authors:
+                personID = emails.get(author.email.lower())
+                if personID is None:
+                    continue
+                author.personID = personID
+
+        self.next_author_id = authors[-1].id + 1
+        transaction.commit()
+
+
+class HWSubmissionLinker(TunableLoop):
+    """A TunableLoop that links `HWSubmission` objects to `Person` objects.
+
+    `EmailAddress` objects are looked up for `HWSubmission` objects
+    that have not yet been linked to a `Person`.  If the
+    `EmailAddress` is linked to a person, then the the `HWSubmission`
+    is linked to the same.
+    """
+
+    maximum_chunk_size = 100
+
+    def __init__(self):
+        self.submission_store = IMasterStore(HWSubmission)
+        self.email_store = IMasterStore(EmailAddress)
+
+        (self.min_submission_id,
+         self.max_submission_id) = self.submission_store.find(
+            (Min(HWSubmission.id), Max(HWSubmission.id))).one()
+
+        self.next_submission_id = self.min_submission_id
+
+    def isDone(self):
+        return (self.min_submission_id is None or
+                self.next_submission_id > self.max_submission_id)
+
+    def __call__(self, chunk_size):
+        result = self.submission_store.find(
+            HWSubmission,
+            HWSubmission.id >= self.next_submission_id,
+            HWSubmission.ownerID == None,
+            HWSubmission.raw_emailaddress != None)
+        result.order_by(HWSubmission.id)
+        submissions = list(result[:chunk_size])
+
+        # No more submissions found.
+        if len(submissions) == 0:
+            self.next_submission_id = self.max_submission_id + 1
+            transaction.commit()
+            return
+
+        emails = dict(self.email_store.find(
+            (EmailAddress.email.lower(), EmailAddress.personID),
+            EmailAddress.email.lower().is_in(
+                    [submission.raw_emailaddress.lower()
+                     for submission in submissions]),
+            EmailAddress.status.is_in([EmailAddressStatus.PREFERRED,
+                                       EmailAddressStatus.VALIDATED]),
+            EmailAddress.personID != None))
+
+        if emails:
+            for submission in submissions:
+                personID = emails.get(submission.raw_emailaddress.lower())
+                if personID is None:
+                    continue
+                submission.ownerID = personID
+
+        self.next_submission_id = submissions[-1].id + 1
+        transaction.commit()
+
+
 class BaseDatabaseGarbageCollector(LaunchpadCronScript):
     """Abstract base class to run a collection of TunableLoops."""
     script_name = None # Script name for locking and database user. Override.
@@ -178,5 +301,7 @@ class DailyDatabaseGarbageCollector(BaseDatabaseGarbageCollector):
     script_name = 'garbo-daily'
     tunable_loops = [
         CodeImportResultPruner,
+        RevisionAuthorLinker,
+        HWSubmissionLinker,
         ]
 
