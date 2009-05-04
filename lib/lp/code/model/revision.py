f8d412325c5b68b0e01a8a6efa265881815abbee
Jonathan Lange <jml@canonical.com>
2009-05-04 16:28:24 +1000

    Looks like revisions might not actually have authors.

--- lib/lp/code/model/revision.py
+++ lib/lp/code/model/revision.py
@@ -259,14 +259,19 @@ class RevisionSet:
         """See `IRevisionSet`."""
         revision_id = bzr_revision.revision_id
         revision_date = self._timestampToDatetime(bzr_revision.timestamp)
+        authors = bzr_revision.get_apparent_authors()
+        # XXX: JonathanLange 2009-05-01 bug=362686: We can only have one
+        # author per revision, so we use the first on the assumption that
+        # this is the primary author.
+        try:
+            author = bzr_revision.get_apparent_authors()[0]
+        except IndexError:
+            author = None
         return self.new(
             revision_id=revision_id,
             log_body=bzr_revision.message,
             revision_date=revision_date,
-            # XXX: JonathanLange 2009-05-01 bug=362686: We can only have one
-            # author per revision, so we use the first on the assumption that
-            # this is the primary author.
-            revision_author=bzr_revision.get_apparent_authors()[0],
+            revision_author=author,
             parent_ids=bzr_revision.parent_ids,
             properties=bzr_revision.properties)
 
