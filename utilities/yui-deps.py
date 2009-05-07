e3b54ad953976c6e74be48e07683119ebbb739da
Michael Nelson <michael.nelson@canonical.com>
2009-05-07 17:04:16 +0200

    Moving the weight from launchpad.js to the individual pages that use the picker.

--- utilities/yui-deps.py
+++ utilities/yui-deps.py
@@ -26,50 +26,26 @@ MAIN_TEMPLATE = os.path.join(
 YUI_ROOT_RE = re.compile('yui string:\${icingroot}/(.*);')
 YUI_MOD_RE = re.compile('\${yui}/(.*?)\.js')
 
-# We need to include some YUI2 js files for the moment too until we
-# have a YUI3 datepicker.
-YUI2_ROOT_RE = re.compile('yui2 string:\${icingroot}/(.*);')
-YUI2_MOD_RE = re.compile('\${yui2}/(.*?)\.js')
-
-def check_for_yui_root(line, yui_root_re):
-    match = yui_root_re.search(line)
-    if not match:
-        return None
-
-    yui_root = os.path.join(ICING_ROOT, match.group(1))
-    if not os.path.isdir(yui_root):
-        sys.stderr.write(
-            "The found YUI root isn't valid: %s\n" % yui_root)
-        sys.exit(1)
-
-    return yui_root
 
 yui_root = None
-yui2_root = None
 template = open(MAIN_TEMPLATE, 'r')
 for line in template:
     if yui_root is None:
-        yui_root = check_for_yui_root(line, YUI_ROOT_RE)
+        match = YUI_ROOT_RE.search(line)
+        if not match:
+            continue
 
-    if yui2_root is None:
-        yui2_root = check_for_yui_root(line, YUI2_ROOT_RE)
-
-    # If either of the yui or yui2 root directories are not yet known
-    # then just keep looking for them.
-    if yui_root is None or yui2_root is None:
-        continue
+        yui_root = os.path.join(ICING_ROOT, match.group(1))
+        if not os.path.isdir(yui_root):
+            sys.stderr.write(
+                "The found YUI root isn't valid: %s\n" % yui_root)
+            sys.exit(1)
     else:
         match = YUI_MOD_RE.search(line)
-        if match is not None:
-            # We want to bundle the minimized version
-            module = os.path.join(yui_root, match.group(1)) + '-min.js'
-        else:
-            match = YUI2_MOD_RE.search(line)
-            if match is not None:
-                module = os.path.join(yui2_root, match.group(1)) + '-min.js'
-            else:
-                continue
-
+        if not match:
+            continue
+        # We want to bundle the minimized version
+        module = os.path.join(yui_root, match.group(1)) + '-min.js'
         if not os.path.isfile(module):
             sys.stderr.write(
                 "Found invalid YUI module: %s\n" % module)
