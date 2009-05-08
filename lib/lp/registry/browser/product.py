473b7004997b60d46dab49c29c96c8f58724e45b
Barry Warsaw <barry@canonical.com>
2009-05-08 16:43:45 -0400

    Add a redirect from projects/+new-guided to projects/+new

--- lib/lp/registry/browser/product.py
+++ lib/lp/registry/browser/product.py
@@ -1617,6 +1617,10 @@ class ProductAddView(MultiStepView):
     def first_step(self):
         return ProjectAddStepOne
 
+    def redirect_to_plusnew(self):
+        """Redirect from the old +new-guided to the current +new page."""
+        self.request.response.redirect(canonical_url(self.context) + '/+new')
+
 
 class ProductEditPeopleView(LaunchpadEditFormView):
     """Enable editing of important people on the project."""
