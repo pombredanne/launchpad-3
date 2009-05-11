f01fb1b2e5355add663ca1eb495ec82d675750a6
Leonard Richardson <leonard.richardson@canonical.com>
2009-05-11 11:34:36 -0400

    Initial try at refactored test runner.

--- lib/lp/services/scripts/tests/test_doc.py
+++ lib/lp/services/scripts/tests/test_doc.py
@@ -3,19 +3,13 @@
 Run the doctests and pagetests.
 """
 
-import logging
 import os
-import unittest
-
-from canonical.launchpad.testing.pages import PageTestSuite
 from canonical.launchpad.testing.systemdocs import (
     LayeredDocFileSuite, setUp, tearDown)
-from canonical.testing import (
-    LaunchpadFunctionalLayer, LaunchpadZopelessLayer)
-
+from canonical.testing import LaunchpadZopelessLayer
+from lp.services.testing import build_test_suite
 
 here = os.path.dirname(os.path.realpath(__file__))
-
 
 special = {
     'script-monitoring.txt': LayeredDocFileSuite(
@@ -25,43 +19,5 @@ special = {
             ),
 }
 
-
 def test_suite():
-    suite = unittest.TestSuite()
-
-    # Add the pagetests.
-    stories_dir = os.path.join(os.path.pardir, 'stories')
-    suite.addTest(PageTestSuite(stories_dir))
-    stories_path = os.path.join(here, stories_dir)
-    for story_dir in os.listdir(stories_path):
-        full_story_dir = os.path.join(stories_path, story_dir)
-        if not os.path.isdir(full_story_dir):
-            continue
-        story_path = os.path.join(stories_dir, story_dir)
-        suite.addTest(PageTestSuite(story_path))
-
-    # Add the special doctests.
-    for key in sorted(special):
-        special_suite = special[key]
-        suite.addTest(special_suite)
-
-    testsdir = os.path.abspath(
-        os.path.normpath(os.path.join(here, os.path.pardir, 'doc'))
-        )
-
-    # Add doctests using default setup/teardown
-    filenames = [filename
-                 for filename in os.listdir(testsdir)
-                 if filename.endswith('.txt') and filename not in special]
-    # Sort the list to give a predictable order.
-    filenames.sort()
-    for filename in filenames:
-        path = os.path.join('../stories/', filename)
-        one_test = LayeredDocFileSuite(
-            path, setUp=setUp, tearDown=tearDown,
-            layer=LaunchpadFunctionalLayer,
-            stdout_logging_level=logging.WARNING
-            )
-        suite.addTest(one_test)
-
-    return suite
+    return build_test_suite(here, special)
