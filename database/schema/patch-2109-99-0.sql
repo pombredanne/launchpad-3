5518920819018fd42fef6842940fa25d9c5b5bf7
Jeroen Vermeulen <jeroen.vermeulen@canonical.com>
2009-05-12 15:20:02 +0700

    Added index as per review; assigned patch number.

--- database/schema/patch-2109-99-0.sql
+++
@@ -1,6 +0,0 @@
-SET client_min_messages=ERROR;
-
-ALTER TABLE ProductSeries
-ADD COLUMN translations_branch INTEGER REFERENCES Branch(id);
-
--- INSERT INTO LaunchpadDatabaseRevision VALUES (2109, 99, 0);
