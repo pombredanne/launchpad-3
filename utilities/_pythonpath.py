38cfcec534094574ddb8ab1b3fcdecc48ce39577
Gary Poster <gary@ubuntu>
2009-05-13 18:03:33 -0400

    make all the _pythonpath.py files work

--- utilities/_pythonpath.py
+++ utilities/_pythonpath.py
@@ -1,12 +1,1 @@
-# Copyright 2004-2005 Canonical Ltd.  All rights reserved.
-
-__metaclass__ = type
-
-import sys, os, os.path
-
-sys.path.insert(0, os.path.join(
-    os.path.dirname(__file__), os.pardir, 'lib'
-    ))
-sys.path.insert(0, os.path.join(
-    os.path.dirname(__file__), os.pardir, 'lib', 'mailman'
-    ))
+../_pythonpath.py
