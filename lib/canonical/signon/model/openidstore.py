012cc4e51187b1f47756a5b5bbe64b45c29bdc50
James Henstridge <james.henstridge@canonical.com>
2009-05-14 22:24:08 +0800

    Add nonce handling, and move code into a base class so that it can be shared by the OpenID consumer implementation.

--- lib/canonical/signon/model/openidstore.py
+++ lib/canonical/signon/model/openidstore.py
@@ -7,108 +7,24 @@ __all__ = [
     'ProviderOpenIDStore',
     ]
 
-from operator import attrgetter
-import time
-
-from openid.association import Association
-from openid.store.interface import OpenIDStore
-from storm.properties import Int, RawStr, Unicode
 from zope.interface import implements
 
-from canonical.launchpad.interfaces.lpstorm import IMasterStore
+from canonical.launchpad.database.baseopenidstore import (
+    BaseStormOpenIDAssociation, BaseStormOpenIDNonce, BaseStormOpenIDStore)
 from canonical.signon.interfaces.openidstore import IProviderOpenIDStore
 
 
-class DatabaseAssociation:
-    """Database representation of a stored OpenID association."""
-
+class ProviderAssociation(BaseStormOpenIDAssociation):
     __storm_table__ = 'OpenIDAssociation'
-    __storm_primary__ = ('server_url', 'handle')
-
-    server_url = Unicode()
-    handle = Unicode()
-    secret = RawStr()
-    issued = Int()
-    lifetime = Int()
-    assoc_type = Unicode()
-
-    def __init__(self, server_url, association):
-        super(DatabaseAssociation, self).__init__()
-        self.server_url = server_url.decode('UTF-8')
-        self.handle = association.handle.decode('ASCII')
-        self.secret = association.secret
-        self.issued = association.issued
-        self.lifetime = association.lifetime
-        self.assoc_type = association.assoc_type.decode('ASCII')
-
-    def as_association(self):
-        """Return an equivalent openid-python `Association` object."""
-        return Association(
-            self.handle.encode('ASCII'), self.secret, self.issued,
-            self.lifetime, self.assoc_type.encode('ASCII'))
 
 
-class ProviderOpenIDStore(OpenIDStore):
-    """An association store for the OpenID Provider."""
+class ProviderNonce(BaseStormOpenIDNonce):
+    __storm_table__ = 'OpenIDNonce'
+
+
+class ProviderOpenIDStore(BaseStormOpenIDStore):
     implements(IProviderOpenIDStore)
 
-    def storeAssociation(self, server_url, association):
-        """See `OpenIDStore`."""
-        store = IMasterStore(DatabaseAssociation)
-        store.add(DatabaseAssociation(server_url, association))
+    Association = ProviderAssociation
+    Nonce = ProviderNonce
 
-    def getAssociation(self, server_url, handle=None):
-        """See `OpenIDStore`."""
-        store = IMasterStore(DatabaseAssociation)
-        server_url = server_url.decode('UTF-8')
-        if handle is None:
-            result = store.find(DatabaseAssociation, server_url=server_url)
-        else:
-            handle = handle.decode('ASCII')
-            result = store.find(
-                DatabaseAssociation, server_url=server_url, handle=handle)
-
-        db_associations = list(result)
-        associations = []
-        for db_assoc in db_associations:
-            assoc = db_assoc.as_association()
-            if assoc.getExpiresIn() == 0:
-                store.remove(db_assoc)
-            else:
-                associations.append(assoc)
-
-        if len(associations) == 0:
-            return None
-        associations.sort(key=attrgetter('issued'))
-        return associations[-1]
-
-    def removeAssociation(self, server_url, handle):
-        """See `OpenIDStore`."""
-        store = IMasterStore(DatabaseAssociation)
-        assoc = store.get(DatabaseAssociation, (
-                server_url.decode('UTF-8'), handle.decode('ASCII')))
-        if assoc is None:
-            return False
-        store.remove(assoc)
-        return True
-
-    def useNonce(self, server_url, timestamp, salt):
-        """See `OpenIDStore`."""
-        raise NotImplementedError("OpenID server should not need useNonce")
-
-    def cleanupNonces(self):
-        """See `OpenIDStore`."""
-        # We don't store nonces, so there are none to clean up.
-        return 0
-
-    def cleanupAssociations(self):
-        """See `OpenIDStore`."""
-        store = IMasterStore(DatabaseAssociation)
-        now = int(time.time())
-        expired = store.find(
-            DatabaseAssociation,
-            DatabaseAssociation.issued + DatabaseAssociation.lifetime < now)
-        count = expired.count()
-        if count > 0:
-            expired.remove()
-        return count
--- lib/canonical/signon/model/tests/test_openidstore.py
+++ lib/canonical/signon/model/tests/test_openidstore.py
@@ -1,26 +1,23 @@
 # Copyright 2009 Canonical Ltd.  All rights reserved.
 
-"""Tests for the OpenID Provider's `OpenIDStore` implementation."""
+"""Module docstring goes here."""
 
 __metaclass__ = type
 
-import time
 import unittest
 
-from openid.association import Association
 from zope.component import getUtility
 
-from canonical.launchpad.interfaces.lpstorm import IMasterStore
+from canonical.launchpad.database.tests.test_baseopenidstore import (
+    BaseStormOpenIDStoreTestsMixin)
 from canonical.launchpad.webapp.dbpolicy import SSODatabasePolicy
 from canonical.launchpad.webapp.interfaces import IStoreSelector
 from canonical.signon.interfaces.openidstore import IProviderOpenIDStore
-from canonical.signon.model.openidstore import (
-    DatabaseAssociation, ProviderOpenIDStore)
 from canonical.testing import DatabaseFunctionalLayer
 from lp.testing import TestCase
 
 
-class ProviderOpenIDStoreTests(TestCase):
+class ProviderOpenIDStoreTests(BaseStormOpenIDStoreTestsMixin, TestCase):
     """Tests for the `ProviderOpenIDStore` utility."""
 
     layer = DatabaseFunctionalLayer
@@ -28,105 +25,11 @@ class ProviderOpenIDStoreTests(TestCase):
     def setUp(self):
         super(ProviderOpenIDStoreTests, self).setUp()
         getUtility(IStoreSelector).push(SSODatabasePolicy())
-        self.openid_store = getUtility(IProviderOpenIDStore)
+        self.store = getUtility(IProviderOpenIDStore)
 
     def tearDown(self):
         getUtility(IStoreSelector).pop()
         super(ProviderOpenIDStoreTests, self).tearDown()
-
-    def test_utility(self):
-        self.assertIsInstance(self.openid_store, ProviderOpenIDStore)
-
-    def test_storeAssociation(self):
-        self.openid_store.storeAssociation('server-url\xC2\xA9', Association(
-                'handle', 'secret', 42, 600, 'HMAC-SHA1'))
-        db_assoc = IMasterStore(DatabaseAssociation).get(
-            DatabaseAssociation, (u'server-url\xA9', u'handle'))
-        self.assertEquals(db_assoc.server_url, u'server-url\xA9')
-        self.assertEquals(db_assoc.handle, u'handle')
-        self.assertEquals(db_assoc.secret, 'secret')
-        self.assertEquals(db_assoc.issued, 42)
-        self.assertEquals(db_assoc.lifetime, 600)
-        self.assertEquals(db_assoc.assoc_type, u'HMAC-SHA1')
-
-    def test_getAssociation(self):
-        timestamp = int(time.time())
-        self.openid_store.storeAssociation('server-url', Association(
-                'handle', 'secret', timestamp, 600, 'HMAC-SHA1'))
-
-        assoc = self.openid_store.getAssociation('server-url', 'handle')
-        self.assertIsInstance(assoc, Association)
-        self.assertEquals(assoc.handle, 'handle')
-        self.assertEquals(assoc.secret, 'secret')
-        self.assertEquals(assoc.issued, timestamp)
-        self.assertEquals(assoc.lifetime, 600)
-        self.assertEquals(assoc.assoc_type, 'HMAC-SHA1')
-
-    def test_getAssociation_unknown(self):
-        assoc = self.openid_store.getAssociation('server-url', 'unknown')
-        self.assertEquals(assoc, None)
-
-    def test_getAssociation_expired(self):
-        lifetime = 600
-        timestamp = int(time.time()) - 2 * lifetime
-        self.openid_store.storeAssociation('server-url', Association(
-                'handle', 'secret', timestamp, lifetime, 'HMAC-SHA1'))
-        # The association is not returned because it is out of date.
-        # Further more, it is removed from the database.
-        assoc = self.openid_store.getAssociation('server-url', 'handle')
-        self.assertEquals(assoc, None)
-
-        store = IMasterStore(DatabaseAssociation)
-        db_assoc = store.get(DatabaseAssociation, (u'server-url', u'handle'))
-        self.assertEqual(db_assoc, None)
-
-    def test_getAssociation_no_handle(self):
-        timestamp = int(time.time())
-        self.openid_store.storeAssociation('server-url', Association(
-                'handle1', 'secret', timestamp, 600, 'HMAC-SHA1'))
-        self.openid_store.storeAssociation('server-url', Association(
-                'handle2', 'secret', timestamp + 1, 600, 'HMAC-SHA1'))
-
-        # The most recent handle is returned.
-        assoc = self.openid_store.getAssociation('server-url', None)
-        self.assertNotEqual(assoc, None)
-        self.assertEqual(assoc.handle, 'handle2')
-
-    def test_removeAssociation(self):
-        self.assertEquals(
-            self.openid_store.removeAssociation('server-url', 'unknown'),
-            False)
-
-        timestamp = int(time.time())
-        self.openid_store.storeAssociation('server-url', Association(
-                'handle', 'secret', timestamp, 600, 'HMAC-SHA1'))
-        self.assertEquals(
-            self.openid_store.removeAssociation('server-url', 'handle'), True)
-        self.assertEquals(
-            self.openid_store.getAssociation('server-url', 'handle'), None)
-
-    def test_useNonce(self):
-        # OpenID Providers do not need to keep track of nonces, so the
-        # nonce handling method remains unimplemented.
-        self.assertRaises(NotImplementedError, self.openid_store.useNonce,
-                          'server-url', 42, 'salt')
-
-    def test_cleanupNonces(self):
-        # As we don't track nonces, there is never any nonces to clean up.
-        self.assertEqual(self.openid_store.cleanupNonces(), 0)
-
-    def test_cleanupAssociations(self):
-        timestamp = int(time.time()) - 100
-        self.openid_store.storeAssociation('server-url', Association(
-                'handle1', 'secret', timestamp, 50, 'HMAC-SHA1'))
-        self.openid_store.storeAssociation('server-url', Association(
-                'handle2', 'secret', timestamp, 200, 'HMAC-SHA1'))
-
-        self.assertEquals(self.openid_store.cleanupAssociations(), 1)
-
-        # The second (non-expired) association is left behind.
-        self.assertNotEqual(
-            self.openid_store.getAssociation('server-url', 'handle2'), None)
 
 
 def test_suite():
