be246299d51f052a1f309ddcd3ea8ed4067e22ef
Guilherme Salgado <salgado@canonical.com>
2009-05-15 11:00:01 -0300

    Fix tests that run scripts as subprocesses

---
+++ lib/canonical/shipit/scripts/_pythonpath.py
@@ -0,0 +1,12 @@
+# Copyright 2009 Canonical Ltd.  All rights reserved.
+
+__metaclass__ = type
+
+import sys, os, os.path
+
+sys.path.insert(0, os.path.join(
+    os.path.dirname(__file__), os.pardir, os.pardir, os.pardir, 'lib'
+    ))
+
+# Enable Storm's C extensions
+os.environ['STORM_CEXTENSIONS'] = '1'
