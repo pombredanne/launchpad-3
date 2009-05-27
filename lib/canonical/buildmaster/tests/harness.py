8a4b2b202958772103762a57dbd779892cb0f860
Celso Providelo <celso.providelo@canonical.com>
2009-05-27 22:14:56 +0200

    applying review comments, r=jml.

--- lib/canonical/buildmaster/tests/harness.py
+++ lib/canonical/buildmaster/tests/harness.py
@@ -19,16 +19,22 @@ class BuilddManagerTestSetup(TacTestSetup):
     """Setup BuilddManager for use by functional tests."""
 
     def setUpRoot(self):
+        """Create `TacTestSetup.root` for storing the log and pid files.
+
+        Remove the directory and create a new one if it exists.
+        """
         if os.path.exists(self.root):
             shutil.rmtree(self.root)
         os.makedirs(self.root)
 
     @property
     def root(self):
+        """Directory where log and pid files will be stored."""
         return '/var/tmp/buildd-manager/'
 
     @property
     def tacfile(self):
+        """Absolute path to the 'buildd-manager' tac file."""
         return os.path.abspath(os.path.join(
             os.path.dirname(canonical.__file__), os.pardir, os.pardir,
             'daemons/buildd-manager.tac'
@@ -36,8 +42,16 @@ class BuilddManagerTestSetup(TacTestSetup):
 
     @property
     def pidfile(self):
+        """The tac pid file path.
+
+        Will be created when the tac file actually runs.
+        """
         return os.path.join(self.root, 'buildd-manager.pid')
 
     @property
     def logfile(self):
+        """The tac log file path.
+
+        Will be created when the tac file actually runs.
+        """
         return os.path.join(self.root, 'buildd-manager.log')
