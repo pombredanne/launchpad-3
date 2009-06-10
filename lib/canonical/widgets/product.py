8168e89996408cac7d234498811ab1cc27415448
Barry Warsaw <barry@canonical.com>
2009-06-10 11:06:07 -0400

    Revert the bogus lint warnings workarounds.

--- lib/canonical/widgets/product.py
+++ lib/canonical/widgets/product.py
@@ -235,7 +235,7 @@ class LicenseWidget(CheckBoxMatrixWidget):
         # This will return just the DBItem's text.  We want to wrap that text
         # in the URL to the license, which is stored in the DBItem's
         # description.
-        value = CheckBoxMatrixWidget.textForValue(self, term)
+        value = super(LicenseWidget, self).textForValue(term)
         if term.value.url is None:
             # There's no link.
             return value
@@ -243,7 +243,7 @@ class LicenseWidget(CheckBoxMatrixWidget):
             return '<a href="%s">%s</a>' % (term.value.url, value)
 
     def __call__(self):
-        self.checkbox_matrix = CheckBoxMatrixWidget.__call__(self)
+        self.checkbox_matrix = super(LicenseWidget, self).__call__()
         return self.template()
 
 
