ab1e7610f43a9e3418fb3fc85180972b46f822fe
Jeroen Vermeulen <jeroen.vermeulen@canonical.com>
2009-06-18 20:13:17 +0700

    DirectBranchCommit, for commit-translations-to-branch work.

---
+++ lib/lp/code/model/directbranchcommit.py
@@ -0,0 +1,169 @@
+# Copyright 2009 Canonical Ltd.  All rights reserved.
+
+"""Commit files straight to bzr branch."""
+
+__metaclass__ = type
+__all__ = [
+    'ConcurrentUpdateError',
+    'DirectBranchCommit',
+    ]
+
+
+import os.path
+
+from zope.security.proxy import removeSecurityProxy
+
+from bzrlib.branch import Branch as BzrBranch
+from bzrlib.commit import Commit
+from bzrlib.generate_ids import gen_file_id
+from bzrlib.revision import NULL_REVISION
+from bzrlib.transform import TransformPreview
+
+
+class ConcurrentUpdateError(Exception):
+    """Bailout exception for concurrent updates.
+
+    This is raised when committing to a branch would risk overwriting
+    concurrent changes made by another party.
+    """
+
+
+class DirectBranchCommit:
+    """Commit a set of files straight into a branch.
+
+    Use this to write a set of files into a branch efficiently, without
+    caring what was in there before.  The files may be new to the branch
+    or they may exist there already; in the latter case they will be
+    overwritten.
+
+    The branch is write-locked for the entire lifetime of this object.
+    Be sure to call unlock() when done.  This will be done for you as
+    part of a successful commit, but unlocking more than once will do no
+    harm.
+
+    The trick for this was invented by Aaron Bentley.  It saves having
+    to do a full checkout of the branch.
+    """
+    is_open = False
+    is_locked = False
+
+    def __init__(self, db_branch, committer=None):
+        """Create context for direct commit to branch.
+
+        :param db_branch: a Launchpad Branch object.
+        """
+        self.db_branch = db_branch
+
+        if committer is None:
+            committer = db_branch.owner
+        self.committer = committer
+
+        self.bzrbranch = BzrBranch.open(self.db_branch.getPullURL())
+        self.bzrbranch.lock_write()
+        self.is_locked = True
+
+        try:
+            self.revision_tree = self.bzrbranch.basis_tree()
+            self.transform_preview = TransformPreview(self.revision_tree)
+
+            self.is_open = True
+        except:
+            self.unlock()
+            self.is_locked = False
+            raise
+
+        self.files = set()
+
+    def _getDir(self, path):
+        """Get trans_id for directory "path."  Create if necessary."""
+        dir = self.revision_tree.path2id(path)
+        if dir:
+            return dir
+
+        parent_dir, dirname = os.path.split(path)
+        if dirname:
+            parent_id = self._getDir(parent_dir)
+        else:
+            parent_id = None
+
+        dirfile_id = gen_file_id(path)
+        return self.transform_preview.new_directory(
+            dirname, parent_id, dirfile_id)
+
+    def writeFile(self, path, contents):
+        """Write file to branch; may be an update or a new file.
+
+        If you write a file multiple times, the first one is used and
+        the rest ignored.
+        """
+        assert self.is_open, "Writing file to closed DirectBranchCommit."
+
+        if path in self.files:
+            # We already have this file.  Ignore second write.
+            return
+
+        file_id = self.revision_tree.path2id(path)
+        if file_id is None:
+            parent_path, name = os.path.split(path)
+            parent_id = self._getDir(parent_path)
+            file_id = gen_file_id(name)
+            self.transform_preview.new_file(
+                name, parent_id, [contents], file_id)
+        else:
+            trans_id = self.transform_preview.trans_id_tree_path(path)
+            # Delete old contents.  It doesn't actually matter whether
+            # we do this before creating the new contents.
+            self.transform_preview.delete_contents(trans_id)
+            self.transform_preview.create_file([contents], trans_id)
+
+        self.files.add(path)
+
+    def _checkForRace(self):
+        """Check if bzrbranch has any changes waiting to be scanned.
+
+        If it does, raise `ConcurrentUpdateError`.
+        """
+        assert self.is_locked, "Getting revision on un-locked branch."
+        last_revision = None
+        last_revision = self.bzrbranch.last_revision()
+        if last_revision != self.db_branch.last_scanned_id:
+            raise ConcurrentUpdateError(
+                "Branch has been changed.  Not committing.")
+
+    def commit(self, commit_message):
+        """Commit to branch."""
+        assert self.is_open, "Committing closed DirectBranchCommit."
+        assert self.is_locked, "Not locked at commit time."
+
+        try:
+            self._checkForRace()
+
+            preview_tree = self.transform_preview.get_preview_tree()
+
+            rev_id = self.revision_tree.get_revision_id()
+            if rev_id == NULL_REVISION:
+                parents = []
+            else:
+                parents = [rev_id]
+
+            builder = self.bzrbranch.get_commit_builder(parents)
+
+            list(builder.record_iter_changes(
+                preview_tree, rev_id, self.transform_preview.iter_changes()))
+
+            builder.finish_inventory()
+
+            new_rev_id = builder.commit(commit_message)
+
+            revno, old_rev_id = self.bzrbranch.last_revision_info()
+            self.bzrbranch.set_last_revision_info(revno + 1, new_rev_id)
+
+        finally:
+            self.unlock()
+            self.is_open = False
+
+    def unlock(self):
+        """Release commit lock, if held."""
+        if self.is_locked:
+            self.bzrbranch.unlock()
+            self.is_locked = False
---
+++ lib/lp/code/tests/test_directbranchcommit.py
@@ -0,0 +1,134 @@
+# Copyright 2009 Canonical Ltd.  All rights reserved.
+
+"""Tests for `DirectBranchCommit`."""
+
+__metaclass__ = type
+
+import os.path
+from unittest import TestLoader
+
+from zope.component import getUtility
+from zope.security.proxy import removeSecurityProxy
+
+from bzrlib.branch import Branch as BzrBranch
+from bzrlib.revision import NULL_REVISION
+
+from lp.code.model.directbranchcommit import (
+    ConcurrentUpdateError, DirectBranchCommit)
+from lp.testing import TestCaseWithFactory
+from canonical.testing.layers import LaunchpadZopelessLayer
+
+
+class TestDirectBranchCommit(TestCaseWithFactory):
+    """Test `DirectBranchCommit`."""
+
+    layer = LaunchpadZopelessLayer
+
+    db_branch = None
+    committer = None
+
+    def setUp(self):
+        super(TestDirectBranchCommit, self).setUp()
+        self.useBzrBranches()
+
+        self.series = self.factory.makeProductSeries()
+        self.db_branch, tree = self.create_branch_and_tree(
+            db_branch=self.db_branch, hosted=True)
+
+        self.series.translations_branch = self.db_branch
+
+        self._setUpCommitter()
+
+    def _setUpCommitter(self, update_last_scanned_id=True):
+        if self.committer:
+            self.committer.unlock()
+
+        self.committer = DirectBranchCommit(self.db_branch)
+        if update_last_scanned_id:
+            self.db_branch.last_scanned_id = (
+                self.committer.bzrbranch.last_revision())
+
+    def tearDown(self):
+        self.committer.unlock()
+
+    def _getContents(self):
+        """Return branch contents as dict mapping filenames to contents."""
+        contents = {}
+        branch = BzrBranch.open(self.committer.db_branch.getPullURL())
+        tree = branch.basis_tree()
+        tree.lock_read()
+        try:
+            for dir, entries in tree.walkdirs():
+                dirname, id = dir
+                for entry in entries:
+                    file_path, file_name, file_type = entry[:3]
+                    if file_type == 'file':
+                        stored_file = tree.get_file_by_path(file_path)
+                        contents[file_path] = stored_file.read()
+        finally:
+            tree.unlock()
+
+        return contents
+
+    def test_DirectBranchCommit_commits_no_changes(self):
+        # Committing to an empty branch leaves the branch empty.
+        self.committer.commit('')
+        self.assertEqual({}, self._getContents())
+
+    def test_DirectBranchCommit_rejects_change_after_commit(self):
+        # Changes are not accepted after commit.
+        self.committer.commit('')
+        self.assertRaises(AssertionError, self.committer.writeFile, 'x', 'y')
+
+    def test_DirectBranchCommit_adds_file(self):
+        # DirectBranchCommit can add a new file to the branch.
+        self.committer.writeFile('file.txt', 'contents')
+        self.committer.commit('')
+        self.assertEqual({'file.txt': 'contents'}, self._getContents())
+
+    def test_DirectBranchCommit_updates_file(self):
+        # DirectBranchCommit can replace a file in the branch.
+        self.committer.writeFile('file.txt', 'contents')
+        self.committer.commit('')
+        self._setUpCommitter()
+        self.committer.writeFile('file.txt', 'changed')
+        self.committer.commit('')
+        self.assertEqual({'file.txt': 'changed'}, self._getContents())
+
+    def test_DirectBranchCommit_creates_directories(self):
+        # Files can be in subdirectories.
+        self.committer.writeFile('a/b/c.txt', 'ctext')
+        self.committer.commit('')
+        self.assertEqual({'a/b/c.txt': 'ctext'}, self._getContents())
+
+    def test_DirectBranchCommit_writes_new_file_twice(self):
+        # If you write the same new file multiple times before
+        # committing, the original wins.
+        self.committer.writeFile('x.txt', 'aaa')
+        self.committer.writeFile('x.txt', 'bbb')
+        self.committer.commit('')
+        self.assertEqual({'x.txt': 'aaa'}, self._getContents())
+
+    def test_DirectBranchCommit_updates_file_twice(self):
+        # If you update the same file multiple times before committing,
+        # the original wins.
+        self.committer.writeFile('y.txt', 'aaa')
+        self.committer.commit('')
+        self._setUpCommitter()
+        self.committer.writeFile('y.txt', 'bbb')
+        self.committer.writeFile('y.txt', 'ccc')
+        self.committer.commit('')
+        self.assertEqual({'y.txt': 'bbb'}, self._getContents())
+
+    def test_DirectBranchCommit_detects_race_condition(self):
+        # If the branch has been updated since it was last scanned,
+        # attempting to commit to it will raise ConcurrentUpdateError.
+        self.committer.writeFile('hi.c', 'main(){puts("hi world");}')
+        self.committer.commit('')
+        self._setUpCommitter(False)
+        self.committer.writeFile('hi.py', 'print "hi world"')
+        self.assertRaises(ConcurrentUpdateError, self.committer.commit, '')
+
+
+def test_suite():
+    return TestLoader().loadTestsFromName(__name__)
