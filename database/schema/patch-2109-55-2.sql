fcf48c71f9814601934041fe9b75da8bdb1bb3b1
Stuart Bishop <stuart.bishop@canonical.com>
2009-06-23 17:42:38 +0700

    Backport HWSubmission(lower(raw_emailaddress)) index from production

--- database/schema/patch-2109-55-2.sql
+++ database/schema/patch-2109-55-2.sql
@@ -1,5 +1,7 @@
 SET client_min_messages=ERROR;
 
 CREATE INDEX revisionauthor__lower_email__idx ON RevisionAuthor(lower(email));
+CREATE INDEX HWSubmission__lower_raw_emailaddress__idx
+    ON HWSubmission(lower(raw_emailaddress));
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2109, 55, 2);
