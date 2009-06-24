344f56b984d1107501301766008d5795ba5e8109
Karl Fogel <karl.fogel@canonical.com>
2009-06-24 17:17:33 -0400

    Add the copyright header block to more files (everything under database/).

--- database/schema/patch-2109-51-2.sql
+++ database/schema/patch-2109-51-2.sql
@@ -1,3 +1,6 @@
+-- Copyright 2009 Canonical Ltd.  This software is licensed under the
+-- GNU Affero General Public License version 3 (see the file LICENSE).
+
 SET client_min_messages = error;
 -- Fixes unique indexes from patch-44-2.sql: replace two partial unique
 -- indexes that include the variant column but are for cases where variant
