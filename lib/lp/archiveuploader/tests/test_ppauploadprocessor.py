d29cafa8397c44a401357f826d1369d96709dd8b
Julian Edwards <julian.edwards@canonical.com>
2009-06-25 15:39:18 +0100

    Add test.  TDD BABY.

--- lib/lp/archiveuploader/tests/test_ppauploadprocessor.py
+++ lib/lp/archiveuploader/tests/test_ppauploadprocessor.py
@@ -571,14 +571,27 @@ class TestPPAUploadProcessor(TestPPAUploadProcessorBase):
             self.uploadprocessor.last_processed_upload.rejection_message,
             "Signer has no upload rights to this PPA.")
 
-    def testPPAPartnerUploadFails(self):
-        """Upload a partner package to a PPA and ensure it's rejected."""
+    def testPPAPartnerUpload(self):
+        """Upload a partner package to a PPA and ensure it's not rejected."""
         upload_dir = self.queueUpload("foocomm_1.0-1", "~name16/ubuntu")
         self.processUpload(self.uploadprocessor, upload_dir)
 
+        # Check it's been successfully accepted.
         self.assertEqual(
-            self.uploadprocessor.last_processed_upload.rejection_message,
-            "PPA does not support partner uploads.")
+            self.uploadprocessor.last_processed_upload.queue_root.status,
+            PackageUploadStatus.DONE)
+
+        # We rely on the fact that the component on the source package
+        # release is unmodified, only the publishing component is
+        # changed to 'main'.  This allows the package to get copied to
+        # the main archive later, and will be published using the
+        # standard auto-overrides or fall back to the source's component
+        # if it's a new package.
+        pub_sources = self.name16.archive.getPublishedSources(name='foocomm')
+        [pub_foocomm] = pub_sources
+        self.assertEqual(
+            pub_foocomm.sourcepackagerelease.component.name, 'partner')
+        self.assertEqual(pub_foocomm.component.name, 'main')
 
     def testUploadSignedByNonUbuntero(self):
         """Check if a non-ubuntero can upload to his PPA."""
