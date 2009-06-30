73f8396e24c84ec14e55d857568ad5ae8053c841
Paul Hummer <paul@canonical.com>
2009-06-30 17:02:11 +0000

    Removed circular imports

--- lib/canonical/launchpad/interfaces/_schema_circular_imports.py
+++ lib/canonical/launchpad/interfaces/_schema_circular_imports.py
@@ -86,7 +86,6 @@ IHasMergeProposals['getMergeProposals'].queryTaggedValue(
     LAZR_WEBSERVICE_EXPORTED)['return_type'].value_type.schema = \
         IBranchMergeProposal
 
-# IBug
 
 IBug['linkBranch'].queryTaggedValue(
     LAZR_WEBSERVICE_EXPORTED)['return_type'].schema = IBugBranch
@@ -131,9 +130,6 @@ ISourcePackage['setBranch'].queryTaggedValue(
 ISourcePackage['setBranch'].queryTaggedValue(
     LAZR_WEBSERVICE_EXPORTED)['params']['branch'].schema = IBranch
 patch_reference_property(ISourcePackage, 'distribution', IDistribution)
-
-ISpecification['linkBranch'].queryTaggedValue(
-    LAZR_WEBSERVICE_EXPORTED)['return_type'].schema = ISpecificationBranch
 
 IPerson['hardware_submissions'].value_type.schema = IHWSubmission
 
