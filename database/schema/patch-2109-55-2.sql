be787665f1f0b0b702cc2a90b1a74a80483a5eef
Stuart Bishop <stuart.bishop@canonical.com>
2009-06-30 13:33:22 +0700

    Backport index needed for answers front page, as per Bug #393745

--- database/schema/patch-2109-55-2.sql
+++ database/schema/patch-2109-55-2.sql
@@ -3,5 +3,7 @@ SET client_min_messages=ERROR;
 CREATE INDEX revisionauthor__lower_email__idx ON RevisionAuthor(lower(email));
 CREATE INDEX HWSubmission__lower_raw_emailaddress__idx
     ON HWSubmission(lower(raw_emailaddress));
+CREATE INDEX question__status__datecreated__idx
+    ON Question(status, datecreated);
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2109, 55, 2);
