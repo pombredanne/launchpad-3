6e9258b30a1dabbbd67e05cd2d9cced94d205aba
Danilo Šegan <danilo@canonical.com>
2009-07-02 19:24:00 +0200

    Re-add a glob import for translationimportqueue.

--- lib/canonical/launchpad/interfaces/__init__.py
+++ lib/canonical/launchpad/interfaces/__init__.py
@@ -149,6 +149,6 @@ from lp.answers.interfaces.questionreopening import *
 from lp.answers.interfaces.questionsubscription import *
 from lp.answers.interfaces.questiontarget import *
 
-from lp.translations.interfaces.translationimportqueue import ITranslationImportQueue
+from lp.translations.interfaces.translationimportqueue import *
 from canonical.launchpad.interfaces._schema_circular_imports import *
 
