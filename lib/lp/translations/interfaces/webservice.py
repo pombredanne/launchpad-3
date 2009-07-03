365daae501d33635ca776473fab1803b9b18ece6
Danilo Šegan <danilo@canonical.com>
2009-07-03 15:35:32 +0200

    Add forgotten webservice.py file.

---
+++ lib/lp/translations/interfaces/webservice.py
@@ -0,0 +1,6 @@
+# Copyright 2009 Canonical Ltd.  All rights reserved.
+
+"""All the interfaces that are exposed through the webservice."""
+
+from lp.translations.interfaces.translationimportqueue import (
+    ITranslationImportQueue, ITranslationImportQueueEntry)
