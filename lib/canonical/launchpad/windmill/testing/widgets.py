4cf96cb1ab67419e5889b03f63bf9b35b7627435
Barry Warsaw <barry@canonical.com>
2009-07-06 15:08:58 -0400

    InlineEditorWidgetTest.__init__(): The widget tag must be configurable here too.
    With the change to sprites, there is no longer an a/img tag, so just click the
    a tag.
    
    Add a test for the programming languages.

--- lib/canonical/launchpad/windmill/testing/widgets.py
+++ lib/canonical/launchpad/windmill/testing/widgets.py
@@ -15,7 +15,8 @@ class InlineEditorWidgetTest:
     """Test that the inline editor widget is working properly on a page."""
 
     def __init__(self, url, widget_id, expected_value, new_value, name=None,
-                 suite='inline_editor', user=lpuser.NO_PRIV):
+                 suite='inline_editor', user=lpuser.NO_PRIV,
+                 widget_tag='h1'):
         """Create a new InlineEditorWidgetTest.
 
         :param url: The URL to the page on which the widget lives.
@@ -24,6 +25,7 @@ class InlineEditorWidgetTest:
         :param new_value: The value to change the field to.
         :param suite: The suite in which this test is part of.
         :param user: The user who should be logged in.
+        :param widget_tag: Element tag the widget is inside of.
         """
         self.url = url
         if name is None:
@@ -36,6 +38,7 @@ class InlineEditorWidgetTest:
         self.new_value = new_value
         self.suite = suite
         self.user = user
+        self.widget_tag = widget_tag
 
     def __call__(self):
         """Tests the widget is hooked and works properly.
@@ -53,35 +56,32 @@ class InlineEditorWidgetTest:
 
         client.open(url=self.url)
         client.waits.forPageLoad(timeout=u'20000')
+        widget_base = u"//%s[@id='%s']" % (self.widget_tag, self.widget_id)
         client.waits.forElement(
-            xpath=u"//h1[@id='%s']/a/img" % self.widget_id, timeout=u'8000')
+            xpath=widget_base + '/a', timeout=u'8000')
         client.asserts.assertText(
-            xpath=u"//h1[@id='%s']/span[1]" % self.widget_id,
-            validator=self.expected_value)
-        client.click(xpath=u"//h1[@id='%s']/a/img" % self.widget_id)
+            xpath=widget_base + '/span[1]', validator=self.expected_value)
+        client.click(xpath=widget_base + '/a')
         client.waits.forElement(
-            xpath=u"//h1[@id='%s']/a/img" % self.widget_id, timeout=u'8000')
+            xpath=widget_base + '/a', timeout=u'8000')
         client.waits.forElement(
-            timeout=u'8000',
-            xpath=u"//h1[@id='%s']//textarea" % self.widget_id)
+            xpath=widget_base + '//textarea', timeout=u'8000')
         client.type(
-            xpath=u"//h1[@id='%s']//textarea" % self.widget_id,
-            text=self.new_value)
-        client.click(xpath=u"//h1[@id='%s']//button[1]" % self.widget_id)
+            xpath=widget_base + '//textarea', text=self.new_value)
+        client.click(xpath=widget_base + '//button[1]')
         client.asserts.assertNode(
-            xpath=u"//h1[@id='%s']/span[1]" % self.widget_id)
+            xpath=widget_base + '/span[1]')
         client.asserts.assertText(
-            xpath=u"//h1[@id='%s']/span[1]" % self.widget_id,
-            validator=self.new_value)
+            xpath=widget_base + '/span[1]', validator=self.new_value)
 
         # And make sure it's actually saved on the server.
         client.open(url=self.url)
         client.waits.forPageLoad(timeout=u'20000')
         client.asserts.assertNode(
-            xpath=u"//h1[@id='%s']/span[1]" % self.widget_id)
+            xpath=widget_base + '/span[1]')
         client.asserts.assertText(
-            xpath=u"//h1[@id='%s']/span[1]" % self.widget_id,
-            validator=self.new_value)
+            xpath=widget_base + '/span[1]', validator=self.new_value)
+
 
 def _search_picker_widget(client, search_text, result_index):
     """Search in picker widget and select an item."""
