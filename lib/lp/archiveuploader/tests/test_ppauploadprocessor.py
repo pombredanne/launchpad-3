dc09eb6efdd00b5ba1629f411468f8e963827016
Julian Edwards <julian.edwards@canonical.com>
2009-07-06 15:06:02 +0100

    Recreate bug with test.

--- lib/lp/archiveuploader/tests/test_ppauploadprocessor.py
+++ lib/lp/archiveuploader/tests/test_ppauploadprocessor.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Copyright 2006 Canonical Ltd.  All rights reserved.
 
 """Functional tests for uploadprocessor.py."""
@@ -1005,6 +1006,40 @@ class TestPPAUploadProcessorFileLookups(TestPPAUploadProcessorBase):
         # Upload a higher version of bar that relies on the official
         # orig.tar.gz availability.
         self.uploadHigherBarToUbuntu()
+
+    def testErrorMessagesWithUnicode(self):
+        """Check that unicode errors messages are handled correctly.
+
+        Some error messages can contain the PPA display name, which may
+        sometimes contain unicode characters.  There was a bug
+        https://bugs.edge.launchpad.net/bugs/275509 reported about getting
+        upload errors related to unicode.  This only happened when the
+        uploder was attaching a .orig.tar.gz file with different contents
+        than the one already in the PPA.
+        """
+        # Ensure the displayname of the PPA has got unicode in it.
+        self.name16.archive.displayname = u"unicode PPA name: áří"
+
+        # Upload the first version.
+        upload_dir = self.queueUpload("bar_1.0-1", "~name16/ubuntu")
+        self.processUpload(self.uploadprocessor, upload_dir)
+        self.assertEqual(
+            self.uploadprocessor.last_processed_upload.queue_root.status,
+            PackageUploadStatus.DONE)
+
+        # The same 'bar' version will fail due to the conflicting
+        # 'orig.tar.gz' contents.
+        upload_dir = self.queueUpload("bar_1.0-1-ppa-orig", "~name16/ubuntu")
+        self.processUpload(self.uploadprocessor, upload_dir)
+
+        self.assertEqual(
+            self.uploadprocessor.last_processed_upload.rejection_message,
+            u'File bar_1.0.orig.tar.gz already exists in unicode PPA name: '
+            'áří, but uploaded version has different '
+            'contents. See more information about this error in '
+            'https://help.launchpad.net/Packaging/UploadErrors.\nFiles '
+            'specified in DSC are broken or missing, skipping package '
+            'unpack verification.')
 
     def testPPAConflictingOrigFiles(self):
         """When available, the official 'orig.tar.gz' restricts PPA uploads.
