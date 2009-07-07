cd0fa9eb1c862af9025129b720335d692ad82af9
Michael Nelson <michael.nelson@canonical.com>
2009-07-07 10:37:22 +0200

    Removed lint in test_doc.py.

--- lib/lp/registry/tests/test_doc.py
+++ lib/lp/registry/tests/test_doc.py
@@ -6,14 +6,11 @@ Run the doctests and pagetests.
 import logging
 import os
 import transaction
-import unittest
 
-from canonical.launchpad.testing.pages import PageTestSuite
 from canonical.launchpad.testing.systemdocs import (
     LayeredDocFileSuite, setUp, tearDown)
 from canonical.testing import (
-    DatabaseLayer, DatabaseFunctionalLayer, LaunchpadFunctionalLayer,
-    LaunchpadZopelessLayer)
+    DatabaseLayer, LaunchpadFunctionalLayer, LaunchpadZopelessLayer)
 
 from lp.registry.tests import mailinglists_helper
 from lp.services.testing import build_test_suite
