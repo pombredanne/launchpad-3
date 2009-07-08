794145a72ee1016964e3138950f39b911e4ab877
Francis J. Lacoste <francis.lacoste@canonical.com>
2009-07-08 17:50:44 -0400

    Move test_publisher to their proper location.

--- lib/canonical/shipit/tests/test_publisher.py
+++ lib/canonical/shipit/tests/test_publisher.py
@@ -1,25 +1,39 @@
 # Copyright 2006 Canonical Ltd.  All rights reserved.
-"""Tests the registration of the SSO request publication factory."""
+"""Tests the registration of ShipIt request publication factories."""
 
 __metaclass__ = type
 
 import unittest
 
-from canonical.signon.publisher import OpenIDBrowserRequest, OpenIDPublication
+from canonical.shipit.publisher import (
+    EdubuntuShipItBrowserRequest, KubuntuShipItBrowserRequest,
+    ShipItPublication, UbuntuShipItBrowserRequest)
 from canonical.testing.layers import FunctionalLayer
 
 from lp.testing import TestCase
 from lp.testing.publication import get_request_and_publication
 
 
-class SSORequestPublicationFactoryTestCase(TestCase):
+class ShipItRequestPublicationFactoryTestCase(TestCase):
     layer = FunctionalLayer
 
-    def test_openid(self):
+    def test_ubuntu_shipit(self):
         request, publication = get_request_and_publication(
-            'openid.launchpad.dev')
-        self.assertIsInstance(request, OpenIDBrowserRequest)
-        self.assertIsInstance(publication, OpenIDPublication)
+            'shipit.ubuntu.dev')
+        self.assertIsInstance(request, UbuntuShipItBrowserRequest)
+        self.assertIsInstance(publication, ShipItPublication)
+
+    def test_edubuntu_shipit(self):
+        request, publication = get_request_and_publication(
+            'shipit.edubuntu.dev')
+        self.assertIsInstance(request, EdubuntuShipItBrowserRequest)
+        self.assertIsInstance(publication, ShipItPublication)
+
+    def test_kubuntu_shipit(self):
+        request, publication = get_request_and_publication(
+            'shipit.kubuntu.dev')
+        self.assertIsInstance(request, KubuntuShipItBrowserRequest)
+        self.assertIsInstance(publication, ShipItPublication)
 
 
 def test_suite():
--- lib/canonical/signon/tests/test_publisher.py
+++ lib/canonical/signon/tests/test_publisher.py
@@ -1,39 +1,25 @@
 # Copyright 2006 Canonical Ltd.  All rights reserved.
-"""Tests the registration of ShipIt request publication factories."""
+"""Tests the registration of the SSO request publication factory."""
 
 __metaclass__ = type
 
 import unittest
 
-from canonical.shipit.publisher import (
-    EdubuntuShipItBrowserRequest, KubuntuShipItBrowserRequest,
-    ShipItPublication, UbuntuShipItBrowserRequest)
+from canonical.signon.publisher import OpenIDBrowserRequest, OpenIDPublication
 from canonical.testing.layers import FunctionalLayer
 
 from lp.testing import TestCase
 from lp.testing.publication import get_request_and_publication
 
 
-class ShipItRequestPublicationFactoryTestCase(TestCase):
+class SSORequestPublicationFactoryTestCase(TestCase):
     layer = FunctionalLayer
 
-    def test_ubuntu_shipit(self):
+    def test_openid(self):
         request, publication = get_request_and_publication(
-            'shipit.ubuntu.dev')
-        self.assertIsInstance(request, UbuntuShipItBrowserRequest)
-        self.assertIsInstance(publication, ShipItPublication)
-
-    def test_edubuntu_shipit(self):
-        request, publication = get_request_and_publication(
-            'shipit.edubuntu.dev')
-        self.assertIsInstance(request, EdubuntuShipItBrowserRequest)
-        self.assertIsInstance(publication, ShipItPublication)
-
-    def test_kubuntu_shipit(self):
-        request, publication = get_request_and_publication(
-            'shipit.kubuntu.dev')
-        self.assertIsInstance(request, KubuntuShipItBrowserRequest)
-        self.assertIsInstance(publication, ShipItPublication)
+            'openid.launchpad.dev')
+        self.assertIsInstance(request, OpenIDBrowserRequest)
+        self.assertIsInstance(publication, OpenIDPublication)
 
 
 def test_suite():
