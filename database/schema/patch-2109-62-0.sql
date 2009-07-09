13ddfc1284b29932cf453259f0fec5871594b1f8
Jeroen Vermeulen <jeroen.vermeulen@canonical.com>
2009-07-09 20:40:12 +0700

    Oops, insert new patch number into the database as well.

--- database/schema/patch-2109-62-0.sql
+++ database/schema/patch-2109-62-0.sql
@@ -2,4 +2,4 @@ SET client_min_messages=ERROR;
 
 DROP VIEW IF EXISTS POExport;
 
-INSERT INTO LaunchpadDatabaseRevision VALUES (2109, 99, 0);
+INSERT INTO LaunchpadDatabaseRevision VALUES (2109, 62, 0);
