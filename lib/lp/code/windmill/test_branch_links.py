3d144cc2700e40ca8f75658ef0f014a2e90fa421
Paul Hummer <paul@canonical.com>
2009-07-13 14:58:14 -0600

    Added windmill tests

--- lib/lp/code/windmill/test_branch_links.py
+++ lib/lp/code/windmill/test_branch_links.py
@@ -23,14 +23,29 @@ def test_inline_branch_bug_link_unlink():
     client.click(id=u'linkbug')
 
     client.waits.forElement(id=u'field.bug')
+    client.type(text=u'1', id=u'field.bug')
     client.click(xpath=u'//button[@name="buglink.actions.change"]')
 
     client.waits.forElement(id=u'buglink-1', timeout=u'10000')
-    client.asserts.assertText(id=u'buglink-1',
-        validator=u'')
+    client.asserts.assertText(id=u'linkbug',
+        validator=u'Link to another bug')
+
+    client.click(id=u'linkbug')
+    client.waits.forElement(id=u'field.bug')
+    client.type(text=u'2', id=u'field.bug')
+    client.click(xpath=u'//button[@name="buglink.actions.change"]')
+
+    client.waits.forElement(id=u'buglink-1', timeout=u'10000')
+    client.asserts.assertText(id=u'linkbug',
+        validator=u'Link to another bug')
 
     # And now to unlink.
-    client.click(id=u'remove-buglink-1')
-    # TODO: Add client.waits
-    # TODO: is there assertNoElement?
+    client.click(id=u'delete-buglink-1')
+    client.waits.sleep(milliseconds=3000)
+    client.asserts.assertNotNode(id=u'buglink-1')
+    client.click(id=u'delete-buglink-2')
+    client.waits.sleep(milliseconds=3000)
+    client.asserts.assertNotNode(id=u'buglink-2')
+    client.asserts.assertText(id=u'linkbug',
+        validator=u'Link to a bug report')
 
