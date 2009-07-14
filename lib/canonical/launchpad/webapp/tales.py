fb59c541fa9be60a0bcaaab80e374990c4e375de
Curtis Hovey <curtis.hovey@canonical.com>
2009-07-13 23:58:46 -0400

    Simplify the fmt:link options and ensure edit, remove and trash icons are after link text.

--- lib/canonical/launchpad/webapp/tales.py
+++ lib/canonical/launchpad/webapp/tales.py
@@ -1958,8 +1958,8 @@ class LinkFormatterAPI(ObjectFormatterAPI):
     """Adapter from Link objects to a formatted anchor."""
     final_traversable_names = {
         'icon': 'icon',
-        'icon-link': 'icon_link',
-        'link-icon': 'link_icon',
+        'icon-link': 'link',
+        'link-icon': 'link',
         }
 
     def icon(self):
@@ -1968,17 +1968,7 @@ class LinkFormatterAPI(ObjectFormatterAPI):
         return getMultiAdapter(
             (self._context, request), name="+inline-icon")()
 
-    def link_icon(self):
-        """Return the text and icon representation of the link."""
-        request = get_current_browser_request()
-        return getMultiAdapter(
-            (self._context, request), name="+inline-suffix")()
-
-    def icon_link(self):
-        """Return the icon and text representation of the link."""
-        return self.link(None)
-
-    def link(self, view_name, rootsite=None):
+    def link(self, view_name=None, rootsite=None):
         """Return the default representation of the link."""
         return self._context.render()
 
