298b219eb7f242b59b4697828a7d90775bf6a160
Jamal Fanaian <jfanaian@gmail.com>
2009-07-31 17:12:09 -0400

    Created a sort dictionary that defines the priority of each displayed category

--- lib/lp/registry/browser/person.py
+++ lib/lp/registry/browser/person.py
@@ -2517,7 +2517,10 @@ class PersonView(LaunchpadView, FeedsMixin):
         categories = set()
         for contrib in self.contributions:
             categories.update(category for category in contrib['categories'])
-        return sorted(categories, key=attrgetter('title'))
+        sort = {'code': 0, 'bugs': 1, 'blueprints': 2, 'translations': 3,
+                'answers': 4, 'specs': 5, 'soyuz': 6}
+        return sorted(categories, key=attrgetter('name'),
+                      cmp=lambda x,y: cmp(sort[x], sort[y]))
 
     @cachedproperty
     def context_is_probably_a_team(self):
