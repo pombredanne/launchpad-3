dddcec9199ce7e6deedce5b4675bb467c421c076
James Westby <james.westby@canonical.com>
2009-08-06 14:06:53 +0000

    Comments & docstrings.

--- lib/canonical/launchpad/webapp/tales.py
+++ lib/canonical/launchpad/webapp/tales.py
@@ -2350,7 +2350,11 @@ class FormattersAPI:
                 cgi.escape(lp_url),
                 cgi.escape(trailers))
         elif match.group("clbug") is not None:
+            # 'clbug' matches Ubuntu changelog format bugs. 'bugnumbers' is
+            # all of the bug numbers, that look something like "#1234, #434".
+            # 'leader' is the 'LP: ' bit at the beginning.
             bug_parts = []
+            # Split the bug numbers into multiple bugs.
             splitted = re.split("(,(?:\s|<br\s*/>)+)",
                     match.group("bugnumbers")) + [""]
             for bug_id, spacer in zip(splitted[::2], splitted[1::2]):
