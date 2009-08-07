2e08192307a321c9f13c693bbc6db93832efde73
Gavin Panella <gavin.panella@canonical.com>
2009-08-07 12:49:07 +0100

    Stupid thing works.

--- lib/lp/bugs/windmill/tests/test_bugs/test_bug_me_too.py
+++ lib/lp/bugs/windmill/tests/test_bugs/test_bug_me_too.py
@@ -6,6 +6,15 @@ from canonical.launchpad.windmill.testing import lpuser
 from windmill.authoring import WindmillTestClient
 
 
+AFFECTS_ME_TOO_XPATH = u"//span[@id='affectsmetoo']"
+DYNAMIC_SPAN_XPATH = AFFECTS_ME_TOO_XPATH + u"/span[@class='dynamic']"
+VALUE_LOCATION_XPATH = DYNAMIC_SPAN_XPATH + u"//span[@class='value']"
+EDIT_ICON_XPATH = DYNAMIC_SPAN_XPATH + u"//img[@class='editicon']"
+FLAME_ICON_XPATH = DYNAMIC_SPAN_XPATH + u"//img[contains(@src, 'flame-icon')]"
+
+OVERLAY_XPATH = u"//div[@id='yui-pretty-overlay-modal']"
+
+
 def test_me_too():
     """Test the "this bug affects me too" options on bug pages.
 
@@ -24,19 +33,22 @@ def test_me_too():
         xpath=(u"//span[@id='affectsmetoo' and "
                u"@class='yui-metoocs-content']"))
 
-    # Currently this bug does not affect the logged-in user.
+    # Currently it's unknown if this bug affects the logged-in user.
     client.asserts.assertText(
-        xpath=u"//span[@id='affectsmetoo']/span[@class='value']",
-        validator=u"This bug doesn't affect me")
+        xpath=VALUE_LOCATION_XPATH, validator=u"Does this bug affect you?")
+
+    # A flame icon is available in the page, but not visible owing to
+    # the unseen class.
+    client.asserts.assertElemJS(
+        xpath=FLAME_ICON_XPATH,
+        js="element.getAttribute('class').match(/unseen/) !== null")
 
     # There is an edit icon next to the text which can be clicked to
     # edit the "me too" status. However, we won't click it with
     # Windmill because the widget actually responds to mouse-down, and
     # Windmill seems to do something funny instead.
-    client.mouseDown(
-        xpath=u"//span[@id='affectsmetoo']//img[@class='editicon']")
-    client.mouseUp(
-        xpath=u"//span[@id='affectsmetoo']//img[@class='editicon']")
+    client.mouseDown(xpath=EDIT_ICON_XPATH)
+    client.mouseUp(xpath=EDIT_ICON_XPATH)
 
     # Wait for the modal dialog to appear.
     client.waits.forElement(id=u'yui-pretty-overlay-modal')
@@ -48,8 +60,7 @@ def test_me_too():
 
     # Wait for the modal dialog to disappear. Unfortunately the test
     # below doesn't work, nor does testing clientWidth, or anything I
-    # could think of, so it's commented out for now because chasing
-    # this is not a good use of time.
+    # could think of, so it's commented out.
 
     # client.asserts.assertElemJS(
     #     id=u'yui-pretty-overlay-modal',
@@ -59,7 +70,53 @@ def test_me_too():
     # However, we want to mark this bug as affecting the logged-in
     # user. We can also click on the content box of the "me too"
     # widget; we are not forced to use the edit icon.
-    client.click(xpath=u"//span[@id='affectsmetoo']")
+    client.click(xpath=AFFECTS_ME_TOO_XPATH)
     client.waits.forElement(id=u'yui-pretty-overlay-modal')
 
-    # XXX: Gavin Panella bug=361097 2009-07-31: Finish this.
+    # Let's say the bug does not affect the logged-in user.
+    client.click(
+        xpath=OVERLAY_XPATH + u"//a[contains(@href, '#false')]")
+
+    # Wait for the save to complete, attempt 1. Which doesn't work, so
+    # commented out.
+
+    # client.waits.forElementProperty(
+    #     xpath=EDIT_ICON_XPATH, option=(
+    #         u"src|https://bugs.launchpad.dev:8085/@@/edit|20000"))
+
+    # Wait for the save to complete, attempt 2. Which doesn't work
+    # either, so commented out.
+
+    # client.waits.forJS(js="""
+    #     document.evaluate(
+    #         "%s", document, null,
+    #         XPathResult.FIRST_ORDERED_NODE_TYPE, null
+    #     ).singleNodeValue.src.match(/edit$/) !== null
+    #     """ % EDIT_ICON_XPATH)
+
+    # Wait for the save to complete, attempt 3, the really stupid way,
+    # which can and will break occassionally.
+    client.waits.sleep(milliseconds=10000)
+
+    # What was I trying to test again? Oh yes. The bug is now marked
+    # as not affecting the current user.
+    client.asserts.assertText(
+        xpath=VALUE_LOCATION_XPATH, validator=u"This bug doesn't affect me")
+
+    # Hah! But it does! We made a mistake, oh noes.
+    client.click(xpath=AFFECTS_ME_TOO_XPATH)
+    client.waits.forElement(id=u'yui-pretty-overlay-modal')
+    client.click(
+        xpath=OVERLAY_XPATH + u"//a[contains(@href, '#true')]")
+
+    # The stupid broken wait again.
+    client.waits.sleep(milliseconds=10000)
+
+    # The bug is now marked as affecting the current user.
+    client.asserts.assertText(
+        xpath=VALUE_LOCATION_XPATH, validator=u"This bug affects me too")
+
+    # The flame icon is now visible.
+    client.asserts.assertElemJS(
+        xpath=FLAME_ICON_XPATH,
+        js="element.getAttribute('class').match(/unseen/) === null")
