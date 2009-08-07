d28b6ffa196c7d847b7f4eed15bd2f7c53ae063a
Ursula Junque <ursinha@canonical.com>
2009-08-07 02:44:55 -0300

    Adding the functions that actually do the test.

--- benchmarks/test_auth.py
+++ benchmarks/test_auth.py
@@ -20,15 +20,48 @@ class Auth(FunkLoadTestCase):
         self.logd("setUp")
         self.server_url = self.conf_get('main', 'url')
 
-        _error_re = re.compile('(?s)class="error message">(.*?)</')
+    _error_re = re.compile('(?s)class="error message">(.*?)</')
+
+    def assertNoFormErrors(self, response):
+        """Checks if the page returned ok and the login was sucessful."""
+        self.assertEquals(response.code, 200)
+        match = self._error_re.search(response.body)
+        if match is not None:
+            self.fail('Form contained error: %s' % match.group(1))
 
     def test_auth(self):
         """Runs the steps of a simple Launchpad login."""
-        pass
+
+        server_url = self.server_url
+
+        # Get the login page
+        response = self.get(server_url + "/+login", description="GET /+login")
+        self.assertNoFormErrors(response)
+
+        # The credentials of foo user, the loginator
+        email = 'foo@mailinator.com'
+        password = 'test'
+
+        # Get only the first form of the page, the login one,
+        # and fill it
+        fields = response.extractForm(path=[('form',0)], include_submit=True)
+        fields['loginpage_email'] = email
+        fields['loginpage_password'] = password
+
+        # Submit the login form.
+        response = self.post(
+            self.absolute_url(response, '/+login'),
+            fields, "POST /+login")
+        self.assertNoFormErrors(response)
+
+    def absolute_url(self, response, path):
+        """Calculate an absolute URL using the response and the path."""
+        return '%s://%s:%s%s' % (
+            response.protocol, response.server, response.port, path)
 
     def tearDown(self):
         """Finishes the test."""
-        pass
+        self.logd("tearDown.\n")
 
 
 if __name__ in ('main', '__main__'):
