3d42168554647c08d682f067b133eb3243ba503e
Curtis Hovey <curtis.hovey@canonical.com>
2009-08-09 23:39:09 -0400

    Pushed productseries driver rule into the model.

--- lib/lp/registry/model/product.py
+++ lib/lp/registry/model/product.py
@@ -897,8 +897,14 @@ class Product(SQLBase, BugTargetBase, MakesAnnouncements,
         # XXX: jamesh 2008-04-11
         # Set the ID of the new ProductSeries to avoid flush order
         # loops in ProductSet.createProduct()
-        return ProductSeries(productID=self.id, owner=owner, name=name,
+        series = ProductSeries(productID=self.id, owner=owner, name=name,
                              summary=summary, branch=branch)
+        if owner != self.owner and owner in self.drivers:
+            # The user is a product driver, and should be the driver of this
+            # series to make him the release manager.
+            series.driver = owner
+        return series
+
 
     def getRelease(self, version):
         """See `IProduct`."""
