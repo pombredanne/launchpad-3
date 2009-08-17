342be8753ccdf87b25718f498afcb9eba2588c4a
Gavin Panella <gavin.panella@canonical.com>
2009-08-17 17:14:15 +0100

    Use a retry() decorator to make checking for a save more reliable.

--- lib/lp/bugs/windmill/tests/test_bugs/test_bug_me_too.py
+++ lib/lp/bugs/windmill/tests/test_bugs/test_bug_me_too.py
@@ -1,9 +1,11 @@
 # Copyright 2009 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
+from itertools import count
+
 from canonical.launchpad.windmill.testing import lpuser
 
-from windmill.authoring import WindmillTestClient
+from windmill.authoring import WindmillTestClient, WindmillTestClientException
 
 
 AFFECTS_ME_TOO_XPATH = u"//span[@id='affectsmetoo']"
@@ -13,6 +15,27 @@ EDIT_ICON_XPATH = DYNAMIC_SPAN_XPATH + u"//img[@class='editicon']"
 FLAME_ICON_XPATH = DYNAMIC_SPAN_XPATH + u"//img[contains(@src, 'flame-icon')]"
 
 OVERLAY_XPATH = u"//div[@id='yui-pretty-overlay-modal']"
+
+
+def retry(client, attempts=3, delay=2000, initial_delay=1000):
+    """Decorator for attempting Windmill operations multiple times.
+
+    This exists because a lot of time has been spent trying to get
+    waits.forProperty() and waits.forJS() to work properly (at all,
+    really) in this test module, without success. It's also near
+    impossible to figure out why. Simply put, it's more reliable,
+    quicker and therefore less expensive to fix this here.
+    """
+    def decorator(func):
+        client.waits.sleep(milliseconds=initial_delay)
+        for attempt in count(1):
+            try:
+                return func(client)
+            except WindmillTestClientException:
+                if attempt == attempts:
+                    raise
+            client.waits.sleep(milliseconds=delay)
+    return decorator
 
 
 def test_me_too():
@@ -77,44 +100,27 @@ def test_me_too():
     client.click(
         xpath=OVERLAY_XPATH + u"//a[contains(@href, '#false')]")
 
-    # Wait for the save to complete, attempt 1. Which doesn't work, so
-    # commented out.
+    # Wait for the save to complete, by observing that the bug is now
+    # marked as affecting the current user.
+    @retry(client)
+    def check_for_save_not_affects(client):
+        client.asserts.assertText(
+            xpath=VALUE_LOCATION_XPATH,
+            validator=u"This bug doesn't affect me")
 
-    # client.waits.forElementProperty(
-    #     xpath=EDIT_ICON_XPATH, option=(
-    #         u"src|https://bugs.launchpad.dev:8085/@@/edit|20000"))
-
-    # Wait for the save to complete, attempt 2. Which doesn't work
-    # either, so commented out.
-
-    # client.waits.forJS(js="""
-    #     document.evaluate(
-    #         "%s", document, null,
-    #         XPathResult.FIRST_ORDERED_NODE_TYPE, null
-    #     ).singleNodeValue.src.match(/edit$/) !== null
-    #     """ % EDIT_ICON_XPATH)
-
-    # Wait for the save to complete, attempt 3, the really stupid way,
-    # which can and will break occassionally.
-    client.waits.sleep(milliseconds=10000)
-
-    # What was I trying to test again? Oh yes. The bug is now marked
-    # as not affecting the current user.
-    client.asserts.assertText(
-        xpath=VALUE_LOCATION_XPATH, validator=u"This bug doesn't affect me")
-
-    # Hah! But it does! We made a mistake, oh noes.
+    # Hah! But this bug does affect the logged-in user! The logged-in
+    # user made a mistake, oh noes. Better fix that.
     client.click(xpath=AFFECTS_ME_TOO_XPATH)
     client.waits.forElement(id=u'yui-pretty-overlay-modal')
     client.click(
         xpath=OVERLAY_XPATH + u"//a[contains(@href, '#true')]")
 
-    # The stupid broken wait again.
-    client.waits.sleep(milliseconds=10000)
-
     # The bug is now marked as affecting the current user.
-    client.asserts.assertText(
-        xpath=VALUE_LOCATION_XPATH, validator=u"This bug affects me too")
+    @retry(client)
+    def check_for_save_does_affect(client):
+        client.asserts.assertText(
+            xpath=VALUE_LOCATION_XPATH,
+            validator=u"This bug affects me too")
 
     # The flame icon is now visible.
     client.asserts.assertElemJS(
