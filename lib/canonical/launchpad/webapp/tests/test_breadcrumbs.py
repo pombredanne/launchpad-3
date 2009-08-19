1cac1b13f43e1bec3ee2b52c8964b0da904798a2
Guilherme Salgado <salgado@canonical.com>
2009-08-19 11:16:45 -0300

    A few small improvements to some tests

--- lib/canonical/launchpad/webapp/tests/test_breadcrumbs.py
+++ lib/canonical/launchpad/webapp/tests/test_breadcrumbs.py
@@ -11,7 +11,7 @@ from lp.testing import login
 
 
 class TestExtraVHostBreadcrumbsOnHierarchyView(BaseBreadcrumbTestCase):
-    """How our breadcrumbs behave when using a vhost other the main one?
+    """How our breadcrumbs behave when using a vhost other than the main one?
 
     When we go to bugs.lp.net/ubuntu, we only traversed the Ubuntu distro, so
     that's what we'd have a breadcrumb for, but we also want to generate a
@@ -24,12 +24,15 @@ class TestExtraVHostBreadcrumbsOnHierarchyView(BaseBreadcrumbTestCase):
         super(TestExtraVHostBreadcrumbsOnHierarchyView, self).setUp()
         login('test@canonical.com')
         self.product = self.factory.makeProduct(name='crumb-tester')
-        self.product_bug = self.factory.makeBug(product=self.product)
-        self.product_bug_url = canonical_url(self.product_bug)
+        self.product_url = canonical_url(self.product)
+        self.product_bugs_url = canonical_url(self.product, rootsite='bugs')
+        product_bug = self.factory.makeBug(product=self.product)
+        self.product_bugtask = product_bug.default_bugtask
+        self.product_bugtask_url = canonical_url(self.product_bugtask)
         self.source_package = self.factory.makeSourcePackage()
-        self.package_bug = self.factory.makeBugTask(
+        self.package_bugtask = self.factory.makeBugTask(
             target=self.source_package)
-        self.package_bug_url = canonical_url(self.package_bug)
+        self.package_bugtask_url = canonical_url(self.package_bugtask)
 
     def test_root_on_mainsite(self):
         urls = self._getBreadcrumbsURLs('http://launchpad.dev/', [self.root])
@@ -37,9 +40,8 @@ class TestExtraVHostBreadcrumbsOnHierarchyView(BaseBreadcrumbTestCase):
 
     def test_product_on_mainsite(self):
         urls = self._getBreadcrumbsURLs(
-            'http://launchpad.dev/%s' % self.product.name,
-            [self.root, self.product])
-        self.assertEquals(urls, ['http://launchpad.dev/crumb-tester'])
+            self.product_url, [self.root, self.product])
+        self.assertEquals(urls, [self.product_url])
 
     def test_root_on_vhost(self):
         urls = self._getBreadcrumbsURLs(
@@ -48,33 +50,31 @@ class TestExtraVHostBreadcrumbsOnHierarchyView(BaseBreadcrumbTestCase):
 
     def test_product_on_vhost(self):
         urls = self._getBreadcrumbsURLs(
-            'http://bugs.launchpad.dev/%s' % self.product.name,
-            [self.root, self.product])
-        self.assertEquals(
-            urls, ['http://launchpad.dev/crumb-tester',
-                   'http://bugs.launchpad.dev/crumb-tester'])
+            self.product_bugs_url, [self.root, self.product])
+        self.assertEquals(urls, [self.product_url, self.product_bugs_url])
 
-    def test_product_bug(self):
+    def test_product_bugtask(self):
         urls = self._getBreadcrumbsURLs(
-            self.product_bug_url, [self.root, self.product, self.product_bug])
+            self.product_bugtask_url,
+            [self.root, self.product, self.product_bugtask])
         self.assertEquals(
-            urls, ['http://launchpad.dev/crumb-tester',
-                   'http://bugs.launchpad.dev/crumb-tester'])
+            urls, [self.product_url, self.product_bugs_url,
+                   self.product_bugtask_url])
 
-    def test_package_bug(self):
-        target = self.package_bug.target
+    def test_package_bugtask(self):
+        target = self.package_bugtask.target
         distro_url = canonical_url(target.distribution)
         distroseries_url = canonical_url(target.distroseries)
         package_url = canonical_url(target)
         package_bugs_url = canonical_url(target, rootsite='bugs')
         urls = self._getBreadcrumbsURLs(
-            self.package_bug_url,
+            self.package_bugtask_url,
             [self.root, target.distribution, target.distroseries, target,
-             self.package_bug])
+             self.package_bugtask])
         self.assertEquals(
             urls,
             [distro_url, distroseries_url, package_url, package_bugs_url,
-             self.package_bug_url])
+             self.package_bugtask_url])
 
 
 def test_suite():
