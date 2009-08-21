9a54a29120102a0af2b816fe8f5a5d7cf24480af
Stuart Bishop <stuart.bishop@canonical.com>
2009-08-21 18:03:01 +0700

    Instead of os.stat to discover if files exist, refactor to use os.walk instead for much speed gain.

--- lib/canonical/librarian/librariangc.py
+++ lib/canonical/librarian/librariangc.py
@@ -7,6 +7,7 @@ __metaclass__ = type
 
 from datetime import datetime, timedelta
 import errno
+import re
 import sys
 from time import time
 import os
@@ -522,33 +523,69 @@ def delete_unwanted_files(con):
         else:
             return result[0]
 
-    count = 0
+    removed_count = 0
     next_wanted_content_id = get_next_wanted_content_id()
 
-    for content_id in range(1, max_id+1):
-        while (next_wanted_content_id is not None
-                and content_id > next_wanted_content_id):
-            next_wanted_content_id = get_next_wanted_content_id()
+    hex_content_id_re = re.compile('^[0-9a-f]{8}$')
 
-        file_wanted = (
-                next_wanted_content_id is not None
-                and next_wanted_content_id == content_id)
+    for dirpath, dirnames, filenames in os.walk(config.librarian_server.root):
 
-        path = get_file_path(content_id)
+        # Ignore known and harmless noise in the Librarian storage area.
+        if 'incoming' in dirnames:
+            dirnames.remove('incoming')
+        if 'librarian.pid' in filenames:
+            filenames.remove('librarian.pid')
 
-        if file_wanted:
-            if (config.librarian_server.upstream_host is None
-                and not os.path.exists(path)):
-                # It is normal to have files in the database that are
-                # not on disk if the Librarian has an upstream
-                # Librarian, such as on staging. Don't spam in this
-                # case.
+        for dirname in dirnames[:]:
+            if len(dirname) != 2:
+                dirnames.remove(dirname)
                 log.error(
-                    "LibraryFileContent %d exists in the db but not at %s"
-                    % (content_id, path))
-
-        else:
+                    "Ignoring directory %s that shouldn't be here" % dirname)
             try:
+                int(dirname, 16)
+            except ValueError:
+                dirnames.remove(dirname)
+                log.error("Ignoring invalid directory %s" % dirname)
+
+        # We need everything in order to ensure we visit files in the
+        # same order we retrieve wanted files from the database.
+        dirnames.sort()
+        filenames.sort()
+
+        # Noise in the storage area, or maybe we are looking at the wrong
+        # path?
+        if dirnames and filenames:
+            log.error(
+                "%s contains both files %r and subdirectories %r. Skipping."
+                % (dirpath, filenames, dirnames))
+            continue
+
+        for filename in filenames:
+            path = os.path.join(dirpath, filename)
+            hex_content_id = ''.join(path.split(os.sep)[-4:])
+            if hex_content_id_re.search(hex_content_id) is None:
+                log.error(
+                    "Ignoring invalid path %s" % path)
+                continue
+
+            content_id = int(''.join(path.split(os.sep)[-4:]), 16)
+
+            while (next_wanted_content_id is not None
+                    and content_id > next_wanted_content_id):
+
+                next_wanted_content_id = get_next_wanted_content_id()
+
+                if (config.librarian_server.upstream_host is None
+                        and next_wanted_content_id < content_id):
+                    log.error(
+                        "LibraryFileContent %d exists in the database but "
+                        "was not found on disk." % next_wanted_content_id)
+
+            file_wanted = (
+                    next_wanted_content_id is not None
+                    and next_wanted_content_id == content_id)
+
+            if not file_wanted:
                 one_day = 24 * 60 * 60
                 if time() - os.path.getctime(path) < one_day:
                     log.debug(
@@ -558,14 +595,21 @@ def delete_unwanted_files(con):
                     # File uploaded a while ago but no longer wanted.
                     os.unlink(path)
                     log.debug("Deleted %s" % path)
-                    count += 1
-            except OSError, e:
-                if e.errno != errno.ENOENT:
-                    raise
+                    removed_count += 1
+
+    # Report any remaining LibraryFileContent that the database says
+    # should exist but we didn't find on disk.
+    if next_wanted_content_id == content_id:
+        next_wanted_content_id = get_next_wanted_content_id()
+    while next_wanted_content_id is not None:
+        log.error(
+            "LibraryFileContent %d exists in the database but "
+            "was not found on disk." % next_wanted_content_id)
+        next_wanted_content_id = get_next_wanted_content_id()
 
     log.info(
             "Deleted %d files from disk that where no longer referenced "
-            "in the db" % count
+            "in the db" % removed_count
             )
 
 
