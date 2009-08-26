6f425696c07118a3d74857a45255902cb24c5653
William Grant <me@williamgrant.id.au>
2009-08-26 11:54:58 +1000

    Extend canBeNominatedFor tests to DistroSeries.

--- lib/lp/bugs/tests/test_bugnomination.py
+++ lib/lp/bugs/tests/test_bugnomination.py
@@ -12,18 +12,24 @@ from canonical.testing import DatabaseFunctionalLayer
 from lp.testing import TestCaseWithFactory
 
 
-class TestBugCanBeNominatedFor(TestCaseWithFactory):
+class TestBugCanBeNominatedForProductSeries(TestCaseWithFactory):
+    """Test IBug.canBeNominated for IProductSeries nominations."""
 
     layer = DatabaseFunctionalLayer
 
     def setUp(self):
-        super(TestBugCanBeNominatedFor, self).setUp()
+        super(TestBugCanBeNominatedForProductSeries, self).setUp()
         login('foo.bar@canonical.com')
         self.eric = self.factory.makePerson(name='eric')
         self.michael = self.factory.makePerson(name='michael')
+        self.setUpTarget()
 
+
+    def setUpTarget(self):
         self.series = self.factory.makeProductSeries()
         self.bug = self.factory.makeBug(product=self.series.product)
+        self.milestone = self.factory.makeMilestone(productseries=self.series)
+        self.random_series = self.factory.makeProductSeries()
 
     def test_canBeNominatedFor_series(self):
         # A bug may be nominated for a series of a product with an existing
@@ -38,8 +44,7 @@ class TestBugCanBeNominatedFor(TestCaseWithFactory):
 
     def test_not_canBeNominatedFor_non_series(self):
         # A bug may not be nominated for something other than a series.
-        milestone = self.factory.makeMilestone(productseries=self.series)
-        self.assertFalse(self.bug.canBeNominatedFor(milestone))
+        self.assertFalse(self.bug.canBeNominatedFor(self.milestone))
 
     def test_not_canBeNominatedFor_already_targeted_series(self):
         # A bug may not be nominated for a series if a task already exists.
@@ -53,12 +58,32 @@ class TestBugCanBeNominatedFor(TestCaseWithFactory):
     def test_not_canBeNominatedFor_random_series(self):
         # A bug may only be nominated for a series if that series' pillar
         # already has a task.
-        random_series = self.factory.makeProductSeries()
-        self.assertFalse(self.bug.canBeNominatedFor(random_series))
+        self.assertFalse(self.bug.canBeNominatedFor(self.random_series))
 
     def tearDown(self):
         logout()
-        super(TestBugCanBeNominatedFor, self).tearDown()
+        super(TestBugCanBeNominatedForProductSeries, self).tearDown()
+
+
+class TestBugCanBeNominatedForDistroSeries(
+    TestBugCanBeNominatedForProductSeries):
+    """Test IBug.canBeNominated for IDistroSeries nominations."""
+
+    def setUpTarget(self):
+        self.series = self.factory.makeDistroRelease()
+        # The factory can't create a distro bug directly.
+        self.bug = self.factory.makeBug()
+        self.bug.addTask(self.eric, self.series.distribution)
+        self.milestone = self.factory.makeMilestone(
+            distribution=self.series.distribution)
+        self.random_series = self.factory.makeDistroRelease()
+
+    def test_not_canBeNominatedFor_source_package(self):
+        # A bug may not be nominated directly for a source package. The
+        # distroseries must be nominated instead.
+        spn = self.factory.makeSourcePackageName()
+        source_package = self.series.getSourcePackage(spn)
+        self.assertFalse(self.bug.canBeNominatedFor(source_package))
 
 
 def test_suite():
