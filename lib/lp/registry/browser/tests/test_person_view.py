cad59b9dc305662c3cc1192f20ae6fb92eeec764
Jamal Fanaian <jamal@jfvm1>
2009-08-26 22:59:16 -0400

    Simplified the karma test by only creating totals of the specified products

--- lib/lp/registry/browser/tests/test_person_view.py
+++ lib/lp/registry/browser/tests/test_person_view.py
@@ -8,7 +8,7 @@ import unittest
 from canonical.config import config
 from canonical.launchpad.webapp.servers import LaunchpadTestRequest
 from canonical.testing import LaunchpadZopelessLayer
-from canonical.database.sqlbase import cursor
+from canonical.database.sqlbase import cursor, sqlvalues
 from lp.registry.browser.person import PersonView
 from lp.testing import TestCaseWithFactory
 from zope.component import getUtility
@@ -28,111 +28,44 @@ class TestPerson(TestCaseWithFactory):
 
     def test_karma_category_sort(self):
         # Add karma to some categories for the user
-        cur = cursor()
-        karmacache = self.makeKarmaCache(person=self.person, category_id=2)
-        karmacache = self.makeKarmaCache(person=self.person, category_id=7)
-        karmacache = self.makeKarmaCache(person=self.person, category_id=8) 
+        self.makeKarmaCache(person=self.person, category_id=2)
+        self.makeKarmaCache(person=self.person, category_id=7)
+        self.makeKarmaCache(person=self.person, category_id=8) 
 
-        # Update Karma totals
-        self.updateKarmaTotals()
-
-        # Get contributed categories for the user
+        # Get contributed categories for the factory user 
         categories = self.view.contributed_categories
         category_names = []
         for category in categories:
             category_names.append(category.name)
 
+        # Assert that the contributed categories are sorted correctly
         self.assertEqual(category_names, [u'code', u'bugs', u'answers'], 
                          'Categories are not sorted correctly')
 
     def makeKarmaCache(self, person=None, value=10, category_id=2,
                        product_id=5):
-
+        # Create KarmaCache Record
         LaunchpadZopelessLayer.switchDbUser(config.karmacacheupdater.dbuser)
         karmacache = getUtility(IKarmaCacheManager).new(
             person_id=person.id, value=value, category_id=category_id,
             product_id=product_id)
         LaunchpadZopelessLayer.commit()
 
-        return karmacache
-
-    def updateKarmaTotals(self):
-        LaunchpadZopelessLayer.switchDbUser(config.karmacacheupdater.dbuser)
-
-        cur = cursor()
-        cur.execute("DELETE FROM KarmaCache WHERE category IS NULL")
-        cur.execute("""
-            DELETE FROM KarmaCache
-            WHERE project IS NOT NULL AND product IS NULL""")
-        cur.execute("""
-            DELETE FROM KarmaCache
-            WHERE category IS NOT NULL AND project IS NULL AND product IS NULL
-                  AND distribution IS NULL AND sourcepackagename IS NULL""")
-        cur.execute("DELETE FROM KarmaCache WHERE karmavalue <= 0")
-
-        # - All actions with a specific category of a person.
-        cur.execute("""
-            INSERT INTO KarmaCache 
-                (person, category, karmavalue, product, distribution,
-                 sourcepackagename, project)
-            SELECT person, category, SUM(karmavalue), NULL, NULL, NULL, NULL
-            FROM KarmaCache
-            WHERE category IS NOT NULL
-            GROUP BY person, category
-            """)
-
-        # - All actions of a person on a given product.
-        cur.execute("""
+        # Update totals for this product
+        query = """
             INSERT INTO KarmaCache 
                 (person, category, karmavalue, product, distribution,
                  sourcepackagename, project)
             SELECT person, NULL, SUM(karmavalue), product, NULL, NULL, NULL
             FROM KarmaCache
-            WHERE product IS NOT NULL
+            WHERE product = %(product)s
+               AND person = %(person)s 
             GROUP BY person, product
-            """)
+            """ % sqlvalues(person=person.id, product=product_id)
+        cur = cursor()
+        cur.execute(query)
 
-        # - All actions of a person on a given distribution.
-        cur.execute("""
-            INSERT INTO KarmaCache 
-                (person, category, karmavalue, product, distribution,
-                 sourcepackagename, project)
-            SELECT person, NULL, SUM(karmavalue), NULL, distribution, NULL, NULL
-            FROM KarmaCache
-            WHERE distribution IS NOT NULL
-            GROUP BY person, distribution
-            """)
-
-        # - All actions of a person on a given project.
-        cur.execute("""
-            INSERT INTO KarmaCache 
-                (person, category, karmavalue, product, distribution,
-                 sourcepackagename, project)
-            SELECT person, NULL, SUM(karmavalue), NULL, NULL, NULL,
-                   Product.project
-            FROM KarmaCache
-            JOIN Product ON product = Product.id
-            WHERE Product.project IS NOT NULL AND product IS NOT NULL
-                  AND category IS NOT NULL
-            GROUP BY person, Product.project
-            """)
-
-        # - All actions with a specific category of a person on a given project
-        # IMPORTANT: This has to be the latest step; otherwise the rows
-        # inserted here will be included in the calculation of the overall
-        # karma of a person on a given project.
-        cur.execute("""
-            INSERT INTO KarmaCache 
-                (person, category, karmavalue, product, distribution,
-                 sourcepackagename, project)
-            SELECT person, category, SUM(karmavalue), NULL, NULL, NULL,
-                   Product.project
-            FROM KarmaCache
-            JOIN Product ON product = Product.id
-            WHERE Product.project IS NOT NULL AND product IS NOT NULL
-                  AND category IS NOT NULL
-            GROUP BY person, category, Product.project
-            """)
+        return karmacache
 
 def test_suite():
     return unittest.TestLoader().loadTestsFromName(__name__)
