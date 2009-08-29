133124f1ca2b58f083fff1385ca21d416ab61f28
Brad Crittenden <bac@canonical.com>
2009-08-29 15:37:54 -0400

    Reverted r 9261 for testfix

--- lib/lp/code/model/branchmergeproposal.py
+++ lib/lp/code/model/branchmergeproposal.py
@@ -34,7 +34,7 @@ from lp.code.model.branchrevision import BranchRevision
 from lp.code.model.codereviewcomment import CodeReviewComment
 from lp.code.model.codereviewvote import (
     CodeReviewVoteReference)
-from lp.code.model.diff import PreviewDiff
+from lp.code.model.diff import Diff, PreviewDiff
 from lp.registry.model.person import Person
 from lp.code.event.branchmergeproposal import (
     BranchMergeProposalStatusChangeEvent, NewCodeReviewCommentEvent,
@@ -658,8 +658,13 @@ class BranchMergeProposal(SQLBase):
                           source_revision_id, target_revision_id,
                           dependent_revision_id=None, conflicts=None):
         """See `IBranchMergeProposal`."""
-        # Create the PreviewDiff.
-        self.preview_diff = PreviewDiff.create(
+        if self.preview_diff is None:
+            # Create the PreviewDiff.
+            preview = PreviewDiff()
+            preview.diff = Diff()
+            self.preview_diff = preview
+
+        self.preview_diff.update(
             diff_content, diff_stat, source_revision_id, target_revision_id,
             dependent_revision_id, conflicts)
 
