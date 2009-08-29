1453c4f321dfb55dd9221f1be5c81968d3de794c
Danilo Šegan <danilo@canonical.com>
2009-08-29 14:07:15 +0200

    Replace distribution-language-pack-admin.pt with generic-edit.pt.

--- lib/lp/translations/browser/distribution.py
+++ lib/lp/translations/browser/distribution.py
@@ -54,6 +54,19 @@ class DistributionLanguagePackAdminView(LaunchpadEditFormView):
     label = "Change the language pack administrator"
     field_names = ['language_pack_admin']
 
+    @property
+    def cancel_url(self):
+        return canonical_url(self.context)
+
+    @property
+    def next_url(self):
+        return self.cancel_url
+
+    @property
+    def page_title(self):
+        'Change the %s language pack administrator' % (
+            self.context.displayname)
+
     @action("Change", name='change')
     def change_action(self, action, data):
         self.updateContextFromData(data)
