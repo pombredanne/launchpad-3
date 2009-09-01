1dec3167945de0c5bb238784e1e4f2c661af1064
Jonathan Lange <jml@canonical.com>
2009-09-01 00:01:48 +0000

    Don't export pocketsuffix

--- lib/lp/archivepublisher/publishing.py
+++ lib/lp/archivepublisher/publishing.py
@@ -1,8 +1,11 @@
 # Copyright 2009 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
-# XXX: Why is this exporting pocketsuffix? - jml
-__all__ = ['Publisher', 'pocketsuffix', 'suffixpocket', 'getPublisher']
+__all__ = [
+    'Publisher',
+    'suffixpocket',
+    'getPublisher',
+    ]
 
 __metaclass__ = type
 
