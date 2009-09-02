f32ff4b1723965b9fdf48a37d609a51f91bfc07b
Julian Edwards <julian.edwards@canonical.com>
2009-09-02 17:30:48 +0100

    Add distroarchseriesbinarypackagerelease breadcrumbs.

--- lib/lp/soyuz/browser/tests/test_breadcrumbs.py
+++ lib/lp/soyuz/browser/tests/test_breadcrumbs.py
@@ -27,8 +27,9 @@ class TestDistroArchSeriesBreadcrumb(BaseBreadcrumbTestCase):
     def test_distroarchseries(self):
         das_url = canonical_url(self.hoary_i386)
         urls = self._getBreadcrumbsURLs(das_url, self.traversed_objects)
-        self.assertEquals(urls[-1], das_url)
         texts = self._getBreadcrumbsTexts(das_url, self.traversed_objects)
+
+        self.assertEquals(urls[-1], das_url)
         self.assertEquals(texts[-1], "i386")
 
     def test_distroarchseriesbinarypackage(self):
@@ -36,9 +37,24 @@ class TestDistroArchSeriesBreadcrumb(BaseBreadcrumbTestCase):
         self.traversed_objects.append(pmount_hoary_i386)
         pmount_url = canonical_url(pmount_hoary_i386)
         urls = self._getBreadcrumbsURLs(pmount_url, self.traversed_objects)
-        self.assertEquals(urls[-1], pmount_url)
         texts = self._getBreadcrumbsTexts(pmount_url, self.traversed_objects)
+
+        self.assertEquals(urls[-1], pmount_url)
         self.assertEquals(texts[-1], "pmount")
+
+    def test_distroarchseriesbinarypackagerelease(self):
+        pmount_hoary_i386 = self.hoary_i386.getBinaryPackage("pmount")
+        pmount_release = pmount_hoary_i386['0.1-1']
+        self.traversed_objects.extend([pmount_hoary_i386, pmount_release])
+        pmount_release_url = canonical_url(pmount_release)
+        urls = self._getBreadcrumbsURLs(
+            pmount_release_url, self.traversed_objects)
+        texts = self._getBreadcrumbsTexts(
+            pmount_release_url, self.traversed_objects)
+
+        self.assertEquals(urls[-1], pmount_release_url)
+        self.assertEquals(texts[-1], "0.1-1")
+
 
 def test_suite():
     return unittest.TestLoader().loadTestsFromName(__name__)
