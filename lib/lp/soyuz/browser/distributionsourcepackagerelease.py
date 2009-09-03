e414ed45b88120fb907439b3ba9ea7003ff9830b
Celso Providelo <celso.providelo@canonical.com>
2009-09-03 13:47:23 -0300

    Adding view tests

--- lib/lp/soyuz/browser/distributionsourcepackagerelease.py
+++ lib/lp/soyuz/browser/distributionsourcepackagerelease.py
@@ -39,7 +39,7 @@ class DistributionSourcePackageReleaseNavigation(Navigation):
 
 
 class DistroSeriesBuilds:
-
+    """Group of `IDistroSeries` and `IBuild`s."""
     def __init__(self, series, builds):
         self.series = series
         self.builds = [
@@ -56,16 +56,26 @@ class DistributionSourcePackageReleaseView(LaunchpadView):
         return self.context.title
 
     @cachedproperty
+    def _cached_publishing_history(self):
+        return list(self.context.publishing_history)
+
+    @property
     def currently_published(self):
-        published_records = [
-            publishing for publishing in self.context.publishing_history
+        """A list of published publications for this source package release.
+
+        :return: a `list` of `SourcePackagePublishingHistory` currently
+            published in the main archives.
+        """
+        return [
+            publishing
+            for publishing in self._cached_publishing_history
             if publishing.status == PackagePublishingStatus.PUBLISHED
             ]
-        return list(published_records)
 
     @property
     def files(self):
-        last_publication = self.currently_published[0]
+        """The source package release files as `ProxiedLibraryFileAlias`."""
+        last_publication = self._cached_publishing_history[0]
         return [
             ProxiedLibraryFileAlias(
                 source_file.libraryfile, last_publication.archive)
@@ -73,6 +83,14 @@ class DistributionSourcePackageReleaseView(LaunchpadView):
 
     @cachedproperty
     def sponsor(self):
+        """The sponsor of this source package.
+
+        A source package was sponsored if the owner of the key used to sign
+        its upload is different of its 'creator' (DSC 'Changed-by:')
+
+        :return: return the sponsor `IPerson` or None if the source
+            upload was not sponsored.
+        """
         upload = self.context.package_upload
         if upload is None:
             return None
@@ -85,6 +103,11 @@ class DistributionSourcePackageReleaseView(LaunchpadView):
 
     @cachedproperty
     def grouped_builds(self):
+        """Builds for this source in the primary archive grouped by series.
+
+        :return: a `list` of `DistroSeriesBuilds` ordered by descending
+             versions.
+        """
         cached_builds = sorted(
             self.context.builds, key=operator.attrgetter('arch_tag'))
         series = set(
