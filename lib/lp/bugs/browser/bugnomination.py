608c30b1604334ef8fb1233a7f01da23c077f57c
Abel Deuring <abel.deuring@canonical.com>
2009-09-08 16:07:17 +0200

    bugnomination-edit.pt converted to 3.0 layout

--- lib/lp/bugs/browser/bugnomination.py
+++ lib/lp/bugs/browser/bugnomination.py
@@ -239,6 +239,11 @@ class BugNominationEditView(LaunchpadView):
         """Return the URL of the current bugtask."""
         return canonical_url(getUtility(ILaunchBag).bugtask)
 
+    @property
+    def page_title(self):
+        return 'Approve or decline nomination for bug #%d in %s' % (
+            self.context.bug.id, self.context.target.bugtargetdisplayname)
+
 
 class BugNominationContextMenu(BugContextMenu):
     usedfor = IBugNomination
