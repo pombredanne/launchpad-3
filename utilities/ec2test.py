645fe1e9ddf705c00d0a993b9ed71feed6f95cea
Michael Hudson <michael.hudson@canonical.com>
2009-09-10 17:21:20 +1200

    another header

--- utilities/ec2test.py
+++ utilities/ec2test.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python2.4
+
+# Copyright 2009 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Executable for the ec2test script."""
+
+__metaclass__ = type
+__all__ = []
+
 import _pythonpath
 from devscripts.ec2test import main
 main()
