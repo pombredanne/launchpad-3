4af6663e754d25178706a76694e112af54292e90
Michael Hudson <michael.hudson@canonical.com>
2009-09-10 17:20:38 +1200

    move main to its own file, add standard header to all new files

--- lib/devscripts/ec2test/ec2credentials.py
+++ lib/devscripts/ec2test/ec2credentials.py
@@ -1,3 +1,11 @@
+# Copyright 2009 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Module docstring goes here."""
+
+__metaclass__ = type
+__all__ = []
+
 import os
 
 import boto
