cb9010c20462ba7f60b50567fb7f90f143dd79c5
Michael Nelson <michael.nelson@canonical.com>
2009-09-15 15:55:00 +0200

    Updated to ensure the disable redirect link is only presented when redirect is currently enabled.

--- lib/canonical/launchpad/webapp/publisher.py
+++ lib/canonical/launchpad/webapp/publisher.py
@@ -272,6 +272,10 @@ class LaunchpadView(UserAttributeCache):
         """
         return self.request.response.getStatus() in [301, 302, 303, 307]
 
+    def isRedirectInhibited(self):
+        """Returns True if redirection has been inhibited."""
+        return self.request.cookies.get('inhibit_beta_redirect', '0') == '1'
+
     def __call__(self):
         self.initialize()
         if self._isRedirected():
