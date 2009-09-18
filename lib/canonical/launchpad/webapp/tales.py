59e1c0a66e06559cb3b98d5c4c9524d4088e6088
Michael Nelson <michael.nelson@canonical.com>
2009-09-18 16:53:15 +0200

    Improved solution after barry's review by adding a macro.

--- lib/canonical/launchpad/webapp/tales.py
+++ lib/canonical/launchpad/webapp/tales.py
@@ -3100,6 +3100,8 @@ class PageMacroDispatcher:
             return self.pagetype()
         elif name == 'show_actions_menu':
             return self.show_actions_menu()
+        elif name == 'isbetauser':
+            return getattr(self.context, 'isBetaUser', False)
         else:
             raise TraversalError(name)
 
