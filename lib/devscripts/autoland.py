f0ec86ea4c61933153ea68413b1ab46f4b896e4d
Jonathan Lange <jml@canonical.com>
2009-09-20 17:34:33 +1000

    Tie everything together to make the default command line.

--- lib/devscripts/autoland.py
+++ lib/devscripts/autoland.py
@@ -21,8 +21,6 @@ LPNET_SERVICE_ROOT = 'https://api.launchpad.net/beta/'
 # - whether or not it has been approved
 # - the branch owner
 
-# Given the branch owner, get their email
-
 # If the commit message is not given, insist on one from the command line
 
 # If the review has not been approved, warn.
@@ -71,6 +69,27 @@ class LaunchpadBranchLander:
         # XXX: Bug in lazr.uri -- it allows a path without a leading '/' and
         # then doesn't insert the '/' in the final product.
         return URI(scheme='bzr+ssh', host=host, path='/' + branch.unique_name)
+
+    def get_stakeholders(self, mp):
+        """Return a collection of people who should know about branch landing.
+
+        Used to determine who to email with the ec2 test results.
+
+        :param mp: A merge proposal.
+        :return: A set of `IPerson`s.
+        """
+        # XXX: Should this also include the registrant?
+        # XXX: Untested.
+        return set([mp.source_branch.owner, self._launchpad.me])
+
+
+def get_email(person):
+    email_object = person.preferred_email_address
+    # XXX: This raises a very obscure error when the email address isn't set.
+    # e.g. with name12 in the sample data. Not sure why this is -- file a bug.
+    # httplib2.RelativeURIError:
+    # Only absolute URIs are allowed. uri = tag:launchpad.net:2008:redacted
+    return email_object.email
 
 
 def get_bugs_clause(bugs):
@@ -148,6 +167,7 @@ def get_reviewer_clause(reviewers):
 def get_lp_commit_message(mp):
     """Get the Launchpad-style commit message for a merge proposal."""
     # XXX: Point to docs describing the rules for this.
+    # XXX: Handle testfix mode
     reviews = get_reviews(mp)
     bugs = get_bugs(mp)
     return '%s%s %s' % (
@@ -174,12 +194,26 @@ def get_bazaar_host(api_root):
             'API root.' % (api_root,))
 
 
+def assemble_command_line(emails, source_url, target_url, commit_message):
+    # XXX: no unit tests
+    # XXX: do shell quoting properly
+    # XXX: maybe not needed for normal operation, maybe call ec2 APIs directly
+    # XXX: maybe make a version that just does a pqm-submit w/ no tests
+    # XXX: maybe make a version that doesn't do headless
+    # XXX: maybe make a version that doesn't commit
+    emails = ' '.join(['--email=%s' % email for email in emails])
+    return 'ec2 test --headless %s -b launchpad=%s -s "%s" %s' % (
+        emails, target_url, commit_message, source_url)
+
+
 def main(argv):
     lander = LaunchpadBranchLander.load(DEV_SERVICE_ROOT)
     mp = lander.load_merge_proposal(argv[1])
-    print get_lp_commit_message(mp)
-    print 'Source:', lander.get_push_url(mp.source_branch)
-    print 'Target:', lander.get_push_url(mp.target_branch)
+    commit_message = get_lp_commit_message(mp)
+    source_url = lander.get_push_url(mp.source_branch)
+    target_url = lander.get_push_url(mp.target_branch)
+    emails = map(get_email, lander.get_stakeholders(mp))
+    print assemble_command_line(emails, source_url, target_url, commit_message)
     return 0
 
 
