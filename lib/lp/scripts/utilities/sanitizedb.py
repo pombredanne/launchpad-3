faf01debf1f6909e51bfdd74e4fcbff3ea6d1cd8
Stuart Bishop <stuart.bishop@canonical.com>
2009-09-21 19:23:26 +0700

    Set foreign key constraints to ON DELETE CASCADE temporarily

--- lib/lp/scripts/utilities/sanitizedb.py
+++ lib/lp/scripts/utilities/sanitizedb.py
@@ -3,11 +3,22 @@
 
 """Scrub a Launchpad database of private data."""
 
+import _pythonpath
+
 __metaclass__ = type
 __all__ = []
 
+import re
+import subprocess
+import sys
+
+import transaction
+from zope.component import getUtility
 
 from canonical.database.postgresql import ConnectionString
+from canonical.launchpad.interfaces import IMasterStore
+from canonical.launchpad.webapp.interfaces import (
+    IStoreSelector, MAIN_STORE, MASTER_FLAVOR)
 from canonical.lp import initZopeless
 from lp.services.scripts.base import LaunchpadScript
 
@@ -27,34 +38,47 @@ class SanitizeDb(LaunchpadScript):
         elif len(self.args) > 1:
             self.parser.error("Too many arguments.")
 
-        pg_connection_string = ConnectionString(self.args[0])
+        self.pg_connection_string = ConnectionString(self.args[0])
 
-        if 'prod' in str(pg_connection_string) and not self.options.force:
+        if ('prod' in str(self.pg_connection_string)
+            and not self.options.force):
             self.parser.error(
             "Attempting to sanitize a potential production database '%s'. "
             "--force required." % pg_connection_string.dbname)
 
-        self.logger.debug("Connect using '%s'." % pg_connection_string)
+        self.logger.debug("Connect using '%s'." % self.pg_connection_string)
 
         self.txn = initZopeless(
-            dbname=pg_connection_string.dbname,
-            dbhost=pg_connection_string.host,
-            dbuser=pg_connection_string.user,
+            dbname=self.pg_connection_string.dbname,
+            dbhost=self.pg_connection_string.host,
+            dbuser=self.pg_connection_string.user,
             implicitBegin=implicit_begin,
             isolation=isolation)
 
+        self.store = getUtility(IStoreSelector).get(MAIN_STORE, MASTER_FLAVOR)
+
     def main(self):
-        self.removePrivateBugs()
-        self.removePrivateBranches()
-        self.removeUnlinkedRevisions()
-        self.removePrivateTeams()
-        self.removePrivatePeople()
-        self.removeNonLaunchpadAccounts()
-        self.scrambleHiddenEmailAddresses()
+        self._allForeignKeysCascade()
+        transaction.commit()
+        try:
+            self.removePrivateBugs()
+            self.removePrivateBranches()
+            self.removeUnlinkedRevisions()
+            self.removePrivateTeams()
+            self.removePrivatePeople()
+            self.removeNonLaunchpadAccounts()
+            self.scrambleHiddenEmailAddresses()
+            transaction.commit()
+        finally:
+            transaction.abort()
+            self._resetForeignKeysCascade()
+            transaction.commit()
 
     def removePrivateBugs(self):
         """Remove all private bugs."""
-        raise NotImplemetnedError
+        from lp.bugs.model.bug import Bug
+        count = self.store.find(Bug, Bug.private == True).remove()
+        self.logger.info("Removed %d private bugs.", count)
 
     def removePrivateBranches(self):
         """Remove all private branches."""
@@ -75,6 +99,7 @@ class SanitizeDb(LaunchpadScript):
 
     def removeUnlinkedEmailAddresses(self):
         """Remove EmailAddress records not linked to a Person."""
+        raise NotImplementedError
 
     def scrambleHiddenEmailAddresses(self):
         """Hide email addresses users have requested to not be public.
@@ -82,4 +107,82 @@ class SanitizeDb(LaunchpadScript):
         This replaces the email addresses of all people with
         hide_email_addresses set with an @example.com email address.
         """
+        raise NotImplementedError
+
+    def _allForeignKeysCascade(self):
+        """Set all foreign key constraints to ON DELETE CASCADE.
+
+        The current state is recorded first so _resetForeignKeysCascade
+        can repair the changes.
+
+        Only tables in the public schema are modified.
+        """
+        # Get the SQL needed to create the foreign key constraints.
+        # pg_dump seems the only sane way of getting this. We could
+        # generate the SQL ourselves using the pg_constraints table,
+        # but that can change between PostgreSQL releases.
+        # Ideally we could use ALTER CONSTRAINT, but that doesn't exist.
+        # Or modify pg_constraints, but that doesn't work.
+        cmd = [
+            'pg_dump', '--no-privileges', '--no-owner', '--schema-only',
+            '--schema=public']
+        cmd.extend(
+            self.pg_connection_string.asPGCommandLineArgs().split(' '))
+        self.logger.debug("Running %s", ' '.join(cmd))
+        pg_dump = subprocess.Popen(
+            cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+            stdin=subprocess.PIPE)
+        (pg_dump_out, pg_dump_err) = pg_dump.communicate()
+        if pg_dump.returncode != 0:
+            self.fail("pg_dump returned %d" % pg_dump.returncode)
+
+        cascade_sql = []
+        restore_sql = []
+        pattern = r"""
+            (?x) ALTER \s+ TABLE \s+ ONLY \s+ (".*?"|\w+?) \s+
+            ADD \s+ CONSTRAINT \s+ (".*?"|\w+?) \s+ FOREIGN \s+ KEY [^;]+;
+            """
+        for match in re.finditer(pattern, pg_dump_out):
+            table = match.group(1)
+            constraint = match.group(2)
+
+            sql = match.group(0)
+
+            # Drop the existing constraint so we can recreate it.
+            drop_sql =  'ALTER TABLE %s DROP CONSTRAINT %s;' % (
+                table, constraint)
+            restore_sql.append(drop_sql)
+            cascade_sql.append(drop_sql)
+
+            # Store the SQL needed to restore the constraint.
+            restore_sql.append(sql)
+
+            # Recreate the constraint as ON DELETE CASCADE
+            sql = re.sub(r"""(?xs)^
+                (.*?)
+                (?:ON \s+ DELETE \s+ (?:NO\s+|SET\s+)?\w+)? \s*
+                ((?:NOT\s+)? DEFERRABLE|) \s*
+                (INITIALLY\s+(?:DEFERRED|IMMEDIATE)|) \s*;
+                """, r"\1 ON DELETE CASCADE \2 \3;", sql)
+            cascade_sql.append(sql)
+
+        # Set all the foreign key constraints to ON DELETE CASCADE, really.
+        self.logger.info(
+            "Setting %d constraints to ON DELETE CASCADE",
+            len(cascade_sql) / 2)
+        self.store.execute('\n'.join(cascade_sql))
+
+        # Store the recovery SQL.
+        self._reset_foreign_key_sql = restore_sql
+
+    def _resetForeignKeysCascade(self):
+        """Reset the foreign key constraints' ON DELETE mode."""
+        self.logger.info(
+            "Resetting %d foreign key constraints to initial state.",
+            len(self._reset_foreign_key_sql)/2)
+        self.store.execute('\n'.join(self._reset_foreign_key_sql))
+
+    def _fail(self, error_message):
+        self.logger.fatal(error_message)
+        sys.exit(1)
 
