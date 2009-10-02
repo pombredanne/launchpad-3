5282cb4dc9b593cf947a9f3f0539c71955f583e6
Gavin Panella <gavin.panella@canonical.com>
2009-10-02 09:39:28 +0100

    Change EC2SessionName.timestamp to .expires to correspond to its new role.

--- lib/devscripts/ec2test/session.py
+++ lib/devscripts/ec2test/session.py
@@ -8,8 +8,13 @@ __all__ = [
     'EC2SessionName',
     ]
 
+from datetime import datetime, timedelta
+
 from devscripts.ec2test.utils import (
     find_datetime_string, make_datetime_string, make_random_string)
+
+
+DEFAULT_LIFETIME = timedelta(hours=6)
 
 
 class EC2SessionName(str):
@@ -18,21 +23,49 @@ class EC2SessionName(str):
     This is used when naming key pairs and security groups, so it's
     useful to be unique. However, to aid garbage collection of old key
     pairs and security groups, the name contains a common element and
-    a timestamp. The form taken should always be:
+    an expiry timestamp. The form taken should always be:
 
-      <base-name>/<timestamp>/<random-data>
+      <base-name>/<expires-timestamp>/<random-data>
 
     None of the parts should contain forward-slashes, and the
-    timestamp should acceptable input to `find_datetime_string`.
+    timestamp should be acceptable input to `find_datetime_string`.
 
-    `EC2SessionName.make()` will generate a suitable name for you.
+    `EC2SessionName.make()` will generate a suitable name given a
+    suitable base name.
     """
 
     @classmethod
-    def make(cls, base):
+    def make(cls, base, expires=None):
+        """Create an `EC2SessionName`.
+
+        This checks that `base` does not contain a forward-slash, and
+        provides some convenient functionality for `expires`:
+
+        - If `expires` is None, it defaults to now (UTC) plus
+          `DEFAULT_LIFETIME`.
+
+        - If `expires` is a `datetime`, it is converted to a timestamp
+          in the correct form.
+
+        - If `expires` is a `timedelta`, it is added to now (UTC) then
+          converted to a timestamp.
+
+        - Otherwise `expires` is assumed to be a string, so is checked
+          for the absense of forward-slashes, and that a correctly
+          formed timestamp can be discovered.
+
+        """
         assert '/' not in base
-        return cls("%s/%s/%s" % (
-                base, make_datetime_string(), make_random_string()))
+        if expires is None:
+            expires = DEFAULT_LIFETIME
+        if isinstance(expires, timedelta):
+            expires = datetime.utcnow() + expires
+        if isinstance(expires, datetime):
+            expires = make_datetime_string(expires)
+        else:
+            assert '/' not in expires
+            assert find_datetime_string(expires) is not None
+        return cls("%s/%s/%s" % (base, expires, make_random_string()))
 
     @property
     def base(self):
@@ -42,7 +75,7 @@ class EC2SessionName(str):
         return parts[0]
 
     @property
-    def timestamp(self):
+    def expires(self):
         parts = self.split('/')
         if len(parts) != 3:
             return None
