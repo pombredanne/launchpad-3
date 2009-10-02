cf7e6e094f0d90530e4b7c74336968a3a2e84703
Muharem Hrnjadovic <muharem@ubuntu.com>
2009-10-02 04:25:10 +0200

    Expanded the test_package_upload_permissions_grant_branch_edit() test a bit and added another method (personMayEditBranch()) to make it work. Maybe personMayEditBranch() can be moved to a more fitting place and used in the forthcoming "source package branch" work.

--- lib/lp/code/tests/test_branch.py
+++ lib/lp/code/tests/test_branch.py
@@ -249,6 +249,59 @@ class TestWriteToBranch(PermissionTest):
         branch = self.makeOfficialPackageBranch()
         self.assertCanEdit(branch.owner, branch)
 
+    def personMayEditBranch(self, person, branch):
+        """Return True if person may edit branch.
+
+        A person P may be allowed to edit the branch B on the following
+        grounds:
+
+          - P is owner of B or member of team owning B
+          - B is a source package branch (i.e. a branch linked to a
+            source package SP in the distro series DS, component C) and
+            - P is authorised to upload SP in DS.distribution.main_archive
+            - P is authorised to upload to C in DS.distribution.main_archive
+            - P is authorised to upload SP via a package set
+
+        Please note: this method is not in the proper place here and needs
+        to find a better home.
+        """
+        def current_component(ds, package):
+            releases = ds.getCurrentSourceReleases(
+                [package.sourcepackagename])
+            return releases.get(package, None)
+
+        # This is a bit hacky but works in the context of the test at hand.
+        result = run_with_login(
+            person, check_permission, 'launchpad.Edit', branch)
+        # P is owner of B or member of team owning B.
+        if result == True:
+            return result
+
+        # Check whether we're dealing with a source package branch and
+        # whether person is authorised to upload the respective source
+        # package.
+        package = branch.sourcepackage
+        if package is None:
+            # No package .. hmm .. this can't be a source package branch
+            # then. Abort.
+            return False
+
+        distroseries = branch.distroseries
+        if distroseries is None:
+            # No distro series? Very fishy .. abort.
+            return False
+
+        archive = branch.distroseries.distribution.main_archive
+        spn = package.sourcepackagename
+        component = current_component(distroseries, package)
+
+        # Is person authorised to upload the source package this branch
+        # is targeting?
+        result = verify_upload(person, spn, archive, component)
+        # verify_upload() indicates that person *is* allowed to upload by
+        # returning None.
+        return result is None
+        
     def assertCanUpload(self, person, spn, archive, component,
                         strict_component=True):
         """Assert that 'person' can upload 'spn' to 'archive'."""
@@ -256,6 +309,20 @@ class TestWriteToBranch(PermissionTest):
         self.assertIs(
             None,
             verify_upload(person, spn, archive, component, strict_component))
+
+    def assertCannotUpload(self, reason, person, spn, archive, component):
+        """Assert that 'person' cannot upload to the archive.
+
+        :param reason: The expected reason for not being able to upload. A
+            string.
+        :param person: The person trying to upload.
+        :param spn: The `ISourcePackageName` being uploaded to. None if the
+            package does not yet exist.
+        :param archive: The `IArchive` being uploaded to.
+        :param component: The IComponent to which the package belongs.
+        """
+        exception = verify_upload(person, spn, archive, component)
+        self.assertEqual(reason, str(exception))
 
     def test_package_upload_permissions_grant_branch_edit(self):
         # If you can upload to the package, then you are also allowed to write
@@ -271,8 +338,22 @@ class TestWriteToBranch(PermissionTest):
         archive = branch.distroseries.distribution.main_archive
         spn = package.sourcepackagename
         self.permission_set.newPackageUploader(archive, person, spn)
+
+        # Make sure person *is* authorised to upload the source package
+        # targeted by the branch at hand.
         self.assertCanUpload(person, spn, archive, None)
-        self.assertCanEdit(person, branch)
+        # Now person should be able to edit.
+        self.assertTrue(self.personMayEditBranch(person, branch))
+
+        # person2 has no upload rights ..
+        person2 = self.factory.makePerson()
+        self.assertCannotUpload(
+            ("The signer of this package has no upload rights to this "
+             "distribution's primary archive.  Did you mean to upload to "
+             "a PPA?"),
+            person2, spn, archive, None)
+        # .. and is not authorised to edit the branch.
+        self.assertFalse(self.personMayEditBranch(person2, branch))
 
 
 def test_suite():
