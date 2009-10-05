cf1405a1e448cea07cee1344d1584d027aa69b93
Michael Hudson <michael.hudson@canonical.com>
2009-10-05 18:07:16 +1300

    stuff

--- lib/lp/codehosting/branch_ubuntu.py
+++ lib/lp/codehosting/branch_ubuntu.py
@@ -3,8 +3,33 @@
 
 """XXX write me."""
 
+from zope.component import getUtility
+
+from lp.code.interfaces.branchnamespace import IBranchNamespaceSet
+from lp.code.enums import BranchType
+from lp.registry.interfaces.sourcepackage import ISourcePackageFactory
+
 __metaclass__ = type
 __all__ = []
+
+
+def make_db_branch_in_new_distro_series(db_branch, new_distro_series):
+    new_namespace = getUtility(IBranchNamespaceSet).get(
+        person=db_branch.owner, product=None, distroseries=new_distro_series,
+        sourcepackagename=db_branch.sourcepackagename)
+    # Specify title?
+    new_branch = new_namespace.new(
+        BranchType.HOSTED, db_branch.name, db_branch.registrant)
+    return new_branch
+
+def clone_branch(db_branch, new_distro_series):
+    assert db_branch.product is None
+    assert db_branch.distroseries is not None
+    new_branch = make_db_branch_in_new_distro_series(
+        db_branch, new_distro_series)
+    getUtility(ISourcePackageFactory).new(
+        sourcepackagename=new_branch.sourcepackagename,
+        distroseries=new_distro_series)
 
 
 def branch_ubuntu():
