33f10f214db168542734a3b3454385d32397accb
Paul Hummer <paul@canonical.com>
2009-10-09 14:08:54 -0600

    Migrated test_code_review to CodeWindmillLayer

--- lib/lp/code/windmill/tests/test_code_review.py
+++ lib/lp/code/windmill/tests/test_code_review.py
@@ -5,35 +5,49 @@
 __metaclass__ = type
 __all__ = []
 
+import unittest
+
 import windmill
 from windmill.authoring import WindmillTestClient
 
 from canonical.launchpad.windmill.testing import lpuser
 from canonical.launchpad.windmill.testing.widgets import (
     search_picker_widget)
+from lp.code.windmill.testing import CodeWindmillLayer
+from lp.testing import TestCaseWithFactory
 
 
-MERGE_LINK = '%s/~name12/gnome-terminal/klingon/+register-merge' % (
-    windmill.settings['TEST_URL'])
 
-def test_inline_request_a_reviewer():
-    """Test inline request a reviewer."""
-    client = WindmillTestClient("Code review")
+class TestCodeReview(TestCaseWithFactory):
+    """Test the javascript functions of code review."""
 
-    lpuser.FOO_BAR.ensure_login(client)
+    layer = CodeWindmillLayer
 
-    client.open(
-        url=windmill.settings['TEST_URL'] + '/~name12/gnome-terminal/klingon/')
-    client.waits.forPageLoad(timeout=u'10000')
+    def test_inline_request_a_reviewer(self):
+        """Test inline request a reviewer."""
+        MERGE_LINK = '%s/~name12/gnome-terminal/klingon/+register-merge' % (
+            windmill.settings['TEST_URL'])
 
-    client.click(xpath=u'//a[@href="%s"]' % MERGE_LINK)
-    client.type(text=u'~name12/gnome-terminal/main',
-        id=u'field.target_branch.target_branch')
-    client.click(id=u'field.actions.register')
+        client = WindmillTestClient("Code review")
 
-    client.waits.forPageLoad(timeout=u'10000')
-    client.click(id=u'request-review')
+        lpuser.FOO_BAR.ensure_login(client)
 
-    search_picker_widget(client, u'mark', 1)
+        client.open(
+            url=windmill.settings['TEST_URL'] + '/~name12/gnome-terminal/klingon/')
+        client.waits.forPageLoad(timeout=u'10000')
 
-    client.waits.forElement(id=u'review-mark', timeout=u'10000')
+        client.click(xpath=u'//a[@href="%s"]' % MERGE_LINK)
+        client.type(text=u'~name12/gnome-terminal/main',
+            id=u'field.target_branch.target_branch')
+        client.click(id=u'field.actions.register')
+
+        client.waits.forPageLoad(timeout=u'10000')
+        client.click(id=u'request-review')
+
+        search_picker_widget(client, u'mark', 1)
+
+        client.waits.forElement(id=u'review-mark', timeout=u'10000')
+
+
+def test_suite():
+    return unittest.TestLoader().loadTestsFromName(__name__)
