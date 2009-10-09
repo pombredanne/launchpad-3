59c5a4fd8900f52bf8bbf2c91d16d5cfd12ecb6e
Paul Hummer <paul@canonical.com>
2009-10-09 12:52:32 -0600

    Converted branch_subscriptions to new CodeWindmillLayer

--- lib/lp/code/windmill/tests/test_branch_subscriptions.py
+++ lib/lp/code/windmill/tests/test_branch_subscriptions.py
@@ -6,74 +6,91 @@
 __metaclass__ = type
 __all__ = []
 
+import unittest
+
 import windmill
 from windmill.authoring import WindmillTestClient
 
 from canonical.launchpad.windmill.testing import lpuser
+from lp.code.windmill.testing import CodeWindmillLayer
+from lp.testing import TestCaseWithFactory
+
 
 
 # XXX: rockstar - Also needs testing : Admins can edit/delete, members of the
 # team can edit/delete.  At least the latter will require the Launchpad object
 # factory.
 
-def test_branch_subscription_ajax_load():
-    """Test branch subscriptions loaded via ajax."""
-    client = WindmillTestClient("Branch Subscription Ajax Load Test")
+class TestBranchSubscriptions(TestCaseWithFactory):
+    """Test subscriptions to branches."""
 
-    lpuser.FOO_BAR.ensure_login(client)
+    layer = CodeWindmillLayer
 
-    client.open(
-        url=windmill.settings['TEST_URL'] + '/~mark/firefox/release--0.9.1')
-    client.waits.forElement(id=u'none-subscribers', timeout=u'10000')
-    client.asserts.assertText(id=u'selfsubscription',
-        validator='Subscribe yourself')
-    client.asserts.assertText(id=u'none-subscribers',
-        validator=u'No subscribers.')
+    def test_branch_subscription_ajax_load():
+        """Test branch subscriptions loaded via ajax.
+        """
 
-    client.click(id=u'selfsubscription')
-    client.waits.forElement(id=u'yui-pretty-overlay-modal')
-    client.click(xpath=u'//button[@name="field.actions.change"]')
+        client = WindmillTestClient("Branch Subscription Ajax Load Test")
 
-    client.waits.forElement(id=u'editsubscription-icon-name16',
-        timeout=u'10000')
-    client.asserts.assertText(id=u'subscriber-name16',
-        validator=u'Foo Bar')
-    client.asserts.assertText(id=u'selfsubscription',
-        validator=u'Edit your subscription')
+        lpuser.FOO_BAR.ensure_login(client)
 
+        client.open(url=(
+            windmill.settings['TEST_URL'] + '/~mark/firefox/release--0.9.1'))
+        client.waits.forElement(id=u'none-subscribers', timeout=u'10000')
+        client.asserts.assertText(id=u'selfsubscription',
+            validator='Subscribe yourself')
+        client.asserts.assertText(id=u'none-subscribers',
+            validator=u'No subscribers.')
 
-    # And now to unsubscribe
-    client.click(id=u'editsubscription-icon-name16')
+        client.click(id=u'selfsubscription')
+        client.waits.forElement(id=u'yui-pretty-overlay-modal')
+        client.click(xpath=u'//button[@name="field.actions.change"]')
 
-    client.waits.forPageLoad(timeout=u'100000')
-    client.click(id=u'field.actions.unsubscribe')
-
-    client.waits.forElement(id=u'none-subscribers', timeout=u'10000')
-    client.asserts.assertText(id=u'selfsubscription',
-        validator='Subscribe yourself')
-    client.asserts.assertText(id=u'none-subscribers',
-        validator=u'No subscribers.')
+        client.waits.forElement(id=u'editsubscription-icon-name16',
+            timeout=u'10000')
+        client.asserts.assertText(id=u'subscriber-name16',
+            validator=u'Foo Bar')
+        client.asserts.assertText(id=u'selfsubscription',
+            validator=u'Edit your subscription')
 
 
-def test_team_edit_subscription_ajax_load():
-    """Test that team subscriptions are editable through the ajax portlet."""
-    client = WindmillTestClient("Branch Subscription Ajax Load Test")
+        # And now to unsubscribe
+        client.click(id=u'editsubscription-icon-name16')
 
-    lpuser.SAMPLE_PERSON.ensure_login(client)
+        client.waits.forPageLoad(timeout=u'100000')
+        client.click(id=u'field.actions.unsubscribe')
 
-    client.open(
-        url=windmill.settings['TEST_URL'] + '/~name12/landscape/feature-x/')
-    client.waits.forPageLoad(timeout=u'10000')
+        client.waits.forElement(id=u'none-subscribers', timeout=u'10000')
+        client.asserts.assertText(id=u'selfsubscription',
+            validator='Subscribe yourself')
+        client.asserts.assertText(id=u'none-subscribers',
+            validator=u'No subscribers.')
 
-    client.waits.forElement(id=u'editsubscription-icon-landscape-developers',
-        timeout=u'10000')
-    client.asserts.assertText(id=u'subscriber-landscape-developers',
-        validator=u'Landscape Developers')
-    client.click(id=u'editsubscription-icon-landscape-developers')
+    def test_team_edit_subscription_ajax_load():
+        """Test that team subscriptions are editable through the ajax portlet.
+        """
 
-    client.waits.forPageLoad(timeout=u'100000')
-    client.click(id=u'field.actions.unsubscribe')
+        client = WindmillTestClient("Branch Subscription Ajax Load Test")
 
-    client.waits.forElement(id=u'none-subscribers', timeout=u'10000')
-    client.asserts.assertText(id=u'none-subscribers',
-        validator=u'No subscribers.')
+        lpuser.SAMPLE_PERSON.ensure_login(client)
+
+        client.open(
+            url=windmill.settings['TEST_URL'] + '/~name12/landscape/feature-x/')
+        client.waits.forPageLoad(timeout=u'10000')
+
+        client.waits.forElement(id=u'editsubscription-icon-landscape-developers',
+            timeout=u'10000')
+        client.asserts.assertText(id=u'subscriber-landscape-developers',
+            validator=u'Landscape Developers')
+        client.click(id=u'editsubscription-icon-landscape-developers')
+
+        client.waits.forPageLoad(timeout=u'100000')
+        client.click(id=u'field.actions.unsubscribe')
+
+        client.waits.forElement(id=u'none-subscribers', timeout=u'10000')
+        client.asserts.assertText(id=u'none-subscribers',
+            validator=u'No subscribers.')
+
+
+def test_suite():
+    return unittest.TestLoader().loadTestsFromName(__name__)
