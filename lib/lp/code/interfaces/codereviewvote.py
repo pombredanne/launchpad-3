cfc7f9b2f8de17ba6a8726be491adfc3e47687f2
Nathan Handler <nhandler@ubuntu.com>
2009-10-08 23:15:55 -0500

    Make changes requested by Michael Hudson (mwhudson)

--- lib/lp/code/interfaces/codereviewvote.py
+++ lib/lp/code/interfaces/codereviewvote.py
@@ -58,8 +58,7 @@ class ICodeReviewVoteReference(Interface):
         TextLine(
             title=_('Review type'), required=False,
             description=_(
-                "Lowercase keywords describing the type of review you are "
-                 "performing or would like to be performed.")))
+                "Lowercase keywords describing the type of review.")))
 
     comment = exported(
         Reference(
