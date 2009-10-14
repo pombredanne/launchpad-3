024fce3da9deed2fb24e51681af9fac7d9726fa9
William Grant <me@williamgrant.id.au>
2009-10-14 12:15:43 +1100

    Add intermediate ddeb<->deb link attributes in nascentupload, and allow ddebs to be new.

--- lib/lp/archiveuploader/nascentuploadfile.py
+++ lib/lp/archiveuploader/nascentuploadfile.py
@@ -365,6 +365,7 @@ class BaseBinaryUploadFile(PackageUploadFile):
     """Base methods for binary upload modeling."""
 
     format = None
+    ddeb_file = None
 
     # Capitalised because we extract these directly from the control file.
     mandatory_fields = set(["Package", "Architecture", "Version"])
@@ -925,22 +926,6 @@ class DebBinaryUploadFile(BaseBinaryUploadFile):
 
 
 class DdebBinaryUploadFile(DebBinaryUploadFile):
-    """Represents an uploaded binary package file in ddeb format.
-
-    DDEBs are never considered 'NEW', they don't require review since
-    they are automatically generated.
-    """
+    """Represents an uploaded binary package file in ddeb format."""
     format = BinaryPackageFormat.DDEB
-
-    # Override the 'new' flag in a way any values set are ignored and
-    # it always return False.
-    def _get_new(self):
-        """DDEBs are never considered NEW."""
-        return False
-
-    def _set_new(self, value):
-        """DDEBs cannot be made NEW."""
-        pass
-
-    new = property(
-        _get_new, _set_new, doc="DDEBs are never flagged as NEW.")
+    deb_file = None
