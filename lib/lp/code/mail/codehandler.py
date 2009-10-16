8f484e5afcb9ca878fcf49a21be3faa0f269495c
Aaron Bentley <aaron@canonical.com>
2009-10-16 12:08:30 -0400

    Stop storing merge directive diffs.

--- lib/lp/code/mail/codehandler.py
+++ lib/lp/code/mail/codehandler.py
@@ -582,25 +582,10 @@ class CodeHandler:
                 'Error Creating Merge Proposal', body)
             return
 
-        if md.patch is not None:
-            diff_source = getUtility(IStaticDiffSource)
-            # XXX: Tim Penhey, 2009-02-12, bug 328271
-            # If the branch is private we should probably use the restricted
-            # librarian.
-            # Using the .txt suffix to allow users to view the file in
-            # firefox without firefox trying to get them to download it.
-            filename = '%s.diff.txt' % source.name
-            review_diff = diff_source.acquireFromText(
-                md.base_revision_id, md.revision_id, md.patch,
-                filename=filename)
-            transaction.commit()
-        else:
-            review_diff = None
 
         try:
             bmp = source.addLandingTarget(submitter, target,
-                                          needs_review=True,
-                                          review_diff=review_diff)
+                                          needs_review=True)
 
             context = CodeReviewEmailCommandExecutionContext(
                 bmp, submitter, notify_event_listeners=False)
