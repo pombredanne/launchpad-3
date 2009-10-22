8a442d9badfd7e5bf9ca0939c7f20b82f2e91def
Curtis Hovey <curtis.hovey@canonical.com>
2009-10-22 18:21:07 -0400

    Extracted PackagingDeleteView from DistributionSourcePackageView So that other product(series) can delete packaing links form their side.

--- lib/lp/registry/browser/distributionsourcepackage.py
+++ lib/lp/registry/browser/distributionsourcepackage.py
@@ -20,19 +20,15 @@ import operator
 import pytz
 
 from zope.component import getUtility
-from zope.formlib import form
 from zope.interface import implements, Interface
-from zope.schema import Choice
-from zope.schema.vocabulary import SimpleTerm, SimpleVocabulary
 
 from canonical.cachedproperty import cachedproperty
-from canonical.launchpad import _
 from lp.answers.interfaces.questionenums import QuestionStatus
 from lp.soyuz.interfaces.archive import IArchiveSet
 from lp.soyuz.interfaces.distributionsourcepackagerelease import (
     IDistributionSourcePackageRelease)
 from lp.soyuz.interfaces.packagediff import IPackageDiffSet
-from lp.registry.interfaces.packaging import IPackagingUtil
+from lp.registry.browser.packaging import PackagingDeleteView
 from lp.registry.interfaces.pocket import pocketsuffix
 from lp.registry.interfaces.product import IDistributionSourcePackage
 from lp.bugs.browser.bugtask import BugTargetTraversalMixin
@@ -41,7 +37,7 @@ from lp.answers.browser.questiontarget import (
 from canonical.launchpad.browser.structuralsubscription import (
     StructuralSubscriptionTargetTraversalMixin)
 from canonical.launchpad.webapp import (
-    ApplicationMenu, LaunchpadEditFormView, LaunchpadFormView, LaunchpadView,
+    ApplicationMenu, LaunchpadEditFormView, LaunchpadView,
     Link, Navigation, StandardLaunchpadFacets, action, canonical_url,
     redirection)
 from canonical.launchpad.webapp.menu import (
@@ -193,7 +189,7 @@ class DistributionSourcePackageBaseView:
 
 
 class DistributionSourcePackageView(DistributionSourcePackageBaseView,
-                                    LaunchpadFormView):
+                                    PackagingDeleteView):
     """View class for DistributionSourcePackage."""
     implements(IDistributionSourcePackageActionMenu)
 
@@ -201,17 +197,15 @@ class DistributionSourcePackageView(DistributionSourcePackageBaseView,
     def label(self):
         return self.context.title
 
-    def setUpFields(self):
-        """See `LaunchpadFormView`."""
-        # No schema is set in this form, because all fields are created with
-        # custom vocabularies. So we must not call the inherited setUpField
-        # method.
-        self.form_fields = self._createPackagingField()
-
     @property
-    def can_delete_packaging(self):
-        """Whether the user can delete existing packaging links."""
-        return self.user is not None
+    def next_url(self):
+        """See `LaunchpadFormView`."""
+        return canonical_url(self.context)
+
+    def get_packaging(self):
+        """See `PackagingDeleteView`."""
+        return [sourcepackage.direct_packaging
+                for sourcepackage in self.context.get_distroseries_packages()]
 
     @property
     def all_published_in_active_distroseries(self):
@@ -307,68 +301,6 @@ class DistributionSourcePackageView(DistributionSourcePackageBaseView,
             canonical_url(self.context.distribution),
             self.context.name,
             )
-
-    def _createPackagingField(self):
-        """Create a field to specify a Packaging association.
-
-        Create a contextual vocabulary that can specify one of the Packaging
-        associated to this DistributionSourcePackage.
-        """
-        terms = []
-        for sourcepackage in self.context.get_distroseries_packages():
-            packaging = sourcepackage.direct_packaging
-            if packaging is None:
-                continue
-            terms.append(SimpleTerm(packaging, packaging.id))
-        return form.Fields(
-            Choice(__name__='packaging', vocabulary=SimpleVocabulary(terms),
-                   required=True))
-
-    def _renderHiddenPackagingField(self, packaging):
-        """Render a hidden input that fills in the packaging field."""
-        if not self.can_delete_packaging:
-            return None
-        vocabulary = self.form_fields['packaging'].field.vocabulary
-        return '<input type="hidden" name="field.packaging" value="%s" />' % (
-            vocabulary.getTerm(packaging).token)
-
-    def renderDeletePackagingAction(self):
-        """Render a submit input for the delete_packaging_action."""
-        assert self.can_delete_packaging, 'User cannot delete Packaging.'
-        return ('<input type="submit" class="button" value="Delete Link" '
-                'style="padding: 0pt; font-size: 80%%" '
-                'name="%s"/>' % (self.delete_packaging_action.__name__,))
-
-    def handleDeletePackagingError(self, action, data, errors):
-        """Handle errors on package link deletion.
-
-        If 'packaging' is not set in the form data, we assume that means the
-        provided Packaging id was not found, which should only happen if the
-        same Packaging object was concurrently deleted. In this case, we want
-        to display a more informative error message than the default 'Invalid
-        value'.
-        """
-        if data.get('packaging') is None:
-            self.setFieldError(
-                'packaging',
-                _("This upstream association was deleted already."))
-
-    @action(_("Delete Link"), name='delete_packaging',
-            failure=handleDeletePackagingError)
-    def delete_packaging_action(self, action, data):
-        """Delete a Packaging association."""
-        packaging = data['packaging']
-        productseries = packaging.productseries
-        distroseries = packaging.distroseries
-        getUtility(IPackagingUtil).deletePackaging(
-            productseries, packaging.sourcepackagename, distroseries)
-        self.request.response.addNotification(
-            _("Removed upstream association between ${product} "
-              "${productseries} and ${distroseries}.", mapping=dict(
-              product=productseries.product.displayname,
-              productseries=productseries.displayname,
-              distroseries=distroseries.displayname)))
-        self.next_url = canonical_url(self.context)
 
     @cachedproperty
     def active_distroseries_packages(self):
--- lib/lp/registry/browser/packaging.py
+++ lib/lp/registry/browser/packaging.py
@@ -5,9 +5,13 @@ __metaclass__ = type
 
 __all__ = [
     'PackagingAddView',
+    'PackagingDeleteView',
     ]
 
 from zope.component import getUtility
+from zope.formlib import form
+from zope.schema import Choice
+from zope.schema.vocabulary import SimpleTerm, SimpleVocabulary
 
 from canonical.launchpad import _
 from lp.registry.interfaces.packaging import (
@@ -77,3 +81,84 @@ class PackagingAddView(LaunchpadFormView):
         getUtility(IPackagingUtil).createPackaging(
             productseries, data['sourcepackagename'], data['distroseries'],
             data['packaging'], owner=self.user)
+
+
+class PackagingDeleteView(LaunchpadFormView):
+    """A base view that provides packaging link deletion."""
+
+    def get_packaging(self):
+        """An iterator of the context's packaging links."""
+        raise NotImplementedError
+
+    def setUpFields(self):
+        """See `LaunchpadFormView`."""
+        # No schema is set in this form, because all fields are created with
+        # custom vocabularies. So we must not call the inherited setUpField
+        # method.
+        self.form_fields = self._createPackagingField()
+
+    @property
+    def can_delete_packaging(self):
+        """Whether the user can delete existing packaging links."""
+        return self.user is not None
+
+    def _createPackagingField(self):
+        """Create a field to specify a Packaging association.
+
+        Create a contextual vocabulary that can specify one of the Packaging
+        associated to this DistributionSourcePackage.
+        """
+        terms = []
+        for sourcepackage in self.context.get_distroseries_packages():
+            packaging = sourcepackage.direct_packaging
+            if packaging is None:
+                continue
+            terms.append(SimpleTerm(packaging, packaging.id))
+        return form.Fields(
+            Choice(__name__='packaging', vocabulary=SimpleVocabulary(terms),
+                   required=True))
+
+    def _renderHiddenPackagingField(self, packaging):
+        """Render a hidden input that fills in the packaging field."""
+        if not self.can_delete_packaging:
+            return None
+        vocabulary = self.form_fields['packaging'].field.vocabulary
+        return '<input type="hidden" name="field.packaging" value="%s" />' % (
+            vocabulary.getTerm(packaging).token)
+
+    def renderDeletePackagingAction(self):
+        """Render a submit input for the delete_packaging_action."""
+        assert self.can_delete_packaging, 'User cannot delete Packaging.'
+        return ('<input type="submit" class="button" value="Delete Link" '
+                'style="padding: 0pt; font-size: 80%%" '
+                'name="%s"/>' % (self.delete_packaging_action.__name__,))
+
+    def handleDeletePackagingError(self, action, data, errors):
+        """Handle errors on package link deletion.
+
+        If 'packaging' is not set in the form data, we assume that means the
+        provided Packaging id was not found, which should only happen if the
+        same Packaging object was concurrently deleted. In this case, we want
+        to display a more informative error message than the default 'Invalid
+        value'.
+        """
+        if data.get('packaging') is None:
+            self.setFieldError(
+                'packaging',
+                _("This upstream association was deleted already."))
+
+    @action(_("Delete Link"), name='delete_packaging',
+            failure=handleDeletePackagingError)
+    def delete_packaging_action(self, action, data):
+        """Delete a Packaging association."""
+        packaging = data['packaging']
+        productseries = packaging.productseries
+        distroseries = packaging.distroseries
+        getUtility(IPackagingUtil).deletePackaging(
+            productseries, packaging.sourcepackagename, distroseries)
+        self.request.response.addNotification(
+            _("Removed upstream association between ${product} "
+              "${productseries} and ${distroseries}.", mapping=dict(
+              product=productseries.product.displayname,
+              productseries=productseries.displayname,
+              distroseries=distroseries.displayname)))
