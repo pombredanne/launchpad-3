ea20268cd8979e50316e26931dcb8ffbd3827c39
Paul Hummer <paul@canonical.com>
2009-10-22 11:40:09 -0600

    Added upgrade_branches.py cronscript

---
+++ cronscripts/upgrade_branches.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python2.4
+#
+# Copyright 2009 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Upgrade branches to the most recent format."""
+
+__metaclass__ = type
+
+import _pythonpath
+
+from lp.services.job.runner import JobCronScript
+from lp.code.interfaces.branchjob import IBranchUpgradeJobSource
+#from lp.code.interfaces.branchmergeproposal import (
+#    IUpdatePreviewDiffJobSource,)
+
+
+class RunUpgradeBranches(JobCronScript):
+    """Run UpdatePreviewDiff jobs."""
+
+    config_name = 'upgrade_branches'
+    source_interface = IBranchUpgradeJobSource
+
+
+if __name__ == '__main__':
+    script = RunUpgradeBranches()
+    script.lock_and_run()
