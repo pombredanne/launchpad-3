da55aa94d0c8db7a48d8c98bbd7944d9ea49a77f
Henning Eggers <henning@canonical.com>
2009-10-27 13:00:21 +0100

    Fixed the bug.

--- lib/lp/translations/model/potemplate.py
+++ lib/lp/translations/model/potemplate.py
@@ -781,6 +781,10 @@ class POTemplate(SQLBase, RosettaStats):
         # Update cache to reflect the change.
         self._cached_pofiles_by_language[language_code, variant] = pofile
 
+        # Set dummy translations for translation credits in this POFile
+        for credits in self.getTranslationCredits():
+            credits.setTranslationCreditsToTranslated(pofile)
+
         if create_sharing:
             self._createPOFilesInSharingPOTemplates(pofile)
         # Store the changes.
