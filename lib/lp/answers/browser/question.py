b52fca4cc3354b187aeaf56e41c23f9159d31da9
Curtis Hovey <curtis.hovey@canonical.com>
2009-11-01 01:02:04 -0400

    Removed the old zope action hack--question-add-in-other-languages.txt passes with the favoured use of the method's name as a string.

--- lib/lp/answers/browser/question.py
+++ lib/lp/answers/browser/question.py
@@ -602,10 +602,7 @@ class QuestionAddView(QuestionSupportLanguageMixin, LaunchpadFormView):
             return self.search_template()
         return self.continue_action.success(data)
 
-    # XXX flacoste 2006-07-26: We use the method here instead of
-    # using the method name 'handleAddError' because of Zope issue 573
-    # which is fixed in 3.3.0b1 and 3.2.1
-    @action(_('Post Question'), name='add', failure=handleAddError)
+    @action(_('Post Question'), name='add', failure='handleAddError')
     def add_action(self, action, data):
         """Add a Question to an `IQuestionTarget`."""
         if self.shouldWarnAboutUnsupportedLanguage():
