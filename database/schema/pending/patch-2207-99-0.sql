bcc15c659ac105dbad65d7c783262d9c13ecd8de
Ursula Junque <ursinha@canonical.com>
2009-11-04 22:03:51 -0300

    the name of the foreign key was wrong, fixed it to represent the field

--- database/schema/pending/patch-2207-99-0.sql
+++ database/schema/pending/patch-2207-99-0.sql
@@ -2,7 +2,7 @@ SET client_min_messages=ERROR;
 
 ALTER TABLE Product
     ADD COLUMN translation_focus int,
-    ADD CONSTRAINT product__productseries__fk
+    ADD CONSTRAINT product__translation_focus__fk
         FOREIGN KEY (translation_focus) REFERENCES productseries(id);
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2207, 99, 0);
