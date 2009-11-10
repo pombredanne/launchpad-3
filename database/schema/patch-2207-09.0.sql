c95a033dd52e1c17c435117c20492ec345b8229a
Curtis Hovey <curtis.hovey@canonical.com>
2009-11-09 23:31:57 -0500

    Revised the patch pre review.

--- database/schema/patch-2207-09.0.sql
+++ database/schema/patch-2207-09.0.sql
@@ -3,8 +3,8 @@ SET client_min_messages=ERROR;
 -- Per Bug #196774
 ALTER TABLE Packaging
     DROP CONSTRAINT packaging_uniqueness,
-    ADD CONSTRAINT packaging_uniqueness
+    ADD CONSTRAINT packaging__distroseries__sourcepackagename__key
         UNIQUE (distroseries, sourcepackagename);
 
-INSERT INTO LaunchpadDatabaseRevision VALUES (2297, 0, 0);
+INSERT INTO LaunchpadDatabaseRevision VALUES (2207, 9, 0);
 
