5ce3a1facf9442718875503b6e11083e31ccb1a2
Curtis Hovey <curtis.hovey@canonical.com>
2009-11-14 20:05:49 -0500

    Remove launchpad.Editlocation permission. Updatd all affected objects to use launchpad.Edit. Remove UI and email elements that suggest a user can edit another user's location. A user can only set his location.

--- lib/lp/registry/browser/person.py
+++ lib/lp/registry/browser/person.py
@@ -954,7 +954,7 @@ class PersonOverviewMenu(ApplicationMenu, PersonMenuMixin):
         text = 'Update Jabber IDs'
         return Link(target, text, icon='edit')
 
-    @enabled_with_permission('launchpad.EditLocation')
+    @enabled_with_permission('launchpad.Edit')
     def editlocation(self):
         target = '+editlocation'
         text = 'Set location and time zone'
@@ -5333,6 +5333,7 @@ class PersonEditLocationView(LaunchpadFormView):
     """Edit a person's location."""
 
     schema = PersonLocationForm
+    field_names = ['location', 'hide']
     custom_widget('location', LocationWidget)
 
     @property
@@ -5341,19 +5342,6 @@ class PersonEditLocationView(LaunchpadFormView):
             "%s's location and timezone" % self.context.displayname)
 
     label = page_title
-
-    @property
-    def field_names(self):
-        """See `LaunchpadFormView`.
-
-        If the user has launchpad.Edit on this context, then allow him to set
-        whether or not the location should be visible.  The field for setting
-        the person's location is always shown.
-        """
-        if check_permission('launchpad.Edit', self.context):
-            return ['location', 'hide']
-        else:
-            return ['location']
 
     @property
     def initial_values(self):
