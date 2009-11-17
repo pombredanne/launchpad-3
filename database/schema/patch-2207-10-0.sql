fea853555d9262ccfd42aa8efd37fc8e3a01ee9e
Ursula Junque <ursinha@canonical.com>
2009-11-17 11:34:46 -0300

    changed the line in the patch with the correct patch number

--- database/schema/patch-2207-10-0.sql
+++ database/schema/patch-2207-10-0.sql
@@ -5,4 +5,4 @@ ALTER TABLE Product
     ADD CONSTRAINT product__translation_focus__fk
         FOREIGN KEY (translation_focus) REFERENCES productseries(id);
 
-INSERT INTO LaunchpadDatabaseRevision VALUES (2207, 99, 0);
+INSERT INTO LaunchpadDatabaseRevision VALUES (2207, 10, 0);
