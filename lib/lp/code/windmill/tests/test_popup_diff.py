65d5a8aeddaf209ca37ccad8c5afad35b49ec4c1
Tim Penhey <tim.penhey@canonical.com>
2009-11-17 18:01:36 +1300

    Clean up the windmill test and introduce a delay for the JS onload stuff as sometimes it appears that windmill notices that the page has loaded before all the JS connect functions have finised executing.

--- lib/lp/code/windmill/tests/test_popup_diff.py
+++ lib/lp/code/windmill/tests/test_popup_diff.py
@@ -18,32 +18,39 @@ from lp.code.windmill.testing import CodeWindmillLayer
 from lp.testing import TestCaseWithFactory
 
 
-POPUP_DIFF = u'//dd[contains(@class, "popup-diff")]/a'
+POPUP_DIFF = (
+    u'//dd[contains(@class, "popup-diff")]'
+    '/a[contains(@class, "js-action")]')
 VISIBLE_DIFF = (
     u'//table[contains(@class, "yui-diff-overlay") and '
      'not(contains(@class, "yui-diff-overlay-hidden"))]')
 CLOSE_VISIBLE_DIFF = (
     u'//table[contains(@class, "yui-diff-overlay")]'
      '//a[@class="close-button"]')
-
+JS_ONLOAD_EXECUTE_DELAY = 1000
 
 class TestPopupOnBranchPage(TestCaseWithFactory):
     """Test the popup diff."""
 
     layer = CodeWindmillLayer
 
-    def test_inline_branch_bug_link_unlink(self):
+    def test_branch_popup_diff(self):
         """Test branch diff popups."""
         client = WindmillTestClient("Branch popup diffs")
         make_erics_fooix_project(self.factory)
         transaction.commit()
 
         start_url = (
-            windmill.settings['TEST_URL'] + '/~fred/fooix/proposed')
+            windmill.settings['TEST_URL'] + '~fred/fooix/proposed')
         client.open(url=start_url)
         client.waits.forPageLoad(timeout=PAGE_LOAD)
+        # Sleep for a bit to make sure that the JS onload has had time to execute.
+        client.waits.sleep(milliseconds=JS_ONLOAD_EXECUTE_DELAY)
 
+        # Make sure that the link anchor has the js-action class.
+        client.asserts.assertNode(xpath=POPUP_DIFF)
         client.click(xpath=POPUP_DIFF)
+
         # Wait for the diff to show.
         client.waits.forElement(xpath=VISIBLE_DIFF)
         # Click on the close button.
