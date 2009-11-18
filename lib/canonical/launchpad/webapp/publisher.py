a60b65960f30637660f8fe4d316f92413130edd8
Gavin Panella <gavin.panella@canonical.com>
2009-11-18 15:54:47 +0000

    Remove striped_class() because it is not used anywhere and looks odd sitting on LaunchpadView.

--- lib/canonical/launchpad/webapp/publisher.py
+++ lib/canonical/launchpad/webapp/publisher.py
@@ -232,7 +232,6 @@ class LaunchpadView(UserAttributeCache):
                        many templates not set via zcml, or you want to do
                        rendering from Python.
     - isBetaUser   <-- whether the logged-in user is a beta tester
-    - striped_class<-- a tr class for an alternating row background
     """
 
     def __init__(self, context, request):
@@ -284,19 +283,6 @@ class LaunchpadView(UserAttributeCache):
             return u''
         else:
             return self.render()
-
-    @cachedproperty
-    def striped_class(self):
-        """Return a generator which yields alternating CSS classes.
-
-        This is to be used for HTML tables in which the row colors should be
-        alternated.
-        """
-        def bg_stripe_generator():
-            while True:
-                yield 'white'
-                yield 'shaded'
-        return bg_stripe_generator()
 
     def _getErrorMessage(self):
         """Property getter for `error_message`."""
