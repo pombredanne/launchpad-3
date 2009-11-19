6a4984bb8d50a9da3bc69ee243f3c94c1ae2e564
Brad Crittenden <bac@canonical.com>
2009-11-19 12:14:27 -0300

    Moved makeMirror to the factory.

--- lib/lp/registry/browser/tests/test_breadcrumbs.py
+++ lib/lp/registry/browser/tests/test_breadcrumbs.py
@@ -11,9 +11,6 @@ from canonical.launchpad.interfaces.launchpad import ILaunchpadCelebrities
 from canonical.launchpad.webapp.publisher import canonical_url
 from canonical.launchpad.webapp.tests.breadcrumbs import (
     BaseBreadcrumbTestCase)
-from lp.registry.interfaces.distributionmirror import (
-    MirrorContent, MirrorSpeed)
-from lp.services.worlddata.interfaces.country import ICountrySet
 
 
 class TestDistroseriesBreadcrumb(BaseBreadcrumbTestCase):
@@ -42,37 +39,16 @@ class TestDistributionMirrorBreadcrumb(BaseBreadcrumbTestCase):
     def setUp(self):
         super(TestDistributionMirrorBreadcrumb, self).setUp()
         self.distribution = getUtility(ILaunchpadCelebrities).ubuntu
-        self.distribution_url = canonical_url(self.distribution)
-
-    def _makeMirror(self, displayname, http_url=None, ftp_url=None,
-                    rsync_url=None):
-        """Create a mirror for the distribution."""
-        # If no URL is specified create an HTTP URL.
-        if http_url is None and ftp_url is None and rsync_url is None:
-            http_url = self.factory.getUniqueURL()
-        argentina = getUtility(ICountrySet)['AR']
-
-        mirror = self.distribution.newMirror(
-            owner=self.distribution.owner,
-            speed=MirrorSpeed.S256K,
-            country=argentina,
-            content=MirrorContent.ARCHIVE,
-            displayname=displayname,
-            description=None,
-            http_base_url=http_url,
-            ftp_base_url=ftp_url,
-            rsync_base_url=rsync_url,
-            official_candidate=False)
-        return (mirror, canonical_url(mirror))
 
     def test_distributionmirror_withDisplayName(self):
         # If a displayname is given, the breadcrumb text will be the
         # displayname.
         displayname = "Akbar and Jeff's Hut of Mirrors"
-        mirror, mirror_url = self._makeMirror(
+        mirror = self.factory.makeMirror(
+            distribution=self.distribution,
             displayname=displayname)
         crumbs = self._getBreadcrumbs(
-            mirror_url,
+            canonical_url(mirror),
             [self.root, self.distribution, mirror])
         last_crumb = crumbs[-1]
         self.assertEquals(
@@ -82,11 +58,12 @@ class TestDistributionMirrorBreadcrumb(BaseBreadcrumbTestCase):
         # If no displayname, the breadcrumb text will be the mirror name,
         # which is derived from the URL.
         http_url = "http://example.com/akbar"
-        mirror, mirror_url = self._makeMirror(
+        mirror = self.factory.makeMirror(
+            distribution=self.distribution,
             displayname=None,
             http_url=http_url)
         crumbs = self._getBreadcrumbs(
-            mirror_url,
+            canonical_url(mirror),
             [self.root, self.distribution, mirror])
         last_crumb = crumbs[-1]
         self.assertEquals(
@@ -96,11 +73,12 @@ class TestDistributionMirrorBreadcrumb(BaseBreadcrumbTestCase):
         # If no displayname, the breadcrumb text will be the mirror name,
         # which is derived from the URL.
         ftp_url = "ftp://example.com/jeff"
-        mirror, mirror_url = self._makeMirror(
+        mirror = self.factory.makeMirror(
+            distribution=self.distribution,
             displayname=None,
             ftp_url=ftp_url)
         crumbs = self._getBreadcrumbs(
-            mirror_url,
+            canonical_url(mirror),
             [self.root, self.distribution, mirror])
         last_crumb = crumbs[-1]
         self.assertEquals(
