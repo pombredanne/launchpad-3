10323e32af91d9c67df40c2087936e52936f13d0
Abel Deuring <abel.deuring@canonical.com>
2009-11-20 11:16:13 +0100

    Removed the column LibraryFileAlias.deleted again; having LibraryFileAlias.content nullable has the same meaning

--- database/schema/comments.sql
+++ database/schema/comments.sql
@@ -1438,7 +1438,6 @@ COMMENT ON COLUMN LibraryFileContent.sha256 IS 'The SHA256 sum of the file''s co
 
 COMMENT ON TABLE LibraryFileAlias IS 'LibraryFileAlias: A librarian file''s alias. The librarian stores, along with the file contents, a record stating the file name and mimetype. This table represents it.';
 COMMENT ON COLUMN LibraryFileAlias.content IS 'The libraryfilecontent which is the data in this file.';
-COMMENT ON COLUMN LibraryFileAlias.deleted IS 'This alias has been marked as deleted. The record will be deleted during the garbage collector run; attempts to access this alias via HTTP result in a 404 error.';
 COMMENT ON COLUMN LibraryFileAlias.filename IS 'The name of the file. E.g. "foo_1.0-1_i386.deb"';
 COMMENT ON COLUMN LibraryFileAlias.mimetype IS 'The mime type of the file. E.g. "application/x-debian-package"';
 COMMENT ON COLUMN LibraryFileAlias.expires IS 'The expiry date of this file. If NULL, this item may be removed as soon as it is no longer referenced. If set, the item will not be removed until this date. Once the date is passed, the file may be removed from disk even if this item is still being referenced (in which case content.deleted will be true)';
--- database/schema/patch-2207-99-0.sql
+++ database/schema/patch-2207-99-0.sql
@@ -1,10 +1,7 @@
 SET client_min_messages=ERROR;
 
 ALTER TABLE LibraryFileAlias
-    ADD COLUMN deleted boolean DEFAULT FALSE NOT NULL,
     ALTER COLUMN content DROP NOT NULL;
-
-CREATE INDEX libraryfilealias__deleted ON libraryfilealias(deleted);
 
 ALTER TABLE LibraryFileContent
     DROP COLUMN datemirrored,
