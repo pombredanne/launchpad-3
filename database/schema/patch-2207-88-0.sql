533f4d9e08694f439b6da0fb5e54c08f9a846e1e
Michael Hudson <michael.hudson@canonical.com>
2009-11-26 15:01:52 +1300

    more

--- database/schema/patch-2207-88-0.sql
+++ database/schema/patch-2207-88-0.sql
@@ -1,5 +1,26 @@
 SET client_min_messages=ERROR;
 
+CREATE TABLE SourcePackageRecipeData (
+    id serial PRIMARY KEY,
+    date_created timestamp without time zone DEFAULT timezone('UTC'::text, now()) NOT NULL,
+    distroseries integer NOT NULL REFERENCES DistroSeries,
+    sourcepackagename integer NOT NULL REFERENCES SourcePackageName,
+    recipe text NOT NULL
+);
+
+CREATE TABLE SourcePackageRecipeDataBranch (
+    id serial PRIMARY KEY,
+    sourcepackagerecipedata integer NOT NULL REFERENCES SourcePackageRecipeData,
+    branch integer NOT NULL REFERENCES branch
+);
+
+CREATE INDEX sourcepackagerecipedatabranch__sourcepackagerecipe
+    ON SourcePackageRecipeDataBranch USING btree(sourcepackagerecipedata);
+CREATE INDEX sourcepackagerecipedatabranch__branch
+    ON SourcePackageRecipeDataBranch USING btree(branch);
+
+-- unique on (branch, sourcepackagerecipe) ??
+
 CREATE TABLE SourcePackageRecipe (
     id serial PRIMARY KEY,
     date_created timestamp without time zone DEFAULT timezone('UTC'::text, now()) NOT NULL,
@@ -9,34 +30,45 @@ CREATE TABLE SourcePackageRecipe (
     distroseries integer NOT NULL REFERENCES DistroSeries,
     sourcepackagename integer NOT NULL REFERENCES SourcePackageName,
     name text NOT NULL,
-    recipe text NOT NULL
+    recipe_data integer NOT NULL REFERENCES SourcePackageRecipeData
 );
 
 CREATE UNIQUE INDEX sourcepackagerecipe__owner_distroseries_sourcepackagename_name
     ON SourcePackageRecipe
  USING btree(distroseries, sourcepackagename, name);
 
-CREATE TABLE SourcePackageRecipeBranch (
+CREATE TABLE SourcePackageBuild (
     id serial PRIMARY KEY,
-    sourcepackagerecipe integer NOT NULL REFERENCES SourcePackageRecipe,
-    branch integer NOT NULL REFERENCES branch
+    -- most of this is just copied from Build
+
+    -- I've dropped: processor, sourcepackagerelease, pocket
+    -- changed: distroarchseries to distroseries
+    -- add: recipe and manifest
+    date_created timestamp without time zone DEFAULT timezone('UTC'::text, ('now'::text)::timestamp(6) with time zone) NOT NULL,
+    processor integer NOT NULL,
+    distroseries integer NOT NULL REFERENCES distroseries,
+    archive integer NOT NULL REFERENCES Archive,
+    buildstate integer NOT NULL,
+    date_built timestamp without time zone,
+    buildduration interval,
+    build_log integer REFERENCES libraryfilealias,
+    builder integer REFERENCES builder,
+    dependencies text, -- do we want that?
+    estimated_build_duration interval,
+    build_warnings text,
+    date_first_dispatched timestamp without time zone,
+    requester integer REFERENCES Person,
+    recipe integer REFERENCES SourcePackageRecipe,
+    manifest integer REFERENCES SourcePackageRecipeData
 );
 
-CREATE INDEX sourcepackagerecipebranch__sourcepackagerecipe
-    ON SourcePackageRecipeBranch USING btree(sourcepackagerecipe);
-CREATE INDEX sourcepackagerecipebranch__branch
-    ON SourcePackageRecipeBranch USING btree(branch);
+ALTER TABLE SourcePackageRelease
+  ADD COLUMN source_package_build integer REFERENCES SourcePackageBuild;
 
 CREATE TABLE BuildSourcePackageFromRecipeJob (
     id serial PRIMARY KEY,
     job integer NOT NULL REFERENCES Job,
-    recipe integer NOT NULL REFERENCES SourcePackageRecipe,
-    archive integer NOT NULL REFERENCES Archive
-    -- requester is on Job already!
+    source_package_build integer REFERENCES SourcePackageBuild
 );
 
--- What about the results of building the package?  the manifest and
--- the log, mainly.
-
 INSERT INTO LaunchpadDatabaseRevision VALUES (2207, 88, 0);
-
