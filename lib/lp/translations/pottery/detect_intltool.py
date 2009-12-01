a7ca0c51b6df5b05cef63d8be41054fbc114f10f
Henning Eggers <henning@canonical.com>
2009-12-01 12:23:46 +0100

    Added intltool detection code and test.

---
+++ lib/lp/translations/pottery/detect_intltool.py
@@ -0,0 +1,71 @@
+#! /usr/bin/python2.5
+#
+# Copyright 2009 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Functions to detect if intltool can be used to generate a POT file for the
+package in the current directory."""
+
+__metaclass__ = type
+__all__ = [
+    'find_potfiles_in',
+    'check_potfiles_in',
+    'find_intltool_dirs',
+    ]
+
+import errno
+import os
+from subprocess import Popen, PIPE
+
+POTFILES_in = "POTFILES.in"
+
+def run_shell_command(cmd, env=None, input_data=None, raise_on_error=False):
+    """Run a shell command and return the output and status."""
+    stdin = None
+    if input_data:
+        stdin = PIPE
+    if env:
+        os.environ.update(env)
+        env = os.environ
+    pipe = Popen(cmd, shell=True, env=env, stdin=stdin, stdout=PIPE, stderr=PIPE)
+    if input_data:
+        try:
+            pipe.stdin.write(input_data)
+        except IOError, e:
+            if e.errno != errno.EPIPE:
+                raise
+    (output, errout) = pipe.communicate()
+    status = pipe.returncode
+    if raise_on_error and status != STATUS_OK:
+        raise OSError(status, errout)
+
+    return (status, output, errout)
+
+def find_potfiles_in():
+    """ Search the current directory and its subdirectories for POTFILES.in.
+
+    :returns: A list of names of directories that contain a file POTFILES.in.
+    """
+    result_dirs = []
+    for dirpath, dirnames, dirfiles in os.walk("."):
+        if POTFILES_in in dirfiles:
+            result_dirs.append(dirpath)
+    return result_dirs
+
+def check_potfiles_in(path):
+    """Check if the files listed in the POTFILES.in file exist."""
+    command = ("cd \"%(dir)s\" && rm -f missing notexist && "
+               "intltool-update -m" % { "dir" : path, })
+    (status, output, errs) = run_shell_command(command)
+
+    if status != 0:
+        return False
+
+    notexist = os.path.join(path, "notexist")
+    return not os.access(notexist, os.R_OK)
+
+def find_intltool_dirs():
+    """Search the current directory and its subdiretories for intltool
+    structure.
+    """
+    return []
---
+++ lib/lp/translations/tests/test_pottery_detect_intltool.py
@@ -0,0 +1,88 @@
+#! /usr/bin/python2.5
+#
+# Copyright 2009 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+import os
+import os.path
+import shutil
+import tarfile
+import tempfile
+import unittest
+
+from lp.translations.pottery import detect_intltool
+from lp.testing import TestCase
+
+class TestDetectIntltool(TestCase):
+
+    def setUp(self):
+        super(TestDetectIntltool, self).setUp()
+        # Determine test directoy and create temporary working directory.
+        self.curdir = os.getcwd()
+        self.testdir = os.path.join(self.curdir, os.path.dirname(__file__))
+        self.workdir = tempfile.mkdtemp()
+        os.chdir(self.workdir)
+
+    def tearDown(self):
+        # Remove temporary directory.
+        os.chdir(self.curdir)
+        shutil.rmtree(self.workdir)
+
+    def _prepare_package(self, packagename):
+        # Unpack the specified pacakge to run the test against it.
+        # Change to this directory.
+        packagepath = os.path.join(self.testdir, packagename+".tar.bz2")
+        tar = tarfile.open(packagepath, "r:bz2")
+        tar.extractall()
+        tar.close()
+        os.chdir(packagename)
+
+    def test_detect_potfiles_in(self):
+        # Find POTFILES.in in a packge with multiple dirs,
+        # only one has POTFILES.in.
+        self._prepare_package("intltool_POTFILES_in_1")
+        dirs = detect_intltool.find_potfiles_in()
+        self.assertContentEqual(["./po-intltool"], dirs)
+
+    def test_detect_potfiles_in_module(self):
+        # Find POTFILES.in in a packge with POTFILES.in at different levels.
+        self._prepare_package("intltool_POTFILES_in_2")
+        dirs = detect_intltool.find_potfiles_in()
+        self.assertContentEqual(["./po", "./module1/po"], dirs)
+
+    def test_check_potfiles_in_content_ok(self):
+        # Ideally all files listed in POTFILES.in exist in the source package.
+        self._prepare_package("intltool_single_ok")
+        self.assertTrue(detect_intltool.check_potfiles_in("./po")) 
+
+    def test_check_potfiles_in_content_ok_file_added(self):
+        # If a file is not listed in POTFILES.in, the file is still good for
+        # our purposes.
+        self._prepare_package("intltool_single_ok")
+        added_file = file("./src/sourcefile_new.c", "w")
+        added_file.write("/* Test file. */")
+        added_file.close()
+        self.assertTrue(detect_intltool.check_potfiles_in("./po")) 
+
+    def test_check_potfiles_in_content_not_ok_file_removed(self):
+        # If a file is missing that is listed in POTFILES.in, the file
+        # intltool structure is probably broken and cannot be used for
+        # our purposes.
+        self._prepare_package("intltool_single_ok")
+        os.remove("./src/sourcefile1.c")
+        self.assertFalse(detect_intltool.check_potfiles_in("./po")) 
+
+    def test_check_potfiles_in_wrong_directory(self):
+        # Passing in the wrong directory will cause the check to fail
+        # gracefully and return False.
+        self._prepare_package("intltool_single_ok")
+        self.assertFalse(detect_intltool.check_potfiles_in("./foo")) 
+
+    def disabled_test_find_intltool_dirs():
+        # Complete run: find all directories with intltool structure.
+        self._prepare_package("intltool_full_ok")
+#        self.assert
+
+def test_suite():
+    return unittest.TestLoader().loadTestsFromName(__name__)
+
