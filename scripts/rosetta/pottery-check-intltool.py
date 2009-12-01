71d126afd249130a75df29a8a7f597580ee04e72
Henning Eggers <henning@canonical.com>
2009-12-01 15:57:05 +0100

    Created top-level script for intltool detection.

---
+++ scripts/rosetta/pottery-check-intltool.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python2.5
+#
+# Copyright 2009 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Print a list of directories that contain a valid intltool structure."""
+
+from lp.translations.pottery.detect_intltool import find_intltool_dirs
+
+
+if __name__ == "__main__":
+    print "\n".join(find_intltool_dirs())
