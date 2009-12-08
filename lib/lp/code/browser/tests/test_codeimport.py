b6e83e489627615d381f86e082dd00768edd43d6
Michael Hudson <michael.hudson@canonical.com>
2009-12-08 15:32:03 +1300

    more tests, create bzr-svn imports by default

--- lib/lp/code/browser/tests/test_codeimport.py
+++ lib/lp/code/browser/tests/test_codeimport.py
@@ -5,9 +5,10 @@
 
 __metaclass__ = type
 
+import re
 import unittest
 
-#from canonical.launchpad.testing.pages import find_tag_by_id
+from canonical.launchpad.testing.pages import extract_text, find_tag_by_id
 from canonical.launchpad.webapp import canonical_url
 from canonical.testing.layers import DatabaseFunctionalLayer
 
@@ -19,12 +20,37 @@ class TestImportDetails(TestCaseWithFactory):
 
     layer = DatabaseFunctionalLayer
 
+    def assertSvnDetailsDisplayed(self, svn_details, rcs_type, url):
+        """Assert the `svn_details` tag described a Subversion import."""
+        self.assertEquals(rcs_type.title, svn_details.span['title'])
+        text = re.sub('\s+', ' ', extract_text(svn_details))
+        self.assertTrue(
+            text.startswith(
+                'This branch is an import of the Subversion branch'))
+        self.assertEquals(url, svn_details.a['href'])
+
     def test_bzr_svn_import(self):
-        # XXX
+        # The branch page for a bzr-svn-imported branch contains a summary of
+        # the import details.
         bzr_svn_import = self.factory.makeCodeImport(
             rcs_type=RevisionControlSystems.BZR_SVN)
         browser = self.getUserBrowser(canonical_url(bzr_svn_import.branch))
-        browser.getLink(bzr_svn_import.svn_branch_url)
+        svn_details = find_tag_by_id(browser.contents, 'svn-import-details')
+        self.assertSvnDetailsDisplayed(
+            svn_details, RevisionControlSystems.BZR_SVN,
+            bzr_svn_import.svn_branch_url)
+
+    def test_cscvs_svn_import(self):
+        # The branch page for a cscvs-imported svn branch contains a summary
+        # of the import details.
+        bzr_svn_import = self.factory.makeCodeImport(
+            rcs_type=RevisionControlSystems.SVN)
+        browser = self.getUserBrowser(canonical_url(bzr_svn_import.branch))
+        svn_details = find_tag_by_id(browser.contents, 'svn-import-details')
+        self.assertSvnDetailsDisplayed(
+            svn_details, RevisionControlSystems.SVN,
+            bzr_svn_import.svn_branch_url)
+
 
 
 def test_suite():
