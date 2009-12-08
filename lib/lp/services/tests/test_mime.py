12f2d7e5ecf4ec303821d2201f920ad6dd8d55b9
Brad Crittenden <bac@canonical.com>
2009-12-08 16:02:30 -0500

    Added test file that was not originally checked in.

---
+++ lib/lp/services/tests/test_mime.py
@@ -0,0 +1,33 @@
+# Copyright 2009 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Tests for mime module."""
+
+__metaclass__ = type
+
+import unittest
+
+from lp.services.mime import mimetypes
+from lp.testing import TestCase
+
+
+class TestBzip(TestCase):
+    """Tests for iter_split."""
+
+    def test_bzip2(self):
+        # Test for '.tar.bzip2' support.
+        filename = "foo.tar.bz2"
+        (application, encoding) = mimetypes.guess_type(filename)
+        self.assertEqual('application/x-tar', application)
+        self.assertEqual('bzip2', encoding)
+
+    def test_tbz2(self):
+        # Test for '.tar.bzip2' support.
+        filename = "foo.tbz2"
+        (application, encoding) = mimetypes.guess_type(filename)
+        self.assertEqual('application/x-tar', application)
+        self.assertEqual('bzip2', encoding)
+
+
+def test_suite():
+    return unittest.TestLoader().loadTestsFromName(__name__)
