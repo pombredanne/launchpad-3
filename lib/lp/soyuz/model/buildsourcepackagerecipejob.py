f9d61f6f80c75cb79da95b91af4c86fee8a063ba
Michael Hudson <michael.hudson@canonical.com>
2009-12-17 17:24:43 +1300

    remove stuff that is not close to ready for review

--- lib/lp/soyuz/model/buildsourcepackagerecipejob.py
+++
@@ -1,8 +0,0 @@
-# Copyright 2009 Canonical Ltd.  This software is licensed under the
-# GNU Affero General Public License version 3 (see the file LICENSE).
-
-"""Module docstring goes here."""
-
-__metaclass__ = type
-__all__ = []
-
