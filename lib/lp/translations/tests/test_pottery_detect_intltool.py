37485d6e70daa863a4e56ad846fb978aa6b5e9af
Henning Eggers <henning@canonical.com>
2009-12-23 12:30:37 +0100

    Satisfied lint checker even more.

--- lib/lp/translations/tests/test_pottery_detect_intltool.py
+++ lib/lp/translations/tests/test_pottery_detect_intltool.py
@@ -1,7 +1,6 @@
 # Copyright 2009 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
-import os
 import os.path
 import shutil
 import tarfile
