301bbf62ddba79cf9f44b34d6ee8787566858f00
Jelmer Vernooij <jelmer@canonical.com>
2010-01-05 02:59:28 +0100

    Implement branch deletion as DELETE operation.

--- lib/lp/code/interfaces/branch.py
+++ lib/lp/code/interfaces/branch.py
@@ -50,8 +50,8 @@ from lazr.restful.declarations import (
     call_with, collection_default_content, export_as_webservice_collection,
     export_as_webservice_entry, export_factory_operation,
     export_operation_as, export_read_operation, export_write_operation,
-    exported, operation_parameters, operation_returns_collection_of,
-    operation_returns_entry, REQUEST_USER)
+    export_destructor_operation, exported, operation_parameters,
+    operation_returns_collection_of, operation_returns_entry, REQUEST_USER)
 
 from canonical.config import config
 
@@ -681,10 +681,9 @@ class IBranch(IHasOwner, IPrivacy, IHasBranchTarget, IHasMergeProposals):
             required=True,
             readonly=False))
 
-    @export_operation_as('delete')
     @operation_parameters(
         break_references=Bool(title=_("Break references to this branch")))
-    @export_write_operation()
+    @export_destructor_operation()
     def destroySelf(break_references=False):
         """Delete the specified branch.
 
