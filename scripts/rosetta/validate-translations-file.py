887040026e1196f1c2ed1b74ecff6d0a015fbac2
Jeroen Vermeulen <jeroen.vermeulen@canonical.com>
2010-01-05 20:44:13 +0700

    Got working validator script.

---
+++ scripts/rosetta/validate-translations-file.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python2.5
+# Copyright 2009 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+__metaclass__ = type
+
+import _pythonpath
+
+import sys
+
+from lp.translations.scripts.validate_translations_file import (
+    ValidateTranslationsFile)
+
+
+if __name__ == "__main__":
+    sys.exit(ValidateTranslationsFile().main())
