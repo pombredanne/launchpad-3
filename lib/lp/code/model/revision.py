e8e9f6e29a0108174515a2db2a7a5860566c505a
Tim Penhey <tim.penhey@canonical.com>
2010-01-05 13:14:05 +1300

    Use the branch target to assign karma, which enables allocating karma to packages for revisions on package branches.

--- lib/lp/code/model/revision.py
+++ lib/lp/code/model/revision.py
@@ -87,9 +87,7 @@ class Revision(SQLBase):
         # If we know who the revision author is, give them karma.
         author = self.revision_author.person
         if (author is not None and branch.product is not None):
-            # No karma for junk branches as we need a product to link
-            # against.
-            karma = author.assignKarma('revisionadded', branch.product)
+            karma = branch.target.assignKarma(author, 'revisionadded')
             # Backdate the karma to the time the revision was created.  If the
             # revision_date on the revision is in future (for whatever weird
             # reason) we will use the date_created from the revision (which
