a6163bef18063277fc4a16889fa62885e2015dce
Jeroen Vermeulen <jeroen.vermeulen@canonical.com>
2010-01-05 22:23:05 +0700

    Completed tests.

--- lib/lp/translations/scripts/tests/test_validate_translations_file.py
+++ lib/lp/translations/scripts/tests/test_validate_translations_file.py
@@ -8,11 +8,18 @@
 __metaclass__ = type
 
 import logging
+import os.path
 from textwrap import dedent
 from unittest import TestCase, TestLoader
 
+from canonical.launchpad.ftests.script import run_script
+
+import lp.translations
+
 from lp.translations.scripts.validate_translations_file import (
     UnknownFileType, ValidateTranslationsFile)
+from lp.translations.utilities.tests.xpi_helpers import (
+    get_en_US_xpi_file_to_import)
 
 
 class TestValidateTranslationsFile(TestCase):
@@ -28,6 +35,12 @@ class TestValidateTranslationsFile(TestCase):
     def _strip(self, file_contents):
         """Remove leading newlines & indentation from file_contents."""
         return dedent(file_contents.strip())
+
+    def _findTestData(self):
+        """Return base path to this test's test data."""
+        return os.path.join(
+            os.path.dirname(lp.translations.__file__),
+            'scripts/tests/test-data')
 
     def test_validate_unknown(self):
         # Unknown filename extensions result in UnknownFileType.
@@ -101,10 +114,22 @@ class TestValidateTranslationsFile(TestCase):
         self.assertFalse(result)
 
     def test_validate_xpi_good(self):
-        pass
+        validator = self._makeValidator()
+        xpi_content = get_en_US_xpi_file_to_import('en-US').read()
+        result = validator._validateContent('pl.xpi', xpi_content)
+        self.assertTrue(result)
+
     def test_validate_xpi_bad(self):
-        pass
-    def test_readAndValidate(self):
-        pass
+        validator = self._makeValidator()
+        result = validator._validateContent('de.xpi', 'garble')
+        self.assertFalse(result)
+
     def test_script(self):
-        pass
+        test_input = os.path.join(self._findTestData(), 'minimal.pot')
+        script = 'scripts/rosetta/validate-translations-file.py'
+        result, out, err = run_script(script, [test_input])
+        self.assertEqual(0, result)
+
+
+def test_suite():
+    return TestLoader().loadTestsFromName(__name__)
