c5fe62ecbdb6f427d5a214cde8d95823c37dfd73
Henning Eggers <henning@canonical.com>
2010-01-06 15:00:20 +0100

    Implemented __getattr__.

--- lib/canonical/launchpad/utilities/personroles.py
+++ lib/canonical/launchpad/utilities/personroles.py
@@ -23,92 +23,13 @@ class PersonRoles:
         self._celebrities = getUtility(ILaunchpadCelebrities)
         self.inTeam = self.person.inTeam
 
-
-#    def __getattr__(self, name):
-#        """Handle all in_* attributes."""
-#        if not name.startswith('in_'):
-#            raise AttributeError
-#        return False
-
-    @property
-    def is_admin(self):
-        return self.person.inTeam(self._celebrities.admin)
-
-    @property
-    def is_bazaar_expert(self):
-        return self.person.inTeam(self._celebrities.bazaar_experts)
-
-    @property
-    def is_bug_importer(self):
-        return self.person.inTeam(self._celebrities.bug_importer)
-
-    @property
-    def is_bug_watch_updater(self):
-        return self.person.inTeam(self._celebrities.bug_watch_updater)
-
-    @property
-    def is_buildd_admin(self):
-        return self.person.inTeam(self._celebrities.buildd_admin)
-
-    @property
-    def is_commercial_admin(self):
-        return self.person.inTeam(self._celebrities.commercial_admin)
-
-    @property
-    def is_in_hwdb_team(self):
-        return self.person.inTeam(self._celebrities.hwdb_team)
-
-    @property
-    def is_janitor(self):
-        return self.person.inTeam(self._celebrities.janitor)
-
-    @property
-    def is_katie(self):
-        return self.person.inTeam(self._celebrities.katie)
-
-    @property
-    def is_lp_beta_tester(self):
-        return self.person.inTeam(self._celebrities.launchpad_beta_testers)
-
-    @property
-    def is_lp_developer(self):
-        return self.person.inTeam(self._celebrities.launchpad_developers)
-
-    @property
-    def is_mailing_list_expert(self):
-        return self.person.inTeam(self._celebrities.mailing_list_experts)
-
-    @property
-    def is_ppa_key_guard(self):
-        return self.person.inTeam(self._celebrities.ppa_key_guard)
-
-    @property
-    def is_registry_expert(self):
-        return self.person.inTeam(self._celebrities.registry_experts)
-
-    @property
-    def is_rosetta_expert(self):
-        return self.person.inTeam(self._celebrities.rosetta_experts)
-
-    @property
-    def is_shipit_admin(self):
-        return self.person.inTeam(self._celebrities.shipit_admin)
-
-    @property
-    def is_in_ubuntu_branches(self):
-        return self.person.inTeam(self._celebrities.ubuntu_branches)
-
-    @property
-    def is_in_ubuntu_security(self):
-        return self.person.inTeam(self._celebrities.ubuntu_security)
-
-    @property
-    def is_in_ubuntu_techboard(self):
-        return self.person.inTeam(self._celebrities.ubuntu_techboard)
-
-    @property
-    def is_in_vcs_imports(self):
-        return self.person.inTeam(self._celebrities.vcs_imports)
+    def __getattr__(self, name):
+        """Handle all in_* attributes."""
+        prefix = 'in_'
+        if not name.startswith(prefix):
+            raise AttributeError
+        attribute = name[len(prefix):]
+        return self.person.inTeam(getattr(self._celebrities, attribute))
 
     def isOwner(self, obj):
         """See IPersonRoles."""
