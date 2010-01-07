f3fb73085f7072701576ab3e41f099523196ffd9
Jonathan Lange <jml@canonical.com>
2010-01-07 16:14:26 +1100

    Catch a few more not exercised by an empty test run.

--- lib/lp/bugs/subscribers/bug.py
+++ lib/lp/bugs/subscribers/bug.py
@@ -1,4 +1,4 @@
-# Copyright 2009 Canonical Ltd.  This software is licensed under the
+# Copyright 2009, 2010 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
 __metaclass__ = type
@@ -6,19 +6,20 @@ __all__ = ['notify_bug_modified']
 
 
 from canonical.database.sqlbase import block_implicit_flushes
-from canonical.launchpad.interfaces._schema_circular_imports import IPerson
+from lp.registry.interfaces.person import IPerson
+
+
 @block_implicit_flushes
 def notify_bug_modified(bug, event):
     """Handle bug change events.
-    
+
     Subscribe the security contacts for a bug when it
     becomes security-related.
     """
     if (event.object.security_related and
         not event.object_before_modification.security_related):
-        # The bug turned to be security-related, 
-        # subscribe the security contact.
+        # The bug turned to be security-related, subscribe the security
+        # contact.
         for pillar in bug.affected_pillars:
             if pillar.security_contact is not None:
                 bug.subscribe(pillar.security_contact, IPerson(event.user))
-
