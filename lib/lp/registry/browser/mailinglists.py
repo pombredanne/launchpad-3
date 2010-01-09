f9c32df8fb9dd3ef80fa7fff92ea9829576f0e06
Curtis Hovey <curtis.hovey@canonical.com>
2010-01-09 11:48:38 -0500

    Make valid HTML to moderate a mailing message.

--- lib/lp/registry/browser/mailinglists.py
+++ lib/lp/registry/browser/mailinglists.py
@@ -130,7 +130,8 @@ class HeldMessageView(LaunchpadView):
         # The author field is very close to what the details has, except that
         # the view wants to include a link to the person's overview page.
         self.author = '<a href="%s">%s</a>' % (
-            canonical_url(self.details.author), self.details.sender)
+            canonical_url(self.details.author),
+            escape(self.details.sender))
 
     def initialize(self):
         """See `LaunchpadView`."""
@@ -157,14 +158,18 @@ class HeldMessageView(LaunchpadView):
         for lineno, line in enumerate(details.splitlines()):
             if lineno > 20:
                 break
-            if len(line.strip()) == 0:
-                paragraphs.append(u'\n'.join(current_paragraph))
-                paragraphs.append('\n<p>\n')
+            if len(line.strip()) == 0 and len(current_paragraph) > 0:
+                self._append_paragraph(paragraphs, current_paragraph)
                 current_paragraph = []
             else:
                 current_paragraph.append(line)
-        paragraphs.append(u'\n'.join(current_paragraph))
+        self._append_paragraph(paragraphs, current_paragraph)
         self.body_details = u''.join(paragraphs)
+
+    def _append_paragraph(self, paragraphs, current_paragraph):
+        paragraphs.append(u'\n<p>\n')
+        paragraphs.append(u'\n'.join(current_paragraph))
+        paragraphs.append(u'\n</p>\n')
 
     def _remove_leading_blank_lines(self):
         """Strip off any leading blank lines.
