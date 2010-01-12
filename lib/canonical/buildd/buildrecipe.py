7b44487a304cf1f45364af8eba1251baf631536b
Aaron Bentley <aaron@canonical.com>
2010-01-12 17:21:45 +1300

    Redo script to call commands in chroot.

--- lib/canonical/buildd/buildrecipe.py
+++ lib/canonical/buildd/buildrecipe.py
@@ -5,6 +5,8 @@ __metaclass__ = type
 
 
 import os.path
+from subprocess import call
+
 import lp.codehosting
 from bzrlib.plugins import builder
 from bzrlib.plugins.builder import recipe
@@ -12,38 +14,70 @@ from bzrlib.plugins.builder import recipe
 
 class BuildRecipe:
 
-    def __init__(self, recipe_text, author_name, author_email, work_dir,
+    def __init__(self, recipe_text, author_name, author_email, build_id,
                  package_name, suite):
         self.recipe_text = recipe_text
         self.author_name = author_name
         self.author_email = author_email
-        self.work_dir = work_dir
+        self.output_dir = os.path.join(os.path.expanduser('~/build-%s',
+                                                          build_id))
         self.package_name = package_name
         self.suite = suite
         self.tree_path = None
         self.base_branch = None
+        self.chroot_path = os.path.join(self.output_dir, 'chroot-autobuild')
+        self.work_dir_relative = '/home/buildd/work'
+        self.work_dir = os.path.join(self.chroot_path, self.work_dir_relative)
+        self.username = pwd.getpwuid(os.getuid())[0]
+
+    def install(self):
+        return self.chroot(['apt-get', 'install', 'pbuilder', 'bzr-builder'])
 
     def buildTree(self):
-        changed, self.base_branch = builder.get_prepared_branch_from_recipe(
-            self.recipe_text)
-        tree_path = builder.calculate_package_dir(self.base_branch,
-                                                  self.package_name,
-                                                  self.work_dir)
-        self.tree_path = tree_path
-        os.mkdir(tree_path)
-        builder.build_tree(self.base_branch, tree_path)
-        builder.add_changelog_entry(self.base_branch, tree_path,
-                                    distribution=self.suite,
-                                    package=self.package_name,
-                                    author_name=self.author_name,
-                                    author_email=self.author_email)
+        recipe_path = os.path.join(self.work_dir, 'recipe')
+        recipe_path_relative = os.path.join(self.work_dir_relative, 'recipe')
+        with open(recipe_path, 'w') as recipe_file:
+            recipe_file.write(self.recipe_text)
+        self.tree_path_relative = os.path.join(self.work_dir_relative, 'tree')
+        env = {'DEBEMAIL': self.author_email,
+               'DEBFULLNAME': self.author_name}
+        retcode = self.chroot(['su' '-c' 'bzr dailydeb --no-build %s %s' % (
+                              recipe_path_relative, self.tree_path_relative,
+                              self.username)], env=env)
+        if retcode == 0:
+            for source in os.listdir(self.tree_path):
+                if source in ('.', '..'):
+                    continue
+                else:
+                    break
+             self.source_dir_relative = os.path.join(self.tree_path_relative,
+                                                     source)
+        return retcode
 
     def installBuildDeps(self):
-        pass
+        return self.chroot(['sh', '-c', 'cd %s &&'
+                     '/usr/lib/pbuilder/pbuilder-satisfydepends'
+                     % self.source_dir_relative])
+
+    def chroot(self, args, cwd=None, env=None):
+        return call(['/usr/bin/sudo',
+                     '/usr/sbin/chroot', self.chroot_path] + args, cwd=cwd,
+                     env=env)
 
     def buildSourcePackage(self):
-        builder.build_source_package(self.tree_path)
+        return chroot(['su', '-c', 'cd %s && /usr/bin/dpkg-buildpackage -i'
+                      ' -I -us -uc -S' % self.source_dir_relative,
+                      self.username])
 
-    def writeManifest(self):
-        manifest_path = os.path.join(self.work_dir, 'manifest')
-        builder.write_manifest_to_path(manifest_path, self.base_branch)
+if __name__ == '__main__':
+    builder = RecipeBuilder(*sys.argv[1:])
+    if builder.install() != 0:
+        sys.exit(1)
+    if builder.buildTree() != 0:
+        sys.exit(2)
+    if builder.installBuildDeps() != 0:
+        sys.exit(3)
+    if builder.buildSourcePackage() != 0:
+        sys.exit(4)
+    sys.exit(0)
+
