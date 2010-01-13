648fcac03c5b7dcc6ec601c3cc9baf0f93cf1c2e
Paul Hummer <paul@canonical.com>
2010-01-14 12:05:37 +1300

    Removed the old canonical_url hack abentley introduced

--- lib/lp/code/windmill/testing.py
+++ lib/lp/code/windmill/testing.py
@@ -19,18 +19,3 @@ class CodeWindmillLayer(BaseWindmillLayer):
     """Layer for Code Windmill tests."""
 
     base_url = 'http://code.launchpad.dev:8085/'
-
-
-# XXX: rockstar 2009-11-10  bug=480137 - This function really needs to go
-# away.  I'm going to fix this in a followup branch.
-def canonical_url(*args, **kwargs):
-    """Wrapper for canonical_url that ensures test URLs are returned.
-
-    Real canonical URLs require SSL support, but our test clients don't
-    trust the certificate.
-    """
-    url = real_canonical_url(*args, **kwargs)
-    url = url.replace(
-        'http://code.launchpad.dev', windmill.settings['TEST_URL'])
-    assert url.startswith(windmill.settings['TEST_URL'])
-    return url
