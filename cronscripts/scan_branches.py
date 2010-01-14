29b23101ea8a1470f70758a93de0e60d03187dca
Paul Hummer <paul@canonical.com>
2010-01-15 11:40:33 +1300

    Fixed a copyright notice and a docstring.

--- cronscripts/scan_branches.py
+++ cronscripts/scan_branches.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python2.5
 #
-# Copyright 2009 Canonical Ltd.  This software is licensed under the
+# Copyright 2010 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
 """Scan branches for new revisions."""
