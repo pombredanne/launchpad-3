2b3249b6ee007bdcfbdb3f52455b5a653ab73a73
Brian Murray <brian@canonical.com>
2010-01-19 16:48:18 -0600

    export a bug's message_count in the API fixes LP: #509883

--- lib/lp/bugs/interfaces/bug.py
+++ lib/lp/bugs/interfaces/bug.py
@@ -282,9 +282,9 @@ class IBug(ICanBeMentored, IPrivacy, IHasLinkedBranches):
     number_of_duplicates = Int(
         title=_('The number of bugs marked as duplicates of this bug'),
         required=True, readonly=True)
-    message_count = Int(
-        title=_('The number of comments on this bug'),
-        required=True, readonly=True)
+    message_count = exported(
+        Int(title=_('The number of comments on this bug'),
+        required=True, readonly=True))
     users_affected_count = exported(
         Int(title=_('The number of users affected by this bug'),
             required=True, readonly=True))
