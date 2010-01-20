9ef2d3ce620a96789e44d1efb587776f08175546
Brian Murray <brian@canonical.com>
2010-01-20 09:28:33 -0600

    remove the flame icon from affects me too since it is being used by bug heat - LP: #510157

--- lib/lp/bugs/windmill/tests/test_bug_me_too.py
+++ lib/lp/bugs/windmill/tests/test_bug_me_too.py
@@ -15,7 +15,6 @@ AFFECTS_ME_TOO_XPATH = u"//span[@id='affectsmetoo']"
 DYNAMIC_SPAN_XPATH = AFFECTS_ME_TOO_XPATH + u"/span[@class='dynamic']"
 VALUE_LOCATION_XPATH = DYNAMIC_SPAN_XPATH + u"//span[@class='value']"
 EDIT_ICON_XPATH = DYNAMIC_SPAN_XPATH + u"//img[@class='editicon']"
-FLAME_ICON_XPATH = DYNAMIC_SPAN_XPATH + u"//img[contains(@src, 'flame-icon')]"
 
 OVERLAY_XPATH = u"//div[@id='yui-pretty-overlay-modal']"
 
@@ -64,12 +63,6 @@ class TestMeToo(TestCaseWithFactory):
         client.asserts.assertText(
             xpath=VALUE_LOCATION_XPATH,
             validator=u"Does this bug affect you?")
-
-        # A flame icon is available in the page, but not visible owing to
-        # the unseen class.
-        client.asserts.assertElemJS(
-            xpath=FLAME_ICON_XPATH,
-            js="element.getAttribute('class').match(/unseen/) !== null")
 
         # There is an edit icon next to the text which can be clicked to
         # edit the "me too" status. However, we won't click it with
@@ -126,11 +119,6 @@ class TestMeToo(TestCaseWithFactory):
             client.asserts.assertText(
                 xpath=VALUE_LOCATION_XPATH,
                 validator=u"This bug affects you")
-
-        # The flame icon is now visible.
-        client.asserts.assertElemJS(
-            xpath=FLAME_ICON_XPATH,
-            js="element.getAttribute('class').match(/unseen/) === null")
 
 def test_suite():
     return unittest.TestLoader().loadTestsFromName(__name__)
