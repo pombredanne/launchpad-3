c7ca0d1032ca09f116d0b71d4b07c2968936fdbf
Curtis Hovey <curtis.hovey@canonical.com>
2010-01-27 14:06:49 -0500

    Moved the milestone createProductRelease "fix committed" to "fix release" rules to a closeBugsAndBlueprints() because this is too much work to complete in a request.

--- lib/lp/registry/interfaces/milestone.py
+++ lib/lp/registry/interfaces/milestone.py
@@ -163,6 +163,17 @@ class IMilestone(IHasBugs, IStructuralSubscriptionTarget):
         :returns: `IProductRelease` object.
         """
 
+    @call_with(user=REQUEST_USER)
+    @export_write_operation()
+    def closeBugsAndBlueprints(user):
+        """Close completed bugs and blueprints.
+
+        Bugs that are fix committed status are updated to fix released.
+        Blueprints that are in deployment status are updated to implemented
+        status.
+        XXX sinzui 2010-01-27 bug=341687: blueprints not yet implemented.
+        """
+
     @export_write_operation()
     @export_operation_as('delete')
     def destroySelf():
