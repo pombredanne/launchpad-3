1d7f069fc2716c95abe11850fead05a80be70295
Aaron Bentley <aaron@canonical.com>
2010-01-28 11:59:27 -0500

    Set up zope event handling.

--- lib/lp/code/model/branchjob.py
+++ lib/lp/code/model/branchjob.py
@@ -48,7 +48,13 @@ from lp.code.model.branch import Branch
 from lp.code.model.branchmergeproposal import BranchMergeProposal
 from lp.code.model.diff import StaticDiff
 from lp.code.model.revision import RevisionSet
-from lp.codehosting.scanner.bzrsync import BzrSync
+from lp.codehosting.scanner import buglinks, email, mergedetection
+from lp.codehosting.scanner.fixture import (
+    Fixtures, make_zope_event_fixture, run_with_fixture
+)
+from lp.codehosting.scanner.bzrsync import (
+    BzrSync, schedule_diff_updates, schedule_translation_upload
+)
 from lp.codehosting.vfs import (branch_id_to_path, get_multi_server,
     get_scanner_server)
 from lp.services.job.model.job import Job
@@ -264,6 +270,7 @@ class BranchScanJob(BranchJobDerived):
 
     classProvides(IBranchScanJobSource)
     class_job_type = BranchJobType.SCAN_BRANCH
+    server = None
 
     @classmethod
     def create(cls, branch):
@@ -275,17 +282,27 @@ class BranchScanJob(BranchJobDerived):
         """See `IBranchScanJob`."""
         from canonical.launchpad.scripts import log
         bzrsync = BzrSync(self.branch, log)
-        bzrsync.syncBranchAndClose()
+        event_handlers = [
+            email.queue_tip_changed_email_jobs,
+            buglinks.got_new_revision,
+            mergedetection.auto_merge_branches,
+            mergedetection.auto_merge_proposals,
+            schedule_diff_updates,
+            schedule_translation_upload,
+            ]
+        fixture = Fixtures(
+            [self.server, make_zope_event_fixture(*event_handlers)])
+        run_with_fixture(fixture, bzrsync.syncBranchAndClose)
 
-    @staticmethod
+    @classmethod
     @contextlib.contextmanager
-    def contextManager():
+    def contextManager(cls):
         """See `IBranchScanJobSource`."""
         errorlog.globalErrorUtility.configure('branchscanner')
-        server = get_scanner_server()
-        server.setUp()
+        cls.server = get_scanner_server()
+        cls.server.setUp()
         yield
-        server.tearDown()
+        cls.server.tearDown()
 
 
 class BranchUpgradeJob(BranchJobDerived):
