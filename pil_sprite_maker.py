516c089ae3496885b1c6393408661eafdbf5c5dc
Edwin Grubbs <edwin.grubbs@canonical.com>
2010-02-01 09:22:54 -0600

    Updated comment.

--- pil_sprite_maker.py
+++ pil_sprite_maker.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python2.5
 
-# This work is licensed under the Creative Commons Attribution 3.0 United
-# States License. To view a copy of this license, visit
-# http://creativecommons.org/licenses/by/3.0/us/ or send a letter to Creative
-# Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
 # Derived from make_master.py by Oran Looney.
 # http://oranlooney.com/make-css-sprites-python-image-library/
 
