5630145e2598bbbbe5f6de03e1787d24bcae46e4
Abel Deuring <abel.deuring@canonical.com>
2010-02-02 11:57:26 +0100

    Add a column latest_patch_uploaded to table Bug; define triggers to automatically update the column

--- database/schema/comments.sql
+++ database/schema/comments.sql
@@ -154,6 +154,7 @@ COMMENT ON COLUMN Bug.number_of_duplicates IS 'The number of bugs marked as dupl
 COMMENT ON COLUMN Bug.message_count IS 'The number of messages (currently just comments) on this bugbug, maintained by the set_bug_message_count_t trigger.';
 COMMENT ON COLUMN Bug.users_affected_count IS 'The number of users affected by this bug, maintained by the set_bug_users_affected_count_t trigger.';
 COMMENT ON COLUMN Bug.hotness IS 'The relevance of this bug. This value is computed periodically using bug_affects_person and other bug values.';
+COMMENT ON COLUMN Bug.latest_patch_uploaded IS 'The time when the most recent patch has been attached to this bug or NULL if no patches are attached';
 
 -- BugBranch
 COMMENT ON TABLE BugBranch IS 'A branch related to a bug, most likely a branch for fixing the bug.';
