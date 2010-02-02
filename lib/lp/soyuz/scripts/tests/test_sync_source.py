a4306bf49bc44e36b7123ce800b5b78d429cf39c
Jelmer Vernooij <jelmer@canonical.com>
2010-02-02 16:53:35 +0100

    Fix module docstring for lp.soyuz.scripts.tests.test_sync_source.

--- lib/lp/soyuz/scripts/tests/test_sync_source.py
+++ lib/lp/soyuz/scripts/tests/test_sync_source.py
@@ -1,7 +1,7 @@
 # Copyright 2009 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
-"""ChrootManager facilities tests."""
+"""SyncSource facilities tests."""
 
 __metaclass__ = type
 
