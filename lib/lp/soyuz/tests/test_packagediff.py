b61a0d4cf4478117923317e383b89a5d50cad374
Muharem Hrnjadovic <muharem@ubuntu.com>
2010-02-04 17:41:01 +0100

    refactored second test

--- lib/lp/soyuz/tests/test_packagediff.py
+++ lib/lp/soyuz/tests/test_packagediff.py
@@ -13,80 +13,20 @@ from zope.security.proxy import removeSecurityProxy
 
 from canonical.config import config
 from canonical.database.sqlbase import sqlvalues
-from canonical.launchpad.ftests import import_public_test_keys
-from lp.registry.interfaces.distribution import IDistributionSet
-from lp.soyuz.interfaces.packagediff import IPackageDiffSet, PackageDiffStatus
-from canonical.launchpad.testing.fakepackager import FakePackager
-from canonical.launchpad.database import LibraryFileAlias
 from canonical.launchpad.webapp.interfaces import (
         IStoreSelector, MAIN_STORE, DEFAULT_FLAVOR)
 from canonical.testing import LaunchpadZopelessLayer
+from lp.soyuz.interfaces.packagediff import PackageDiffStatus
+from lp.soyuz.tests.soyuz import TestPackageDiffsBase
 
 
-class TestPackageDiffs(unittest.TestCase):
+class TestPackageDiffs(TestPackageDiffsBase):
     """Test package diffs."""
     layer = LaunchpadZopelessLayer
     dbuser = config.uploader.dbuser
 
     def setUp(self):
-        """Setup proper DB connection and contents for tests
-
-        Connect to the DB as the 'uploader' user.
-
-        Store the `FakePackager` object used in the test uploads as `packager`
-        so the tests can reuse it if necessary.
-        """
-        self.layer.alterConnection(dbuser='launchpad')
-
-        fake_chroot = LibraryFileAlias.get(1)
-        ubuntu = getUtility(IDistributionSet).getByName('ubuntu')
-        warty = ubuntu.getSeries('warty')
-        warty['i386'].addOrUpdateChroot(fake_chroot)
-
-        self.layer.txn.commit()
-
-        self.layer.alterConnection(dbuser=self.dbuser)
-        self.packager = self.uploadTestPackages()
-        self.layer.txn.commit()
-
-    def uploadTestPackages(self):
-        """Upload packages for testing `PackageDiff` generation.
-
-        Upload zeca_1.0-1 and zeca_1.0-2 sources, so a `PackageDiff` between
-        them is created.
-
-        Assert there is not pending `PackageDiff` in the DB before uploading
-        the package and also assert that there is one after the uploads.
-
-        :return: the FakePackager object used to generate and upload the test,
-            packages, so the tests can upload subsequent version if necessary.
-        """
-        # No pending PackageDiff available in sampledata.
-        self.assertEqual(self.getPendingDiffs().count(), 0)
-
-        import_public_test_keys()
-        # Use FakePackager to upload a base package to ubuntu.
-        packager = FakePackager(
-            'zeca', '1.0', 'foo.bar@canonical.com-passwordless.sec')
-        packager.buildUpstream()
-        packager.buildSource()
-        packager.uploadSourceVersion('1.0-1', suite="warty-updates")
-
-        # Upload a new version of the source, so a PackageDiff can
-        # be created.
-        packager.buildVersion('1.0-2', changelog_text="cookies")
-        packager.buildSource(include_orig=False)
-        packager.uploadSourceVersion('1.0-2', suite="warty-updates")
-
-        # Check if there is exactly one pending PackageDiff record and
-        # It's the one we have just created.
-        self.assertEqual(self.getPendingDiffs().count(), 1)
-
-        return packager
-
-    def getPendingDiffs(self):
-        """Pending `PackageDiff` available."""
-        return getUtility(IPackageDiffSet).getPendingDiffs()
+        super(TestPackageDiffs, self).setUp()
 
     def test_packagediff_working(self):
         # Test the case where none of the files required for the diff are
