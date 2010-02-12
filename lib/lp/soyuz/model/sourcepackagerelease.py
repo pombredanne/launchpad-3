321ad08e34f9b32c95f902c0c0965b8d1af17203
Jelmer Vernooij <jelmer@canonical.com>
2010-02-12 18:54:01 +0100

    Remove unused import.

--- lib/lp/soyuz/model/sourcepackagerelease.py
+++ lib/lp/soyuz/model/sourcepackagerelease.py
@@ -38,7 +38,7 @@ from lp.translations.interfaces.translationimportqueue import (
 from canonical.launchpad.webapp.interfaces import NotFoundError
 from lp.archiveuploader.utils import determine_source_file_type
 from lp.soyuz.interfaces.archive import (
-    ArchivePurpose, IArchiveSet, MAIN_ARCHIVE_PURPOSES)
+    IArchiveSet, MAIN_ARCHIVE_PURPOSES)
 from lp.soyuz.interfaces.build import BuildStatus
 from lp.soyuz.interfaces.packagediff import (
     PackageDiffAlreadyRequested, PackageDiffStatus)
