03fea406ed7492eb3f32c94cc59f6193a7cfca9a
William Grant <me@williamgrant.id.au>
2010-02-20 21:08:03 +1100

    Add and test parse-ppa-apache-access-logs.py.

--- lib/lp/soyuz/scripts/tests/test_ppa_apache_log_parser.py
+++ lib/lp/soyuz/scripts/tests/test_ppa_apache_log_parser.py
@@ -1,10 +1,22 @@
 # Copyright 2010 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
+from datetime import date
+import subprocess
 import unittest
 
+from zope.component import getUtility
+
+from canonical.launchpad.webapp.interfaces import (
+    IStoreSelector, MAIN_STORE, DEFAULT_FLAVOR)
+from canonical.testing.layers import LaunchpadZopelessLayer
+from lp.registry.interfaces.person import IPersonSet
+from lp.services.worlddata.interfaces.country import ICountrySet
+from lp.soyuz.model.binarypackagerelease import (
+    BinaryPackageReleaseDownloadCount)
 from lp.soyuz.scripts.ppa_apache_log_parser import get_ppa_file_key
-from lp.testing import TestCase
+from lp.soyuz.tests.test_publishing import SoyuzTestPublisher
+from lp.testing import TestCase, TestCaseWithFactory
 
 
 class TestPathParsing(TestCase):
@@ -33,6 +45,89 @@ class TestPathParsing(TestCase):
         # it should be ignored.
         self.assertIs(None, get_ppa_file_key(
             '/cprov/ppa/ubuntu/pool/main/f/foo/foo_1.2.3-4.dsc'))
+
+
+class TestScriptRunning(TestCaseWithFactory):
+    """Run parse-ppa-apache-access-logs.py and test its outcome."""
+
+    layer = LaunchpadZopelessLayer
+
+    def setUp(self):
+        super(TestScriptRunning, self).setUp()
+        self.publisher = SoyuzTestPublisher()
+        self.publisher.prepareBreezyAutotest()
+
+        self.store = getUtility(IStoreSelector).get(
+            MAIN_STORE, DEFAULT_FLAVOR)
+
+        self.archive = getUtility(IPersonSet).getByName('cprov').archive
+        self.archive.require_virtualized = False
+
+        self.foo_i386, self.foo_hppa = self.publisher.getPubBinaries(
+                archive=self.archive, architecturespecific=True)
+        self.bar_i386, self.bar_hppa = self.publisher.getPubBinaries(
+                binaryname='bar-bin', archive=self.archive,
+                architecturespecific=False)
+
+        # Commit so the script can see our changes.
+        import transaction
+        transaction.commit()
+
+    def test_script_run(self):
+        # Before we run the script, there are no binary package
+        # downloads in the database.
+        # After the script's run, we will check that the results in the
+        # database match the sample log files we use for this test:
+        # lib/lp/soyuz/scripts/tests/ppa-apache-log-files
+        self.assertEqual(
+            0, self.store.find(BinaryPackageReleaseDownloadCount).count())
+
+        process = subprocess.Popen(
+            'cronscripts/parse-ppa-apache-access-logs.py', shell=True,
+            stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
+        (out, err) = process.communicate()
+        self.assertEqual(
+            process.returncode, 0, "stdout:%s, stderr:%s" % (out, err))
+
+        # Must commit because the changes were done in another transaction.
+        import transaction
+        transaction.commit()
+        results = self.store.find(BinaryPackageReleaseDownloadCount)
+
+        australia = getUtility(ICountrySet)['AU']
+        austria = getUtility(ICountrySet)['AT']
+
+        self.assertEqual(
+            [(self.foo_hppa.binarypackagerelease,
+              self.archive,
+              date(2008, 6, 13),
+              australia,
+              1),
+             (self.foo_i386.binarypackagerelease,
+              self.archive,
+              date(2008, 6, 13),
+              australia,
+              1),
+             (self.foo_i386.binarypackagerelease,
+              self.archive,
+              date(2008, 6, 13),
+              austria,
+              1),
+             (self.bar_i386.binarypackagerelease,
+              self.archive,
+              date(2008, 6, 14),
+              None,
+              1),
+             (self.bar_i386.binarypackagerelease,
+              self.archive,
+              date(2008, 6, 14),
+              austria,
+              1)],
+            sorted(
+                [(result.binary_package_release, result.archive, result.day,
+                  result.country, result.count) for result in results],
+                 key=lambda r: (r[0].id, r[3])))
 
 
 def test_suite():
