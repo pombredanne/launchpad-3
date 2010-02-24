a881c169e08a55d5ee560dfb9dfe0b29c879a20d
Tim Penhey <tim.penhey@canonical.com>
2010-02-24 21:52:16 +1300

    Official sql patch number.

--- database/schema/patch-2207-33-0.sql
+++ database/schema/patch-2207-33-0.sql
@@ -6,4 +6,4 @@ SET client_min_messages=ERROR;
 ALTER TABLE BranchMergeProposal
     ADD COLUMN description TEXT;
 
-INSERT INTO LaunchpadDatabaseRevision VALUES (2207, 99, 0);
+INSERT INTO LaunchpadDatabaseRevision VALUES (2207, 33, 0);
