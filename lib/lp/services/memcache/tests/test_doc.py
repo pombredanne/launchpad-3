b13cb2528d437bd1ffd5c264e52f527b1168d963
Stuart Bishop <stuart.bishop@canonical.com>
2010-02-26 20:14:57 +0700

    Move setup to .py to make the doctest readable

--- lib/lp/services/memcache/tests/test_doc.py
+++ lib/lp/services/memcache/tests/test_doc.py
@@ -6,13 +6,70 @@
 __metaclass__ = type
 
 import os.path
+from textwrap import dedent
 import unittest
 
+from zope.component import getUtility
+import zope.pagetemplate.engine
+from zope.pagetemplate.pagetemplate import PageTemplate
+from zope.publisher.browser import TestRequest
+
+from canonical.launchpad.testing.systemdocs import (
+    LayeredDocFileSuite, setUp, tearDown)
 from canonical.testing.layers import LaunchpadFunctionalLayer
+from lp.services.memcache.interfaces import IMemcacheClient
 from lp.services.testing import build_test_suite
 from lp.testing import TestCase
 
+
 here = os.path.dirname(os.path.realpath(__file__))
 
+
+class TestPageTemplate(PageTemplate):
+    """A cutdown PageTemplate implementation suitable for our tests."""
+
+    _num_instances = 0
+
+    def __init__(self, source):
+        super(TestPageTemplate, self).__init__()
+        TestPageTemplate._num_instances += 1
+        self._my_instance_num = TestPageTemplate._num_instances
+        self.pt_edit(source, 'text/html')
+
+    def pt_source_file(self):
+        return 'fake/test_%d.pt' % self._my_instance_num
+
+    def pt_getEngine(self):
+        # The <tales:expressiontype> ZCML only registers with this
+        # engine, not the default.
+        return zope.pagetemplate.engine.Engine
+
+    def pt_getContext(self, args=(), options={}):
+        # Build a minimal context. The cache: expression requires
+        # a request.
+        context = {'request': TestRequest()}
+        context.update(options)
+        return context
+
+
+def clear_memcache():
+    """Clear everything from memcached. Only do this in tests!"""
+    getUtility(IMemcacheClient).flush_all()
+
+
+def memcacheSetUp(test):
+    test.globs['TestPageTemplate'] = TestPageTemplate
+    test.globs['dedent'] = dedent
+    test.globs['clear_memcache'] = clear_memcache
+
+
+special = {
+    'tales-cache.txt': LayeredDocFileSuite(
+        '../doc/tales-cache.txt',
+        setUp=memcacheSetUp, tearDown=tearDown,
+        layer=LaunchpadFunctionalLayer),
+    }
+
+
 def test_suite():
-    return build_test_suite(here, {}, layer=LaunchpadFunctionalLayer)
+    return build_test_suite(here, special, layer=LaunchpadFunctionalLayer)
