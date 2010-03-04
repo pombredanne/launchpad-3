c4f8634f63e0ba37705561018a344e8a933badb6
Danilo Šegan <danilo@canonical.com>
2010-03-04 18:32:26 +0100

    Check plural form for zero as well.

--- lib/lp/translations/utilities/tests/test_pluralforms.py
+++ lib/lp/translations/utilities/tests/test_pluralforms.py
@@ -13,15 +13,15 @@ class PluralFormsTest(unittest.TestCase):
     def test_make_friendly_plural_form(self):
         single_form = make_friendly_plural_forms('0', 1)
         self.assertEqual(single_form,
-                         [{'examples': [1, 2, 3, 4, 5, 6], 'form': 0}])
+                         [{'examples': [0, 1, 2, 3, 4, 5], 'form': 0}])
 
         two_forms = make_friendly_plural_forms('n!=1', 2)
         self.assertEqual(two_forms,
                          [{'examples': [1], 'form': 0},
-                          {'examples': [2, 3, 4, 5, 6, 7], 'form': 1}])
+                          {'examples': [0, 2, 3, 4, 5, 6], 'form': 1}])
 
     def test_make_friendly_plural_form_failures(self):
-        # To the degree of is not accepted.
+        # 'To the degree of' is not accepted.
         self.assertRaises(BadPluralExpression,
                           make_friendly_plural_forms, 'n**2', 1)
 
@@ -33,11 +33,23 @@ class PluralFormsTest(unittest.TestCase):
         self.assertRaises(BadPluralExpression,
                           make_friendly_plural_forms, '(a=1)', 1)
 
+        # If number of actual forms doesn't match requested number.
+        self.assertRaises(BadPluralExpression,
+                          make_friendly_plural_forms, 'n!=1', 3)
+
+        # Dividing by zero doesn't work.
+        self.assertRaises(BadPluralExpression,
+                          make_friendly_plural_forms, '(n/0)', 1)
+
     def test_make_friendly_plural_form_zero_handling(self):
         zero_forms = make_friendly_plural_forms('n!=0', 2)
         self.assertEqual(zero_forms,
                          [{'examples': [0], 'form': 0},
                           {'examples': [1, 2, 3, 4, 5, 6], 'form': 1}])
+
+        # Since 'n' can be zero as well, dividing by it won't work.
+        self.assertRaises(BadPluralExpression,
+                          make_friendly_plural_forms, '(1/n)', 1)
 
 
 def test_suite():
