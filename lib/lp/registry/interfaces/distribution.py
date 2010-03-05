853171d0cc46abaea0b7d9262020c4fd4e3655b6
Curtis Hovey <curtis.hovey@canonical.com>
2010-03-04 23:12:27 -0500

    Hushed lint.

--- lib/lp/registry/interfaces/distribution.py
+++ lib/lp/registry/interfaces/distribution.py
@@ -24,7 +24,6 @@ from zope.schema import Bool, Choice, Datetime, List, Object, Text, TextLine
 from zope.interface import Attribute, Interface
 
 from lazr.restful.fields import CollectionField, Reference
-from lazr.restful.interface import copy_field
 from lazr.restful.declarations import (
    collection_default_content, export_as_webservice_collection,
    export_as_webservice_entry, export_operation_as,
