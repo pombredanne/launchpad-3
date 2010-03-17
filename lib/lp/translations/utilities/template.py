a6b9771e51d8e96583098f27c5b055d4325e1476
Henning Eggers <henning@canonical.com>
2010-03-17 13:38:16 +0100

    Added module and test for template name utility functions.

---
+++ lib/lp/translations/utilities/template.py
@@ -0,0 +1,20 @@
+# Copyright 2010 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Functions to help with translation templates."""
+
+__all__ = [
+    'make_domain',
+    'make_name',
+    'make_name_from_path',
+    ]
+
+
+def make_domain(path):
+    pass
+
+def make_name(domain):
+    pass
+
+def make_name_from_path(path):
+    pass
