4b6491f6738620c46cb76da9b4dd3da9e8901ba5
Julian Edwards <julian.edwards@canonical.com>
2010-03-18 17:14:50 +0000

    Import fascist fail and fix set_attributes for new column

--- lib/lp/soyuz/interfaces/archive.py
+++ lib/lp/soyuz/interfaces/archive.py
@@ -13,6 +13,7 @@ __all__ = [
     'ArchiveDependencyError',
     'ArchiveNotPrivate',
     'ArchivePurpose',
+    'ArchiveStatus',
     'CannotCopy',
     'CannotSwitchPrivacy',
     'ComponentNotFound',
