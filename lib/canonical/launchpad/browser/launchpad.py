2dbda0fc6b79035023f28b5e2111a9c55ebc1503
Guilherme Salgado <salgado@canonical.com>
2010-03-19 09:24:20 -0300

    remove more stuff that's no longer used

--- lib/canonical/launchpad/browser/launchpad.py
+++ lib/canonical/launchpad/browser/launchpad.py
@@ -7,7 +7,6 @@ __metaclass__ = type
 __all__ = [
     'AppFrontPageSearchView',
     'ApplicationButtons',
-    'BrowserWindowDimensions',
     'DoesNotExistView',
     'Hierarchy',
     'IcingContribFolder',
@@ -996,13 +995,6 @@ class AppFrontPageSearchView(LaunchpadFormView):
     def scope_error(self):
         """The error message for the scope widget."""
         return self.getFieldError('scope')
-
-
-class BrowserWindowDimensions(LaunchpadView):
-    """Allow capture of browser window dimensions."""
-
-    def render(self):
-        return u'Thanks.'
 
 
 class LaunchpadGraphics(LaunchpadView):
