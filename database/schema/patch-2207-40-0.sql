524b72fb89acbd38968f3d2b46dbcbfbb2a61672
Julian Edwards <julian.edwards@canonical.com>
2010-03-19 10:20:18 +0000

    Review of database patch from stub

--- database/schema/patch-2207-40-0.sql
+++ database/schema/patch-2207-40-0.sql
@@ -5,13 +5,15 @@ SET client_min_messages=ERROR;
 
 ALTER TABLE Archive
    ADD COLUMN status integer DEFAULT 0 NOT NULL;
+CREATE INDEX archive__status__idx ON Archive(status);
+
 
 -- possible values for status are:
 -- 0: ACTIVE
--- 1: DELETED
+-- 1: DELETING
 
 -- In the future, we'll add
 -- DISABLED (instead of the disabled flag)
 -- FROZEN (instead of the publish flag)
 
-INSERT INTO LaunchpadDatabaseRevision VALUES (2207, 99, 0);
+INSERT INTO LaunchpadDatabaseRevision VALUES (2207, 40, 0);
