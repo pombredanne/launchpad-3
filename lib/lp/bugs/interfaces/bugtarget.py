ed69eb4c8a4e201677c2b0eb9acd134a86474968
Deryck Hodge <deryck.hodge@canonical.com>
2010-03-19 10:29:32 -0500

    Clean lint.

--- lib/lp/bugs/interfaces/bugtarget.py
+++ lib/lp/bugs/interfaces/bugtarget.py
@@ -58,7 +58,7 @@ class IHasBugs(Interface):
     all_bugtasks = Attribute(
         "A list of all BugTasks ever reported for this target.")
     has_bugtasks = Attribute(
-        "True if at least one BugTask has ever been reported for this target.")
+        "True if a BugTask has ever been reported for this target.")
 
     @call_with(search_params=None, user=REQUEST_USER)
     @operation_parameters(
