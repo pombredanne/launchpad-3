df6425046675b5d9c41f496d16236aa7a792e250
Henning Eggers <henning@canonical.com>
2010-03-22 16:44:02 -0300

    Really added the patch now.

---
+++ database/schema/patch-2207-99-0.sql
@@ -0,0 +1,8 @@
+-- Copyright 2010 Canonical Ltd.  This software is licensed under the
+-- GNU Affero General Public License version 3 (see the file LICENSE).
+
+SET client_min_messages=ERROR;
+
+ALTER TABLE TranslationImportQueueEntry RENAME COLUMN is_published TO from_upstream;
+
+INSERT INTO LaunchpadDatabaseRevision VALUES (2207, 99, 0);
