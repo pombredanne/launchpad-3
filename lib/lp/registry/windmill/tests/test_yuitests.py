bbe30184c580cb8c03f58e7026f00604b693e512
Curtis Hovey <curtis.hovey@canonical.com>
2010-03-22 18:35:05 -0400

    TestRegistryYUIUnitTests now inspects the YUI unittest results.

--- lib/lp/registry/windmill/tests/test_yuitests.py
+++ lib/lp/registry/windmill/tests/test_yuitests.py
@@ -6,6 +6,7 @@
 __metaclass__ = type
 __all__ = []
 
+import re
 import unittest
 
 from canonical.config import config
@@ -19,15 +20,23 @@ class TestRegistryYUIUnitTests(WindmillTestCase):
 
     layer = RegistryWindmillLayer
     suite_name = 'RegistryYUIUnitTests'
-    view_name = u'/+yui-unittest'
-    test_path = view_name + u'/canonical/launchpad/javascript'
+    log_pattern = re.compile(r'<pre id="python-log">\|\|(.*)</pre>')
+    view_name = u'/+yui-unittest/'
+    test_dir = u'canonical/launchpad/javascript'
+    test_path = test_dir + u'/registry/tests/milestone_table.html'
 
     def test_YUITestFileView(self):
         client = self.client
-        client.open(
-            url=self.test_path + u'/registry/tests/milestone_table.html')
+        client.open(url=self.view_name + self.test_path)
         client.waits.forPageLoad(timeout=constants.PAGE_LOAD)
         client.waits.forElement(id='complete')
+        response = client.commands.getPageText()
+        match = self.log_pattern.search(response['result'])
+        lines = match.group(1).split('||')
+        for line in lines:
+            result, test_name, unit_test_name = line.split('::')
+            self.assertTrue('pass' == result,
+                'Failue in %s.%s' % (unit_test_name, test_name))
 
 
 def test_suite():
