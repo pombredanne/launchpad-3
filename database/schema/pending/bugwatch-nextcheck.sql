3f2885da2e2f3f29df79e158fbf3d70f839e36cb
Graham Binns <graham@canonical.com>
2010-03-22 15:55:20 +0000

    Added a db patch.

---
+++ database/schema/pending/bugwatch-nextcheck.sql
@@ -0,0 +1,7 @@
+SET client_min_messages=ERROR;
+
+ALTER TABLE BugWatch ADD COLUMN next_check timestamp without time zone DEFAULT timezone('UTC'::text, now());
+
+CREATE INDEX bugwatch__next_check__idx ON BugWatch(next_check);
+
+INSERT INTO LaunchpadDatabaseRevision VALUES (2207, 99, 0);
