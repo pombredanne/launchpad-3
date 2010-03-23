b8857bf90e7a877f48a1ca4f8a438322efb0ff86
Henning Eggers <henning@canonical.com>
2010-03-23 11:19:35 -0300

    Fixed copyright notices.

--- database/schema/comments.sql
+++ database/schema/comments.sql
@@ -2,7 +2,7 @@
   Add Comments to Launchpad database. Please keep these alphabetical by
   table.
 
-     Copyright 2009 Canonical Ltd.  This software is licensed under the
+     Copyright 2009-2010 Canonical Ltd.  This software is licensed under the
      GNU Affero General Public License version 3 (see the file LICENSE).
 */
 
