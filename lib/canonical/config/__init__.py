47da3681c81fe4685d36cbf00632b6e4551bf647
Guilherme Salgado <salgado@canonical.com>
2010-03-24 14:37:26 -0300

    Remove the auth_*  attributes from DatabaseConfig and fix a bunch of tests which used to rely on auth stores.

--- lib/canonical/config/__init__.py
+++ lib/canonical/config/__init__.py
@@ -374,13 +374,13 @@ class DatabaseConfig:
     _db_config_attrs = frozenset([
         'dbuser', 'auth_dbuser',
         'rw_main_master', 'rw_main_slave',
-        'ro_main_master', 'ro_main_slave', 'auth_master', 'auth_slave',
+        'ro_main_master', 'ro_main_slave',
         'db_statement_timeout', 'db_statement_timeout_precision',
         'isolation_level', 'randomise_select_results',
         'soft_request_timeout', 'storm_cache', 'storm_cache_size'])
     _db_config_required_attrs = frozenset([
         'dbuser', 'rw_main_master', 'rw_main_slave', 'ro_main_master',
-        'ro_main_slave', 'auth_master', 'auth_slave'])
+        'ro_main_slave'])
 
     @property
     def main_master(self):
