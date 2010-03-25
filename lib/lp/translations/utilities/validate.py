6551b1b149157840c8ae15e4d07f4d4a3a5b8952
Henning Eggers <henning@canonical.com>
2010-03-25 16:03:31 -0300

    Even added the new module...

---
+++ lib/lp/translations/utilities/validate.py
@@ -0,0 +1,30 @@
+# Copyright 2009 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+__metaclass__ = type
+
+import gettextpo
+
+
+def validate_translation(original, translation, flags):
+    """Check with gettext if a translation is correct or not.
+
+    If the translation has a problem, raise gettextpo.error.
+    """
+    msg = gettextpo.PoMessage()
+    msg.set_msgid(original[0])
+
+    if len(original) > 1:
+        # It has plural forms.
+        msg.set_msgid_plural(original[1])
+        for form in range(len(translation)):
+            msg.set_msgstr_plural(form, translation[form])
+    elif len(translation):
+        msg.set_msgstr(translation[0])
+
+    for flag in flags:
+        msg.set_format(flag, True)
+
+    # Check the msg.
+    msg.check_format()
+
