924645927322349c2bbcde04439e1f2df13c46c0
Henning Eggers <henning@canonical.com>
2010-03-25 18:30:18 -0300

    Added validate_translations.

--- lib/lp/translations/utilities/validate.py
+++ lib/lp/translations/utilities/validate.py
@@ -28,3 +28,35 @@ def validate_translation(original, translation, flags):
     # Check the msg.
     msg.check_format()
 
+
+def validate_translations(msgids, translations, flags, ignore_errors):
+    """Validate all the `translations`.
+
+    :param msgids: A list of one or two msgids, depending on whether the
+        message has a plural.
+    :param translation: A list of translations, one for each plural form in
+        the language.
+    :param flags: This message's flags as a list of strings.
+    :param ignore_errors: Set to true to suppress exceptions from propagating.
+    :return: True if the translations could be validated.
+    """
+    validated = True
+    # Validate the translation we got from the translation form
+    # to know if gettext is unhappy with the input.
+    try:
+        validate_translation(msgids, translations, flags)
+    except gettextpo.error:
+        if ignore_errors:
+            # The translations are stored anyway, although they have not been
+            # validated.status
+            validated = False
+        else:
+            # Check to know if there is any translation.
+            if any(translations.values()):
+                # Partial translations cannot be stored, the
+                # exception is raised again and handled outside
+                # this method.
+                raise
+
+    return validated
+
