cb42dd3f6884e516e2a76d199fdf9194e0ce297a
Henning Eggers <henning@canonical.com>
2010-03-26 11:17:58 -0300

    Added tests and a new exception  for validation.

--- lib/lp/translations/utilities/tests/test_validate.py
+++ lib/lp/translations/utilities/tests/test_validate.py
@@ -5,7 +5,8 @@ __metaclass__ = type
 
 import unittest
 
-from lp.translations.utilities.validate import validate_translations
+from lp.translations.utilities.validate import (
+    GettextValidationError, validate_translations)
 
 
 class TestTranslationValidation(unittest.TestCase):
@@ -15,9 +16,59 @@ class TestTranslationValidation(unittest.TestCase):
         # Correct c-format translations will be validated.
         english = ["English %s number %d"]
         flags = ["c-format"]
-        translations = ["Translation %s number %d"]
+        translations = {0: "Translation %s number %d"}
         self.assertTrue(
             validate_translations(english, translations, flags))
+
+    def test_validate_translations_c_format_fail(self):
+        # Not matching format specifiers will not be validated.
+        english = ["English %s number %d"]
+        flags = ["c-format"]
+        translations = {0: "Translation %d"}
+        self.assertFalse(
+            validate_translations(english, translations, flags, True))
+
+    def test_validate_translations_c_format_raises(self):
+        # Failing is usually propagated by an exception.
+        english = ["English %s number %d"]
+        flags = ["c-format"]
+        translations = {0: "Translation %d"}
+        self.assertRaises(
+            GettextValidationError,
+            validate_translations, english, translations, flags)
+
+    def test_validate_translations_c_format_plural(self):
+        # Correct c-format translations will be validated on plurals.
+        english = ["English %s number %d", "English plural %s number %d"]
+        flags = ["c-format"]
+        translations = {
+            0: "Translation singular %s number %d",
+            1: "Translation plural %s number %d",
+            }
+        self.assertTrue(
+            validate_translations(english, translations, flags))
+
+    def test_validate_translations_c_format_plural_no_singular_format(self):
+        # As a special case, the singular does not need format specifiers.
+        english = ["English %s number %d", "English plural %s number %d"]
+        flags = ["c-format"]
+        translations = {
+            0: "Translation singular",
+            1: "Translation plural %s number %d",
+            }
+        self.assertTrue(
+            validate_translations(english, translations, flags))
+
+    def test_validate_translations_c_format_plural_fail(self):
+        # Not matching format specifiers will not be validated.
+        english = ["English %s number %d", "English plural %s number %d"]
+        flags = ["c-format"]
+        translations = {
+            0: "Translation singular %d",
+            1: "Translation plural %s",
+            }
+        self.assertFalse(
+            validate_translations(english, translations, flags, True))
 
 
 def test_suite():
--- lib/lp/translations/utilities/validate.py
+++ lib/lp/translations/utilities/validate.py
@@ -6,6 +6,15 @@ __metaclass__ = type
 import gettextpo
 
 
+class GettextValidationError(ValueError):
+    """Gettext validation failed."""
+
+    def __init__(self, message_or_exception):
+        """Extract message from gettextpo.error if if requested."""
+        super(GettextValidationError, self).__init__(
+            unicode(message_or_exception)) 
+
+
 def validate_translation(original, translation, flags):
     """Check with gettext if a translation is correct or not.
 
@@ -34,8 +43,8 @@ def validate_translations(msgids, translations, flags, ignore_errors=False):
 
     :param msgids: A list of one or two msgids, depending on whether the
         message has a plural.
-    :param translation: A list of translations, one for each plural form in
-        the language.
+    :param translation: A dictionary of translations, indexed with the plural
+        form number.
     :param flags: This message's flags as a list of strings.
     :param ignore_errors: Set to true to suppress exceptions from propagating.
     :return: True if the translations could be validated.
@@ -45,7 +54,7 @@ def validate_translations(msgids, translations, flags, ignore_errors=False):
     # to know if gettext is unhappy with the input.
     try:
         validate_translation(msgids, translations, flags)
-    except gettextpo.error:
+    except gettextpo.error, e:
         if ignore_errors:
             # The translations are stored anyway, although they have not been
             # validated.status
@@ -56,7 +65,7 @@ def validate_translations(msgids, translations, flags, ignore_errors=False):
                 # Partial translations cannot be stored, the
                 # exception is raised again and handled outside
                 # this method.
-                raise
+                raise GettextValidationError(e)
 
     return validated
 
