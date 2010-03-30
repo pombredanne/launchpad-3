3d1b7754be9e8bf4069bdc829aca3d87ba7f92c5
Curtis Hovey <curtis.hovey@canonical.com>
2010-03-30 17:49:22 -0400

    Moved YUIUnitTestCase to lp.testing where it can be used by other apps as needed.

--- lib/lp/registry/windmill/tests/test_yuitests.py
+++ lib/lp/registry/windmill/tests/test_yuitests.py
@@ -6,73 +6,16 @@
 __metaclass__ = type
 __all__ = []
 
-import os
-import unittest
-
-from canonical.config import config
-from canonical.launchpad.windmill.testing import constants
-from canonical.launchpad.testing.pages import find_tags_by_class
-
+from lp.testing import build_yui_unittest_suite, YUIUnitTestCase
 from lp.registry.windmill.testing import RegistryWindmillLayer
-from lp.testing import WindmillTestCase
 
 
-class YUIUnitTestCase(WindmillTestCase):
+class RegistryYUIUnitTestCase(YUIUnitTestCase):
 
     layer = RegistryWindmillLayer
     suite_name = 'RegistryYUIUnitTests'
 
-    _yui_results = None
-    _view_name = u'http://launchpad.dev:8085/+yui-unittest/'
-
-    def initialize(self, test_path):
-        self.test_path = test_path
-        self.yui_runner_url = self._view_name + test_path
-
-    def setUp(self):
-        super(YUIUnitTestCase, self).setUp()
-        if self._yui_results is None:
-            client = self.client
-            client.open(url=self.yui_runner_url)
-            client.waits.forPageLoad(timeout=constants.PAGE_LOAD)
-            client.waits.forElement(id='complete')
-            response = client.commands.getPageText()
-            self._yui_results = {}
-            entries = find_tags_by_class(
-                response['result'], 'yui-console-entry-TestRunner')
-            for entry in entries:
-                category = entry.find(
-                    attrs={'class': 'yui-console-entry-cat'})
-                if category is None:
-                    continue
-                result = category.string
-                if result not in ('pass', 'fail'):
-                    continue
-                message = entry.pre.string
-                test_name, ignore = message.split(':', 1)
-                self._yui_results[test_name] = dict(
-                    result=result, message=message)
-
-    def runTest(self):
-        if self._yui_results is None or len(self._yui_results) == 0:
-            self.fail("Test harness or js failed.")
-        for test_name in self._yui_results:
-            result = self._yui_results[test_name]
-            self.assertTrue('pass' == result['result'],
-                    'Failure in %s.%s: %s' % (
-                        self.test_path, test_name, result['message']))
-
 
 def test_suite():
-    suite = unittest.TestSuite()
-    registry_testing_path = 'canonical/launchpad/javascript/registry/tests'
-    testing_path = os.path.join(config.root, 'lib', registry_testing_path)
-    unit_test_names = [
-        file_name for file_name in os.listdir(testing_path)
-        if file_name.startswith('test_') and file_name.endswith('.html')]
-    for unit_test_name in unit_test_names:
-        test_path = os.path.join(registry_testing_path, unit_test_name)
-        test_case = YUIUnitTestCase()
-        test_case.initialize(test_path)
-        suite.addTest(test_case)
-    return suite
+    app_testing_path = 'canonical/launchpad/javascript/registry/tests'
+    return build_yui_unittest_suite(app_testing_path, RegistryYUIUnitTestCase)
