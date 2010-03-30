defcfa19ca7c5006db424453019682493ad2d8d6
William Grant <me@williamgrant.id.au>
2010-03-30 19:50:10 +1100

    Add XXX and bug reference.

--- lib/canonical/__init__.py
+++ lib/canonical/__init__.py
@@ -15,6 +15,7 @@ filter_pattern = '.*(Zope 3.6|provide.*global site manager).*'
 warnings.filterwarnings(
     'ignore', filter_pattern, category=DeprecationWarning)
 
+# XXX wgrant 2010-03-30 bug=551510:
 # Also filter apt_pkg warnings, since Lucid's python-apt has a new API.
 warnings.filterwarnings(
     'ignore', '.*apt_pkg.*', category=DeprecationWarning)
