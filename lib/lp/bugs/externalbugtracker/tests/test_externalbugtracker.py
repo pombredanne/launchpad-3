34e50c7353a7f88fe7e8ac3ba8866abd756ba774
Gavin Panella <gavin.panella@canonical.com>
2010-03-31 21:18:21 +0100

    Refactor the tests to make more sense.

--- lib/lp/bugs/externalbugtracker/tests/test_externalbugtracker.py
+++ lib/lp/bugs/externalbugtracker/tests/test_externalbugtracker.py
@@ -18,54 +18,73 @@ from lp.bugs.interfaces.externalbugtracker import (
 from lp.testing import TestCase
 
 
-class TestExternalBugTracker(TestCase):
+class BackLinkingExternalBugTracker(ExternalBugTracker):
+    implements(ISupportsBackLinking)
 
-    def test_sync_comments(self):
-        # An ExternalBugTracker has a sync_comments attribute which is
-        # represents when comment syncing is enabled.
+class CommentImportingExternalBugTracker(ExternalBugTracker):
+    implements(ISupportsCommentImport)
+
+class CommentPushingExternalBugTracker(ExternalBugTracker):
+    implements(ISupportsCommentPushing)
+
+
+class TestCheckwatchesConfig(TestCase):
+
+    base_url = "http://www.example.com/"
+
+    def test_sync_comments_enabled(self):
+        # If the global config checkwatches.sync_comments is True,
+        # external bug trackers will set their sync_comments attribute
+        # according to their support of comment syncing.
         self.pushConfig('checkwatches', sync_comments=True)
-        base_url = "http://www.example.com/"
         # A plain tracker will never support syncing comments.
-        tracker = ExternalBugTracker(base_url)
+        tracker = ExternalBugTracker(self.base_url)
         self.assertFalse(tracker.sync_comments)
         # Trackers that support comment pushing, comment pulling or
         # back-linking will have sync_comments set to True.
-        class BackLinkingExternalBugTracker(ExternalBugTracker):
-            implements(ISupportsBackLinking)
-        tracker = BackLinkingExternalBugTracker(base_url)
+        tracker = BackLinkingExternalBugTracker(self.base_url)
         self.assertTrue(tracker.sync_comments)
-        class CommentImportingExternalBugTracker(ExternalBugTracker):
-            implements(ISupportsCommentImport)
-        tracker = CommentImportingExternalBugTracker(base_url)
+        tracker = CommentImportingExternalBugTracker(self.base_url)
         self.assertTrue(tracker.sync_comments)
-        class CommentPushingExternalBugTracker(ExternalBugTracker):
-            implements(ISupportsCommentPushing)
-        tracker = CommentPushingExternalBugTracker(base_url)
+        tracker = CommentPushingExternalBugTracker(self.base_url)
         self.assertTrue(tracker.sync_comments)
-        # If syncing comments is globally disabled, sync_comments will
-        # always be False.
+
+    def test_sync_comments_disabled(self):
+        # If the global config checkwatches.sync_comments is False,
+        # external bug trackers will always set their sync_comments
+        # attribute to False.
         self.pushConfig('checkwatches', sync_comments=False)
-        tracker = ExternalBugTracker(base_url)
+        tracker = ExternalBugTracker(self.base_url)
         self.assertFalse(tracker.sync_comments)
-        tracker = BackLinkingExternalBugTracker(base_url)
+        tracker = BackLinkingExternalBugTracker(self.base_url)
         self.assertFalse(tracker.sync_comments)
-        tracker = CommentImportingExternalBugTracker(base_url)
+        tracker = CommentImportingExternalBugTracker(self.base_url)
         self.assertFalse(tracker.sync_comments)
-        tracker = CommentPushingExternalBugTracker(base_url)
+        tracker = CommentPushingExternalBugTracker(self.base_url)
         self.assertFalse(tracker.sync_comments)
 
-    def test_sync_comments_for_debbugs(self):
+    def test_sync_debbugs_comments_enabled(self):
         # Debian Bugs syncing can also be switched on and off using a
         # separate config variable, sync_debbugs_comments. DebBugs
         # supports comment pushing, comment import and back-linking.
         self.pushConfig(
             'checkwatches', sync_comments=True, sync_debbugs_comments=True)
-        tracker = DebBugs("http://www.example.com/")
+        tracker = DebBugs(self.base_url)
         self.assertTrue(tracker.sync_comments)
-        # When it's disabled, syncing will always be disabled even
-        # when sync_comments is set.
-        self.pushConfig('checkwatches', sync_debbugs_comments=False)
-        tracker = DebBugs("http://www.example.com/")
+        # When either sync_comments or sync_debbugs_comments is False
+        # (or both), the Debian Bugs external bug tracker will claim
+        # to not support any form of comment syncing.
+        self.pushConfig(
+            'checkwatches', sync_comments=True, sync_debbugs_comments=False)
+        tracker = DebBugs(self.base_url)
+        self.assertFalse(tracker.sync_comments)
+        self.pushConfig(
+            'checkwatches', sync_comments=False, sync_debbugs_comments=True)
+        tracker = DebBugs(self.base_url)
+        self.assertFalse(tracker.sync_comments)
+        self.pushConfig(
+            'checkwatches', sync_comments=False, sync_debbugs_comments=False)
+        tracker = DebBugs(self.base_url)
         self.assertFalse(tracker.sync_comments)
 
 
