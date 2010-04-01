220e1cd3aef4bd716deeb9d4ef4adf9ed11a6717
Guilherme Salgado <salgado@canonical.com>
2010-04-01 16:04:10 -0300

    Revert a change to lib/canonical/launchpad/webapp/publication.py that broke testopenid.dev

--- lib/canonical/launchpad/webapp/publication.py
+++ lib/canonical/launchpad/webapp/publication.py
@@ -316,6 +316,12 @@ class LaunchpadBrowserPublication(
         """
         if request.method != 'POST':
             return
+        # XXX: jamesh 2007-11-23 bug=124421:
+        # Allow offsite posts to our TestOpenID endpoint.  Ideally we'd
+        # have a better way of marking this URL as allowing offsite
+        # form posts.
+        if request['PATH_INFO'] == '/+openid':
+            return
         if (IOAuthSignedRequest.providedBy(request)
             or not IBrowserRequest.providedBy(request)
             or request['PATH_INFO']  in (
@@ -422,6 +428,7 @@ class LaunchpadBrowserPublication(
         Because of this we cannot chain to the superclass and implement
         the whole behaviour here.
         """
+        orig_env = request._orig_env
         assert hasattr(request, '_publicationticks_start'), (
             'request._publicationticks_start, which should have been set by '
             'callObject(), was not found.')
