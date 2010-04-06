28efc3ae2d033f0a46702685a2edea3b708a2345
Curtis Hovey <curtis.hovey@canonical.com>
2010-04-06 17:43:55 -0400

    Strip sshkeys of line breaks when using listteammembers. Quiet lint's many complaints about the changed files.

--- lib/lp/registry/tests/test_listteammembers.py
+++ lib/lp/registry/tests/test_listteammembers.py
@@ -6,6 +6,8 @@ import unittest
 from canonical.testing import LaunchpadZopelessLayer
 
 from lp.registry.scripts import listteammembers
+from lp.testing import TestCaseWithFactory
+
 
 ubuntuteam_default = sorted([
     u'cprov, celso.providelo@canonical.com',
@@ -36,11 +38,13 @@ ubuntuteam_email = sorted([
 
 ubuntuteam_full = sorted([
     u'ubuntu-team|10|limi|limi@plone.org|Alexander Limi|no',
-    u'ubuntu-team|11|stevea|steve.alexander@ubuntulinux.com|Steve Alexander|no',
+    u'ubuntu-team|11|stevea|steve.alexander@ubuntulinux.com'
+        u'|Steve Alexander|no',
     u'ubuntu-team|16|name16|foo.bar@canonical.com|Foo Bar|yes',
     u'ubuntu-team|19|warty-gnome|--none--|Warty Gnome Team|no',
     u'ubuntu-team|1|mark|mark@example.com|Mark Shuttleworth|no',
-    u'ubuntu-team|26|kinnison|daniel.silverstone@canonical.com|Daniel Silverstone|no',
+    u'ubuntu-team|26|kinnison|daniel.silverstone@canonical.com'
+        u'|Daniel Silverstone|no',
     u'ubuntu-team|28|cprov|celso.providelo@canonical.com|Celso Providelo|no',
     u'ubuntu-team|33|edgar|edgar@monteparadiso.hr|Edgar Bursic|no',
     u'ubuntu-team|4|kamion|colin.watson@ubuntulinux.com|Colin Watson|no',
@@ -48,13 +52,21 @@ ubuntuteam_full = sorted([
     ])
 
 ubuntuteam_sshkeys = [
-    u'mark: ssh-dss AAAAB3NzaC1kc3MAAABBAL5VoWG5sy3CnLYeOw47L8m9A15hA/PzdX2u0B7c2Z1ktFPcEaEuKbLqKVSkXpYm7YwKj9y88A9Qm61CdvI0c50AAAAVAKGY0YON9dEFH3DzeVYHVEBGFGfVAAAAQCoe0RhBcefm4YiyQVwMAxwTlgySTk7FSk6GZ95EZ5Q8/OTdViTaalvGXaRIsBdaQamHEBB+Vek/VpnF1UGGm8YAAABAaCXDl0r1k93JhnMdF0ap4UJQ2/NnqCyoE8Xd5KdUWWwqwGdMzqB1NOeKN6ladIAXRggLc2E00UsnUXh3GE3Rgw== Private key in lib/lp/codehosting/tests/id_dsa',
+    u'mark: ssh-dss AAAAB3NzaC1kc3MAAABBAL5VoWG5sy3CnLYeOw47L8m9A15hA/PzdX2u0'
+    u'B7c2Z1ktFPcEaEuKbLqKVSkXpYm7YwKj9y88A9Qm61CdvI0c50AAAAVAKGY0YON9dEFH3Dz'
+    u'eVYHVEBGFGfVAAAAQCoe0RhBcefm4YiyQVwMAxwTlgySTk7FSk6GZ95EZ5Q8/OTdViTaalv'
+    u'GXaRIsBdaQamHEBB+Vek/VpnF1UGGm8YAAABAaCXDl0r1k93JhnMdF0ap4UJQ2/NnqCyoE8'
+    u'Xd5KdUWWwqwGdMzqB1NOeKN6ladIAXRggLc2E00UsnUXh3GE3Rgw== Private key in '
+    u'lib/lp/codehosting/tests/id_dsa',
     ]
 
 
-class ListTeamMembersTestCase(unittest.TestCase):
+class ListTeamMembersTestCase(TestCaseWithFactory):
     """Test listing team members."""
     layer = LaunchpadZopelessLayer
+
+    def setUp(self):
+        super(ListTeamMembersTestCase, self).setUp()
 
     def test_listteammembers_default_list(self):
         """Test the default option."""
@@ -64,22 +76,40 @@ class ListTeamMembersTestCase(unittest.TestCase):
     def test_listteammembers_email_only(self):
         """Test the email only option."""
         self.assertEqual(
-            listteammembers.process_team('ubuntu-team', 'email'), ubuntuteam_email)
+            listteammembers.process_team('ubuntu-team', 'email'),
+            ubuntuteam_email)
 
     def test_listteammembers_full_details(self):
         """Test the full details option."""
         self.assertEqual(
-            listteammembers.process_team('ubuntu-team', 'full'), ubuntuteam_full)
+            listteammembers.process_team('ubuntu-team', 'full'),
+            ubuntuteam_full)
 
     def test_listteammembers_sshkeys(self):
         """Test the ssh keys option."""
         self.assertEqual(
-            listteammembers.process_team('ubuntu-team', 'sshkeys'), ubuntuteam_sshkeys)
+            listteammembers.process_team('ubuntu-team', 'sshkeys'),
+            ubuntuteam_sshkeys)
+
+    def test_make_sshkey_params(self):
+        """Test that ssh keys ."""
+        member = self.factory.makePerson(name='biggles')
+        team = self.factory.makeTeam(name='squadron')
+        team.addMember(member, reviewer=team.teamowner)
+        sshkey = self.factory.makeSSHKey(member)
+        sshkey.keytext = u'123badKeysMight\r\nContain\fBadCharacters'
+        sshkey.comment = 'comment'
+        expected = dict(
+            name=u'biggles',
+            sshkey=u'ssh-rsa 123badKeysMightContainBadCharacters comment')
+        result = listteammembers.make_sshkey_params(member, 'ssh-rsa', sshkey)
+        self.assertEqual(expected, result)
 
     def test_listteammembers_unknown_team(self):
         """Test unknown team."""
         self.assertRaises(
-            listteammembers.NoSuchTeamError, listteammembers.process_team, 'nosuchteam-matey')
+            listteammembers.NoSuchTeamError, listteammembers.process_team,
+            'nosuchteam-matey')
 
 
 def test_suite():
