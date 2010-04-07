27d55516a94db3d04fdb0973a635ba6ad90167fe
Graham Binns <graham@canonical.com>
2010-04-07 14:04:43 +0100

    Renamed and updated the patch in accordance with Stuart's approval.

--- database/schema/patch-2207-47-0.sql
+++ database/schema/patch-2207-47-0.sql
@@ -3,4 +3,4 @@ SET client_min_messages=ERROR;
 UPDATE BugWatchActivity SET result = 9 WHERE result IS NULL;
 ALTER TABLE BugWatchActivity ALTER COLUMN result SET NOT NULL;
 
-INSERT INTO LaunchpadDatabaseRevision VALUES (2207, 99, 0);
+INSERT INTO LaunchpadDatabaseRevision VALUES (2207, 47, 0);
