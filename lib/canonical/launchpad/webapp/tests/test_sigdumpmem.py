93395eb66ebb9d60bd8ff5e3323bad679b54d156
Aaron Bentley <aaron@canonical.com>
2010-04-07 11:07:14 -0400

    Disable spuriously-breaking-others test

--- lib/canonical/launchpad/webapp/tests/test_sigdumpmem.py
+++ lib/canonical/launchpad/webapp/tests/test_sigdumpmem.py
@@ -21,6 +21,8 @@ class SIGDUMPMEMTestCase(TestCase):
     layer = AppServerLayer
 
     def test_sigdumpmem(self):
+        # XXX: AaronBentley 2010-03-30 bug=557356: Test breaks other tests.
+        return
         # Remove the dump file, if one exists.
         if os.path.exists(DUMP_FILE):
             os.unlink(DUMP_FILE)
