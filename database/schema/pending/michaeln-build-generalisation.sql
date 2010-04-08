3bbc7ac903c62c3dbe5d0ce82da0eb97e8cf2304
Michael Nelson <michael.nelson@canonical.com>
2010-04-08 16:35:54 +0200

    Updated schema change to use standard create table with migration function.

--- database/schema/pending/michaeln-build-generalisation.sql
+++ database/schema/pending/michaeln-build-generalisation.sql
@@ -8,101 +8,129 @@ SET client_min_messages=ERROR;
 -- blueprint/bug for more information.
 
 -- Still to do:
---   Inventory check of all table columns to ensure nothing is lost
---   Replicate current indexes/foreign key constraints (as noted below)
---   Populate with any (future) SPRecipeBuild data (as noted below)
---   Update all sample data :(
+--   Inventory check of all table columns to ensure nothing is lost (Done)
+--   Replicate current indexes (some span now multiple tables? see below)
+--   Update all sample data.
 
 -- Step 1
--- Create the new BuildFarmJob table using current data from binary
--- builds, with it's own new primary key (as we'll use Build.id for the
--- BinaryPackageBuild table).
-CREATE SEQUENCE buildfarmjob_id;
-CREATE TABLE BuildFarmJob AS
-    SELECT
-        nextval('buildfarmjob_id') AS id,
-        build.processor,
-        archive.require_virtualized AS virtualised,
-        -- Currently we do not know if a build was virtual or not? (it's only
-        -- on the archive and the builder, both of which can change).
-        -- IBuild.is_virtualized just queries the archive.
-        buildqueue.estimated_duration,
-        buildqueue.lastscore AS score,
-        build.datecreated AS date_created,
-        (build.datebuilt - build.buildduration) AS date_started,
-        build.datebuilt AS date_finished,
-        build.builder,
-        build.buildstate AS status,
-        buildqueue.logtail AS log_tail,
-        build.buildlog AS log,
-        build.id AS build_id -- Temporary reference that will be removed below.
-    FROM
-       build LEFT JOIN buildpackagejob ON build.id = buildpackagejob.build
-             LEFT JOIN buildqueue ON buildqueue.job = buildpackagejob.job,
-       archive
-    WHERE
-       build.archive = archive.id;
+-- Create the new BuildFarmJob, PackageBuild and BinaryPackageBuild tables,
+-- with indexes based on the current Build table.
+CREATE TABLE BuildFarmJob (
+    id serial PRIMARY KEY,
+    processor integer NOT NULL CONSTRAINT buildfarmjob__processor__fk REFERENCES processor,
+    virtualised boolean NOT NULL,
+    -- Currently we do not know if a build was virtual or not? (it's only
+    -- on the archive and the builder, both of which can change).
+    -- IBuild.is_virtualized just queries the archive.
+    date_created timestamp without time zone DEFAULT timezone('UTC'::text, now()) NOT NULL,
+    date_started timestamp without time zone,
+    date_finished timestamp without time zone,
+    builder integer CONSTRAINT buildfarmjob__builder__fk REFERENCES builder,
+    status integer NOT NULL,
+    log integer CONSTRAINT buildfarmjob__log__fk REFERENCES libraryfilealias
+);
+CREATE INDEX buildfarmjob__date_created__idx ON buildfarmjob(date_created);
+CREATE INDEX buildfarmjob__date_started__idx ON buildfarmjob(date_started);
+CREATE INDEX buildfarmjob__date_finished__idx ON buildfarmjob(date_finished);
+CREATE INDEX buildfarmjob__builder_and_status__idx ON buildfarmjob(builder, status);
+CREATE INDEX buildfarmjob__log__idx ON buildfarmjob(log) WHERE log IS NOT NULL;
 
-ALTER SEQUENCE buildfarmjob_id OWNED BY BuildFarmJob.id;
-ALTER TABLE buildfarmjob ALTER id SET DEFAULT nextval('buildfarmjob_id');
-ALTER TABLE buildfarmjob ADD PRIMARY KEY (id);
--- TODO: Replicate indexes and foreign key constraints.
--- TODO: Add any data from current SourcePackageRecipeBuild records.
+CREATE TABLE PackageBuild (
+    id serial PRIMARY KEY,
+    build_farm_job integer NOT NULL CONSTRAINT packagebuild__build_farm_job__fk REFERENCES buildfarmjob,
+    archive integer NOT NULL CONSTRAINT packagebuild__archive__fk REFERENCES archive, pocket integer NOT NULL DEFAULT 0,
+    upload_log integer CONSTRAINT packagebuild__log__fk REFERENCES libraryfilealias,
+    dependencies text
+);
+CREATE UNIQUE INDEX packagebuild__build_farm_job__idx ON packagebuild(build_farm_job);
+CREATE INDEX packagebuild__archive__idx ON packagebuild(archive);
+CREATE INDEX packagebuild__upload_log__idx ON packagebuild(upload_log) WHERE upload_log IS NOT NULL;
+
+CREATE TABLE BinaryPackageBuild (
+    package_build integer NOT NULL CONSTRAINT binarypackagebuild__package_build__fk REFERENCES packagebuild,
+    distro_arch_series integer NOT NULL CONSTRAINT binarypackagebuild__distro_arch_series__fk REFERENCES distroarchseries,
+    source_package_release integer NOT NULL CONSTRAINT binarypackagebuild__source_package_release__fk REFERENCES sourcepackagerelease
+);
+
+CREATE UNIQUE INDEX binarypackagebuild__package_build__idx ON binarypackagebuild(package_build);
+-- Is there a way to create an index spanning across tables? Do I need a
+-- trigger instead?
+-- CREATE UNIQUE INDEX binarypackagebuild__distro_arch_series_uniq__idx ON binarypackagebuild(distro_arch_series, source_package_release, archive)
+-- CREATE INDEX binarypackagebuild__distro_arch_series__status__idx ON binarypackagebuild(distro_arch_series, status?)
+-- CREATE INDEX binarypackagebuild__distro_arch_series__date_finished ON binarypackagebuild(distro_arch_series, date_finished)
+CREATE INDEX binarypackagebuild__source_package_release_idx ON binarypackagebuild(source_package_release);
 
 -- Step 2
--- Create the PackageBuild table for information specific to builds of
--- packages that will end up being published in archives.
-CREATE SEQUENCE packagebuild_id;
-CREATE TABLE PackageBuild AS
-    SELECT
-        nextval('packagebuild_id') AS id,
-        buildfarmjob.id AS build_farm_job,
-        build.archive,
-        build.pocket,
-        build.upload_log,
-        build.dependencies
-    FROM
-        build JOIN buildfarmjob ON build.id = buildfarmjob.build_id;
-ALTER TABLE packagebuild ALTER id SET DEFAULT nextval('packagebuild_id');
-ALTER TABLE packagebuild ADD PRIMARY KEY (id);
--- TODO: Replicate indexes and foreign key constraints.
--- TODO: Add any data from current SPRecipeBuild records.
+-- Migrate the current data from the build table to the newly created
+-- relationships.
+CREATE OR REPLACE FUNCTION migrate_build_rows() RETURNS integer
+LANGUAGE plpgsql AS
+$$
+DECLARE
+    build_info RECORD;
+    rows_migrated integer;
+    buildfarmjob_id integer;
+    packagebuild_id integer;
+BEGIN
+    rows_migrated := 0;
+    FOR build_info IN
+        SELECT
+            build.processor,
+            archive.require_virtualized AS virtualised,
+            -- Currently we do not know if a build was virtual or not? (it's
+            -- only on the archive and the builder, both of which can
+            -- change).  IBuild.is_virtualized just queries the archive.
+            build.datecreated AS date_created,
+            (build.datebuilt - build.buildduration) AS date_started,
+            build.datebuilt AS date_finished,
+            build.builder,
+            build.buildstate AS status,
+            build.buildlog AS log,
+            build.archive,
+            build.pocket,
+            build.upload_log,
+            build.dependencies,
+            build.distroarchseries AS distro_arch_series,
+            build.sourcepackagerelease AS source_package_release,
+            build.build_warnings -- We don't seem to use this in LP code at all?
+        FROM
+            build JOIN archive ON build.archive = archive.id
+    LOOP
+        INSERT INTO buildfarmjob(
+            processor, virtualised, date_created, date_started,
+            date_finished, builder, status, log)
+        VALUES (
+            build_info.processor, build_info.virtualised,
+            build_info.date_created, build_info.date_started,
+            build_info.date_finished, build_info.builder,
+            build_info.status, build_info.log);
+        -- Get the key of the BuildFarmJob row just inserted.
+        SELECT currval('buildfarmjob_id_seq') INTO buildfarmjob_id;
+        INSERT INTO packagebuild (
+            build_farm_job, archive, upload_log, dependencies)
+        VALUES (
+            buildfarmjob_id, build_info.archive, build_info.upload_log,
+            build_info.dependencies);
+        -- Get the key of the PackageBuild row just inserted.
+        SELECT currval('packagebuild_id_seq') INTO packagebuild_id;
+        INSERT INTO binarypackagebuild(
+            package_build, distro_arch_series, source_package_release)
+        VALUES (
+            packagebuild_id, build_info.distro_arch_series,
+            build_info.source_package_release);
+        rows_migrated := rows_migrated + 1;
+    END LOOP;
+    RETURN rows_migrated;
+END;
+$$;
+
+-- Run the data migration function.
+SELECT * FROM migrate_build_rows();
+
+DROP FUNCTION migrate_build_rows();
 
 -- Step 3
--- Create the BinaryPackageBuild table for information specific to binary
--- package builds.
-CREATE SEQUENCE binarypackagebuild_id;
-CREATE TABLE BinaryPackageBuild AS
-    SELECT
-        nextval('binarypackagebuild_id') AS id,
-        packagebuild.id AS package_build,
-        build.distroarchseries AS distro_arch_series,
-        build.sourcepackagerelease AS source_package_release,
-        build.build_warnings -- We don't seem to use this in LP code at all?
-    FROM
-        build JOIN buildfarmjob ON build.id = buildfarmjob.build_id
-              JOIN packagebuild ON packagebuild.build_farm_job = buildfarmjob.id;
-ALTER TABLE binarypackagebuild ALTER id SET DEFAULT nextval('binarypackagebuild_id');
-ALTER TABLE binarypackagebuild ADD PRIMARY KEY (id);
--- TODO: Replicate indexes and foreign key constraints.
-
--- Step 4
--- Update the SourcePackageRecipeBuild table with a package_build foreign key
--- to PackageBuild and remove the unnecessary columns.
-
--- Step 5
--- Create an empty TranslationTemplatesBuild table (talk with jtv about this).
-
--- Step 6
--- Note, to enable parallel development getting the codebase updated, we may
--- want to do the following cleanup in a subsequent branch (in the same pipe
--- of course) so we can gradually move the code over to the new schema without
--- breaking all the tests.
--- Data cleanup. We need to:
---   1. Remove the temporary build_id column on BuildFarmJob
---   2. Remove the build table (TODO: check for other references to build and
---      update them to BinaryPackageBuild.id)
---   3. Remove current buildqueue table.
---   4. Remove related job records
---   5. Remove the BuildPackageJob table.
-
+-- Note, to enable parallel development getting the codebase updated, we will
+-- uncomment the following deletion of the build table once the code has been
+-- updated.
+-- ALTER TABLE Build SET SCHEMA todrop;
