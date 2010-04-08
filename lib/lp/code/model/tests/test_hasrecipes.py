a7c7afef0616bbcd07b81c62f498aea6bcd7d165
Paul Hummer <paul@canonical.com>
2010-04-08 19:34:01 +0000

    One more comment fix.

--- lib/lp/code/model/tests/test_hasrecipes.py
+++ lib/lp/code/model/tests/test_hasrecipes.py
@@ -54,7 +54,7 @@ class TestIHasRecipes(TestCaseWithFactory):
 
     def test_product_getRecipes(self):
         # IProduct.recipes should provide all the SourcePackageRecipes attached
-        # to that branch.
+        # to that product.
         product = self.factory.makeProduct()
         branch = self.factory.makeBranch(product=product)
         recipe1 = self.factory.makeSourcePackageRecipe(
