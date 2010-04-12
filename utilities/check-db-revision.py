1992ccee043e5dbd031cec7ed87646be1410c2ed
Maris Fogels <maris.fogels@canonical.com>
2010-04-12 13:57:26 -0400

    Added a simple script and makefile check to ensure that the current database schema is up-to-date before we try and fire up the application server.

--- utilities/check-db-revision.py
+++ utilities/check-db-revision.py
@@ -6,6 +6,17 @@ Check that the database revision of the current branch matches the current
 database schema number.
 """
 
-import canonical.database.revision
+import _pythonpath
+import sys
 
-canonical.database.revision.confirm_dbrevision_on_startup()
+from canonical.database.revision import (
+    confirm_dbrevision_on_startup, InvalidDatabaseRevision)
+
+try:
+    confirm_dbrevision_on_startup()
+except InvalidDatabaseRevision, err:
+    print "Oops, we are trying to use an invalid database revision!"
+    print err
+    sys.exit(1)
+else:
+    sys.exit(0)
