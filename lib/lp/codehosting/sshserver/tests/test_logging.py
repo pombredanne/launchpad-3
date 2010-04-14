5f6ae0e3757123c592c8dce34e22a5dc697bb17b
Jonathan Lange <jml@canonical.com>
2010-04-14 19:56:04 +0100

    Now that we've split the events out, split out their tests too

--- lib/lp/codehosting/sshserver/tests/test_logging.py
+++ lib/lp/codehosting/sshserver/tests/test_logging.py
@@ -14,13 +14,11 @@ import sys
 from bzrlib.tests import TestCase as BzrTestCase
 
 import zope.component.event
-from zope.event import notify
 
 from canonical.config import config
 from canonical.launchpad.scripts import WatchedFileHandler
 from lp.codehosting.sshserver.accesslog import (
     get_access_logger, get_codehosting_logger, LoggingManager)
-from lp.codehosting.sshserver.events import ILoggingEvent, LoggingEvent
 from lp.testing import TestCase
 
 
@@ -127,76 +125,6 @@ class TestLoggingManager(TestCase, LoggingManagerMixin):
         manager = self.installLoggingManager()
         manager.tearDown()
         self.assertEqual(handlers, log.handlers)
-
-
-class ListHandler(logging.Handler):
-    """Logging handler that just appends records to a list.
-
-    This handler isn't intended to be used by production code -- memory leak
-    city! -- instead it's useful for unit tests that want to make sure the
-    right events are being logged.
-    """
-
-    def __init__(self, logging_list):
-        """Construct a `ListHandler`.
-
-        :param logging_list: A list that will be appended to. The handler
-             mutates this list.
-        """
-        logging.Handler.__init__(self)
-        self._list = logging_list
-
-    def emit(self, record):
-        """Append 'record' to the list."""
-        self._list.append(record)
-
-
-class TestLoggingEvent(TestCase):
-
-    def assertLogs(self, records, function, *args, **kwargs):
-        """Assert 'function' logs 'records' when run with the given args."""
-        logged_events = []
-        handler = ListHandler(logged_events)
-        self.logger.addHandler(handler)
-        result = function(*args, **kwargs)
-        self.logger.removeHandler(handler)
-        self.assertEqual(
-            [(record.levelno, record.getMessage())
-             for record in logged_events], records)
-        return result
-
-    def assertEventLogs(self, record, logging_event):
-        self.assertLogs([record], notify, logging_event)
-
-    def setUp(self):
-        TestCase.setUp(self)
-        logger = logging.getLogger(self.factory.getUniqueString())
-        logger.setLevel(logging.DEBUG)
-        self.logger = logger
-
-        @zope.component.adapter(ILoggingEvent)
-        def _log_event(event):
-            logger.log(event.level, event.message)
-
-        zope.component.provideHandler(_log_event)
-        self.addCleanup(
-            zope.component.getGlobalSiteManager().unregisterHandler,
-            _log_event)
-
-    def test_level(self):
-        event = LoggingEvent(logging.CRITICAL, "foo")
-        self.assertEventLogs((logging.CRITICAL, 'foo'), event)
-
-    def test_formatting(self):
-        event = LoggingEvent(logging.DEBUG, "foo: %(name)s", name="bar")
-        self.assertEventLogs((logging.DEBUG, 'foo: bar'), event)
-
-    def test_subclass(self):
-        class SomeEvent(LoggingEvent):
-            template = "%(something)s happened."
-            level = logging.INFO
-        self.assertEventLogs(
-            (logging.INFO, 'foo happened.'), SomeEvent(something='foo'))
 
 
 def test_suite():
