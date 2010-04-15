c0e65b086990d7de369aa2a162ae352eb70b3e06
Jonathan Lange <jml@canonical.com>
2010-04-15 15:38:14 +0100

    Move the codehosting-specific tests out too

--- lib/lp/codehosting/sshserver/tests/test_auth.py
+++ lib/lp/codehosting/sshserver/tests/test_auth.py
@@ -18,16 +18,13 @@ from twisted.conch.ssh.transport import SSHCiphers, SSHServerTransport
 from twisted.internet import defer
 from twisted.python import failure
 from twisted.python.util import sibpath
-from twisted.test.proto_helpers import StringTransport
 
 from twisted.trial.unittest import TestCase as TrialTestCase
 
 from canonical.config import config
 from canonical.launchpad.xmlrpc import faults
 from canonical.testing.layers import TwistedLayer
-from lp.codehosting.sshserver import auth, service
-from lp.codehosting.sshserver.daemon import (
-    get_key_path, get_portal, PRIVATE_KEY_FILE, PUBLIC_KEY_FILE)
+from lp.codehosting.sshserver import auth
 from lp.services.twistedsupport import suppress_stderr
 
 
@@ -500,74 +497,6 @@ class TestPublicKeyFromLaunchpadChecker(TrialTestCase):
         d.addCallback(check_one_call)
         return d
 
-
-class StringTransportWith_setTcpKeepAlive(StringTransport):
-    def __init__(self, hostAddress=None, peerAddress=None):
-        StringTransport.__init__(self, hostAddress, peerAddress)
-        self._keepAlive = False
-
-    def setTcpKeepAlive(self, flag):
-        self._keepAlive = flag
-
-
-class TestFactory(TrialTestCase):
-    """Tests for our SSH factory."""
-
-    layer = TwistedLayer
-
-    def makeFactory(self):
-        """Create and start the factory that our SSH server uses."""
-        factory = service.Factory(
-            get_portal(None, None),
-            private_key=Key.fromFile(
-                get_key_path(PRIVATE_KEY_FILE)),
-            public_key=Key.fromFile(
-                get_key_path(PUBLIC_KEY_FILE)))
-        factory.startFactory()
-        return factory
-
-    def startConnecting(self, factory):
-        """Connect to the `factory`."""
-        server_transport = factory.buildProtocol(None)
-        server_transport.makeConnection(StringTransportWith_setTcpKeepAlive())
-        return server_transport
-
-    def test_set_keepalive_on_connection(self):
-        # The server transport sets TCP keep alives on the underlying
-        # transport.
-        factory = self.makeFactory()
-        server_transport = self.startConnecting(factory)
-        self.assertTrue(server_transport.transport._keepAlive)
-
-    def beginAuthentication(self, factory):
-        """Connect to `factory` and begin authentication on this connection.
-
-        :return: The `SSHServerTransport` after the process of authentication
-            has begun.
-        """
-        server_transport = self.startConnecting(factory)
-        server_transport.ssh_SERVICE_REQUEST(NS('ssh-userauth'))
-        self.addCleanup(server_transport.service.serviceStopped)
-        return server_transport
-
-    def test_authentication_uses_our_userauth_service(self):
-        # The service of a SSHServerTransport after authentication has started
-        # is an instance of our SSHUserAuthServer class.
-        factory = self.makeFactory()
-        transport = self.beginAuthentication(factory)
-        self.assertIsInstance(transport.service, auth.SSHUserAuthServer)
-
-    def test_two_connections_two_minds(self):
-        # Two attempts to authenticate do not share the user-details cache.
-        factory = self.makeFactory()
-
-        server_transport1 = self.beginAuthentication(factory)
-        server_transport2 = self.beginAuthentication(factory)
-
-        mind1 = server_transport1.service.getMind()
-        mind2 = server_transport2.service.getMind()
-
-        self.assertNotIdentical(mind1.cache, mind2.cache)
 
 def test_suite():
     return unittest.TestLoader().loadTestsFromName(__name__)
