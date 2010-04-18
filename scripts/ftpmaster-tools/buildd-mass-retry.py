dbe6a74bdb83675359d0907ff147ae89865e2cbd
William Grant <me@williamgrant.id.au>
2010-04-18 14:29:48 +1000

    Convince buildd-mass-retry.py to not retry superseded builds.

--- scripts/ftpmaster-tools/buildd-mass-retry.py
+++ scripts/ftpmaster-tools/buildd-mass-retry.py
@@ -121,6 +121,10 @@ def main():
             build_state=target_state, pocket=pocket)
 
         for build in target_builds:
+            # Skip builds for superseded sources; they won't ever
+            # actually build.
+            if not build.current_source_publication:
+                continue
 
             if not build.can_be_retried:
                 log.warn('Can not retry %s (%s)' % (build.title, build.id))
