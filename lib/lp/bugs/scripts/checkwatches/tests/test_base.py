aa860a170fcfedb76636f3742e049a935ebba214
Gavin Panella <gavin.panella@canonical.com>
2010-04-19 14:30:29 +0100

    Add tests for WorkingBase.error() and WorkingBase.warning().

--- lib/lp/bugs/scripts/checkwatches/tests/test_base.py
+++ lib/lp/bugs/scripts/checkwatches/tests/test_base.py
@@ -3,41 +3,52 @@
 
 """Tests for the `base` module."""
 
+from __future__ import with_statement
+
 __metaclass__ = type
 
 import unittest
 
-from canonical.launchpad.webapp.adapter import (
-    clear_request_started, get_request_statements, set_request_started)
+from contextlib import contextmanager
+
+import transaction
+
+from canonical.launchpad.webapp.adapter import get_request_statements
+from canonical.launchpad.scripts.logger import QuietFakeLogger
 from canonical.testing import LaunchpadZopelessLayer
 
 from lp.testing import TestCaseWithFactory
 
-from ..base import report_oops
+from ..base import WorkingBase
 
 
-class TestWorkingBase(TestCaseWithFactory):
+class TestWorkingBaseErrorReporting(TestCaseWithFactory):
 
     layer = LaunchpadZopelessLayer
 
-    def test_sql_log_cleared_in_oops_reporting(self):
-        # The log of SQL statements is cleared after an OOPS has been
-        # reported by report_oops().
-
-        # Enable SQL statment logging.
-        set_request_started()
-        try:
-            self.factory.makeBug()
+    @contextmanager
+    def _test_sql_log_cleared_after_x(self):
+        person = self.factory.makePerson()
+        email = person.preferredemail.email
+        logger = QuietFakeLogger()
+        base = WorkingBase(email, transaction.manager, logger)
+        with base.statement_logging:
+            self.factory.makeEmail('numpty@example.com', person)
             self.assertTrue(
                 len(get_request_statements()) > 0,
                 "We need at least one statement in the SQL log.")
-            report_oops()
+            yield base
             self.assertTrue(
                 len(get_request_statements()) == 0,
-                "SQL statement log not cleared by report_oops().")
-        finally:
-            # Stop SQL statement logging.
-            clear_request_started()
+                "SQL statement log not cleared by WorkingBase.warning().")
+
+    def test_sql_log_cleared_after_warning(self):
+        with self._test_sql_log_cleared_after_x() as base:
+            base.warning("Numpty on deck.")
+
+    def test_sql_log_cleared_after_error(self):
+        with self._test_sql_log_cleared_after_x() as base:
+            base.error("Numpty on deck.")
 
 
 def test_suite():
