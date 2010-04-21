277f66b1b0ab734a167379f4c16a5de8ba54f42e
Michael Nelson <michael.nelson@canonical.com>
2010-04-21 14:08:21 +0200

    Added test for default status of a BuildFarmJob.

--- lib/lp/buildmaster/tests/test_buildfarmjob.py
+++ lib/lp/buildmaster/tests/test_buildfarmjob.py
@@ -13,6 +13,7 @@ from zope.component import getUtility
 from canonical.database.sqlbase import flush_database_updates
 from canonical.testing.layers import DatabaseFunctionalLayer
 
+from lp.buildmaster.interfaces.buildbase import BuildStatus
 from lp.buildmaster.interfaces.buildfarmjob import (
     BuildFarmJobType, IBuildFarmJob, IBuildFarmJobSource)
 from lp.buildmaster.model.buildfarmjob import BuildFarmJob
@@ -42,6 +43,12 @@ class TestBuildFarmJob(TestCaseWithFactory):
             BuildFarmJob,
             BuildFarmJob.id == build_farm_job.id).one()
         self.assertEqual(build_farm_job, retrieved_job)
+
+    def test_default_status(self):
+        # A build farm job defaults to the NEEDSBUILD status.
+        build_farm_job = self.makeBuildFarmJob()
+        self.assertEqual(
+            BuildStatus.NEEDSBUILD, build_farm_job.status)
 
 def test_suite():
     return unittest.TestLoader().loadTestsFromName(__name__)
