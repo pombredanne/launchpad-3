f9d92f3b29479ce842ca2039b07cf8340854872f
William Grant <me@williamgrant.id.au>
2010-04-21 20:49:08 +1000

    Log (with level DEBUG) when we skip a superseded build.

--- scripts/ftpmaster-tools/buildd-mass-retry.py
+++ scripts/ftpmaster-tools/buildd-mass-retry.py
@@ -124,6 +124,8 @@ def main():
             # Skip builds for superseded sources; they won't ever
             # actually build.
             if not build.current_source_publication:
+                log.debug(
+                    'Skipping superseded %s (%s)' % (build.title, build.id))
                 continue
 
             if not build.can_be_retried:
