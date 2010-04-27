05d04b365a04c948c5fe6b4615f64d92e565f9e6
Tim Penhey <tim.penhey@canonical.com>
2010-04-28 10:30:15 +1200

    Add a test_traverse method.

--- lib/lp/testing/publication.py
+++ lib/lp/testing/publication.py
@@ -13,7 +13,13 @@ from cStringIO import StringIO
 
 # Z3 doesn't make this available as a utility.
 from zope.app.publication.requestpublicationregistry import factoryRegistry
+from zope.component import getUtility
+from zope.security.proxy import ProxyFactory, removeSecurityProxy
+
+from canonical.launchpad.interfaces.launchpad import IOpenLaunchBag
+from canonical.launchpad.webapp import urlsplit
 from canonical.launchpad.webapp.servers import ProtocolErrorPublication
+
 
 # Defines an helper function that returns the appropriate
 # IRequest and IPublication.
@@ -39,6 +45,7 @@ def get_request_and_publication(host='localhost', port=None,
     publication = publication_factory(None)
     return request, publication
 
+
 def print_request_and_publication(host='localhost', port=None,
                                   method='GET',
                                   mime_type='text/html',
@@ -56,3 +63,20 @@ def print_request_and_publication(host='localhost', port=None,
             print "  %s: %s" % (name, value)
     else:
         print publication_classname
+
+
+def test_traverse(url):
+    url_parts = urlsplit(url)
+    server_url = '://'.join(url_parts[0:2])
+    path_info = url_parts[2]
+    request, publication = get_request_and_publication(
+        host=url_parts[1], extra_environment={
+            'SERVER_URL': server_url,
+            'PATH_INFO': path_info})
+    getUtility(IOpenLaunchBag).clear()
+    request.setPublication(publication)
+    app = publication.getApplication(request)
+    view = request.traverse(app)
+    # Get the object from the view, but make sure it is proxied.
+    obj = ProxyFactory(removeSecurityProxy(view).context)
+    return obj, view, request
