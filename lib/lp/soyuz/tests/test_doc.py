d95244468febe5235976bc7b368dbb951f812c20
Michael Nelson <michael.nelson@canonical.com>
2010-04-30 11:42:38 +0200

    Re-enabled buildd-slavescanner.txt after removing stray ellipses, and removed a block of the doctest that was removed in devel but missed during the conflict resolution.

--- lib/lp/soyuz/tests/test_doc.py
+++ lib/lp/soyuz/tests/test_doc.py
@@ -133,14 +133,12 @@ special = {
         setUp=builddmasterSetUp,
         layer=LaunchpadZopelessLayer,
         ),
-    # XXX Tim Penhey 2010-04-30 bug=572005
-    # Disabled as part of conflict resolution.
-    #'buildd-slavescanner.txt': LayeredDocFileSuite(
-    #    '../doc/buildd-slavescanner.txt',
-    #    setUp=builddmasterSetUp,
-    #    layer=LaunchpadZopelessLayer,
-    #    stdout_logging_level=logging.WARNING
-    #    ),
+    'buildd-slavescanner.txt': LayeredDocFileSuite(
+        '../doc/buildd-slavescanner.txt',
+        setUp=builddmasterSetUp,
+        layer=LaunchpadZopelessLayer,
+        stdout_logging_level=logging.WARNING
+        ),
     # XXX Michael Nelson 2010-04-01 bug=553259
     # Disabling the buildd-slave.txt test until the
     # timing issue is sorted (see bug).
