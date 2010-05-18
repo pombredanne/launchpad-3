b3893d62ea3bbcf23db1fc0e63b2ebbde795534a
Adi Roiban <adi@roiban.ro>
2010-05-18 18:27:05 +0300

    Test that the api template count match the ones from the DB.

--- lib/lp/translations/interfaces/webservice.py
+++ lib/lp/translations/interfaces/webservice.py
@@ -1,6 +1,8 @@
 # Copyright 2009 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
+# pylint: disable-msg=W0611
+
 """All the interfaces that are exposed through the webservice."""
 
 from lp.translations.interfaces.translationimportqueue import (
