f6fcf0880b7efd48d51c4e055ef7be112a1025b0
Bryce Harrington <bryce.harrington@canonical.com>
2010-05-21 10:04:28 -0700

    Fix typo of visiblity (should be visibility) throughout codebase

--- lib/lp/bugs/browser/bug.py
+++ lib/lp/bugs/browser/bug.py
@@ -668,7 +668,7 @@ class BugSecrecyEditView(BugEditViewBase):
 
     @property
     def label(self):
-        return 'Bug #%i - Set visiblity and security' % self.context.bug.id
+        return 'Bug #%i - Set visibility and security' % self.context.bug.id
 
     page_title = label
 
