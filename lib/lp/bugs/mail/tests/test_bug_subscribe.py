a05e7f3f7b6fbe8164e6f65fe7842d6ddc3dc93d
Deryck Hodge <deryck.hodge@canonical.com>
2010-05-25 10:08:38 -0500

    Fill out test for confirming notification is sent.

--- lib/lp/bugs/mail/tests/test_bug_subscribe.py
+++ lib/lp/bugs/mail/tests/test_bug_subscribe.py
@@ -5,8 +5,11 @@
 
 from unittest import TestLoader
 
+import transaction
+
 from canonical.testing import DatabaseFunctionalLayer
 
+from lp.services.mail import stub
 from lp.testing import TestCaseWithFactory
 
 
@@ -19,14 +22,21 @@ class TestSubscribedBySomeoneElseNotification(TestCaseWithFactory):
         # Run the tests as a logged-in user.
         TestCaseWithFactory.setUp(self, user='test@canonical.com')
 
-    def test_subscribed_by_someone_else(self):
+    def test_subscribed_by_someone_else_with_notification(self):
+        """Test notifications are sent when suppress_notify is False."""
         bug = self.factory.makeBug()
         person_subscribing = self.factory.makePerson()
         person_subscribed = self.factory.makePerson()
+        # There are no email notifications before subscribing.
+        self.assertEqual(len(stub.test_emails), 0)
         bug_subscription = bug.subscribe(
             person_subscribed, person_subscribing, suppress_notify=False)
+        transaction.commit()
+        # Ensure the subscription was properly created.
         self.assertEqual(bug_subscription.person, person_subscribed)
         self.assertEqual(bug_subscription.subscribed_by, person_subscribing)
+        # Confirm a notification has been sent.
+        self.assertEqual(len(stub.test_emails), 1)
 
 
 def test_suite():
