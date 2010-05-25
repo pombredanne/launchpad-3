c224d57ea015aa04ae192bb595744d0a0fc6963b
Paul Hummer <paul@canonical.com>
2010-05-25 04:00:00 +0000

    Changed the patch number.

--- database/schema/patch-2207-58-0.sql
+++ database/schema/patch-2207-58-0.sql
@@ -9,4 +9,4 @@ ALTER TABLE SourcePackageRecipe
 ALTER TABLE SourcePackageRecipeBuild
    DROP COLUMN sourcepackagename;
 
-INSERT INTO LaunchpadDatabaseRevision VALUES (2207, 99, 0);
+INSERT INTO LaunchpadDatabaseRevision VALUES (2207, 58, 0);
