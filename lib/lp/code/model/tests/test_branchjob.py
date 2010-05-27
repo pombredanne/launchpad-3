6e4fe733d618e709b8ddb4e6ef1129756a45baa3
Tim Penhey <tim.penhey@canonical.com>
2010-05-27 13:34:14 +1200

    Update call sites.

--- lib/lp/code/model/tests/test_branchjob.py
+++ lib/lp/code/model/tests/test_branchjob.py
@@ -312,10 +312,12 @@ class TestRevisionMailJob(TestCaseWithFactory):
     def test_run_sends_mail(self):
         """Ensure RevisionMailJob.run sends mail with correct values."""
         branch = self.factory.makeAnyBranch()
-        branch.subscribe(branch.registrant,
+        branch.subscribe(
+            branch.registrant,
             BranchSubscriptionNotificationLevel.FULL,
             BranchSubscriptionDiffSize.WHOLEDIFF,
-            CodeReviewNotificationLevel.FULL)
+            CodeReviewNotificationLevel.FULL,
+            branch.registrant)
         job = RevisionMailJob.create(
             branch, 0, 'from@example.com', 'hello', False, 'subject')
         job.run()
@@ -505,10 +507,12 @@ class TestRevisionsAddedJob(TestCaseWithFactory):
         product = self.factory.makeProduct(name='foo')
         branch = self.factory.makeProductBranch(
             name='bar', product=product, owner=jrandom)
-        branch.subscribe(branch.registrant,
+        branch.subscribe(
+            branch.registrant,
             BranchSubscriptionNotificationLevel.FULL,
             BranchSubscriptionDiffSize.WHOLEDIFF,
-            CodeReviewNotificationLevel.FULL)
+            CodeReviewNotificationLevel.FULL,
+            branch.registrant)
         branch, tree = self.create_branch_and_tree(db_branch=branch)
         tree.branch.nick = 'nicholas'
         tree.lock_write()
