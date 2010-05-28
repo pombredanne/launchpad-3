19cbc54220b0f3f32fb91ade9e0e61bf00e950bd
Tim Penhey <tim.penhey@canonical.com>
2010-05-28 21:43:57 +1200

    Remove obsolete code.

--- lib/lp/code/browser/branch.py
+++ lib/lp/code/browser/branch.py
@@ -354,19 +354,6 @@ class BranchView(LaunchpadView, FeedsMixin):
 
     def initialize(self):
         self.notices = []
-        # TODO: FIXME - this is almost certainly not what we want.
-        self._add_subscription_notice()
-
-    def _add_subscription_notice(self):
-        """Add the appropriate notice after posting the subscription form."""
-        if self.user and self.request.method == 'POST':
-            newsub = self.request.form.get('subscribe', None)
-            if newsub == 'Subscribe':
-                self.context.subscribe(self.user, self.user)
-                self.notices.append("You have subscribed to this branch.")
-            elif newsub == 'Unsubscribe':
-                self.context.unsubscribe(self.user, self.user)
-                self.notices.append("You have unsubscribed from this branch.")
 
     def user_is_subscribed(self):
         """Is the current user subscribed to this branch?"""
