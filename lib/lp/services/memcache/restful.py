dc64d415b705c696cec24512004ecae35f6b8753
Leonard Richardson <leonard.richardson@canonical.com>
2010-06-03 14:27:25 -0400

    Response to feedback.

--- lib/lp/services/memcache/restful.py
+++ lib/lp/services/memcache/restful.py
@@ -5,8 +5,10 @@
 
 import storm
 
+from zope.component import getUtility
+
 from canonical.config import config
-from lp.services.memcache.client import memcache_client_factory
+from lp.services.memcache.interfaces import IMemcacheClient
 from lazr.restful.simple import BaseRepresentationCache
 
 __metaclass__ = type
@@ -19,24 +21,30 @@ class MemcachedStormRepresentationCache(BaseRepresentationCache):
     """Caches lazr.restful representations of Storm objects in memcached."""
 
     def __init__(self):
-        self.client = memcache_client_factory()
+        """Initialize with the memcached client."""
+        self.client = getUtility(IMemcacheClient)
 
     def key_for(self, obj, media_type, version):
+        """See `BaseRepresentationCache`."""
         storm_info = storm.info.get_obj_info(obj)
         table_name = storm_info.cls_info.table
         primary_key = tuple(var.get() for var in storm_info.primary_vars)
 
         key = (table_name + repr(primary_key)
-                + ',' + media_type + ',' + str(version))
+               + ',' + config._instance_name
+               + ',' + media_type + ',' + str(version))
         return key
 
     def get_by_key(self, key, default=None):
+        """See `BaseRepresentationCache`."""
         return self.client.get(key) or default
 
     def set_by_key(self, key, value):
+        """See `BaseRepresentationCache`."""
         self.client.set(
             key, value,
             time=config.vhost.api.representation_cache_expiration_time)
 
     def delete_by_key(self, key):
+        """See `BaseRepresentationCache`."""
         self.client.delete(key)
