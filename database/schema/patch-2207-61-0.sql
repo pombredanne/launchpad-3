2eeb80f07fbdeab2825a2a6284c96d242e634c61
Aaron Bentley <aaron@canonical.com>
2010-06-03 09:28:31 -0400

    Updates from review.

--- database/schema/patch-2207-61-0.sql
+++ database/schema/patch-2207-61-0.sql
@@ -4,4 +4,10 @@
 SET client_min_messages=ERROR;
 ALTER TABLE SourcePackageRecipe ADD COLUMN is_stale BOOLEAN NOT NULL DEFAULT TRUE;
 ALTER TABLE SourcePackageRecipeBuild ADD COLUMN manifest INTEGER REFERENCES SourcePackageRecipeData;
-INSERT INTO LaunchpadDatabaseRevision VALUES (2207, 99, 0);
+
+CREATE INDEX sourcepackagerecipe__is_stale__build_daily__idx
+ON SourcepackageRecipe(is_stale, build_daily);
+
+CREATE INDEX sourcepackagerecipebuild__manifest__idx ON SourcepackageRecipeBuild(manifest);
+
+INSERT INTO LaunchpadDatabaseRevision VALUES (2207, 61, 0);
