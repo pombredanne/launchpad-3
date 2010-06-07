3d7efda881cabf807e007ae6f5930ace16795fc6
Jeroen Vermeulen <jeroen.vermeulen@canonical.com>
2010-06-07 09:52:21 +0700

    Test fixups.

--- lib/lp/translations/model/distroseries_translations_copy.py
+++ lib/lp/translations/model/distroseries_translations_copy.py
@@ -44,7 +44,7 @@ def copy_active_translations(child, transaction, logger):
     copier = MultiTableCopy(full_name, translation_tables, logger=logger)
 
     # Incremental copy of updates is no longer supported
-    assert child.getTranslationTemplates().is_empty(), (
+    assert len(list(child.getTranslationTemplates())) == 0, (
            "The child series must not yet have any translation templates.")
 
     logger.info(
