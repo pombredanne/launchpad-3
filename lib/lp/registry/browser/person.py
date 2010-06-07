cf9ea028eb39da347819b239655e80c215292bf8
Curtis Hovey <curtis.hovey@canonical.com>
2010-06-07 15:01:55 -0400

    Refactored the team participation page to show roles in teams presented in a single tabular listing ordered by displayname

--- lib/lp/registry/browser/person.py
+++ lib/lp/registry/browser/person.py
@@ -2503,7 +2503,7 @@ class PersonKarmaView(LaunchpadView):
         if self.user == self.context:
             return 'Your Launchpad Karma'
         else:
-            return 'Launchpad Karma for %s' %  self.context.displayname
+            return 'Launchpad Karma for %s' % self.context.displayname
 
     @cachedproperty
     def has_karma(self):
@@ -3071,6 +3071,40 @@ class PersonParticipationView(LaunchpadView):
     @property
     def label(self):
         return 'Team participation for ' + self.context.displayname
+
+    def _asParticipation(self, membership, team_path=None):
+        """Return a dict of participation information for the membership."""
+        team = membership.team
+        if membership.person == team.teamowner:
+            role = 'Owner'
+        elif membership.status == TeamMembershipStatus.ADMIN:
+            role = 'Admin'
+        else:
+            role = 'Member'
+        if team_path is None:
+            via = None
+        else:
+            via = COMMASPACE.join(team.displayname for team in team_path)
+        return dict(
+            displayname=team.displayname, team=team, membership=membership,
+            role=role, via=via)
+
+    @cachedproperty
+    def active_participations(self):
+        """Return the participation information for active memberships."""
+        participations = [self._asParticipation(membership)
+                for membership in self.context.myactivememberships
+                if check_permission('launchpad.View', membership)]
+        membership_set = getUtility(ITeamMembershipSet)
+        for team in self.context.teams_indirectly_participated_in:
+            # The key points of the path for presentation are:
+            # [-?] indirect memberships, [-2] direct membership, [-1] team.
+            team_path = self.context.findPathToTeam(team)
+            membership = membership_set.getByPersonAndTeam(
+                team_path[-2], team)
+            participations.append(
+                self._asParticipation(membership, team_path=team_path[:-1]))
+        return sorted(participations, key=itemgetter('displayname'))
 
     @property
     def indirect_teams_via(self):
