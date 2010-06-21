a9262b2cd3c8e96851599032b56d64c5a9b4b349
Steve Kowalik <stevenk@ubuntu.com>
2010-06-21 19:53:10 +1000

    * Stop creating an owner, and have the factory do it for us. * Import Unauthorized, so we can .assertRaises() from it. * Add a _getDescription() method, so we can assertRaises() it.

--- lib/lp/soyuz/tests/test_archive_privacy.py
+++ lib/lp/soyuz/tests/test_archive_privacy.py
@@ -4,6 +4,7 @@
 """Test Archive privacy features."""
 
 from zope.component import getUtility
+from zope.security.interfaces import Unauthorized
 from lp.soyuz.interfaces.archive import IArchiveSet
 
 from canonical.testing import LaunchpadFunctionalLayer
@@ -15,24 +16,25 @@ class TestArchivePrivacy(TestCaseWithFactory):
 
     def setUp(self):
         super(TestArchivePrivacy, self).setUp()
-        owner = self.factory.makePerson()
-        self.private_ppa = self.factory.makeArchive(
-            owner=owner, description='Foo')
+        self.private_ppa = self.factory.makeArchive(description='Foo')
         login('admin@canonical.com')
         self.private_ppa.buildd_secret = 'blah'
         self.private_ppa.private = True
         self.joe = self.factory.makePerson(name='joe')
         self.fred = self.factory.makePerson(name='fred')
-        login_person(owner)
-        self.private_ppa.newSubscription(self.joe, owner)
+        login_person(self.private_ppa.owner)
+        self.private_ppa.newSubscription(self.joe, self.private_ppa.owner)
+
+    def _getDescription(self, p3a):
+        return p3a.description
 
     def test_no_subscription(self):
         login_person(self.fred)
         p3a = getUtility(IArchiveSet).get(self.private_ppa.id)
-        self.assertRaises(p3a.description, Unauthorized)
+        self.assertRaises(Unauthorized, self._getDescription(p3a))
 
     def test_subscription(self):
         login_person(self.joe)
         p3a = getUtility(IArchiveSet).get(self.private_ppa.id)
-        self.assertEqual(p3a.description, "Foo")
+        self.assertEqual(self._getDescription(p3a), "Foo")
 
