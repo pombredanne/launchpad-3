771088932d6030d8991175ad299d006356601786
William Grant <me@williamgrant.id.au>
2010-07-10 14:12:04 +1000

    Remove DB patch from pending/.

--- database/schema/pending/deb-to-ddeb-link.sql
+++
@@ -1,7 +0,0 @@
--- Copyright 2009 Canonical Ltd.  This software is licensed under the
--- GNU Affero General Public License version 3 (see the file LICENSE).
-
-ALTER TABLE BinaryPackageRelease
-    ADD COLUMN ddeb_package integer REFERENCES BinaryPackageRelease;
-
-INSERT INTO LaunchpadDatabaseRevision VALUES (2207, 99, 0);
