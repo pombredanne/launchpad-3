8d61e16495c2cf0fe6034ab2a13be5cf4d0f4d7c
Danilo Šegan <danilo@canonical.com>
2010-07-14 00:10:43 +0200

    Recreate indexes dropped on column removals.

--- database/schema/patch-2207-94-0.sql
+++ database/schema/patch-2207-94-0.sql
@@ -54,4 +54,23 @@ ALTER TABLE TranslationMessage
 ALTER TABLE POFile
     DROP COLUMN variant;
 
+
+-- Recreate indexes that used variant in the past.
+
+CREATE UNIQUE INDEX pofile_template_and_language_idx
+   ON pofile USING btree (potemplate, language);
+
+CREATE UNIQUE INDEX tm__potmsgset__language__shared__current__key ON translationmessage USING btree (potmsgset, language) WHERE (((is_current IS TRUE) AND (potemplate IS NULL)));
+
+CREATE UNIQUE INDEX tm__potmsgset__language__shared__imported__key ON translationmessage USING btree (potmsgset, language) WHERE (((is_imported IS TRUE) AND (potemplate IS NULL)));
+
+CREATE INDEX tm__potmsgset__language__not_used__idx ON translationmessage USING btree (potmsgset, language) WHERE (NOT ((is_current IS TRUE) AND (is_imported IS TRUE)));
+
+CREATE UNIQUE INDEX tm__potmsgset__potemplate__language__diverged__current__idx ON translationmessage USING btree (potmsgset, potemplate, language) WHERE (((is_current IS TRUE) AND (potemplate IS NOT NULL)));
+
+CREATE UNIQUE INDEX tm__potmsgset__potemplate__language__diverged__imported__idx ON translationmessage USING btree (potmsgset, potemplate, language) WHERE (((is_imported IS TRUE) AND (potemplate IS NOT NULL)));
+
+CREATE INDEX translationmessage__language__submitter__idx ON translationmessage USING btree (language, submitter);
+
+
 INSERT INTO LaunchpadDatabaseRevision VALUES (2207, 94, 0);
