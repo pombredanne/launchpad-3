be63bbadf6a08bdc28856c40dc0236e3910acb8b
Jeroen Vermeulen <jeroen.vermeulen@canonical.com>
2010-07-14 17:50:11 +0200

    Corrected usage example in docstring.

--- lib/lp/services/database/prejoin.py
+++ lib/lp/services/database/prejoin.py
@@ -10,6 +10,7 @@ from storm.zope.interfaces import IResultSet
 
 from lazr.delegates import delegates
 
+
 class PrejoinResultSet:
     """Prejoin support.
 
@@ -21,12 +22,12 @@ class PrejoinResultSet:
     The preferred solution is support in Storm core, so we can just do
     something like:
 
-    >>> results = store.find(Product).prejoin(
-    ...     (Person, EmailAddress),
-    ...     Product._ownerID == Person.id,
-    ...     EmailAddress.personID == Person.id)
+    # Select Product, but prejoin the owner as well.
+    >>> join = store.find((Product, Person), Product.name == name)
+    >>> results = prejoin(join, slice(0, 1))
     """
     delegates(IResultSet, context='result_set')
+
     def __init__(self, result_set, return_slice=slice(0, 1)):
         self.result_set = result_set
         self.return_slice = return_slice
