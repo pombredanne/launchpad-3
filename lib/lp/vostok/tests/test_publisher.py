83d988ed492c47dc540e5cf3e1fdbd8e47b42052
Michael Hudson <michael.hudson@canonical.com>
2010-07-14 15:00:32 +0200

    make test a lot better

--- lib/lp/vostok/tests/test_publisher.py
+++ lib/lp/vostok/tests/test_publisher.py
@@ -17,6 +17,11 @@ from lp.vostok.publisher import VostokLayer
 from zope.app.publication.requestpublicationregistry import factoryRegistry
 
 
+VOSTOK_ENVIRONMENT = {
+    'HTTP_HOST': config.vhost.vostok.hostname,
+    'REQUEST_METHOD': 'GET',
+    }
+
 class TestRegistration(TestCase):
     """Vostok's publication customizations are installed correctly."""
 
@@ -26,19 +31,17 @@ class TestRegistration(TestCase):
         # There is a vostok-specific request factory registered for the
         # hostname configured for vostok.
         factory = factoryRegistry.lookup(
-            "GET", "text/html", {'HTTP_HOST': config.vhost.vostok.hostname})
+            "GET", "text/html", VOSTOK_ENVIRONMENT)
         self.assertEqual('vostok', factory.vhost_name)
-
 
     def test_vostok_request_provides_vostok_layer(self):
         # The Request object constructed for requests to the vostok hostname
         # provide VostokLayer.
         factory = factoryRegistry.lookup(
-            "GET", "text/html",
-            {'HTTP_HOST': config.vhost.vostok.hostname,
-             'REQUEST_METHOD': 'GET'})
+            "GET", "text/html", VOSTOK_ENVIRONMENT)
         request_factory, publication = factory()
-        self.assert_(VostokLayer.implementedBy(request_factory.requestfactory))
+        request = request_factory('', VOSTOK_ENVIRONMENT)
+        self.assertProvides(request, VostokLayer)
 
 
 def test_suite():
