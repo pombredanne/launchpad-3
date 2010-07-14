e45872eb1d31df691625793583d11c2c3e49cb04
Michael Hudson <michael.hudson@canonical.com>
2010-07-14 17:09:37 +0200

    register (extremely simple) default view for the vostok root

---
+++ lib/lp/vostok/browser/root.py
@@ -0,0 +1,12 @@
+# Copyright 2010 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Browser code for the Vostok root."""
+
+__metaclass__ = type
+__all__ = []
+
+from canonical.launchpad.webapp import LaunchpadView
+
+class VostokRootView(LaunchpadView):
+    pass
---
+++ lib/lp/vostok/browser/tests/test_root.py
@@ -0,0 +1,39 @@
+# Copyright 2010 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""XXX: Module docstring goes here."""
+
+__metaclass__ = type
+
+import unittest
+
+from zope.app.publisher.browser import getDefaultViewName
+from zope.component import getMultiAdapter
+
+from canonical.launchpad.webapp.servers import LaunchpadTestRequest
+from canonical.testing.layers import DatabaseFunctionalLayer
+
+from lp.testing import TestCase
+from lp.vostok.browser.root import VostokRootView
+from lp.vostok.publisher import VostokRoot
+
+
+class TestBrowseRoot(TestCase):
+
+    layer = DatabaseFunctionalLayer
+
+    def test_root_default_view_name(self):
+        # The default view for the vostok root object is called "+index".
+        view_name = getDefaultViewName(VostokRoot(), LaunchpadTestRequest())
+        self.assertEquals('+index', view_name)
+
+    def test_root_index_view(self):
+        # VostokRootView is registered as the view for the VostokRoot object.
+        view = getMultiAdapter(
+            (VostokRoot(), LaunchpadTestRequest()), name='+index')
+        view.initialize()
+        self.assertIsInstance(view, VostokRootView)
+
+
+def test_suite():
+    return unittest.TestLoader().loadTestsFromName(__name__)
