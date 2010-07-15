768163d33fbc55c83917c3d72e773f8b9bf22f1d
Martin Pool <mbp@canonical.com>
2010-07-15 17:29:28 +0200

    Move database comments into comments.py

--- database/schema/pending/patch-2207-68-0.sql
+++ database/schema/pending/patch-2207-68-0.sql
@@ -15,20 +15,6 @@ CREATE TABLE FeatureFlag (
     constraint feature_flag_pkey primary key (scope, flag),
     constraint feature_flag_unique_priority_per_flag unique (flag, priority)
 );
-comment on table FeatureFlag is
-    'Configuration that varies by the active scope and that \
-can be changed without restarting Launchpad
-<https://dev.launchpad.net/LEP/FeatureFlags>';
-
-comment on column FeatureFlag.scope is 
-    'Scope in which this setting is active';
-
-comment on column FeatureFlag.priority is 
-    'Higher priority flags override lower';
-
-comment on column FeatureFlag.flag is 
-    'Name of the flag being controlled';
-
 
 insert into LaunchpadDatabaseRevision values (2207, 68, 0);
 
