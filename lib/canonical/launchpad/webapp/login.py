97fa4e553afb273e77015bc812b33facc559a858
Benji York <benji.york@canonical.com>
2010-07-19 15:36:37 -0400

    fix lint (docstrings have to be closer to the top to be real docstrings)

--- lib/canonical/launchpad/webapp/login.py
+++ lib/canonical/launchpad/webapp/login.py
@@ -1,8 +1,8 @@
 # Copyright 2009 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
-from __future__ import with_statement
-
 """Stuff to do with logging in and logging out."""
+
+from __future__ import with_statement
 
 __metaclass__ = type
 
