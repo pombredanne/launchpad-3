2aec4965884fd7b9aa4517052bf049b49fe40f62
Paul Hummer <paul@canonical.com>
2010-07-19 14:33:15 +0200

    Moved the source package recipe vocabularies to lp.code.vocabularies.sourcepackagerecipe

---
+++ lib/lp/code/vocabularies/sourcepackagerecipe.py
@@ -0,0 +1,34 @@
+# Copyright 2010 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Source Package Recipe vocabularies used in the lp/code modules."""
+from zope.component import getUtility
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+
+from canonical.launchpad.interfaces import ILaunchBag
+from canonical.launchpad.webapp.authorization import check_permission
+from canonical.launchpad.webapp.sorting import sorted_dotted_numbers
+from lp.registry.interfaces.distroseries import IDistroSeriesSet
+from lp.soyuz.browser.archive import make_archive_vocabulary
+from lp.soyuz.interfaces.archive import IArchiveSet
+
+
+def buildable_distroseries_vocabulary(context):
+    """Return a vocabulary of buildable distroseries."""
+    ppas = getUtility(IArchiveSet).getPPAsForUser(getUtility(ILaunchBag).user)
+    supported_distros = [ppa.distribution for ppa in ppas]
+    dsset = getUtility(IDistroSeriesSet).search()
+    terms = sorted_dotted_numbers(
+        [SimpleTerm(distro, distro.id, distro.displayname)
+         for distro in dsset if (
+         distro.active and distro.distribution in supported_distros)],
+        key=lambda term: term.value.version)
+    terms.reverse()
+    return SimpleVocabulary(terms)
+
+def target_ppas_vocabulary(context):
+    """Return a vocabulary of ppas that the current user can target."""
+    ppas = getUtility(IArchiveSet).getPPAsForUser(getUtility(ILaunchBag).user)
+    return make_archive_vocabulary(
+        ppa for ppa in ppas
+        if check_permission('launchpad.Append', ppa))
