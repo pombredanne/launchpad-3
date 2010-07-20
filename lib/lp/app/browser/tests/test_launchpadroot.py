2cbd59c34c825b654af46d8a3e3fc9ca1bb32293
Francis J. Lacoste <francis.lacoste@canonical.com>
2010-07-20 15:26:38 -0400

    Stub out getRecentBlogPosts since the fixture cannot use urlfetch

--- lib/lp/app/browser/tests/test_launchpadroot.py
+++ lib/lp/app/browser/tests/test_launchpadroot.py
@@ -67,6 +67,9 @@ class LaunchpadRootPermissionTest(TestCaseWithFactory):
         self.setUpRegistryExpert()
         view = create_initialized_view(
             self.root, 'index.html', principal=self.expert)
+        # Stub out the getRecentBlogPosts which fetches a blog feed using
+        # urlfetch.
+        view.getRecentBlogPosts = lambda: []
         content = BeautifulSoup(view(), parseOnlyThese=SoupStrainer('a'))
         self.failUnless(
             content.find('a', href='+featuredprojects'),
