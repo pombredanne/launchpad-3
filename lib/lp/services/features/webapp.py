4ef89bec4f256d41e76481e92f482aa79f56152f
Martin Pool <mbp@canonical.com>
2010-07-23 16:28:53 +0200

    Construct FeatureController at webapp request start

---
+++ lib/lp/services/features/webapp.py
@@ -0,0 +1,24 @@
+# Copyright 2010 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Connect Feature flags into webapp requests."""
+
+__all__ = []
+
+__metaclass__ = type
+
+from lp.services.features.flags import (
+    FeatureController,
+    per_thread,
+    )
+
+
+def start_request(event):
+    """Register FeatureController."""
+    # TODO: determine all the interesting scopes, based on event.request
+    per_thread.features = FeatureController([])
+
+
+def end_request(event):
+    """Done with this FeatureController."""
+    per_thread.features = None
