452e496b40b0fdfaa5bf321100b87790c05b4c46
Martin Pool <mbp@canonical.com>
2010-07-27 16:39:46 +1000

    Get active scopes from current request

--- lib/lp/services/features/webapp.py
+++ lib/lp/services/features/webapp.py
@@ -7,16 +7,31 @@ __all__ = []
 
 __metaclass__ = type
 
+import canonical.config
+
 from lp.services.features.flags import (
     FeatureController,
     per_thread,
     )
 
 
+class ScopesFromRequest(object):
+    """Identify feature scopes based on request state."""
+
+    def __init__(self, request):
+        self._request = request
+
+    def lookup(self, scope_name):
+        parts = scope_name.split('.')
+        if len(parts) == 2 and parts[0] == 'server':
+            return canonical.config.config['launchpad']['is_' + parts[1]]
+
+
 def start_request(event):
     """Register FeatureController."""
     # TODO: determine all the interesting scopes, based on event.request
-    event.request.features = per_thread.features = FeatureController(['default'])
+    event.request.features = per_thread.features = FeatureController(
+        ScopesFromRequest(event.request).lookup)
 
 
 def end_request(event):
