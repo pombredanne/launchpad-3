1639e2287f911ced800f0a97170b642c200fefa1
Michael Hudson <michael.hudson@linaro.org>
2010-07-28 16:06:06 +1200

    make test simpler

--- lib/lp/vostok/tests/test_publisher.py
+++ lib/lp/vostok/tests/test_publisher.py
@@ -11,16 +11,9 @@ from canonical.config import config
 from canonical.testing.layers import FunctionalLayer
 
 from lp.testing import TestCase
+from lp.testing.publication import get_request_and_publication
 
 from lp.vostok.publisher import VostokLayer
-
-from zope.app.publication.requestpublicationregistry import factoryRegistry
-
-
-VOSTOK_ENVIRONMENT = {
-    'HTTP_HOST': config.vhost.vostok.hostname,
-    'REQUEST_METHOD': 'GET',
-    }
 
 
 class TestRegistration(TestCase):
@@ -28,20 +21,11 @@ class TestRegistration(TestCase):
 
     layer = FunctionalLayer
 
-    def test_publication_factory_is_registered(self):
-        # There is a vostok-specific request factory registered for the
-        # hostname configured for vostok.
-        factory = factoryRegistry.lookup(
-            "GET", "text/html", VOSTOK_ENVIRONMENT)
-        self.assertEqual('vostok', factory.vhost_name)
-
-    def test_vostok_request_provides_vostok_layer(self):
-        # The Request object constructed for requests to the vostok hostname
-        # provides VostokLayer.
-        factory = factoryRegistry.lookup(
-            "GET", "text/html", VOSTOK_ENVIRONMENT)
-        request_factory, publication = factory()
-        request = request_factory('', VOSTOK_ENVIRONMENT)
+    def test_code_request_provides_code_layer(self):
+        # The request constructed for requests to the code hostname provides
+        # CodeLayer.
+        request, publication = get_request_and_publication(
+            host=config.vhost.code.hostname)
         self.assertProvides(request, VostokLayer)
 
 
