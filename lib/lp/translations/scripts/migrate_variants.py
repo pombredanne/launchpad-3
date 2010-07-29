9178e3faa182d92e781a8d08fb1fee8622c903f8
Danilo Segan <danilo@canonical.com>
2010-07-29 11:59:18 +0200

    Fix copyright years.

--- lib/lp/translations/scripts/migrate_variants.py
+++ lib/lp/translations/scripts/migrate_variants.py
@@ -1,4 +1,4 @@
-# Copyright 2009 Canonical Ltd.  This software is licensed under the
+# Copyright 2010 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
 """Remove 'variant' usage in POFiles and TranslationMessages."""
