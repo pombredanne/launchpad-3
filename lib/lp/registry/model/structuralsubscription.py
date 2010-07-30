7895925d457f7091debddb548a04de99ee4caf16
Brian Murray <brian@canonical.com>
2010-07-30 13:48:03 -0700

    create helper method userCanAlterBugSubscription for the view

--- lib/lp/registry/model/structuralsubscription.py
+++ lib/lp/registry/model/structuralsubscription.py
@@ -179,6 +179,22 @@ class StructuralSubscriptionTargetMixin:
                 subscribed_by=subscribed_by,
                 **self._target_args)
 
+    def userCanAlterBugSubscription(self, subscriber, subscribed_by):
+        """See `IStructuralSubscriptionTarget`."""
+
+        admins = getUtility(ILaunchpadCelebrities).admin
+        # If the object to be structurally subscribed to for bug
+        # notifications is a distribution and that distribution has a
+        # bug supervisor then only the bug supervisor or a member of
+        # that team or, of course, admins, can subscribe someone to it.
+        if IDistribution.providedBy(self) and (
+            self.bug_supervisor is not None
+            and subscriber != self.bug_supervisor
+            and not subscriber.inTeam(self.bug_supervisor)
+            and not subscribed_by.inTeam(admins)):
+            return False
+        return True
+
     def addBugSubscription(self, subscriber, subscribed_by):
         """See `IStructuralSubscriptionTarget`."""
         # This is a helper method for creating a structural
@@ -186,14 +202,10 @@ class StructuralSubscriptionTargetMixin:
         # bug notification level. It is useful so long as
         # subscriptions are mainly used to implement bug contacts.
 
-        admins = getUtility(ILaunchpadCelebrities).admin
-
-        if IDistribution.providedBy(self) and (
-           subscriber != self.bug_supervisor
-           and self.bug_supervisor is not None
-           and not subscribed_by.inTeam(admins)):
+        if not self.userCanAlterBugSubscription(subscriber, subscribed_by):
             raise UserCannotSubscribePerson(
-                'Only the bug supervisor can subscribe to distribution bugs.')
+                '%s does not have permission to subscribe %s' % (
+                    subscribed_by.name, subscriber.name))
 
         sub = self.addSubscription(subscriber, subscribed_by)
         sub.bug_notification_level = BugNotificationLevel.COMMENTS
