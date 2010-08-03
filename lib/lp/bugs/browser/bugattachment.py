63184cf2fc17023daa455dbd196a37fad9f9e529
Abel Deuring <abel.deuring@canonical.com>
2010-08-03 19:20:07 +0200

    change the bug attachment download URL shown in a otification when an attachment is deleted.

--- lib/lp/bugs/browser/bugattachment.py
+++ lib/lp/bugs/browser/bugattachment.py
@@ -18,7 +18,8 @@ from zope.interface import implements
 from zope.component import getUtility
 from zope.contenttype import guess_content_type
 
-from canonical.launchpad.browser.librarian import FileNavigationMixin
+from canonical.launchpad.browser.librarian import (
+    FileNavigationMixin, ProxiedLibraryFileAlias)
 from canonical.launchpad.webapp import (
     canonical_url, custom_widget, GetitemNavigation, Navigation)
 from canonical.launchpad.interfaces.librarian import ILibraryFileAliasSet
@@ -154,9 +155,11 @@ class BugAttachmentEditView(LaunchpadFormView, BugAttachmentContentCheck):
 
     @action('Delete Attachment', name='delete')
     def delete_action(self, action, data):
+        libraryfile_url = ProxiedLibraryFileAlias(
+            self.context.libraryfile, self.context).http_url
         self.request.response.addInfoNotification(structured(
             'Attachment "<a href="%(url)s">%(name)s</a>" has been deleted.',
-            url=self.context.libraryfile.http_url, name=self.context.title))
+            url=libraryfile_url, name=self.context.title))
         self.context.removeFromBug(user=self.user)
 
     def updateContentType(self, new_content_type):
