8fa16e2eb1f86a2e670fc5fed4ea4dfd6f5c28de
William Grant <me@williamgrant.id.au>
2010-08-06 07:12:36 +1000

    Remove missed checkSlaveArchitecture references.

--- lib/lp/soyuz/tests/soyuzbuilddhelpers.py
+++ lib/lp/soyuz/tests/soyuzbuilddhelpers.py
@@ -70,9 +70,6 @@ class MockBuilder:
     def checkSlaveAlive(self):
         pass
 
-    def checkSlaveArchitecture(self):
-        pass
-
     def rescueIfLost(self, logger=None):
         rescueBuilderIfLost(self, logger)
 
