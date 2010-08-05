099a2131c219b352fbd4b87103d105340584ec20
j.c.sackett <jonathan.sackett@canonical.com>
2010-08-05 14:35:51 -0400

    Disabled needs_linking in the distoseries view. * needs_linking returns none. * test_needs_linking updated to assert that needs_linking in fact returns None.

--- lib/lp/registry/browser/distroseries.py
+++ lib/lp/registry/browser/distroseries.py
@@ -332,7 +332,8 @@ class DistroSeriesView(MilestoneOverlayMixin):
     @cachedproperty
     def needs_linking(self):
         """Return a list of 10 packages most in need of upstream linking."""
-        return self.context.getPrioritizedUnlinkedSourcePackages()[:10]
+        #return self.context.getPrioritizedUnlinkedSourcePackages()[:10]
+        return None
 
     milestone_can_release = False
 
