1a219b99d2d9eea4dac5dd23cfa747501b3083ee
Michael Hudson <michael.hudson@canonical.com>
2010-08-06 14:24:03 +1200

    rewrite navigation tests to use test_traverse

--- lib/lp/vostok/tests/test_navigation.py
+++ lib/lp/vostok/tests/test_navigation.py
@@ -5,43 +5,39 @@
 
 __metaclass__ = type
 
-from zope.component import getMultiAdapter
-from zope.publisher.interfaces.browser import IBrowserPublisher
+from zope.publisher.interfaces import NotFound
+from zope.security.proxy import removeSecurityProxy
 
-from canonical.launchpad.webapp import canonical_url
-from canonical.launchpad.webapp.interfaces import NotFoundError
-from canonical.launchpad.webapp.servers import LaunchpadTestRequest
+from canonical.launchpad.webapp import urlparse
 from canonical.testing.layers import DatabaseFunctionalLayer
 
 from lp.testing import TestCaseWithFactory
-from lp.vostok.publisher import VostokRootNavigation, VostokRoot
+from lp.testing.publication import test_traverse
 
 
 class TestRootNavigation(TestCaseWithFactory):
 
     layer = DatabaseFunctionalLayer
 
-    @property
-    def _navigation(self):
-        return getMultiAdapter(
-            (VostokRoot(), LaunchpadTestRequest()), IBrowserPublisher,
-            name='')
-
-    def test_use_VostokRootNavigation(self):
-        self.assertIsInstance(self._navigation, VostokRootNavigation)
-
     def test_traverse_to_distributions(self):
+        # We can traverse to a distribution by name from the vostok
+        # root.
         distro = self.factory.makeDistribution()
-        traversed = self._navigation.traverse(distro.name)
-        self.assertEqual(distro, traversed)
+        obj, view, request = test_traverse('http://vostok.dev/' + distro.name)
+        self.assertEqual(distro, obj)
 
     def test_traverse_to_distribution_aliases(self):
         # When we traverse to a distribution using one of its aliases, we're
-        # redirected to the distribution's page.
+        # redirected to the distribution's page on the vostok vhost.
         distro = self.factory.makeDistribution(aliases=['f00'])
-        redirection_view = self._navigation.traverse('f00')
-        self.assertEqual(canonical_url(distro), redirection_view.target)
+        obj, view, request = test_traverse('http://vostok.dev/f00')
+        naked_view = removeSecurityProxy(view)
+        parse_result = urlparse(naked_view.target)
+        self.assertEqual('vostok.dev', parse_result.netloc)
+        self.assertEqual('/' + distro.name, parse_result.path)
 
     def test_can_not_traverse_to_projects(self):
+        # 
         path = self.factory.makeProject().name
-        self.assertRaises(NotFoundError, self._navigation.traverse, path)
+        self.assertRaises(
+            NotFound, test_traverse, 'http://vostok.dev/' + path)
