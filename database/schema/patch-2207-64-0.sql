0afe7208ace3a313e177d5c54a057a78a57e7ffc
Stuart Bishop <stuart.bishop@canonical.com>
2010-08-10 12:49:16 +0700

    Revert ArchiveJob -> DistroSeriesJob rename, as there is code using ArchiveJob

--- database/schema/patch-2207-64-0.sql
+++ database/schema/patch-2207-64-0.sql
@@ -6,26 +6,22 @@ SET client_min_messages=ERROR;
 -- The schema patch required for adding archive jobs, the first being
 -- creation of copy archives.
 
--- The `DistroSeriesJob` table captures the data required for an archive job.
+-- The `ArchiveJob` table captures the data required for an archive job.
 
-CREATE TABLE DistroSeriesJob (
+CREATE TABLE ArchiveJob (
     id serial PRIMARY KEY,
     -- FK to the `Job` record with the "generic" data about this archive
     -- job.
-    job integer NOT NULL CONSTRAINT distroseries__job__fk
-        REFERENCES job ON DELETE CASCADE,
+    job integer NOT NULL CONSTRAINT archivejob__job__fk REFERENCES job ON DELETE CASCADE,
     -- FK to the associated `Archive` record.
-    distroseries integer NOT NULL
-        CONSTRAINT distroseriesjob__archive__fk REFERENCES DistroSeries,
+    archive integer NOT NULL CONSTRAINT archivejob__archive__fk REFERENCES archive,
     -- The particular type of archive job
     job_type integer NOT NULL,
     -- JSON data for use by the job
     json_data text
 );
 
-ALTER TABLE DistroSeriesJob ADD CONSTRAINT distroseries__job__key
-    UNIQUE (job);
-CREATE INDEX distroseriesjob__distroseries__job_type__idx
-    ON DistroSeriesJob(distroseries, job_type);
+ALTER TABLE ArchiveJob ADD CONSTRAINT archivejob__job__key UNIQUE (job);
+CREATE INDEX archivejob__archive__job_type__idx ON ArchiveJob(archive, job_type);
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2207, 64, 0);
