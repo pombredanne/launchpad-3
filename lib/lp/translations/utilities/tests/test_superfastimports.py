bb5f6c231ce23c78363f11dca4d7048528b2cd23
Henning Eggers <henning@canonical.com>
2010-08-10 16:37:24 +0200

    Use makeSuggestion in test_superfastimports.py.

--- lib/lp/translations/utilities/tests/test_superfastimports.py
+++ lib/lp/translations/utilities/tests/test_superfastimports.py
@@ -66,8 +66,7 @@ class TestSuperFastImports(TestCaseWithFactory):
     def test_inactive_messages(self):
         # Make sure non-current messages (i.e. suggestions) are
         # not cached in ExistingPOFileInDatabase.
-        inactive_message = self.factory.makeTranslationMessage(
-            pofile=self.pofile, suggestion=True)
+        inactive_message = self.factory.makeSuggestion(pofile=self.pofile)
         cached_file = ExistingPOFileInDatabase(self.pofile)
         message_data = self.getTranslationMessageData(inactive_message)
         self.assertFalse(
