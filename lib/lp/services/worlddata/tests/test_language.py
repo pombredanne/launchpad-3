63972c089435b899efbcf1cbeb45ddba47ac3f3a
Jeroen Vermeulen <jeroen.vermeulen@canonical.com>
2010-08-10 14:02:08 +0700

    Move generic "guessed" plural forms into Language.

--- lib/lp/services/worlddata/tests/test_language.py
+++ lib/lp/services/worlddata/tests/test_language.py
@@ -3,7 +3,9 @@
 
 __metaclass__ = type
 
-from canonical.testing import FunctionalLayer
+from zope.security.proxy import removeSecurityProxy
+
+from canonical.testing import DatabaseFunctionalLayer
 from lazr.lifecycle.interfaces import IDoNotSnapshot
 from lp.services.worlddata.interfaces.language import ILanguage
 from lp.testing import TestCaseWithFactory
@@ -12,10 +14,20 @@ from lp.testing import TestCaseWithFactory
 class TestLanguageWebservice(TestCaseWithFactory):
     """Test Language web service API."""
 
-    layer = FunctionalLayer
+    layer = DatabaseFunctionalLayer
 
     def test_translators(self):
         self.failUnless(
             IDoNotSnapshot.providedBy(ILanguage['translators']),
             "ILanguage.translators should not be included in snapshots, "
             "see bug 553093.")
+
+    def test_guessed_pluralforms_guesses(self):
+        language = self.factory.makeLanguage()
+        self.assertIs(None, language.pluralforms)
+        self.assertEqual(2, language.guessed_pluralforms)
+
+    def test_guessed_pluralforms_knows(self):
+        language = self.factory.makeLanguage()
+        removeSecurityProxy(language).pluralforms = 3
+        self.assertEqual(language.pluralforms, language.guessed_pluralforms)
