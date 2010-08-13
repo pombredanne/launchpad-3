61197e6abeeca5ac588cdf674aea0953a5a879e8
Guilherme Salgado <salgado@canonical.com>
2010-08-13 12:33:40 -0300

    merge two tests and fix the navigation declaration for IDistribution on the vostok layer

--- lib/lp/vostok/browser/tests/test_navigation.py
+++ lib/lp/vostok/browser/tests/test_navigation.py
@@ -6,17 +6,43 @@
 __metaclass__ = type
 
 from zope.publisher.interfaces import NotFound
+from zope.security.proxy import removeSecurityProxy
 
-from canonical.launchpad.webapp import canonical_url
+from canonical.launchpad.webapp import canonical_url, urlparse
 from canonical.testing.layers import DatabaseFunctionalLayer
 
 from lp.testing import TestCaseWithFactory
 from lp.testing.publication import test_traverse
 
 
-# XXX: Should move the contents of lp/vostok/tests/test_navigation.py in here
-# before merging.  Haven't done that yet because that file is not in trunk
-# yet.
+class TestRootNavigation(TestCaseWithFactory):
+
+    layer = DatabaseFunctionalLayer
+
+    def test_traverse_to_distributions(self):
+        # We can traverse to a distribution by name from the vostok
+        # root.
+        distro = self.factory.makeDistribution()
+        obj, view, request = test_traverse('http://vostok.dev/' + distro.name)
+        self.assertEqual(distro, obj)
+
+    def test_traverse_to_distribution_aliases(self):
+        # When we traverse to a distribution using one of its aliases, we're
+        # redirected to the distribution's page on the vostok vhost.
+        distro = self.factory.makeDistribution(aliases=['f00'])
+        obj, view, request = test_traverse('http://vostok.dev/f00')
+        naked_view = removeSecurityProxy(view)
+        parse_result = urlparse(naked_view.target)
+        self.assertEqual('vostok.dev', parse_result.netloc)
+        self.assertEqual('/' + distro.name, parse_result.path)
+
+    def test_can_not_traverse_to_projects(self):
+        # We cannot traverse to a project from the vostok root.
+        path = self.factory.makeProject().name
+        self.assertRaises(
+            NotFound, test_traverse, 'http://vostok.dev/' + path)
+
+
 class TestDistributionNavigation(TestCaseWithFactory):
 
     layer = DatabaseFunctionalLayer
