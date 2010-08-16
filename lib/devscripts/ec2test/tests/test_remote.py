eb5b9dd413cc15b2732529719f5e1b088238aec2
Jonathan Lange <jml@canonical.com>
2010-08-16 15:57:36 +0100

    Unit tests for FlagFallStream.

--- lib/devscripts/ec2test/tests/test_remote.py
+++ lib/devscripts/ec2test/tests/test_remote.py
@@ -11,7 +11,40 @@ import unittest
 
 from testtools import TestCase
 
-from devscripts.ec2test.remote import SummaryResult
+from devscripts.ec2test.remote import FlagFallStream, SummaryResult
+
+
+class TestFlagFallStream(TestCase):
+    """Tests for `FlagFallStream`."""
+
+    def test_doesnt_write_before_flag(self):
+        # A FlagFallStream does not forward any writes before it sees the
+        # 'flag'.
+        stream = StringIO()
+        flag = self.getUniqueString('flag')
+        flagfall = FlagFallStream(stream, flag)
+        flagfall.write('foo')
+        flagfall.flush()
+        self.assertEqual('', stream.getvalue())
+
+    def test_writes_after_flag(self):
+        # After a FlagFallStream sees the flag, it forwards all writes.
+        stream = StringIO()
+        flag = self.getUniqueString('flag')
+        flagfall = FlagFallStream(stream, flag)
+        flagfall.write('foo')
+        flagfall.write(flag)
+        flagfall.write('bar')
+        self.assertEqual('%sbar' % (flag,), stream.getvalue())
+
+    def test_mixed_write(self):
+        # If a single call to write has pre-flagfall and post-flagfall data in
+        # it, then only the post-flagfall data is forwarded to the stream.
+        stream = StringIO()
+        flag = self.getUniqueString('flag')
+        flagfall = FlagFallStream(stream, flag)
+        flagfall.write('foo%sbar' % (flag,))
+        self.assertEqual('%sbar' % (flag,), stream.getvalue())
 
 
 class TestSummaryResult(TestCase):
