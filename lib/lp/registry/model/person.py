ff13b35a92c5f5ad663c9dc347be9ef4b010c077
j.c.sackett <jonathan.sackett@canonical.com>
2010-08-16 13:54:03 -0400

    Stormified Person.team_memberships

--- lib/lp/registry/model/person.py
+++ lib/lp/registry/model/person.py
@@ -1560,13 +1560,13 @@ class Person(
     @property
     def team_memberships(self):
         """See `IPerson`."""
-        return TeamMembership.select("""
-            TeamMembership.person = %s AND status in %s AND
-            Person.id = TeamMembership.team
-            """ % sqlvalues(self.id, [TeamMembershipStatus.APPROVED,
-                                      TeamMembershipStatus.ADMIN]),
-            clauseTables=['Person'],
-            orderBy=Person.sortingColumns)
+        Team = ClassAlias(Person, "Team")
+        store = Store.of(self)
+        return store.find(TeamMembership,
+            And(TeamMembership.personID == self.id,
+                Team.id == TeamMembership.teamID,
+                TeamMembership.status.is_in(
+                [TeamMembershipStatus.APPROVED,TeamMembershipStatus.ADMIN])))
 
     def _getMappedParticipantsLocations(self, limit=None):
         """See `IPersonViewRestricted`."""
