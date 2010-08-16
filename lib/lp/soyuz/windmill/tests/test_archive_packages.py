a3df229615e56494521fa94def659c44312196cf
Michael Nelson <michael.nelson@canonical.com>
2010-08-16 11:53:55 +0200

    Added test for extra info for sources on packages page.

---
+++ lib/lp/soyuz/windmill/tests/test_archive_packages.py
@@ -0,0 +1,27 @@
+# Copyright 2010 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+from canonical.launchpad.windmill.testing import constants
+from lp.soyuz.windmill.testing import SoyuzWindmillLayer
+from lp.testing import WindmillTestCase
+
+
+class TestArchivePackagesSourcesExtra(WindmillTestCase):
+    """Each listed source package can be expanded for extra information."""
+
+    layer = SoyuzWindmillLayer
+
+    def test_sources_extra_available(self):
+        """A successful request for the extra info updates the display."""
+
+        self.client.open(
+            url='http://launchpad.dev:8085/~cprov/+archive/ppa/+packages')
+        self.client.waits.forPageLoad(timeout=constants.PAGE_LOAD)
+
+        self.client.click(id="pub29-expander")
+
+        self.client.waits.forElement(
+            link=u'i386', timeout=constants.FOR_ELEMENT)
+
+
+
