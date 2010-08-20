52ce73fbe27fe2c2c2cc8026eaf8ed2215e0d347
Julian Edwards <julian.edwards@canonical.com>
2010-08-20 17:18:11 +0100

    Add a failing test

--- lib/lp/buildmaster/tests/test_builder.py
+++ lib/lp/buildmaster/tests/test_builder.py
@@ -17,6 +17,7 @@ from lp.buildmaster.interfaces.builder import IBuilderSet
 from lp.buildmaster.interfaces.buildfarmjobbehavior import (
     IBuildFarmJobBehavior)
 from lp.buildmaster.interfaces.buildqueue import IBuildQueueSet
+from lp.buildmaster.model.builder import rescueBuilderIfLost
 from lp.buildmaster.model.buildfarmjobbehavior import IdleBuildBehavior
 from lp.buildmaster.model.buildqueue import BuildQueue
 from lp.soyuz.interfaces.archive import ArchivePurpose
@@ -24,6 +25,10 @@ from lp.soyuz.interfaces.binarypackagebuild import IBinaryPackageBuildSet
 from lp.soyuz.interfaces.publishing import PackagePublishingStatus
 from lp.soyuz.model.binarypackagebuildbehavior import (
     BinaryPackageBuildBehavior)
+from lp.soyuz.tests.soyuzbuilddhelpers import (
+    AbortedSlave,
+    MockBuilder,
+    )
 from lp.soyuz.tests.test_publishing import SoyuzTestPublisher
 from lp.testing import TestCaseWithFactory
 from lp.testing.fakemethod import FakeMethod
@@ -83,6 +88,24 @@ class TestBuilder(TestCaseWithFactory):
         # builder.updateStatus should never call handleTimeout() for a
         # single EINTR.
         self.assertEqual(0, builder.handleTimeout.call_count)
+
+
+class Test_rescueBuilderIfLost(TestCaseWithFactory):
+    """Tests for lp.buildmaster.model.builder.rescueBuilderIfLost."""
+
+    layer = LaunchpadZopelessLayer
+
+    def test_recovery_of_aborted_slave(self):
+        # If a slave is in the ABORTED state, rescueBuilderIfLost should
+        # clean it.
+        aborted_slave = AbortedSlave()
+        # The slave's clean() method is normally an XMLRPC call, so we
+        # can just stub it out and check that it got called.
+        aborted_slave.clean = FakeMethod()
+        builder = MockBuilder("mock_builder", aborted_slave)
+        rescueBuilderIfLost(builder)
+
+        self.assertEqual(1, aborted_slave.clean.call_count)
 
 
 class TestFindBuildCandidateBase(TestCaseWithFactory):
