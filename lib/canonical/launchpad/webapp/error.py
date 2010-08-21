3482eb5b7d7a048dea5bf22ccdd5d5290b651e8c
Andrea Corbellini <corbellini.andrea@gmail.com>
2010-08-21 11:57:45 +0200

    Use the fonts recommended by the Canonical Design guidelines for websites.

--- lib/canonical/launchpad/webapp/error.py
+++ lib/canonical/launchpad/webapp/error.py
@@ -114,8 +114,9 @@ class SystemErrorView(LaunchpadView):
     def inside_div(self, html):
         """Returns the given HTML inside a div of an appropriate class."""
 
-        return ('<div class="highlighted" '
-                'style="font-family: monospace; font-size: smaller;">'
+        return ('<div class="highlighted" style="'
+                "font-family: 'UbuntuBeta Mono', 'Ubuntu', monospace;"
+                ' font-size: smaller;">'
                 '%s'
                 '</div>') % html
 
