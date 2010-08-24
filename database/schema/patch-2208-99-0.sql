8e3bdb7ec4e727bfd94bfd476a9ae4fcce2f1f65
Michael Nelson <michael.nelson@canonical.com>
2010-08-24 16:55:03 +0200

    Added package_diff FK and difference_type columns.

--- database/schema/patch-2208-99-0.sql
+++ database/schema/patch-2208-99-0.sql
@@ -4,11 +4,14 @@ CREATE TABLE DistroSeriesDifference (
     id serial PRIMARY KEY,
     derived_series integer NOT NULL CONSTRAINT distroseriesdifference__derived_series__fk REFERENCES distroseries,
     source_package_name integer NOT NULL CONSTRAINT distroseriesdifference__source_package_name__fk REFERENCES sourcepackagename,
+    last_package_diff integer CONSTRAINT distroseriesdifference__last_package_diff__fk REFERENCES packagediff,
     activity_log text,
-    status integer NOT NULL
+    status integer NOT NULL,
+    difference_type integer NOT NULL
 );
 CREATE INDEX distroseriesdifference__derived_series__idx ON distroseriesdifference(derived_series);
 CREATE INDEX distroseriesdifference__source_package_name__idx ON distroseriesdifference(source_package_name);
 CREATE INDEX distroseriesdifference__status__idx ON distroseriesdifference(status);
+CREATE INDEX distroseriesdifference__difference_type__idx ON distroseriesdifference(difference_type);
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2208, 99, 0);
