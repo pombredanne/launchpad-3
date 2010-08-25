981de4be749b83170d02b73638c3957fb10a934e
Julian Edwards <julian.edwards@canonical.com>
2010-08-25 16:01:37 +0100

    relocate a pythonpath import

--- scripts/gina.py
+++ scripts/gina.py
@@ -19,6 +19,8 @@ The callstack is essentially:
 
 __metaclass__ = type
 
+import _pythonpath
+
 # Yes we do need to import this here to stop circular imports from the
 # abomination piece of crap that is
 # canonical.launchpad.interfaces.__init__.py that imports the world.
@@ -27,8 +29,6 @@ import canonical.launchpad.interfaces
 
 # Set to non-zero if you'd like to be warned every so often
 COUNTDOWN = 0
-
-import _pythonpath
 
 from optparse import OptionParser
 import os
