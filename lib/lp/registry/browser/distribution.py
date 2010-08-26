73c8d683666a0da145c37b3c18217748b8be0004
Curtis Hovey <curtis.hovey@canonical.com>
2010-08-26 10:08:37 -0400

    Long lists -> veritical lists.

--- lib/lp/registry/browser/distribution.py
+++ lib/lp/registry/browser/distribution.py
@@ -199,8 +199,14 @@ class DistributionFacets(QuestionTargetFacetMixin, StandardLaunchpadFacets):
 
     usedfor = IDistribution
 
-    enable_only = ['overview', 'branches', 'bugs', 'answers',
-                   'specifications', 'translations']
+    enable_only = [
+        'overview',
+        'branches',
+        'bugs',
+        'answers',
+        'specifications',
+        'translations',
+        ]
 
     def specifications(self):
         text = 'Blueprints'
@@ -313,14 +319,30 @@ class DistributionOverviewMenu(ApplicationMenu, DistributionLinksMixin):
 
     usedfor = IDistribution
     facet = 'overview'
-    links = ['edit', 'branding', 'driver', 'search', 'members',
-             'mirror_admin', 'reassign', 'addseries', 'series', 'milestones',
-             'top_contributors',
-             'builds', 'cdimage_mirrors', 'archive_mirrors',
-             'pending_review_mirrors', 'disabled_mirrors',
-             'unofficial_mirrors', 'newmirror', 'announce', 'announcements',
-             'ppas', 'configure_answers',
-             ]
+    links = [
+        'edit',
+        'branding',
+        'driver',
+        'search',
+        'members',
+        'mirror_admin',
+        'reassign',
+        'addseries',
+        'series',
+        'milestones',
+        'top_contributors',
+        'builds',
+        'cdimage_mirrors',
+        'archive_mirrors',
+        'pending_review_mirrors',
+        'disabled_mirrors',
+        'unofficial_mirrors',
+        'newmirror',
+        'announce',
+        'announcements',
+        'ppas',
+        'configure_answers',
+        ]
 
     @enabled_with_permission('launchpad.Edit')
     def branding(self):
@@ -732,10 +754,19 @@ class DistributionAddView(LaunchpadFormView):
 
     schema = IDistribution
     label = "Register a new distribution"
-    field_names = ["name", "displayname", "title", "summary", "description",
-                   "domainname", "members",
-                   "official_malone", "official_blueprints",
-                   "official_rosetta", "official_answers"]
+    field_names = [
+        "name",
+        "displayname",
+        "title",
+        "summary",
+        "description",
+        "domainname",
+        "members",
+        "official_malone",
+        "official_blueprints",
+        "official_rosetta",
+        "official_answers",
+        ]
 
     @property
     def page_title(self):
@@ -766,12 +797,23 @@ class DistributionAddView(LaunchpadFormView):
 class DistributionEditView(RegistryEditFormView):
 
     schema = IDistribution
-    field_names = ['displayname', 'title', 'summary', 'description',
-                   'bug_reporting_guidelines', 'bug_reported_acknowledgement',
-                   'icon', 'logo', 'mugshot',
-                   'official_malone', 'enable_bug_expiration',
-                   'official_blueprints', 'official_rosetta',
-                   'official_answers', 'translation_focus', ]
+    field_names = [
+        'displayname',
+        'title',
+        'summary',
+        'description',
+        'bug_reporting_guidelines',
+        'bug_reported_acknowledgement',
+        'icon',
+        'logo',
+        'mugshot',
+        'official_malone',
+        'enable_bug_expiration',
+        'official_blueprints',
+        'official_rosetta',
+        'official_answers',
+        'translation_focus',
+        ]
 
     custom_widget('icon', ImageChangeWidget, ImageChangeWidget.EDIT_STYLE)
     custom_widget('logo', ImageChangeWidget, ImageChangeWidget.EDIT_STYLE)
