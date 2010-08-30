b21c727be2888666d6f6a323abdba3f849bc40dc
Michael Nelson <michael.nelson@canonical.com>
2010-08-30 09:48:22 +0200

    Renamed last_package_diff->package_diff and updated unique constraint on messages join table.

--- database/schema/patch-2208-07-0.sql
+++ database/schema/patch-2208-07-0.sql
@@ -4,7 +4,7 @@ CREATE TABLE DistroSeriesDifference (
     id serial PRIMARY KEY,
     derived_series integer NOT NULL CONSTRAINT distroseriesdifference__derived_series__fk REFERENCES distroseries,
     source_package_name integer NOT NULL CONSTRAINT distroseriesdifference__source_package_name__fk REFERENCES sourcepackagename,
-    last_package_diff integer CONSTRAINT distroseriesdifference__last_package_diff__fk REFERENCES packagediff,
+    package_diff integer CONSTRAINT distroseriesdifference__package_diff__fk REFERENCES packagediff,
     status integer NOT NULL,
     difference_type integer NOT NULL
 );
@@ -12,13 +12,12 @@ CREATE INDEX distroseriesdifference__derived_series__idx ON distroseriesdifferen
 CREATE INDEX distroseriesdifference__source_package_name__idx ON distroseriesdifference(source_package_name);
 CREATE INDEX distroseriesdifference__status__idx ON distroseriesdifference(status);
 CREATE INDEX distroseriesdifference__difference_type__idx ON distroseriesdifference(difference_type);
-CREATE INDEX distroseriesdifference__last_package_diff__idx ON distroseriesdifference(last_package_diff);
+CREATE INDEX distroseriesdifference__package_diff__idx ON distroseriesdifference(package_diff);
 
 CREATE TABLE DistroSeriesDifferenceMessage(
     id serial PRIMARY KEY,
     distro_series_difference integer NOT NULL CONSTRAINT distroseriesdifferencemessage__distro_series_difference__fk REFERENCES distroseriesdifference,
-    message integer NOT NULL CONSTRAINT distroseriesdifferencemessage__message__fk REFERENCES message,
-    UNIQUE (distro_series_difference, message)
+    message integer NOT NULL CONSTRAINT distroseriesdifferencemessage__message__fk REFERENCES message UNIQUE
 );
 CREATE INDEX distroseriesdifferencemessage__distroseriesdifference__idx ON distroseriesdifferencemessage(distro_series_difference);
 CREATE INDEX distroseriesdifferencemessage__message__idx ON distroseriesdifferencemessage(message);
