5eada47a910a0de7e20dab1562fb9793cae44e0d
Edwin Grubbs <edwin.grubbs@canonical.com>
2010-08-30 23:11:22 -0500

    Added tests.

--- lib/lp/registry/browser/tests/test_distroseries_views.py
+++ lib/lp/registry/browser/tests/test_distroseries_views.py
@@ -3,13 +3,13 @@
 
 __metaclass__ = type
 
-import unittest
-
 from storm.zope.interfaces import IResultSet
 from zope.component import getUtility
 from zope.security.proxy import removeSecurityProxy
 
+from canonical.config import config
 from canonical.launchpad.interfaces.launchpad import ILaunchpadCelebrities
+from canonical.launchpad.webapp.batching import BatchNavigator
 from canonical.testing import LaunchpadZopelessLayer
 from lp.testing import TestCaseWithFactory
 from lp.testing.views import create_initialized_view
@@ -43,6 +43,26 @@ class TestDistroSeriesView(TestCaseWithFactory):
         view = create_initialized_view(distroseries, '+index')
         self.assertEqual(view.needs_linking, None)
 
-
-def test_suite():
-    return unittest.TestLoader().loadTestsFromName(__name__)
+    def test_milestone_batch_navigator(self):
+        ubuntu = getUtility(ILaunchpadCelebrities).ubuntu
+        distroseries = self.factory.makeDistroSeries(distribution=ubuntu)
+        for name in ('a', 'b', 'c', 'd'):
+            distroseries.newMilestone(name)
+        view = create_initialized_view(distroseries, name='+index')
+        config.push('default-batch-size', """
+        [launchpad]
+        default_batch_size: 2
+        """)
+        self.assert_(
+            isinstance(view.milestone_batch_navigator, BatchNavigator),
+            'milestone_batch_navigator is not a BatchNavigator object: %r'
+            % view.milestone_batch_navigator)
+        self.assertEqual(4, view.milestone_batch_navigator.batch.total())
+        expected = [
+            '0.9.2',
+            '0.9.1',
+            ]
+        milestone_names = [
+            item.name
+            for item in view.milestone_batch_navigator.currentBatch()]
+        config.pop('default-batch-size')
