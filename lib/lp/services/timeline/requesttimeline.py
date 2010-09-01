579081cbcb27a732a945c054922bf9842c8be365
Robert Collins <robert@canonical.com>
2010-09-01 10:43:16 +1000

    Adjust the requesttimeline API to be a shallow proxy for lib/canonical/launchpad/webapp/adapter.py rather than using request annoations.

--- lib/lp/services/timeline/requesttimeline.py
+++ lib/lp/services/timeline/requesttimeline.py
@@ -3,10 +3,12 @@
 
 """Manage a Timeline for a request."""
 
-__all__ = ['get_request_timeline']
+__all__ = ['get_request_timeline', 'set_request_timeline']
 
 __metaclass__ = type
 
+# XXX: undesirable but pragmatic. bug=623199 RBC 20100901
+from canonical.launchpad import webapp
 from timeline import Timeline
 
 
@@ -18,4 +20,23 @@ def get_request_timeline(request):
     :param request: A zope/launchpad request object.
     :return: A lp.services.timeline.timeline.Timeline object for the request.
     """
+    try:
+        return webapp.adapter._local.request_timeline
+    except AttributeError:
+        return set_request_timeline(request, Timeline())
+    # Disabled code path: bug 623199
     return request.annotations.setdefault('timeline', Timeline())
+
+
+def set_request_timeline(request, timeline):
+    """Explicitly set a tiemline for request.
+
+    This is used by code which wants to manually assemble a timeline.
+
+    :param request: A zope/launchpad request object.
+    :param timeline: A Timeline.
+    """
+    webapp.adapter._local.request_timeline = timeline
+    return timeline
+    # Disabled code path: bug 623199
+    request.annotations['timeline'] = timeline
