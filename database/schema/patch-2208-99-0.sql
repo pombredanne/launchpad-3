912ceb3b487d24ce613dba7a777b0412999b141a
Bryce Harrington <bryce.harrington@canonical.com>
2010-09-02 16:45:41 -0700

    Review by lifeless - add name constraints, clarify field comments

--- database/schema/patch-2208-99-0.sql
+++ database/schema/patch-2208-99-0.sql
@@ -7,6 +7,8 @@ CREATE TABLE BugTrackerComponentGroup (
     id serial PRIMARY KEY,
     name text NOT NULL,
     bug_tracker integer NOT NULL REFERENCES BugTracker
+
+    CONSTRAINT valid_name CHECK (valid_name(name))
 );
 
 CREATE INDEX bugtrackercomponentgroup__bugtracker__idx
@@ -15,19 +17,21 @@ CREATE INDEX bugtrackercomponentgroup__bugtracker__idx
 CREATE TABLE BugTrackerComponent (
     id serial PRIMARY KEY,
     name text NOT NULL,
-    is_active boolean DEFAULT True,
+    is_shown  boolean DEFAULT True,
     is_custom boolean DEFAULT True,
     component_group integer NOT NULL REFERENCES BugTrackerComponentGroup,
-    source_package integer REFERENCES DistributionSourcePackage,
+    distro_source_package integer REFERENCES DistributionSourcePackage,
+
+    CONSTRAINT valid_name CHECK (valid_name(name))
 
     -- There should be only one component per source package
     CONSTRAINT bugtrackercomponent__distributionsourcepackage__fk
-        FOREIGN KEY(source_package) REFERENCES DistributionSourcePackage
+        FOREIGN KEY(distro_source_package) REFERENCES DistributionSourcePackage
 );
 
 CREATE INDEX bugtrackercomponent__componentgroup__idx
     ON BugTrackerComponent(component_group);
 CREATE INDEX bugtrackercomponent__distributionsourcepackage__idx
-    ON BugTrackerComponent(source_package);
+    ON BugTrackerComponent(distro_source_package);
 
 INSERT INTO LaunchpadDatabaseRevision VALUES(2208, 99, 0);
