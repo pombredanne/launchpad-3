529e020468ff61adb491a145d1a3ed2cd7e91ab0
Henning Eggers <henning@canonical.com>
2010-09-13 15:26:27 +0200

    It's a dict.

--- lib/lp/translations/utilities/validate.py
+++ lib/lp/translations/utilities/validate.py
@@ -36,7 +36,7 @@ def validate_translation(original_singular, original_plural,
         msg.set_msgid_plural(original_plural)
         for form, translation in translations.items():
             msg.set_msgstr_plural(form, translation)
-    elif len(translation) > 0:
+    elif 0 in translations:
         msg.set_msgstr(translations[0])
     else:
         pass
