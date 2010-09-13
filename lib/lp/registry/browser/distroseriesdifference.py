af70dd2ad15a73a62ca42372e42c7445fea82778
Michael Nelson <michael.nelson@canonical.com>
2010-09-13 10:04:20 +0000

    DistroSeriesDifference view/template skeleton.

---
+++ lib/lp/registry/browser/distroseriesdifference.py
@@ -0,0 +1,15 @@
+# Copyright 2010 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Browser views for DistroSeriesDifferences."""
+
+__metaclass__ = type
+__all__ = [
+    'DistroSeriesDifferenceView',
+    ]
+
+from canonical.launchpad.webapp.publisher import LaunchpadView
+
+
+class DistroSeriesDifferenceView(LaunchpadView):
+    pass
