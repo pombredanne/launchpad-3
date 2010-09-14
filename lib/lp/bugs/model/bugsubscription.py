7ebadbb9df951886b2c354ce7e6201c792512996
Gavin Panella <gavin.panella@canonical.com>
2010-09-14 14:31:52 +0100

    Add BugSubscriptionFilterStatus.

--- lib/lp/bugs/model/bugsubscription.py
+++ lib/lp/bugs/model/bugsubscription.py
@@ -18,9 +18,13 @@ from zope.interface import implements
 
 from canonical.database.constants import UTC_NOW
 from canonical.database.datetimecol import UtcDateTimeCol
-from canonical.database.enumcol import EnumCol
+from canonical.database.enumcol import (
+    DBEnum,
+    EnumCol,
+    )
 from canonical.database.sqlbase import SQLBase
 from lp.bugs.interfaces.bugsubscription import IBugSubscription
+from lp.bugs.interfaces.bugtask import BugTaskStatus
 from lp.registry.enum import BugNotificationLevel
 from lp.registry.interfaces.person import validate_person
 
@@ -95,3 +99,15 @@ class BugSubscriptionFilter(Storm):
     other_parameters = Unicode("other_parameters")
 
     description = Unicode("description")
+
+
+class BugSubscriptionFilterStatus(Storm):
+
+    __storm_table__ = "BugSubscriptionFilterStatus"
+
+    id = Int(primary=True)
+
+    filter_id = Int("filter", allow_none=False)
+    filter = Reference(filter_id, "BugSubscriptionFilter.id")
+
+    status = DBEnum(enum=BugTaskStatus, allow_none=False)
--- lib/lp/bugs/model/tests/test_bugsubscription.py
+++ lib/lp/bugs/model/tests/test_bugsubscription.py
@@ -7,7 +7,11 @@ __metaclass__ = type
 
 from canonical.launchpad.interfaces.lpstorm import IStore
 from canonical.testing import DatabaseFunctionalLayer
-from lp.bugs.model.bugsubscription import BugSubscriptionFilter
+from lp.bugs.interfaces.bugtask import BugTaskStatus
+from lp.bugs.model.bugsubscription import (
+    BugSubscriptionFilter,
+    BugSubscriptionFilterStatus,
+    )
 from lp.testing import (
     login_person,
     TestCaseWithFactory,
@@ -65,3 +69,37 @@ class TestBugSubscriptionFilter(TestCaseWithFactory):
         self.assertIs(False, bug_subscription_filter.exclude_any_tags)
         self.assertIs(None, bug_subscription_filter.other_parameters)
         self.assertIs(None, bug_subscription_filter.description)
+
+
+class TestBugSubscriptionFilterStatus(TestCaseWithFactory):
+
+    layer = DatabaseFunctionalLayer
+
+    def setUp(self):
+        super(TestBugSubscriptionFilterStatus, self).setUp()
+        self.target = self.factory.makeProduct()
+        self.subscriber = self.target.owner
+        login_person(self.subscriber)
+        self.subscription = self.target.addBugSubscription(
+            self.subscriber, self.subscriber)
+        self.subscription_filter = BugSubscriptionFilter()
+        self.subscription_filter.structural_subscription = self.subscription
+
+    def test_basics(self):
+        """Test the basics of `BugSubscriptionFilterStatus` objects."""
+        # Create.
+        bug_sub_filter_status = BugSubscriptionFilterStatus()
+        bug_sub_filter_status.filter = self.subscription_filter
+        bug_sub_filter_status.status = BugTaskStatus.NEW
+        # Flush and reload.
+        IStore(bug_sub_filter_status).flush()
+        IStore(bug_sub_filter_status).reload(bug_sub_filter_status)
+        # Check.
+        self.assertIsNot(None, bug_sub_filter_status.id)
+        self.assertEqual(
+            self.subscription_filter.id,
+            bug_sub_filter_status.filter_id)
+        self.assertEqual(
+            self.subscription_filter,
+            bug_sub_filter_status.filter)
+        self.assertEqual(BugTaskStatus.NEW, bug_sub_filter_status.status)
