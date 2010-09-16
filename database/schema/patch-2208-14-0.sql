a4bbc228b7d569e130d5b902872db19344e3b595
Robert Collins <robert@canonical.com>
2010-09-16 17:00:26 +1000

    Stubs tweaks.

--- database/schema/patch-2208-14-0.sql
+++ database/schema/patch-2208-14-0.sql
@@ -3,9 +3,7 @@ SET client_min_messages=ERROR;
 -- Store the row index of bug messages so we don't have to calculate it all the time.
 ALTER TABLE BugMessage ADD COLUMN index integer;
 
--- BugMessage.indexes must be unique per bug (index can be added post-rollout
--- if its slow).
-CREATE UNIQUE INDEX bugmessage__bug_index_unique ON BugMessage (bug, index);
-
+-- BugMessage.indexes must be unique per bug.
+ALTER TABLE BugMessage ADD CONSTRAINT bugmessage__bug__index__key UNIQUE (bug, index);
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2208, 14, 0);
