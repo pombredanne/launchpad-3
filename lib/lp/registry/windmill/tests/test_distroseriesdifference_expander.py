8984f0090b18f8df9c37f8778002d3d0f33b133b
Michael Nelson <michael.nelson@canonical.com>
2010-09-16 16:02:30 +0200

    Got windmill test passing

--- lib/lp/registry/windmill/tests/test_distroseriesdifference_expander.py
+++ lib/lp/registry/windmill/tests/test_distroseriesdifference_expander.py
@@ -1,6 +1,8 @@
 # Copyright 2010 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
+import transaction
+
 from canonical.launchpad.webapp.publisher import canonical_url
 from canonical.launchpad.windmill.testing import constants
 from lp.registry.windmill.testing import RegistryWindmillLayer
@@ -25,12 +27,16 @@ class TestDistroSeriesDifferenceExtraJS(WindmillTestCase):
         self.diff = self.factory.makeDistroSeriesDifference(
             source_package_name_str="foo", versions=dict(
                 derived='1.15-2ubuntu1derilucid2', parent='1.17-1'))
+        transaction.commit()
 
-        self.package_diffs_url = canonical_url(
-            self.diff.derived_series, '+localpackagediffs')
+        self.package_diffs_url = (
+            canonical_url(self.diff.derived_series) + '/+localpackagediffs')
 
     def test_diff_extra_details_available(self):
         """A successful request for the extra info updates the display."""
-
-        self.client.open(self.package_diffs_url)
+        self.client.open(url=self.package_diffs_url)
         self.client.waits.forPageLoad(timeout=constants.PAGE_LOAD)
+        self.client.click(link=u'foo')
+        self.client.waits.forElement(
+            classname=u'diff-extra', timeout=constants.FOR_ELEMENT)
+
