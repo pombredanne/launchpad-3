7f3297c59def612eb47e6493f33e4a04d7774c0c
j.c.sackett <jonathan.sackett@canonical.com>
2010-09-20 17:45:49 -0400

    Updated distroseries to use distribution rather than IServiceUsage cast.

--- lib/lp/registry/model/distroseries.py
+++ lib/lp/registry/model/distroseries.py
@@ -268,18 +268,18 @@ class DistroSeries(SQLBase, BugTargetBase, HasSpecificationsMixin,
     @property
     def answers_usage(self):
         """See `IServiceUsage.`"""
-        return IServiceUsage(self).answers_usage
+        return self.product.answers_usage
 
     @property
     def blueprints_usage(self):
         """See `IServiceUsage.`"""
-        return IServiceUsage(self).blueprints_usage
+        return self.product.blueprints_usage
 
     @property
     def translations_usage(self):
         """See `IServiceUsage.`"""
         # If translations_usage is set for the Product, respect it.
-        usage = IServiceUsage(self).translations_usage
+        usage = self.product.translations_usage
         if usage != ServiceUsage.UNKNOWN:
             return usage
        
@@ -293,12 +293,12 @@ class DistroSeries(SQLBase, BugTargetBase, HasSpecificationsMixin,
     @property
     def codehosting_usage(self):
         """See `IServiceUsage.`"""
-        return IServiceUsage(self).codehosting_usage
+        return self.product.codehosting_usage
 
     @property
     def bug_tracking_usage(self):
         """See `IServiceUsage.`"""
-        return IServiceUsage(self).bug_tracking_usage
+        return self.product.bug_tracking_usage
 
     # DistroArchSeries lookup properties/methods.
     architectures = SQLMultipleJoin(
