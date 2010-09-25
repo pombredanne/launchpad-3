841316416494cf36c3d5ea52cbff08da67f79c79
Martin Pool <mbp@canonical.com>
2010-09-25 03:53:44 +0000

    Add crude and not connected edit form

--- lib/lp/services/features/browser/__init__.py
+++ lib/lp/services/features/browser/__init__.py
@@ -5,8 +5,12 @@
 
 
 from zope.interface import Interface
+from zope.schema import (
+    Text,
+    )
 
 from canonical.launchpad.webapp import (
+    action,
     LaunchpadFormView,
     )
 
@@ -16,15 +20,24 @@ from lp.services.features.model import (
 
 
 class IFeatureControlForm(Interface):
+    """Interface specifically for editing a text form of feature rules"""
 
     def __init__(self, context):
         self.context = context
+
+    feature_rules = Text(title=u"Feature rules")
 
 
 class FeatureControlView(LaunchpadFormView):
 
     schema = IFeatureControlForm
     page_title = label = 'Feature control'
+
+    @action(u"Change", name="change")
+    def change_action(self, action, data):
+        print "Change feature rules to:"
+        print self.request.get('feature_rules')
+        pass
 
     def rules_text(self):
         """Return all rules in an editable text form"""
