35b30ae2a0b668b79df28d48493226b8f88969b9
Ian Booth <ian.booth@canonical.com>
2010-09-30 12:24:40 +1000

    Working

--- lib/canonical/launchpad/browser/linkchecker.py
+++ lib/canonical/launchpad/browser/linkchecker.py
@@ -12,22 +12,85 @@ __all__ = [
     'LinkCheckerURL',
     ]
 
-from zope.interface import (
-    implements,
-    )
-
 from canonical.launchpad.interfaces.linkchecker import ILinkCheckerAPI
 from canonical.launchpad.webapp.interfaces import ICanonicalUrlData
+
+from lazr.restful.interfaces import IWebServiceClientRequest
+import simplejson
+from zope.app.form.interfaces import MissingInputError
+from zope.app.schema.vocabulary import IVocabularyFactory
+from zope.component import (
+    adapter,
+    getUtility,
+    )
+from zope.component.interfaces import ComponentLookupError
+from zope.interface import (
+    Attribute,
+    implementer,
+    implements,
+    Interface,
+    )
+from zope.security.interfaces import Unauthorized
+
+from canonical.launchpad.webapp.batching import BatchNavigator
+from canonical.launchpad.webapp.interfaces import NoCanonicalUrl
+from canonical.launchpad.webapp.publisher import canonical_url
+from canonical.launchpad.webapp.tales import ObjectImageDisplayAPI
+from canonical.launchpad.webapp.vocabulary import IHugeVocabulary
+from lp.app.errors import UnexpectedFormData
+from lp.code.interfaces.branch import IBranch
+from lp.registry.interfaces.person import IPerson
+from lp.registry.interfaces.sourcepackagename import ISourcePackageName
+from lp.registry.model.sourcepackagename import getSourcePackageDescriptions
 
 class LinkCheckerAPI:
     """See `ILinkCheckerAPI`."""
 
     implements(ILinkCheckerAPI)
 
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
     def check_links(self, links):
         if links is None:
             links = ['a', 'b', 'c']
         return ','.join(links)
+
+    def __call__(self):
+
+        result = []
+    #    for term in batch_navigator.currentBatch():
+    #        entry = dict(value=term.token, title=term.title)
+    #        # The canonical_url without just the path (no hostname) can
+    #        # be passed directly into the REST PATCH call.
+    #        api_request = IWebServiceClientRequest(self.request)
+    #        try:
+    #            entry['api_uri'] = canonical_url(
+    #                term.value, request=api_request,
+    #                path_only_if_possible=True)
+    #        except NoCanonicalUrl:
+    #            # The exception is caught, because the api_url is only
+    #            # needed for inplace editing via a REST call. The
+    #            # form picker doesn't need the api_url.
+    #            entry['api_uri'] = 'Could not find canonical url.'
+    #        picker_entry = IPickerEntry(term.value)
+    #        if picker_entry.description is not None:
+    #            if len(picker_entry.description) > MAX_DESCRIPTION_LENGTH:
+    #                entry['description'] = (
+    #                    picker_entry.description[:MAX_DESCRIPTION_LENGTH-3]
+    #                    + '...')
+    #            else:
+    #                entry['description'] = picker_entry.description
+    #        if picker_entry.image is not None:
+    #            entry['image'] = picker_entry.image
+    #        if picker_entry.css is not None:
+    #            entry['css'] = picker_entry.css
+    #        result.append(entry)
+
+        result.append({'test': '123'})
+        self.request.response.setHeader('Content-type', 'application/json')
+        return simplejson.dumps(dict(total_size=4, entries=result))
 
 
 class LinkCheckerURL:
--- lib/canonical/launchpad/interfaces/linkchecker.py
+++ lib/canonical/launchpad/interfaces/linkchecker.py
@@ -14,16 +14,24 @@ __all__ = [
 from zope.interface import (
     Interface,
     )
+from zope.schema import (
+    Text,
+    )
 
 from lazr.restful.declarations import (
     export_as_webservice_entry,
     export_read_operation,
+    export_operation_as,
+    operation_parameters,
     )
 
 
 class ILinkCheckerAPI(Interface):
     """export_as_webservice_entry()"""
 
+#    @operation_parameters(
+#        links=Text(title="Links to check"), true=False, default=False)
     @export_read_operation()
+    @export_operation_as('check_links')
     def check_links(links=None):
         """ Checks links are valid."""
