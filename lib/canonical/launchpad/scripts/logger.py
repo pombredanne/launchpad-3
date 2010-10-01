8945bbde68a0e3c664916ca26b7184d3b9b574b8
Julian Edwards <julian.edwards@canonical.com>
2010-10-01 14:42:46 +0100

    Escape extra percent signs when logging in the FakeLogger

--- lib/canonical/launchpad/scripts/logger.py
+++ lib/canonical/launchpad/scripts/logger.py
@@ -39,6 +39,7 @@ from logging.handlers import WatchedFileHandler
 from optparse import OptionParser
 import os.path
 import re
+import string
 import sys
 import time
 import traceback
@@ -87,6 +88,7 @@ class FakeLogger:
             output_file = sys.stdout
         else:
             output_file = self.output_file
+        msg = string.replace(msg, "%", "%%")
         print >> output_file, prefix, msg % stuff
 
         if 'exc_info' in kw:
