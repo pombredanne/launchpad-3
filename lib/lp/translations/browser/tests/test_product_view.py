0496d208f42d23d5ef9e00420fae3d21edbb440a
j.c.sackett <jonathan.sackett@canonical.com>
2010-10-04 13:22:09 -0400

    Added can_configure tests for product.

--- lib/lp/translations/browser/tests/test_product_view.py
+++ lib/lp/translations/browser/tests/test_product_view.py
@@ -85,5 +85,18 @@ class TestProduct(TestCaseWithFactory):
             u'trunk'], series_names)
 
 
+class TestSearchQuestionsViewCanConfigureAnswers(TestSearchQuestionsView):
+
+    def test_cannot_configure_translations_product_no_edit_permission(self):
+        product = self.factory.makeProduct()
+        view = create_initialized_view(product, '+translations')
+        self.assertEqual(False, view.can_configure_answers)
+
+    def test_can_configure_translations_product_with_edit_permission(self):
+        product = self.factory.makeProduct()
+        login_person(product.owner)
+        view = create_initialized_view(product, '+translations')
+        self.assertEqual(True, view.can_configure_answers)
+
 def test_suite():
     return unittest.TestLoader().loadTestsFromName(__name__)
