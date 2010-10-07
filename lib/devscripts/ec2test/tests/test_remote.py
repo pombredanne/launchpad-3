10cf7ffeecbc02a352a504f8efe306be64368a8d
Steve Kowalik <stevenk@ubuntu.com>
2010-10-07 19:21:45 +1100

    Change charset to be utf-8 for the ec2test mailer tests.

--- lib/devscripts/ec2test/tests/test_remote.py
+++ lib/devscripts/ec2test/tests/test_remote.py
@@ -536,7 +536,7 @@ class TestRequest(TestCaseWithTransport, RequestHelpers):
         [body, attachment] = email.get_payload()
         self.assertIsInstance(body, MIMEText)
         self.assertEqual('inline', body['Content-Disposition'])
-        self.assertEqual('text/plain; charset="utf8"', body['Content-Type'])
+        self.assertEqual('text/plain; charset="utf-8"', body['Content-Type'])
         self.assertEqual("foo", get_body_text(email))
 
     def test_report_email_attachment(self):
@@ -779,7 +779,7 @@ class TestWebTestLogger(TestCaseWithTransport, RequestHelpers):
         [body, attachment] = email.get_payload()
         self.assertIsInstance(body, MIMEText)
         self.assertEqual('inline', body['Content-Disposition'])
-        self.assertEqual('text/plain; charset="utf8"', body['Content-Type'])
+        self.assertEqual('text/plain; charset="utf-8"', body['Content-Type'])
         self.assertEqual(
             logger.get_summary_contents(), body.get_payload(decode=True))
         self.assertIsInstance(attachment, MIMEApplication)
