a596e649e942c65749c9828db374c5f8915f1260
Brad Crittenden <bac@canonical.com>
2010-10-13 10:23:05 +0700

    Cleaned up post-review

--- lib/lp/translations/browser/tests/test_noindex.py
+++ lib/lp/translations/browser/tests/test_noindex.py
@@ -19,70 +19,69 @@ from lp.translations.publisher import TranslationsLayer
 
 
 class TestRobotsMixin:
-    """Test the inclusion of the meta "noindex,nofollow" directives."""
+    """Test the inclusion of the meta "noindex,nofollow" directives.
+
+    Subclasses using this mixin must set the following attributes:
+    target - the pillar under test
+    naked_translatable - the translatable object for the test, with the
+    security proxy removed.  It may be the target or a subordinate object.
+    (For example when testing a ProjectGroup the translatable is one of the
+    products in the project group.)
+    """
 
     layer = DatabaseFunctionalLayer
 
-    def set_usage(self, usage):
+    def setUsage(self, usage):
         self.naked_translatable.translations_usage = usage
 
-    def get_view(self):
-        """Return an initialized view's rendered contents."""
-        raise NotImplementedError
+    def getView(self):
+        view = create_initialized_view(
+            self.target, '+translations',
+            current_request=True, layer=TranslationsLayer)
+        return view
 
-    def get_rendered_contents(self):
+    def getRenderedContents(self):
         """Return an initialized view's rendered contents."""
+        # Using create_initialized_view for distroseries causes an error when
+        # rendering the view due to the way the view is registered and menus
+        # are adapted.  Getting the contents via a browser does work.
         url = canonical_url(self.target, rootsite='translations')
         browser = self.getUserBrowser(url)
         return browser.contents
 
-    @property
-    def target(self):
-        """The target of this test.
-
-        Must be overridden.
-        """
-        raise NotImplementedError
-
-    @property
-    def naked_translatable(self):
-        """The actual object that is translatable, not necessarily the target.
-
-        For instance, for a ProjectGroup the translatable is the product.
-        Must be overridden.
-        """
-        raise NotImplementedError
-
-    def get_soup_and_robots(self, usage):
-        self.set_usage(usage)
-        contents = self.get_rendered_contents()
+    def getSoupAndRobots(self, usage):
+        self.setUsage(usage)
+        contents = self.getRenderedContents()
         soup = BeautifulSoup(contents)
         robots = soup.find('meta', attrs={'name': 'robots'})
         return soup, robots
 
-    def verify_robots_are_blocked(self, usage):
-        soup, robots = self.get_soup_and_robots(usage)
+    def verifyRobotsAreBlocked(self, usage):
+        soup, robots = self.getSoupAndRobots(usage)
         self.assertIsNot(None, robots,
                          "Robot blocking meta information not present.")
         self.assertEqual('noindex,nofollow', robots['content'])
+        expected = ('noindex', 'nofollow')
+        actual = robots['content'].split(',')
+        self.assertContentEqual(expected, actual)
 
-    def verify_robots_not_blocked(self, usage):
-        soup, robots = self.get_soup_and_robots(usage)
+    def verifyRobotsNotBlocked(self, usage):
+        soup, robots = self.getSoupAndRobots(usage)
         self.assertIs(
             None, robots,
             "Robot blocking metadata present when it should not be.")
 
     def test_UNKNOWN_blocks_robots(self):
-        self.verify_robots_are_blocked(ServiceUsage.UNKNOWN)
+        self.verifyRobotsAreBlocked(ServiceUsage.UNKNOWN)
 
     def test_EXTERNAL_blocks_robots(self):
-        self.verify_robots_are_blocked(ServiceUsage.EXTERNAL)
+        self.verifyRobotsAreBlocked(ServiceUsage.EXTERNAL)
 
     def test_NOT_APPLICABLE_blocks_robots(self):
-        self.verify_robots_are_blocked(ServiceUsage.NOT_APPLICABLE)
+        self.verifyRobotsAreBlocked(ServiceUsage.NOT_APPLICABLE)
 
     def test_LAUNCHPAD_does_not_block_robots(self):
-        self.verify_robots_not_blocked(ServiceUsage.LAUNCHPAD)
+        self.verifyRobotsNotBlocked(ServiceUsage.LAUNCHPAD)
 
 
 class TestRobotsProduct(BrowserTestCase, TestRobotsMixin):
@@ -90,64 +89,35 @@ class TestRobotsProduct(BrowserTestCase, TestRobotsMixin):
 
     def setUp(self):
         super(TestRobotsProduct, self).setUp()
-        self.product = self.factory.makeProduct()
+        self.target = self.factory.makeProduct()
         self.factory.makePOTemplate(
-            productseries=self.product.development_focus)
-
-    @property
-    def target(self):
-        return self.product
-
-    @property
-    def naked_translatable(self):
-        return removeSecurityProxy(self.product)
-
-    def get_view(self):
-        view = create_initialized_view(
-            self.target, '+translations',
-            current_request=True, layer=TranslationsLayer)
-        return view
+            productseries=self.target.development_focus)
+        self.naked_translatable = removeSecurityProxy(self.target)
 
 
-class TestRobotsProjectGroup(TestRobotsProduct):
+class TestRobotsProjectGroup(BrowserTestCase, TestRobotsMixin):
     """Test noindex,nofollow for project groups."""
 
     def setUp(self):
         super(TestRobotsProjectGroup, self).setUp()
-        self.projectgroup = self.factory.makeProject()
+        self.target = self.factory.makeProject()
         self.product = self.factory.makeProduct()
         self.factory.makePOTemplate(
             productseries=self.product.development_focus)
-        self.naked_translatable.project = self.projectgroup
-
-    @property
-    def target(self):
-        return self.projectgroup
-
-    def test_has_translatables_true(self):
-        self.set_usage(ServiceUsage.LAUNCHPAD)
-        view = self.get_view()
-        self.assertTrue(view.has_translatables)
-
-    def test_has_translatables_false(self):
-        self.set_usage(ServiceUsage.UNKNOWN)
-        view = self.get_view()
-        self.assertFalse(view.has_translatables)
+        self.naked_translatable = removeSecurityProxy(self.product)
+        self.naked_translatable.project = self.target
 
 
-class TestRobotsProductSeries(TestRobotsProduct):
+class TestRobotsProductSeries(BrowserTestCase, TestRobotsMixin):
     """Test noindex,nofollow for product series."""
 
     def setUp(self):
         super(TestRobotsProductSeries, self).setUp()
         self.product = self.factory.makeProduct()
-        self.productseries = self.product.development_focus
+        self.target = self.product.development_focus
         self.factory.makePOTemplate(
-            productseries=self.productseries)
-
-    @property
-    def target(self):
-        return self.productseries
+            productseries=self.target)
+        self.naked_translatable = removeSecurityProxy(self.product)
 
 
 class TestRobotsDistroSeries(BrowserTestCase, TestRobotsMixin):
@@ -159,63 +129,30 @@ class TestRobotsDistroSeries(BrowserTestCase, TestRobotsMixin):
         login_person(self.owner)
         self.distro = self.factory.makeDistribution(
             name="whobuntu", owner=self.owner)
-        self.distroseries = self.factory.makeDistroSeries(
+        self.target = self.factory.makeDistroSeries(
             name="zephyr", distribution=self.distro)
-        self.distroseries.hide_all_translations = False
+        self.target.hide_all_translations = False
         new_sourcepackagename = self.factory.makeSourcePackageName()
         self.factory.makePOTemplate(
-            distroseries=self.distroseries,
+            distroseries=self.target,
             sourcepackagename=new_sourcepackagename)
-
-    @property
-    def target(self):
-        return self.distroseries
-
-    @property
-    def naked_translatable(self):
-        return removeSecurityProxy(self.distro)
-
-    def get_view(self):
-        view = create_initialized_view(
-            self.target, '+translations',
-            layer=TranslationsLayer,
-            rootsite='translations',
-            current_request=True)
-        return view
-
-    def get_rendered_contents(self):
-        # Using create_initialized_view for distroseries causes an error when
-        # rendering the view due to the way the view is registered and menus
-        # are adapted.  Getting the contents via a browser does work.
-        url = canonical_url(self.target, rootsite='translations')
-        browser = self.getUserBrowser(url)
-        return browser.contents
-
-    def test_LAUNCHPAD_does_not_block_robots(self):
-        self.verify_robots_not_blocked(ServiceUsage.LAUNCHPAD)
+        self.naked_translatable = removeSecurityProxy(self.distro)
 
 
-class TestRobotsDistro(TestRobotsDistroSeries):
+class TestRobotsDistro(BrowserTestCase, TestRobotsMixin):
     """Test noindex,nofollow for distro."""
 
     def setUp(self):
-        super(TestRobotsDistroSeries, self).setUp()
+        super(TestRobotsDistro, self).setUp()
         self.owner = self.factory.makePerson()
         login_person(self.owner)
-        self.distro = self.factory.makeDistribution(
+        self.target = self.factory.makeDistribution(
             name="whobuntu", owner=self.owner)
         self.distroseries = self.factory.makeDistroSeries(
-            name="zephyr", distribution=self.distro)
+            name="zephyr", distribution=self.target)
         self.distroseries.hide_all_translations = False
         new_sourcepackagename = self.factory.makeSourcePackageName()
         self.factory.makePOTemplate(
             distroseries=self.distroseries,
             sourcepackagename=new_sourcepackagename)
-
-    @property
-    def target(self):
-        return self.distro
-
-    @property
-    def naked_translatable(self):
-        return removeSecurityProxy(self.distro)
+        self.naked_translatable = removeSecurityProxy(self.target)
