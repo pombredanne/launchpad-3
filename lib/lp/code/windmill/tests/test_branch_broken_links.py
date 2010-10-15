09e2de154951be56801a4d7f2fe8cc3c100ffb5f
Ian Booth <ian.booth@canonical.com>
2010-10-15 11:17:21 +1000

    Fix javascript on windmill test

--- lib/lp/code/windmill/tests/test_branch_broken_links.py
+++ lib/lp/code/windmill/tests/test_branch_broken_links.py
@@ -57,8 +57,8 @@ class TestBranchBugLinks(WindmillTestCase):
         client.click(xpath=CREATE_QUESTION_BUTTON)
 
         client.waits.forElement(xpath=ADD_TEXT_BUTTON)
-        valid_links = ['lp:firefox/trunk', 'lp:gnome-terminal/trunk']
-        invalid_links = ['lp:foo']
+        valid_links = ['lp:~mark/firefox/release-0.8', 'lp:gnome-terminal/trunk']
+        invalid_links = []#'lp:foo']
         question_text = self.question_text_template % (
             ', '.join(valid_links), ', '.join(invalid_links)
         )
@@ -71,17 +71,18 @@ class TestBranchBugLinks(WindmillTestCase):
         client.waits.sleep(milliseconds=SLEEP)
 
         code = """
-            var bad_a = document.getElementsByClassName('invalid-link', 'a');
-            var bad_links = [];
-            for( i=0; i<bad_a.length; i++ ) {
-                result.push(bad_a[i].innerHTML);
-            }
-
-            var good_a = document.getElementsByClassName('branch-short-link', 'a');
+            var good_a = windmill.testWin().document.getElementsByClassName('branch-short-link', 'a');
             var good_links = [];
             for( i=0; i<good_a.length; i++ ) {
-                result.push(good_a[i].innerHTML);
+                good_links.push(good_a[i].innerHTML);
             }
+
+            var bad_a = windmill.testWin().document.getElementsByClassName('invalid-link', 'a');
+            var bad_links = [];
+            for( i=0; i<bad_a.length; i++ ) {
+                bad_links.push(bad_a[i].innerHTML);
+            }
+
 
             var result = {};
             result.good = good_links;
@@ -91,13 +92,12 @@ class TestBranchBugLinks(WindmillTestCase):
         raw_result = self.client.commands.execJS(js=code)
         self.assertEqual(True, 'result' in raw_result.keys(), raw_result)
         result = raw_result['result']
-        print result
-#        result_valid_links = result['good']
-#        result_invalid_links = result['bad']
-#        self.assertEqual(len(invalid_links), len(result_invalid_links))
-#        self.assertEqual(set(invalid_links), set(result_invalid_links))
-#        self.assertEqual(len(valid_links), len(result_valid_links))
-#        self.assertEqual(set(valid_links), set(result_valid_links))
+        result_valid_links = result['good']
+        result_invalid_links = result['bad']
+        self.assertEqual(len(invalid_links), len(result_invalid_links))
+        self.assertEqual(set(invalid_links), set(result_invalid_links))
+        self.assertEqual(len(valid_links), len(result_valid_links))
+        self.assertEqual(set(valid_links), set(result_valid_links))
 
 
 def test_suite():
