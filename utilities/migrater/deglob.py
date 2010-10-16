f27091d8911cd4f1cf97cd11f74f85b0d8a6b61a
Curtis Hovey <curtis.hovey@canonical.com>
2010-10-16 17:48:06 -0400

    Updated deglobber to convert multiline doctest imports so that normal single line deglobs can be run next.

--- utilities/migrater/deglob.py
+++ utilities/migrater/deglob.py
@@ -63,14 +63,83 @@ def update_doctest_globs_to_interfaces():
         pattern = glob_interface % interface
         substitution = 'from %s import %s' % (module_, interface)
         for summary in find_matches(
-            root, types, pattern, substitution=substitution):
+            root, types, pattern, substitution=substitution,
+            extract_match=multiline_extract_match):
             print "\n%(file_path)s" % summary
             for line in summary['lines']:
                 print "    %(lineno)4s: %(text)s" % line
 
 
+def multiline_extract_match(file_path, match_re, substitution=None):
+    """Return a summary of matches in a file."""
+    lines = []
+    content = []
+    match = None
+    file_ = open(file_path, 'r')
+    in_match = False
+    current_match = None
+    try:
+        for lineno, line in enumerate(file_):
+            if in_match:
+                identifiers = line.split(',')
+                for idf in identifiers:
+                    idf = idf.strip()
+                    idf = idf.strip('...')
+                    idf = idf.strip()
+                    if idf.endswith(')'):
+                        in_match = False
+                        idf = idf[0:-1]
+                    idf = idf.strip()
+                    if idf == '':
+                        continue
+                    expanded_line = (
+                        '%s %s\n' % (current_match.group(0), idf))
+                    lines.append(
+                        {'lineno': lineno + 1, 'text': expanded_line,
+                         'match': None})
+                    if substitution is not None:
+                        content.append(expanded_line)
+                    if not in_match:
+                        current_match = None
+                continue
+            # Else check if this is the start of a multi-line.
+            match = match_re.search(line)
+            if match and line.strip().endswith('('):
+                in_match = True
+                current_match = match
+                continue
+            # Always append the non-matching lines to content to rebuild
+            # the file.
+            if substitution is not None:
+                content.append(line)
+    finally:
+        file_.close()
+    if lines:
+        if substitution is not None:
+            file_ = open(file_path, 'w')
+            try:
+                file_.write(''.join(content))
+            finally:
+                file_.close()
+        return {'file_path': file_path, 'lines': lines}
+    return None
+
+
+def update_multi_doctest_globs_to_interfaces():
+    root = 'lib/lp/registry/doc'
+    types = r'\.(txt)'
+    pattern = r'[ ]+>>> from canonical\.launchpad\.interfaces import'
+    substitution = True
+    for summary in find_matches(
+        root, types, pattern, substitution=substitution,
+        extract_match=multiline_extract_match):
+        print "\n%(file_path)s" % summary
+        for line in summary['lines']:
+            print "    %(lineno)4s: %(text)s" % line
+
+
 def main():
-    update_doctest_globs_to_interfaces()
+    update_multi_doctest_globs_to_interfaces()
 
 
 if __name__ == '__main__':
