57a44f6c78841d5ba2996395384e330dc52f230a
Paul Hummer <paul@canonical.com>
2010-10-27 10:14:06 -0600

    Added tests for creating merge queues.

--- lib/lp/code/browser/tests/test_branchmergequeue.py
+++ lib/lp/code/browser/tests/test_branchmergequeue.py
@@ -7,6 +7,8 @@ from __future__ import with_statement
 
 __metaclass__ = type
 
+from mechanize import LinkNotFoundError
+
 from canonical.launchpad.testing.pages import (
     extract_text,
     find_main_content,
@@ -66,3 +68,35 @@ class TestBranchMergeQueue(BrowserTestCase):
         main_text = extract_text(find_main_content(browser.contents))
         self.assertTextMatchesExpressionIgnoreWhitespace(
             pattern, main_text)
+
+    def test_create(self):
+        """Test that branch merge queues can be created from a branch."""
+        with person_logged_in(ANONYMOUS):
+            rockstar = self.factory.makePerson(name='rockstar')
+            branch = self.factory.makeBranch(owner=rockstar)
+            self.factory.makeBranch(product=branch.product)
+            owner_name = branch.owner.name
+
+        browser = self.getUserBrowser(canonical_url(branch), user=rockstar)
+        browser.getLink('Create a new queue').click()
+
+        browser.getControl('Name').value = 'libbob-queue'
+        browser.getControl('Description').value = (
+            'This is a queue for the libbob projects.')
+        browser.getControl('Create Queue').click()
+
+        self.assertEqual(
+            'http://code.launchpad.dev/~rockstar/+merge-queues/libbob-queue',
+            browser.url)
+
+    def test_create_unauthorized(self):
+        """Test that queues can't be created by unauthorized users."""
+        with person_logged_in(ANONYMOUS):
+            branch = self.factory.makeBranch()
+            self.factory.makeBranch(product=branch.product)
+
+        browser = self.getUserBrowser(canonical_url(branch))
+        self.assertRaises(
+            LinkNotFoundError,
+            browser.getLink,
+            'Create a new queue')
