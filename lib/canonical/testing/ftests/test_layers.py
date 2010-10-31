bf954bcefb1ac9bcd004d31375c30a64accd4974
Robert Collins <robert@canonical.com>
2010-11-01 05:56:15 +1100

    annotate the DB layer failing parallel tests.

--- lib/canonical/testing/ftests/test_layers.py
+++ lib/canonical/testing/ftests/test_layers.py
@@ -327,6 +327,12 @@ class DatabaseTestCase(BaseTestCase):
         num = cur.fetchone()[0]
         return num
 
+    # XXX: Parallel-fail: because layers are not cleanly integrated with
+    # unittest, what should be one test is expressed as three distinct
+    # tests here. We need to either write enough glue to push/pop the
+    # global state of zope.testing.runner or we need to stop using layers,
+    # before these tests will pass in a parallel run. Robert Collins
+    # 2010-11-01
     def testNoReset1(self):
         # Ensure that we can switch off database resets between tests
         # if necessary, such as used by the page tests
