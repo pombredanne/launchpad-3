55767e2b5eec05a08eea10831c33bad6afbdf802
Jelmer Vernooij <jelmer@canonical.com>
2010-11-04 15:00:34 +0100

    Revert SyncPackageJob.run for the time being.

--- lib/lp/soyuz/model/syncpackagejob.py
+++ lib/lp/soyuz/model/syncpackagejob.py
@@ -97,8 +97,4 @@ class SyncPackageJob(DistributionJobDerived):
 
     def run(self):
         """See `IRunnableJob`."""
-        self.target_archive.syncSource(
-            self.source_package_name, self.source_package_version,
-            self.source_archive, to_pocket=self.pocket,
-            to_series=self.distroseries,
-            include_binaries=self.include_binaries)
+        raise NotImplementedError(self.run)
