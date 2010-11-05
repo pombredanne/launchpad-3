87098fcb155728657bb4c9811c7098044760f458
Jonathan Lange <jml@canonical.com>
2010-11-05 16:11:34 +0000

    Remove a bunch of imports from c.l.database.__init__

--- lib/canonical/launchpad/database/__init__.py
+++ lib/canonical/launchpad/database/__init__.py
@@ -10,30 +10,5 @@ from canonical.launchpad.database.logintoken import *
 from canonical.launchpad.database.message import *
 from canonical.launchpad.database.oauth import *
 from canonical.launchpad.database.temporaryblobstorage import *
-from lp.buildmaster.model.builder import *
 from lp.services.worlddata.model.language import *
 from lp.services.worlddata.model.spokenin import *
-from lp.soyuz.model.archive import *
-from lp.soyuz.model.archivedependency import *
-from lp.soyuz.model.archivepermission import *
-from lp.soyuz.model.binarypackagebuild import *
-from lp.soyuz.model.binarypackagename import *
-from lp.soyuz.model.binarypackagerelease import *
-from lp.soyuz.model.component import *
-from lp.soyuz.model.distributionsourcepackagerelease import *
-from lp.soyuz.model.distroarchseries import *
-from lp.soyuz.model.distroarchseriesbinarypackage import *
-from lp.soyuz.model.distroarchseriesbinarypackagerelease import *
-from lp.soyuz.model.distroseriesbinarypackage import *
-from lp.soyuz.model.distroseriespackagecache import *
-from lp.soyuz.model.distroseriessourcepackagerelease import *
-from lp.soyuz.model.files import *
-# XXX flacoste 2009/03/18 We should use specific imports instead of
-# importing from this module.
-from lp.soyuz.model.packagediff import *
-from lp.soyuz.model.packageset import *
-from lp.soyuz.model.processor import *
-from lp.soyuz.model.publishing import *
-from lp.soyuz.model.queue import *
-from lp.soyuz.model.section import *
-from lp.soyuz.model.sourcepackagerelease import *
