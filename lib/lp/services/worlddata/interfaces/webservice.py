76c68d49b648b5df0b43ebd6c60d2955fe34cc58
Jonathan Lange <jml@canonical.com>
2010-11-08 16:52:39 +0000

    Explicitly export worlddata. Just bugs, registry and soyuz to go.

---
+++ lib/lp/services/worlddata/interfaces/webservice.py
@@ -0,0 +1,21 @@
+# Copyright 2010 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""All the interfaces that are exposed through the webservice."""
+
+__metaclass__ = type
+__all__ = [
+    'ICountry',
+    'ICountrySet',
+    'ILanguage',
+    'ILanguageSet',
+    ]
+
+from lp.services.worlddata.interfaces.country import (
+    ICountry,
+    ICountrySet,
+    )
+from lp.services.worlddata.interfaces.language import (
+    ILanguage,
+    ILanguageSet,
+    )
