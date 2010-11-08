0c4ea76582057307d388a940a47a227255b187ba
Jonathan Lange <jml@canonical.com>
2010-11-08 13:22:37 +0000

    Update docstring

--- lib/canonical/launchpad/database/__init__.py
+++ lib/canonical/launchpad/database/__init__.py
@@ -3,6 +3,9 @@
 
 """Database / model code for bits of Launchpad that don't fit anywhere else.
 
+See also `canonical.launchpad.interfaces` for the public interfaces for this
+code.
+
 DEPRECATED: This package is deprecated.  Do not add any new modules to this
 package.  Where possible, move things out of this package into better
 locations under the 'lp' package.  See the `lp` docstring for more details.
