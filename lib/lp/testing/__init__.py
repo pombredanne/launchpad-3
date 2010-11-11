ee54cc1c3f478e345037ad5609dcc89d88d554b4
Deryck Hodge <deryck.hodge@canonical.com>
2010-11-11 10:46:41 -0600

    Add timeout value for yuitests test case.

--- lib/lp/testing/__init__.py
+++ lib/lp/testing/__init__.py
@@ -738,7 +738,7 @@ class YUIUnitTestCase(WindmillTestCase):
         client = self.client
         client.open(url=self.yui_runner_url)
         client.waits.forPageLoad(timeout=constants.PAGE_LOAD)
-        client.waits.forElement(id='complete')
+        client.waits.forElement(id='complete', timeout=constants.FOR_ELEMENT)
         response = client.commands.getPageText()
         self._yui_results = {}
         # Maybe testing.pages should move to lp to avoid circular imports.
