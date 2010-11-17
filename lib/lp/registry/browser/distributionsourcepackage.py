337ad53bd5b8956d3a803a54990972501c0a78ac
Edwin Grubbs <edwin.grubbs@canonical.com>
2010-11-16 18:23:19 -0600

    Formatted imports.

--- lib/lp/registry/browser/distributionsourcepackage.py
+++ lib/lp/registry/browser/distributionsourcepackage.py
@@ -31,7 +31,6 @@ from zope.interface import (
     Interface,
     )
 
-from canonical.launchpad.webapp.interfaces import IBreadcrumb
 from canonical.launchpad.helpers import shortlist
 from canonical.launchpad.webapp import (
     action,
@@ -43,6 +42,7 @@ from canonical.launchpad.webapp import (
     StandardLaunchpadFacets,
     )
 from canonical.launchpad.webapp.breadcrumb import Breadcrumb
+from canonical.launchpad.webapp.interfaces import IBreadcrumb
 from canonical.launchpad.webapp.menu import (
     ApplicationMenu,
     enabled_with_permission,
@@ -50,15 +50,14 @@ from canonical.launchpad.webapp.menu import (
     NavigationMenu,
     )
 from canonical.launchpad.webapp.sorting import sorted_dotted_numbers
-from lp.registry.interfaces.series import SeriesStatus
-from lp.app.interfaces.launchpad import IServiceUsage
-from lp.app.browser.tales import CustomizableFormatter
 from canonical.lazr.utils import smartquote
 from lp.answers.browser.questiontarget import (
     QuestionTargetFacetMixin,
     QuestionTargetTraversalMixin,
     )
 from lp.answers.interfaces.questionenums import QuestionStatus
+from lp.app.browser.tales import CustomizableFormatter
+from lp.app.interfaces.launchpad import IServiceUsage
 from lp.bugs.browser.bugtask import BugTargetTraversalMixin
 from lp.bugs.interfaces.bug import IBugSet
 from lp.registry.browser.pillar import PillarBugsMenu
@@ -69,6 +68,7 @@ from lp.registry.interfaces.distributionsourcepackage import (
     IDistributionSourcePackage,
     )
 from lp.registry.interfaces.pocket import pocketsuffix
+from lp.registry.interfaces.series import SeriesStatus
 from lp.services.propertycache import cachedproperty
 from lp.soyuz.browser.sourcepackagerelease import (
     extract_bug_numbers,
