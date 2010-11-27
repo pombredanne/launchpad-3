a4db06ad98ff44e9da3f3a71b52dcbe91d50eada
Aaron Bentley <aaron@canonical.com>
2010-11-27 13:04:19 -0500

    Fix patch number.

--- database/schema/patch-2208-27-0.sql
+++ database/schema/patch-2208-27-0.sql
@@ -7,4 +7,4 @@ ALTER TABLE SourcePackageRecipeDataInstruction ADD COLUMN source_directory text;
 ALTER TABLE SourcePackageRecipeDataInstruction ADD CONSTRAINT sourcepackagerecipedatainstruction__source_directory_null CHECK (type in (1, 2) AND source_directory IS NULL OR type = 3 AND source_directory IS NOT NULL);
 
 
-INSERT INTO LaunchpadDatabaseRevision VALUES (2208, 99, 0);
+INSERT INTO LaunchpadDatabaseRevision VALUES (2208, 27, 0);
