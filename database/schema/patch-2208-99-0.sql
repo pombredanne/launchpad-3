678a71d20587f469146e85e0df2a71dee932ebfa
Steve Kowalik <stevenk@ubuntu.com>
2010-12-02 17:48:37 +1100

    Jump from 99 to 34 and add an index, both per db review.

--- database/schema/patch-2208-99-0.sql
+++
@@ -1,7 +0,0 @@
-SET client_min_messages=ERROR;
-
-ALTER TABLE SourcePackagePublishingHistory
-    ADD COLUMN ancestor INTEGER REFERENCES SourcePackagePublishingHistory;
-
-INSERT INTO LaunchpadDatabaseRevision VALUES (2208, 99, 0);
-
