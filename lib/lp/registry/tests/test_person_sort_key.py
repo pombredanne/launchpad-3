a723f197b73613d146b26caccc6c7247bdad8e16
Gavin Panella <gavin.panella@canonical.com>
2010-12-03 11:26:51 +0000

    New in-app twin for the person_sort_key stored procedure.

--- lib/lp/registry/tests/test_person_sort_key.py
+++ lib/lp/registry/tests/test_person_sort_key.py
@@ -1,13 +1,22 @@
 # Copyright 2009 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
-"""Test the person_sort_key stored procedure."""
+"""Test the person_sort_key stored procedure and its in-app twin."""
 
 __metaclass__ = type
 
 import unittest
 
 from canonical.testing.layers import LaunchpadLayer
+from lp.registry.model.person import person_sort_key
+
+
+class PersonNames:
+    """A fake with enough information for `person_sort_key`."""
+
+    def __init__(self, displayname, name):
+        self.displayname = displayname
+        self.name = name
 
 
 class TestPersonSortKey(unittest.TestCase):
@@ -20,64 +29,64 @@ class TestPersonSortKey(unittest.TestCase):
     def tearDown(self):
         self.con.close()
 
-    def person_sort_key(self, displayname, name):
-        '''Calls the person_sort_key stored procedure
+    def db_person_sort_key(self, displayname, name):
+        '''Calls the `person_sort_key` stored procedure.
 
         Note that although the stored procedure returns a UTF-8 encoded
         string, our database driver converts that to Unicode for us.
         '''
+        # Note that as we are testing a PostgreSQL stored procedure, we should
+        # pass it UTF-8 encoded strings to match our database encoding.
         self.cur.execute(
-                "SELECT person_sort_key(%(displayname)s, %(name)s)", vars()
-                )
+            "SELECT person_sort_key(%s, %s)", (
+                displayname.encode("UTF-8"), name.encode("UTF-8")))
         return self.cur.fetchone()[0]
+
+    def assertSortKeysEqual(self, displayname, name, expected):
+        # The sort key from the database matches the expected sort key.
+        self.assertEqual(
+            expected, self.db_person_sort_key(
+                displayname, name))
+        # The sort key calculated in-process matches the expected sort key.
+        self.assertEqual(
+            expected, person_sort_key(
+                PersonNames(displayname, name)))
 
     def test_person_sort_key(self):
 
         # person_sort_key returns the concatenation of the display name
         # and the name for use in sorting.
-        self.failUnlessEqual(
-                self.person_sort_key("Stuart Bishop", "stub"),
-                "stuart bishop, stub"
-                )
+        self.assertSortKeysEqual(
+            u"Stuart Bishop", u"stub",
+            u"stuart bishop, stub")
 
         # Leading and trailing whitespace is removed
-        self.failUnlessEqual(
-                self.person_sort_key(" Stuart Bishop\t", "stub"),
-                "stuart bishop, stub"
-                )
+        self.assertSortKeysEqual(
+            u" Stuart Bishop\t", u"stub",
+            u"stuart bishop, stub")
 
         # 'name' is assumed to be lowercase and not containing anything
         # we don't want. This should never happen as the valid_name database
         # constraint should prevent it.
-        self.failUnlessEqual(
-                self.person_sort_key("Stuart Bishop", " stub42!!!"),
-                "stuart bishop,  stub42!!!"
-                )
+        self.assertSortKeysEqual(
+            u"Stuart Bishop", u" stub42!!!",
+            u"stuart bishop,  stub42!!!")
 
         # Everything except for letters and whitespace is stripped.
-        self.failUnlessEqual(
-                self.person_sort_key("-= Mass1v3 T0SSA =-", "tossa"),
-                "massv tssa, tossa"
-                )
+        self.assertSortKeysEqual(
+            u"-= Mass1v3 T0SSA =-", u"tossa",
+            u"massv tssa, tossa")
 
         # Non ASCII letters are currently allowed. Eventually they should
         # become transliterated to ASCII but we don't do this yet.
-        # Note that as we are testing a PostgreSQL stored procedure, we
-        # should pass it UTF-8 encoded strings to match our database encoding.
-        self.failUnlessEqual(
-                self.person_sort_key(
-                    u"Bj\N{LATIN SMALL LETTER O WITH DIAERESIS}rn".encode(
-                        "UTF-8"), "bjorn"),
-                u"bj\xf6rn, bjorn"
-                )
+        self.assertSortKeysEqual(
+            u"Bj\N{LATIN SMALL LETTER O WITH DIAERESIS}rn", "bjorn",
+            u"bj\xf6rn, bjorn")
 
         # Case conversion is handled correctly using Unicode
-        self.failUnlessEqual(
-                self.person_sort_key(
-                    u"Bj\N{LATIN CAPITAL LETTER O WITH DIAERESIS}rn".encode(
-                        "UTF-8"), "bjorn"),
-                u"bj\xf6rn, bjorn" # Lower case o with diaeresis
-                )
+        self.assertSortKeysEqual(
+            u"Bj\N{LATIN CAPITAL LETTER O WITH DIAERESIS}rn", "bjorn",
+            u"bj\xf6rn, bjorn") # Lower case o with diaeresis
 
 
 def test_suite():
