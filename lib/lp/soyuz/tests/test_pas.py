be16bc2524b5436a578eb33887f202954a6e1ac4
William Grant <me@williamgrant.id.au>
2010-12-08 14:45:39 +1100

    Add comments to tests.

--- lib/lp/soyuz/tests/test_pas.py
+++ lib/lp/soyuz/tests/test_pas.py
@@ -8,6 +8,7 @@ from lp.testing import TestCaseWithFactory
 
 
 class TestDetermineArchitecturesToBuild(TestCaseWithFactory):
+    """Test that determineArchitecturesToBuild correctly interprets hints."""
 
     layer = LaunchpadZopelessLayer
 
@@ -18,6 +19,7 @@ class TestDetermineArchitecturesToBuild(TestCaseWithFactory):
         self.publisher.addFakeChroots()
 
     def assertArchsForHint(self, hint_string, expected_arch_tags):
+        """Assert that the given hint resolves to the expected archtags."""
         pub = self.publisher.getPubSource(architecturehintlist=hint_string)
         architectures = determineArchitecturesToBuild(
             pub, self.publisher.breezy_autotest.architectures,
@@ -26,47 +28,72 @@ class TestDetermineArchitecturesToBuild(TestCaseWithFactory):
             expected_arch_tags, [a.architecturetag for a in architectures])
 
     def testSingleArchitecture(self):
+        # A hint string with a single arch resolves to just that arch.
         self.assertArchsForHint('hppa', ['hppa'])
 
     def testThreeArchitectures(self):
+        # A hint string with multiple archs resolves to just those
+        # archs.
         self.assertArchsForHint('amd64 i386 hppa', ['hppa', 'i386'])
 
     def testIndependent(self):
+        # 'all' is special, meaning just a single build. The
+        # nominatedarchindep architecture is used -- in this case i386.
         self.assertArchsForHint('all', ['i386'])
 
     def testOneAndIndependent(self):
+        # 'all' is redundant if we have another build anyway.
         self.assertArchsForHint('hppa all', ['hppa'])
 
     def testFictionalAndIndependent(self):
+        # But 'all' is useful if present with an arch that wouldn't
+        # generate a build.
         self.assertArchsForHint('foo all', ['i386'])
 
     def testWildcard(self):
+        # 'any' is a wildcard that matches all available archs.
         self.assertArchsForHint('any', ['hppa', 'i386'])
 
     def testKernelSpecificArchitecture(self):
+        # Since we only support Linux-based architectures, 'linux-foo'
+        # is treated the same as 'foo'.
         self.assertArchsForHint('linux-hppa', ['hppa'])
 
     def testUnknownKernelSpecificArchitecture(self):
+        # Non-Linux architectures aren't supported.
         self.assertArchsForHint('kfreebsd-hppa', [])
 
     def testKernelWildcardArchitecture(self):
+        # Wildcards work for kernels: 'any-foo' is treated like 'foo'.
         self.assertArchsForHint('any-hppa', ['hppa'])
 
     def testKernelSpecificArchitectureWildcard(self):
+        # Wildcards work for archs too: 'linux-any' is treated like 'any'.
         self.assertArchsForHint('linux-any', ['hppa', 'i386'])
 
     def testUnknownKernelSpecificArchitectureWildcard(self):
+        # But unknown kernels continue to result in nothing.
         self.assertArchsForHint('kfreebsd-any', [])
 
     def testWildcardAndIndependent(self):
+        # 'all' continues to be ignored alongside a valid wildcard.
         self.assertArchsForHint('all linux-any', ['hppa', 'i386'])
 
     def testKernelIndependentIsInvalid(self):
+        # 'linux-all' isn't supported.
         self.assertArchsForHint('linux-all', [])
 
     def testDoubleWildcardIsInvalid(self):
+        # 'any-any' is invalid; you want 'any'.
         self.assertArchsForHint('any-any', [])
 
     def testDisabledArchitecturesOmitted(self):
+        # Disabled architectures are not buildable, so are excluded.
         self.publisher.breezy_autotest['hppa'].enabled = False
+        self.assertArchsForHint('any', ['i386'])
+
+    def testVirtualizedArchivesHaveOnlyVirtualizedArchs(self):
+        # For archives which must build on virtual builders, only
+        # virtual archs are returned.
+        self.publisher.breezy_autotest.main_archive.require_virtualized = True
         self.assertArchsForHint('any', ['i386'])
