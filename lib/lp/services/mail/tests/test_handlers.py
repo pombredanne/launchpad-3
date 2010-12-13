49de236478763e39abdd222e9191ccdcf52ed8cf
Gavin Panella <gavin.panella@canonical.com>
2010-12-13 20:41:15 +0000

    Document tests.

--- lib/lp/services/mail/tests/test_handlers.py
+++ lib/lp/services/mail/tests/test_handlers.py
@@ -8,13 +8,17 @@ from lp.testing import TestCase
 
 
 class TestMailHandlers(TestCase):
+    """Tests for the `MailHandlers` class."""
 
     def test_get(self):
+        # MailHandlers.get() should return the registered handler for the
+        # given domain.
         handlers = MailHandlers()
         self.assertIsNot(None, handlers.get("bugs.launchpad.net"))
         self.assertIs(None, handlers.get("no.such.domain"))
 
     def test_get_is_case_insensitive(self):
+        # The domain passed to get() is treated case-insentitively.
         handlers = MailHandlers()
         handler = object()
         handlers.add("some.domain", handler)
@@ -23,6 +27,7 @@ class TestMailHandlers(TestCase):
         self.assertIs(handler, handlers.get("Some.Domain"))
 
     def test_add_for_new_domain(self):
+        # MailHandlers.add() registers a handler for the given domain.
         handlers = MailHandlers()
         self.assertIs(None, handlers.get("some.domain"))
         handler = object()
@@ -30,7 +35,7 @@ class TestMailHandlers(TestCase):
         self.assertIs(handler, handlers.get("some.domain"))
 
     def test_add_for_existing_domain(self):
-        # When adding a new handler for a already congfigured domain, the
+        # When adding a new handler for an already congfigured domain, the
         # existing handler is overwritten.
         handlers = MailHandlers()
         handler1 = object()
