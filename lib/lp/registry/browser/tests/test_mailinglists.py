57fda17b53e3283f394062682db08a13dca5206c
Curtis Hovey <curtis.hovey@canonical.com>
2010-12-15 13:42:36 -0500

    Updated the mailing-list portlet to state if the archive is public or private. Removed redundant tests.

--- lib/lp/registry/browser/tests/test_mailinglists.py
+++ lib/lp/registry/browser/tests/test_mailinglists.py
@@ -8,8 +8,12 @@ __metaclass__ = type
 
 
 from canonical.launchpad.ftests import login_person
-from canonical.launchpad.testing.pages import find_tag_by_id
+from canonical.launchpad.testing.pages import (
+    extract_text,
+    find_tag_by_id,
+    )
 from canonical.testing.layers import DatabaseFunctionalLayer
+from lp.registry.interfaces.person import PersonVisibility
 from lp.testing import (
     person_logged_in,
     TestCaseWithFactory,
@@ -41,7 +45,7 @@ class MailingListSubscriptionControlsTestCase(TestCaseWithFactory):
         link_tag = find_tag_by_id(content, "link-list-subscribe")
         self.assertNotEqual(None, link_tag)
 
-    def test_subscribe_control_doesnt_render_for_anon(self):
+    def test_subscribe_control_doesnt_render_for_non_member(self):
         other_person = self.factory.makePerson()
         login_person(other_person)
         view = create_view(self.b_team, name='+index',
@@ -51,3 +55,35 @@ class MailingListSubscriptionControlsTestCase(TestCaseWithFactory):
         self.assertNotEqual('', content)
         link_tag = find_tag_by_id(content, "link-list-subscribe")
         self.assertEqual(None, link_tag)
+
+
+class MailingListPortletTestCase(TestCaseWithFactory):
+    """Verify the team index subscribe/unsubscribe to mailing list content."""
+
+    layer = DatabaseFunctionalLayer
+
+    def makeTeamAndMailingList(self, visibility=None):
+        team = self.factory.makeTeam(visibility=visibility)
+        login_person(team.teamowner)
+        team_list = self.factory.makeMailingList(
+            team=team, owner=team.teamowner)
+        return team
+
+    def test_public_archive(self):
+        # Public teams have public archives.
+        team = self.makeTeamAndMailingList()
+        view = create_view(
+            team, name='+portlet-mailinglist',
+            server_url='http://launchpad.dev', path_info='/~%s' % team.name)
+        link = find_tag_by_id(view(), 'mailing-list-archive')
+        self.assertEqual('View public archive', extract_text(link))
+
+    def test_private_archive(self):
+        # Private teams have private archives.
+        team = self.makeTeamAndMailingList(
+            visibility=PersonVisibility.PRIVATE)
+        view = create_view(
+            team, name='+portlet-mailinglist',
+            server_url='http://launchpad.dev', path_info='/~%s' % team.name)
+        link = find_tag_by_id(view(), 'mailing-list-archive')
+        self.assertEqual('View private archive', extract_text(link))
