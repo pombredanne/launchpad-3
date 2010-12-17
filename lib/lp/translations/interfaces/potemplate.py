0318d1c89113f7cce61d1c48b55891c635941ae6
Henning Eggers <henning@canonical.com>
2010-12-17 17:07:47 +0100

    Better tittle for a templates current flag.

--- lib/lp/translations/interfaces/potemplate.py
+++ lib/lp/translations/interfaces/potemplate.py
@@ -134,7 +134,7 @@ class IPOTemplate(IRosettaStats):
         required=True)
 
     iscurrent = exported(Bool(
-        title=_("Accept translations?"),
+        title=_("Template active?"),
         description=_(
             "If unchecked, people can no longer change the template's "
             "translations."),
