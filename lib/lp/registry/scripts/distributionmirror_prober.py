1f1d500f797222bf970dc99f46c42ee442195fd0
Jonathan Lange <jml@canonical.com>
2010-12-24 10:39:49 +0000

    Document the public names

--- lib/lp/registry/scripts/distributionmirror_prober.py
+++ lib/lp/registry/scripts/distributionmirror_prober.py
@@ -1,6 +1,10 @@
 # Copyright 2009-2010 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
+__all__ = [
+    'DistroMirrorProber',
+    ]
+
 from datetime import datetime
 import httplib
 import itertools
