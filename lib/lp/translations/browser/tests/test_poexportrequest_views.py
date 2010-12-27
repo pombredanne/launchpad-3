36476b4514fa08b963cf23d7739bee766b69103b
Henning Eggers <henning@canonical.com>
2010-12-27 16:05:24 +0100

    Tests for POTemplateExportView.

--- lib/lp/translations/browser/tests/test_poexportrequest_views.py
+++ lib/lp/translations/browser/tests/test_poexportrequest_views.py
@@ -39,12 +39,28 @@ class TestPOTEmplateExportView(TestCaseWithFactory):
         view.initialize()
         return view
 
-    def test_request_all(self):
-        # Selecting 'all' will place all pofiles and the template in the
-        # request queue.
-        pofile = self.factory.makePOFile(potemplate=self.potemplate)
+    def test_request_all_only_template(self):
+        # Selecting 'all' will place the template into the request queue.
         self._createView({'what': 'all', 'format': 'PO'}) 
 
         self.assertContentEqual(
-            [(self.potemplate, None), (self.potemplate, pofile)],
-            get_poexportrequests())
+            [(self.potemplate, None)], get_poexportrequests())
+
+    def test_request_some_potemplate(self):
+        # Using 'some' allows to select only the template.
+        pofile = self.factory.makePOFile(potemplate=self.potemplate)
+        self._createView({'what': 'some', 'potemplate': True, 'format': 'PO'}) 
+
+        self.assertContentEqual(
+            [(self.potemplate, None)], get_poexportrequests())
+
+    def test_request_some_pofile(self):
+        # Using 'some' allows to select only a pofile.
+        pofile = self.factory.makePOFile(potemplate=self.potemplate)
+        self._createView({
+            'what': 'some',
+            pofile.language.code: True,
+            'format': 'PO'}) 
+
+        self.assertContentEqual(
+            [(self.potemplate, pofile)], get_poexportrequests())
