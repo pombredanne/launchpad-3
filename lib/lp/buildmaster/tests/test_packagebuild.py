d15c17aec4f5b1bef81d9729cd102d10ae338ef2
William Grant <william.grant@canonical.com>
2011-01-07 11:23:44 +1100

    PackageBuild.createUploadLog no longer chokes on Unicode input.

--- lib/lp/buildmaster/tests/test_packagebuild.py
+++ lib/lp/buildmaster/tests/test_packagebuild.py
@@ -141,6 +141,14 @@ class TestPackageBuild(TestPackageBuildBase):
         self.package_build.storeUploadLog("Some content")
         self.failUnless(self.package_build.upload_log.restricted)
 
+    def test_storeUploadLog_unicode(self):
+        # Unicode upload logs are uploaded as UTF-8.
+        self.package_build.storeUploadLog(u"Some content \N{SNOWMAN}")
+        self.failIfEqual(None, self.package_build.upload_log)
+        self.failUnlessEqual(
+            hashlib.sha1("Some content \xe2\x98\x83").hexdigest(),
+            self.package_build.upload_log.content.sha1)
+
     def test_upload_log_url(self):
         # The url of the upload log file is determined by the PackageBuild.
         Store.of(self.package_build).flush()
