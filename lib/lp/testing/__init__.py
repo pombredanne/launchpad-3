8ec0979a2ff0ed93edd2f0a9c9dcdd2ee5f30553
Stuart Bishop <stuart.bishop@canonical.com>
2011-01-17 16:31:30 -0600

    Fix bad conflict resolution

--- lib/lp/testing/__init__.py
+++ lib/lp/testing/__init__.py
@@ -512,7 +512,7 @@ class TestCase(testtools.TestCase, fixtures.TestWithFixtures):
         """Include the logChunks from fixture in the test details."""
         # Evaluate the log when called, not later, to permit the librarian to
         # be shutdown before the detail is rendered.
-        chunks = fixture.logChunks()
+        chunks = fixture.getLogChunks()
         content = Content(UTF8_TEXT, lambda: chunks)
         self.addDetail('librarian-log', content)
 
