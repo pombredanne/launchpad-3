13b10684ccb7b525185c5440ad233e6862ccbf7d
Tim Penhey <tim.penhey@canonical.com>
2011-01-18 14:49:35 -0600

    Unify the text xhtml representations.

--- lib/lp/registry/browser/webservice.py
+++ lib/lp/registry/browser/webservice.py
@@ -15,14 +15,16 @@ from zope.interface import (
     implementer,
     Interface,
     )
+from zope.schema.interfaces import IText
 
+from lp.app.browser.stringformatter import FormattersAPI
 from lp.app.browser.tales import PersonFormatterAPI
 from lp.services.fields import IPersonChoice
 
 
 @component.adapter(Interface, IPersonChoice, IWebServiceClientRequest)
 @implementer(IFieldHTMLRenderer)
-def person_renderer(context, field, request):
+def person_xhtml_representation(context, field, request):
     """Render a person as a link to the person."""
 
     def render(value):
@@ -36,3 +38,17 @@ def person_renderer(context, field, request):
             # lp.app.javascript.picker (we think).
             return '<span>%s</span>' % link
     return render
+
+
+@component.adapter(Interface, IText, IWebServiceClientRequest)
+@implementer(IFieldHTMLRenderer)
+def text_xhtml_representation(context, field, request):
+    """Render text as XHTML using the webservice."""
+    formatter = FormattersAPI
+
+    def renderer(value):
+        nomail = formatter(value).obfuscate_email()
+        html = formatter(nomail).text_to_html()
+        return html.encode('utf-8')
+
+    return renderer
