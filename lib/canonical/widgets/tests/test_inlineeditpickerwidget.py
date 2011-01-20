701e2e0522a279426271f6d98d4d59b9e6c1e6a2
Tim Penhey <tim.penhey@canonical.com>
2011-01-20 15:11:34 -0600

    Tests for show_remove_button and refactoring.

--- lib/canonical/widgets/tests/test_inlineeditpickerwidget.py
+++ lib/canonical/widgets/tests/test_inlineeditpickerwidget.py
@@ -21,22 +21,29 @@ class TestInlineEditPickerWidget(TestCase):
 
     layer = DatabaseFunctionalLayer
 
+    def getWidget(self, **kwargs):
+        class ITest(Interface):
+            test_field = Choice(**kwargs)
+        login(ANONYMOUS)
+        return InlineEditPickerWidget(None, None, ITest['test_field'], None)
+
     def test_huge_vocabulary_is_searchable(self):
         # Make sure that when given a field for a huge vocabulary, the picker
         # is set to show the search box.
-        class ITest(Interface):
-            test_field = Choice(vocabulary='ValidPersonOrTeam')
-        login(ANONYMOUS)
-        widget = InlineEditPickerWidget(
-            None, None, ITest['test_field'], None)
+        widget = self.getWidget(vocabulary='ValidPersonOrTeam')
         self.assertTrue(widget.show_search_box)
 
     def test_normal_vocabulary_is_not_searchable(self):
         # Make sure that when given a field for a normal vocabulary, the picker
         # is set to show the search box.
-        class ITest(Interface):
-            test_field = Choice(vocabulary='UserTeamsParticipation')
-        login(ANONYMOUS)
-        widget = InlineEditPickerWidget(
-            None, None, ITest['test_field'], None)
+        widget = self.getWidget(vocabulary='UserTeamsParticipation')
         self.assertFalse(widget.show_search_box)
+
+    def test_required_fields_dont_have_a_remove_link(self):
+        widget = self.getWidget(vocabulary='ValidPersonOrTeam', required=True)
+        self.assertFalse(widget.show_remove_button)
+
+    def test_optional_fields_do_have_a_remove_link(self):
+        widget = self.getWidget(
+            vocabulary='ValidPersonOrTeam', required=False)
+        self.assertTrue(widget.show_remove_button)
