1275f57f226a01bc19c923c0d5290bec5a18c115
Jeroen Vermeulen <jeroen.vermeulen@canonical.com>
2011-01-20 11:47:51 -0600

    Use revision.id, not revision.revision_id.

--- lib/lp/code/model/tests/test_branchjob.py
+++ lib/lp/code/model/tests/test_branchjob.py
@@ -519,7 +519,7 @@ class TestRevisionsAddedJob(TestCaseWithFactory):
                 branchrevision = IMasterStore(branch).find(
                     BranchRevision,
                     BranchRevision.branch_id == branch.id,
-                    BranchRevision.revision_id == revision.revision_id)
+                    BranchRevision.revision_id == revision.id)
                 branchrevision.remove()
             branch.createBranchRevision(revno, revision)
 
