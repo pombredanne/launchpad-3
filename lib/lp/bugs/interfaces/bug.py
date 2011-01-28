aa27f57a09d9180c5c863cc74b758d5ec4946aa3
Graham Binns <graham@canonical.com>
2011-01-28 04:03:12 -0600

    Reverted the reversion of devel r12272.

--- lib/lp/bugs/interfaces/bug.py
+++ lib/lp/bugs/interfaces/bug.py
@@ -22,6 +22,7 @@ __all__ = [
 
 from textwrap import dedent
 
+from lazr.enum import DBEnumeratedType
 from lazr.lifecycle.snapshot import doNotSnapshot
 from lazr.restful.declarations import (
     call_with,
@@ -432,7 +433,13 @@ class IBug(IPrivacy, IHasLinkedBranches):
     # subscription-related methods
 
     @operation_parameters(
-        person=Reference(IPerson, title=_('Person'), required=True))
+        person=Reference(IPerson, title=_('Person'), required=True),
+        # level actually uses BugNotificationLevel as its vocabulary,
+        # but due to circular import problems we fix that in
+        # _schema_circular_imports.py rather than here.
+        level=Choice(
+            vocabulary=DBEnumeratedType, required=False,
+            title=_('Level')))
     @call_with(subscribed_by=REQUEST_USER, suppress_notify=False)
     @export_write_operation()
     def subscribe(person, subscribed_by, suppress_notify=True, level=None):
