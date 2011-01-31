07a44563af54e3242291f17a9947ea76d2e3b0c5
Stuart Bishop <stuart.bishop@canonical.com>
2011-01-31 22:05:43 +0700

    Reenable test_devSampledata

--- lib/canonical/launchpad/tests/test_sampledata.py
+++ lib/canonical/launchpad/tests/test_sampledata.py
@@ -34,7 +34,7 @@ class SampleDataTestCase(TestCase):
         """Test the sample data used by the test suite."""
         self.dump_and_restore('launchpad_ftest_template')
 
-    def disabled_test_devSampledata(self):
+    def test_devSampledata(self):
         """Test the sample data used by developers for manual testing."""
         self.dump_and_restore('launchpad_dev_template')
 
