bf8f3fb00f35b01aaf41a4cb4b17d27689be97f9
Gary Poster <gary.poster@canonical.com>
2011-02-03 19:10:07 -0800

    make the helper a bit leaner

--- lib/lp/testing/dbuser.py
+++ lib/lp/testing/dbuser.py
@@ -15,18 +15,15 @@ from canonical.database.sqlbase import commit
 from canonical.testing.layers import LaunchpadZopelessLayer
 
 @contextmanager
-def dbuser(temporary_name, restore_name=None):
+def dbuser(temporary_name):
     """A context manager that temporarily changes the dbuser.
     
     Use with the LaunchpadZopelessLayer layer and subclasses.
     
     temporary_name is the name of the dbuser that should be in place for the
-    code in the "with" block.  restore_name is the name of the dbuser that
-    should be restored after the with block.  If it is not supplied, the
-    dbuser currently set (as far as LaunchpadZopelessLayer knows) is used.
+    code in the "with" block.
     """
-    if restore_name is None:
-        restore_name = LaunchpadZopelessLayer.txn._dbuser
+    restore_name = LaunchpadZopelessLayer.txn._dbuser
     commit()
     # Note that this will raise an assertion error if the
     # LaunchpadZopelessLayer is not already set up.
@@ -35,13 +32,9 @@ def dbuser(temporary_name, restore_name=None):
     commit()
     LaunchpadZopelessLayer.switchDbUser(restore_name)
 
-def lp_dbuser(restore_name=None):
+def lp_dbuser():
     """A context manager that temporarily changes to the launchpad dbuser.
     
     Use with the LaunchpadZopelessLayer layer and subclasses.
-    
-    restore_name is the name of the dbuser that should be restored after
-    the with block.  If it is not supplied, the dbuser currently set (as
-    far as LaunchpadZopelessLayer knows) is used.
     """
-    return dbuser('launchpad', restore_name=restore_name)
+    return dbuser('launchpad')
