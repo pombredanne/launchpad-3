35b26e8888a51fd9b6fac5de31dba4c33bcf4d4d
Aaron Bentley <aaron@canonical.com>
2011-02-08 12:11:11 -0500

    Check that TranslationMergeJob combines msgsets and translations.

--- lib/lp/translations/tests/test_translationmergejob.py
+++ lib/lp/translations/tests/test_translationmergejob.py
@@ -33,7 +33,7 @@ class TestTranslationMergeJob(TestCaseWithFactory):
         ubuntu_potemplate = self.factory.makePOTemplate(
             side=TranslationSide.UBUNTU, name=upstream_pofile.potemplate.name)
         ubuntu_pofile = self.factory.makePOFile(
-            potemplate=ubuntu_potemplate)
+            potemplate=ubuntu_potemplate, language=upstream_pofile.language)
         ubuntu_potmsgset = self.factory.makePOTMsgSet(
             ubuntu_pofile.potemplate, singular, sequence=1)
         ubuntu = self.factory.makeCurrentTranslationMessage(
@@ -50,15 +50,55 @@ class TestTranslationMergeJob(TestCaseWithFactory):
         job = self.makeTranslationMergeJob()
         verifyObject(IRunnableJob, job)
 
-    def test_run(self):
-        job = self.makeTranslationMergeJob()
-        self.becomeDbUser('rosettaadmin')
-        job.run()
+    @staticmethod
+    def getMsgSets(productseries=None, distroseries=None,
+                   sourcepackagename=None):
         msg_sets = []
-        for template in POTemplateSubset(productseries=job.productseries):
+        for template in POTemplateSubset(
+            productseries=productseries, distroseries=distroseries,
+            sourcepackagename=sourcepackagename):
             msg_sets.extend(template.getPOTMsgSets())
-        (package_template,) = list(POTemplateSubset(
+        return msg_sets
+
+    @classmethod
+    def getTranslations(cls, productseries=None, distroseries=None,
+                        sourcepackagename=None):
+        msg_sets = cls.getMsgSets(
+            productseries=productseries, distroseries=distroseries,
+            sourcepackagename=sourcepackagename)
+        translations = set()
+        for msg_set in msg_sets:
+            translations.update(msg_set.getAllTranslationMessages())
+        return translations
+
+    @classmethod
+    def countTranslations(cls, job):
+        tm = cls.getTranslations(productseries=job.productseries)
+        tm.update(cls.getTranslations(
             sourcepackagename=job.sourcepackagename,
             distroseries=job.distroseries))
-        (package_msg_set,) = list(package_template.getPOTMsgSets())
-        self.assertEqual([package_msg_set], msg_sets)
+        return len(tm)
+
+    def test_run_merges_msgset(self):
+        """Run should merge msgsets."""
+        job = self.makeTranslationMergeJob()
+        self.becomeDbUser('rosettaadmin')
+        product_msg = self.getMsgSets(productseries=job.productseries)
+        package_msg = self.getMsgSets(
+            sourcepackagename=job.sourcepackagename,
+            distroseries=job.distroseries)
+        self.assertNotEqual(package_msg, product_msg)
+        job.run()
+        product_msg = self.getMsgSets(productseries=job.productseries)
+        package_msg = self.getMsgSets(
+            sourcepackagename=job.sourcepackagename,
+            distroseries=job.distroseries)
+        self.assertEqual(package_msg, product_msg)
+
+    def test_run_merges_translations(self):
+        """Run should merge translations."""
+        job = self.makeTranslationMergeJob()
+        self.becomeDbUser('rosettaadmin')
+        self.assertEqual(2, self.countTranslations(job))
+        job.run()
+        self.assertEqual(1, self.countTranslations(job))
