6013df151a9b39291c4b19a8c853e819e75f5ef7
Aaron Bentley <aaron@canonical.com>
2011-02-09 10:39:26 -0500

    Update docs.

--- lib/lp/translations/model/translationmergejob.py
+++ lib/lp/translations/model/translationmergejob.py
@@ -1,7 +1,7 @@
 # Copyright 2011 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
-"""Jobs for merging translations."""
+"""Job for merging translations."""
 
 __metaclass__ = type
 
@@ -20,16 +20,25 @@ __all__ = ['TranslationMergeJob']
 
 
 class TranslationMergeJob(BaseRunnableJob):
+    """Job for merging translations between a product and sourcepackage."""
 
     implements(IRunnableJob)
 
     def __init__(self, job, productseries, distroseries, sourcepackagename):
+        """"Constructor.
+
+        :param job: The `Job` to use for storing basic job state.
+        :param productseries: The ProductSeries side of the Packaging.
+        :param distroseries: The distroseries of the Packaging sourcepackage.
+        :param sourcepackagename: The name of the Packaging sourcepackage.
+        """
         self.job = job
         self.distroseries = distroseries
         self.sourcepackagename = sourcepackagename
         self.productseries = productseries
 
     def run(self):
+        """See `IRunnableJob`."""
         template_map = dict()
         tm = TransactionManager(None, False)
         all_templates = list(POTemplateSubset(
