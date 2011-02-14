961bbeb55d32073b6394d9cbeda38b488f68f1ae
Ian Booth <ian.booth@canonical.com>
2011-02-14 16:29:47 +1000

    Fix issue with logging binary data in parameters

--- lib/canonical/launchpad/webapp/adapter.py
+++ lib/canonical/launchpad/webapp/adapter.py
@@ -608,11 +608,15 @@ class LaunchpadStatementTracer:
         if self._debug_sql_extra:
             traceback.print_stack()
             sys.stderr.write("." * 70 + "\n")
-        param_strings = list(Connection.to_database(params))
+        query_params = list(Connection.to_database(params))
         # We need to ensure % symbols used for LIKE statements etc are
         # properly quoted or else the string format operation will fail.
         quoted_statement = re.sub("%%%", "%%%%",
                                   re.sub("%(\W|$|\s)", r"%%\1", statement))
+        # We need to massage the query parameters a little to deal with string
+        # parameters which represent encoded binary data.
+        param_strings = [repr(p) if isinstance(p, basestring) else p
+                             for p in query_params]
         statement_with_values = quoted_statement % tuple(param_strings)
         if self._debug_sql or self._debug_sql_extra:
             sys.stderr.write(statement_with_values + "\n")
