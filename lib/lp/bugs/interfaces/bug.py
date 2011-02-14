b6fd8524c9979cda03573cabd847a7293c040448
Robert Collins <robert@canonical.com>
2011-02-14 11:15:22 +1100

    Remove the temporary bug message indexing code and simplify the message indexing logic as a result.

--- lib/lp/bugs/interfaces/bug.py
+++ lib/lp/bugs/interfaces/bug.py
@@ -464,9 +464,6 @@ class IBug(IPrivacy, IHasLinkedBranches):
     def unsubscribeFromDupes(person, unsubscribed_by):
         """Remove this person's subscription from all dupes of this bug."""
 
-    def reindexMessages():
-        """Fill in the `BugMessage`.index column for this bugs messages."""
-
     def isSubscribed(person):
         """Is person subscribed to this bug?
 
