e494fc88d46051fdf800ca4c2ea29500423926c8
Jonathan Lange <jml@canonical.com>
2011-02-17 16:13:35 +0000

    Remove canonical.base and its old tests

--- lib/canonical/tests/test_base.py
+++
@@ -1,9 +0,0 @@
-# Copyright 2009 Canonical Ltd.  This software is licensed under the
-# GNU Affero General Public License version 3 (see the file LICENSE).
-
-from doctest import DocTestSuite
-from lp.services.utils import base
-
-def test_suite():
-    suite = DocTestSuite(base.__doc__)
-    return suite
