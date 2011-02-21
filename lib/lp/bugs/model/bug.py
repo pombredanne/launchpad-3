4d20d5a16126fdb076175016bdde80faec30bc9a
Robert Collins <robert@canonical.com>
2011-02-21 13:38:56 +1100

    Move pillar owner access rule for bugs to userCanView, removing 75 queries per bug search page.

--- lib/lp/bugs/model/bug.py
+++ lib/lp/bugs/model/bug.py
@@ -1779,11 +1779,15 @@ BugMessage""" % sqlvalues(self.id))
     def _known_viewers(self):
         """A set of known persons able to view this bug.
 
-        Seed it by including the list of all owners of pillars for bugtasks
-        for the bug.
+        This method must return an empty set or bug searches will trigger late
+        evaluation. Any 'should be set on load' propertis must be done by the
+        bug search.
+
+        If you are tempted to change this method, don't. Instead see
+        userCanView which defines the just-in-time policy for bug visibility,
+        and BugTask._search which honours visibility rules.
         """
-        pillar_owners = [bt.pillar.owner.id for bt in self.bugtasks]
-        return set(pillar_owners)
+        return set()
 
     def userCanView(self, user):
         """See `IBug`.
@@ -1817,6 +1821,11 @@ BugMessage""" % sqlvalues(self.id))
             # Explicit subscribers may also view it.
             for subscription in self.subscriptions:
                 if user.inTeam(subscription.person):
+                    self._known_viewers.add(user.id)
+                    return True
+            # Pillar owners can view it.
+            for pillar_owner in [bt.pillar.owner for bt in self.bugtasks]:
+                if user.inTeam(pillar_owner):
                     self._known_viewers.add(user.id)
                     return True
         return False
