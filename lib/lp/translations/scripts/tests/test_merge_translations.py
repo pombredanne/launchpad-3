ee0b620e6cd3e95da18cbe870536178771657b51
Aaron Bentley <aaron@canonical.com>
2011-02-25 14:12:36 -0500

    Test running TranslationSplitJob from script.

--- lib/lp/translations/scripts/tests/test_merge_translations.py
+++ lib/lp/translations/scripts/tests/test_merge_translations.py
@@ -10,6 +10,8 @@ import transaction
 
 from canonical.launchpad.scripts.tests import run_script
 from canonical.testing.layers import ZopelessAppServerLayer
+from lp.translations.model.translationpackagingjob import (
+    TranslationSplitJob)
 from lp.testing import TestCaseWithFactory
 from lp.translations.tests.test_translationpackagingjob import (
     count_translations,
@@ -23,6 +25,8 @@ class TestMergeTranslations(TestCaseWithFactory):
 
     def test_merge_translations(self):
         job = make_translation_merge_job(self.factory)
+        TranslationSplitJob.create(
+            job.productseries, job.distroseries, job.sourcepackagename)
         transaction.commit()
         retcode, stdout, stderr = run_script(
             'cronscripts/run_jobs.py', ['merge_translations'],
@@ -32,6 +36,6 @@ class TestMergeTranslations(TestCaseWithFactory):
             INFO    Running synchronously.
             INFO    Deleted POTMsgSets: 1.  TranslationMessages: 1.
             INFO    Ran 1 TranslationMergeJob jobs.
+            INFO    Ran 1 TranslationSplitJob jobs.
             """), stderr)
         self.assertEqual('', stdout)
-        self.assertEqual(1, count_translations(job))
