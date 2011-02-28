78418cb7ee48f64513b3454eabb2e5bbc981c094
William Grant <william.grant@canonical.com>
2011-02-28 19:59:27 +1100

    Log bugtracker-wide update errors to the log. Only UnknownBugTrackerTypeErrors get OOPSes.

--- lib/lp/bugs/scripts/checkwatches/core.py
+++ lib/lp/bugs/scripts/checkwatches/core.py
@@ -414,10 +414,17 @@ class CheckwatchesMaster(WorkingBase):
                 with self.transaction:
                     getUtility(IBugWatchSet).bulkSetError(
                         bug_watches_to_update, error_type)
-                message = (
-                    "ExternalBugtracker for BugTrackerType '%s' is not "
-                    "known." % (error.bugtrackertypename))
-                self.warning(message)
+                    self.logger.info(
+                        "'%s' error updating %s: %s" % (
+                            error_type.title, bug_tracker.baseurl, error))
+                # This is retained to make tests happy. It will be
+                # removed later when the checkwatches OOPS
+                # infrastructure can be.
+                if isinstance(error, UnknownBugTrackerTypeError):
+                    message = (
+                        "ExternalBugtracker for BugTrackerType '%s' is not "
+                        "known." % (error.bugtrackertypename))
+                    self.warning(message)
             else:
                 for remotesystem, bug_watch_batch in trackers_and_watches:
                     self.updateBugWatches(
