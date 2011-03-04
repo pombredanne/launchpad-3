a241fc112b1f18e1fb78d0691ec2135eee7aeb4b
Stuart Bishop <stuart.bishop@canonical.com>
2011-03-04 18:42:26 +0700

    Fix comment in test template - declaring tests to use BaseLayer is better than no layer due to test runner command line limitations

--- lib/lp/testing/tests/test_standard_test_template.py
+++ lib/lp/testing/tests/test_standard_test_template.py
@@ -12,9 +12,9 @@ from lp.testing import TestCase # or TestCaseWithFactory
 class TestSomething(TestCase):
     # XXX: Sample test class.  Replace with your own test class(es).
 
-    # XXX: Optional layer--see lib/canonical/testing/layers.py
-    # Get the simplest layer that your test will work on, or if you
-    # don't even use the database, don't set it at all.
+    # XXX: layer--see lib/canonical/testing/layers.py
+    # Get the simplest layer that your test will work on. For unit tests
+    # requiring no resources, this is BaseLayer.
     layer = DatabaseFunctionalLayer
 
     # XXX: Sample test.  Replace with your own test methods.
