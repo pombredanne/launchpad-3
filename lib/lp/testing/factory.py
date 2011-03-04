a1acfa72498d1fad3c8a100bf989fc2be4beb03e
William Grant <william.grant@canonical.com>
2011-03-04 15:28:07 +1100

    makeDistroArchSeries now takes an enabled arg.

--- lib/lp/testing/factory.py
+++ lib/lp/testing/factory.py
@@ -2307,7 +2307,7 @@ class BareLaunchpadObjectFactory(ObjectFactory):
     def makeDistroArchSeries(self, distroseries=None,
                              architecturetag=None, processorfamily=None,
                              official=True, owner=None,
-                             supports_virtualized=False):
+                             supports_virtualized=False, enabled=True):
         """Create a new distroarchseries"""
 
         if distroseries is None:
@@ -2323,7 +2323,7 @@ class BareLaunchpadObjectFactory(ObjectFactory):
             architecturetag = self.getUniqueString('arch')
         return distroseries.newArch(
             architecturetag, processorfamily, official, owner,
-            supports_virtualized)
+            supports_virtualized, enabled)
 
     def makeComponent(self, name=None):
         """Make a new `IComponent`."""
