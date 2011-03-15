1464277025cd64d89b13c4d3617977949c87b886
Tim Penhey <tim.penhey@canonical.com>
2011-03-16 11:20:08 +1300

    More tests.

--- lib/lp/bugs/tests/test_bugtaskfilter.py
+++ lib/lp/bugs/tests/test_bugtaskfilter.py
@@ -5,6 +5,7 @@
 
 __metaclass__ = type
 
+from storm.locals import Store
 from testtools.matchers import Equals
 
 from canonical.testing.layers import DatabaseFunctionalLayer
@@ -22,3 +23,27 @@ class TestFilterBugTasksByContext(TestCaseWithFactory):
         self.assertThat(
             filter_bugtasks_by_context(None, tasks),
             Equals(tasks))
+
+    def test_two_product_tasks_case_no_context(self):
+        widget = self.factory.makeProduct()
+        bug = self.factory.makeBug(product=widget)
+        # Make sure the bug and the first task is flushed first.
+        Store.of(bug).flush()
+        cogs = self.factory.makeProduct()
+        self.factory.makeBugTask(bug=bug, target=cogs)
+        tasks = list(bug.bugtasks)
+        self.assertThat(
+            filter_bugtasks_by_context(None, tasks),
+            Equals([bug.getBugTask(widget)]))
+
+    def test_two_product_tasks_case(self):
+        widget = self.factory.makeProduct()
+        bug = self.factory.makeBug(product=widget)
+        # Make sure the bug and the first task is flushed first.
+        Store.of(bug).flush()
+        cogs = self.factory.makeProduct()
+        task = self.factory.makeBugTask(bug=bug, target=cogs)
+        tasks = list(bug.bugtasks)
+        self.assertThat(
+            filter_bugtasks_by_context(cogs, tasks),
+            Equals([task]))
