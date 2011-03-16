4283368e37b3f6c9c273f8c9de6e70213445b50c
William Grant <william.grant@canonical.com>
2011-03-16 15:18:01 +1100

    Setify primary keys before bulk loading.

--- lib/lp/services/database/bulk.py
+++ lib/lp/services/database/bulk.py
@@ -69,7 +69,7 @@ def load(object_type, primary_keys, store=None):
             "Compound primary keys are not supported: %s." %
             object_type.__name__)
     primary_key_column = primary_key[0]
-    condition = primary_key_column.is_in(primary_keys)
+    condition = primary_key_column.is_in(set(primary_keys))
     if store is None:
         store = IStore(object_type)
     query = store.find(object_type, condition)
