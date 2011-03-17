d4015a78311fe7c512554721a372d5c691b1fa71
Raphael Badin <raphael.badin@canonical.com>
2011-03-17 12:18:11 +0100

    Rename column instead of dropping/adding it.

--- database/schema/patch-2208-99-0.sql
+++ database/schema/patch-2208-99-0.sql
@@ -5,22 +5,16 @@ SET client_min_messages=ERROR;
 
 -- Renaming owner to registrant for DistroSeries
 
--- Add a registrant column to DistroSeries.
-ALTER TABLE DistroSeries
-    ADD COLUMN registrant integer REFERENCES Person;
+-- Rename owner into registrant.
+ALTER TABLE distroseries 
+    RENAME COLUMN owner TO registrant;
 
--- Migrate owner to registrant.
-UPDATE DistroSeries
-    SET registrant = owner;
+-- 'Rename' constraint.
+ALTER TABLE distroseries 
+    ADD CONSTRAINT distroseries__registrant__fk 
+    FOREIGN KEY (registrant) REFERENCES Person(id);
+ALTER TABLE distroseries
+    DROP CONSTRAINT distroseries__owner__fk;
 
--- Kill the old column.
-ALTER TABLE DistroSeries
-    DROP COLUMN owner;
-
--- Add NOT NULL constraint to registrant column.
-ALTER TABLE DistroSeries ALTER COLUMN registrant SET NOT NULL;
-
--- Add index.
-CREATE INDEX distroseries__registrant__idx ON DistroSeries(registrant);
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2208, 99, 0);
