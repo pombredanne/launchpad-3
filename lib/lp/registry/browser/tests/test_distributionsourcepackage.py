4ec6d923a1792bbbfc4c7737fa8082b93e4cd2cb
Curtis Hovey <curtis.hovey@canonical.com>
2011-03-18 19:52:06 -0400

    Revised the test for clarity.

--- lib/lp/registry/browser/tests/test_distributionsourcepackage.py
+++ lib/lp/registry/browser/tests/test_distributionsourcepackage.py
@@ -68,40 +68,40 @@ class TestDistributionSourceView(TestCaseWithFactory):
         self.dsp = distro.getSourcePackage('mouse')
 
     def test_bugs_answers_usage_none(self):
-        # The dict values are all None.
+        # The dict values are all False.
         view = create_view(self.dsp, '+index')
-        self.assertFalse(view.bugs_answers_usage['uses_bugs'])
-        self.assertFalse(view.bugs_answers_usage['uses_answers'])
-        self.assertFalse(view.bugs_answers_usage['uses_both'])
-        self.assertFalse(view.bugs_answers_usage['uses_either'])
+        self.assertIs(False, view.bugs_answers_usage['uses_bugs'])
+        self.assertIs(False, view.bugs_answers_usage['uses_answers'])
+        self.assertIs(False, view.bugs_answers_usage['uses_both'])
+        self.assertIs(False, view.bugs_answers_usage['uses_either'])
 
     def test_bugs_answers_usage_bugs(self):
-        # The dict values are all None.
+        # The dict values are True for bugs and either.
         with celebrity_logged_in('admin'):
             self.dsp.distribution.official_malone = True
         view = create_view(self.dsp, '+index')
-        self.assertTrue(view.bugs_answers_usage['uses_bugs'])
-        self.assertFalse(view.bugs_answers_usage['uses_answers'])
-        self.assertFalse(view.bugs_answers_usage['uses_both'])
-        self.assertTrue(view.bugs_answers_usage['uses_either'])
+        self.assertIs(True, view.bugs_answers_usage['uses_bugs'])
+        self.assertIs(False, view.bugs_answers_usage['uses_answers'])
+        self.assertIs(False, view.bugs_answers_usage['uses_both'])
+        self.assertIs(True, view.bugs_answers_usage['uses_either'])
 
     def test_bugs_answers_usage_answers(self):
-        # The dict values are all None.
+        # The dict values are True for answers and either.
         with celebrity_logged_in('admin'):
             self.dsp.distribution.answers_usage = ServiceUsage.LAUNCHPAD
         view = create_view(self.dsp, '+index')
-        self.assertFalse(view.bugs_answers_usage['uses_bugs'])
-        self.assertTrue(view.bugs_answers_usage['uses_answers'])
-        self.assertFalse(view.bugs_answers_usage['uses_both'])
-        self.assertTrue(view.bugs_answers_usage['uses_either'])
+        self.assertIs(False, view.bugs_answers_usage['uses_bugs'])
+        self.assertIs(True, view.bugs_answers_usage['uses_answers'])
+        self.assertIs(False, view.bugs_answers_usage['uses_both'])
+        self.assertIs(True, view.bugs_answers_usage['uses_either'])
 
     def test_bugs_answers_usage_both(self):
-        # The dict values are all None.
+        # The dict values are all True.
         with celebrity_logged_in('admin'):
             self.dsp.distribution.official_malone = True
             self.dsp.distribution.answers_usage = ServiceUsage.LAUNCHPAD
         view = create_view(self.dsp, '+index')
-        self.assertTrue(view.bugs_answers_usage['uses_bugs'])
-        self.assertTrue(view.bugs_answers_usage['uses_answers'])
-        self.assertTrue(view.bugs_answers_usage['uses_both'])
-        self.assertTrue(view.bugs_answers_usage['uses_either'])
+        self.assertIs(True, view.bugs_answers_usage['uses_bugs'])
+        self.assertIs(True, view.bugs_answers_usage['uses_answers'])
+        self.assertIs(True, view.bugs_answers_usage['uses_both'])
+        self.assertIs(True, view.bugs_answers_usage['uses_either'])
