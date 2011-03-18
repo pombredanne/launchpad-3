aece9ccaaaafc42180219047f87ac065b834b1dd
Raphael Badin <raphael.badin@canonical.com>
2011-03-18 12:19:52 +0100

    Fix db patch (db patch 56).

--- database/schema/patch-2208-56-0.sql
+++ database/schema/patch-2208-56-0.sql
@@ -16,5 +16,17 @@ ALTER TABLE distroseries
 ALTER TABLE distroseries
     DROP CONSTRAINT distroseries__owner__fk;
 
+-- Rename index.
+ALTER INDEX distroseries__owner__idx 
+    RENAME TO distroseries__registrant__idx;
 
-INSERT INTO LaunchpadDatabaseRevision VALUES (2208, 99, 0);
+-- Rename old misnamed indexes.
+ALTER INDEX distrorelease_pkey
+    RENAME TO distroseries_pkey;
+ALTER INDEX distrorelease_distribution_key
+    RENAME TO distrorelease__distribution__name__key;
+ALTER INDEX distrorelease_distro_release_unique
+    RENAME TO distroseries__distribution__id__key;
+
+
+INSERT INTO LaunchpadDatabaseRevision VALUES (2208, 56, 0);
