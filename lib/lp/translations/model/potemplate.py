61d02055f5727c5538087da28065fe61c84dac86
Henning Eggers <henning@canonical.com>
2011-03-23 07:13:40 +0100

    Removed pillar from TranslationTemplateCollection.

--- lib/lp/translations/model/potemplate.py
+++ lib/lp/translations/model/potemplate.py
@@ -1649,39 +1649,15 @@ class TranslationTemplatesCollection(Collection):
     """A `Collection` of `POTemplate`."""
     starting_table = POTemplate
 
-    # The Product or Distribution that this collection is restricted to.
-    target_pillar = None
-
-    def __init__(self, *args, **kwargs):
-        super(TranslationTemplatesCollection, self).__init__(*args, **kwargs)
-        if self.base is not None:
-            self.target_pillar = self.base.target_pillar
-
     def restrictProductSeries(self, productseries):
-        product = productseries.product
-        new_collection = self.refine(
-            POTemplate.productseriesID == productseries.id)
-        new_collection._setTargetPillar(product)
-        return new_collection
+        return self.refine(POTemplate.productseriesID == productseries.id)
 
     def restrictDistroSeries(self, distroseries):
-        distribution = distroseries.distribution
-        new_collection = self.refine(
-            POTemplate.distroseriesID == distroseries.id)
-        new_collection._setTargetPillar(distribution)
-        return new_collection
+        return self.refine(POTemplate.distroseriesID == distroseries.id)
 
     def restrictSourcePackageName(self, sourcepackagename):
         return self.refine(
             POTemplate.sourcepackagenameID == sourcepackagename.id)
-
-    def _setTargetPillar(self, target_pillar):
-        assert (
-            self.target_pillar is None or
-            self.target_pillar == target_pillar), (
-                "Collection restricted to both %s and %s." % (
-                    self.target_pillar, target_pillar))
-        self.target_pillar = target_pillar
 
     def restrictCurrent(self, current_value=True):
         """Select based on `POTemplate.iscurrent`.
