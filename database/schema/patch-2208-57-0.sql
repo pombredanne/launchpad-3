98b1c66b490a9492ca87bc76d6c7d6b5edef69d4
William Grant <william.grant@canonical.com>
2011-03-29 18:21:45 +1100

    Official patch number

--- database/schema/patch-2208-57-0.sql
+++ database/schema/patch-2208-57-0.sql
@@ -3,4 +3,4 @@ SET client_min_messages=ERROR;
 ALTER TABLE distroseries
     ADD COLUMN backports_not_automatic BOOLEAN NOT NULL DEFAULT FALSE;
 
-INSERT INTO LaunchpadDatabaseRevision VALUES (2208, 99, 0);
+INSERT INTO LaunchpadDatabaseRevision VALUES (2208, 57, 0);
