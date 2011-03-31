b2fd53b759ca8f5a45318cd00692aaba9ea2ade7
Jonathan Lange <jml@canonical.com>
2011-03-31 13:34:21 +0100

    Restore timeout after successful experiment.

--- lib/devscripts/ec2test/builtins.py
+++ lib/devscripts/ec2test/builtins.py
@@ -326,7 +326,7 @@ class cmd_test(EC2Command):
             open_browser=open_browser, pqm_email=pqm_email,
             include_download_cache_changes=include_download_cache_changes,
             instance=instance, launchpad_login=instance._launchpad_login,
-            timeout=5)
+            timeout=480)
 
         instance.set_up_and_run(postmortem, attached, runner.run_tests)
 
