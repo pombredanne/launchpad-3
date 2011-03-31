457604e40c5154b634da2ff8091c6d38c08bca83
Jeroen Vermeulen <jeroen.vermeulen@canonical.com>
2011-03-31 12:12:09 +0700

    Somre review changes.

--- cronscripts/publish-ftpmaster.py
+++ cronscripts/publish-ftpmaster.py
@@ -3,7 +3,7 @@
 # Copyright 2011 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
-"""Master FTP distro publishing script."""
+"""Master distro publishing script."""
 
 import _pythonpath
 
