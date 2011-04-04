6bf7a1e63627e6c790cf256d65dc7e70919866b1
Gavin Panella <gavin.panella@canonical.com>
2011-04-04 16:01:19 +0100

    Add the ability to add help links onto widgets via the view.

--- lib/lp/app/browser/tests/test_launchpadform.py
+++ lib/lp/app/browser/tests/test_launchpadform.py
@@ -5,16 +5,23 @@
 
 __metaclass__ = type
 
+from lxml import html
+from z3c.ptcompat import ViewPageTemplateFile
 from zope.interface import Interface
 from zope.schema import Text
 
+from canonical.config import config
 from canonical.launchpad.webapp.servers import LaunchpadTestRequest
 from canonical.testing.layers import DatabaseFunctionalLayer
 from lp.app.browser.launchpadform import (
     has_structured_doc,
     LaunchpadFormView,
     )
-from lp.testing import TestCase, test_tales
+from lp.testing import (
+    test_tales,
+    TestCase,
+    TestCaseWithFactory,
+    )
 
 
 class TestInterface(Interface):
@@ -64,3 +71,65 @@ class TestQueryTalesForHasStructuredDoc(TestCase):
                 'widget/query:has-structured-doc', widget=normal_widget))
         self.assertTrue(test_tales(
                 'widget/query:has-structured-doc', widget=structured_widget))
+
+
+class TestHelpLinksInterface(Interface):
+    """Test interface for the view below."""
+
+    nickname = Text(title=u'name')
+
+    displayname = Text(title=u'name')
+
+
+class TestHelpLinksView(LaunchpadFormView):
+    """A trivial view that contains help links."""
+
+    schema = TestHelpLinksInterface
+
+    page_title = u"TestHelpLinksView"
+    template = ViewPageTemplateFile(
+        config.root + '/lib/lp/app/templates/generic-edit.pt')
+
+    help_links = {
+        "nickname": u"http://widget.example.com/name",
+        "displayname": u"http://widget.example.com/displayname",
+        }
+
+
+class TestHelpLinks(TestCaseWithFactory):
+
+    layer = DatabaseFunctionalLayer
+
+    def test_help_links_on_widget(self):
+        # The values in a view's help_links dictionary gets copied into the
+        # corresponding widgets' help_link attributes.
+        request = LaunchpadTestRequest()
+        view = TestHelpLinksView(None, request)
+        view.initialize()
+        nickname_widget, displayname_widget = list(view.widgets)
+        self.assertEqual(
+            u"http://widget.example.com/name",
+            nickname_widget.help_link)
+        self.assertEqual(
+            u"http://widget.example.com/displayname",
+            displayname_widget.help_link)
+
+    def test_help_links_render(self):
+        # The values in a view's help_links dictionary are rendered in the
+        # default generic-edit template.
+        user = self.factory.makePerson()
+        request = LaunchpadTestRequest(PATH_INFO="/")
+        request.setPrincipal(user)
+        view = TestHelpLinksView(user, request)
+        view.initialize()
+        root = html.fromstring(view.render())
+        [nickname_help_link] = root.cssselect(
+            "label[for$=nickname] ~ a[target=help]")
+        self.assertEqual(
+            u"http://widget.example.com/name",
+            nickname_help_link.get("href"))
+        [displayname_help_link] = root.cssselect(
+            "label[for$=displayname] ~ a[target=help]")
+        self.assertEqual(
+            u"http://widget.example.com/displayname",
+            displayname_help_link.get("href"))
