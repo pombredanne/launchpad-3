ce36678979acba71545667bbd012415035dfc6ba
Graham Binns <graham@canonical.com>
2011-04-04 11:13:47 +0100

    Fixed every test failure in the entire world, it seems, with one line of code.

--- lib/lp/registry/model/person.py
+++ lib/lp/registry/model/person.py
@@ -3963,6 +3963,9 @@ class PersonSet:
         self._mergeBugNotificationRecipient(cur, from_id, to_id)
         skip.append(('bugnotificationrecipient', 'person'))
 
+        # We ignore BugSubscriptionFilterMutes.
+        skip.append(('bugsubscriptionfiltermute', 'person'))
+
         self._mergePackageBugSupervisor(cur, from_id, to_id)
         skip.append(('packagebugsupervisor', 'bug_supervisor'))
 
