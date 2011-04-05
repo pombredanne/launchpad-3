e42e12a8bd6c257b22367995bd0e3197ab7d80de
Curtis Hovey <curtis.hovey@canonical.com>
2011-04-05 12:02:29 -0400

    Inlined mlist-sync test helpers. Remove unused module.

--- lib/lp/services/mailman/tests/test_mlist_sync.py
+++ lib/lp/services/mailman/tests/test_mlist_sync.py
@@ -7,21 +7,26 @@ __all__ = []
 
 from contextlib import contextmanager
 import os
+import shutil
 import sys
+import tempfile
 from textwrap import dedent
 from transaction import commit
 from subprocess import Popen, PIPE
 
 from Mailman import mm_cfg
+from Mailman.MailList import MailList
+from Mailman.Utils import list_names
+
+from zope.component import getUtility
 
 from canonical.config import config
+from canonical.launchpad.interfaces.emailaddress import IEmailAddressSet
 from canonical.launchpad.interfaces.lpstorm import IStore
 from canonical.testing.layers import DatabaseFunctionalLayer
-from canonical.database.sqlbase import rollback
-from lp.services.mailman.testing import (
-    MailmanTestCase,
-    sync,
-    )
+from lp.registry.interfaces.person import IPersonSet
+from lp.services.mailman.testing import MailmanTestCase
+from lp.testing import celebrity_logged_in
 
 
 @contextmanager
@@ -56,18 +61,30 @@ class TestMListSync(MailmanTestCase):
             self.mailing_list = self.factory.makeMailingList(
                 self.team, self.team.teamowner)
             self.mm_list = self.makeMailmanList(self.mailing_list)
+        self.email_address_set = getUtility(IEmailAddressSet)
+        self.person_set = getUtility(IPersonSet)
 
     def tearDown(self):
         super(TestMListSync, self).tearDown()
         self.cleanMailmanList(self.mm_list)
 
-    def runMListSync(self, sync_details):
+    def setupProductionFiles(self):
+        "Setup a production file structure to sync."
+        tempdir = tempfile.mkdtemp()
+        source_dir = os.path.join(tempdir, 'production')
+        shutil.copytree(
+            config.mailman.build_var_dir, source_dir, symlinks=True)
+        mhonarc_path = os.path.join(
+            mm_cfg.VAR_PREFIX, 'mhonarc', 'fake-team')
+        return source_dir
+
+    def runMListSync(self, source_dir):
         """Run mlist-sync.py."""
         IStore(self.team).flush()
         commit()
         proc = Popen(
             ('scripts/mlist-sync.py', '--hostname',
-             'lists.prod.launchpad.dev', sync_details.source_dir),
+             'lists.prod.launchpad.dev', source_dir),
             stdout=PIPE, stderr=PIPE,
             cwd=config.root,
             env=dict(LPCONFIG=DatabaseFunctionalLayer.appserver_config_name,
@@ -76,11 +93,32 @@ class TestMListSync(MailmanTestCase):
         stdout, stderr = proc.communicate()
         return proc.returncode, stderr
 
+    def getListInfo(self):
+        """Return a list of 4-tuples of Mailman mailing list info."""
+        list_info = []
+        for list_name in sorted(list_names()):
+            if list_name == mm_cfg.MAILMAN_SITE_LIST:
+                continue
+            team = self.person_set.getByName(list_name)
+            emails = []
+            if team is None:
+                emails.append('No Launchpad team: %s' % list_name)
+            else:
+                addresses = self.email_address_set.getByPerson(team)
+                with celebrity_logged_in('admin'):
+                    for email in sorted(email.email for email in addresses):
+                        emails.append(email)
+            mailing_list = MailList(list_name, lock=False)
+            list_info.append(
+                (mailing_list.internal_name(), mailing_list.host_name,
+                 mailing_list.web_page_url, ' '.join(emails)))
+        return list_info
+
     def test_staging_sync(self):
         # List is synced with updated URLs and email addresses.
-        sync_details = sync.prepare_for_sync()
-        self.addCleanup(sync_details.cleanup)
-        returncode, stderr = self.runMListSync(sync_details)
+        source_dir = self.setupProductionFiles()
+        self.addCleanup(shutil.rmtree, source_dir)
+        returncode, stderr = self.runMListSync(source_dir)
         self.assertEqual(0, returncode, stderr)
         #rollback()
         IStore(self.team).invalidate()
@@ -90,4 +128,4 @@ class TestMListSync(MailmanTestCase):
             'http://lists.launchpad.dev/mailman/',
             'team-1@lists.launchpad.dev'),
             ]
-        self.assertEqual(list_summary, sync.dump_list_info())
+        self.assertEqual(list_summary, self.getListInfo())
