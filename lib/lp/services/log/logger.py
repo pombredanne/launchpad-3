3cbf62bedf7338ba2e07acc2ee981d610752c05b
Stuart Bishop <stuart.bishop@canonical.com>
2011-04-05 19:41:25 +0700

    Move LaunchpadLogger to a better location

--- lib/lp/services/log/logger.py
+++ lib/lp/services/log/logger.py
@@ -1,15 +1,17 @@
 # Copyright 2010-2011 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
-"""Simple loggers."""
+"""Loggers."""
 
 __metaclass__ = type
 __all__ = [
     'BufferLogger',
     'DevNullLogger',
     'FakeLogger',
+    'LaunchpadLogger',
     ]
 
+import logging
 from StringIO import StringIO
 import sys
 import traceback
@@ -28,6 +30,46 @@ LEVEL_PREFIXES.update({
     loglevels.ERROR: 'ERROR',
     loglevels.CRITICAL: 'CRITICAL',
 })
+
+
+class LaunchpadLogger(logging.Logger):
+    """Logger that support our custom levels."""
+
+    def debug1(self, msg, *args, **kwargs):
+        if self.isEnabledFor(loglevels.DEBUG1):
+            self._log(loglevels.DEBUG1, msg, args, **kwargs)
+
+    def debug2(self, msg, *args, **kwargs):
+        if self.isEnabledFor(loglevels.DEBUG2):
+            self._log(loglevels.DEBUG2, msg, args, **kwargs)
+
+    def debug3(self, msg, *args, **kwargs):
+        if self.isEnabledFor(loglevels.DEBUG3):
+            self._log(loglevels.DEBUG3, msg, args, **kwargs)
+
+    def debug4(self, msg, *args, **kwargs):
+        if self.isEnabledFor(loglevels.DEBUG4):
+            self._log(loglevels.DEBUG4, msg, args, **kwargs)
+
+    def debug5(self, msg, *args, **kwargs):
+        if self.isEnabledFor(loglevels.DEBUG5):
+            self._log(loglevels.DEBUG5, msg, args, **kwargs)
+
+    def debug6(self, msg, *args, **kwargs):
+        if self.isEnabledFor(loglevels.DEBUG6):
+            self._log(loglevels.DEBUG6, msg, args, **kwargs)
+
+    def debug7(self, msg, *args, **kwargs):
+        if self.isEnabledFor(loglevels.DEBUG7):
+            self._log(loglevels.DEBUG7, msg, args, **kwargs)
+
+    def debug8(self, msg, *args, **kwargs):
+        if self.isEnabledFor(loglevels.DEBUG8):
+            self._log(loglevels.DEBUG8, msg, args, **kwargs)
+
+    def debug9(self, msg, *args, **kwargs):
+        if self.isEnabledFor(loglevels.DEBUG9):
+            self._log(loglevels.DEBUG9, msg, args, **kwargs)
 
 
 class FakeLogger:
