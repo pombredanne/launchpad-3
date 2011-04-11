e84fd53c0d07e31c4b97e47e229f7c46e792892d
Ian Booth <ian.booth@canonical.com>
2011-04-11 10:18:45 +1000

    Add new windmill, unit, javascript tests

--- lib/lp/bugs/windmill/tests/test_bug_inline_assignment.py
+++ lib/lp/bugs/windmill/tests/test_bug_inline_assignment.py
@@ -14,7 +14,7 @@ class TestInlineAssignment(WindmillTestCase):
     layer = BugsWindmillLayer
 
     def test_inline_assignment_non_contributer(self):
-        """Test assigning bug to a non contributer displays a notification."""
+        """Test assigning bug to a non contributor displays a confirmation."""
 
         import transaction
         # Create a person who has not contributed
@@ -24,32 +24,25 @@ class TestInlineAssignment(WindmillTestCase):
         client, start_url = self.getClientFor(
             "/firefox/+bug/1", lpuser.SAMPLE_PERSON)
 
-        ASSIGN_BUTTON = (u'("#affected-software tr td:nth-child(5) '
-            '.yui3-activator-act")')
-        client.waits.forElement(jquery=ASSIGN_BUTTON, timeout=FOR_ELEMENT)
-        client.click(jquery=ASSIGN_BUTTON+'[0]')
+        ASSIGN_BUTTON = (u'//*[@id="affected-software"]//tr//td[5]' +
+            '//button[contains(@class, "yui3-activator-act")]')
+        client.waits.forElement(xpath=ASSIGN_BUTTON, timeout=FOR_ELEMENT)
+        client.click(xpath=ASSIGN_BUTTON)
 
         VISIBLE_PICKER_OVERLAY = (
             u'//div[contains(@class, "yui3-picker ") and '
              'not(contains(@class, "yui3-picker-hidden"))]')
 
-        def full_picker_element_xpath(element_path):
-            return VISIBLE_PICKER_OVERLAY + element_path
-
         client.waits.forElement(
             xpath=VISIBLE_PICKER_OVERLAY, timeout=FOR_ELEMENT)
+
+        def full_picker_element_xpath(element_path):
+            return VISIBLE_PICKER_OVERLAY + element_path
 
         client.type(xpath=full_picker_element_xpath(
             "//input[@class='yui3-picker-search']"), text='fred')
         client.click(xpath=full_picker_element_xpath(
             "//div[@class='yui3-picker-search-box']/button"))
-#        client.waits.forElement(
-#            jquery=u'(.yui-picker-assign-me-button)', timeout=FOR_ELEMENT)
-#        client.type(jquery=u'(.yui3-picker-search)', text='fred')
-#        client.click(jquery=u'(button.lazr-search)[0]')
-#        client.click(jquery=u'(button.lazr-search)[0]')
-#        client.waits.forElement(
-#            jquery='u(ul.yui3-picker-results)[0]', timeout=FOR_ELEMENT)
 
         PICKER_RESULT = full_picker_element_xpath(
             "//ul[@class='yui3-picker-results']/li[1]/span")
@@ -57,9 +50,8 @@ class TestInlineAssignment(WindmillTestCase):
         client.waits.forElement(xpath=PICKER_RESULT, timeout=FOR_ELEMENT)
         client.click(xpath=PICKER_RESULT)
 
-        WARNING_NOTIFICATION = ("//div[contains(@class, 'warning') and "
-            "contains(@class, 'message')]")
-        client.waits.forElement(xpath=WARNING_NOTIFICATION, timeout=60000)
+        CONFIRMATION = ("//div[@class='important-notice-balloon']")
+        client.waits.forElement(xpath=CONFIRMATION, timeout=FOR_ELEMENT)
         self.client.asserts.assertTextIn(
-            xpath=WARNING_NOTIFICATION,
+            xpath=CONFIRMATION,
             validator="Fred did not previously have any assigned bugs")
