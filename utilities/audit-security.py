0b39350b32e2899b5a1f6b370eb7907a83be3b69
j.c.sackett <jonathan.sackett@canonical.com>
2011-04-18 12:55:47 -0400

    Added audit-security.py script.

---
+++ utilities/audit-security.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python -S
+
+# Copyright 2011 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Check that everything is alright in security.cfg
+
+Usage hint:
+
+% utilities/audit-security.py
+"""
+
+import os
+
+
