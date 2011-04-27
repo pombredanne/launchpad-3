c181f9bd8b68015e0fa46dcf6480234a2f5a58b9
Henning Eggers <henning@canonical.com>
2011-04-27 18:14:32 +0200

    Moved AuthorizationBAse.

--- lib/canonical/launchpad/security.py
+++ lib/canonical/launchpad/security.py
@@ -8,20 +8,13 @@
 __metaclass__ = type
 __all__ = [
     'AnonymousAuthorization',
-    'AuthorizationBase',
     ]
 
 from zope.component import (
     getAdapter,
     getUtility,
     )
-from zope.interface import (
-    implements,
-    Interface,
-    )
-from zope.security.permission import (
-    checkPermission as check_permission_is_registered,
-    )
+from zope.interface import Interface
 
 from canonical.config import config
 from canonical.launchpad.interfaces.account import IAccount
@@ -46,6 +39,7 @@ from lp.answers.interfaces.question import IQuestion
 from lp.answers.interfaces.questionsperson import IQuestionsPerson
 from lp.answers.interfaces.questiontarget import IQuestionTarget
 from lp.app.interfaces.security import IAuthorization
+from lp.app.security import AuthorizationBase
 from lp.archivepublisher.interfaces.publisherconfig import IPublisherConfig
 from lp.blueprints.interfaces.specification import (
     ISpecification,
@@ -211,69 +205,6 @@ from lp.translations.interfaces.translator import (
     IEditTranslator,
     ITranslator,
     )
-
-
-class AuthorizationBase:
-    implements(IAuthorization)
-    permission = None
-    usedfor = None
-
-    def __init__(self, obj):
-        self.obj = obj
-
-    def checkUnauthenticated(self):
-        """See `IAuthorization.checkUnauthenticated`.
-
-        :return: True or False.
-        """
-        return False
-
-    def checkAuthenticated(self, user):
-        """Return True if the given person has the given permission.
-
-        This method is implemented by security adapters that have not
-        been updated to work in terms of IAccount.
-
-        :return: True or False.
-        """
-        return False
-
-    def forwardCheckAuthenticated(self, user,
-                                  obj=None, permission=None):
-        """Forward request to another security adapter.
-
-        Find a matching adapter and call checkAuthenticated on it. Intended
-        to be used in checkAuthenticated.
-
-        :param user: The IRolesPerson object that was passed in.
-        :param obj: The object to check the permission for. If None, use
-            the same object as this adapter.
-        :param permission: The permission to check. If None, use the same
-            permission as this adapter.
-        :return: True or False.
-        """
-        if obj is None:
-            obj = self.obj
-        if permission is None:
-            permission = self.permission
-        else:
-            # This will raise ValueError if the permission doesn't exist.
-            check_permission_is_registered(obj, permission)
-        next_adapter = getAdapter(obj, IAuthorization, permission)
-        return next_adapter.checkAuthenticated(user)
-
-    def checkAccountAuthenticated(self, account):
-        """See `IAuthorization.checkAccountAuthenticated`.
-
-        :return: True or False.
-        """
-        # For backward compatibility, delegate to one of
-        # checkAuthenticated() or checkUnauthenticated().
-        person = IPerson(account, None)
-        if person is None:
-            return self.checkUnauthenticated()
-        else:
-            return self.checkAuthenticated(IPersonRoles(person))
 
 
 class ViewByLoggedInUser(AuthorizationBase):
---
+++ lib/lp/app/security.py
@@ -0,0 +1,83 @@
+# Copyright 2011 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Base class to implement the Launchpad security policy."""
+
+__metaclass__ = type
+
+__all__ = [
+    'AuthorizationBase',
+    ]
+
+from zope.component import getAdapter
+from zope.interface import implements
+from zope.security.permission import (
+    checkPermission as check_permission_is_registered,
+    )
+
+from canonical.launchpad.interfaces.launchpad import IPersonRoles
+from lp.app.interfaces.security import IAuthorization
+from lp.registry.interfaces.person import IPerson
+
+
+class AuthorizationBase:
+    implements(IAuthorization)
+    permission = None
+    usedfor = None
+
+    def __init__(self, obj):
+        self.obj = obj
+
+    def checkUnauthenticated(self):
+        """See `IAuthorization.checkUnauthenticated`.
+
+        :return: True or False.
+        """
+        return False
+
+    def checkAuthenticated(self, user):
+        """Return True if the given person has the given permission.
+
+        This method is implemented by security adapters that have not
+        been updated to work in terms of IAccount.
+
+        :return: True or False.
+        """
+        return False
+
+    def forwardCheckAuthenticated(self, user,
+                                  obj=None, permission=None):
+        """Forward request to another security adapter.
+
+        Find a matching adapter and call checkAuthenticated on it. Intended
+        to be used in checkAuthenticated.
+
+        :param user: The IRolesPerson object that was passed in.
+        :param obj: The object to check the permission for. If None, use
+            the same object as this adapter.
+        :param permission: The permission to check. If None, use the same
+            permission as this adapter.
+        :return: True or False.
+        """
+        if obj is None:
+            obj = self.obj
+        if permission is None:
+            permission = self.permission
+        else:
+            # This will raise ValueError if the permission doesn't exist.
+            check_permission_is_registered(obj, permission)
+        next_adapter = getAdapter(obj, IAuthorization, permission)
+        return next_adapter.checkAuthenticated(user)
+
+    def checkAccountAuthenticated(self, account):
+        """See `IAuthorization.checkAccountAuthenticated`.
+
+        :return: True or False.
+        """
+        # For backward compatibility, delegate to one of
+        # checkAuthenticated() or checkUnauthenticated().
+        person = IPerson(account, None)
+        if person is None:
+            return self.checkUnauthenticated()
+        else:
+            return self.checkAuthenticated(IPersonRoles(person))
