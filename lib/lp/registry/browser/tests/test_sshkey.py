7a2adcca4cf93dd79d22c497f5eeccf675331181
Deryck Hodge <deryck.hodge@canonical.com>
2011-04-29 10:59:22 -0500

    Add a test to cover that the remove message is escaped.

--- lib/lp/registry/browser/tests/test_sshkey.py
+++ lib/lp/registry/browser/tests/test_sshkey.py
@@ -5,8 +5,15 @@
 
 __metaclass__ = type
 
+from zope.component import getUtility
+
+from canonical.launchpad.testing.pages import (
+    extract_text,
+    find_tags_by_class,
+    )
 from canonical.launchpad.webapp import canonical_url
 from canonical.testing.layers import DatabaseFunctionalLayer
+from lp.registry.interfaces.ssh import ISSHKeySet
 from lp.testing import TestCaseWithFactory
 
 
@@ -24,3 +31,22 @@ class TestCanonicalUrl(TestCaseWithFactory):
             canonical_url(sshkey))
 
 
+class TestSSHKeyView(TestCaseWithFactory):
+
+    layer = DatabaseFunctionalLayer
+
+    def test_escaped_message_when_removing_key(self):
+        """Confirm that messages are escaped when removing keys."""
+        person = self.factory.makePerson()
+        public_key = "ssh-rsa %s x<script>alert()</script>example.com" % (
+            self.getUniqueString())
+        # Add the key for the user here,
+        # since we only care about testing removal.
+        added_key = getUtility(ISSHKeySet).new(person, public_key)
+        browser = self.getUserBrowser(
+            canonical_url(person) + '/+editsshkeys', user=person)
+        browser.getControl('Remove').click()
+        msg = 'Key "x&lt;script&gt;alert()&lt;/script&gt;example.com" removed'
+        self.assertEqual(
+            extract_text(find_tags_by_class(browser.contents, 'message')[0]),
+            msg)
