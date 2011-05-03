19d61718d699b65f0da62b39d3bbe98ac03ead44
Aaron Bentley <aaron@canonical.com>
2011-05-03 11:59:06 -0400

    lint fixes.

--- lib/lp/code/model/tests/test_codereviewvote.py
+++ lib/lp/code/model/tests/test_codereviewvote.py
@@ -238,8 +238,7 @@ class TestCodeReviewVoteReferenceReassignReview(TestCaseWithFactory):
         # pending review assigned to them.
         bmp, review = self.makeMergeProposalWithReview()
         reviewer = self.factory.makePerson(name='eric')
-        user_review = bmp.nominateReviewer(
-            reviewer=reviewer, registrant=bmp.registrant)
+        bmp.nominateReviewer(reviewer=reviewer, registrant=bmp.registrant)
         self.assertRaisesWithContent(
             UserHasExistingReview,
             'Eric (eric) has already been asked to review this',
@@ -263,7 +262,7 @@ class TestCodeReviewVoteReferenceReassignReview(TestCaseWithFactory):
         # review assigned to them.
         bmp, review = self.makeMergeProposalWithReview()
         reviewer_team = self.factory.makeTeam()
-        team_review = bmp.nominateReviewer(
+        bmp.nominateReviewer(
             reviewer=reviewer_team, registrant=bmp.registrant)
         review.reassignReview(reviewer_team)
         self.assertEqual(reviewer_team, review.reviewer)
