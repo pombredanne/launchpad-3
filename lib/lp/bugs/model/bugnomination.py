0bac5f92193535693da35130b4e8ef1b895cc3c3
William Grant <william.grant@canonical.com>
2011-05-09 16:42:01 +1000

    lint.

--- lib/lp/bugs/model/bugnomination.py
+++ lib/lp/bugs/model/bugnomination.py
@@ -148,6 +148,7 @@ class BugNomination(SQLBase):
                     return True
         return False
 
+
 class BugNominationSet:
     """See IBugNominationSet."""
     implements(IBugNominationSet)
