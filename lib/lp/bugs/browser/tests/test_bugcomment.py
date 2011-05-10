73efdc1791daf60161c90cb2b5df9b43139555a5
j.c.sackett <jonathan.sackett@canonical.com>
2011-05-10 10:24:28 -0400

    Merged test_bugcomment and test_bugcomment_visibility.

--- lib/lp/bugs/browser/tests/test_bugcomment.py
+++ lib/lp/bugs/browser/tests/test_bugcomment.py
@@ -12,10 +12,16 @@ from datetime import (
 from itertools import count
 
 from pytz import utc
+from zope.component import getUtility
 
+from canonical.launchpad.interfaces.launchpad import ILaunchpadCelebrities
+from canonical.testing.layers import DatabaseFunctionalLayer
 from lp.bugs.browser.bugcomment import group_comments_with_activity
-from lp.testing import TestCase
-
+from lp.testing import (
+    BrowserTestCase,
+    person_logged_in,
+    TestCase,
+    )
 
 class BugActivityStub:
 
@@ -178,3 +184,117 @@ class TestGroupCommentsWithActivities(TestCase):
         self.assertEqual([comment1, comment2], grouped)
         self.assertEqual([activity1, activity2], comment1.activity)
         self.assertEqual([activity3], comment2.activity)
+
+
+class TestBugCommentVisibility(BrowserTestCase):
+
+    layer = DatabaseFunctionalLayer
+
+    def _makeBugWithHiddenComment(self, bugbody=None):
+        administrator = getUtility(ILaunchpadCelebrities).admin.teamowner
+        bug = self.factory.makeBug()
+        with person_logged_in(administrator):
+            comment = self.factory.makeBugComment(bug=bug, body=bugbody)
+            comment.visible = False
+        return bug
+
+    def _getUserForTest(self, team=None):
+        person = self.factory.makePerson()
+        if team is not None:
+            with person_logged_in(team.teamowner):
+                team.addMember(person, team.teamowner)
+        return person
+
+    def test_admin_can_see_comments(self):
+        comment_text = "You can't see me."
+        bug = self._makeBugWithHiddenComment(comment_text)
+        admin_team = getUtility(ILaunchpadCelebrities).admin
+        administrator = self._getUserForTest(admin_team)
+        view = self.getViewBrowser(
+            context=bug.default_bugtask, user=administrator)
+        self.assertTrue(
+           comment_text in view.contents,
+           "Administrator cannot see the hidden comment.")
+
+    def test_registry_can_see_comments(self):
+        comment_text = "You can't see me."
+        bug = self._makeBugWithHiddenComment(comment_text)
+        registry_team = getUtility(ILaunchpadCelebrities).registry_experts
+        registry_expert = self._getUserForTest(registry_team)
+        view = self.getViewBrowser(
+            context=bug.default_bugtask, user=registry_expert)
+        self.assertTrue(
+           comment_text in view.contents,
+           "Registy member cannot see the hidden comment.")
+
+    def test_anon_cannot_see_comments(self):
+        comment_text = "You can't see me."
+        bug = self._makeBugWithHiddenComment(comment_text)
+        view = self.getViewBrowser(context=bug.default_bugtask, no_login=True)
+        self.assertFalse(
+           comment_text in view.contents,
+           "Anonymous person can see the hidden comment.")
+
+    def test_random_cannot_see_comments(self):
+        comment_text = "You can't see me."
+        bug = self._makeBugWithHiddenComment(comment_text)
+        view = self.getViewBrowser(context=bug.default_bugtask)
+        self.assertFalse(
+           comment_text in view.contents,
+           "Random user can see the hidden comment.")
+class TestBugCommentVisibility(BrowserTestCase):
+
+    layer = DatabaseFunctionalLayer
+
+    def _makeBugWithHiddenComment(self, bugbody=None):
+        administrator = getUtility(ILaunchpadCelebrities).admin.teamowner
+        bug = self.factory.makeBug()
+        with person_logged_in(administrator):
+            comment = self.factory.makeBugComment(bug=bug, body=bugbody)
+            comment.visible = False
+        return bug
+
+    def _getUserForTest(self, team=None):
+        person = self.factory.makePerson()
+        if team is not None:
+            with person_logged_in(team.teamowner):
+                team.addMember(person, team.teamowner)
+        return person
+
+    def test_admin_can_see_comments(self):
+        comment_text = "You can't see me."
+        bug = self._makeBugWithHiddenComment(comment_text)
+        admin_team = getUtility(ILaunchpadCelebrities).admin
+        administrator = self._getUserForTest(admin_team)
+        view = self.getViewBrowser(
+            context=bug.default_bugtask, user=administrator)
+        self.assertTrue(
+           comment_text in view.contents,
+           "Administrator cannot see the hidden comment.")
+
+    def test_registry_can_see_comments(self):
+        comment_text = "You can't see me."
+        bug = self._makeBugWithHiddenComment(comment_text)
+        registry_team = getUtility(ILaunchpadCelebrities).registry_experts
+        registry_expert = self._getUserForTest(registry_team)
+        view = self.getViewBrowser(
+            context=bug.default_bugtask, user=registry_expert)
+        self.assertTrue(
+           comment_text in view.contents,
+           "Registy member cannot see the hidden comment.")
+
+    def test_anon_cannot_see_comments(self):
+        comment_text = "You can't see me."
+        bug = self._makeBugWithHiddenComment(comment_text)
+        view = self.getViewBrowser(context=bug.default_bugtask, no_login=True)
+        self.assertFalse(
+           comment_text in view.contents,
+           "Anonymous person can see the hidden comment.")
+
+    def test_random_cannot_see_comments(self):
+        comment_text = "You can't see me."
+        bug = self._makeBugWithHiddenComment(comment_text)
+        view = self.getViewBrowser(context=bug.default_bugtask)
+        self.assertFalse(
+           comment_text in view.contents,
+           "Random user can see the hidden comment.")
