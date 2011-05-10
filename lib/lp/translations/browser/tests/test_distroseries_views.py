29f704e9cd02799bbb1ef995f1f97fc2e9445816
Henning Eggers <henning@canonical.com>
2011-05-10 12:22:09 +0200

    Extended test to include delta packs.

--- lib/lp/translations/browser/tests/test_distroseries_views.py
+++ lib/lp/translations/browser/tests/test_distroseries_views.py
@@ -20,7 +20,7 @@ class TestLanguagePacksView(TestCaseWithFactory):
 
     layer = LaunchpadFunctionalLayer
 
-    def test_unused_language_packs__handles_many_language_packs(self):
+    def test_unused_language_packs_many_language_packs(self):
         distroseries = self.factory.makeUbuntuDistroSeries()
         # This is one more than the default for shortlist.
         number_of_language_packs = 16
@@ -33,12 +33,26 @@ class TestLanguagePacksView(TestCaseWithFactory):
         self.assertEqual(
             number_of_language_packs, len(view.unused_language_packs))
 
-    def test_unused_language_packs__handles_identical_pack(self):
+    def test_unused_language_packs_identical_base_proposed_pack(self):
         distroseries = self.factory.makeUbuntuDistroSeries()
         pack = self.factory.makeLanguagePack(distroseries)
         with person_logged_in(distroseries.distribution.owner):
             distroseries.language_pack_base = pack 
             distroseries.language_pack_proposed = pack
+
+        view = create_initialized_view(
+            distroseries, '+language-packs', rootsite='translations')
+        self.assertEqual(0, len(view.unused_language_packs))
+
+    def test_unused_language_packs_identical_delta_proposed_pack(self):
+        distroseries = self.factory.makeUbuntuDistroSeries()
+        with person_logged_in(distroseries.distribution.owner):
+            distroseries.language_pack_base = self.factory.makeLanguagePack(
+                distroseries)
+            delta_pack = self.factory.makeLanguagePack(
+                distroseries, LanguagePackType.DELTA)
+            distroseries.language_pack_delta = delta_pack
+            distroseries.language_pack_proposed = delta_pack
 
         view = create_initialized_view(
             distroseries, '+language-packs', rootsite='translations')
