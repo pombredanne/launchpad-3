021da06d476b9628de2344677a77c3efb2f0a8c2
j.c.sackett <jonathan.sackett@canonical.com>
2011-05-13 14:35:16 -0400

    Changed doc per reviewer suggestions.

--- lib/lp/services/messages/tests/test_doc.py
+++ lib/lp/services/messages/tests/test_doc.py
@@ -1,17 +1,12 @@
 # Copyright 2011 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
-"""
-Run the doctests.
-"""
+"""Test message documentation."""
+
+__metaclass__ = type
 
 import os
 
-from canonical.launchpad.testing.systemdocs import (
-    LayeredDocFileSuite,
-    setUp,
-    tearDown,
-    )
 from canonical.testing.layers import LaunchpadFunctionalLayer
 from lp.services.testing import build_test_suite
 
@@ -19,13 +14,6 @@ from lp.services.testing import build_test_suite
 here = os.path.dirname(os.path.realpath(__file__))
 
 
-special = {
-    'message.txt': LayeredDocFileSuite(
-        '../doc/message.txt',
-        setUp=setUp, tearDown=tearDown, layer=LaunchpadFunctionalLayer),
-    }
-
-
 def test_suite():
-    suite = build_test_suite(here, special, layer=LaunchpadFunctionalLayer)
+    suite = build_test_suite(here, {}, layer=LaunchpadFunctionalLayer)
     return suite
