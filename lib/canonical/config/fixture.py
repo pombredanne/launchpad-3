3f3bd1cce1aeffd5c94740b2fbe89cdbbd39d1db
Robert Collins <robert@canonical.com>
2011-05-18 17:21:14 +1200

    More explanation.

--- lib/canonical/config/fixture.py
+++ lib/canonical/config/fixture.py
@@ -43,7 +43,8 @@ class ConfigFixture(Fixture):
         """Add sectioncontent to the lazy config."""
         with open(self.absroot + '/launchpad-lazr.conf', 'ab') as out:
             out.write(sectioncontent)
-        # Trigger a refresh IFF the config is in use at the moment.
+        # Trigger a refresh if and only if the config is in use at the moment
+        # in order to make these new values available.
         if config.instance_name == self.instance_name:
             config._invalidateConfig()
 
