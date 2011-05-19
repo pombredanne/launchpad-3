93e19fb4821d8753eebf4fd12efc29383a7327c9
William Grant <william.grant@canonical.com>
2011-05-19 15:28:42 +1000

    Replace most of parse_tagfile_lines with apt_pkg.ParseTagFile.

--- lib/lp/archiveuploader/tagfiles.py
+++ lib/lp/archiveuploader/tagfiles.py
@@ -14,6 +14,9 @@ __all__ = [
 
 import apt_pkg
 import re
+import tempfile
+
+from lp.services.mail.signedmessage import strip_pgp_signature
 
 
 class TagFile(object):
@@ -97,124 +100,28 @@ def parse_tagfile_lines(lines, dsc_whitespace_rules=0, allow_unsigned=False,
     o The data section must end with a blank line and must be followed by
       '-----BEGIN PGP SIGNATURE-----'.
     """
-    error = ""
+    assert allow_unsigned
+    # XXX: No dsc_whitespace_rules
 
-    changes = {}
-
-    # Reindex by line number so we can easily verify the format of
-    # .dsc files...
-    index = 0
-    indexed_lines = {}
-    for line in lines:
-        index += 1
-        indexed_lines[index] = line[:-1]
-
-    inside_signature = 0
-
-    num_of_lines = len(indexed_lines.keys())
-    index = 0
-    first_value_for_newline_delimited_field = False
-    more_values_can_follow = False
-    while index < num_of_lines:
-        index += 1
-        line = indexed_lines[index]
-
-        # If the line is empty and we're not strictly enforcing whitespace
-        # rules, then just continue.
-        # If we're enforcing the rules, then check those rules, and maybe
-        # complain.
-        if line == "":
-            if dsc_whitespace_rules:
-                index += 1
-                if index > num_of_lines:
-                    raise TagFileParseError(
-                        "%s: invalid .dsc file at line %d" % (
-                            filename, index))
-                line = indexed_lines[index]
-                if not line.startswith("-----BEGIN PGP SIGNATURE"):
-                    raise TagFileParseError(
-                        "%s: invalid .dsc file at line %d -- "
-                        "expected PGP signature; got '%s'" % (
-                            filename, index,line))
-                inside_signature = 0
-                break
-            else:
-                continue
-        if line.startswith("-----BEGIN PGP SIGNATURE"):
-            break
-
-        # If we're at the start of a signed section, then consume the
-        # signature information, and remember that we're inside the signed
-        # data.
-        if line.startswith("-----BEGIN PGP SIGNED MESSAGE"):
-            inside_signature = 1
-            if dsc_whitespace_rules:
-                while index < num_of_lines and line != "":
-                    index += 1
-                    line = indexed_lines[index]
-            continue
-        # If we're not inside the signed data, don't process anything, unless
-        # we've decided to allow unsigned files.
-        if not (inside_signature or allow_unsigned):
-            continue
-        slf = re_single_line_field.match(line)
-        if slf:
-            field = slf.groups()[0]
-            changes[field] = slf.groups()[1]
-
-            # If there is no value on this line, we assume this is
-            # the first line of a multiline field, such as the 'files'
-            # field.
-            if changes[field] == '':
-                first_value_for_newline_delimited_field = True
-
-            # Either way, more values for this field could follow
-            # on the next line.
-            more_values_can_follow = True
-            continue
-        if line.rstrip() == " .":
-            changes[field] += '\n' + line
-            continue
-        mlf = re_multi_line_field.match(line)
-        if mlf:
-            if more_values_can_follow is False:
-                raise TagFileParseError(
-                    "%s: could not parse .changes file line %d: '%s'\n"
-                    " [Multi-line field continuing on from nothing?]" % (
-                        filename, index,line))
-
-            # XXX Michael Nelson 20091001 bug=440014
-            # Is there any reason why we're not simply using
-            # apt_pkg.ParseTagFile instead of this looong function.
-            # If we can get rid of this code that is trying to mimic
-            # what ParseTagFile does out of the box, it would be a good
-            # thing.
-
-            # The first value for a newline delimited field, such as
-            # the 'files' field, has its leading spaces stripped. Other
-            # fields (such as a 'binary' field spanning multiple lines)
-            # should *not* be l-stripped of their leading spaces otherwise
-            # they will be re-parsed incorrectly by apt_get.ParseTagFiles()
-            # (for example, from a Source index).
-            value = mlf.groups()[0]
-            if first_value_for_newline_delimited_field:
-                changes[field] = value.lstrip()
-            else:
-                changes[field] += '\n' + value
-
-            first_value_for_newline_delimited_field = False
-            continue
-        error += line
-
-    if dsc_whitespace_rules and inside_signature:
+    with tempfile.TemporaryFile() as f:
+        f.write("".join(lines))
+        f.seek(0)
+        stanzas = list(apt_pkg.ParseTagFile(f))
+    if len(stanzas) != 1:
         raise TagFileParseError(
-            "%s: invalid .dsc format at line %d" % (filename, index))
+            "%s: multiple stanzas where only one is expected" % filename)
 
-    if error:
-        raise TagFileParseError(
-            "%s: unable to parse .changes file: %s" % (filename, error))
+    [stanza] = stanzas
 
-    return changes
+    # We can't do this sensibly with dict() or update(), as it has some
+    # keys without values.
+    trimmed_dict = {}
+    for key in stanza.keys():
+        try:
+            trimmed_dict[key] = stanza[key]
+        except KeyError:
+            pass
+    return trimmed_dict
 
 
 def parse_tagfile(filename, dsc_whitespace_rules=0, allow_unsigned=False):
@@ -227,7 +134,7 @@ def parse_tagfile(filename, dsc_whitespace_rules=0, allow_unsigned=False):
     dsc_whitespace_rules and allow_unsigned arguments.
     """
     changes_in = open(filename, "r")
-    lines = changes_in.readlines()
+    lines = strip_pgp_signature(changes_in.read()).splitlines(True)
     changes_in.close()
     if not lines:
         raise TagFileParseError( "%s: empty file" % filename )
