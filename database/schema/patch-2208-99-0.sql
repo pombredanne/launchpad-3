94f62246c6f921d53631d37e2708953a940a8ceb
Raphael Badin <raphael.badin@canonical.com>
2011-05-20 11:42:59 +0200

    Add ordering for DSPs.

---
+++ database/schema/patch-2208-99-0.sql
@@ -0,0 +1,19 @@
+-- Copyright 2011 Canonical Ltd.  This software is licensed under the
+-- GNU Affero General Public License version 3 (see the file LICENSE).
+
+SET client_min_messages=ERROR;
+
+-- Add a column to order DSP (will be used to control the build order
+-- for overlays).
+ALTER TABLE DistroSeriesParent
+    ADD COLUMN ordering INTEGER NOT NULL DEFAULT 1;
+
+-- Update existing DSPs.
+UPDATE DistroSeriesParent
+    SET ordering = 1;
+
+-- Create index.
+CREATE INDEX distroseriesparent__ordering
+    ON DistroSeriesParent (ordering);
+
+INSERT INTO LaunchpadDatabaseRevision VALUES (2208, 99, 0);
