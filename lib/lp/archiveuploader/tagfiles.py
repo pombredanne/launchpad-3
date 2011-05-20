2c37bba06e2daf3b99bc825aebc695fdd9fce060
William Grant <william.grant@canonical.com>
2011-05-20 17:47:17 +1000

    Drop unused cruft from tagfiles.

--- lib/lp/archiveuploader/tagfiles.py
+++ lib/lp/archiveuploader/tagfiles.py
@@ -4,80 +4,22 @@
 """Utility classes for parsing Debian tag files."""
 
 __all__ = [
-    'TagFile',
-    'TagStanza',
     'TagFileParseError',
     'parse_tagfile',
     'parse_tagfile_content'
     ]
 
 
-import apt_pkg
-import re
 import tempfile
+
+import apt_pkg
 
 from lp.services.mail.signedmessage import strip_pgp_signature
 
 
-class TagFile(object):
-    """Provide an iterable interface to the apt_pkg.TagFile object"""
-
-    def __init__(self, f):
-        """Initialise apt_pkg and parse the tagfile provided by f"""
-        if not isinstance(f, file):
-            raise ValueError()
-        apt_pkg.init()
-        self.stanzas = apt_pkg.ParseTagFile(f)
-
-    def __iter__(self):
-        """Iterate across the stanzas in the tagfile"""
-        self.stanzas.Jump(0)
-        yield TagStanza(self.stanzas.Section)
-        while self.stanzas.Step():
-            yield TagStanza(self.stanzas.Section)
-
-    def __getitem__(self, item):
-        """Implement the [] operator"""
-        self.stanzas.Jump(item)
-        return TagStanza(self.stanzas.Section)
-
-class TagStanza(object):
-    """Provide an iterable interface to apt_pkg.TagStanza"""
-
-    def __init__(self, stanza):
-        """Initialise given a stanza (usually from TagFile.__iter__)"""
-        self.stanza = stanza
-
-    def __getitem__(self, item):
-        """The [] operator"""
-        return self.stanza[item]
-
-    def __iter__(self):
-        """Iterate across keys"""
-        for k in self.stanza.keys():
-            yield k
-
-    def keys(self):
-        """Expose the .keys() method"""
-        return self.stanza.keys()
-
-    def has_key(self, key):
-        """Expose a dicty has_key"""
-        return key in self.stanza.keys()
-
-    # Enables (foo in bar) functionality.
-    __contains__ = has_key
-
-    def items(self):
-        """Allows for k,v in foo.items()"""
-        return [ (k, self.stanza[k]) for k in self.stanza.keys() ]
-
 class TagFileParseError(Exception):
     """This exception is raised if parse_changes encounters nastiness"""
     pass
-
-re_single_line_field = re.compile(r"^(\S*)\s*:\s*(.*)")
-re_multi_line_field = re.compile(r"^(\s.*)")
 
 
 def parse_tagfile_content(content, filename=None):
