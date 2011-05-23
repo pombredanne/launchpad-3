55b425a1679fa84f88ad25c5af9b62f642bc70b1
Stuart Bishop <stuart.bishop@canonical.com>
2011-05-23 17:53:53 +0700

    Simplify db patch by using the existing pg_depends and pg_constraint entries rather than try to migrate the existing primary key entries

--- database/schema/patch-2208-62-0.sql
+++ database/schema/patch-2208-62-0.sql
@@ -1,5 +1,3 @@
-BEGIN;
-
 -- Copyright 2011 Canonical Ltd.  This software is licensed under the
 -- GNU Affero General Public License version 3 (see the file LICENSE).
 SET client_min_messages=ERROR;
@@ -8,80 +6,28 @@ SET client_min_messages=ERROR;
 UPDATE pg_index SET indisprimary = FALSE
 WHERE pg_index.indexrelid = 'revisionnumber_pkey'::regclass;
 
+UPDATE pg_constraint SET contype = 'u'
+WHERE
+    conrelid='branchrevision'::regclass
+    AND conname='revisionnumber_pkey';
+
 
 -- Make an existing index think it is the primary key.
 UPDATE pg_index SET indisprimary = TRUE
 WHERE pg_index.indexrelid = 'revision__revision__branch__key'::regclass;
 
-
--- The primary key constraint is on different columns now.
-UPDATE pg_constraint SET
-    conname='revision__revision__branch__key',
-    conkey='{4,3}'
+UPDATE pg_constraint SET contype='p'
 WHERE
-    contype='p'
-    AND pg_constraint.conrelid = 'branchrevision'::regclass;
+    conrelid='branchrevision'::regclass
+    AND conname='revision__revision__branch__key';
 
-
--- The primary key constraint now depends on the new index
-UPDATE pg_depend
-    SET objid='revision__revision__branch__key'::regclass
-WHERE
-    objid = 'revisionnumber_pkey'::regclass
-    AND refobjid = (
-        SELECT pg_constraint.oid FROM pg_constraint
-        WHERE
-            contype='p'
-            AND pg_constraint.conrelid = 'branchrevision'::regclass);
-
-
--- Delete old dependency between the constraint and the table columns.
-DELETE FROM pg_depend
-WHERE
-    classid = 'pg_constraint'::regclass
-    AND objid = (
-        SELECT pg_constraint.oid FROM pg_constraint
-        WHERE
-            contype='p'
-            AND pg_constraint.conrelid = 'branchrevision'::regclass)
-    AND refclassid = 'pg_class'::regclass
-    AND refobjid = 'branchrevision'::regclass
-    AND deptype = 'a';
-
--- Transfer dependencies of the new key's index on its table
--- columns to the constraint. It depends on the columns now.
-UPDATE pg_depend
-SET
-    classid = 'pg_constraint'::regclass,
-    objid = (
-        SELECT pg_constraint.oid FROM pg_constraint
-        WHERE
-            contype='p'
-            AND pg_constraint.conrelid = 'branchrevision'::regclass)
-WHERE
-    classid = 'pg_class'::regclass
-    AND objid = 'revision__revision__branch__key'::regclass
-    AND refclassid = 'pg_class'::regclass
-    AND refobjid = 'branchrevision'::regclass
-    AND deptype = 'a';
-
--- Delete the old UNIQUE constraint.
--- XXX: This fails! If we dump the database after applying this patch,
--- the dump attempts to create both a primary key and a unique constraint
--- with the same name on the same columns.
-DELETE FROM pg_constraint
-WHERE conname='revision__revision__branch__key' AND contype='u';
 
 -- This view is no longer used - no need to recreate it.
 DROP VIEW RevisionNumber;
-
--- Need to drop this manually as we have repurposed the pg_dependency
--- between the id column and this index.
-DROP INDEX revisionnumber_pkey;
 
 ALTER TABLE BranchRevision
     DROP COLUMN id,
     DROP CONSTRAINT revision__branch__revision__key;
 
-INSERT INTO LaunchpadDatabaseRevision VALUES (2208, 99, 0);
+INSERT INTO LaunchpadDatabaseRevision VALUES (2208, 62, 0);
 
