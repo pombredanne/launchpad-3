2d12d1b33b9ea82e57afa4657243dca643840d3d
Curtis Hovey <curtis.hovey@canonical.com>
2011-05-26 18:12:51 -0400

    Added a test to verify that the search all bugs form is rendered.

--- lib/lp/bugs/browser/tests/test_bugs.py
+++ lib/lp/bugs/browser/tests/test_bugs.py
@@ -8,6 +8,7 @@ __metaclass__ = type
 from zope.component import getUtility
 
 from canonical.launchpad.webapp.publisher import canonical_url
+from canonical.launchpad.testing.pages import find_tag_by_id
 from canonical.testing.layers import DatabaseFunctionalLayer
 from lp.bugs.interfaces.malone import IMaloneApplication
 from lp.bugs.publisher import BugsLayer
@@ -67,3 +68,26 @@ class TestMaloneView(TestCaseWithFactory):
         self.assertEqual(
             "Bug ['fnord', 'pting'] is not registered.", view.error_message)
         self.assertEqual(None, view.request.response.getHeader('Location'))
+
+    def test_search_specifications_form_rendering(self):
+        # The view's template directly renders the form widgets.
+        view = create_initialized_view(self.application, '+index')
+        content = find_tag_by_id(view.render(), 'search-all-bugs')
+        self.assertEqual('form', content.name)
+        self.assertTrue(
+            content.find(True, id='field.searchtext') is not None)
+        self.assertTrue(
+            content.find(True, id='field.actions.search') is not None)
+        self.assertTrue(
+            content.find(True, id='field.scope.option.all') is not None)
+        self.assertTrue(
+            content.find(True, id='field.scope.option.project') is not None)
+        target_widget = view.widgets['scope'].target_widget
+        self.assertTrue(
+            content.find(True, id=target_widget.show_widget_id) is not None)
+        text = str(content)
+        picker_script = (
+            "Y.lp.app.picker.create('DistributionOrProductOrProjectGroup'")
+        self.assertTrue(picker_script in text)
+        focus_script = "setFocusByName('field.searchtext')"
+        self.assertTrue(focus_script in text)
