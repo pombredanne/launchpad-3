0fc46cdc20ca01bc5ea1a1370a3f18e46d73d7e5
Gavin Panella <gavin.panella@canonical.com>
2011-05-26 11:44:17 +0100

    Disable TestRabbitFixture.test_start_check_shutdown due to bug 788557.

--- lib/lp/services/rabbit/tests/test_fixture.py
+++ lib/lp/services/rabbit/tests/test_fixture.py
@@ -58,6 +58,10 @@ def gather_details(source, target):
 class TestRabbitFixture(TestCase):
 
     def test_start_check_shutdown(self):
+        # XXX: GavinPanella 2011-05-26 bug=788557 : Disabled due to spurious
+        # failures (cannot create cookie file).
+        self.skip("Disabled (bug 788557)")
+
         # Rabbit needs to fully isolate itself: an existing per user
         # .erlange.cookie has to be ignored, and ditto bogus HOME if other
         # tests fail to cleanup.
