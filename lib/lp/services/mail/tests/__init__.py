84283d8a37587bfad246d78d6319a3ff2557949f
Aaron Bentley <aaron@canonical.com>
2011-05-27 11:31:46 -0400

    Cleanup

--- lib/lp/services/mail/tests/__init__.py
+++ lib/lp/services/mail/tests/__init__.py
@@ -1,6 +1,9 @@
 # Copyright 2011 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
+"""Tests for mail services."""
+
+__metaclass__ = type
 __all__ = ['ListHandler']
 
 
