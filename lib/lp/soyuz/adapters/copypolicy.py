4aac762de09daafad6de8b72e19bd965d1cb0ec0
Julian Edwards <julian.edwards@canonical.com>
2011-05-27 21:46:16 +0100

    fix the copy policy checker to take parameters other than the packageupload

--- lib/lp/soyuz/adapters/copypolicy.py
+++ lib/lp/soyuz/adapters/copypolicy.py
@@ -21,15 +21,25 @@ from lp.registry.interfaces.series import SeriesStatus
 class BaseCopyPolicy:
     """Encapsulation of the policies for copying a package in Launchpad."""
 
-    def autoApprove(self, packageupload):
-        """Decide if the packageupload can be approved automatically or
+    def autoApprove(self, archive, distroseries, pocket):
+        """Decide if the upload can be approved automatically or
         should be held in the queue.
+
+        This should only be called for packages that are known not new.
+
+        :param archive: The target `IArchive` for the upload.
+        :param distroseries: The target `IDistroSeries` for the upload.
+        :param pocket: The target `PackagePublishingPocket` for the upload.
         """
         raise AssertionError("Subclass must provide autoApprove")
 
-    def autoApproveNew(self, packageupload):
+    def autoApproveNew(self, archive, distroseries, pocket):
         """Decide if a previously unknown package is approved automatically
         or should be held in the queue.
+
+        :param archive: The target `IArchive` for the upload.
+        :param distroseries: The target `IDistroSeries` for the upload.
+        :param pocket: The target `PackagePublishingPocket` for the upload.
         """
         raise AssertionError("Subclass must provide autoApproveNew")
 
@@ -37,19 +47,22 @@ class BaseCopyPolicy:
 class InsecureCopyPolicy(BaseCopyPolicy):
     """A policy for copying from insecure sources."""
 
-    def autoApproveNew(self, packageupload):
-        if packageupload.isPPA():
+    def autoApproveNew(self, archive, distroseries=None, pocket=None):
+        if archive.is_ppa:
             return True
         return False
 
-    def autoApprove(self, packageupload):
-        if packageupload.isPPA():
+    def autoApprove(self, archive, distroseries, pocket):
+        if archive.is_ppa:
             return True
 
+        # If the pocket is RELEASE and we're not frozen then you can
+        # upload to it.  Any other states mean the upload is unapproved.
+        #
         # This check is orthogonal to the
         # IDistroSeries.canUploadToPocket check.
-        if (packageupload.pocket == PackagePublishingPocket.RELEASE and
-            packageupload.distroseries.status != SeriesStatus.FROZEN):
+        if (pocket == PackagePublishingPocket.RELEASE and
+            distroseries.status != SeriesStatus.FROZEN):
             return True
 
         return False
