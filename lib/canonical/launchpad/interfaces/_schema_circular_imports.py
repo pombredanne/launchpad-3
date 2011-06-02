539fd472d75f907deb390ca50d4b26c33e01791d
Curtis Hovey <curtis.hovey@canonical.com>
2011-06-02 18:27:11 -0400

    Updated the copyrights in the very old branch.

--- lib/canonical/launchpad/interfaces/_schema_circular_imports.py
+++ lib/canonical/launchpad/interfaces/_schema_circular_imports.py
@@ -1,4 +1,4 @@
-# Copyright 2009-2010 Canonical Ltd.  This software is licensed under the
+# Copyright 2009-2011 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
 """Update the interface schema values due to circular imports.
