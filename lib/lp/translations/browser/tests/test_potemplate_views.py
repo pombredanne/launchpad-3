b7bacf8c8ad4a639c21cbdc2bd6440b343ea751c
Henning Eggers <henning@canonical.com>
2011-06-06 16:09:30 +0200

    Added failing tests.

--- lib/lp/translations/browser/tests/test_potemplate_views.py
+++ lib/lp/translations/browser/tests/test_potemplate_views.py
@@ -15,7 +15,7 @@ class TestPOTemplateAdminViewValidation(TestCaseWithFactory):
 
     layer = DatabaseFunctionalLayer
 
-    def test_validates_name(self):
+    def test_detects_name_clash_on_name_change(self):
         # A template name may not already be used.
         existing_name = self.factory.getUniqueString()
         existing_potemplate = self.factory.makePOTemplate(name=existing_name)
@@ -33,7 +33,7 @@ class TestPOTemplateAdminViewValidation(TestCaseWithFactory):
         view.validate(data)
         self.assertEqual([u'Name is already in use.'], view.errors)
 
-    def test_validates_translation_domain(self):
+    def test_detects_domain_clash_on_domain_change(self):
         # A translation domain may not already be used.
         existing_domain = self.factory.getUniqueString()
         existing_potemplate = self.factory.makePOTemplate(
@@ -51,3 +51,46 @@ class TestPOTemplateAdminViewValidation(TestCaseWithFactory):
             )
         view.validate(data)
         self.assertEqual([u'Domain is already in use.'], view.errors)
+
+    def test_detects_name_clash_on_productseries_change(self):
+        # Detect changing to a productseries that already has a template of
+        # the same name. 
+        template_name = self.factory.getUniqueString()
+        existing_potemplate = self.factory.makePOTemplate(name=template_name)
+        new_series = existing_potemplate.productseries
+        potemplate = self.factory.makePOTemplate(name=template_name)
+
+        view = POTemplateAdminView(potemplate, LaunchpadTestRequest())
+        data = dict(
+            distroseries=None,
+            sourcepackagename=None,
+            productseries=new_series,
+            name=template_name,
+            translation_domain=potemplate.translation_domain,
+            )
+        view.validate(data)
+        self.assertEqual(
+            [u'Series already has a template with that name.'], view.errors)
+
+    def test_detects_domain_clash_on_productseries_change(self):
+        # Detect changing to a productseries that already has a template with
+        # the same translation domain. 
+        translation_domain = self.factory.getUniqueString()
+        existing_potemplate = self.factory.makePOTemplate(
+            translation_domain=translation_domain)
+        new_series = existing_potemplate.productseries
+        potemplate = self.factory.makePOTemplate(
+            translation_domain=translation_domain)
+
+        view = POTemplateAdminView(potemplate, LaunchpadTestRequest())
+        data = dict(
+            distroseries=None,
+            sourcepackagename=None,
+            productseries=new_series,
+            name=potemplate.name,
+            translation_domain=translation_domain,
+            )
+        view.validate(data)
+        self.assertEqual(
+            [u'Series already has a template with that domain.'], view.errors)
+
