488665a37b0fc3038e44a490a9a9fac5d7584b3a
Stuart Bishop <stuart.bishop@canonical.com>
2011-06-09 18:50:58 +0700

    Add mega bugsummary index

--- database/schema/patch-2208-63-2.sql
+++ database/schema/patch-2208-63-2.sql
@@ -6,4 +6,8 @@ SET client_min_messages=ERROR;
 CREATE INDEX bugsummary__milestone__idx
 ON BugSummary(milestone) WHERE milestone IS NOT NULL;
 
+
+CREATE INDEX bugsummary__full__idx
+ON BugSummary(status, product, productseries, distribution, distroseries, sourcepackagename, viewed_by, milestone, tag);
+
 INSERT INTO LaunchpadDatabaseRevision VALUES (2208, 63, 2);
