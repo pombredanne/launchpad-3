351067e0fd8feb11662502f3b13d3fcdd98f529d
Robert Collins <robert@canonical.com>
2011-06-10 08:01:45 +1200

    Fixed _inc and _dec for non-unary updates.

--- database/schema/patch-2208-63-4.sql
+++ database/schema/patch-2208-63-4.sql
@@ -72,6 +72,79 @@ BEGIN
 END;
 $$;
 
+CREATE OR REPLACE FUNCTION bug_summary_dec(bugsummary) RETURNS VOID
+LANGUAGE SQL AS
+$$
+    -- We own the row reference, so in the absence of bugs this cannot
+    -- fail - just decrement the row.
+    UPDATE BugSummary SET count = count - $1.count
+    WHERE
+        product IS NOT DISTINCT FROM $1.product
+        AND productseries IS NOT DISTINCT FROM $1.productseries
+        AND distribution IS NOT DISTINCT FROM $1.distribution
+        AND distroseries IS NOT DISTINCT FROM $1.distroseries
+        AND sourcepackagename IS NOT DISTINCT FROM $1.sourcepackagename
+        AND viewed_by IS NOT DISTINCT FROM $1.viewed_by
+        AND tag IS NOT DISTINCT FROM $1.tag
+        AND status IS NOT DISTINCT FROM $1.status
+        AND milestone IS NOT DISTINCT FROM $1.milestone;
+    -- gc the row (perhaps should be garbo but easy enough to add here:
+    DELETE FROM bugsummary
+    WHERE
+        count=0
+        AND product IS NOT DISTINCT FROM $1.product
+        AND productseries IS NOT DISTINCT FROM $1.productseries
+        AND distribution IS NOT DISTINCT FROM $1.distribution
+        AND distroseries IS NOT DISTINCT FROM $1.distroseries
+        AND sourcepackagename IS NOT DISTINCT FROM $1.sourcepackagename
+        AND viewed_by IS NOT DISTINCT FROM $1.viewed_by
+        AND tag IS NOT DISTINCT FROM $1.tag
+        AND status IS NOT DISTINCT FROM $1.status
+        AND milestone IS NOT DISTINCT FROM $1.milestone;
+    -- If its not found then someone else also dec'd and won concurrently.
+$$;
+
+CREATE OR REPLACE FUNCTION bug_summary_inc(d bugsummary) RETURNS VOID
+LANGUAGE plpgsql AS
+$$
+BEGIN
+    -- Shameless adaption from postgresql manual
+    LOOP
+        -- first try to update the row
+        UPDATE BugSummary SET count = count + d.count
+        WHERE
+            product IS NOT DISTINCT FROM d.product
+            AND productseries IS NOT DISTINCT FROM d.productseries
+            AND distribution IS NOT DISTINCT FROM d.distribution
+            AND distroseries IS NOT DISTINCT FROM d.distroseries
+            AND sourcepackagename IS NOT DISTINCT FROM d.sourcepackagename
+            AND viewed_by IS NOT DISTINCT FROM d.viewed_by
+            AND tag IS NOT DISTINCT FROM d.tag
+            AND status IS NOT DISTINCT FROM d.status
+            AND milestone IS NOT DISTINCT FROM d.milestone;
+        IF found THEN
+            RETURN;
+        END IF;
+        -- not there, so try to insert the key
+        -- if someone else inserts the same key concurrently,
+        -- we could get a unique-key failure
+        BEGIN
+            INSERT INTO BugSummary(
+                count, product, productseries, distribution,
+                distroseries, sourcepackagename, viewed_by, tag,
+                status, milestone)
+            VALUES (
+                d.count, d.product, d.productseries, d.distribution,
+                d.distroseries, d.sourcepackagename, d.viewed_by, d.tag,
+                d.status, d.milestone);
+            RETURN;
+        EXCEPTION WHEN unique_violation THEN
+            -- do nothing, and loop to try the UPDATE again
+        END;
+    END LOOP;
+END;
+$$;
+
 COMMENT ON FUNCTION bugsummary_rollup_journal() IS
 'Collate and migrate rows from BugSummaryJournal to BugSummary';
 
