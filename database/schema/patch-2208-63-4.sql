6c5ecd3df082dc09868556bd81df44fcc0d6403c
Robert Collins <robert@canonical.com>
2011-06-10 07:37:43 +1200

    Use the temp table journal to reduce traffic to the persistent journal.

--- database/schema/patch-2208-63-4.sql
+++ database/schema/patch-2208-63-4.sql
@@ -75,206 +75,24 @@ $$;
 COMMENT ON FUNCTION bugsummary_rollup_journal() IS
 'Collate and migrate rows from BugSummaryJournal to BugSummary';
 
-
-CREATE OR REPLACE FUNCTION unsummarise_bug(BUG_ROW bug) RETURNS VOID
+CREATE OR REPLACE FUNCTION bug_summary_flush_temp_journal() RETURNS VOID
 LANGUAGE plpgsql VOLATILE AS
 $$
 DECLARE
     d bugsummary%ROWTYPE;
 BEGIN
-    FOR d IN SELECT * FROM bugsummary_locations(BUG_ROW) LOOP
-        d.count = -1;
+    -- may get called even though no summaries were made (for simplicity in the
+    -- callers)
+    PERFORM ensure_bugsummary_temp_journal();
+    FOR d IN SELECT * FROM bugsummary_temp_journal LOOP
         PERFORM bugsummary_journal_ins(d);
     END LOOP;
+    DELETE FROM bugsummary_temp_journal;
 END;
 $$;
 
-
-CREATE OR REPLACE FUNCTION summarise_bug(BUG_ROW bug) RETURNS VOID
-LANGUAGE plpgsql VOLATILE AS
-$$
-DECLARE
-    d bugsummary%ROWTYPE;
-BEGIN
-    FOR d IN SELECT * FROM bugsummary_locations(BUG_ROW) LOOP
-        d.count = 1;
-        PERFORM bugsummary_journal_ins(d);
-    END LOOP;
-END;
-$$;
-
-CREATE OR REPLACE FUNCTION bugsubscription_maintain_bug_summary()
-RETURNS TRIGGER LANGUAGE plpgsql VOLATILE
-SECURITY DEFINER SET search_path TO public AS
-$$
-BEGIN
-    -- This trigger only works if we are inserting, updating or deleting
-    -- a single row per statement.
-    IF TG_OP = 'INSERT' THEN
-        IF NOT (bug_row(NEW.bug)).private THEN
-            -- Public subscriptions are not aggregated.
-            RETURN NEW;
-        END IF;
-        IF TG_WHEN = 'BEFORE' THEN
-            PERFORM unsummarise_bug(bug_row(NEW.bug));
-        ELSE
-            PERFORM summarise_bug(bug_row(NEW.bug));
-        END IF;
-        RETURN NEW;
-    ELSIF TG_OP = 'DELETE' THEN
-        IF NOT (bug_row(OLD.bug)).private THEN
-            -- Public subscriptions are not aggregated.
-            RETURN OLD;
-        END IF;
-        IF TG_WHEN = 'BEFORE' THEN
-            PERFORM unsummarise_bug(bug_row(OLD.bug));
-        ELSE
-            PERFORM summarise_bug(bug_row(OLD.bug));
-        END IF;
-        RETURN OLD;
-    ELSE
-        IF (OLD.person IS DISTINCT FROM NEW.person
-            OR OLD.bug IS DISTINCT FROM NEW.bug) THEN
-            IF TG_WHEN = 'BEFORE' THEN
-                IF (bug_row(OLD.bug)).private THEN
-                    -- Public subscriptions are not aggregated.
-                    PERFORM unsummarise_bug(bug_row(OLD.bug));
-                END IF;
-                IF OLD.bug <> NEW.bug AND (bug_row(NEW.bug)).private THEN
-                    -- Public subscriptions are not aggregated.
-                    PERFORM unsummarise_bug(bug_row(NEW.bug));
-                END IF;
-            ELSE
-                IF (bug_row(OLD.bug)).private THEN
-                    -- Public subscriptions are not aggregated.
-                    PERFORM summarise_bug(bug_row(OLD.bug));
-                END IF;
-                IF OLD.bug <> NEW.bug AND (bug_row(NEW.bug)).private THEN
-                    -- Public subscriptions are not aggregated.
-                    PERFORM summarise_bug(bug_row(NEW.bug));
-                END IF;
-            END IF;
-        END IF;
-        RETURN NEW;
-    END IF;
-END;
-$$;
-
-
-CREATE OR REPLACE FUNCTION bugtag_maintain_bug_summary() RETURNS TRIGGER
-LANGUAGE plpgsql VOLATILE SECURITY DEFINER SET search_path TO public AS
-$$
-BEGIN
-    IF TG_OP = 'INSERT' THEN
-        IF TG_WHEN = 'BEFORE' THEN
-            PERFORM unsummarise_bug(bug_row(NEW.bug));
-        ELSE
-            PERFORM summarise_bug(bug_row(NEW.bug));
-        END IF;
-        RETURN NEW;
-    ELSIF TG_OP = 'DELETE' THEN
-        IF TG_WHEN = 'BEFORE' THEN
-            PERFORM unsummarise_bug(bug_row(OLD.bug));
-        ELSE
-            PERFORM summarise_bug(bug_row(OLD.bug));
-        END IF;
-        RETURN OLD;
-    ELSE
-        IF TG_WHEN = 'BEFORE' THEN
-            PERFORM unsummarise_bug(bug_row(OLD.bug));
-            IF OLD.bug <> NEW.bug THEN
-                PERFORM unsummarise_bug(bug_row(NEW.bug));
-            END IF;
-        ELSE
-            PERFORM summarise_bug(bug_row(OLD.bug));
-            IF OLD.bug <> NEW.bug THEN
-                PERFORM summarise_bug(bug_row(NEW.bug));
-            END IF;
-        END IF;
-        RETURN NEW;
-    END IF;
-END;
-$$;
-
--- fixed to use the journal
-CREATE OR REPLACE FUNCTION bug_maintain_bug_summary() RETURNS TRIGGER
-LANGUAGE plpgsql VOLATILE SECURITY DEFINER SET search_path TO public AS
-$$
-BEGIN
-    -- There is no INSERT logic, as a bug will not have any summary
-    -- information until BugTask rows have been attached.
-    IF TG_OP = 'UPDATE' THEN
-        IF OLD.duplicateof IS DISTINCT FROM NEW.duplicateof
-            OR OLD.private IS DISTINCT FROM NEW.private THEN
-            PERFORM unsummarise_bug(OLD);
-            PERFORM summarise_bug(NEW);
-        END IF;
-
-    ELSIF TG_OP = 'DELETE' THEN
-        PERFORM unsummarise_bug(OLD);
-    END IF;
-
-    RETURN NULL; -- Ignored - this is an AFTER trigger
-END;
-$$;
-
--- fixed to use the journal
-CREATE OR REPLACE FUNCTION bugtask_maintain_bug_summary() RETURNS TRIGGER
-LANGUAGE plpgsql VOLATILE SECURITY DEFINER SET search_path TO public AS
-$$
-BEGIN
-    -- This trigger only works if we are inserting, updating or deleting
-    -- a single row per statement.
-
-    -- Unlike bug_maintain_bug_summary, this trigger does not have access
-    -- to the old bug when invoked as an AFTER trigger. To work around this
-    -- we install this trigger as both a BEFORE and an AFTER trigger.
-    IF TG_OP = 'INSERT' THEN
-        IF TG_WHEN = 'BEFORE' THEN
-            PERFORM unsummarise_bug(bug_row(NEW.bug));
-        ELSE
-            PERFORM summarise_bug(bug_row(NEW.bug));
-        END IF;
-        RETURN NEW;
-
-    ELSIF TG_OP = 'DELETE' THEN
-        IF TG_WHEN = 'BEFORE' THEN
-            PERFORM unsummarise_bug(bug_row(OLD.bug));
-        ELSE
-            PERFORM summarise_bug(bug_row(OLD.bug));
-        END IF;
-        RETURN OLD;
-
-    ELSE
-        IF (OLD.product IS DISTINCT FROM NEW.product
-            OR OLD.productseries IS DISTINCT FROM NEW.productseries
-            OR OLD.distribution IS DISTINCT FROM NEW.distribution
-            OR OLD.distroseries IS DISTINCT FROM NEW.distroseries
-            OR OLD.sourcepackagename IS DISTINCT FROM NEW.sourcepackagename
-            OR OLD.status IS DISTINCT FROM NEW.status
-            OR OLD.milestone IS DISTINCT FROM NEW.milestone) THEN
-            IF TG_WHEN = 'BEFORE' THEN
-                PERFORM unsummarise_bug(bug_row(OLD.bug));
-                IF OLD.bug <> NEW.bug THEN
-                    PERFORM unsummarise_bug(bug_row(NEW.bug));
-                END IF;
-            ELSE
-                PERFORM summarise_bug(bug_row(OLD.bug));
-                IF OLD.bug <> NEW.bug THEN
-                    PERFORM summarise_bug(bug_row(NEW.bug));
-                END IF;
-            END IF;
-        END IF;
-        RETURN NEW;
-    END IF;
-END;
-$$;
-
--- No longer needed - we have a persistent and replicated journal.
-DROP FUNCTION ensure_bugsummary_temp_journal();
-DROP FUNCTION bug_summary_flush_temp_journal();
-DROP FUNCTION bug_summary_temp_journal_dec(d bugsummary);
-DROP FUNCTION bug_summary_temp_journal_inc(d bugsummary);
+COMMENT ON FUNCTION bug_summary_flush_temp_journal() IS
+'flush the temporary bugsummary journal into the bugsummaryjournal table';
 
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2208, 63, 4);
