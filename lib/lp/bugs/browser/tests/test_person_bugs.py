588e338a095d735330a7a02c81e7131785490beb
Abel Deuring <abel.deuring@canonical.com>
2011-06-10 12:50:45 +0200

    call canonical_url(self.conext) exactly once when BugSubscriberPackageBugsSearchListingView is rendered.

--- lib/lp/bugs/browser/tests/test_person_bugs.py
+++ lib/lp/bugs/browser/tests/test_person_bugs.py
@@ -7,7 +7,13 @@ __metaclass__ = type
 
 from canonical.testing.layers import DatabaseFunctionalLayer
 from lp.app.errors import UnexpectedFormData
-from lp.testing import TestCaseWithFactory
+from lp.bugs.interfaces.bugtask import BugTaskStatus
+from lp.registry.browser import person
+from lp.testing import (
+    person_logged_in,
+    TestCaseWithFactory,
+    )
+from lp.testing.fakemethod import FakeMethod
 from lp.testing.views import create_initialized_view
 
 
@@ -68,3 +74,26 @@ class TestBugSubscriberPackageBugsSearchListingView(TestCaseWithFactory):
             self.person, name='+packagebugs-search', form=form)
         self.assertRaises(
             UnexpectedFormData, getattr, view, 'current_package')
+
+    def test_one_call_of_canonical_url(self):
+        # canonical_url(self.context) is frequently needed to build
+        # URLs pointing to specific search listings in the
+        # +packagebugs page. These URLs are returned, among other
+        # data, by
+        # BugSubscriberPackageBugsSearchListingView.package_bug_counts
+        # This call is relatively expensive, hence a cached value is
+        # used.
+        view = create_initialized_view(self.person, name='+packagebugs')
+        self.factory.makeBug(
+            distribution=self.distribution, sourcepackagename=self.spn,
+            status=BugTaskStatus.INPROGRESS)
+        with person_logged_in(self.person):
+            self.dsp.addSubscription(self.person, subscribed_by=self.person)
+        fake_canonical_url = FakeMethod(result='')
+        real_canonical_url = person.canonical_url
+        person.canonical_url = fake_canonical_url
+        try:
+            view.package_bug_counts
+            self.assertEqual(1, fake_canonical_url.call_count)
+        finally:
+            person.canonical_url = real_canonical_url
