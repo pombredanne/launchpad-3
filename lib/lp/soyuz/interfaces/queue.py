9c3019da26c0958ce33e09e626c0440fccf8b615
Jeroen Vermeulen <jeroen.vermeulen@canonical.com>
2011-06-11 22:01:46 +0700

    Adding package name filter to IPackageUploadSet.getAll.

--- lib/lp/soyuz/interfaces/queue.py
+++ lib/lp/soyuz/interfaces/queue.py
@@ -604,7 +604,7 @@ class IPackageUploadSet(Interface):
         """
 
     def getAll(distroseries, created_since_date=None, status=None,
-               archive=None, pocket=None, custom_type=None):
+               archive=None, pocket=None, custom_type=None, name=None):
         """Get package upload records for a series with optional filtering.
 
         :param created_since_date: If specified, only returns items uploaded
@@ -613,6 +613,7 @@ class IPackageUploadSet(Interface):
         :param archive: Filter results for this `IArchive`
         :param pocket: Filter results by this `PackagePublishingPocket`
         :param custom_type: Filter results by this `PackageUploadCustomFormat`
+        :param name: Filter results by this package name.
         :return: A result set containing `IPackageUpload`s
         """
 
