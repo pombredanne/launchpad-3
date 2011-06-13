2ed1b4a509f6109d4577a49b7a44ee6fb3a06d5a
Raphael Badin <raphael.badin@canonical.com>
2011-06-13 10:10:49 +0200

    Fix doc.

--- lib/lp/soyuz/scripts/initialise_distroseries.py
+++ lib/lp/soyuz/scripts/initialise_distroseries.py
@@ -43,20 +43,19 @@ class InitialisationError(Exception):
 
 
 class InitialiseDistroSeries:
-    """Copy in all of the parent distroseries's configuration. This
+    """Copy in all of the parents distroseries's configuration. This
     includes all configuration for distroseries as well as distroarchseries,
     publishing and all publishing records for sources and binaries.
 
     Preconditions:
       The distroseries must exist, and be completly unused, with no source
       or binary packages existing, as well as no distroarchseries set up.
-      Section and component selections must be empty. It must not have a
-      parent series.
+      Section and component selections must be empty. It must not have any      parent series.
 
     Outcome:
       The distroarchseries set up in the parent series will be copied.
-      The publishing structure will be copied from the parent. All
-      PUBLISHED and PENDING packages in the parent will be created in
+      The publishing structure will be copied from the parents. All
+      PUBLISHED and PENDING packages in the parents will be created in
       this distroseries and its distroarchseriess. All component and section
       selections will be duplicated, as will any permission-related
       structures.
@@ -65,7 +64,7 @@ class InitialiseDistroSeries:
       This method will raise a InitialisationError when the pre-conditions
       are not met. After this is run, you still need to construct chroots
       for building, you need to add anything missing wrt. ports etc. This
-      method is only meant to give you a basic copy of a parent series in
+      method is only meant to give you a basic copy of parent series in
       order to assist you in preparing a new series of a distribution or
       in the initialisation of a derivative.
     """
