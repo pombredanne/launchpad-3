178411a6a68d9b1b97536f39f9e9aaa910fac82d
Steve Kowalik <stevenk@ubuntu.com>
2011-06-16 14:21:52 +1000

    Create BPRC.

---
+++ database/schema/patch-2208-99-0.sql
@@ -0,0 +1,12 @@
+-- Copyright 2011 Canonical Ltd.  This software is licensed under the
+-- GNU Affero General Public License version 3 (see the file LICENSE).
+
+SET client_min_messages=ERROR;
+
+CREATE TABLE BinaryPackageReleaseContents (
+    binarypackagerelease integer REFERENCES BinaryPackageRelease,
+    filename text,
+    PRIMARY KEY (binarypackagerelease, filename)
+);
+
+INSERT INTO LaunchpadDatabaseRevision VALUES (2208, 99, 0);
