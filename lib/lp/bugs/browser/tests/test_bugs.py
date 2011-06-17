509bac90946b6b9c98e1e59feea129eca2bc1bcd
j.c.sackett <jonathan.sackett@canonical.com>
2011-06-17 11:09:15 -0400

    Rollback bad js fix for bug 798081

--- lib/lp/bugs/browser/tests/test_bugs.py
+++ lib/lp/bugs/browser/tests/test_bugs.py
@@ -87,7 +87,7 @@ class TestMaloneView(TestCaseWithFactory):
             None, content.find(True, id=target_widget.show_widget_id))
         text = str(content)
         picker_script = (
-            "vocabulary_name: 'DistributionOrProductOrProjectGroup'")
+            "Y.lp.app.picker.create('DistributionOrProductOrProjectGroup'")
         self.assertIn(picker_script, text)
         focus_script = "setFocusByName('field.searchtext')"
         self.assertIn(focus_script, text)
