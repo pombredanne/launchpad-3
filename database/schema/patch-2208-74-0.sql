f019723309b29452669a7c0b4c242fc74278a3a8
Jelmer Vernooij <jelmer@canonical.com>
2011-06-27 12:38:18 +0200

    Use IN

--- database/schema/patch-2208-74-0.sql
+++ database/schema/patch-2208-74-0.sql
@@ -7,8 +7,8 @@ ALTER TABLE CodeImport DROP CONSTRAINT valid_vcs_details;
 ALTER TABLE CodeImport ADD CONSTRAINT "valid_vcs_details" CHECK (
 CASE
     WHEN rcs_type = 1 THEN cvs_root IS NOT NULL AND cvs_root <> ''::text AND cvs_module IS NOT NULL AND cvs_module <> ''::text AND url IS NULL
-    WHEN rcs_type = ANY (ARRAY[2, 3]) THEN cvs_root IS NULL AND cvs_module IS NULL AND url IS NOT NULL AND valid_absolute_url(url)
-    WHEN rcs_type = ANY (ARRAY[4, 5, 6]) THEN cvs_root IS NULL AND cvs_module IS NULL AND url IS NOT NULL
+    WHEN rcs_type IN (2, 3) THEN cvs_root IS NULL AND cvs_module IS NULL AND url IS NOT NULL AND valid_absolute_url(url)
+    WHEN rcs_type IN (4, 5, 6) THEN cvs_root IS NULL AND cvs_module IS NULL AND url IS NOT NULL
     ELSE false
 END);
 
