ffb855e82fa7bff24b8e0e608ce2e3afe74a5ae1
Gavin Panella <gavin.panella@canonical.com>
2011-06-30 14:18:38 +0100

    Long poll interfaces.

---
+++ lib/lp/app/longpoll/__init__.py
@@ -0,0 +1,9 @@
+# Copyright 2011 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Long-poll infrastructure."""
+
+__metaclass__ = type
+__all__ = []
+
+
---
+++ lib/lp/app/longpoll/interfaces.py
@@ -0,0 +1,31 @@
+# Copyright 2011 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Long-poll infrastructure interfaces."""
+
+__metaclass__ = type
+__all__ = []
+
+
+from zope.interface import Attribute, Interface
+
+
+class ILongPollEmitter(Interface):
+
+    emit_key = Attribute(
+        "The key with which events will be emitted. Should be predictable "
+        "and stable.")
+
+
+class ILongPollSubscriber(Interface):
+
+    subscribe_key = Attribute(
+        "The key which the subscriber must know in order to be able "
+        "to long-poll for subscribed events. Should be infeasible to "
+        "guess, a UUID for example.")
+
+    def subscribe(emitter):
+        """Subscribe to the given event emitter.
+
+        :type emitter: ILongPollEmitter
+        """
