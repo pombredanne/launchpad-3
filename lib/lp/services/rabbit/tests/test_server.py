579001423b8c1b3d6ff2b3aa2ac2b1ca9d768480
Gavin Panella <gavin.panella@canonical.com>
2011-07-05 21:35:00 +0100

    On second thoughts, don't inherit a test from rabbitfixture.

--- lib/lp/services/rabbit/tests/test_server.py
+++ lib/lp/services/rabbit/tests/test_server.py
@@ -8,29 +8,29 @@ __metaclass__ = type
 from textwrap import dedent
 
 from fixtures import EnvironmentVariableFixture
-from rabbitfixture.tests.test_server import TestRabbitFixture
 
 from canonical.testing.layers import BaseLayer
 from lp.services.rabbit.server import RabbitServer
+from lp.testing import TestCase
 
 
-class TestRabbitServer(TestRabbitFixture):
+class TestRabbitServer(TestCase):
 
     layer = BaseLayer
 
     def test_service_config(self):
         # Rabbit needs to fully isolate itself: an existing per user
-        # .erlange.cookie has to be ignored, and ditto bogus HOME if other
+        # .erlang.cookie has to be ignored, and ditto bogus HOME if other
         # tests fail to cleanup.
         self.useFixture(EnvironmentVariableFixture('HOME', '/nonsense/value'))
 
         # RabbitServer pokes some .ini configuration into its config.
-        with RabbitServer() as fixture:
-            expected = dedent("""\
-                [rabbitmq]
-                host: localhost:%d
-                userid: guest
-                password: guest
-                virtual_host: /
-                """ % fixture.config.port)
-            self.assertEqual(expected, fixture.config.service_config)
+        fixture = self.useFixture(RabbitServer())
+        expected = dedent("""\
+            [rabbitmq]
+            host: localhost:%d
+            userid: guest
+            password: guest
+            virtual_host: /
+            """ % fixture.config.port)
+        self.assertEqual(expected, fixture.config.service_config)
