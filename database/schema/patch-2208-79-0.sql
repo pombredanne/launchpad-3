755e64dd618bf55c1ae1763a48660d539e909be6
Colin Watson <cjwatson@canonical.com>
2011-07-11 18:12:46 +0100

    Official patch number.

--- database/schema/patch-2208-79-0.sql
+++ database/schema/patch-2208-79-0.sql
@@ -6,4 +6,4 @@ SET client_min_messages=ERROR;
 ALTER TABLE distroseries
     ADD COLUMN split_long_descriptions BOOLEAN NOT NULL DEFAULT FALSE;
 
-INSERT INTO LaunchpadDatabaseRevision VALUES (2208, 99, 0);
+INSERT INTO LaunchpadDatabaseRevision VALUES (2208, 79, 0);
