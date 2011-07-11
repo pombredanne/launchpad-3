b628d5a41c48eeff9e65862da405e4b20b21b517
Colin Watson <cjwatson@canonical.com>
2011-07-11 17:27:52 +0100

    Add DB patch with distroseries.split_long_descriptions.

---
+++ database/schema/patch-2208-99-0.sql
@@ -0,0 +1,9 @@
+-- Copyright 2011 Canonical Ltd.  This software is licensed under the
+-- GNU Affero General Public License version 3 (see the file LICENSE).
+
+SET client_min_messages=ERROR;
+
+ALTER TABLE distroseries
+    ADD COLUMN split_long_descriptions BOOLEAN NOT NULL DEFAULT FALSE;
+
+INSERT INTO LaunchpadDatabaseRevision VALUES (2208, 99, 0);
