b6c007a8de50d9a890c3a1da540183bcafd50e10
Bryce Harrington <bryce@canonical.com>
2011-07-12 19:21:20 -0700

    Since importing a single module, no need for tuple syntax here

--- lib/lp/bugs/browser/bugtracker.py
+++ lib/lp/bugs/browser/bugtracker.py
@@ -66,9 +66,7 @@ from lp.app.browser.launchpadform import (
 from lp.app.interfaces.launchpad import ILaunchpadCelebrities
 from lp.app.widgets.itemswidgets import LaunchpadRadioWidget
 from lp.app.widgets.textwidgets import DelimitedListWidget
-from lp.bugs.browser.widgets.bugtask import (
-    UbuntuSourcePackageNameWidget,
-    )
+from lp.bugs.browser.widgets.bugtask import UbuntuSourcePackageNameWidget
 from lp.bugs.interfaces.bugtracker import (
     BugTrackerType,
     IBugTracker,
