a9b9fa156e35c7c8a8f5419c138220ac5917aa95
Ian Booth <ian.booth@canonical.com>
2011-07-19 13:27:34 +1000

    Tidy up picker creation

--- lib/lp/app/widgets/tests/test_popup.py
+++ lib/lp/app/widgets/tests/test_popup.py
@@ -62,21 +62,32 @@ class TestVocabularyPickerWidget(TestCaseWithFactory):
         widget_config = simplejson.loads(picker_widget.json_config)
         self.assertEqual(
             'ValidTeamOwner', picker_widget.vocabulary_name)
-        self.assertEqual(
-            simplejson.dumps(self.vocabulary.displayname),
-            widget_config['header'])
-        self.assertEqual(
-            simplejson.dumps(self.vocabulary.step_title),
+        self.assertEqual(self.vocabulary.displayname, widget_config['header'])
+        self.assertEqual(self.vocabulary.step_title,
             widget_config['step_title'])
         self.assertEqual(
             'show-widget-field-test_valid-item', picker_widget.show_widget_id)
         self.assertEqual(
             'field.test_valid.item', picker_widget.input_id)
-        self.assertEqual(
-            simplejson.dumps(None), picker_widget.extra_no_results_message)
+        self.assertIsNone(picker_widget.extra_no_results_message)
         markup = picker_widget()
-        self.assertIn(
-            "Y.lp.app.picker.create('ValidTeamOwner', config);", markup)
+        self.assertTextMatchesExpressionIgnoreWhitespace("""\
+            .*
+            picker = Y\\.lp\\.app\\.picker\\.create\\(
+                'ValidTeamOwner',
+                {"show_remove_button": "false",
+                "show_assign_me_button": "false",
+                "assign_me_text": "Pick me",
+                "remove_team_text": "Remove team",
+                "extra_no_results_message": null,
+                "remove_person_text": "Remove person",
+                "header": "Select a Team or Person",
+                "selected_value_metadata": null,
+                "picker_type": "default",
+                "step_title": "Search"},
+                'field\\.test_valid.item'\\);
+            .*
+            """, markup)
 
     def test_widget_fieldname_with_invalid_html_chars(self):
         # Check the picker widget is correctly set up for a field which has a
