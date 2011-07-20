65e3e2a40cf6fd5abf1440765ac0f757bfe9e743
Jelmer Vernooij <jelmer@canonical.com>
2011-07-20 19:20:03 +0200

    Start autoupgrading again.

--- lib/lp/codehosting/codeimport/tests/test_worker.py
+++ lib/lp/codehosting/codeimport/tests/test_worker.py
@@ -208,15 +208,17 @@ class TestBazaarBranchStore(WorkerTest):
         store = self.makeBranchStore()
         target_url = store._getMirrorURL(self.arbitrary_branch_id)
         knit_format = format_registry.get('knit')()
-        create_branch_with_one_revision(target_url, format=knit_format)
+        tree = create_branch_with_one_revision(target_url, format=knit_format)
+        self.assertNotEquals(tree.bzrdir._format.repository_format.network_name(),
+            default_format.repository_format.network_name())
 
         # The fetched branch is in the default format.
         new_branch = store.pull(
             self.arbitrary_branch_id, self.temp_dir, default_format)
         # Make sure backup.bzr is removed, as it interferes with CSCVS.
         self.assertEquals(os.listdir(self.temp_dir), [".bzr"])
-        self.assertEqual(
-            default_format, new_branch.bzrdir._format)
+        self.assertEquals(new_branch.repository._format.network_name(),
+            default_format.repository_format.network_name())
 
     def test_pushUpgradesFormat(self):
         # A branch should always be in the most up-to-date format before a
