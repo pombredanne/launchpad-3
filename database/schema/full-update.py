f8703de73693a6400c9a6dee3c2cc04a6fb5ec3e
Stuart Bishop <stuart.bishop@canonical.com>
2011-07-25 20:56:30 +0700

    Bring pgbouncer backup even if some database update steps failed

--- database/schema/full-update.py
+++ database/schema/full-update.py
@@ -33,6 +33,21 @@ def run_script(script, *extra_args):
     return subprocess.call([script_path] + sys.argv[1:] + list(extra_args))
 
 
+def run_pgbouncer(log, cmd):
+    """Invoke the pgbouncer initscript.
+
+    :param cmd: One of 'start', 'stop' or 'status'.
+    """
+    assert cmd in ('start', 'stop', 'status'), (
+        'sudo access needs to be granted to new commands on staging and prod'
+        )
+    pgbouncer_rc = subprocess.call(PGBOUNCER_INITD + [cmd])
+    sys.stdout.flush()
+    if pgbouncer_rc != 0:
+        log.error("pgbouncer '%s' failed [%s]", cmd, pgbouncer_rc)
+    return pgbouncer_rc
+
+
 def run_upgrade(options, log):
     """Invoke upgrade.py in-process.
 
@@ -101,10 +116,8 @@ def main():
 
     # Confirm we can invoke PGBOUNCER_INITD
     pgbouncer_status_cmd = PGBOUNCER_INITD + ['status']
-    pgbouncer_rc = subprocess.call(pgbouncer_status_cmd)
-    sys.stdout.flush()
+    pgbouncer_rc = run_pgbouncer(log, 'status')
     if pgbouncer_rc != 0:
-        log.fatal("Unable to invoke %s", ' '.join(pgbouncer_status_cmd))
         return pgbouncer_rc
 
     #
@@ -121,8 +134,7 @@ def main():
 
     try:
         # Shutdown pgbouncer
-        pgbouncer_rc = subprocess.call(PGBOUNCER_INITD + ['stop'])
-        sys.stdout.flush()
+        pgbouncer_rc = run_pgbouncer(log, 'stop')
         if pgbouncer_rc != 0:
             log.fatal("pgbouncer not shut down [%s]", pgbouncer_rc)
             return pgbouncer_rc
@@ -151,8 +163,7 @@ def main():
 
         log.info("All database upgrade steps completed")
 
-        pgbouncer_rc = subprocess.call(PGBOUNCER_INITD + ['start'])
-        sys.stdout.flush()
+        pgbouncer_rc = run_pgbouncer(log, 'start')
         if pgbouncer_rc != 0:
             log.fatal("pgbouncer not restarted [%s]", pgbouncer_rc)
             return pgbouncer_rc
@@ -168,7 +179,24 @@ def main():
 
     finally:
         if pgbouncer_down:
-            log.warning("pgbouncer is down and will need to be restarted")
+            # Even if upgrade.py or security.py failed, we should be in
+            # a good enough state to continue operation so restart
+            # pgbouncer and allow connections.
+            #  - upgrade.py may have failed to update the master, and 
+            #    changes should have rolled back.
+            #  - upgrade.py may have failed to update a slave, breaking
+            #    replication. The master is still operational, but
+            #    slaves may be lagging and have the old schema.
+            #  - security.py may have died, rolling back its changes on
+            #    one or more nodes.
+            # In all cases except the first, we have recovery to do but
+            # systems are probably ok, or at least providing some
+            # services.
+            pgbouncer_rc = run_pgbouncer(log, 'start')
+            if pgbouncer_rc == 0:
+                log.info("Despite failures, pgbouncer restarted.")
+            else:
+                log.fatal("pgbouncer is down and refuses to restart")
         if not upgrade_run:
             log.warning("upgrade.py still needs to be run")
         # Bug #815717
