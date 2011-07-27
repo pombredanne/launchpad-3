e00b0c9de97a8d2d8aae6ca67a0b145e99244598
Abel Deuring <abel.deuring@canonical.com>
2011-07-27 18:09:29 +0200

    new class StormRangeFactory

--- lib/canonical/launchpad/webapp/batching.py
+++ lib/canonical/launchpad/webapp/batching.py
@@ -3,8 +3,11 @@
 
 __metaclass__ = type
 
+from datetime import datetime
+
 import lazr.batchnavigator
 from lazr.batchnavigator.interfaces import IRangeFactory
+import simplejson
 from storm import Undef
 from storm.expr import Desc
 from storm.properties import PropertyColumn
@@ -14,6 +17,7 @@ from zope.interface import implements
 from zope.interface.common.sequence import IFiniteSequence
 from zope.security.proxy import (
     isinstance as zope_isinstance,
+    ProxyFactory,
     removeSecurityProxy,
     )
 
@@ -30,7 +34,7 @@ from canonical.launchpad.webapp.publisher import LaunchpadView
 
 class FiniteSequenceAdapter:
 
-    adapts(IResultSet) # and ISQLObjectResultSet
+    adapts(IResultSet)
     implements(IFiniteSequence)
 
     def __init__(self, context):
@@ -139,6 +143,17 @@ class TableBatchNavigator(BatchNavigator):
                 self.show_column[column_to_show] = True
 
 
+class DateTimeJSONEncoder(simplejson.JSONEncoder):
+    """A JSON encoder that understands datetime objects.
+
+    Datetime objects are formatted according to ISO 1601.
+    """
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            return obj.isoformat()
+        return simplejson.JSONEncoder.default(self, obj)
+
+
 class StormRangeFactory:
     """A range factory for Storm result sets.
 
@@ -163,17 +178,21 @@ class StormRangeFactory:
     """
     implements(IRangeFactory)
 
-    def __init__(self, resultset):
+    def __init__(self, resultset, error_cb=None):
         """Create a new StormRangeFactory instance.
 
         :param resultset: A Storm ResultSet instance or a DecoratedResultSet
             instance.
+        :param error_cb: A function which takes one string as a parameter.
+            It is called when the parameter endpoint_memo of getSlice()
+            does not match the order settings of a resultset.
         """
         self.resultset = resultset
         if zope_isinstance(resultset, DecoratedResultSet):
             self.plain_resultset = resultset.getPlainResultSet()
         else:
             self.plain_resultset = resultset
+        self.error_cb = error_cb
 
     def getSortExpressions(self):
         """Return the order_by expressions of the result set."""
@@ -214,8 +233,128 @@ class StormRangeFactory:
         """See `IRangeFactory`."""
         lower = self.getOrderValuesFor(batch.first())
         upper = self.getOrderValuesFor(batch.last())
-        # xxx incomplete
+        return (
+            simplejson.dumps(lower, cls=DateTimeJSONEncoder),
+            simplejson.dumps(upper, cls=DateTimeJSONEncoder),
+            )
+
+    def reportError(self, message):
+        if self.error_cb is not None:
+            self.error_cb(message)
+
+    def parseMemo(self, memo):
+        """Convert the given memo string into a sequence of Python objects.
+
+        memo should be a JSON string as returned by getEndpointMemos().
+
+        Note that memo originates from a URL query parameter and can thus
+        not be trusted to always contain formally valid and consistent
+        data.
+
+        Parsing errors or data not matching the sort parameters of the
+        result set are simply ignored.
+        """
+        if memo == '':
+            return None
+        try:
+            parsed_memo = simplejson.loads(memo)
+        except simplejson.JSONDecodeError:
+            self.reportError('memo is not a valid JSON string.')
+            return None
+        if not isinstance(parsed_memo, list):
+            self.reportError(
+                'memo must be the JSON representation of a list.')
+            return None
+
+        sort_expressions = self.getSortExpressions()
+        if len(sort_expressions) != len(parsed_memo):
+            self.reportError(
+                'Invalid number of elements in memo string. '
+                'Expected: %i, got: %i'
+                % (len(sort_expressions), len(parsed_memo)))
+            return None
+
+        converted_memo = []
+        for expression, value in zip(sort_expressions, parsed_memo):
+            if isinstance(expression, Desc):
+                expression = expression.expr
+            try:
+                expression.variable_factory(value=value)
+            except TypeError, error:
+                # A TypeError is raised when the type of value cannot
+                # be used for expression. All expected types are
+                # properly created by simplejson.loads() above, except
+                # time stamps which are represented as strings in
+                # ISO format. If value is a string and if it can be
+                # converted into a datetime object, we have a valid
+                # value.
+                if (str(error).startswith('Expected datetime') and
+                    isinstance(value, str)):
+                    try:
+                        value = datetime.strptime(
+                            value, '%Y-%m-%dT%H:%M:%S.%f')
+                    except ValueError:
+                        # One more attempt: If the fractions of a second
+                        # are zero, datetime.isoformat() omits the
+                        # entire part '.000000', so we need a different
+                        # format for strptime().
+                        try:
+                            value = datetime.strptime(
+                                value, '%Y-%m-%dT%H:%M:%S')
+                        except ValueError:
+                            self.reportError(
+                                'Invalid datetime value: %r' % value)
+                            return None
+                else:
+                    self.reportError(
+                        'Invalid parameter: %r' % value)
+                    return None
+            converted_memo.append(value)
+        return converted_memo
+
+    def reverseSortOrder(self):
+        """Return a list of reversed sort expressions."""
+        def invert_sort_expression(expr):
+            if isinstance(expression, Desc):
+                return expression.expr
+            else:
+                return Desc(expression)
+
+        return [
+            invert_sort_expression(expression)
+            for expression in self.getSortExpressions()]
+
+    def whereExpressionFromSortExpression(self, expression, forwards, memo):
+        """Create a Storm expression to be used in the WHERE clause of the
+        slice query.
+        """
+        if isinstance(expression, Desc):
+            forwards = not forwards
+            expression = expression.expr
+        if forwards:
+            return expression > memo
+        else:
+            return expression < memo
 
     def getSlice(self, size, endpoint_memo='', forwards=True):
         """See `IRangeFactory`."""
-        pass #xxxxx incomplete
+        parsed_memo = self.parseMemo(endpoint_memo)
+        if parsed_memo is None:
+            if not forwards:
+                self.resultset.order_by(*self.reverseSortOrder())
+            return self.resultset.config(limit=size)
+        else:
+            sort_expressions = self.getSortExpressions()
+            where = [
+                self.whereExpressionFromSortExpression(
+                    expression, forwards, memo)
+                for expression, memo in zip(sort_expressions, parsed_memo)]
+            # From storm.zope.interfaces.IResultSet.__doc__:
+            #     - C{find()}, C{group_by()} and C{having()} are really
+            #       used to configure result sets, so are mostly intended
+            #       for use on the model side.
+            naked_result = removeSecurityProxy(self.resultset).find(*where)
+            result = ProxyFactory(naked_result)
+            if not forwards:
+                result.order_by(*self.reverseSortOrder())
+            return result.config(limit=size)
--- lib/canonical/launchpad/webapp/tests/test_batching.py
+++ lib/canonical/launchpad/webapp/tests/test_batching.py
@@ -3,16 +3,30 @@
 
 __metaclass__ = type
 
+from datetime import datetime
+import simplejson
 from unittest import TestLoader
 
 from lazr.batchnavigator.interfaces import IRangeFactory
+from storm.expr import (
+    Desc,
+    Gt,
+    Lt,
+    )
+from storm.variables import IntVariable
 from zope.security.proxy import isinstance as zope_isinstance
+
 from canonical.launchpad.components.decoratedresultset import (
     DecoratedResultSet,
     )
 from canonical.launchpad.database.librarian import LibraryFileAlias
-from canonical.launchpad.webapp.batching import StormRangeFactory
+from canonical.launchpad.webapp.batching import (
+    BatchNavigator,
+    DateTimeJSONEncoder,
+    StormRangeFactory,
+    )
 from canonical.launchpad.webapp.interfaces import StormRangeFactoryError
+from canonical.launchpad.webapp.servers import LaunchpadTestRequest
 from canonical.launchpad.webapp.testing import verifyObject
 from canonical.testing.layers import LaunchpadFunctionalLayer
 from lp.registry.model.person import Person
@@ -26,6 +40,10 @@ class TestStormRangeFactory(TestCaseWithFactory):
     """Tests for StormRangeFactory."""
 
     layer = LaunchpadFunctionalLayer
+
+    def setUp(self):
+        super(TestStormRangeFactory, self).setUp()
+        self.error_messages = []
 
     def makeStormResultSet(self):
         bug = self.factory.makeBug()
@@ -125,6 +143,15 @@ class TestStormRangeFactory(TestCaseWithFactory):
             [plain_resultset[0][1].id],
             range_factory.getOrderValuesFor(plain_resultset[0]))
 
+    def test_getOrderValuesFor__descending_sort_order(self):
+        # getOrderValuesFor() can retrieve values from reverse sorted
+        # columns.
+        resultset = self.makeStormResultSet()
+        resultset = resultset.order_by(Desc(Person.id))
+        range_factory = StormRangeFactory(resultset)
+        self.assertEqual(
+            [resultset[0].id], range_factory.getOrderValuesFor(resultset[0]))
+
     def test_getOrderValuesFor__table_not_included_in_results(self):
         # Attempts to use a sort by a column which does not appear in the
         # data returned by the query raise a StormRangeFactoryError.
@@ -140,6 +167,266 @@ class TestStormRangeFactory(TestCaseWithFactory):
             "not contained in the result set, but are required to retrieve "
             "the value of LibraryFileAlias.id.",
             str(exception))
+
+    def test_DatetimeJSONEncoder(self):
+        # DateTimeJSONEncoder represents Pytjon datetime objects as strings
+        # where the value is represented in the ISO time format.
+        self.assertEqual(
+            '"2011-07-25T00:00:00"',
+            simplejson.dumps(datetime(2011, 7, 25), cls=DateTimeJSONEncoder))
+
+        # DateTimeJSONEncoder works for the regular Python types that can
+        # represented as JSON strings.
+        encoded = simplejson.dumps(
+            ('foo', 1, 2.0, [3, 4], {5: 'bar'}, datetime(2011, 7, 24)),
+            cls=DateTimeJSONEncoder)
+        self.assertEqual(
+            '["foo", 1, 2.0, [3, 4], {"5": "bar"}, "2011-07-24T00:00:00"]',
+            encoded
+            )
+
+    def test_getEndpointMemos(self):
+        # getEndpointMemos() returns JSON representations of the
+        # sort fields of the first and last element of a batch.
+        resultset = self.makeStormResultSet()
+        resultset.order_by(Person.name)
+        request = LaunchpadTestRequest()
+        batchnav = BatchNavigator(
+            resultset, request, size=3, range_factory=StormRangeFactory)
+        range_factory = StormRangeFactory(resultset)
+        first, last = range_factory.getEndpointMemos(batchnav.batch)
+        expected_first = simplejson.dumps(
+            range_factory.getOrderValuesFor(batchnav.batch.first()),
+            cls=DateTimeJSONEncoder)
+        expected_last = simplejson.dumps(
+            range_factory.getOrderValuesFor(batchnav.batch.last()),
+            cls=DateTimeJSONEncoder)
+        self.assertEqual(expected_first, first)
+        self.assertEqual(expected_last, last)
+
+    def logError(self, message):
+        # An error callback for StormResultSet.
+        self.error_messages.append(message)
+
+    def test_parseMemo__empty_value(self):
+        # parseMemo() returns None for an empty memo value.
+        resultset = self.makeStormResultSet()
+        range_factory = StormRangeFactory(resultset, self.logError)
+        self.assertIs(None, range_factory.parseMemo(''))
+        self.assertEqual(0, len(self.error_messages))
+
+    def test_parseMemo__json_error(self):
+        # parseMemo() returns None for formally invalid JSON strings.
+        resultset = self.makeStormResultSet()
+        range_factory = StormRangeFactory(resultset, self.logError)
+        self.assertIs(None, range_factory.parseMemo('foo'))
+        self.assertEqual(
+            ['memo is not a valid JSON string.'], self.error_messages)
+
+    def test_parseMemo__json_no_sequence(self):
+        # parseMemo() accepts only JSON representations of lists.
+        resultset = self.makeStormResultSet()
+        range_factory = StormRangeFactory(resultset, self.logError)
+        self.assertIs(None, range_factory.parseMemo(simplejson.dumps(1)))
+        self.assertEqual(
+            ['memo must be the JSON representation of a list.'],
+            self.error_messages)
+
+    def test_parseMemo__wrong_list_length(self):
+        # parseMemo() accepts only lists which have as many elements
+        # as the number of sort expressions used in the SQL query of
+        # the result set.
+        resultset = self.makeStormResultSet()
+        resultset.order_by(Person.name, Person.id)
+        range_factory = StormRangeFactory(resultset, self.logError)
+        self.assertIs(
+            None, range_factory.parseMemo(simplejson.dumps([1])))
+        expected_message = (
+            'Invalid number of elements in memo string. Expected: 2, got: 1')
+        self.assertEqual([expected_message], self.error_messages)
+
+    def test_parseMemo__memo_type_check(self):
+        # parseMemo() accepts only lists containing values that can
+        # be used in sort expression of the given result set.
+        resultset = self.makeStormResultSet()
+        resultset.order_by(Person.datecreated, Person.name, Person.id)
+        range_factory = StormRangeFactory(resultset, self.logError)
+        invalid_memo = [datetime(2011, 7, 25, 11, 30, 30, 45), 'foo', 'bar']
+        json_data = simplejson.dumps(invalid_memo, cls=DateTimeJSONEncoder)
+        self.assertIs(None, range_factory.parseMemo(json_data))
+        self.assertEqual(["Invalid parameter: 'bar'"], self.error_messages)
+
+    def test_parseMemo__valid_data(self):
+        # If a memo string contains valid data, parseMemo returns this data.
+        resultset = self.makeStormResultSet()
+        resultset.order_by(Person.datecreated, Person.name, Person.id)
+        range_factory = StormRangeFactory(resultset, self.logError)
+        valid_memo = [datetime(2011, 7, 25, 11, 30, 30, 45), 'foo', 1]
+        json_data = simplejson.dumps(valid_memo, cls=DateTimeJSONEncoder)
+        self.assertEqual(valid_memo, range_factory.parseMemo(json_data))
+        self.assertEqual(0, len(self.error_messages))
+
+    def test_parseMemo__short_iso_timestamp(self):
+        # An ISO timestamp without fractions of a second
+        # (YYYY-MM-DDThh:mm:ss) is a valid value for colums which
+        # store datetime values.
+        resultset = self.makeStormResultSet()
+        resultset.order_by(Person.datecreated)
+        range_factory = StormRangeFactory(resultset, self.logError)
+        valid_short_timestamp_json = '["2011-07-25T11:30:30"]'
+        self.assertEqual(
+            [datetime(2011, 7, 25, 11, 30, 30)],
+            range_factory.parseMemo(valid_short_timestamp_json))
+        self.assertEqual(0, len(self.error_messages))
+
+    def test_parseMemo__long_iso_timestamp(self):
+        # An ISO timestamp with fractions of a second
+        # (YYYY-MM-DDThh:mm:ss.ffffff) is a valid value for colums
+        # which store datetime values.
+        resultset = self.makeStormResultSet()
+        resultset.order_by(Person.datecreated)
+        range_factory = StormRangeFactory(resultset, self.logError)
+        valid_long_timestamp_json = '["2011-07-25T11:30:30.123456"]'
+        self.assertEqual(
+            [datetime(2011, 7, 25, 11, 30, 30, 123456)],
+            range_factory.parseMemo(valid_long_timestamp_json))
+        self.assertEqual(0, len(self.error_messages))
+
+    def test_parseMemo__invalid_iso_timestamp_value(self):
+        # An ISO timestamp with an invalid date is rejected as a memo
+        # string.
+        resultset = self.makeStormResultSet()
+        resultset.order_by(Person.datecreated)
+        range_factory = StormRangeFactory(resultset, self.logError)
+        invalid_timestamp_json = '["2011-05-35T11:30:30"]'
+        self.assertIs(
+            None, range_factory.parseMemo(invalid_timestamp_json))
+        self.assertEqual(
+            ["Invalid datetime value: '2011-05-35T11:30:30'"],
+            self.error_messages)
+
+    def test_parseMemo__nonsencial_iso_timestamp_value(self):
+        # A memo string is rejected when an ISO timespamp is expected
+        # but a nonsensical string is provided.
+        resultset = self.makeStormResultSet()
+        resultset.order_by(Person.datecreated)
+        range_factory = StormRangeFactory(resultset, self.logError)
+        nonsensical_timestamp_json = '["bar"]'
+        self.assertIs(
+            None, range_factory.parseMemo(nonsensical_timestamp_json))
+        self.assertEqual(
+            ["Invalid datetime value: 'bar'"],
+            self.error_messages)
+
+    def test_parseMemo__descending_sort_order(self):
+        # Validation of a memo string against a descending sort order works.
+        resultset = self.makeStormResultSet()
+        resultset.order_by(Desc(Person.id))
+        range_factory = StormRangeFactory(resultset, self.logError)
+        self.assertEqual(
+            [1], range_factory.parseMemo(simplejson.dumps([1])))
+
+    def test_reverseSortOrder(self):
+        # reverseSortOrder() wraps a plain PropertyColumn instance into
+        # Desc(), and it returns the plain PropertyCOlumn for a Desc()
+        # expression.
+        resultset = self.makeStormResultSet()
+        resultset.order_by(Person.id, Desc(Person.name))
+        range_factory = StormRangeFactory(resultset, self.logError)
+        reverse_person_id, person_name = range_factory.reverseSortOrder()
+        self.assertTrue(isinstance(reverse_person_id, Desc))
+        self.assertIs(Person.id, reverse_person_id.expr)
+        self.assertIs(Person.name, person_name)
+
+    def test_whereExpressionFromSortExpression__forwards_asc(self):
+        """For ascending sort order and forward slicing,
+        whereExpressionFromSortExpression() returns the WHERE clause
+        expression > memo.
+        """
+        resultset = self.makeStormResultSet()
+        range_factory = StormRangeFactory(resultset, self.logError)
+        where_clause = range_factory.whereExpressionFromSortExpression(
+            expression=Person.id, forwards=True, memo=1)
+        self.assertTrue(isinstance(where_clause, Gt))
+        self.assertIs(where_clause.expr1, Person.id)
+        self.assertTrue(where_clause.expr2, IntVariable)
+
+    def test_whereExpressionFromSortExpression__forwards_desc(self):
+        """For descending sort order and forward slicing,
+        whereExpressionFromSortExpression() returns the WHERE clause
+        expression < memo.
+        """
+        resultset = self.makeStormResultSet()
+        range_factory = StormRangeFactory(resultset, self.logError)
+        where_clause = range_factory.whereExpressionFromSortExpression(
+            expression=Desc(Person.id), forwards=True, memo=1)
+        self.assertTrue(isinstance(where_clause, Lt))
+        self.assertIs(where_clause.expr1, Person.id)
+        self.assertTrue(where_clause.expr2, IntVariable)
+
+    def test_whereExpressionFromSortExpression__backwards_asc(self):
+        """For ascending sort order and forward slicing,
+        whereExpressionFromSortExpression() returns the WHERE clause
+        expression < memo.
+        """
+        resultset = self.makeStormResultSet()
+        range_factory = StormRangeFactory(resultset, self.logError)
+        where_clause = range_factory.whereExpressionFromSortExpression(
+            expression=Person.id, forwards=False, memo=1)
+        self.assertTrue(isinstance(where_clause, Lt))
+        self.assertIs(where_clause.expr1, Person.id)
+        self.assertTrue(where_clause.expr2, IntVariable)
+
+    def test_whereExpressionFromSortExpression__backwards_desc(self):
+        """For descending sort order and forward slicing,
+        whereExpressionFromSortExpression() returns the WHERE clause
+        expression > memo.
+        """
+        resultset = self.makeStormResultSet()
+        range_factory = StormRangeFactory(resultset, self.logError)
+        where_clause = range_factory.whereExpressionFromSortExpression(
+            expression=Desc(Person.id), forwards=False, memo=1)
+        self.assertTrue(isinstance(where_clause, Gt))
+        self.assertIs(where_clause.expr1, Person.id)
+        self.assertTrue(where_clause.expr2, IntVariable)
+
+    def test_getSlice__forward_without_memo(self):
+        resultset = self.makeStormResultSet()
+        resultset.order_by(Person.name, Person.id)
+        all_results = list(resultset)
+        range_factory = StormRangeFactory(resultset)
+        sliced_result = range_factory.getSlice(3)
+        self.assertEqual(all_results[:3], list(sliced_result))
+
+    def test_getSlice__forward_with_memo(self):
+        resultset = self.makeStormResultSet()
+        resultset.order_by(Person.name, Person.id)
+        all_results = list(resultset)
+        memo = simplejson.dumps([all_results[0].name, all_results[0].id])
+        range_factory = StormRangeFactory(resultset)
+        sliced_result = range_factory.getSlice(3, memo)
+        self.assertEqual(all_results[1:4], list(sliced_result))
+
+    def test_getSlice__backward_without_memo(self):
+        resultset = self.makeStormResultSet()
+        resultset.order_by(Person.name, Person.id)
+        all_results = list(resultset)
+        expected = all_results[-3:]
+        expected.reverse()
+        range_factory = StormRangeFactory(resultset)
+        sliced_result = range_factory.getSlice(3, forwards=False)
+        self.assertEqual(expected, list(sliced_result))
+
+    def test_getSlice_backward_with_memo(self):
+        resultset = self.makeStormResultSet()
+        resultset.order_by(Person.name, Person.id)
+        all_results = list(resultset)
+        expected = all_results[1:4]
+        expected.reverse()
+        memo = simplejson.dumps([all_results[4].name, all_results[4].id])
+        range_factory = StormRangeFactory(resultset)
+        sliced_result = range_factory.getSlice(3, memo, forwards=False)
+        self.assertEqual(expected, list(sliced_result))
 
 
 def test_suite():
