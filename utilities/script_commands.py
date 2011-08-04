4816fd0b58606571119a67c05d0f0dde68b85c85
Aaron Bentley <aaron@canonical.com>
2011-08-04 16:26:07 -0400

    Switch to command-from-function approach.

--- utilities/script_commands.py
+++ utilities/script_commands.py
@@ -1,8 +1,44 @@
 import inspect
+from optparse import OptionParser
 
 
 class UserError(Exception):
     pass
+
+
+def add_dict(name, **kwargs):
+    def decorator(func):
+        setattr(func, name, kwargs)
+        return func
+    return decorator
+
+
+def types(**kwargs):
+    return add_dict('_types', **kwargs)
+
+def helps(**kwargs):
+    return add_dict('_helps', **kwargs)
+
+
+
+def get_function_parser(function):
+    parser = OptionParser()
+    args, ignore, ignored, defaults = inspect.getargspec(function)
+    for arg in args:
+        arg_type = function._types.get(arg)
+        if arg_type is None:
+            continue
+        arg_help = getattr(function, '_helps', {}).get(arg)
+        if arg_help is not None:
+            arg_help +=' Default: %default.'
+        parser.add_option('--%s' % arg, type=arg_type, help=arg_help)
+    if defaults is not None:
+        defaults_dict = dict(zip(args, defaults))
+        option_defaults = dict(
+            (key, value) for key, value in defaults_dict.items()
+            if parser.defaults.get(key, '') is None)
+        parser.set_defaults(**option_defaults)
+    return parser
 
 
 class Command:
@@ -11,30 +47,18 @@ class Command:
     commands = {}
 
     @classmethod
-    def parse_args(cls, args):
+    def parse_args(cls, command, args):
         if len(args) != 0:
             raise UserError('Too many arguments.')
         return {}
 
     @classmethod
-    def set_defaults(cls, parser):
-        args, ignored, ignored, defaults = inspect.getargspec(cls.run)
-        if defaults is None:
-            return
-        defaults_dict = dict(zip(args, defaults))
-        option_defaults = dict(
-            (key, value) for key, value in defaults_dict.items()
-            if parser.defaults.get(key, '') is None)
-        parser.set_defaults(**option_defaults)
-
-    @classmethod
-    def run_from_args(cls, cmd_args):
-        parser = cls.get_parser()
-        cls.set_defaults(parser)
+    def run_from_args(cls, command, cmd_args):
+        parser = get_function_parser(command)
         options, args = parser.parse_args(cmd_args)
-        kwargs = cls.parse_args(args)
+        kwargs = cls.parse_args(command, args)
         kwargs.update(options.__dict__)
-        cls.run(**kwargs)
+        command(**kwargs)
 
     @classmethod
     def run_subcommand(cls, argv):
@@ -46,4 +70,4 @@ class Command:
         except KeyError:
             raise UserError('%s invalid.  Valid commands: %s.' %
                             (argv[0], ', '.join(cls.commands.keys())))
-        command.run_from_args(argv[1:])
+        cls.run_from_args(command, argv[1:])
