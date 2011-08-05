2efbbe5450c5a30900053671927822407886110f
William Grant <william.grant@canonical.com>
2011-08-05 21:38:26 +1000

    Fix grant option handling in parse_postgres_acl.

--- database/schema/security.py
+++ database/schema/security.py
@@ -77,6 +77,7 @@ def parse_postgres_acl(acl):
         for perm in perms:
             if perm == '*':
                 parsed_perms[-1] = (parsed_perms[-1][0], True)
+                continue
             parsed_perms.append((POSTGRES_ACL_MAP[perm], False))
         parsed[unquote_identifier(user.encode('utf-8'))] = dict(parsed_perms)
     return parsed
