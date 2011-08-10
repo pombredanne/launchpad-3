d2a6031db35d3baf36912ef743d03396b5682357
Aaron Bentley <aaron@canonical.com>
2011-08-10 11:27:58 -0400

    Fix lint.

--- lib/canonical/launchpad/webapp/publication.py
+++ lib/canonical/launchpad/webapp/publication.py
@@ -153,7 +153,7 @@ def maybe_block_offsite_form_post(request):
         # exception was added as a result of bug 597324 (message #10 in
         # particular).
         return
-    referrer = request.getHeader('referer') # match HTTP spec misspelling
+    referrer = request.getHeader('referer')  # match HTTP spec misspelling
     if not referrer:
         raise NoReferrerError('No value for REFERER header')
     # XXX: jamesh 2007-04-26 bug=98437:
@@ -548,7 +548,7 @@ class LaunchpadBrowserPublication(
         if request.method in ['GET', 'HEAD']:
             self.finishReadOnlyRequest(txn)
         elif txn.isDoomed():
-            txn.abort() # Sends an abort to the database, even though
+            txn.abort()  # Sends an abort to the database, even though
             # transaction is still doomed.
         else:
             txn.commit()
@@ -749,11 +749,11 @@ class LaunchpadBrowserPublication(
             if IBrowserRequest.providedBy(request):
                 OpStats.stats['http requests'] += 1
                 status = request.response.getStatus()
-                if status == 404: # Not Found
+                if status == 404:  # Not Found
                     OpStats.stats['404s'] += 1
-                elif status == 500: # Unhandled exceptions
+                elif status == 500:  # Unhandled exceptions
                     OpStats.stats['500s'] += 1
-                elif status == 503: # Timeouts
+                elif status == 503:  # Timeouts
                     OpStats.stats['503s'] += 1
 
                 # Increment counters for status code groups.
