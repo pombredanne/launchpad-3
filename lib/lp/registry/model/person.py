674b0d72abad9b7d937da557e64ebe60e465e724
Brad Crittenden <bac@canonical.com>
2011-08-16 15:50:41 -0400

    Take into account the possibility of getting None for the sshkey.

--- lib/lp/registry/model/person.py
+++ lib/lp/registry/model/person.py
@@ -4419,7 +4419,7 @@ class SSHKeySet:
     def new(self, person, sshkey):
         try:
             kind, keytext, comment = sshkey.split(' ', 2)
-        except ValueError:
+        except (ValueError, AttributeError):
             raise SSHKeyAdditionError
 
         if not (kind and keytext and comment):
