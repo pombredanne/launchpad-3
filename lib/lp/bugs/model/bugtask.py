c4a9ccdb418888b2842c9f1bc832e3badd0f139f
Steve Kowalik <stevenk@ubuntu.com>
2011-08-17 15:39:51 +1000

    Re-flow SQL in bugtask.

--- lib/lp/bugs/model/bugtask.py
+++ lib/lp/bugs/model/bugtask.py
@@ -2036,9 +2036,9 @@ class BugTaskSet:
                 archive.id
                 for archive in distroseries.distribution.all_distro_archives]
             with_clauses.append("""spns as (
-                SELECT spr.sourcepackagename from
-                SourcePackagePublishingHistory
-                JOIN SourcePackageRelease as spr on spr.id =
+                SELECT spr.sourcepackagename
+                FROM SourcePackagePublishingHistory
+                JOIN SourcePackageRelease AS spr ON spr.id =
                     SourcePackagePublishingHistory.sourcepackagerelease AND
                 SourcePackagePublishingHistory.distroseries = %s AND
                 SourcePackagePublishingHistory.archive IN %s AND
