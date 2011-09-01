5e5296c28eca8d2f5080672651737ac2f9015ce7
Henning Eggers <henning@canonical.com>
2011-09-01 18:00:18 +0200

    Test passing.

--- lib/lp/hardwaredb/scripts/tests/test_hwdbsubmissions.py
+++ lib/lp/hardwaredb/scripts/tests/test_hwdbsubmissions.py
@@ -5,7 +5,9 @@
 
 __metaclass__ = type
 
-from canonical.testing.layers import ZopelessDatabaseLayer
+
+from canonical.testing.layers import LaunchpadScriptLayer
+from lp.hardwaredb.interfaces.hwdb import HWSubmissionProcessingStatus
 from lp.hardwaredb.scripts.hwdbsubmissions import (
     ProcessingLoopForPendingSubmissions,
     ProcessingLoopForReprocessingBadSubmissions,
@@ -14,5 +16,17 @@ from lp.testing import TestCaseWithFactory
 
 
 class TestProcessingLoops(TestCaseWithFactory):
+    layer = LaunchpadScriptLayer
 
-    layer = ZopelessDatabaseLayer
+    def _makePendingSubmissionsLoop(self):
+        """Parameters don't matter for these tests."""
+        return ProcessingLoopForPendingSubmissions(None, None, 0, False)
+
+    def test_PendingSubmissions_submitted_found(self):
+        # The PendingSubmissions loop finds submitted entries.
+        submission = self.factory.makeHWSubmission(
+            status=HWSubmissionProcessingStatus.SUBMITTED)
+        loop = self._makePendingSubmissionsLoop()
+        # The sample data already contains one
+        submissions = loop.getUnprocessedSubmissions(2)
+        self.assertEqual([submission], submissions[1:])
