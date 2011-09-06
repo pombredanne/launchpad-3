5ea634945677a03deab42a6c7ab703064a6d5193
Martin Pool <mbp@canonical.com>
2011-09-06 12:06:13 +1000

    Accept as trusted mail signed by the Sender domain not the From address (bug 643223).
    This should allow people who use eg gmail (which signs from the Sender) but
    with their own domain in the From address to use Launchpad, as long as they
    have validated the Sender address.
    
    This also adds a process-one-mail.py script that reads a mail from stdin 
    and can more easily be used to locally test incoming mail code.

---
+++ scripts/process-one-mail.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python -S
+#
+# Copyright 2011 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Process one email message, read from stdin."""
+
+import _pythonpath
+
+import sys
+
+from zope.component.interfaces import ComponentLookupError
+
+from canonical.config import config
+from lp.services.scripts.base import (
+    LaunchpadScript, LaunchpadScriptFailure)
+from lp.services.mail.incoming import (
+    handle_one_mail)
+from canonical.launchpad.interfaces.mailbox import IMailBox
+from lp.services.mail.helpers import (
+    save_mail_to_librarian,
+    )
+from lp.services.mail.signedmessage import signed_message_from_string    
+
+class ProcessMail(LaunchpadScript):
+    usage = """%prog [options]
+
+    Process one incoming email, read from stdin.
+
+    """ + __doc__
+
+    def main(self):
+        self.txn.begin()
+        # NB: This somewhat duplicates handleMail, but there it's mixed in
+        # with handling a mailbox, which we're avoiding here.
+        self.logger.debug("reading message from stdin")
+        raw_mail = sys.stdin.read()
+        self.logger.debug("got %d bytes" % len(raw_mail))
+        file_alias = save_mail_to_librarian(raw_mail)
+        self.logger.debug("saved to librarian as %r" % (file_alias,))
+        parsed_mail = signed_message_from_string(raw_mail)
+        handle_one_mail(
+            self.logger, parsed_mail,
+            file_alias, file_alias.http_url,
+            signature_timestamp_checker=None)
+        self.logger.debug("mail handling complete")
+        self.txn.commit()
+
+
+if __name__ == '__main__':
+    script = ProcessMail('process-one-mail', dbuser=config.processmail.dbuser)
+    # No need to lock; you can run as many as you want as they use no global
+    # resources (like a mailbox).
+    script.run(use_web_security=True)
