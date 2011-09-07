6b11fcd46339e2ae49ac371ed575a3e3238448d7
Jeroen Vermeulen <jeroen.vermeulen@canonical.com>
2011-09-07 14:17:35 +0700

    Test gina addition.

--- lib/lp/soyuz/scripts/tests/test_gina.py
+++ lib/lp/soyuz/scripts/tests/test_gina.py
@@ -1,13 +1,41 @@
-# Copyright 2009-2010 Canonical Ltd.  This software is licensed under the
+# Copyright 2009-2011 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
 from doctest import DocTestSuite
-import unittest
+from unittest import TestLoader
 
+from canonical.testing.layers import ZopelessDatabaseLayer
+from lp.services.log.logger import DevNullLogger
+from lp.soyuz.enums import PackagePublishingStatus
 import lp.soyuz.scripts.gina.handlers
+from lp.soyuz.scripts.gina.retire import dominate_imported_source_packages
+from lp.testing import TestCaseWithFactory
+
+
+class TestGina(TestCaseWithFactory):
+
+    layer = ZopelessDatabaseLayer
+
+    def test_dominate_imported_source_packages(self):
+
+        class SimpleFakePackagesMap:
+            def __init__(self, src_map):
+                self.src_map = src_map
+
+        logger = DevNullLogger()
+        pub = self.factory.makeSourcePackagePublishingHistory(
+            status=PackagePublishingStatus.PUBLISHED)
+        series = pub.distroseries
+        spr = pub.sourcepackagerelease
+        package = spr.sourcepackagename
+        packages_map = SimpleFakePackagesMap({package.name: []})
+        dominate_imported_source_packages(
+            logger, series.distribution.name, series.name, pub.pocket,
+            packages_map)
+        self.assertEqual(PackagePublishingStatus.DELETED, pub.status)
 
 
 def test_suite():
-    suite = unittest.TestSuite()
+    suite = TestLoader().loadTestsFromName(__name__)
     suite.addTest(DocTestSuite(lp.soyuz.scripts.gina.handlers))
     return suite
