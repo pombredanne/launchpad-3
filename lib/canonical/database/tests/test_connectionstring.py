3fb996f395d3a5acbc298eb474703887b4c18bc0
William Grant <william.grant@canonical.com>
2011-09-07 00:24:46 +0000

    ConnectionString now assumes values continue until a space character, and rejects values that seem to be quoted or escaped. Also now has better tests.

---
+++ lib/canonical/database/tests/test_connectionstring.py
@@ -0,0 +1,39 @@
+# Copyright 2009 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+from canonical.database.postgresql import ConnectionString
+from lp.testing import TestCase
+
+
+class TestConnectionString(TestCase):
+
+    def test_relevant_fields_parsed(self):
+        s = ('dbname=dbname user=user host=host port=port '
+             'connect_timeout=timeout sslmode=mode')
+        cs = ConnectionString(s)
+        self.assertEqual('dbname', cs.dbname)
+        self.assertEqual('user', cs.user)
+        self.assertEqual('host', cs.host)
+        self.assertEqual('port', cs.port)
+        self.assertEqual('timeout', cs.connect_timeout)
+        self.assertEqual('mode', cs.sslmode)
+
+        # and check that str/repr have the same keys and values.
+        self.assertEqual(set(s.split()), set(str(cs).split()))
+        self.assertEqual(set(s.split()), set(repr(cs).split()))
+
+    def test_hyphens_in_values(self):
+        cs = ConnectionString('user=foo-bar host=foo.bar-baz.quux')
+        self.assertEqual('foo-bar', cs.user)
+        self.assertEqual('foo.bar-baz.quux', cs.host)
+
+    def test_str_with_changes(self):
+        initial = 'dbname=foo host=bar'
+        expected = 'dbname=foo user=baz host=bar'
+        cs = ConnectionString(initial)
+        cs.user = 'baz'
+        self.assertEqual(expected, str(cs))
+
+    def test_rejects_quoted_strings(self):
+        self.assertRaises(
+            AssertionError, ConnectionString, "dbname='quoted string'")
