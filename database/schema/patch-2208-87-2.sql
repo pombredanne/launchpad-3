b8b17e6bb6c26b486bfa788002befadbc4757c8c
Steve Kowalik <stevenk@ubuntu.com>
2011-09-13 20:22:15 +1000

    87-1 -> 87-2

--- database/schema/patch-2208-87-2.sql
+++ database/schema/patch-2208-87-2.sql
@@ -5,5 +5,5 @@ SET client_min_messages=ERROR;
 CREATE INDEX binarypackagepublishinghistory__binarypackagename__idx ON BinaryPackagePublishingHistory(binarypackagename);
 CREATE INDEX sourcepackagepublishinghistory__sourcepackagename__idx ON SourcePackagePublishingHistory(sourcepackagename);
 
-INSERT INTO LaunchpadDatabaseRevision VALUES (2208, 87, 1);
+INSERT INTO LaunchpadDatabaseRevision VALUES (2208, 87, 2);
 
