867b6ee9b2c3fb99a51e223a817cfc788c82fe4e
William Grant <william.grant@canonical.com>
2011-09-13 12:07:19 +0000

    Remove general ZTM tests from canonical.lp. initZopeless' guts live in canonical.database.sqlbase, but that now just delegates to Storm so these can die.

--- lib/canonical/lp/ftests/test_zopeless.py
+++ lib/canonical/lp/ftests/test_zopeless.py
@@ -6,29 +6,12 @@ Tests to make sure that initZopeless works as expected.
 """
 
 from doctest import DocTestSuite
-from threading import Thread
 import unittest
 import warnings
 
-import psycopg2
-from sqlobject import StringCol, IntCol
-
-from canonical.database.sqlbase import (
-    alreadyInstalledMsg,
-    connect,
-    cursor,
-    SQLBase,
-    )
+from canonical.database.sqlbase import alreadyInstalledMsg
 from canonical.lp import initZopeless
 from canonical.testing.layers import LaunchpadScriptLayer
-
-
-class MoreBeer(SQLBase):
-    '''Simple SQLObject class used for testing'''
-    # test_sqlos defines a Beer SQLObject already, so we call this one
-    # MoreBeer to avoid confusing SQLObject.
-    name = StringCol(alternateID=True, notNull=True)
-    rating = IntCol(default=None)
 
 
 class TestInitZopeless(unittest.TestCase):
@@ -67,137 +50,6 @@ class TestInitZopeless(unittest.TestCase):
         self.warned = True
 
 
-class TestZopeless(unittest.TestCase):
-
-    layer = LaunchpadScriptLayer
-
-    def setUp(self):
-        self.tm = initZopeless(dbuser='launchpad')
-
-        c = cursor()
-        c.execute("CREATE TABLE morebeer ("
-                  "  id SERIAL PRIMARY KEY,"
-                  "  name text NOT NULL UNIQUE,"
-                  "  rating integer"
-                  ")")
-        self.tm.commit()
-
-    def tearDown(self):
-        self.tm.uninstall()
-
-    def test_simple(self):
-        # Create a few MoreBeers and make sure we can access them
-        b = MoreBeer(name='Victoria Bitter')
-        id1 = b.id
-        b = MoreBeer(name='XXXX')
-        id2 = b.id
-
-        b = MoreBeer.get(id1)
-        b.rating = 3
-        b = MoreBeer.get(id2)
-        b.rating = 2
-
-        b = MoreBeer.get(id1)
-        self.failUnlessEqual(b.rating, 3)
-
-    def test_multipleTransactions(self):
-        # Here we create a MoreBeer and make modifications in a number
-        # of different transactions
-
-        b = MoreBeer(name='Victoria Bitter')
-        id = b.id
-        self.tm.commit()
-
-        b = MoreBeer.get(id)
-        self.failUnlessEqual(b.name, 'Victoria Bitter')
-        b.rating = 4
-        self.tm.commit()
-
-        b = MoreBeer.get(id)
-        self.failUnlessEqual(b.rating, 4)
-        b.rating = 5
-        self.tm.commit()
-
-        b = MoreBeer.get(id)
-        self.failUnlessEqual(b.rating, 5)
-        b.rating = 2
-        self.tm.abort()
-
-        b = MoreBeer.get(id)
-        self.failUnlessEqual(b.rating, 5)
-        b.rating = 4
-        self.tm.commit()
-
-        b = MoreBeer.get(id)
-        self.failUnlessEqual(b.rating, 4)
-
-    def test_threads(self):
-        # Here we create a number of MoreBeers in seperate threads
-        def doit():
-            self.tm.begin()
-            b = MoreBeer(name=beer_name)
-            b.rating = beer_rating
-            self.tm.commit()
-
-        beer_name = 'Victoria Bitter'
-        beer_rating = 4
-        t = Thread(target=doit)
-        t.start()
-        t.join()
-
-        beer_name = 'Singa'
-        beer_rating = 6
-        t = Thread(target=doit)
-        t.start()
-        t.join()
-
-        # And make sure they are both seen
-        beers = MoreBeer.select()
-        self.failUnlessEqual(beers.count(), 2)
-        self.tm.commit()
-
-    def test_exception(self):
-
-        # We have observed if a database transaction ends badly, it is
-        # not reset for future transactions. To test this, we cause
-        # a database exception
-        beer1 = MoreBeer(name='Victoria Bitter')
-        beer1.syncUpdate()
-        try:
-            beer2 = MoreBeer(name='Victoria Bitter')
-            beer2.syncUpdate()
-        except psycopg2.DatabaseError:
-            pass
-        else:
-            self.fail('Unique constraint was not triggered')
-        self.tm.abort()
-
-        # Now start a new transaction and see if we can do anything
-        self.tm.begin()
-        beer3 = MoreBeer(name='Singa')
-        beer3.syncUpdate()
-
-    def test_externalChange(self):
-        # Make a change
-        MoreBeer(name='Victoria Bitter')
-
-        # Commit our local change
-        self.tm.commit()
-
-        # Make another change from a non-SQLObject connection, and commit that
-        conn = connect()
-        cur = conn.cursor()
-        cur.execute("BEGIN TRANSACTION;")
-        cur.execute("UPDATE MoreBeer SET rating=4 "
-                    "WHERE name='Victoria Bitter';")
-        cur.execute("COMMIT TRANSACTION;")
-        cur.close()
-        conn.close()
-
-        # We should now be able to see the external change in our connection
-        self.failUnlessEqual(4, MoreBeer.byName('Victoria Bitter').rating)
-
-
 def test_isZopeless():
     """
     >>> from canonical.lp import isZopeless
@@ -218,7 +70,6 @@ def test_isZopeless():
 
 def test_suite():
     suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(TestZopeless))
     suite.addTest(unittest.makeSuite(TestInitZopeless))
     doctests = DocTestSuite()
     doctests.layer = LaunchpadScriptLayer
