d5fffa23212fcde929ca3e577a0b9632b550c8b6
Gavin Panella <gavin.panella@canonical.com>
2011-09-16 14:42:48 +0100

    Clean-up test_config a bit.

--- lib/canonical/config/tests/test_config.py
+++ lib/canonical/config/tests/test_config.py
@@ -38,8 +38,11 @@ lazr_schema_file = os.path.join(here, 'schema-lazr.conf')
 def make_test(config_file, description):
     def test_function():
         root, handlers = ZConfig.loadConfig(schema, config_file)
+    # Hack the config file name into test_function's __name__ so that the test
+    # -vv output is more informative. Unfortunately, FunctionTestCase's
+    # description argument doesn't do what we want.
     test_function.__name__ = description
-    return test_function
+    return unittest.FunctionTestCase(test_function)
 
 
 def make_config_test(config_file, description):
@@ -93,33 +96,27 @@ def test_suite():
         'canonical.config',
         optionflags=NORMALIZE_WHITESPACE | ELLIPSIS,
         ))
+    load_testcase = unittest.defaultTestLoader.loadTestsFromTestCase
     # Add a test for every launchpad[.lazr].conf file in our tree.
     for config_dir in canonical.config.CONFIG_ROOT_DIRS:
-        prefix_len = len(os.path.dirname(config_dir)) + 1
         for dirpath, dirnames, filenames in os.walk(config_dir):
             if os.path.basename(dirpath) in EXCLUDED_CONFIGS:
+                del dirnames[:]  # Don't look in subdirectories.
                 continue
             for filename in filenames:
                 if filename == 'launchpad.conf':
                     config_file = os.path.join(dirpath, filename)
-                    description = config_file[prefix_len:]
-                    # Hack the config file name into the test_function's
-                    # __name__ # so that the test -vv output is more
-                    # informative. Unfortunately, FunctionTestCase's
-                    # description argument doesn't do what we want.
-                    suite.addTest(unittest.FunctionTestCase(
-                        make_test(config_file, description)))
+                    description = os.path.relpath(config_file, config_dir)
+                    suite.addTest(make_test(config_file, description))
                 elif filename.endswith('-lazr.conf'):
                     # Test the lazr.config conf files.
                     config_file = os.path.join(dirpath, filename)
-                    description = config_file[prefix_len:]
-                    test = make_config_test(config_file, description)
-                    suite.addTest(test('testConfig'))
+                    description = os.path.relpath(config_file, config_dir)
+                    testcase = make_config_test(config_file, description)
+                    suite.addTest(load_testcase(testcase))
                 else:
                     # This file is not a config that can be validated.
                     pass
     # Other tests.
-    suite.addTest(
-        unittest.defaultTestLoader.loadTestsFromTestCase(
-            TestCanonicalConfig))
+    suite.addTest(load_testcase(TestCanonicalConfig))
     return suite
