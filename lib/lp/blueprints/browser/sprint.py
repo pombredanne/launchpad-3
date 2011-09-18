c63e24990befbedc66aca1c52f67086cc83994a7
Curtis Hovey <curtis.hovey@canonical.com>
2011-09-18 14:51:51 -0400

    Hushed lint.

--- lib/lp/blueprints/browser/sprint.py
+++ lib/lp/blueprints/browser/sprint.py
@@ -230,6 +230,7 @@ class SprintView(HasSpecificationsView):
         return dt.strftime('%Y-%m-%d')
 
     _local_timeformat = '%H:%M %Z on %A, %Y-%m-%d'
+
     @property
     def local_start(self):
         """The sprint start time, in the local time zone, as text."""
@@ -381,7 +382,6 @@ class SprintTopicSetView(HasSpecificationsView, LaunchpadView):
         self.attendee_ids = set(
             attendance.attendeeID for attendance in self.context.attendances)
 
-
     @cachedproperty
     def spec_filter(self):
         """Return the specification links with PROPOSED status for this
@@ -405,7 +405,7 @@ class SprintTopicSetView(HasSpecificationsView, LaunchpadView):
         if 'SUBMIT_CANCEL' in form:
             self.status_message = 'Cancelled'
             self.request.response.redirect(
-                canonical_url(self.context)+'/+specs')
+                canonical_url(self.context) + '/+specs')
             return
 
         if 'SUBMIT_ACCEPT' not in form and 'SUBMIT_DECLINE' not in form:
@@ -444,7 +444,7 @@ class SprintTopicSetView(HasSpecificationsView, LaunchpadView):
         if leftover == 0:
             # they are all done, so redirect back to the spec listing page
             self.request.response.redirect(
-                canonical_url(self.context)+'/+specs')
+                canonical_url(self.context) + '/+specs')
 
 
 class SprintMeetingExportView(LaunchpadView):
@@ -482,9 +482,9 @@ class SprintMeetingExportView(LaunchpadView):
                 model_specs, ['specificationID']):
             if subscription.personID not in attendee_set:
                 continue
-            people[subscription.specificationID][subscription.personID] = \
-                subscription.essential
-        # Spec specials - drafter/assignee. Don't need approver for performance
+            people[subscription.specificationID][
+                subscription.personID] = subscription.essential
+        # Specials - drafter/assignee. Don't need approver for performance
         # as specifications() above eager loaded the people, and approvers
         # don't count as a 'required person'.
         for spec in model_specs:
@@ -588,10 +588,10 @@ class SprintAttendeesCsvExportView(LaunchpadView):
                  # We used to store phone, organization, city and
                  # country, but this was a lie because users could not
                  # update these fields.
-                 '', # attendance.attendee.phone
-                 '', # attendance.attendee.organization
-                 '', # attendance.attendee.city
-                 '', # country
+                 '',  # attendance.attendee.phone
+                 '',  # attendance.attendee.organization
+                 '',  # attendance.attendee.city
+                 '',  # country
                  time_zone,
                  attendance.time_starts.strftime('%Y-%m-%dT%H:%M:%SZ'),
                  attendance.time_ends.strftime('%Y-%m-%dT%H:%M:%SZ'),
