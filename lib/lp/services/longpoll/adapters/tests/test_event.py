0291b68f933d3d910cd157b33aa7712c5da54e88
Gavin Panella <gavin.panella@canonical.com>
2011-09-19 16:17:32 +0100

    The basic parts of the zope.event -> longpoll event bridge.

--- lib/lp/services/longpoll/adapters/tests/test_event.py
+++ lib/lp/services/longpoll/adapters/tests/test_event.py
@@ -5,6 +5,10 @@
 
 __metaclass__ = type
 
+from lazr.lifecycle.event import ObjectCreatedEvent
+from zope.component import adapts
+from zope.component.interfaces import IObjectEvent
+from zope.event import notify
 from zope.interface import implements
 
 from canonical.testing.layers import (
@@ -16,8 +20,13 @@ from lp.services.longpoll.adapters.event import (
     LongPollEvent,
     )
 from lp.services.longpoll.interfaces import ILongPollEvent
+from lp.services.longpoll.testing import (
+    capture_longpoll_emissions,
+    LongPollEventRecord,
+    )
 from lp.services.messaging.queue import RabbitMessageBase
 from lp.testing import TestCase
+from lp.testing.fixture import ZopeAdapterFixture
 from lp.testing.matchers import Contains
 
 
@@ -28,6 +37,19 @@ class FakeEvent(LongPollEvent):
     @property
     def event_key(self):
         return "event-key-%s-%s" % (self.source, self.event)
+
+
+class FakeObjectEvent(LongPollEvent):
+
+    adapts(basestring, IObjectEvent)
+    implements(ILongPollEvent)
+
+    @property
+    def event_key(self):
+        return "event-key-%s-created" % self.source
+
+    def emit(self, data):
+        super(FakeObjectEvent, self).emit(data.object)
 
 
 class TestLongPollEvent(TestCase):
@@ -57,6 +79,32 @@ class TestLongPollEvent(TestCase):
             message for (call, message) in
             RabbitMessageBase.class_locals.messages]
         self.assertThat(pending_messages, Contains(expected_message))
+
+
+class TestObjectEventBridge(TestCase):
+
+    layer = LaunchpadFunctionalLayer
+
+    def test_object_event_no_adapter(self):
+        # When no adapter is registered for the given event, no longpoll
+        # emissions are recorded.
+        with capture_longpoll_emissions() as log:
+            notify(ObjectCreatedEvent("bob"))
+        self.assertEqual([], log)
+
+    def test_object_event(self):
+        # When an adapter is registered for the given event, a single longpoll
+        # emission is recorded.
+        self.useFixture(ZopeAdapterFixture(FakeObjectEvent))
+        with capture_longpoll_emissions() as log:
+            notify(ObjectCreatedEvent("bob"))
+        expected = [
+            LongPollEventRecord(
+                "event-key-bob-created",
+                {"event_key": "event-key-bob-created",
+                 "event_data": "bob"}),
+            ]
+        self.assertEqual(expected, log)
 
 
 class TestFunctions(TestCase):
