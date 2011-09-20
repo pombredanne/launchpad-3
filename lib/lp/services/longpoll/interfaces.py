5cc9007092f5e51bf3204755114def119165f695
Gavin Panella <gavin.panella@canonical.com>
2011-09-20 20:04:19 +0100

    Update long_poll_event() to only take source_spec.

--- lib/lp/services/longpoll/interfaces.py
+++ lib/lp/services/longpoll/interfaces.py
@@ -47,19 +47,14 @@ class ILongPollSubscriber(Interface):
         """
 
 
-def long_poll_event(source_spec, event_spec=basestring):
+def long_poll_event(source_spec):
     """Class decorator to declare an `ILongPollEvent`.
 
     :param source_spec: An interface or other specification understood by
         `zope.component` (a plain class can be passed too) that defines the
         source of an event. `IJob` or `storm.base.Storm` for example.
-    :param source_event: An interface or other specification understood by
-        `zope.component`. The exact use here is left to implementers. By
-        default it is `basestring` so that terms like "modified" or
-        "lifecycle" can be used when looking up the event, but it could also
-        be `IObjectModifiedEvent`. The dominant use case is evolving.
     """
-    declare_adapter = adapter(source_spec, event_spec)
+    declare_adapter = adapter(source_spec)
 
     def declare_event(cls):
         classImplements(cls, ILongPollEvent)
