98619dcd6dcbcb1b7cd80c542cc7b1f95f2eaa78
Curtis Hovey <curtis.hovey@canonical.com>
2011-09-22 15:07:48 -0400

    Adapt to a distribution and do not accept a non-distro.

--- lib/lp/bugs/tests/test_vocabulary.py
+++ lib/lp/bugs/tests/test_vocabulary.py
@@ -18,11 +18,20 @@ class UsesBugsDistributionVocabularyTestCase(TestCaseWithFactory):
     layer = DatabaseFunctionalLayer
 
     def test_init_with_distribution(self):
-        # When the context is adaptable to IDistribution, it also provides
-        # the distribution.
+        # When the context is adaptable to IDistribution, the distribution
+        # property is the distribution.
         distribution = self.factory.makeDistribution()
         vocabulary = UsesBugsDistributionVocabulary(distribution)
         self.assertEqual(distribution, vocabulary.context)
+        self.assertEqual(distribution, vocabulary.distribution)
+
+    def test_init_without_distribution(self):
+        # When the context is not adaptable to IDistribution, the
+        # distribution property is None
+        thing = self.factory.makeProduct()
+        vocabulary = UsesBugsDistributionVocabulary(thing)
+        self.assertEqual(thing, vocabulary.context)
+        self.assertEqual(None, vocabulary.distribution)
 
     def test_contains_distros_that_use_bugs(self):
         # The vocabulary contains distributions that also use
@@ -49,3 +58,13 @@ class UsesBugsDistributionVocabularyTestCase(TestCaseWithFactory):
         self.assertTrue(
             distro_less_bugs in vocabulary,
             "Vocabulary missing context distro.")
+
+    def test_contains_missing_context(self):
+        # The vocabulary contains the context distro even it it does not
+        # use Launchpad to track bugs. The distro may have tracked bugs
+        # in the past so it is a legitimate choise for historic data.
+        thing = self.factory.makeProduct()
+        vocabulary = UsesBugsDistributionVocabulary(thing)
+        self.assertFalse(
+            thing in vocabulary,
+            "Vocabulary contains a non-distribution.")
