ef1227b20e69eeb349f88669421f4ebddf16e946
Raphael Badin <raphael.badin@canonical.com>
2011-09-29 16:50:32 +0200

    Add comment and link about queue leases.

--- lib/lp/services/messaging/rabbit.py
+++ lib/lp/services/messaging/rabbit.py
@@ -232,6 +232,9 @@ class RabbitQueue(RabbitMessageBase):
     def __init__(self, session, name):
         super(RabbitQueue, self).__init__(session)
         self.name = name
+        # The queue will be auto-deleted 5 minutes after the last
+        # use of the queue.
+        # http://www.rabbitmq.com/extensions.html#queue-leases
         self.channel.queue_declare(
             self.name, nowait=False, auto_delete=False,
             arguments={"x-expires": 300000})  # 5 minutes.
