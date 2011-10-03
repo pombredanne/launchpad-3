0368e51734fef89554e191b90137cd8f114ea6ef
Raphael Badin <raphael.badin@canonical.com>
2011-10-03 10:11:36 +0200

    Split feature flag testing into 2 different tests to cope with the usage of cachedpropery.

--- lib/lp/registry/browser/tests/test_distroseries.py
+++ lib/lp/registry/browser/tests/test_distroseries.py
@@ -748,15 +748,20 @@ class TestDistroSeriesInitializeView(TestCaseWithFactory):
         view = create_initialized_view(distroseries, "+initseries")
         self.assertTrue(view)
 
-    def test_is_derived_series_feature_enabled(self):
-        # The feature is disabled by default, but can be enabled by setting
-        # the soyuz.derived_series_ui.enabled flag.
+    def test_is_derived_series_feature_disabled(self):
+        # The feature is disabled by default.
         distroseries = self.factory.makeDistroSeries()
         view = create_initialized_view(distroseries, "+initseries")
         with FeatureFixture({}):
             self.assertFalse(view.is_derived_series_feature_enabled)
+
+    def test_is_derived_series_feature_enabled(self):
+        # The feature is disabled by default, but can be enabled by setting
+        # the soyuz.derived_series_ui.enabled flag.
+        distroseries = self.factory.makeDistroSeries()
         flags = {u"soyuz.derived_series_ui.enabled": u"true"}
         with FeatureFixture(flags):
+            view = create_initialized_view(distroseries, "+initseries")
             self.assertTrue(view.is_derived_series_feature_enabled)
 
     def test_form_hidden_when_derived_series_feature_disabled(self):
