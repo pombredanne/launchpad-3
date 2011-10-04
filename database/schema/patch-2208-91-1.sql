f68ac48d132691716e3c001f409a2283e745d658
Ian Booth <ian.booth@canonical.com>
2011-10-04 13:03:57 +1000

    Add trigger to ensure transitively_private is not null on insert

--- database/schema/patch-2208-91-1.sql
+++ database/schema/patch-2208-91-1.sql
@@ -3,6 +3,23 @@
 
 SET client_min_messages=ERROR;
 
+CREATE OR REPLACE FUNCTION initialise_transitively_private() RETURNS TRIGGER
+LANGUAGE plpgsql VOLATILE AS
+$$
+BEGIN
+    NEW.transitively_private := COALESCE(NEW.private, FALSE);
+    RETURN NEW;
+END;
+$$;
+
+COMMENT ON FUNCTION initialise_transitively_private() IS
+    'Trigger ensuring the initial value of the Branch transitively_private column is not null';
+
+CREATE TRIGGER check_branch_transitive_privacy_t
+    BEFORE INSERT ON Branch
+    FOR EACH ROW
+    EXECUTE PROCEDURE initialise_transitively_private();
+
 ALTER TABLE Branch ALTER COLUMN transitively_private SET NOT NULL;
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2208, 91, 1);
