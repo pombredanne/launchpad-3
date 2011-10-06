bfb33b5a1604b73854ee01795b96d0a3f65b3968
Nigel Babu <nigelbabu@gmail.com>
2011-10-06 09:10:25 +0530

    Changed the year

--- database/schema/patch-2208-90-1.sql
+++ database/schema/patch-2208-90-1.sql
@@ -1,4 +1,4 @@
--- Copyright 2010 Canonical Ltd.  This software is licensed under the
+-- Copyright 2011 Canonical Ltd.  This software is licensed under the
 -- GNU Affero General Public License version 3 (see the file LICENSE).
 
 SET client_min_messages=ERROR;
