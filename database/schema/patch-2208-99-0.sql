66d5b56b47966356f4c72106bb5d6fa95c41eecc
William Grant <william.grant@canonical.com>
2011-10-24 04:17:24 +0000

    Add PillarObserver(Policy|Artifact|Permission) tables.

---
+++ database/schema/patch-2208-99-0.sql
@@ -0,0 +1,38 @@
+-- Copyright 2011 Canonical Ltd.  This software is licensed under the
+-- GNU Affero General Public License version 3 (see the file LICENSE).
+SET client_min_messages=ERROR;
+
+CREATE TABLE PillarObserverPolicy (
+    id serial PRIMARY KEY,
+    product integer REFERENCES Product,
+    distribution integer REFERENCES Distribution,
+    display_name text NOT NULL,
+    CONSTRAINT pillarobserverpolicy__product__display_name__key
+        UNIQUE (product, display_name),
+    CONSTRAINT pillarobserverpolicy__distribution__display_name__key
+        UNIQUE (distribution, display_name),
+    CONSTRAINT has_target CHECK (product IS NULL != distribution IS NULL)
+);
+
+CREATE TABLE PillarObserverArtifact (
+    id serial PRIMARY KEY,
+    bug integer REFERENCES Bug,
+    branch integer REFERENCES Branch,
+    CONSTRAINT pillarobserverartifact__bug__key UNIQUE (bug),
+    CONSTRAINT pillarobserverartifact__branch__key UNIQUE (branch),
+    CONSTRAINT has_artifact CHECK (bug IS NULL != branch IS NULL)
+);
+
+CREATE TABLE PillarObserverPermission (
+    id serial PRIMARY KEY,
+    policy integer NOT NULL REFERENCES PillarObserverPolicy,
+    person integer NOT NULL REFERENCES Person,
+    artifact integer REFERENCES PillarObserverArtifact,
+    CONSTRAINT pillarobserverpermission__policy__person__artifact__key
+        UNIQUE (policy, person, artifact)
+);
+
+CREATE UNIQUE INDEX pillarobserverpermission__policy__person__key
+    ON PillarObserverPermission(policy, person) WHERE artifact IS NULL;
+
+INSERT INTO LaunchpadDatabaseRevision VALUES (2208, 99, 0);
