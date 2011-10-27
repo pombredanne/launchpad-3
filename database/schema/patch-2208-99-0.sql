a55195e03ccfdf10a3caf8a01546aae3c5936eec
Benji York <benji.york@canonical.com>
2011-10-27 16:58:49 -0400

    add forgotten patch and remove non-db changes

---
+++ database/schema/patch-2208-99-0.sql
@@ -0,0 +1,13 @@
+SET client_min_messages=ERROR;
+
+/* POFileStatsJob holds scheduled jobs that are to update POFile statistics */
+CREATE TABLE POFileStatsJob (
+    id           SERIAL PRIMARY KEY,
+    job          INTEGER NOT NULL UNIQUE REFERENCES Job(id),
+    pofile       INTEGER NOT NULL UNIQUE REFERENCES POFile(id)
+);
+
+CREATE INDEX pofilestatsjob__job__idx ON Job(id);
+CREATE INDEX pofilestatsjob__pofile__idx ON POFile(id);
+
+INSERT INTO LaunchpadDatabaseRevision VALUES (2208, 99, 0);
