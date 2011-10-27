952b1187b8ac14d3d379e6937030d001598e4748
Benji York <benji.york@canonical.com>
2011-10-27 16:08:18 -0400

    add pofile staticstics update job and related config

---
+++ lib/lp/translations/model/pofilestatsjob.py
@@ -0,0 +1,103 @@
+# Copyright 2011 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Job for merging translations."""
+
+
+__metaclass__ = type
+
+
+__all__ = [
+    'POFileStatsJob',
+    ]
+
+import logging
+from zope.component import getUtility
+
+from storm.locals import (
+    And,
+    Int,
+    Reference,
+    )
+from zope.interface import (
+    classProvides,
+    implements,
+    )
+
+from canonical.launchpad.webapp.interfaces import (
+    DEFAULT_FLAVOR,
+    IStoreSelector,
+    MAIN_STORE,
+    )
+from lp.services.database.stormbase import StormBase
+from lp.services.job.interfaces.job import IRunnableJob
+from lp.services.job.model.job import Job
+from lp.services.job.runner import BaseRunnableJob
+from lp.translations.interfaces.pofilestatsjob import IPOFileStatsJobSource
+from lp.translations.model.pofile import POFile
+
+
+class POFileStatsJob(StormBase, BaseRunnableJob):
+    """The details for a POFile status update job."""
+
+    __storm_table__ = 'POFileStatsJob'
+
+    # Instances of this class are runnable jobs.
+    implements(IRunnableJob)
+
+    # Oddly, BaseRunnableJob inherits from BaseRunnableJobSource so this class
+    # is both the factory for jobs (the "implements", above) and the source
+    # for runnable jobs (not the constructor of the job source, the class
+    # provides the IJobSource interface itself).
+    classProvides(IPOFileStatsJobSource)
+
+    id = Int(primary=True)
+
+    # The Job table contains core job details.
+    job_id = Int('job')
+    job = Reference(job_id, Job.id)
+
+    # This is the POFile which needs its statistics updated.
+    pofile_id = Int('pofile')
+    pofile = Reference(pofile_id, POFile.id)
+
+    def __init__(self, pofile):
+        self.job = Job()
+        self.pofile = pofile
+        super(POFileStatsJob, self).__init__()
+
+    def getOperationDescription(self):
+        """See `IRunnableJob`."""
+        return 'updating POFile statistics'
+
+    def run(self):
+        """See `IRunnableJob`."""
+        logger = logging.getLogger()
+        logger.info('Updating statistics for %s' % self.pofile.title)
+        self.pofile.updateStatistics()
+
+    @staticmethod
+    def iterReady():
+        """See `IJobSource`."""
+        store = getUtility(IStoreSelector).get(MAIN_STORE, DEFAULT_FLAVOR)
+        return store.find((POFileStatsJob),
+            And(POFileStatsJob.job == Job.id,
+                Job.id.is_in(Job.ready_jobs)))
+
+
+def schedule(pofile):
+    """Schedule a job to update a POFile's stats (if not scheduled)."""
+    # If there's already a job for the pofile, don't create a new one.
+    store = getUtility(IStoreSelector).get(MAIN_STORE, DEFAULT_FLAVOR)
+    job = store.find(
+        POFileStatsJob,
+        POFileStatsJob.pofile == pofile,
+        POFileStatsJob.job == Job.id,
+        Job.id.is_in(Job.ready_jobs)
+        ).any()
+
+    if job is None:
+        job = POFileStatsJob(pofile)
+
+    return job
+
---
+++ lib/lp/translations/tests/test_pofilestatsjob.py
@@ -0,0 +1,76 @@
+# Copyright 2011 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Tests for merging translations."""
+
+__metaclass__ = type
+
+
+from canonical.launchpad.webapp.testing import verifyObject
+from canonical.testing.layers import (
+    LaunchpadZopelessLayer,
+    )
+from lp.registry.interfaces.packaging import IPackagingUtil
+from lp.services.job.interfaces.job import (
+    IJobSource,
+    IRunnableJob,
+    )
+from lp.testing import (
+    person_logged_in,
+    TestCaseWithFactory,
+    )
+
+from lp.translations.interfaces.pofilestatsjob import IPOFileStatsJobSource
+from lp.translations.interfaces.side import TranslationSide
+from lp.translations.model import pofilestatsjob
+from lp.translations.model.pofilestatsjob import POFileStatsJob
+
+
+def runable_jobs():
+    """Returns a list of the currently runnable stats update jobs.
+
+    Provides a nicer spelling for tests than the crazy static method on the
+    job class.
+
+    The return value is listified because that's what we want for tests.
+    """
+    return list(POFileStatsJob.iterReady())
+
+
+class TestPOFileStatsJob(TestCaseWithFactory):
+
+    layer = LaunchpadZopelessLayer
+
+    def test_job_interface(self):
+        # Instances of POFileStatsJob are runnable jobs.
+        verifyObject(IRunnableJob, POFileStatsJob(0))
+
+    def test_source_interface(self):
+        # The POFileStatsJob class is a source of POFileStatsJobs.
+        verifyObject(IPOFileStatsJobSource, POFileStatsJob)
+        verifyObject(IJobSource, POFileStatsJob)
+
+    def test_run(self):
+        # Running a job causes the POFile statistics to be updated.
+        singular = self.factory.getUniqueString()
+        pofile = self.factory.makePOFile(side=TranslationSide.UPSTREAM)
+        # Create a message so we have something to have statistics about.
+        self.factory.makePOTMsgSet(pofile.potemplate, singular)
+        # The statistics start at 0.
+        self.assertEqual(pofile.potemplate.messageCount(), 0)
+        job = pofilestatsjob.schedule(pofile.id)
+        # Just scheduling the job doesn't update the statistics.
+        self.assertEqual(pofile.potemplate.messageCount(), 0)
+        job.run()
+        # Now that the job ran, the statistics have been updated.
+        self.assertEqual(pofile.potemplate.messageCount(), 1)
+
+    def test_iterReady(self):
+        # The POFileStatsJob class provides a way to iterate over the jobs
+        # that are ready to run.  Initially, there aren't any.
+        self.assertEqual(len(list(POFileStatsJob.iterReady())), 0)
+        # We need a POFile to update.
+        pofile = self.factory.makePOFile(side=TranslationSide.UPSTREAM)
+        # If we schedule a job, then we'll get it back.
+        job = pofilestatsjob.schedule(pofile.id)
+        self.assertIs(list(POFileStatsJob.iterReady())[0], job)
