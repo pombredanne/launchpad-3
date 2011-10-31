e21f1b402431c00527817542519264d193b7171e
Steve Kowalik <stevenk@ubuntu.com>
2011-10-31 15:46:01 +1100

    Revert now unneeded changes to lp.testing.views.

--- lib/lp/testing/views.py
+++ lib/lp/testing/views.py
@@ -1,4 +1,4 @@
-# Copyright 2009-2011 Canonical Ltd.  This software is licensed under the
+# Copyright 2009 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
 """Functions to help with the testing of views."""
@@ -24,10 +24,7 @@ from canonical.launchpad.webapp.interfaces import (
     ICanonicalUrlData,
     IPlacelessAuthUtility,
     )
-from canonical.launchpad.webapp.publisher import (
-    canonical_url,
-    layer_for_rootsite,
-    )
+from canonical.launchpad.webapp.publisher import layer_for_rootsite
 from canonical.launchpad.webapp.servers import LaunchpadTestRequest
 
 
