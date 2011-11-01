e47a9095adb74fd1196d46f491bae2443444a143
Gavin Panella <gavin.panella@canonical.com>
2011-11-01 21:16:33 +0000

    Make check_teamparticipation_consistency super efficient.

--- lib/lp/registry/scripts/teamparticipation.py
+++ lib/lp/registry/scripts/teamparticipation.py
@@ -8,13 +8,20 @@ __all__ = [
     "check_teamparticipation",
     ]
 
-from itertools import islice
+from collections import (
+    defaultdict,
+    namedtuple,
+    )
+from itertools import (
+    chain,
+    imap,
+    islice,
+    )
 
 import transaction
-from zope.component import getUtility
 
 from canonical.database.sqlbase import cursor
-from lp.registry.interfaces.person import IPersonSet
+from lp.registry.interfaces.teammembership import ACTIVE_STATES
 from lp.services.scripts.base import LaunchpadScriptFailure
 
 
@@ -58,39 +65,69 @@ def check_teamparticipation_circular(log):
 
 def check_teamparticipation_consistency(log):
     # Check if there are any missing/spurious TeamParticipation entries.
+
     cur = cursor()
-    cur.execute("SELECT id FROM Person WHERE teamowner IS NOT NULL")
-    team_ids = [row[0] for row in cur.fetchall()]
+
+    # Slurp everything in.
+    people = dict(
+        cur.execute(
+            "SELECT id, name FROM Person"
+            " WHERE teamowner IS NULL"
+            "   AND merged IS NULL"))
+    teams = dict(
+        cur.execute(
+            "SELECT id, name FROM Person"
+            " WHERE teamowner IS NOT NULL"
+            "   AND merged IS NULL"))
+
+    team_memberships = defaultdict(set)
+    results = cur.execute(
+        "SELECT team, person FROM TeamMembership"
+        " WHERE status in %s", (ACTIVE_STATES,))
+    for (team, person) in results:
+        team_memberships[team].add(person)
+
+    team_participations = defaultdict(set)
+    results = cur.execute(
+        "SELECT team, person FROM TeamParticipation")
+    for (team, person) in results:
+        team_participations[team].add(person)
+
+    # Don't hold any locks.
+    cur.close()
     transaction.abort()
 
     def get_participants(team):
-        """Recurse through the team's members to get all its participants."""
-        participants = set()
-        for member in team.activemembers:
-            participants.add(member)
-            if member.is_team:
-                participants.update(get_participants(member))
-        return participants
+        """Recurse through membership records to get participants."""
+        member_people = team_memberships[team].intersection(people)
+        member_people.add(team)  # Teams are always members of themselves.
+        member_teams = team_memberships[team].intersection(teams)
+        return member_people.union(
+            chain.from_iterable(imap(get_participants, member_teams)))
 
-    load_teams = getUtility(IPersonSet).getPrecachedPersonsFromIDs
+    errors = []
+    error_rec = namedtuple("error_rec", ("type", "team", "people"))
 
-    for batch in chunked(team_ids):
-        for team in load_teams(batch):
-            expected = get_participants(team)
-            found = set(team.allmembers)
-            difference = expected.difference(found)
-            if len(difference) > 0:
-                people = ", ".join("%s (%s)" % (person.name, person.id)
-                                   for person in difference)
-                log.warn("%s (%s): missing TeamParticipation entries for %s."
-                         % (team.name, team.id, people))
-            reverse_difference = found.difference(expected)
-            if len(reverse_difference) > 0:
-                people = ", ".join("%s (%s)" % (person.name, person.id)
-                                   for person in reverse_difference)
-                log.warn("%s (%s): spurious TeamParticipation entries for %s."
-                         % (team.name, team.id, people))
-            transaction.abort()
+    for team in teams:
+        participants_observed = team_participations[team]
+        participants_expected = get_participants(team)
+        participants_spurious = participants_expected - participants_observed
+        participants_missing = participants_observed - participants_expected
+        if len(participants_spurious) > 0:
+            errors.append(error_rec("spurious", team, participants_spurious))
+        if len(participants_missing) > 0:
+            errors.append(error_rec("missing", team, participants_missing))
+
+    def get_repr(id):
+        return "%s (%d)" % (people[id] if id in people else teams[id], id)
+
+    for error in errors:
+        people_repr = ", ".join(imap(get_repr, error.people))
+        log.warn(
+            "%s: %s TeamParticipation entries for %s.",
+            get_repr(error.team), error.type, people_repr)
+
+    return errors
 
 
 def check_teamparticipation(log):
