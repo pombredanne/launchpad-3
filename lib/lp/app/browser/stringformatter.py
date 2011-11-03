ebbfc01b470e0d582e01bf417b3d2be8029e9acc
Robert Collins <robertc@robertcollins.net>
2011-11-03 02:46:53 +0000

    Change the OOP urlification regex to be simpler and accept whatever single opaque string a system has generated.

--- lib/lp/app/browser/stringformatter.py
+++ lib/lp/app/browser/stringformatter.py
@@ -395,7 +395,7 @@ class FormattersAPI:
                 return text
 
             root_url = config.launchpad.oops_root_url
-            url = root_url + match.group('oopscode')
+            url = root_url + "OOPS-" + match.group('oopscode')
             return '<a href="%s">%s</a>' % (url, text)
         elif match.group('lpbranchurl') is not None:
             lp_url = match.group('lpbranchurl')
@@ -561,7 +561,7 @@ class FormattersAPI:
       ) |
       (?P<oops>
         \boops\s*-?\s*
-        (?P<oopscode> \d* [a-z]+ \d+)
+        (?P<oopscode> \w+)
       ) |
       (?P<lpbranchurl>
         \blp:(?:///|/)?
