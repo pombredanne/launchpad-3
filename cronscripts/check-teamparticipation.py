83234d180ec77181a1d5220e06c08ddc8309fdbd
Gavin Panella <gavin.panella@canonical.com>
2011-11-04 11:07:42 +0000

    Update copyright.

--- cronscripts/check-teamparticipation.py
+++ cronscripts/check-teamparticipation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python -S
 #
-# Copyright 2009 Canonical Ltd.  This software is licensed under the
+# Copyright 2009-2011 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
 # pylint: disable-msg=C0103,W0403
