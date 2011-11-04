3eff2740a026dbd4c3d1f5c7524f4a4b00782c0d
Benji York <benji.york@canonical.com>
2011-11-04 09:35:04 -0400

    remove some unnecessary DB bits per Stuart

--- database/schema/patch-2208-93-0.sql
+++ database/schema/patch-2208-93-0.sql
@@ -2,12 +2,8 @@ SET client_min_messages=ERROR;
 
 /* POFileStatsJob holds scheduled jobs that are to update POFile statistics */
 CREATE TABLE POFileStatsJob (
-    id           SERIAL PRIMARY KEY,
-    job          INTEGER NOT NULL UNIQUE REFERENCES Job(id),
+    job          INTEGER NOT NULL UNIQUE REFERENCES Job(id) PRIMARY KEY,
     pofile       INTEGER NOT NULL UNIQUE REFERENCES POFile(id)
 );
-
-CREATE INDEX pofilestatsjob__job__idx ON Job(id);
-CREATE INDEX pofilestatsjob__pofile__idx ON POFile(id);
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2208, 93, 0);
