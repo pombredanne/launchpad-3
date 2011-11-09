1f1fc5ba93757cf71cf8b9954dbe577e868a83f6
Benji York <benji.york@canonical.com>
2011-11-09 16:24:46 -0500

    the pofile column isn't unique

--- database/schema/patch-2208-93-0.sql
+++ database/schema/patch-2208-93-0.sql
@@ -3,7 +3,7 @@ SET client_min_messages=ERROR;
 /* POFileStatsJob holds scheduled jobs that are to update POFile statistics */
 CREATE TABLE POFileStatsJob (
     job          INTEGER NOT NULL UNIQUE REFERENCES Job(id) PRIMARY KEY,
-    pofile       INTEGER NOT NULL UNIQUE REFERENCES POFile(id)
+    pofile       INTEGER NOT NULL REFERENCES POFile(id)
 );
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2208, 93, 0);
