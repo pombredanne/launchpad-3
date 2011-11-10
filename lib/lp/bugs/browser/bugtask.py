f258472cdee9b1cfb00be58e9ac65f004f25df76
Abel Deuring <abel.deuring@canonical.com>
2011-11-10 11:32:25 +0100

    Infrastructure to show a banner about enabled beta features on a page; property BugTaskSearchListingView.beta_features

--- lib/lp/bugs/browser/bugtask.py
+++ lib/lp/bugs/browser/bugtask.py
@@ -2423,6 +2423,8 @@ class BugTaskSearchListingView(LaunchpadFormView, FeedsMixin, BugsInfoMixin):
 
     implements(IBugTaskSearchListingMenu)
 
+    beta_features = ['bugs.dynamic_bug_listings.enabled']
+
     # Only include <link> tags for bug feeds when using this view.
     feed_types = (
         BugTargetLatestBugsFeedLink,
