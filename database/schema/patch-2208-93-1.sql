1d2fd3260cfc8f9c00b13d3cbc14cc367174e286
William Grant <william.grant@canonical.com>
2011-11-11 05:02:55 +0000

    Renormalise schema after extensive performance experiments. APA.policy_use exists, APG.(policy_use|artifact) are mutually exclusive.

--- database/schema/patch-2208-93-1.sql
+++ database/schema/patch-2208-93-1.sql
@@ -2,29 +2,24 @@
 -- GNU Affero General Public License version 3 (see the file LICENSE).
 SET client_min_messages=ERROR;
 
-CREATE TABLE AccessPolicy (
+CREATE TABLE AccessPolicyUse (
     id serial PRIMARY KEY,
     product integer REFERENCES Product,
     distribution integer REFERENCES Distribution,
-    name text NOT NULL,
-    display_name text NOT NULL,
-    CONSTRAINT has_target CHECK (product IS NULL != distribution IS NULL),
-    CONSTRAINT valid_name CHECK (valid_name(name))
+    policy integer,
+    CONSTRAINT has_target CHECK (product IS NULL != distribution IS NULL)
 );
 
-CREATE UNIQUE INDEX accesspolicy__product__name__key
-    ON AccessPolicy(product, name) WHERE product IS NOT NULL;
-CREATE UNIQUE INDEX accesspolicy__product__display_name__key
-    ON AccessPolicy(product, display_name) WHERE product IS NOT NULL;
-CREATE UNIQUE INDEX accesspolicy__distribution__name__key
-    ON AccessPolicy(distribution, name) WHERE distribution IS NOT NULL;
-CREATE UNIQUE INDEX accesspolicy__distribution__display_name__key
-    ON AccessPolicy(distribution, display_name) WHERE distribution IS NOT NULL;
+CREATE UNIQUE INDEX accesspolicyuse__product__policy__key
+    ON AccessPolicyUse(product, policy) WHERE product IS NOT NULL;
+CREATE UNIQUE INDEX accesspolicyuse__distribution__policy__key
+    ON AccessPolicyUse(distribution, policy) WHERE distribution IS NOT NULL;
 
 CREATE TABLE AccessPolicyArtifact (
     id serial PRIMARY KEY,
     bug integer REFERENCES Bug,
     branch integer REFERENCES Branch,
+    policy_use integer NOT NULL REFERENCES AccessPolicyUse,
     CONSTRAINT has_artifact CHECK (bug IS NULL != branch IS NULL)
 );
 
@@ -32,26 +27,32 @@ CREATE UNIQUE INDEX accesspolicyartifact__bug__key
     ON AccessPolicyArtifact(bug) WHERE branch IS NULL;
 CREATE UNIQUE INDEX accesspolicyartifact__branch__key
     ON AccessPolicyArtifact(branch) WHERE bug IS NULL;
+CREATE INDEX accesspolicyartifact__policy_use__key
+    ON AccessPolicyArtifact(policy_use);
 
 CREATE TABLE AccessPolicyGrant (
     id serial PRIMARY KEY,
-    policy integer NOT NULL REFERENCES AccessPolicy,
     person integer NOT NULL REFERENCES Person,
-    artifact integer REFERENCES AccessPolicyArtifact
+    policy_use integer REFERENCES AccessPolicyUse,
+    artifact integer REFERENCES AccessPolicyArtifact,
+    creator integer NOT NULL REFERENCES Person,
+    date_created timestamp without time zone
+        DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC') NOT NULL,
+    CONSTRAINT has_target CHECK (policy_use IS NULL != artifact IS NULL)
 );
 
-CREATE UNIQUE INDEX accesspolicygrant__policy__person__artifact__key
-    ON AccessPolicyGrant(policy, person, artifact) WHERE artifact IS NOT NULL;
-CREATE UNIQUE INDEX accesspolicygrant__policy__person__key
-    ON AccessPolicyGrant(policy, person) WHERE artifact IS NULL;
+CREATE UNIQUE INDEX accesspolicygrant__policy_use__person__key
+    ON AccessPolicyGrant(policy_use, person) WHERE policy_use IS NOT NULL;
+CREATE UNIQUE INDEX accessartifactgrant__artifact__person__key
+    ON AccessPolicyGrant(artifact, person) WHERE artifact IS NOT NULL;
 CREATE INDEX accesspolicygrant__person__idx ON AccessPolicyGrant(person);
 
 ALTER TABLE bug
-    ADD COLUMN access_policy integer REFERENCES AccessPolicy;
-CREATE INDEX bug__access_policy__idx ON bug(access_policy);
+    ADD COLUMN access_policy_use integer REFERENCES AccessPolicyUse;
+CREATE INDEX bug__access_policy_use__idx ON bug(access_policy_use);
 
 ALTER TABLE branch
-    ADD COLUMN access_policy integer REFERENCES AccessPolicy;
-CREATE INDEX branch__access_policy__idx ON branch(access_policy);
+    ADD COLUMN access_policy_use integer REFERENCES AccessPolicyUse;
+CREATE INDEX branch__access_policy_use__idx ON branch(access_policy_use);
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2208, 93, 1);
