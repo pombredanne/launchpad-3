70060c9b3a571c76fb3607afd3ae13fcad575f5e
William Grant <william.grant@canonical.com>
2011-11-16 11:33:20 +1100

    Add patch 93-2, creating the APG.grantee/grantor FKs now that person merge knows about them.

---
+++ database/schema/patch-2208-93-2.sql
@@ -0,0 +1,10 @@
+-- Copyright 2011 Canonical Ltd.  This software is licensed under the
+-- GNU Affero General Public License version 3 (see the file LICENSE).
+SET client_min_messages=ERROR;
+
+ALTER TABLE AccessPolicyGrant ADD CONSTRAINT
+    "accesspolicygrant_grantee_fkey" FOREIGN KEY (grantee) REFERENCES Person;
+ALTER TABLE AccessPolicyGrant ADD CONSTRAINT
+    "accesspolicygrant_grantor_fkey" FOREIGN KEY (grantor) REFERENCES Person;
+
+INSERT INTO LaunchpadDatabaseRevision VALUES (2208, 93, 2);
