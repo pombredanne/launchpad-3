3ec0554bf296f46afa8d0c46da157af9326f33a1
Curtis Hovey <curtis.hovey@canonical.com>
2011-12-03 22:43:29 -0500

    Removed page_title that is supresses by this view.

--- lib/lp/bugs/browser/bugtarget.py
+++ lib/lp/bugs/browser/bugtarget.py
@@ -1354,8 +1354,6 @@ class BugTargetBugsView(BugTaskSearchListingView, FeedsMixin):
         """The display label for the view."""
         return 'Bugs in %s' % self.context.title
 
-    page_title = label
-
     def initialize(self):
         super(BugTargetBugsView, self).initialize()
         bug_statuses_to_show = list(UNRESOLVED_BUGTASK_STATUSES)
