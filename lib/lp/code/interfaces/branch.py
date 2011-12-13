0fd78ff2adaaf142ba80977a7a3ecd71fcca6a5e
Jelmer Vernooij <jelmer@canonical.com>
2011-12-13 12:12:28 +0000

    Avoid revision history in a few more places.

--- lib/lp/code/interfaces/branch.py
+++ lib/lp/code/interfaces/branch.py
@@ -898,8 +898,7 @@ class IBranchView(IHasOwner, IHasBranchTarget, IHasMergeProposals,
 
         :return: tuple of three items.
             1. Ancestry set of bzr revision-ids.
-            2. History list of bzr revision-ids. Similar to the result of
-               bzrlib.Branch.revision_history().
+            2. History list of bzr revision-ids, oldest first.
             3. Dictionnary mapping bzr bzr revision-ids to the database ids of
                the corresponding BranchRevision rows for this branch.
         """
