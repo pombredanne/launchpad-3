10ff582f6df094be480db6ba1a3d34af412182ce
Curtis Hovey <curtis.hovey@canonical.com>
2011-12-15 15:31:54 -0500

    Moved page_type test into unittest.

--- lib/lp/app/browser/tests/test_page_macro.py
+++ lib/lp/app/browser/tests/test_page_macro.py
@@ -11,21 +11,34 @@ from zope.component import getMultiAdapter
 from zope.traversing.interfaces import IPathAdapter
 
 from canonical.launchpad.webapp.publisher import rootObject
-from canonical.launchpad.webapp.servers import LaunchpadTestRequest
 from canonical.testing.layers import FunctionalLayer
-from lp.testing import TestCase
+from lp.testing import (
+    TestCase,
+    test_tales,
+    )
+from lp.testing.views import create_view
 
 
 class TestPageMacroDispatcher(TestCase):
 
     layer = FunctionalLayer
 
+    def setUp(self):
+        super(TestPageMacroDispatcher, self).setUp()
+        self.view = create_view(rootObject, name='index.html')
+
     def test_base_template(self):
         # Requests on the launchpad.dev vhost use the Launchpad base template.
-        root_view = getMultiAdapter(
-            (rootObject, LaunchpadTestRequest()), name='index.html')
-        adapter = getMultiAdapter([root_view], IPathAdapter, name='macro')
+        adapter = getMultiAdapter([self.view], IPathAdapter, name='macro')
         template_path = os.path.normpath(adapter.base.filename)
         self.assertIn('lp/app/templates', template_path)
         # The base template defines a 'master' macro as the adapter expects.
         self.assertIn('master', adapter.base.macros.keys())
+
+    def test_page_type(self):
+        page_macro = test_tales('view/macro:page/main_side', view=self.view)
+        self.assertEqual(('mode', 'html'), page_macro[1])
+        source_file = page_macro[3]
+        self.assertEqual('setSourceFile', source_file[0])
+        self.assertEqual(
+            '/templates/base-layout.pt', source_file[1].split('..')[1])
