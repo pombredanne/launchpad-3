cb912f78fe7852576656d3910bda8c45832e75ce
Curtis Hovey <curtis.hovey@canonical.com>
2011-12-21 15:53:29 -0500

    Moved soyuz mail to lp.soyuz.

--- lib/canonical/launchpad/mailnotification.py
+++ lib/canonical/launchpad/mailnotification.py
@@ -1,9 +1,5 @@
 # Copyright 2009-2011 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
-
-# XXX: Gavin Panella 2008-11-21 bug=300725: This module need
-# refactoring and/or splitting into a package or packages.
-
 """Event handlers that send email notifications."""
 
 __metaclass__ = type
@@ -15,27 +11,16 @@ from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 import re
 
-from zope.component import getUtility
 
 from canonical.config import config
-from canonical.database.sqlbase import block_implicit_flushes
 from canonical.launchpad.helpers import get_email_template
-from canonical.launchpad.webapp.interfaces import ILaunchpadRoot
-from canonical.launchpad.webapp.publisher import canonical_url
 from lp.bugs.mail.bugnotificationbuilder import get_bugmail_error_address
 from lp.services.mail.mailwrapper import MailWrapper
 # XXX 2010-06-16 gmb bug=594985
 #     This shouldn't be here, but if we take it out lots of things cry,
 #     which is sad.
 from lp.services.mail.notificationrecipientset import NotificationRecipientSet
-from lp.services.mail.sendmail import (
-    format_address,
-    sendmail,
-    simple_sendmail,
-    )
-
-# Silence lint warnings.
-NotificationRecipientSet
+from lp.services.mail.sendmail import sendmail
 
 
 CC = "CC"
@@ -130,62 +115,3 @@ def get_unified_diff(old_text, new_text, text_width):
         for line in text_diff]
     text_diff = '\n'.join(text_diff)
     return text_diff
-
-
-@block_implicit_flushes
-def notify_new_ppa_subscription(subscription, event):
-    """Notification that a new PPA subscription can be activated."""
-    non_active_subscribers = subscription.getNonActiveSubscribers()
-
-    archive = subscription.archive
-
-    # We don't send notification emails for commercial PPAs as these
-    # are purchased via software center (and do not mention Launchpad).
-    if archive.commercial:
-        return
-
-    registrant_name = subscription.registrant.displayname
-    ppa_displayname = archive.displayname
-    ppa_reference = "ppa:%s/%s" % (
-        archive.owner.name, archive.name)
-    ppa_description = archive.description
-    subject = 'PPA access granted for ' + ppa_displayname
-
-    template = get_email_template('ppa-subscription-new.txt', app='soyuz')
-
-    for person, preferred_email in non_active_subscribers:
-        to_address = [preferred_email.email]
-        root = getUtility(ILaunchpadRoot)
-        recipient_subscriptions_url = "%s~/+archivesubscriptions" % (
-            canonical_url(root))
-        description_blurb = '.'
-        if ppa_description is not None and ppa_description != '':
-            description_blurb = (
-                ' and has the following description:\n\n%s' % ppa_description)
-        replacements = {
-            'recipient_name': person.displayname,
-            'registrant_name': registrant_name,
-            'registrant_profile_url': canonical_url(subscription.registrant),
-            'ppa_displayname': ppa_displayname,
-            'ppa_reference': ppa_reference,
-            'ppa_description_blurb': description_blurb,
-            'recipient_subscriptions_url': recipient_subscriptions_url,
-            }
-        body = MailWrapper(72).format(template % replacements,
-                                      force_wrap=True)
-
-        from_address = format_address(
-            registrant_name, config.canonical.noreply_from_address)
-
-        headers = {
-            'Sender': config.canonical.bounce_address,
-            }
-
-        # If the registrant has a preferred email, then use it for the
-        # Reply-To.
-        if subscription.registrant.preferredemail:
-            headers['Reply-To'] = format_address(
-                registrant_name,
-                subscription.registrant.preferredemail.email)
-
-        simple_sendmail(from_address, to_address, subject, body, headers)
