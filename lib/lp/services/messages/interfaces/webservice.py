dfb4d6b5458506350b63f476e6a99d657ffc9105
Curtis Hovey <curtis.hovey@canonical.com>
2011-12-22 11:47:27 -0500

    Removed c.l interfaces and zcml. Moved webservice zcml to patchwebservice.

--- lib/lp/services/messages/interfaces/webservice.py
+++ lib/lp/services/messages/interfaces/webservice.py
@@ -4,7 +4,7 @@
 """All the interfaces that are exposed through the webservice.
 
 There is a declaration in ZCML somewhere that looks like:
-  <webservice:register module="canonical.launchpad.interfaces.webservice" />
+  <webservice:register module="lp.patchwebservice" />
 
 which tells `lazr.restful` that it should look for webservice exports here.
 """
