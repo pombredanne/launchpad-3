4918822964d8aac31324bd2eb6753e794de1c9e6
William Grant <william.grant@canonical.com>
2012-01-15 12:21:20 +0000

    Drop LaunchpadPrincipal password support.

--- lib/lp/services/webapp/authentication.py
+++ lib/lp/services/webapp/authentication.py
@@ -182,10 +182,9 @@ class PlacelessAuthUtility:
         utility = getUtility(IPlacelessLoginSource)
         return utility.getPrincipals(name)
 
-    def getPrincipalByLogin(self, login, want_password=True):
+    def getPrincipalByLogin(self, login):
         """See IAuthenticationService."""
-        utility = getUtility(IPlacelessLoginSource)
-        return utility.getPrincipalByLogin(login, want_password=want_password)
+        return getUtility(IPlacelessLoginSource).getPrincipalByLogin(login)
 
 
 class SSHADigestEncryptor:
@@ -267,15 +266,9 @@ class LaunchpadLoginSource:
 
     def getPrincipalByLogin(self, login,
                             access_level=AccessLevel.WRITE_PRIVATE,
-                            scope=None, want_password=True):
+                            scope=None):
         """Return a principal based on the account with the email address
         signified by "login".
-
-        :param want_password: If want_password is False, the pricipal
-        will have None for a password. Use this when trying to retrieve a
-        principal in contexts where we don't need the password and the
-        database connection does not have access to the Account or
-        AccountPassword tables.
 
         :return: None if there is no account with the given email address.
 
@@ -295,27 +288,18 @@ class LaunchpadLoginSource:
         person = getUtility(IPersonSet).getByEmail(login)
         if person is None or person.account is None:
             return None
-        return self._principalForAccount(
-            person.account, access_level, scope, want_password)
+        return self._principalForAccount(person.account, access_level, scope)
 
-    def _principalForAccount(self, account, access_level, scope,
-                             want_password=True):
+    def _principalForAccount(self, account, access_level, scope):
         """Return a LaunchpadPrincipal for the given account.
 
         The LaunchpadPrincipal will also have the given access level and
         scope.
-
-        If want_password is True, the principal's password will be set to the
-        account's password.  Otherwise it's set to None.
         """
         naked_account = removeSecurityProxy(account)
-        if want_password:
-            password = naked_account.password
-        else:
-            password = None
         principal = LaunchpadPrincipal(
             naked_account.id, naked_account.displayname,
-            naked_account.displayname, account, password,
+            naked_account.displayname, account,
             access_level=access_level, scope=scope)
         principal.__parent__ = self
         return principal
@@ -331,7 +315,7 @@ class LaunchpadPrincipal:
 
     implements(ILaunchpadPrincipal)
 
-    def __init__(self, id, title, description, account, pwd=None,
+    def __init__(self, id, title, description, account,
                  access_level=AccessLevel.WRITE_PRIVATE, scope=None):
         self.id = id
         self.title = title
@@ -340,16 +324,9 @@ class LaunchpadPrincipal:
         self.scope = scope
         self.account = account
         self.person = IPerson(account, None)
-        self.__pwd = pwd
 
     def getLogin(self):
         return self.title
-
-    def validate(self, pw):
-        encryptor = getUtility(IPasswordEncryptor)
-        pw1 = (pw or '').strip()
-        pw2 = (self.__pwd or '').strip()
-        return encryptor.validate(pw1, pw2)
 
 
 # zope.app.apidoc expects our principals to be adaptable into IAnnotations, so
