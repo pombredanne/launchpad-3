72a36d37e5f3468be24d03a18573c3a89471a2f4
Guilherme Salgado <salgado@canonical.com>
2012-01-20 20:44:03 +0000

    Initial version of the patch to make work items into first class objects

---
+++ database/schema/patch-2209-13-0.sql
@@ -0,0 +1,37 @@
+-- Copyright 2012 Canonical Ltd.  This software is licensed under the
+-- GNU Affero General Public License version 3 (see the file LICENSE).
+
+SET client_min_messages=ERROR;
+
+CREATE TABLE specificationworkitem (
+    id SERIAL PRIMARY KEY,
+    title text NOT NULL,
+    specification integer NOT NULL REFERENCES specification,
+    assignee integer REFERENCES person,
+    milestone integer REFERENCES milestone,
+    date_created timestamp without time zone DEFAULT 
+        timezone('UTC'::text, now()) NOT NULL,
+    status integer NOT NULL,
+    deleted boolean NOT NULL DEFAULT FALSE);
+
+CREATE TABLE specificationworkitemchange (
+    id SERIAL PRIMARY KEY,
+    work_item integer NOT NULL REFERENCES specificationworkitem,
+    new_status integer NOT NULL,
+    new_milestone integer REFERENCES milestone,
+    new_assignee integer REFERENCES person,
+    date date NOT NULL);
+
+CREATE TABLE specificationworkitemstats (
+    specification integer REFERENCES specification,
+    date date NOT NULL,
+    status integer NOT NULL,
+    assignee integer REFERENCES person,
+    milestone integer REFERENCES milestone,
+    count integer NOT NULL);
+
+-- TODO: Add security.cfg entries for the new tables
+-- TODO: Add comments.sql entries for the new tables
+
+-- FIXME: The patch number here is bogus.
+INSERT INTO LaunchpadDatabaseRevision VALUES (2209, 13, 0);
