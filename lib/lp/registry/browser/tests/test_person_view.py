9dd570b914d70910fd873db71459cc595c6ce093
Curtis Hovey <curtis.hovey@canonical.com>
2012-01-27 12:34:13 -0500

    Moved PersonView_openid_identity_url_TestCase to the other person tests module.

--- lib/lp/registry/browser/tests/test_person_view.py
+++ lib/lp/registry/browser/tests/test_person_view.py
@@ -3,6 +3,7 @@
 
 __metaclass__ = type
 
+from textwrap import dedent
 import doctest
 
 import soupmatchers
@@ -75,6 +76,41 @@ from lp.testing.views import (
     create_initialized_view,
     create_view,
     )
+
+
+class PersonViewOpenidIdentityUrlTestCase(TestCaseWithFactory):
+    """Tests for the public OpenID identifier shown on the profile page."""
+
+    layer = DatabaseFunctionalLayer
+
+    def setUp(self):
+        TestCaseWithFactory.setUp(self)
+        self.user = self.factory.makePerson(name='eris')
+        self.request = LaunchpadTestRequest(
+            SERVER_URL="http://launchpad.dev/")
+        login_person(self.user, self.request)
+        self.view = PersonView(self.user, self.request)
+        # Marker allowing us to reset the config.
+        config.push(self.id(), '')
+        self.addCleanup(config.pop, self.id())
+
+    def test_should_be_profile_page_when_delegating(self):
+        """The profile page is the OpenID identifier in normal situation."""
+        self.assertEquals(
+            'http://launchpad.dev/~eris', self.view.openid_identity_url)
+
+    def test_should_be_production_profile_page_when_not_delegating(self):
+        """When the profile page is not delegated, the OpenID identity URL
+        should be the one on the main production site."""
+        config.push('non-delegating', dedent('''
+            [vhost.mainsite]
+            openid_delegate_profile: False
+
+            [launchpad]
+            non_restricted_hostname: prod.launchpad.dev
+            '''))
+        self.assertEquals(
+            'http://prod.launchpad.dev/~eris', self.view.openid_identity_url)
 
 
 class TestPersonIndexView(TestCaseWithFactory):
