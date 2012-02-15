139867a6b8fb8875c21425f7fd16f3441a8d48f3
William Grant <william.grant@canonical.com>
2012-02-15 19:04:32 +1100

    Drop foreign keys from the old access policy schema, so we can remove the person and branch merging code.

---
+++ database/schema/patch-2209-11-0.sql
@@ -0,0 +1,11 @@
+-- Copyright 2012 Canonical Ltd.  This software is licensed under the
+-- GNU Affero General Public License version 3 (see the file LICENSE).
+
+SET client_min_messages=ERROR;
+
+-- Prepare to remove the old access policy schema.
+ALTER TABLE AccessPolicyGrant DROP CONSTRAINT accesspolicygrant_grantee_fkey;
+ALTER TABLE AccessPolicyGrant DROP CONSTRAINT accesspolicygrant_grantor_fkey;
+ALTER TABLE AccessPolicyArtifact DROP CONSTRAINT accesspolicyartifact_branch_fkey;
+
+INSERT INTO LaunchpadDatabaseRevision VALUES (2209, 11, 0);
