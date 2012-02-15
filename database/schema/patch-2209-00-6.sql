47d6c1b74dc78b6de134e3f2462a4e85706582b0
William Grant <william.grant@canonical.com>
2012-02-15 11:47:24 +1100

    Throw in an index on Packaging.productseries too.

--- database/schema/patch-2209-00-6.sql
+++ database/schema/patch-2209-00-6.sql
@@ -5,5 +5,7 @@ SET client_min_messages=ERROR;
 
 CREATE INDEX productreleasefile__productrelease__idx
     ON productreleasefile USING btree (productrelease); 
+CREATE INDEX packaging__productseries__idx
+    ON packaging USING btree (productseries);
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2209, 0, 6);
