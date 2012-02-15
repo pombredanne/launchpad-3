a2f96034a483a4b63a2ea691eb3be628c6069dc2
William Grant <william.grant@canonical.com>
2012-02-15 19:09:27 +1100

    Also kill off the Branch and Bug FKs to AccessPolicy.

--- database/schema/patch-2209-11-0.sql
+++ database/schema/patch-2209-11-0.sql
@@ -4,6 +4,8 @@
 SET client_min_messages=ERROR;
 
 -- Prepare to remove the old access policy schema.
+ALTER TABLE Branch DROP CONSTRAINT branch_access_policy_fkey; 
+ALTER TABLE Bug DROP CONSTRAINT bug_access_policy_fkey; 
 ALTER TABLE AccessPolicyGrant DROP CONSTRAINT accesspolicygrant_grantee_fkey;
 ALTER TABLE AccessPolicyGrant DROP CONSTRAINT accesspolicygrant_grantor_fkey;
 ALTER TABLE AccessPolicyArtifact DROP CONSTRAINT accesspolicyartifact_branch_fkey;
