39fcb1a1b2698e4ed83ac2551f6cc42e612512e7
William Grant <william.grant@canonical.com>
2012-02-15 11:56:48 +1100

    And another two, on milestone series this time.

--- database/schema/patch-2209-00-6.sql
+++ database/schema/patch-2209-00-6.sql
@@ -7,5 +7,9 @@ CREATE INDEX productreleasefile__productrelease__idx
     ON productreleasefile USING btree (productrelease); 
 CREATE INDEX packaging__productseries__idx
     ON packaging USING btree (productseries);
+CREATE INDEX milestone__distroseries__idx
+    ON milestone USING btree (distroseries);
+CREATE INDEX milestone__productseries__idx
+    ON milestone USING btree (productseries);
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2209, 0, 6);
