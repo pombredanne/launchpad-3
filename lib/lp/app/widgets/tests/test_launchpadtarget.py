27f24cc8fd752245d09bd20492b90d21bb389c70
Steve Kowalik <steve.kowalik@canonical.com>
2012-02-22 12:24:55 +1100

    More tea required, obviously.

--- lib/lp/app/widgets/tests/test_launchpadtarget.py
+++ lib/lp/app/widgets/tests/test_launchpadtarget.py
@@ -213,7 +213,7 @@ class LaunchpadTargetWidgetTestCase(TestCaseWithFactory):
         form['field.target.package'] = 'non-existent'
         self.widget.request = LaunchpadTestRequest(form=form)
         message = (
-            "There is no package named 'non-existent' published in Fnord".)
+            "There is no package named 'non-existent' published in Fnord.")
         self.assertRaisesWithContent(
             LaunchpadValidationError, message, self.widget.getInputValue)
         self.assertEqual(message, self.widget.error())
