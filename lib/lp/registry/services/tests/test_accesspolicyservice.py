d1175e30383752cf69c7aa757bd6dcc998d6450a
Ian Booth <ian.booth@canonical.com>
2012-02-23 21:29:15 +1000

    Add test for launchpadlib

--- lib/lp/registry/services/tests/test_accesspolicyservice.py
+++ lib/lp/registry/services/tests/test_accesspolicyservice.py
@@ -12,11 +12,31 @@ from lp.app.enums import InformationVisibilityPolicy
 from lp.registry.services.accesspolicyservice import AccessPolicyService
 from lp.services.webapp.interfaces import ILaunchpadRoot
 from lp.services.webapp.publisher import canonical_url
-from lp.testing import WebServiceTestCase
+from lp.testing import WebServiceTestCase, TestCaseWithFactory
+from lp.testing.layers import AppServerLayer
 from lp.testing.pages import LaunchpadWebServiceCaller
 
 
-class TestWebService(WebServiceTestCase):
+class ApiTestMixin:
+    """Common tests for launchpadlib and webservice."""
+
+    def test_getAccessPolicies(self):
+        # Test the getAccessPolicies method.
+        json_policies = self._getAccessPolicies()
+        policies = simplejson.loads(json_policies)
+        expected_polices = []
+        for x, policy in enumerate(InformationVisibilityPolicy):
+            item = dict(
+                index=x,
+                value=policy.token,
+                title=policy.title,
+                description=policy.value.description
+            )
+            expected_polices.append(item)
+        self.assertContentEqual(expected_polices, policies)
+
+
+class TestWebService(WebServiceTestCase, ApiTestMixin):
     """Test the web service interface for the Access Policy Service."""
 
     def setUp(self):
@@ -37,17 +57,22 @@ class TestWebService(WebServiceTestCase):
             '/services/accesspolicy',
             api_method, api_version='devel', **kwargs).jsonBody()
 
-    def test_getAccessPolicies(self):
-        # Test the getAccessPolicies method.
-        json_policies = self._named_get('getAccessPolicies')
-        policies = simplejson.loads(json_policies)
-        expected_polices = []
-        for x, policy in enumerate(InformationVisibilityPolicy):
-            item = dict(
-                index=x,
-                value=policy.token,
-                title=policy.title,
-                description=policy.value.description
-            )
-            expected_polices.append(item)
-        self.assertContentEqual(expected_polices, policies)
+    def _getAccessPolicies(self):
+        return self._named_get('getAccessPolicies')
+
+
+class TestLaunchpadlib(TestCaseWithFactory, ApiTestMixin):
+    """Test launchpadlib access for the Access Policy Service."""
+
+    layer = AppServerLayer
+
+    def setUp(self):
+        super(TestLaunchpadlib, self).setUp()
+        self.launchpad = self.factory.makeLaunchpadService()
+
+    def _getAccessPolicies(self):
+        # XXX 2012-02-23 wallyworld bug 681767
+        # Launchpadlib can't do relative url's
+        service = self.launchpad.load(
+            '%s/services/accesspolicy' % self.launchpad._root_uri)
+        return service.getAccessPolicies()
