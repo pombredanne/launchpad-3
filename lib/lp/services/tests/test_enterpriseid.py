e05790f685475b8cc7e3c7bd66d4f25c408c130c
Steve Kowalik <steve.kowalik@canonical.com>
2012-03-05 13:24:17 +1100

    Fix docstring.

--- lib/lp/services/tests/test_enterpriseid.py
+++ lib/lp/services/tests/test_enterpriseid.py
@@ -1,7 +1,7 @@
 # Copyright 2012 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
-"""XXX: Module docstring goes here."""
+"""Test Enterprise ID utilities."""
 
 __metaclass__ = type
 
