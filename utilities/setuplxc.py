a96de7d79716cda045224d873001368a72fb0de5
Francesco Banconi <francesco.banconi@canonical.com>
2012-03-05 12:04:16 +0000

    Updated scripts to use a specified ssh key.

--- utilities/setuplxc.py
+++ utilities/setuplxc.py
@@ -754,28 +754,28 @@ def initialize_host(
     # root (see below) but drops root once inside the LXC container.
     build_script_file = '/usr/local/bin/launchpad-lxc-build'
     with open(build_script_file, 'w') as script:
-        script.write(textwrap.dedent("""\
+        script.write(textwrap.dedent("""
             #!/bin/sh
             set -uex
             lxc-start -n lptests -d
             lxc-wait -n lptests -s RUNNING
             sleep 30 # aparently RUNNING isn't quite enough
-            su buildbot -c "/usr/bin/ssh -o StrictHostKeyChecking=no lptests \\
-                make -C /var/lib/buildbot/lp schema"
+            su buildbot -c "/usr/bin/ssh -o StrictHostKeyChecking=no \\
+                -i '{}' lptests make -C /var/lib/buildbot/lp schema"
             lxc-stop -n lptests
             lxc-wait -n lptests -s STOPPED
-            """))
+            """.format(ssh_key_path)))
         os.chmod(build_script_file, 0555)
     test_script_file = '/usr/local/bin/launchpad-lxc-test'
     with open(test_script_file, 'w') as script:
         script.write(textwrap.dedent("""
             #!/bin/sh
             set -uex
-            lxc-start-ephemeral -o lptests -b $PWD -- xvfb-run \\
+            lxc-start-ephemeral -S '{}' -o lptests -b $PWD -- xvfb-run \\
                 --error-file=/var/tmp/xvfb-errors.log \\
                 --server-args='-screen 0 1024x768x24' \\
                 -a $PWD/bin/test --subunit $@
-            """))
+            """.format(ssh_key_path)))
         os.chmod(test_script_file, 0555)
     # Add a file to sudoers.d that will let the buildbot user run the above.
     sudoers_file = '/etc/sudoers.d/launchpad-buildbot'
