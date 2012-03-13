a2501c6e39dca2e5a83b20adbf7d7728060f49b6
Steve Kowalik <steve.kowalik@canonical.com>
2012-03-13 11:45:33 +1100

    Every other enums module contains the s, force bugs into line.

--- lib/lp/bugs/browser/tests/test_bug_context_menu.py
+++ lib/lp/bugs/browser/tests/test_bug_context_menu.py
@@ -8,7 +8,7 @@ __metaclass__ = type
 from zope.component import getUtility
 
 from lp.bugs.browser.bug import BugContextMenu
-from lp.bugs.enum import BugNotificationLevel
+from lp.bugs.enums import BugNotificationLevel
 from lp.services.features import get_relevant_feature_controller
 from lp.services.webapp.interfaces import IOpenLaunchBag
 from lp.services.webapp.servers import LaunchpadTestRequest
