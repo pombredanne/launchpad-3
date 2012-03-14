d1e0c41ad13caff6b884e16909c22d891b093e76
Mattias Backman <mattias.backman@linaro.org>
2012-03-14 15:49:15 +0100

    Fix up the view a little.

--- lib/lp/registry/model/person.py
+++ lib/lp/registry/model/person.py
@@ -1764,6 +1764,8 @@ class Person(
             container = bug_containers_by_date.get(milestone.dateexpected)
             if container is None:
                 container = WorkItemContainer(
+                    # XXX: Perhaps we can use None here, to have something
+                    # easier to check for in the template.
                     'Aggregated bugs', 'N/A', 'N/A', 'N/A', [])
                 bug_containers_by_date[milestone.dateexpected] = container
                 # Also append our new container to the dictionary we're going
