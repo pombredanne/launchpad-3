dec05365a08ccb151b1352c2d7fe9c007ccfe002
Curtis Hovey <curtis.hovey@canonical.com>
2012-03-20 09:27:56 -0400

    Restore the draft of ProductNotificationJob.

--- lib/lp/registry/model/productjob.py
+++ lib/lp/registry/model/productjob.py
@@ -18,18 +18,24 @@ from storm.locals import (
     Reference,
     Unicode,
     )
+from zope.component import getUtility
 from zope.interface import (
     classProvides,
     implements,
     )
 
 from lp.registry.enums import ProductJobType
+from lp.registry.interfaces.person import (
+    IPersonSet,
+    )
 from lp.registry.interfaces.product import (
     IProduct,
     )
 from lp.registry.interfaces.productjob import (
     IProductJob,
     IProductJobSource,
+    IProductNotificationJob,
+    IProductNotificstionJobSource,
     )
 from lp.registry.model.product import Product
 from lp.services.database.decoratedresultset import DecoratedResultSet
@@ -41,6 +47,16 @@ from lp.services.database.lpstorm import (
 from lp.services.database.stormbase import StormBase
 from lp.services.job.model.job import Job
 from lp.services.job.runner import BaseRunnableJob
+from lp.services.mail.helpers import (
+    get_contact_email_addresses,
+    get_email_template,
+    )
+from lp.services.mail.mailwrapper import MailWrapper
+from lp.services.mail.sendmail import (
+    format_address_for_person,
+    simple_sendmail,
+    )
+from lp.services.scripts import log
 
 
 class ProductJob(StormBase):
@@ -149,3 +165,66 @@ class ProductJobDerived(BaseRunnableJob):
             ('product', self.context.product.name),
             ])
         return vars
+
+
+class ProductNotificationJob(ProductJobDerived):
+    """A Job that send an email to the product maintainer."""
+
+    implements(IProductNotificationJob)
+    classProvides(IProductNotificstionJobSource)
+    class_job_type = ProductJobType.REVIEWER_NOTIFICATION
+
+    @classmethod
+    def create(cls, product, email_template_name, subject, reviewer):
+        """See `IProductNotificationJob`."""
+        metadata = {
+            'email_template_name': email_template_name,
+            'subject': subject,
+            'reviewer_id': reviewer.id
+            }
+        return super(ProductNotificationJob, cls).create(product, metadata)
+
+    @property
+    def subject(self):
+        return self.metadata['subject']
+
+    @property
+    def email_template_name(self):
+        return self.metadata['email_template_name']
+
+    @property
+    def reviewer(self):
+        return getUtility(IPersonSet).get(self.metadata['reviewer_id'])
+
+    def getErrorRecipients(self):
+        """See `IPersonMergeJob`."""
+        if self.to_person.is_team:
+            return self.to_person.getTeamAdminsEmailAddresses()
+        else:
+            return [format_address_for_person(self.to_person)]
+
+    def sendEmailToMaintainer(self, template_name, subject, from_address):
+        email_template = get_email_template(
+            "%s.txt" % template_name, app='registry')
+        if self.product.owner.is_team:
+            addresses = self.product.owner.getTeamAdminsEmailAddresses()
+        else:
+            addresses = get_contact_email_addresses(self.product.owner)
+        for address in addresses:
+            msg = MailWrapper().format(
+                email_template % self.metadata, force_wrap=True)
+            simple_sendmail(from_address, address, subject, msg)
+
+    def run(self):
+        """Perform the merge."""
+        product_name = self.product.name
+        template_name = self.email_template_name
+        subject = self.subject
+        from_address = get_contact_email_addresses(self.reviewer)
+        log.debug(
+            "%s is sending a %s notification to the %s maintainers",
+            self.log_name, template_name, product_name)
+        self.sendEmailToMaintainer(template_name, subject, from_address)
+        log.debug(
+            "%s sent a %s notification to the %s maintainers",
+            self.log_name, template_name, product_name)
