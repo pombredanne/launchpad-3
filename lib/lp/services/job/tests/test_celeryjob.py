f13bf6da3081538345ea0cabd17ba7db8975f8f8
Aaron Bentley <aaron@canonical.com>
2012-03-22 15:32:37 -0400

    Fix lint.

--- lib/lp/services/job/tests/test_celeryjob.py
+++ lib/lp/services/job/tests/test_celeryjob.py
@@ -1,8 +1,6 @@
 # Copyright 2009 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
-import os
-
 from celery.exceptions import TimeoutError
 from lazr.jobrunner.tests.test_celerytask import running
 import transaction
