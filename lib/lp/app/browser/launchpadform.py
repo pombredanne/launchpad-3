16ca5beeba18705e5d39c004e3938a79c92c385f
Ian Booth <ian.booth@canonical.com>
2012-03-23 21:49:02 +1000

    Add tests

--- lib/lp/app/browser/launchpadform.py
+++ lib/lp/app/browser/launchpadform.py
@@ -352,6 +352,7 @@ class LaunchpadFormView(LaunchpadView):
         to render any errors. We simply convert the errors to json and return
         that data to the caller so the errors can be rendered.
         """
+
         if not self.request.is_ajax:
             return
         self.request.response.setStatus(400, "Validation")
