6c27e1d2dcaa35ec2ba6e85671d28bcace2bd2bf
Aaron Bentley <aaron@canonical.com>
2012-03-23 12:26:46 -0400

    Use config.rabbitmq.host for Celery.

--- lib/lp/services/job/celeryconfig.py
+++ lib/lp/services/job/celeryconfig.py
@@ -1,2 +1,3 @@
-BROKER_VHOST = "/"
+from lp.services.config import config
+BROKER_URL = "amqplib://%s" % config.rabbitmq.host
 CELERY_RESULT_BACKEND = "amqp"
--- lib/lp/services/job/tests/test_celeryjob.py
+++ lib/lp/services/job/tests/test_celeryjob.py
@@ -1,12 +1,12 @@
 # Copyright 2009 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
-from celery.exceptions import TimeoutError
-from lazr.jobrunner.tests.test_celerytask import running
+
+import os
+
 import transaction
 
 from lp.code.model.branchjob import BranchScanJob
-from lp.services.job.celeryjob import CeleryRunJob
 from lp.testing import TestCaseWithFactory
 from lp.testing.layers import ZopelessAppServerLayer
 
@@ -17,8 +17,16 @@ class TestCelery(TestCaseWithFactory):
 
     def test_run_scan_job(self):
         """Running a job via Celery succeeds and emits expected output."""
+        # Delay importing anything that uses Celery until RabbitMQLayer is
+        # running, so that config.rabbitmq.host is defined when
+        # lp.services.job.celeryconfig is loaded.
+        from lp.services.job.celeryjob import CeleryRunJob
+        from celery.exceptions import TimeoutError
+        from lazr.jobrunner.tests.test_celerytask import running
         cmd_args = ('--config', 'lp.services.job.tests.celeryconfig')
-        with running('bin/celeryd', cmd_args) as proc:
+        env = dict(os.environ)
+        env['BROKER_URL'] = CeleryRunJob.app.conf['BROKER_URL']
+        with running('bin/celeryd', cmd_args, env=env) as proc:
             self.useBzrBranches()
             db_branch, bzr_tree = self.create_branch_and_tree()
             bzr_tree.commit('First commit', rev_id='rev1')
