2d747a97d10cb20bf82f0cdba566dbefc8276f1f
Aaron Bentley <aaron@canonical.com>
2012-04-02 11:20:52 -0400

    Switch to requiring explicit queues.

--- lib/lp/services/job/celeryconfig.py
+++ lib/lp/services/job/celeryconfig.py
@@ -2,3 +2,10 @@ from lp.services.config import config
 BROKER_URL = "amqplib://%s" % config.rabbitmq.host
 CELERY_IMPORTS = ("lp.services.job.celeryjob", )
 CELERY_RESULT_BACKEND = "amqp"
+CELERY_QUEUES = {
+    "branch_write": {"binding_key": "job.branch_write"},
+    "standard": {"binding_key": "job.standard"},
+}
+CELERY_DEFAULT_EXCHANGE = "standard"
+CELERY_DEFAULT_QUEUE = "standard"
+CELERY_CREATE_MISSING_QUEUES = False
