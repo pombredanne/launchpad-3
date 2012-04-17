4564032e5b906878dab1c76153efdf8b191326c2
j.c.sackett <jcsackett@canonical.com>
2012-04-17 18:09:28 -0400

    Tests.

---
+++ lib/lp/soyuz/browser/tests/test_personal_archive_subscription.py
@@ -0,0 +1,33 @@
+# Copyright 2012 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Tests for the PersonalArchiveSubscription components and view."""
+
+__metaclass__ = type
+
+from zope.component import getUtility
+
+from lp.app.interfaces.launchpad import IPrivacy
+from lp.soyuz.interfaces.archivesubscriber import IArchiveSubscriberSet
+from lp.soyuz.browser.archivesubscription import PersonalArchiveSubscription
+from lp.testing import (
+    person_logged_in,
+    TestCaseWithFactory,
+    )
+from lp.testing.layers import DatabaseFunctionalLayer
+
+class TestSomething(TestCaseWithFactory):
+
+    layer = DatabaseFunctionalLayer
+
+    def test_personal_archive_subscription_adapts_to_privacy(self):
+        owner = self.factory.makePerson(name='archiveowner')
+        subscriber = self.factory.makePerson(name='subscriber')
+        pppa = self.factory.makeArchive(
+            owner=owner, private=True, name='pppa')
+        with person_logged_in(owner):
+            sub = pppa.newSubscription(subscriber, owner)
+        pas = PersonalArchiveSubscription(subscriber, pppa)
+        privacy = IPrivacy(pas)
+        self.assertTrue(privacy.private)
+        
