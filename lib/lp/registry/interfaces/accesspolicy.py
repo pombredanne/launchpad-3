27ddd5e45616224123dc252c87f2c20a26a5b477
Ian Booth <ian.booth@canonical.com>
2012-04-26 17:54:34 +1000

    Only enable Some radio button if there are shared artifacts

--- lib/lp/registry/interfaces/accesspolicy.py
+++ lib/lp/registry/interfaces/accesspolicy.py
@@ -241,11 +241,14 @@ class IAccessPolicyGrantFlatSource(Interface):
         :param policies: a collection of `IAccesPolicy`s.
         :param grantees: if not None, the result only includes people in the
             specified list of grantees.
-        :return: a collection of (`IPerson`, `IAccessPolicy`, permission)
+        :return: a collection of
+            (`IPerson`, `IAccessPolicy`, permission, shared_artifact_types)
             where permission is a SharingPermission enum value.
             ALL means the person has an access policy grant and can see all
             artifacts for the associated pillar.
             SOME means the person only has specified access artifact grants.
+            shared_artifact_types contains the information_types for which the
+            user has been granted access one or more artifacts of that type.
         """
 
     def findArtifactsByGrantee(grantee, policies):
