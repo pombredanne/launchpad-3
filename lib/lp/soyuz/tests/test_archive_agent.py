d4eee66c6ea423247b2a10fbbfcbe59ec2dba632
Jonathan Lange <jml@mumak.net>
2012-05-01 16:09:30 +0000

    Drop special commercial-based access for the software center agent.

--- lib/lp/soyuz/tests/test_archive_agent.py
+++ lib/lp/soyuz/tests/test_archive_agent.py
@@ -5,38 +5,43 @@
 
 from zope.component import getUtility
 
+from lp.app.interfaces.launchpad import ILaunchpadCelebrities
 from lp.services.webapp.authorization import check_permission
 from lp.soyuz.interfaces.archivesubscriber import IArchiveSubscriberSet
 from lp.testing import (
     celebrity_logged_in,
+    person_logged_in,
     TestCaseWithFactory,
     )
 from lp.testing.layers import DatabaseFunctionalLayer
 
 
-class TestArchivePrivacy(TestCaseWithFactory):
+class TestSoftwareCenterAgent(TestCaseWithFactory):
 
     layer = DatabaseFunctionalLayer
 
-    def test_check_permission(self):
-        """The software center agent has the relevant permissions for a
-        commercial archive, but not a private one.
-        """
+    def test_commercial_archives(self):
+        # The software center agent has no special permissions for commercial
+        # archives.
         ppa = self.factory.makeArchive(private=True, commercial=True)
         with celebrity_logged_in('software_center_agent'):
-            self.assertEqual(check_permission('launchpad.View', ppa), True)
-            self.assertEqual(check_permission('launchpad.Append', ppa), True)
+            self.assertEqual(check_permission('launchpad.View', ppa), False)
+            self.assertEqual(check_permission('launchpad.Append', ppa), False)
 
-    def test_check_permission_private(self):
+    def test_private_archives(self):
+        # The software center agent has no special permissions for private
+        # archives.
         ppa = self.factory.makeArchive(private=True, commercial=False)
         with celebrity_logged_in('software_center_agent'):
             self.assertEqual(check_permission('launchpad.View', ppa), False)
             self.assertEqual(check_permission('launchpad.Append', ppa), False)
 
     def test_add_subscription(self):
+        # The software center agent can subscribe anyone to an archive that it
+        # owns.  This is what allows people to buy stuff.
         person = self.factory.makePerson()
-        ppa = self.factory.makeArchive(private=True, commercial=True)
-        with celebrity_logged_in('software_center_agent') as agent:
+        ppa = self.factory.makeArchive(private=True)
+        with person_logged_in(ppa.owner) as agent:
             ppa.newSubscription(person, agent)
             subscription = getUtility(IArchiveSubscriberSet).getBySubscriber(
                 person, archive=ppa).one()
@@ -44,10 +49,16 @@ class TestArchivePrivacy(TestCaseWithFactory):
             self.assertEqual(subscription.subscriber, person)
 
     def test_getArchiveSubscriptionURL(self):
-        ppa = self.factory.makeArchive(private=True, commercial=True)
+        # The software center agent can get subscription URLs for any
+        # archive that it's an owner of.
+        owner = self.factory.makePerson()
+        agent = getUtility(ILaunchpadCelebrities).software_center_agent
+        ppa_owner = self.factory.makeTeam(members=[owner, agent])
+        ppa = self.factory.makeArchive(owner=ppa_owner, private=True)
         person = self.factory.makePerson()
         with celebrity_logged_in('software_center_agent') as agent:
             sources = person.getArchiveSubscriptionURL(agent, ppa)
+        with person_logged_in(ppa.owner):
             authtoken = ppa.getAuthToken(person).token
             url = ppa.archive_url.split('http://')[1]
         new_url = "http://%s:%s@%s" % (person.name, authtoken, url)
