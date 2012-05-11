4485e9e9a5e90066c81f9928328308cdf4a0f79f
William Grant <william.grant@canonical.com>
2012-05-11 12:39:08 +1000

    Fix missing imports

--- lib/lp/bugs/model/bugnomination.py
+++ lib/lp/bugs/model/bugnomination.py
@@ -22,6 +22,7 @@ from sqlobject import (
     ForeignKey,
     SQLObjectNotFound,
     )
+from zope.component import getUtility
 from zope.interface import implements
 
 from lp.app.errors import NotFoundError
@@ -32,6 +33,7 @@ from lp.bugs.interfaces.bugnomination import (
     IBugNomination,
     IBugNominationSet,
     )
+from lp.bugs.interfaces.bugtask import IBugTaskSet
 from lp.registry.interfaces.person import validate_public_person
 from lp.services.database.constants import UTC_NOW
 from lp.services.database.datetimecol import UtcDateTimeCol
