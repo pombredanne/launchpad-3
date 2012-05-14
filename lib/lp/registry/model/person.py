ea7b5e193d2fbea5884bca8faeae64746a25f3cf
Danilo Šegan <danilo@segan.org>
2012-05-14 12:50:16 +0200

    Deleted workitems should not show up in the upcoming work views.

--- lib/lp/registry/model/person.py
+++ lib/lp/registry/model/person.py
@@ -1501,6 +1501,7 @@ class Person(
         today = datetime.today().date()
         query = AND(
             Milestone.dateexpected <= date, Milestone.dateexpected >= today,
+            WorkItem.deleted == False,
             OR(WorkItem.assignee_id.is_in(self.participant_ids),
                Specification.assigneeID.is_in(self.participant_ids)))
         result = store.using(*origin).find(WorkItem, query)
