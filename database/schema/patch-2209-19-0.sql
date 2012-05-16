3da5e966781da84c50266cac1d82b955609854d9
William Grant <william.grant@canonical.com>
2012-05-16 16:34:16 +1000

    Reorganise the functions by journal/rollup. No changes yet.

--- database/schema/patch-2209-19-0.sql
+++ database/schema/patch-2209-19-0.sql
@@ -3,35 +3,7 @@
 
 SET client_min_messages=ERROR;
 
-CREATE OR REPLACE FUNCTION public.bug_summary_dec(bugsummary)
- RETURNS void
- LANGUAGE sql
-AS $function$
-    -- We own the row reference, so in the absence of bugs this cannot
-    -- fail - just decrement the row.
-    UPDATE BugSummary SET count = count + $1.count
-    WHERE
-        ((product IS NULL AND $1.product IS NULL)
-            OR product = $1.product)
-        AND ((productseries IS NULL AND $1.productseries IS NULL)
-            OR productseries = $1.productseries)
-        AND ((distribution IS NULL AND $1.distribution IS NULL)
-            OR distribution = $1.distribution)
-        AND ((distroseries IS NULL AND $1.distroseries IS NULL)
-            OR distroseries = $1.distroseries)
-        AND ((sourcepackagename IS NULL AND $1.sourcepackagename IS NULL)
-            OR sourcepackagename = $1.sourcepackagename)
-        AND ((viewed_by IS NULL AND $1.viewed_by IS NULL)
-            OR viewed_by = $1.viewed_by)
-        AND ((tag IS NULL AND $1.tag IS NULL)
-            OR tag = $1.tag)
-        AND status = $1.status
-        AND ((milestone IS NULL AND $1.milestone IS NULL)
-            OR milestone = $1.milestone)
-        AND importance = $1.importance
-        AND has_patch = $1.has_patch
-        AND fixed_upstream = $1.fixed_upstream;
-$function$
+-- Journal functions. Speed is critical -- these are run by appservers.
 
 CREATE OR REPLACE FUNCTION public.bug_summary_flush_temp_journal()
  RETURNS void
@@ -47,61 +19,6 @@ BEGIN
         PERFORM bugsummary_journal_ins(d);
     END LOOP;
     TRUNCATE bugsummary_temp_journal;
-END;
-$function$
-
-CREATE OR REPLACE FUNCTION public.bug_summary_inc(d bugsummary)
- RETURNS void
- LANGUAGE plpgsql
-AS $function$
-BEGIN
-    -- Shameless adaption from postgresql manual
-    LOOP
-        -- first try to update the row
-        UPDATE BugSummary SET count = count + d.count
-        WHERE
-            ((product IS NULL AND $1.product IS NULL)
-                OR product = $1.product)
-            AND ((productseries IS NULL AND $1.productseries IS NULL)
-                OR productseries = $1.productseries)
-            AND ((distribution IS NULL AND $1.distribution IS NULL)
-                OR distribution = $1.distribution)
-            AND ((distroseries IS NULL AND $1.distroseries IS NULL)
-                OR distroseries = $1.distroseries)
-            AND ((sourcepackagename IS NULL AND $1.sourcepackagename IS NULL)
-                OR sourcepackagename = $1.sourcepackagename)
-            AND ((viewed_by IS NULL AND $1.viewed_by IS NULL)
-                OR viewed_by = $1.viewed_by)
-            AND ((tag IS NULL AND $1.tag IS NULL)
-                OR tag = $1.tag)
-            AND status = $1.status
-            AND ((milestone IS NULL AND $1.milestone IS NULL)
-                OR milestone = $1.milestone)
-            AND importance = $1.importance
-            AND has_patch = $1.has_patch
-            AND fixed_upstream = $1.fixed_upstream;
-        IF found THEN
-            RETURN;
-        END IF;
-        -- not there, so try to insert the key
-        -- if someone else inserts the same key concurrently,
-        -- we could get a unique-key failure
-        BEGIN
-            INSERT INTO BugSummary(
-                count, product, productseries, distribution,
-                distroseries, sourcepackagename, viewed_by, tag,
-                status, milestone,
-                importance, has_patch, fixed_upstream)
-            VALUES (
-                d.count, d.product, d.productseries, d.distribution,
-                d.distroseries, d.sourcepackagename, d.viewed_by, d.tag,
-                d.status, d.milestone,
-                d.importance, d.has_patch, d.fixed_upstream);
-            RETURN;
-        EXCEPTION WHEN unique_violation THEN
-            -- do nothing, and loop to try the UPDATE again
-        END;
-    END LOOP;
 END;
 $function$
 
@@ -244,68 +161,6 @@ BEGIN
         FROM bugsummary_tasks(BUG_ROW) AS tasks
         JOIN bugsummary_tags(BUG_ROW) AS bug_tags ON TRUE
         LEFT OUTER JOIN bugsummary_viewers(BUG_ROW) AS bug_viewers ON TRUE;
-END;
-$function$
-
-CREATE OR REPLACE FUNCTION public.bugsummary_rollup_journal(batchsize integer DEFAULT NULL::integer)
- RETURNS void
- LANGUAGE plpgsql
- SECURITY DEFINER
- SET search_path TO public
-AS $function$
-DECLARE
-    d bugsummary%ROWTYPE;
-    max_id integer;
-BEGIN
-    -- Lock so we don't content with other invokations of this
-    -- function. We can happily lock the BugSummary table for writes
-    -- as this function is the only thing that updates that table.
-    -- BugSummaryJournal remains unlocked so nothing should be blocked.
-    LOCK TABLE BugSummary IN ROW EXCLUSIVE MODE;
-
-    IF batchsize IS NULL THEN
-        SELECT MAX(id) INTO max_id FROM BugSummaryJournal;
-    ELSE
-        SELECT MAX(id) INTO max_id FROM (
-            SELECT id FROM BugSummaryJournal ORDER BY id LIMIT batchsize
-            ) AS Whatever;
-    END IF;
-
-    FOR d IN
-        SELECT
-            NULL as id,
-            SUM(count),
-            product,
-            productseries,
-            distribution,
-            distroseries,
-            sourcepackagename,
-            viewed_by,
-            tag,
-            status,
-            milestone,
-            importance,
-            has_patch,
-            fixed_upstream
-        FROM BugSummaryJournal
-        WHERE id <= max_id
-        GROUP BY
-            product, productseries, distribution, distroseries,
-            sourcepackagename, viewed_by, tag, status, milestone,
-            importance, has_patch, fixed_upstream
-        HAVING sum(count) <> 0
-    LOOP
-        IF d.count < 0 THEN
-            PERFORM bug_summary_dec(d);
-        ELSIF d.count > 0 THEN
-            PERFORM bug_summary_inc(d);
-        END IF;
-    END LOOP;
-
-    -- Clean out any counts we reduced to 0.
-    DELETE FROM BugSummary WHERE count=0;
-    -- Clean out the journal entries we have handled.
-    DELETE FROM BugSummaryJournal WHERE id <= max_id;
 END;
 $function$
 
@@ -514,6 +369,157 @@ BEGIN
         PERFORM bug_summary_temp_journal_ins(d);
     END LOOP;
 END;
+$function$
+
+
+-- Rollup functions. Speed isn't critical, as it's done post-request by garbo.
+
+CREATE OR REPLACE FUNCTION public.bugsummary_rollup_journal(batchsize integer DEFAULT NULL::integer)
+ RETURNS void
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+ SET search_path TO public
+AS $function$
+DECLARE
+    d bugsummary%ROWTYPE;
+    max_id integer;
+BEGIN
+    -- Lock so we don't content with other invokations of this
+    -- function. We can happily lock the BugSummary table for writes
+    -- as this function is the only thing that updates that table.
+    -- BugSummaryJournal remains unlocked so nothing should be blocked.
+    LOCK TABLE BugSummary IN ROW EXCLUSIVE MODE;
+
+    IF batchsize IS NULL THEN
+        SELECT MAX(id) INTO max_id FROM BugSummaryJournal;
+    ELSE
+        SELECT MAX(id) INTO max_id FROM (
+            SELECT id FROM BugSummaryJournal ORDER BY id LIMIT batchsize
+            ) AS Whatever;
+    END IF;
+
+    FOR d IN
+        SELECT
+            NULL as id,
+            SUM(count),
+            product,
+            productseries,
+            distribution,
+            distroseries,
+            sourcepackagename,
+            viewed_by,
+            tag,
+            status,
+            milestone,
+            importance,
+            has_patch,
+            fixed_upstream
+        FROM BugSummaryJournal
+        WHERE id <= max_id
+        GROUP BY
+            product, productseries, distribution, distroseries,
+            sourcepackagename, viewed_by, tag, status, milestone,
+            importance, has_patch, fixed_upstream
+        HAVING sum(count) <> 0
+    LOOP
+        IF d.count < 0 THEN
+            PERFORM bug_summary_dec(d);
+        ELSIF d.count > 0 THEN
+            PERFORM bug_summary_inc(d);
+        END IF;
+    END LOOP;
+
+    -- Clean out any counts we reduced to 0.
+    DELETE FROM BugSummary WHERE count=0;
+    -- Clean out the journal entries we have handled.
+    DELETE FROM BugSummaryJournal WHERE id <= max_id;
+END;
+$function$
+
+
+CREATE OR REPLACE FUNCTION public.bug_summary_inc(d bugsummary)
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$
+BEGIN
+    -- Shameless adaption from postgresql manual
+    LOOP
+        -- first try to update the row
+        UPDATE BugSummary SET count = count + d.count
+        WHERE
+            ((product IS NULL AND $1.product IS NULL)
+                OR product = $1.product)
+            AND ((productseries IS NULL AND $1.productseries IS NULL)
+                OR productseries = $1.productseries)
+            AND ((distribution IS NULL AND $1.distribution IS NULL)
+                OR distribution = $1.distribution)
+            AND ((distroseries IS NULL AND $1.distroseries IS NULL)
+                OR distroseries = $1.distroseries)
+            AND ((sourcepackagename IS NULL AND $1.sourcepackagename IS NULL)
+                OR sourcepackagename = $1.sourcepackagename)
+            AND ((viewed_by IS NULL AND $1.viewed_by IS NULL)
+                OR viewed_by = $1.viewed_by)
+            AND ((tag IS NULL AND $1.tag IS NULL)
+                OR tag = $1.tag)
+            AND status = $1.status
+            AND ((milestone IS NULL AND $1.milestone IS NULL)
+                OR milestone = $1.milestone)
+            AND importance = $1.importance
+            AND has_patch = $1.has_patch
+            AND fixed_upstream = $1.fixed_upstream;
+        IF found THEN
+            RETURN;
+        END IF;
+        -- not there, so try to insert the key
+        -- if someone else inserts the same key concurrently,
+        -- we could get a unique-key failure
+        BEGIN
+            INSERT INTO BugSummary(
+                count, product, productseries, distribution,
+                distroseries, sourcepackagename, viewed_by, tag,
+                status, milestone,
+                importance, has_patch, fixed_upstream)
+            VALUES (
+                d.count, d.product, d.productseries, d.distribution,
+                d.distroseries, d.sourcepackagename, d.viewed_by, d.tag,
+                d.status, d.milestone,
+                d.importance, d.has_patch, d.fixed_upstream);
+            RETURN;
+        EXCEPTION WHEN unique_violation THEN
+            -- do nothing, and loop to try the UPDATE again
+        END;
+    END LOOP;
+END;
+$function$
+
+CREATE OR REPLACE FUNCTION public.bug_summary_dec(bugsummary)
+ RETURNS void
+ LANGUAGE sql
+AS $function$
+    -- We own the row reference, so in the absence of bugs this cannot
+    -- fail - just decrement the row.
+    UPDATE BugSummary SET count = count + $1.count
+    WHERE
+        ((product IS NULL AND $1.product IS NULL)
+            OR product = $1.product)
+        AND ((productseries IS NULL AND $1.productseries IS NULL)
+            OR productseries = $1.productseries)
+        AND ((distribution IS NULL AND $1.distribution IS NULL)
+            OR distribution = $1.distribution)
+        AND ((distroseries IS NULL AND $1.distroseries IS NULL)
+            OR distroseries = $1.distroseries)
+        AND ((sourcepackagename IS NULL AND $1.sourcepackagename IS NULL)
+            OR sourcepackagename = $1.sourcepackagename)
+        AND ((viewed_by IS NULL AND $1.viewed_by IS NULL)
+            OR viewed_by = $1.viewed_by)
+        AND ((tag IS NULL AND $1.tag IS NULL)
+            OR tag = $1.tag)
+        AND status = $1.status
+        AND ((milestone IS NULL AND $1.milestone IS NULL)
+            OR milestone = $1.milestone)
+        AND importance = $1.importance
+        AND has_patch = $1.has_patch
+        AND fixed_upstream = $1.fixed_upstream;
 $function$
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2209, 19, 0);
