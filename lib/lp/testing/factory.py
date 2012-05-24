52238c76055803cbfa7e7bc897704addf6c25f6f
Steve Kowalik <steve.kowalik@canonical.com>
2012-05-24 12:16:59 +1000

    Resurrect branch-use-information_type and fix all of the test failures.

--- lib/lp/testing/factory.py
+++ lib/lp/testing/factory.py
@@ -140,6 +140,7 @@ from lp.registry.enums import (
     DistroSeriesDifferenceStatus,
     DistroSeriesDifferenceType,
     InformationType,
+    PRIVATE_INFORMATION_TYPES,
     )
 from lp.registry.interfaces.accesspolicy import (
     IAccessArtifactGrantSource,
@@ -1072,8 +1073,8 @@ class BareLaunchpadObjectFactory(ObjectFactory):
 
     def makeBranch(self, branch_type=None, owner=None,
                    name=None, product=_DEFAULT, url=_DEFAULT, registrant=None,
-                   private=False, stacked_on=None, sourcepackage=None,
-                   reviewer=None, **optional_branch_args):
+                   private=None, information_type=None, stacked_on=None,
+                   sourcepackage=None, reviewer=None, **optional_branch_args):
         """Create and return a new, arbitrary Branch of the given type.
 
         Any parameters for `IBranchNamespace.createBranch` can be specified to
@@ -1119,11 +1120,23 @@ class BareLaunchpadObjectFactory(ObjectFactory):
         branch = namespace.createBranch(
             branch_type=branch_type, name=name, registrant=registrant,
             url=url, **optional_branch_args)
-        if private:
-            removeSecurityProxy(branch).explicitly_private = True
-            removeSecurityProxy(branch).transitively_private = True
+        assert information_type is None or private is None, (
+            "Can not specify both information_type and private")
+        if information_type is not None or private is not None:
+            if information_type:
+                private = information_type in PRIVATE_INFORMATION_TYPES
+            else:
+                information_type = (
+                    InformationType.USERDATA if private else
+                    InformationType.PUBLIC)
+            if private:
+                removeSecurityProxy(branch).explicitly_private = True
+                removeSecurityProxy(branch).transitively_private = True
+            removeSecurityProxy(branch).information_type = information_type
         if stacked_on is not None:
-            removeSecurityProxy(branch).stacked_on = stacked_on
+            removeSecurityProxy(branch).branchChanged(
+                removeSecurityProxy(stacked_on).unique_name, 'rev1', None,
+                None, None)
         if reviewer is not None:
             removeSecurityProxy(branch).reviewer = reviewer
         return branch
