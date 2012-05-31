1ab3d4fc6de759ff28c65bd64ac7e7823fe530b5
Jonathan Lange <jml@mumak.net>
2012-05-31 15:49:58 +0000

    Revert assertContentEqual change. Casting to set() restricted the range of values it would work for.

--- lib/lp/testing/__init__.py
+++ lib/lp/testing/__init__.py
@@ -104,6 +104,7 @@ from testtools.content_type import UTF8_TEXT
 from testtools.matchers import (
     Equals,
     MatchesRegex,
+    MatchesSetwise,
     )
 from testtools.testcase import ExpectedException as TTExpectedException
 import transaction
@@ -566,7 +567,7 @@ class TestCase(testtools.TestCase, fixtures.TestWithFixtures):
 
     def assertContentEqual(self, iter1, iter2):
         """Assert that 'iter1' has the same content as 'iter2'."""
-        self.assertThat(set(iter1), Equals(set(iter2)))
+        self.assertThat(iter1, MatchesSetwise(*(map(Equals, iter2))))
 
     def assertRaisesWithContent(self, exception, exception_content,
                                 func, *args):
