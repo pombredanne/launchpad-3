b341f06fe153016d01f5f80145c1975089dc4bcb
Jonathan Lange <jml@canonical.com>
2012-06-09 16:37:54 +0000

    Sketch out how we want to proceed.

--- lib/lp/soyuz/model/archive.py
+++ lib/lp/soyuz/model/archive.py
@@ -2054,13 +2054,15 @@ class Archive(SQLBase):
         job.destroySelf()
 
 
-def validate_ppa(person, proposed_name, private=False):
+def validate_ppa(owner, proposed_name, private=False):
     """Can 'person' create a PPA called 'proposed_name'?
 
-    :param person: The owner *and* creator.
+    :param owner: The owner *and* creator.
     :param proposed_name: The proposed name.
     :param private: Whether or not to make it private.
     """
+    # XXX: Make this a separate parameter.
+    creator = owner
     ubuntu = getUtility(ILaunchpadCelebrities).ubuntu
     if private:
         # NOTE: This duplicates the policy in lp/soyuz/configure.zcml
@@ -2068,11 +2070,11 @@ def validate_ppa(person, proposed_name, private=False):
         # set 'private', and the logic in `AdminByCommercialTeamOrAdmins`
         # which determines who is granted launchpad.Commercial
         # permissions.
-        role = IPersonRoles(person)
+        role = IPersonRoles(creator)
         if not (role.in_admin or role.in_commercial_admin):
-            return '%s is not allowed to make private PPAs' % person.name
-    if person.is_team and (
-        person.subscriptionpolicy in OPEN_TEAM_POLICY):
+            return '%s is not allowed to make private PPAs' % creator.name
+    if owner.is_team and (
+        owner.subscriptionpolicy in OPEN_TEAM_POLICY):
         return "Open teams cannot have PPAs."
     if proposed_name is not None and proposed_name == ubuntu.name:
         return (
@@ -2080,14 +2082,16 @@ def validate_ppa(person, proposed_name, private=False):
     if proposed_name is None:
         proposed_name = 'ppa'
     try:
-        person.getPPAByName(proposed_name)
+        owner.getPPAByName(proposed_name)
     except NoSuchPPA:
         return None
     else:
+        # XXX: What if the creator is not in the owner's team?
         text = "You already have a PPA named '%s'." % proposed_name
-        if person.is_team:
+        # XXX: Shouldn't this be "owner != creator"?
+        if owner.is_team:
             text = "%s already has a PPA named '%s'." % (
-                person.displayname, proposed_name)
+                owner.displayname, proposed_name)
         return text
 
 
