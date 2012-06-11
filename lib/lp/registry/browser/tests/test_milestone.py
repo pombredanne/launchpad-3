ac75e5d6b1aea347f27e1737fa2a2add5acd4535
Robert Collins <robertc@robertcollins.net>
2012-06-11 12:03:25 +1200

    Back out the memcache part of rev 10891, which used memcache for slow pages (vs for scaling the site), and introduced a large number of user confusing latencies.

--- lib/lp/registry/browser/tests/test_milestone.py
+++ lib/lp/registry/browser/tests/test_milestone.py
@@ -157,57 +157,6 @@ class TestMilestoneMemcache(MemcacheTestCase):
         bugtask.milestone = self.milestone
         self.observer = self.factory.makePerson()
 
-    def test_milestone_index_memcache_anonymous(self):
-        # Miss the cache on first render.
-        login(ANONYMOUS)
-        view = create_initialized_view(
-            self.milestone, name='+index', principal=None)
-        content = view.render()
-        self.assertCacheMiss('<dt>Assigned to you:</dt>', content)
-        self.assertCacheMiss('id="milestone_bugtasks"', content)
-        # Hit the cache on the second render.
-        view = create_initialized_view(
-            self.milestone, name='+index', principal=None)
-        self.assertTrue(view.milestone.active)
-        self.assertEqual(10, view.expire_cache_minutes)
-        content = view.render()
-        self.assertCacheHit(
-            '<dt>Assigned to you:</dt>',
-            'anonymous, view/expire_cache_minutes minute', content)
-        self.assertCacheHit(
-            'id="milestone_bugtasks"',
-            'anonymous, view/expire_cache_minutes minute', content)
-
-    def test_milestone_index_memcache_no_cache_logged_in(self):
-        login_person(self.observer)
-        # Miss the cache on first render.
-        view = create_initialized_view(
-            self.milestone, name='+index', principal=self.observer)
-        content = view.render()
-        self.assertCacheMiss('<dt>Assigned to you:</dt>', content)
-        self.assertCacheMiss('id="milestone_bugtasks"', content)
-        # Miss the cache again on the second render.
-        view = create_initialized_view(
-            self.milestone, name='+index', principal=self.observer)
-        self.assertTrue(view.milestone.active)
-        self.assertEqual(10, view.expire_cache_minutes)
-        content = view.render()
-        self.assertCacheMiss('<dt>Assigned to you:</dt>', content)
-        self.assertCacheMiss('id="milestone_bugtasks"', content)
-
-    def test_milestone_index_active_cache_time(self):
-        # Verify the active milestone cache time.
-        view = create_initialized_view(self.milestone, name='+index')
-        self.assertTrue(view.milestone.active)
-        self.assertEqual(10, view.expire_cache_minutes)
-
-    def test_milestone_index_inactive_cache_time(self):
-        # Verify the inactive milestone cache time.
-        self.milestone.active = False
-        view = create_initialized_view(self.milestone, name='+index')
-        self.assertFalse(view.milestone.active)
-        self.assertEqual(360, view.expire_cache_minutes)
-
 
 class TestMilestoneDeleteView(TestCaseWithFactory):
     """Test the delete rules applied by the Milestone Delete view."""
