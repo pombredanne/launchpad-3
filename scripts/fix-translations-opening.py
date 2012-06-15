b8e43a84ac3bf13bd0e873f57285e0143562dabe
John Arbash Meinel <john@arbash-meinel.com>
2012-06-15 13:04:01 +0200

    Tweak the fix-translations-opening script to import from the right place. No longer explicitly request python2.6, etc.

--- scripts/fix-translations-opening.py
+++ scripts/fix-translations-opening.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python2.6 -S
+#!/usr/bin/python -S
 
 __metaclass__ = type
 
@@ -7,9 +7,11 @@ import _pythonpath
 from zope.component import getUtility
 from zope.interface import implements
 
-from canonical.launchpad.interfaces.looptuner import ITunableLoop
-from canonical.launchpad.utilities.looptuner import DBLoopTuner
-from canonical.launchpad.webapp.interfaces import (
+from lp.services.looptuner import (
+    DBLoopTuner,
+    ITunableLoop,
+    )
+from lp.services.webapp.interfaces import (
     IStoreSelector,
     MAIN_STORE,
     MASTER_FLAVOR,
