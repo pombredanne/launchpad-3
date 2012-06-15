c28a9bc202b5f7780ae19362a17987c0e577a4c4
Steve Kowalik <steve.kowalik@canonical.com>
2012-06-15 15:49:38 +1000

    Add a new DB patch that drops the old privacy from branch and transitions to the new access policy. Add tests for the triggers, and fix sampledata.

---
+++ lib/lp/code/tests/test_branch_access_policy_triggers.py
@@ -0,0 +1,40 @@
+# Copyright 2012 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+__metaclass__ = type
+
+from zope.security.proxy import removeSecurityProxy
+
+from lp.code.model.branch import Branch
+from lp.registry.enums import InformationType
+from lp.services.database.lpstorm import IStore
+from lp.testing import TestCaseWithFactory
+from lp.testing.layers import DatabaseFunctionalLayer
+
+
+class TestBranchAccessPolicyTriggers(TestCaseWithFactory):
+    layer = DatabaseFunctionalLayer
+
+    def fetchPolicies(self, branch):
+        # We may be dealing with private branches, so just ignore security.
+        return IStore(Branch).execute(
+            "SELECT access_policy, access_grants FROM branch WHERE id = ?",
+            (removeSecurityProxy(branch).id,)).get_one()
+
+    def assertAccess(self, branch, expected_policy, expected_grants):
+        policy, grants = self.fetchPolicies(branch)
+        self.assertEqual(expected_policy, policy)
+        self.assertEqual(expected_grants, grants)
+
+    def test_no_access_policy_for_public_branches(self):
+        # A public branch has no access policy or grants.
+        self.assertAccess(self.factory.makeBranch(), None, None)
+
+    def test_adding_apg_with_private_branch(self):
+        # Adding a new AAG updates the branch columns via trigger.
+        branch = self.factory.makeBranch(
+            information_type=InformationType.USERDATA)
+        self.assertAccess(branch, 65, [])
+        artifact = self.factory.makeAccessArtifact(concrete=branch)
+        grant = self.factory.makeAccessArtifactGrant(artifact=artifact)
+        self.assertAccess(branch, 65, [grant.grantee.id])
