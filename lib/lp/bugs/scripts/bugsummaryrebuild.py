1ccfa9a6795e391e0334c5132dd6f863e25842e7
William Grant <william.grant@canonical.com>
2012-06-19 17:05:17 +1000

    Add the first bits of bugsummaryrebuild: helpers to get the current and expected sets of BugSummary targets.

---
+++ lib/lp/bugs/scripts/bugsummaryrebuild.py
@@ -0,0 +1,29 @@
+# Copyright 2012 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+__metaclass__ = type
+
+from lp.bugs.model.bugsummary import BugSummary
+from lp.bugs.model.bugtask import BugTask
+from lp.services.database.lpstorm import IStore
+
+
+def get_existing_targets():
+    """Get the current set of targets represented in BugSummary."""
+    return set(IStore(BugSummary).find(
+        (BugSummary.product_id, BugSummary.productseries_id,
+         BugSummary.distribution_id, BugSummary.distroseries_id,
+         BugSummary.sourcepackagename_id)).config(distinct=True))
+
+
+def get_expected_targets():
+    """Get the current set of targets represented in BugTask."""
+    new_targets = set(IStore(BugTask).find(
+        (BugTask.productID, BugTask.productseriesID,
+         BugTask.distributionID, BugTask.distroseriesID,
+         BugTask.sourcepackagenameID)).config(distinct=True))
+    # BugSummary counts package tasks in the packageless totals, so
+    # ensure that there's also a packageless total for each distro(series).
+    new_targets.update(set(
+        (p, ps, d, ds, None) for (p, ps, d, ds, spn) in new_targets))
+    return new_targets
---
+++ lib/lp/bugs/scripts/tests/test_bugsummaryrebuild.py
@@ -0,0 +1,64 @@
+# Copyright 2012 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+__metaclass__ = type
+
+from zope.component import getUtility
+
+from lp.bugs.interfaces.bugtask import IBugTaskSet
+from lp.bugs.model.bugsummary import BugSummary
+from lp.bugs.scripts.bugsummaryrebuild import (
+    get_existing_targets,
+    get_expected_targets,
+    )
+from lp.services.database.lpstorm import IStore
+from lp.testing import TestCaseWithFactory
+from lp.testing.layers import ZopelessDatabaseLayer
+
+
+def rollup_journal():
+    IStore(BugSummary).execute('SELECT bugsummary_rollup_journal()')
+
+
+def create_tasks(factory):
+    ps = factory.makeProductSeries()
+    product = ps.product
+    sp = factory.makeSourcePackage(publish=True)
+
+    bug = factory.makeBug(product=product)
+    getUtility(IBugTaskSet).createManyTasks(
+        bug, bug.owner, [sp, sp.distribution_sourcepackage, ps])
+
+    # There'll be a target for each task, plus a packageless one for
+    # each package task.
+    expected_targets = [
+        (ps.product.id, None, None, None, None),
+        (None, ps.id, None, None, None),
+        (None, None, sp.distribution.id, None, None),
+        (None, None, sp.distribution.id, None, sp.sourcepackagename.id),
+        (None, None, None, sp.distroseries.id, None),
+        (None, None, None, sp.distroseries.id, sp.sourcepackagename.id)
+        ]
+    return expected_targets
+
+
+class TestBugSummaryRebuild(TestCaseWithFactory):
+
+    layer = ZopelessDatabaseLayer
+
+    def test_get_existing_targets(self):
+        # get_existing_targets returns the set of target tuples that are
+        # currently represented in BugSummary.
+        orig_targets = get_existing_targets()
+        expected_targets = create_tasks(self.factory)
+        rollup_journal()
+        new_targets = get_existing_targets()
+        self.assertContentEqual(expected_targets, new_targets - orig_targets)
+
+    def test_get_expected_targets(self):
+        # get_expected_targets returns the set of target tuples that are
+        # currently represented in BugTask.
+        orig_targets = get_expected_targets()
+        expected_targets = create_tasks(self.factory)
+        new_targets = get_expected_targets()
+        self.assertContentEqual(expected_targets, new_targets - orig_targets)
