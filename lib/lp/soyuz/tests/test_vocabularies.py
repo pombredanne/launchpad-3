efe8cb088c3660442dda9ba3bad5c9f63dce884e
Steve Kowalik <steve.kowalik@canonical.com>
2012-06-20 08:57:09 +1000

    Fix PPAVocabulary with description == '' and actually add a test.

---
+++ lib/lp/soyuz/tests/test_vocabularies.py
@@ -0,0 +1,30 @@
+# Copyright 2012 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Test Soyuz vocabularies."""
+
+__metaclass__ = type
+
+from zope.schema.vocabulary import SimpleTerm
+
+from testtools.matchers import MatchesStructure
+
+from lp.soyuz.vocabularies import PPAVocabulary
+from lp.testing import TestCaseWithFactory
+from lp.testing.layers import DatabaseFunctionalLayer
+
+
+class TestPPAVocabulary(TestCaseWithFactory):
+
+    layer = DatabaseFunctionalLayer
+
+    def test_toTerm_empty_description(self):
+        archive = self.factory.makeArchive(description='')
+        vocab = PPAVocabulary()
+        token = '%s/%s' % (archive.owner.name, archive.name)
+        description = 'No description available'
+        term = vocab.toTerm(archive)
+        self.assertThat(term, MatchesStructure.byEquality(
+            value=archive,
+            token=token,
+            title=description))
