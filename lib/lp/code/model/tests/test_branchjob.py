e3027d6399fb971e71c013f9e7146e6bd8dbdcbe
Jelmer Vernooij <jelmer@samba.org>
2012-06-26 10:19:13 +0200

    Remove some magic from updateDBRevisions.

--- lib/lp/code/model/tests/test_branchjob.py
+++ lib/lp/code/model/tests/test_branchjob.py
@@ -380,7 +380,7 @@ class TestRevisionsAddedJob(TestCaseWithFactory):
         job = RevisionsAddedJob.create(branch, 'rev1', 'rev2', '')
         self.assertEqual([job], list(RevisionsAddedJob.iterReady()))
 
-    def updateDBRevisions(self, branch, bzr_branch, revision_ids=None):
+    def updateDBRevisions(self, branch, bzr_branch, revision_ids):
         """Update the database for the revisions.
 
         :param branch: The database branch associated with the revisions.
@@ -388,8 +388,6 @@ class TestRevisionsAddedJob(TestCaseWithFactory):
         :param revision_ids: The ids of the revisions to update.  If not
             supplied, the branch revision history is used.
         """
-        if revision_ids is None:
-            revision_ids = bzr_branch.revision_history()
         for bzr_revision in bzr_branch.repository.get_revisions(revision_ids):
             existing = branch.getBranchRevision(
                 revision_id=bzr_revision.revision_id)
@@ -777,7 +775,8 @@ class TestRevisionsAddedJob(TestCaseWithFactory):
                 committer="Joe Bloggs <joe@example.com>",
                 timestamp=1000100000.0, timezone=0)
         switch_dbuser('branchscanner')
-        self.updateDBRevisions(db_branch, tree.branch)
+        self.updateDBRevisions(db_branch, tree.branch,
+            tree.branch.revision_history())
         expected = (
             u"-" * 60 + '\n'
             "revno: 1" '\n'
@@ -820,7 +819,8 @@ class TestRevisionsAddedJob(TestCaseWithFactory):
                 committer=u"Non ASCII: \xed", timestamp=1000000000.0,
                 timezone=0)
         switch_dbuser('branchscanner')
-        self.updateDBRevisions(db_branch, tree.branch)
+        self.updateDBRevisions(db_branch, tree.branch,
+            tree.branch.revision_history())
         job = RevisionsAddedJob.create(db_branch, '', '', '')
         message = job.getRevisionMessage(rev_id, 1)
         # The revision message must be a unicode object.
