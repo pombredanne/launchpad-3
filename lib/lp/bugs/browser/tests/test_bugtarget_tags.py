ca977926167dfeb73a7d654691281860739f6bd0
Curtis Hovey <curtis.hovey@canonical.com>
2012-06-27 15:26:39 -0400

    Remove mixin class since the widget provides the script.

--- lib/lp/bugs/browser/tests/test_bugtarget_tags.py
+++ lib/lp/bugs/browser/tests/test_bugtarget_tags.py
@@ -3,13 +3,8 @@
 
 __metaclass__ = type
 
-from lp.bugs.browser.bugtask import BugTargetTagsMixin
 from lp.bugs.publisher import BugsLayer
-from lp.services.webapp.publisher import LaunchpadView
-from lp.testing import (
-    person_logged_in,
-    TestCaseWithFactory,
-    )
+from lp.testing import TestCaseWithFactory
 from lp.testing.layers import DatabaseFunctionalLayer
 from lp.testing.views import create_view
 
@@ -57,50 +52,3 @@ class TestBugTargetTags(TestCaseWithFactory):
         self.assertEqual(
             [u'tag-first', u'tag-middle', u'tag-last'],
             [tag['tag'] for tag in view.tags_cloud_data])
-
-
-class BugTargetTagsMixinTestCase(TestCaseWithFactory):
-
-    layer = DatabaseFunctionalLayer
-
-    class FakeBugTagsView(LaunchpadView, BugTargetTagsMixin):
-        """A test view."""
-
-    def test_official_tags_js_not_adaptable_to_product_or_distro(self):
-        # project groups are not full bug targets so they have no tags.
-        project_group = self.factory.makeProject()
-        view = self.FakeBugTagsView(project_group, None)
-        js = view.official_tags_js
-        self.assertEqual('var official_tags = [];', js)
-
-    def test_official_tags_js_product_without_tags(self):
-        # Products without tags have an empty list.
-        product = self.factory.makeProduct()
-        view = self.FakeBugTagsView(product, None)
-        js = view.official_tags_js
-        self.assertEqual('var official_tags = [];', js)
-
-    def test_official_tags_js_product_with_tags(self):
-        # Products with tags have a list of tags.
-        product = self.factory.makeProduct()
-        with person_logged_in(product.owner):
-            product.official_bug_tags = [u'cows', u'pigs', u'sheep']
-        view = self.FakeBugTagsView(product, None)
-        js = view.official_tags_js
-        self.assertEqual('var official_tags = ["cows", "pigs", "sheep"];', js)
-
-    def test_official_tags_js_distribution_without_tags(self):
-        # Distributions without tags have an empty list.
-        distribution = self.factory.makeDistribution()
-        view = self.FakeBugTagsView(distribution, None)
-        js = view.official_tags_js
-        self.assertEqual('var official_tags = [];', js)
-
-    def test_official_tags_js_distribution_with_tags(self):
-        # Distributions with tags have a list of tags.
-        distribution = self.factory.makeDistribution()
-        with person_logged_in(distribution.owner):
-            distribution.official_bug_tags = [u'cows', u'pigs', u'sheep']
-        view = self.FakeBugTagsView(distribution, None)
-        js = view.official_tags_js
-        self.assertEqual('var official_tags = ["cows", "pigs", "sheep"];', js)
