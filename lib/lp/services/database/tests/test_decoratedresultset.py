58594488f934b4ec7ac6557105428153d5943c66
Abel Deuring <abel.deuring@canonical.com>
2012-07-05 21:16:02 +0200

    correct docstring for test_decoratedresultset.py

--- lib/lp/services/database/tests/test_decoratedresultset.py
+++ lib/lp/services/database/tests/test_decoratedresultset.py
@@ -1,11 +1,7 @@
-# Copyright 2009 Canonical Ltd.  This software is licensed under the
+# Copyright 2012 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
-"""Test harness for running the buglinktarget.txt interface test
-
-This module will run the interface test against the CVE, Specification and
-Question implementations of that interface.
-"""
+"""Test harness for running decoratedresultset.txt."""
 
 __metaclass__ = type
 
