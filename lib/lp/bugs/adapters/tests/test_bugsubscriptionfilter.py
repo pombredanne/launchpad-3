1a08866281c3c9d802f9fd943d4f49c446983959
Curtis Hovey <curtis.hovey@canonical.com>
2012-07-20 11:47:42 -0400

    Extracted duplicate code to a common method.

--- lib/lp/bugs/adapters/tests/test_bugsubscriptionfilter.py
+++ lib/lp/bugs/adapters/tests/test_bugsubscriptionfilter.py
@@ -23,13 +23,17 @@ class BugSubscriptionFilterTestCase(TestCaseWithFactory):
 
     layer = DatabaseFunctionalLayer
 
-    def test_bugsubscriptionfilter_to_product_with_product(self):
-        product = self.factory.makeProduct()
-        subscriber = product.owner
+    def makeBugSubscriptionFilter(self, target):
+        subscriber = self.factory.makePerson()
         login_person(subscriber)
-        subscription = product.addBugSubscription(subscriber, subscriber)
+        subscription = target.addBugSubscription(subscriber, subscriber)
         subscription_filter = BugSubscriptionFilter()
         subscription_filter.structuralsubscription = subscription
+        return subscription_filter
+
+    def test_bugsubscriptionfilter_to_product_with_product(self):
+        product = self.factory.makeProduct()
+        subscription_filter = self.makeBugSubscriptionFilter(product)
         self.assertEqual(
             product, bugsubscriptionfilter_to_product(subscription_filter))
         self.assertEqual(product, IProduct(subscription_filter))
@@ -37,22 +41,14 @@ class BugSubscriptionFilterTestCase(TestCaseWithFactory):
     def test_bugsubscriptionfilter_to_product_with_productseries(self):
         product = self.factory.makeProduct()
         series = product.development_focus
-        subscriber = product.owner
-        login_person(subscriber)
-        subscription = series.addBugSubscription(subscriber, subscriber)
-        subscription_filter = BugSubscriptionFilter()
-        subscription_filter.structuralsubscription = subscription
+        subscription_filter = self.makeBugSubscriptionFilter(series)
         self.assertEqual(
             product, bugsubscriptionfilter_to_product(subscription_filter))
         self.assertEqual(product, IProduct(subscription_filter))
 
     def test_bugsubscriptionfilter_to_distribution_with_distribution(self):
         distribution = self.factory.makeDistribution()
-        subscriber = distribution.owner
-        login_person(subscriber)
-        subscription = distribution.addBugSubscription(subscriber, subscriber)
-        subscription_filter = BugSubscriptionFilter()
-        subscription_filter.structuralsubscription = subscription
+        subscription_filter = self.makeBugSubscriptionFilter(distribution)
         self.assertEqual(
             distribution,
             bugsubscriptionfilter_to_distribution(subscription_filter))
@@ -61,11 +57,7 @@ class BugSubscriptionFilterTestCase(TestCaseWithFactory):
     def test_bugsubscriptionfilter_to_distroseries_with_distribution(self):
         distribution = self.factory.makeDistribution()
         series = self.factory.makeDistroSeries(distribution=distribution)
-        subscriber = distribution.owner
-        login_person(subscriber)
-        subscription = series.addBugSubscription(subscriber, subscriber)
-        subscription_filter = BugSubscriptionFilter()
-        subscription_filter.structuralsubscription = subscription
+        subscription_filter = self.makeBugSubscriptionFilter(series)
         self.assertEqual(
             distribution,
             bugsubscriptionfilter_to_distribution(subscription_filter))
