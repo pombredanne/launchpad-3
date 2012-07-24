54db680e02add898a499980f5ce5387883006816
William Grant <william.grant@canonical.com>
2012-07-24 18:38:07 +1000

    Drop indexes and metadata from base unmodified patch so the MP diff makes sense.

--- database/schema/patch-2209-19-3.sql
+++ database/schema/patch-2209-19-3.sql
@@ -1,7 +1,3 @@
--- Copyright 2012 Canonical Ltd.  This software is licensed under the
--- GNU Affero General Public License version 3 (see the file LICENSE).
-SET client_min_messages=ERROR;
-
 CREATE OR REPLACE FUNCTION public.bug_summary_flush_temp_journal()
  RETURNS void
  LANGUAGE plpgsql
@@ -252,23 +248,3 @@ CREATE OR REPLACE VIEW combinedbugsummary AS
         bugsummaryjournal.has_patch, bugsummaryjournal.fixed_upstream,
         bugsummaryjournal.access_policy
     FROM bugsummaryjournal;
-
-DROP INDEX bugsummary__distribution__unique;
-DROP INDEX bugsummary__distroseries__unique;
-DROP INDEX bugsummary__product__unique;
-DROP INDEX bugsummary__productseries__unique;
-DROP INDEX bugsummary__distribution__idx;
-DROP INDEX bugsummary__distroseries__idx;
-DROP INDEX bugsummary__full__idx;
-DROP INDEX bugsummary__distribution_count__idx;
-DROP INDEX bugsummary__distribution_tag_count__idx;
-DROP INDEX bugsummary__tag_count__idx;
-ALTER INDEX bugsummary__distribution__idx2 RENAME TO bugsummary__distribution__idx;
-ALTER INDEX bugsummary__distroseries__idx2 RENAME TO bugsummary__distroseries__idx;
-ALTER INDEX bugsummary__distribution_count__idx2 RENAME TO bugsummary__distribution_count__idx;
-ALTER INDEX bugsummary__distroseries_count__idx2 RENAME TO bugsummary__distroseries_count__idx;
-ALTER INDEX bugsummary__distribution_tag_count__idx2 RENAME TO bugsummary__distribution_tag_count__idx;
-ALTER INDEX bugsummary__distroseries_tag_count__idx2 RENAME TO bugsummary__distroseries_tag_count__idx;
-ALTER INDEX bugsummary__full__idx2 RENAME TO bugsummary__full__idx;
-
-INSERT INTO LaunchpadDatabaseRevision VALUES (2209, 19, 3);
