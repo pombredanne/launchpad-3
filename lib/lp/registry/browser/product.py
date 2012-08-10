1eaf221f386223ef454ba4521c43be6b0ab12dde
William Grant <william.grant@canonical.com>
2012-08-10 16:38:56 +1000

    Drop branch_sharing_policy from Product:+admin. It's now API-only until we deploy it on +sharing.

--- lib/lp/registry/browser/product.py
+++ lib/lp/registry/browser/product.py
@@ -1574,9 +1574,6 @@ class ProductAdminView(ProductPrivateBugsMixin, ProductEditView,
         "private_bugs",
         ]
 
-    custom_widget(
-        'branch_sharing_policy', LaunchpadRadioWidgetWithDescription)
-
     @property
     def page_title(self):
         """The HTML page title."""
@@ -1594,8 +1591,6 @@ class ProductAdminView(ProductPrivateBugsMixin, ProductEditView,
         if not admin:
             self.field_names.remove('owner')
             self.field_names.remove('autoupdate')
-        if getFeatureFlag('disclosure.branch_sharing_policy.show_to_admin'):
-            self.field_names.append('branch_sharing_policy')
         super(ProductAdminView, self).setUpFields()
         self.form_fields = self._createAliasesField() + self.form_fields
         if admin:
