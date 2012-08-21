5665aad9c964f7ac2e35641f5e64e01991090daf
Ian Booth <ian.booth@canonical.com>
2012-08-21 17:24:45 +1000

    Rename team to person

--- database/schema/patch-2209-30-1.sql
+++ database/schema/patch-2209-30-1.sql
@@ -2,7 +2,7 @@
 -- GNU Affero General Public License version 3 (see the file LICENSE).
 SET client_min_messages=ERROR;
 
-ALTER TABLE accesspolicy ADD COLUMN team integer;
+ALTER TABLE accesspolicy ADD COLUMN person integer;
 
 ALTER TABLE accesspolicy ALTER COLUMN type DROP NOT NULL;
 
@@ -10,14 +10,14 @@ ALTER TABLE accesspolicy DROP CONSTRAINT has_target;
 
 ALTER TABLE accesspolicy ADD CONSTRAINT has_target
     CHECK ((((type is NOT NULL) AND (product IS NULL) <> (distribution IS NULL))
-    OR (team IS NOT NULL AND type is NULL)));
+    OR (person IS NOT NULL AND type is NULL)));
 
 ALTER TABLE ONLY accesspolicy
-    ADD CONSTRAINT accesspolicy_team_fkey FOREIGN KEY (team) REFERENCES person(id);
+    ADD CONSTRAINT accesspolicy_person_fkey FOREIGN KEY (person) REFERENCES person(id);
 
-CREATE UNIQUE INDEX accesspolicy__team__key ON accesspolicy USING btree (team) WHERE (team IS NOT NULL);
+CREATE UNIQUE INDEX accesspolicy__person__key ON accesspolicy USING btree (person) WHERE (person IS NOT NULL);
 
 COMMENT ON TABLE AccessPolicy IS 'An access policy used to manage a project, distribution or private team''s artifacts.';
-COMMENT ON COLUMN AccessPolicy.team IS 'The private team that this policy is used on.';
+COMMENT ON COLUMN AccessPolicy.person IS 'The private team that this policy is used on.';
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2209, 30, 1);
