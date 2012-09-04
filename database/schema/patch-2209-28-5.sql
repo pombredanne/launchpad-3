c8b383ac1a60efdbed79b09322d626887ee35259
Abel Deuring <abel.deuring@canonical.com>
2012-09-04 16:53:06 +0200

    typo fixed.

--- database/schema/patch-2209-28-5.sql
+++ database/schema/patch-2209-28-5.sql
@@ -2,7 +2,7 @@
 -- GNU Affero General Public License version 3 (see the file LICENSE).
 SET client_min_messages=ERROR;
 
-CREATE UNIQUE INDEX accessacrtifact__specification__key
+CREATE UNIQUE INDEX accessartifact__specification__key
     ON AccessArtifact(specification)  WHERE specification IS NOT NULL;
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2209, 28, 5);
