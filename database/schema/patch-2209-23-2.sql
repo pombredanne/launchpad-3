625532a036e8a17cebf6708b1c4a7a01994d8346
William Grant <william.grant@canonical.com>
2012-09-10 19:59:24 +1000

    Make the compound indices UNIQUE. The primary keys already are, but why not.

--- database/schema/patch-2209-23-2.sql
+++ database/schema/patch-2209-23-2.sql
@@ -3,12 +3,12 @@
 
 SET client_min_messages=ERROR;
 
-CREATE INDEX accesspolicygrant__grantee__policy__idx
+CREATE UNIQUE INDEX accesspolicygrant__grantee__policy__idx
     ON accesspolicygrant (grantee, policy);
 CREATE INDEX accesspolicygrant__grantor__idx
     ON accesspolicygrant (grantor);
 
-CREATE INDEX accessartifactgrant__grantee__artifact__idx
+CREATE UNIQUE INDEX accessartifactgrant__grantee__artifact__idx
     ON accessartifactgrant (grantee, artifact);
 CREATE INDEX accessartifactgrant__grantor__idx
     ON accessartifactgrant (grantor);
