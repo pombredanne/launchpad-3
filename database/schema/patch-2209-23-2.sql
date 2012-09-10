d5d71a7799e6ad4253d8942901ce030e75b40eaf
William Grant <william.grant@canonical.com>
2012-09-10 20:06:31 +1000

    __idx -> __key for the uniques.

--- database/schema/patch-2209-23-2.sql
+++ database/schema/patch-2209-23-2.sql
@@ -3,12 +3,12 @@
 
 SET client_min_messages=ERROR;
 
-CREATE UNIQUE INDEX accesspolicygrant__grantee__policy__idx
+CREATE UNIQUE INDEX accesspolicygrant__grantee__policy__key
     ON accesspolicygrant (grantee, policy);
 CREATE INDEX accesspolicygrant__grantor__idx
     ON accesspolicygrant (grantor);
 
-CREATE UNIQUE INDEX accessartifactgrant__grantee__artifact__idx
+CREATE UNIQUE INDEX accessartifactgrant__grantee__artifact__key
     ON accessartifactgrant (grantee, artifact);
 CREATE INDEX accessartifactgrant__grantor__idx
     ON accessartifactgrant (grantor);
