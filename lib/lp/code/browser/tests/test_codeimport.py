1fca167fd80d128316ffcaa41651786bac342ff2
Steve Kowalik <steve.kowalik@canonical.com>
2012-10-03 09:20:29 +1000

    Rewrite test for Branch:+edit-import

--- lib/lp/code/browser/tests/test_codeimport.py
+++ lib/lp/code/browser/tests/test_codeimport.py
@@ -1,4 +1,4 @@
-# Copyright 2009 Canonical Ltd.  This software is licensed under the
+# Copyright 2009-2012 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
 """Tests for the code import browser code."""
@@ -7,14 +7,20 @@ __metaclass__ = type
 
 import re
 
+from zope.security.interfaces import Unauthorized
+
 from lp.code.enums import RevisionControlSystems
 from lp.services.webapp import canonical_url
-from lp.testing import TestCaseWithFactory
+from lp.testing import (
+    person_logged_in,
+    TestCaseWithFactory,
+    )
 from lp.testing.layers import DatabaseFunctionalLayer
 from lp.testing.pages import (
     extract_text,
     find_tag_by_id,
     )
+from lp.testing.views import create_initialized_view
 
 
 class TestImportDetails(TestCaseWithFactory):
@@ -53,3 +59,11 @@ class TestImportDetails(TestCaseWithFactory):
         svn_details = find_tag_by_id(browser.contents, 'svn-import-details')
         self.assertSvnDetailsDisplayed(
             svn_details, RevisionControlSystems.SVN)
+
+    def test_branch_owner_of_import_forbidden(self):
+        # The branch owner is forbidden to edit an import.
+        cimport = self.factory.makeCodeImport()
+        with person_logged_in(cimport.branch.owner):
+            self.assertRaises(
+                Unauthorized, create_initialized_view, cimport.branch,
+                '+edit-import')
