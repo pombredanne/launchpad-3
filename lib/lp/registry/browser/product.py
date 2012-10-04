404ceb33e2082a2d594cf688c257e16c4a63446a
Deryck Hodge <deryck.hodge@canonical.com>
2012-10-03 21:13:21 -0500

    Move private_projects_flag variable that holds the name of the flag to a module-level constant that can be used throughout.

--- lib/lp/registry/browser/product.py
+++ lib/lp/registry/browser/product.py
@@ -204,6 +204,7 @@ from lp.translations.browser.customlanguagecode import (
 
 OR = ' OR '
 SPACE = ' '
+PRIVATE_PROJECTS_FLAG = 'disclosure.private_projects.enabled'
 
 
 class ProductNavigation(
@@ -1974,8 +1975,7 @@ class ProjectAddStepTwo(StepView, ProductLicenseMixin, ReturnToReferrerMixin):
         hidden_names = ['__visited_steps__', 'license_info']
         hidden_fields = self.form_fields.select(*hidden_names)
 
-        private_projects_flag = 'disclosure.private_projects.enabled'
-        private_projects = bool(getFeatureFlag(private_projects_flag))
+        private_projects = bool(getFeatureFlag(PRIVATE_PROJECTS_FLAG))
         if not private_projects or not IProductSet.providedBy(self.context):
             hidden_names.extend([
                 'information_type', 'bug_supervisor', 'driver'])
@@ -2018,8 +2018,7 @@ class ProjectAddStepTwo(StepView, ProductLicenseMixin, ReturnToReferrerMixin):
         self.widgets['source_package_name'].visible = False
         self.widgets['distroseries'].visible = False
 
-        private_projects_flag = 'disclosure.private_projects.enabled'
-        private_projects = bool(getFeatureFlag(private_projects_flag))
+        private_projects = bool(getFeatureFlag(PRIVATE_PROJECTS_FLAG))
 
         if private_projects and IProductSet.providedBy(self.context):
             self.widgets['information_type'].value = InformationType.PUBLIC
@@ -2091,8 +2090,7 @@ class ProjectAddStepTwo(StepView, ProductLicenseMixin, ReturnToReferrerMixin):
             for error in errors:
                 self.errors.remove(error)
 
-        private_projects_flag = 'disclosure.private_projects.enabled'
-        private_projects = bool(getFeatureFlag(private_projects_flag))
+        private_projects = bool(getFeatureFlag(PRIVATE_PROJECTS_FLAG))
         if private_projects:
             if data.get('information_type') != InformationType.PUBLIC:
                 for required_field in ('bug_supervisor', 'driver'):
@@ -2120,8 +2118,7 @@ class ProjectAddStepTwo(StepView, ProductLicenseMixin, ReturnToReferrerMixin):
             owner = data.get('owner')
 
         information_type = InformationType.PUBLIC
-        private_projects_flag = 'disclosure.private_projects.enabled'
-        private_projects = bool(getFeatureFlag(private_projects_flag))
+        private_projects = bool(getFeatureFlag(PRIVATE_PROJECTS_FLAG))
         if private_projects:
             information_type = data.get('information_type')
 
