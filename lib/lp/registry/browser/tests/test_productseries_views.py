9cdd7aa9938ab463e53532543b14a8db4defbcae
Deryck Hodge <deryck.hodge@canonical.com>
2012-10-07 14:54:24 -0500

    Add a series of tests for ProductSeries to ensure information_type is on the view and that we have a privacy portlet.

--- lib/lp/registry/browser/tests/test_productseries_views.py
+++ lib/lp/registry/browser/tests/test_productseries_views.py
@@ -7,7 +7,9 @@ __metaclass__ = type
 
 
 import soupmatchers
+from zope.security.proxy import removeSecurityProxy
 
+from lp.app.enums import InformationType
 from lp.bugs.interfaces.bugtask import (
     BugTaskStatus,
     BugTaskStatusSearch,
@@ -20,6 +22,57 @@ from lp.testing import (
 from lp.testing.layers import DatabaseFunctionalLayer
 from lp.testing.matchers import Contains
 from lp.testing.views import create_initialized_view
+
+
+class TestProductSeries(BrowserTestCase):
+
+    layer = DatabaseFunctionalLayer
+
+    def test_information_type_public(self):
+        # A ProductSeries view should include its information_type,
+        # which defaults to Public for new projects.
+        series = self.factory.makeProductSeries()
+        view = create_initialized_view(series, '+index')
+        self.assertEqual('Public', view.information_type)
+
+    def test_information_type_proprietary(self):
+        # A ProductSeries view should get its information_type
+        # from the related product even if the product is changed to
+        # PROPRIETARY.
+        product = self.factory.makeProduct()
+        self.factory.makeCommercialSubscription(product)
+        information_type = InformationType.PROPRIETARY
+        removeSecurityProxy(product).information_type = information_type
+        series = self.factory.makeProductSeries(product=product)
+        view = create_initialized_view(series, '+index')
+        self.assertEqual('Proprietary', view.information_type)
+
+    def test_privacy_portlet(self):
+        # A ProductSeries page should include a privacy portlet that
+        # accurately describes the information_type.
+        owner = self.factory.makePerson()
+        product = self.factory.makeProduct(owner=owner)
+        self.factory.makeCommercialSubscription(product)
+        information_type = InformationType.PROPRIETARY
+        removeSecurityProxy(product).information_type = information_type
+        series = self.factory.makeProductSeries(product=product)
+        policy = self.factory.makeAccessPolicy(pillar=product)
+        grant = self.factory.makeAccessPolicyGrant(
+            policy=policy, grantee=owner)
+        privacy_portlet = soupmatchers.Tag(
+            'info-type-portlet', 'span',
+            attrs={'id': 'information-type-summary'})
+        privacy_portlet_proprietary = soupmatchers.Tag(
+            'info-type-text', 'strong', attrs={'id': 'information-type'},
+            text='Proprietary')
+        browser = self.getViewBrowser(series, '+index', user=owner)
+        # First, assert that the portlet exists.
+        self.assertThat(
+            browser.contents, soupmatchers.HTMLContains(privacy_portlet))
+        # Then, assert that the text displayed matches the information_type.
+        self.assertThat(
+            browser.contents, soupmatchers.HTMLContains(
+            privacy_portlet_proprietary))
 
 
 class TestProductSeriesHelp(TestCaseWithFactory):
