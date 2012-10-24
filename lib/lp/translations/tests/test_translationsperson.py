6eb1ef3d3cf316308259b84c4d0310880816f3c0
j.c.sackett <jcsackett@canonical.com>
2012-10-24 13:54:40 +0000

    Test.

--- lib/lp/translations/tests/test_translationsperson.py
+++ lib/lp/translations/tests/test_translationsperson.py
@@ -5,8 +5,12 @@
 
 __metaclass__ = type
 
+from lp.app.enums import ServiceUsage
 from lp.services.webapp.testing import verifyObject
-from lp.testing import TestCaseWithFactory
+from lp.testing import (
+    person_logged_in,
+    TestCaseWithFactory,
+    )
 from lp.testing.layers import DatabaseFunctionalLayer
 from lp.translations.interfaces.translationsperson import ITranslationsPerson
 
@@ -24,3 +28,18 @@ class TestTranslationsPerson(TestCaseWithFactory):
         self.assertFalse(translationsperson.hasTranslated())
         self.factory.makeSuggestion(translator=person)
         self.assertTrue(translationsperson.hasTranslated())
+
+    def test_translation_history_inactive_projects(self):
+        # The translation history doesn't include projects that don't use
+        # rosetta.
+        person = self.factory.makePerson()
+        product = self.factory.makeProduct()
+        series = self.factory.makeProductSeries(product=product)
+        template = self.factory.makePOTemplate(productseries=series)
+        pofile = self.factory.makePOFile(potemplate=template)
+        self.factory.makeSuggestion(translator=person, pofile=pofile)
+        with person_logged_in(product.owner):
+            product.translations_usage = ServiceUsage.NOT_APPLICABLE
+        translationsperson = ITranslationsPerson(person)
+        history = translationsperson.translation_history
+        self.assertTrue(history.is_empty()) 
