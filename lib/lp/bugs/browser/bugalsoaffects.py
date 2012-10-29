6756bd532aab54c5d7ee1aa433952a48af7418a8
Ian Booth <ian.booth@canonical.com>
2012-10-29 20:03:05 +1000

    Load a different cached property

--- lib/lp/bugs/browser/bugalsoaffects.py
+++ lib/lp/bugs/browser/bugalsoaffects.py
@@ -451,8 +451,8 @@ class DistroBugTaskCreationStep(BugTaskCreationStep):
                 validate_new_target(
                     self.context.bug, target, check_source_package=False)
                 if sourcepackagename:
-                    # Force dsp.sourcepackagename to be loaded now not later.
-                    spn = target.sourcepackagename
+                    # Force dsp lazy properties to be loaded now not later.
+                    name = target.name
                     data['sourcepackagename'] = target
             except IllegalTarget as e:
                 if sourcepackagename:
