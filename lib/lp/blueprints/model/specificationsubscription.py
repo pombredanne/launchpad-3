13ccc50bc95aeb697a7a396b75e8a411ca0f6ce1
Abel Deuring <abel.deuring@canonical.com>
2012-11-20 15:04:26 +0100

    create an artifact grant when a user is subscribed to a private specification

--- lib/lp/blueprints/model/specificationsubscription.py
+++ lib/lp/blueprints/model/specificationsubscription.py
@@ -11,11 +11,17 @@ from sqlobject import (
     BoolCol,
     ForeignKey,
     )
+from zope.component import getUtility
 from zope.interface import implements
 
 from lp.blueprints.interfaces.specificationsubscription import (
     ISpecificationSubscription,
     )
+from lp.registry.interfaces.accesspolicy import (
+    IAccessArtifactGrantSource,
+    IAccessArtifactSource,
+    )
+from lp.registry.interfaces.role import IPersonRoles
 from lp.registry.interfaces.person import validate_person
 from lp.services.database.sqlbase import SQLBase
 
@@ -37,6 +43,26 @@ class SpecificationSubscription(SQLBase):
         """See `ISpecificationSubscription`."""
         if user is None:
             return False
-        if self.person.is_team:
-            return user.inTeam(self.person)
-        return user == self.person
+        if not IPersonRoles.providedBy(user):
+            user = IPersonRoles(user)
+        if (
+            user.inTeam(self.specification.owner) or
+            user.inTeam(self.person) or
+            user.in_admin):
+            return True
+        # People who subscribed users should be able to unsubscribe
+        # them again, similar to branch subscriptions. This is
+        # essential if somebody was erroneuosly subscribed to a
+        # proprietary or embargoed specification. Unfortunately,
+        # SpecificationSubscription does not record who subscribed
+        # somebody else, but if the specification is private, we can
+        # check who issued the artifact grant.
+        artifacts = getUtility(IAccessArtifactSource).find(
+            [self.specification])
+        wanted = [(artifact, self.person) for artifact in artifacts]
+        if len(wanted) == 0:
+            return False
+        for grant in getUtility(IAccessArtifactGrantSource).find(wanted):
+            if user.inTeam(grant.grantor):
+                return True
+        return False
