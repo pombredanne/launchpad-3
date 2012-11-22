c3cbd9fcfe03853476c252d48879778172546e95
Curtis Hovey <curtis.hovey@canonical.com>
2012-11-22 08:30:16 -0500

    Used db transaction decorators.

--- lib/lp/registry/scripts/productreleasefinder/finder.py
+++ lib/lp/registry/scripts/productreleasefinder/finder.py
@@ -32,6 +32,10 @@ from lp.registry.model.productrelease import (
     )
 from lp.registry.scripts.productreleasefinder.filter import FilterPattern
 from lp.registry.scripts.productreleasefinder.hose import Hose
+from lp.services.database import (
+    read_transaction,
+    write_transaction,
+    )
 from lp.services.database.lpstorm import IStore
 from lp.services.librarian.model import LibraryFileAlias
 
@@ -103,13 +107,13 @@ class ProductReleaseFinder:
         for product_name, filters in self.getFilters():
             self.handleProduct(product_name, filters)
 
+    @read_transaction
     def getFilters(self):
         """Build the list of products and filters.
 
         Returns a list of (product_name, filters) for each product in
         the database, where the filter keys are series names.
         """
-        self.ztm.begin()
         found_globs = IStore(Product).find(
             (Product.name, ProductSeries.name, ProductSeries.releasefileglob),
             Product.id == ProductSeries.productID,
@@ -127,7 +131,6 @@ class ProductReleaseFinder:
             last_product = product_name
             filter_pattern = FilterPattern(series_name, glob)
             products_with_filters[product_name].append(filter_pattern)
-        self.ztm.abort()
         return products_with_filters.items()
 
     def handleProduct(self, product_name, filters):
@@ -150,9 +153,9 @@ class ProductReleaseFinder:
                 self.log.debug("File in %s found that matched no glob: %s",
                                product_name, url)
 
+    @read_transaction
     def getReleaseFileNames(self, product_name):
         """Return a set of all current release file names for the product."""
-        self.ztm.begin()
         found_names = IStore(Product).find(
             LibraryFileAlias.filename,
             Product.name == product_name,
@@ -163,40 +166,30 @@ class ProductReleaseFinder:
             LibraryFileAlias.id == ProductReleaseFile.libraryfileID
             )
         file_names = set(found_names)
-        self.ztm.abort()
         return file_names
 
+    @write_transaction
     def addReleaseTarball(self, product_name, series_name, release_name,
                           filename, size, file, content_type):
         """Create a ProductRelease (if needed), and attach tarball"""
-        self.ztm.begin()
-        try:
-            product = getUtility(IProductSet).getByName(product_name)
-            # XXX: This might match a milestone on a product series that was
-            # not intended, since product release used to have unique
-            # names per product series, but are now dependent on the milestone
-            # name which is unique per product. The series_name method
-            # parameter can be removed.
-            milestone = product.getMilestone(release_name)
-            if milestone is None:
-                series = product.getSeries(series_name)
-                milestone = series.newMilestone(release_name)
-                # Normally, a milestone is deactived when that version is
-                # released. This is only safe to do in an automated script
-                # if we are not using a pre-existing milestone.
-                milestone.active = False
-            release = milestone.product_release
-            if release is None:
-                release = milestone.createProductRelease(
-                    owner=product.owner, datereleased=datetime.now(pytz.UTC))
-                self.log.info("Created new release %s for %s/%s",
-                              release_name, product_name, series_name)
-            release.addReleaseFile(
-                filename, file, content_type, uploader=product.owner)
-            self.ztm.commit()
-        except:
-            self.ztm.abort()
-            raise
+        product = getUtility(IProductSet).getByName(product_name)
+        # This can match a milestone on a differnt series than the
+        # one passed, since milestones (and their releases) are unique
+        # to a product.
+        milestone = product.getMilestone(release_name)
+        if milestone is None:
+            series = product.getSeries(series_name)
+            milestone = series.newMilestone(release_name)
+            # Deactivate the milestone since a user did not create it.
+            milestone.active = False
+        release = milestone.product_release
+        if release is None:
+            release = milestone.createProductRelease(
+                owner=product.owner, datereleased=datetime.now(pytz.UTC))
+            self.log.info("Created new release %s for %s/%s",
+                          release_name, product_name, series_name)
+        release.addReleaseFile(
+            filename, file, content_type, uploader=product.owner)
 
     def handleRelease(self, product_name, series_name, url, file_names):
         """If the given URL looks like a release tarball, download it
