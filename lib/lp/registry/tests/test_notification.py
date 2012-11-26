f0aadbf9bd7498121aa258cea59bcf2bace384e1
Curtis Hovey <curtis.hovey@canonical.com>
2012-11-26 11:10:22 -0500

    Updated the test to verify the headers.

--- lib/lp/registry/tests/test_notification.py
+++ lib/lp/registry/tests/test_notification.py
@@ -1,11 +1,11 @@
 # Copyright 2012 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
-"""Test subscruber classes and functions."""
+"""Test notification classes and functions."""
 
 __metaclass__ = type
 
-from lp.registry.mail.notifications import send_direct_contact_email
+from lp.registry.mail.notification import send_direct_contact_email
 from lp.services.mail.notificationrecipientset import NotificationRecipientSet
 from lp.testing import (
     person_logged_in,
@@ -20,8 +20,8 @@ class SendDirectContactEmailTestCase(TestCaseWithFactory):
     layer = DatabaseFunctionalLayer
 
     def test_send_success(self):
-        sender = self.factory.makePerson(email='me@eg.dom')
-        user = self.factory.makePerson(email='him@eg.dom')
+        sender = self.factory.makePerson(email='me@eg.dom', name='me')
+        user = self.factory.makePerson(email='him@eg.dom', name='him')
         subject = 'test subject'
         body = 'test body'
         recipients_set = NotificationRecipientSet()
@@ -31,8 +31,8 @@ class SendDirectContactEmailTestCase(TestCaseWithFactory):
         notifications = pop_notifications()
         notification = notifications[0]
         self.assertEqual(1, len(notifications))
-        self.assertEqual('me@eg.dom', notification['From'])
-        self.assertEqual('him@eg.dom', notification['To'])
+        self.assertEqual('Me <me@eg.dom>', notification['From'])
+        self.assertEqual('Him <him@eg.dom>', notification['To'])
         self.assertEqual(subject, notification['Subject'])
         self.assertEqual(
             'test rationale', notification['X-Launchpad-Message-Rationale'])
