42644f3da596dc79c3a184027539c49064b817c6
Curtis Hovey <curtis.hovey@canonical.com>
2012-11-26 11:05:55 -0500

    added rudimentary test for send_direct_contact_email.

---
+++ lib/lp/registry/tests/test_notification.py
@@ -0,0 +1,39 @@
+# Copyright 2012 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""Test subscruber classes and functions."""
+
+__metaclass__ = type
+
+from lp.registry.mail.notifications import send_direct_contact_email
+from lp.services.mail.notificationrecipientset import NotificationRecipientSet
+from lp.testing import (
+    person_logged_in,
+    TestCaseWithFactory,
+    )
+from lp.testing.layers import DatabaseFunctionalLayer
+from lp.testing.mail_helpers import pop_notifications
+
+
+class SendDirectContactEmailTestCase(TestCaseWithFactory):
+
+    layer = DatabaseFunctionalLayer
+
+    def test_send_success(self):
+        sender = self.factory.makePerson(email='me@eg.dom')
+        user = self.factory.makePerson(email='him@eg.dom')
+        subject = 'test subject'
+        body = 'test body'
+        recipients_set = NotificationRecipientSet()
+        recipients_set.add(user, 'test reason', 'test rationale')
+        pop_notifications()
+        send_direct_contact_email('me@eg.dom', recipients_set, subject, body)
+        notifications = pop_notifications()
+        notification = notifications[0]
+        self.assertEqual(1, len(notifications))
+        self.assertEqual('me@eg.dom', notification['From'])
+        self.assertEqual('him@eg.dom', notification['To'])
+        self.assertEqual(subject, notification['Subject'])
+        self.assertEqual(
+            'test rationale', notification['X-Launchpad-Message-Rationale'])
+        self.assertTrue('launchpad' in notification['Message-ID'])
