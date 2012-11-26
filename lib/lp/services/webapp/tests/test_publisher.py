f8f1144e5202a2922ad9349e79d9dbf4f4805f89
Curtis Hovey <curtis.hovey@canonical.com>
2012-11-26 09:33:03 -0500

    Added a comprehensive test for _isRedirected()

--- lib/lp/services/webapp/tests/test_publisher.py
+++ lib/lp/services/webapp/tests/test_publisher.py
@@ -140,6 +140,16 @@ class TestLaunchpadView(TestCaseWithFactory):
         self.assertIsCanada(json_dict['context'])
         self.assertIn('my_bool', json_dict)
 
+    def test_isRedirected_status_codes(self):
+        request = LaunchpadTestRequest()
+        view = LaunchpadView(object(), request)
+        for code in [301, 302, 303, 307]:
+            request.response.setStatus(code)
+            self.assertTrue(view._isRedirected())
+        for code in [100, 200, 403, 404, 500]:
+            request.response.setStatus(code)
+            self.assertFalse(view._isRedirected())
+
     def test_related_feature_info__default(self):
         # By default, LaunchpadView.related_feature_info is empty.
         request = LaunchpadTestRequest()
