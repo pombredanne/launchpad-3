7efbf47cdc4b3a451683a78b8d48f9fc26858dea
Curtis Hovey <curtis.hovey@canonical.com>
2012-12-04 16:49:48 -0500

    Completed the tests for account status transition.

--- lib/lp/services/identity/tests/test_account.py
+++ lib/lp/services/identity/tests/test_account.py
@@ -35,17 +35,50 @@ class TestAccount(TestCaseWithFactory):
         ignore, displayname, status_1, status_2 = repr(distro).rsplit(' ', 3)
         self.assertEqual("'\\u0170-account'", displayname)
 
-    def test_status_from_noccount(self):
-        # The status may change from NOACCOUNT to ACTIVE.
-        account = self.factory.makeAccount(status=AccountStatus.NOACCOUNT)
-        login_celebrity('admin')
-        for status in [AccountStatus.DEACTIVATED, AccountStatus.SUSPENDED]:
+    def assertCannotTransition(self, account, statuses):
+        for status in statuses:
             self.assertFalse(
                 can_transition_to_account_status(account.status, status))
             self.assertRaises(
                 AccountStatusError, setattr, account, 'status', status)
-        self.assertTrue(
-            can_transition_to_account_status(
-                account.status, AccountStatus.ACTIVE))
-        account.status = AccountStatus.ACTIVE
-        self.assertEqual(AccountStatus.ACTIVE, account.status)
+
+    def assertCanTransition(self, account, statuses):
+        for status in statuses:
+            self.assertTrue(
+                can_transition_to_account_status(account.status, status))
+        account.status = status
+        self.assertEqual(status, account.status)
+
+    def test_status_from_noaccount(self):
+        # The status may change from NOACCOUNT to ACTIVE.
+        account = self.factory.makeAccount(status=AccountStatus.NOACCOUNT)
+        login_celebrity('admin')
+        self.assertCannotTransition(
+            account, [AccountStatus.DEACTIVATED, AccountStatus.SUSPENDED])
+        self.assertCanTransition(account, [AccountStatus.ACTIVE])
+
+    def test_status_from_active(self):
+        # The status may change from ACTIVE to DEACTIVATED or SUSPENDED.
+        account = self.factory.makeAccount(status=AccountStatus.ACTIVE)
+        login_celebrity('admin')
+        self.assertCannotTransition(account, [AccountStatus.NOACCOUNT])
+        self.assertCanTransition(
+            account, [AccountStatus.DEACTIVATED, AccountStatus.SUSPENDED])
+
+    def test_status_from_deactivated(self):
+        # The status may change from DEACTIVATED to ACTIVATED.
+        account = self.factory.makeAccount()
+        login_celebrity('admin')
+        account.status = AccountStatus.DEACTIVATED
+        self.assertCannotTransition(
+            account, [AccountStatus.NOACCOUNT, AccountStatus.SUSPENDED])
+        self.assertCanTransition(account, [AccountStatus.ACTIVE])
+
+    def test_status_from_suspended(self):
+        # The status may change from SUSPENDED to DEACTIVATED.
+        account = self.factory.makeAccount()
+        login_celebrity('admin')
+        account.status = AccountStatus.SUSPENDED
+        self.assertCannotTransition(
+            account, [AccountStatus.NOACCOUNT, AccountStatus.ACTIVE])
+        self.assertCanTransition(account, [AccountStatus.DEACTIVATED])
