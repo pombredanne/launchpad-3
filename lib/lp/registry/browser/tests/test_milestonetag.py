66cb94aeaafed2eee488c4b382442c7324fd9542
j.c.sackett <jcsackett@canonical.com>
2012-12-11 14:50:56 +0000

    Shut up, lint.

--- lib/lp/registry/browser/tests/test_milestonetag.py
+++ lib/lp/registry/browser/tests/test_milestonetag.py
@@ -5,23 +5,18 @@
 
 __metaclass__ = type
 
-from lp.registry.model.milestonetag import ProjectGroupMilestoneTag
 from lp.services.webapp import canonical_url
 from lp.testing import TestCaseWithFactory
 from lp.testing.layers import DatabaseFunctionalLayer
-from lp.testing.views import create_view
+
 
 class TestProjectMilestoneTagView(TestCaseWithFactory):
 
     layer = DatabaseFunctionalLayer
 
     def test_projectgroup_milestone(self):
-        # The projectgroup milestone index page loads without errors.
+        # The projectgroup milestone tag page loads without errors.
         group = self.factory.makeProject()
-        #product = self.factory.makeProduct(project=group)
-        #milestone = self.factory.makeMilestone(product=product)
-        #project_milestone = group.all_milestones[0]
-        #view = create_view(project_milestone, name="+index")
         url = canonical_url(group) + "/+tags/fab/+index"
         browser = self.getUserBrowser(url=url)
         self.assertTrue('fab' in browser.contents)
