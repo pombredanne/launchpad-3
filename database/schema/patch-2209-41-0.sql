962f1fca8da41a519d8a4d7fa497563c525f6f9b
William Grant <william.grant@canonical.com>
2013-01-03 17:04:12 +1100

    DB patch 2209-41-0 to flatten BFJ/PB schemas into BPB/SPRB/TTB.

---
+++ database/schema/patch-2209-41-0.sql
@@ -0,0 +1,52 @@
+-- Copyright 2013 Canonical Ltd.  This software is licensed under the
+-- GNU Affero General Public License version 3 (see the file LICENSE).
+
+SET client_min_messages=ERROR;
+
+ALTER TABLE binarypackagebuild
+    ADD COLUMN archive integer REFERENCES archive,
+    ADD COLUMN pocket integer,
+    ADD COLUMN processor integer REFERENCES processor,
+    ADD COLUMN virtualized boolean,
+    ADD COLUMN date_created timestamp without time zone,
+    ADD COLUMN date_started timestamp without time zone,
+    ADD COLUMN date_finished timestamp without time zone,
+    ADD COLUMN date_first_dispatched timestamp without time zone,
+    ADD COLUMN builder integer REFERENCES builder,
+    ADD COLUMN status integer,
+    ADD COLUMN log integer REFERENCES libraryfilealias,
+    ADD COLUMN upload_log integer REFERENCES libraryfilealias,
+    ADD COLUMN dependencies text,
+    ADD COLUMN failure_count integer;
+
+ALTER TABLE sourcepackagerecipebuild
+    ADD COLUMN archive integer REFERENCES archive,
+    ADD COLUMN pocket integer,
+    ADD COLUMN processor integer REFERENCES processor,
+    ADD COLUMN virtualized boolean,
+    ADD COLUMN date_created timestamp without time zone,
+    ADD COLUMN date_started timestamp without time zone,
+    ADD COLUMN date_finished timestamp without time zone,
+    ADD COLUMN date_first_dispatched timestamp without time zone,
+    ADD COLUMN builder integer REFERENCES builder,
+    ADD COLUMN status integer,
+    ADD COLUMN log integer REFERENCES libraryfilealias,
+    ADD COLUMN upload_log integer REFERENCES libraryfilealias,
+    ADD COLUMN dependencies text,
+    ADD COLUMN failure_count integer;
+
+ALTER TABLE translationtemplatesbuild
+    ADD COLUMN processor integer REFERENCES processor,
+    ADD COLUMN virtualized boolean,
+    ADD COLUMN date_created timestamp without time zone,
+    ADD COLUMN date_started timestamp without time zone,
+    ADD COLUMN date_finished timestamp without time zone,
+    ADD COLUMN date_first_dispatched timestamp without time zone,
+    ADD COLUMN builder integer REFERENCES builder,
+    ADD COLUMN status integer,
+    ADD COLUMN log integer REFERENCES libraryfilealias,
+    ADD COLUMN failure_count integer;
+
+-- TODO: Indices
+
+INSERT INTO LaunchpadDatabaseRevision VALUES (2209, 41, 0);
