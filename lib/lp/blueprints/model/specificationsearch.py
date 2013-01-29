e5b7f9eb0163b63c6f860c9f1ad1067098aedc7a
Steve Kowalik <steve.kowalik@canonical.com>
2013-01-29 14:39:16 +1100

    Unnest _preload_specification_related_people.

--- lib/lp/blueprints/model/specificationsearch.py
+++ lib/lp/blueprints/model/specificationsearch.py
@@ -108,7 +108,7 @@ def search_specifications(context, base_clauses, user, sort=None,
     if user is not None and not IPersonRoles(user).in_admin:
         decorators.append(_make_cache_user_can_view_spec(user))
     if prejoin_people:
-        decorators.append(_preload_specification_people())
+        decorators.append(_preload_specification_related_people)
     results = store.using(*tables).find(
         Specification, *clauses).order_by(*order).config(limit=quantity)
     return DecoratedResultSet(
@@ -223,14 +223,11 @@ def _make_cache_user_can_view_spec(user):
     return cache_user_can_view_spec
 
 
-def _preload_specification_people():
-
-    def preload_specification_related_people(spec):
-        list(getUtility(IPersonSet).getPrecachedPersonsFromIDs(
-            [spec._assigneeID, spec._approverID, spec._drafterID],
-            need_validity=True))
-        return spec
-    return preload_specification_related_people
+def _preload_specification_related_people(spec):
+    list(getUtility(IPersonSet).getPrecachedPersonsFromIDs(
+        [spec._assigneeID, spec._approverID, spec._drafterID],
+        need_validity=True))
+    return spec
 
 
 def get_specification_started_clause():
