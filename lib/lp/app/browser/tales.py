6d1b854c73641e1974a5795b59dbe7d92a5b4b49
Colin Watson <cjwatson@canonical.com>
2013-02-13 23:00:21 +0000

    Remove unused productseries-macros.pt, and related code that existed only for its benefit.

--- lib/lp/app/browser/tales.py
+++ lib/lp/app/browser/tales.py
@@ -1618,7 +1618,7 @@ class BranchFormatterAPI(ObjectFormatterAPI):
     """Adapter for IBranch objects to a formatted string."""
 
     traversable_names = {
-        'link': 'link', 'url': 'url', 'project-link': 'projectLink',
+        'link': 'link', 'url': 'url',
         'title-link': 'titleLink', 'bzr-link': 'bzrLink',
         'api_url': 'api_url'}
 
@@ -1643,14 +1643,6 @@ class BranchFormatterAPI(ObjectFormatterAPI):
         """A hyperlinked branch icon with the bazaar identity."""
         # Defer to link.
         return self.link(view_name)
-
-    def projectLink(self, view_name):
-        """A hyperlinked branch icon with the name and title."""
-        return structured(
-            '<a href="%(url)s" title="%(display_name)s">'
-            '<img src="/@@/branch" alt=""/>'
-            '&nbsp;%(name)s</a>: %(title)s',
-            **self._args(view_name)).escapedtext
 
     def titleLink(self, view_name):
         """A hyperlinked branch name with following title."""
