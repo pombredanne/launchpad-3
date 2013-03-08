c4e7b356d9d78e5067df8d9e88c7cc2d4615f516
Colin Watson <cjwatson@canonical.com>
2013-03-08 22:17:31 +0000

    Update copyright.

--- lib/lp/testing/factory.py
+++ lib/lp/testing/factory.py
@@ -2,7 +2,7 @@
 # NOTE: The first line above must stay first; do not move the copyright
 # notice to the top.  See http://www.python.org/dev/peps/pep-0263/.
 #
-# Copyright 2009-2012 Canonical Ltd.  This software is licensed under the
+# Copyright 2009-2013 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
 """Testing infrastructure for the Launchpad application.
