bf587f13c6564ca5856808c981450784707b8a8b
Steve Kowalik <steve.kowalik@canonical.com>
2013-05-10 04:23:44 +0000

    Drop the NOT NULL from 44-1, it requires FDT.

--- database/schema/patch-2209-44-1.sql
+++ database/schema/patch-2209-44-1.sql
@@ -5,7 +5,5 @@ SET client_min_messages=ERROR;
 
 CREATE INDEX previewdiff__branch_merge_proposal__date_created__idx
     ON previewdiff (branch_merge_proposal, date_created);
-ALTER TABLE previewdiff ALTER COLUMN branch_merge_proposal SET NOT NULL,
-                        ALTER COLUMN date_created SET NOT NULL;
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2209, 44, 1);
