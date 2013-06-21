4a093d0abc8d94beca8a7280a45eeaa536cd2a17
Colin Watson <cjwatson@canonical.com>
2013-06-21 10:36:02 +0100

    Update copyright.

--- lib/lp/soyuz/adapters/tests/test_overrides.py
+++ lib/lp/soyuz/adapters/tests/test_overrides.py
@@ -1,4 +1,4 @@
-# Copyright 2011 Canonical Ltd.  This software is licensed under the
+# Copyright 2011-2013 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
 """Test generic override policy classes."""
