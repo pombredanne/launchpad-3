890da40da0c3c9edd726c249ab3d284eb943dd04
William Grant <william.grant@canonical.com>
2013-10-31 14:48:37 +1100

    Add DB bits for BuildQueue.{status,build_farm_job} to prepare for BuildFarmJobOld's demolition.

---
+++ database/schema/patch-2209-51-0.sql
@@ -0,0 +1,13 @@
+-- Copyright 2013 Canonical Ltd.  This software is licensed under the
+-- GNU Affero General Public License version 3 (see the file LICENSE).
+
+SET client_min_messages=ERROR;
+
+ALTER TABLE buildqueue
+    ADD COLUMN build_farm_job integer REFERENCES buildfarmjob,
+    ADD COLUMN status integer;
+
+ALTER TABLE buildqueue ALTER COLUMN job DROP NOT NULL;
+ALTER TABLE buildqueue ALTER COLUMN job_type DROP NOT NULL;
+
+INSERT INTO LaunchpadDatabaseRevision VALUES (2209, 51, 0);
