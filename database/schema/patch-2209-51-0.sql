2e28479012ac1cc6ef49236621bc8448fec95089
William Grant <william.grant@canonical.com>
2013-11-21 12:09:06 +1100

    Name the BuildQueue.build_farm_job FK constraint, and add a unique index.

--- database/schema/patch-2209-51-0.sql
+++ database/schema/patch-2209-51-0.sql
@@ -4,11 +4,13 @@
 SET client_min_messages=ERROR;
 
 ALTER TABLE buildqueue
-    ADD COLUMN build_farm_job integer REFERENCES buildfarmjob,
+    ADD COLUMN build_farm_job integer
+        CONSTRAINT buildqueue__build_farm_job__fk REFERENCES buildfarmjob,
     ADD COLUMN status integer,
-    ADD COLUMN date_started timestamp without time zone;
-
-ALTER TABLE buildqueue ALTER COLUMN job DROP NOT NULL;
-ALTER TABLE buildqueue ALTER COLUMN job_type DROP NOT NULL;
+    ADD COLUMN date_started timestamp without time zone,
+    ALTER COLUMN job DROP NOT NULL,
+    ALTER COLUMN job_type DROP NOT NULL;
+CREATE UNIQUE INDEX buildqueue__build_farm_job__key
+    ON buildqueue (build_farm_job);
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2209, 51, 0);
