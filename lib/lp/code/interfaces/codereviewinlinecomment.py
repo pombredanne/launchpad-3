3bd88b9a24bb8bcdfe695fd8d15e7f53f6872b5e
Steve Kowalik <steve.kowalik@canonical.com>
2013-12-16 12:57:55 +1100

    Finish off the CRIC model code.

--- lib/lp/code/interfaces/codereviewinlinecomment.py
+++ lib/lp/code/interfaces/codereviewinlinecomment.py
@@ -6,7 +6,6 @@
 __metaclass__ = type
 __all__ = [
     'ICodeReviewInlineComment',
-    'ICodeReviewInlineCommentDraft',
     'ICodeReviewInlineCommentSet',
     ]
 
@@ -33,22 +32,30 @@ class ICodeReviewInlineComment(Interface):
         title=_('Inline comments'), required=True, readonly=True)
 
 
-class ICodeReviewInlineCommentDraft(Interface):
-    previewdiff = Reference(
-        title=_('The preview diff'), schema=IPreviewDiff, required=True,
-        readonly=True)
-    person = Reference(
-        title=_('Person'), schema=IPerson, required=True, readonly=True)
-    comments = TextLine(
-        title=_('Inline comments'), required=True, readonly=True)
-
-
 class ICodeReviewInlineCommentSet(Interface):
-    def new_draft(previewdiff, person, comments):
-        pass
+    def findDraft(previewdiff, person):
+        """Find the draft comments for a given diff and person.
+        
+        :param previewdiff: The `PreviewDiff` these comments are for.
+        :param person: The `Person` making the comments.
+        """
 
-    def publish_draft(comment, cricd):
-        pass
+    def ensureDraft(previewdiff, person, comments):
+        """Ensure a `ICodeReviewInlineCommentDraft` is up to date. This method
+        will also delete an existing draft if the comments are empty.
+
+        :param previewdiff: The `PreviewDiff` these comments are for.
+        :param person: The `Person` making the comments.
+        :param comments: The comments themselves.
+        """
+
+    def publishDraft(previewdiff, person, comment):
+        """Publish code review inline comments so other people can view them.
+
+        :param previewdiff: The `PreviewDiff` these comments are for.
+        :param person: The `Person` making the comments.
+        :param comment: The `CodeReviewComment` linked to the comments.
+        """
 
     def findByPreviewDiff(previewdiff):
-        pass
+        """Find all comments for a given `PreviewDiff`."""
--- lib/lp/code/model/codereviewinlinecomment.py
+++ lib/lp/code/model/codereviewinlinecomment.py
@@ -10,18 +10,22 @@ __all__ = [
     'CodeReviewInlineCommentSet',
     ]
 
+import simplejson
 from storm.locals import (
     Int,
     Reference,
-    Text,
+    Unicode,
     )
+from zope.component import getUtility
 from zope.interface import implements
 
 from lp.code.interfaces.codereviewinlinecomment import (
     ICodeReviewInlineComment,
-    ICodeReviewInlineCommentDraft,
     ICodeReviewInlineCommentSet,
     )
+from lp.code.model.codereviewcomment import CodeReviewComment
+from lp.registry.interfaces.person import IPersonSet
+from lp.services.database.bulk import load_related
 from lp.services.database.interfaces import IStore
 from lp.services.database.stormbase import StormBase
 
@@ -35,37 +39,58 @@ class CodeReviewInlineComment(StormBase):
     previewdiff = Reference(previewdiff_id, 'PreviewDiff.id')
     person_id = Int(name='person')
     person = Reference(person_id, 'Person.id')
-    comment_id = Int(name='comment')
+    comment_id = Int(name='comment', primary=True)
     comment = Reference(comment_id, 'CodeReviewComment.id')
-    comments = Text()
+    comments = Unicode()
 
 
 class CodeReviewInlineCommentDraft(StormBase):
     __storm_table__ = 'CodeReviewInlineCommentDraft'
     __storm_primary__ = ('previewdiff_id', 'person_id')
 
-    implements(ICodeReviewInlineCommentDraft)
-
     previewdiff_id = Int(name='previewdiff')
     previewdiff = Reference(previewdiff_id, 'PreviewDiff.id')
     person_id = Int(name='person')
     person = Reference(person_id, 'Person.id')
-    comments = Text()
+    comments = Unicode()
 
 
 class CodeReviewInlineCommentSet:
 
     implements(ICodeReviewInlineCommentSet)
 
-    def new_draft(self, previewdiff, person, comments):
-        cricd = CodeReviewInlineCommentDraft()
-        cricd.previewdiff = previewdiff
-        cricd.person = person
-        cricd.comments = comments
-        IStore(CodeReviewInlineCommentDraft).add(cricd)
-        return cricd
+    def _findDraftObject(self, previewdiff, person):
+        return IStore(CodeReviewInlineCommentDraft).find(
+            CodeReviewInlineCommentDraft,
+            CodeReviewInlineCommentDraft.previewdiff_id == previewdiff.id,
+            CodeReviewInlineCommentDraft.person_id == person.id).one()
 
-    def publish_draft(self, comment, cricd):
+    def findDraft(self, previewdiff, person):
+        cricd = self._findDraftObject(previewdiff, person)
+        if cricd:
+            return simplejson.loads(cricd.comments)
+        return {}
+
+    def ensureDraft(self, previewdiff, person, comments):
+        cricd = self._findDraftObject(previewdiff, person)
+        if not comments:
+            if cricd:
+                IStore(CodeReviewInlineCommentDraft).remove(cricd)
+        else:
+            if type(comments) == dict:
+                comments = simplejson.dumps(comments).decode('utf-8')
+            if cricd:
+                cricd.comments = comments
+            else:
+                cricd = CodeReviewInlineCommentDraft()
+                cricd.previewdiff = previewdiff
+                cricd.person = person
+                cricd.comments = comments
+                IStore(CodeReviewInlineCommentDraft).add(cricd)
+
+    def publishDraft(self, previewdiff, person, comment):
+        cricd = self._findDraftObject(previewdiff, person)
+        assert cricd is not None
         cric = CodeReviewInlineComment()
         cric.previewdiff = cricd.previewdiff
         cric.person = cricd.person
@@ -75,7 +100,11 @@ class CodeReviewInlineCommentSet:
         IStore(CodeReviewInlineComment).remove(cricd)
         return cric
 
-    def findByPreviewDiff(self, previewdiff):
-        return IStore(CodeReviewInlineComment).find(
+    def findByPreviewDiff(self, previewdiff, person):
+        crics = IStore(CodeReviewInlineComment).find(
             CodeReviewInlineComment,
             CodeReviewInlineComment.previewdiff_id == previewdiff.id)
+        load_related(CodeReviewComment, crics, ['comment_id'])
+        getUtility(IPersonSet).getPrecachedPersonsFromIDs(
+            [cric.person_id for cric in crics])
+        return [crics, self.findDraft(previewdiff, person)]
