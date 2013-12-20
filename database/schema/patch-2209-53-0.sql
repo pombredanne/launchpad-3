7d08cd2bd980ecda157d23f0de156672fb04dc10
Steve Kowalik <steve.kowalik@canonical.com>
2013-12-20 16:15:26 +1100

    Add two tables for inline comments.

---
+++ database/schema/patch-2209-53-0.sql
@@ -0,0 +1,30 @@
+-- Copyright 2013 Canonical Ltd.  This software is licensed under the
+-- GNU Affero General Public License version 3 (see the file LICENSE).
+
+SET client_min_messages=ERROR;
+
+CREATE TABLE codereviewinlinecomment (
+    previewdiff integer NOT NULL REFERENCES previewdiff,
+    person integer NOT NULL REFERENCES person,
+    comment integer PRIMARY KEY REFERENCES codereviewmessage,
+    comments text
+);
+
+CREATE INDEX codereviewinlinecomment__person__idx ON
+    codereviewinlinecomment(person);
+CREATE INDEX codereviewinlinecomment__previewdiff__idx ON
+    codereviewinlinecomment(previewdiff);
+
+CREATE TABLE codereviewinlinecommentdraft (
+    previewdiff integer NOT NULL REFERENCES previewdiff,
+    person integer NOT NULL REFERENCES person,
+    comments text,
+    PRIMARY KEY (previewdiff, person)
+);
+
+CREATE INDEX codereviewinlinecommentdraft__person__idx ON
+    codereviewinlinecommentdraft(person);
+CREATE INDEX codereviewinlinecommentdraft__previewdiff__idx ON
+    codereviewinlinecommentdraft(previewdiff);
+
+INSERT INTO LaunchpadDatabaseRevision VALUES (2209, 53, 0);
