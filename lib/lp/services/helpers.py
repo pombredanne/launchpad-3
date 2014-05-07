8aa440f9bc1807c850600d6b031041338958f4cd
Colin Watson <cjwatson@canonical.com>
2014-05-07 16:28:50 +0100

    Update copyright again.

--- lib/lp/services/helpers.py
+++ lib/lp/services/helpers.py
@@ -1,4 +1,4 @@
-# Copyright 2009-2011 Canonical Ltd.  This software is licensed under the
+# Copyright 2009-2014 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
 """Various functions and classes that are useful across different parts of
