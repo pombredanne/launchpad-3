77babb5602f4173b4db7a9910dea31fc317921e2
Chris Johnston <chrisjohnston@ubuntu.com>
2014-05-12 23:56:01 +0000

    Update IC emails per review

--- lib/lp/code/mail/codereviewcomment.py
+++ lib/lp/code/mail/codereviewcomment.py
@@ -177,10 +177,9 @@ def build_inline_comments_section(comments, diff_text):
         result_lines.append('> {0}'.format(diff_lines[i]))
         comment = comments.get(str(i))
         if comment is not None:
-            com = comment.splitlines()
-            com.insert(0, '')
-            com.append('')
-            result_lines.extend(com)
+            result_lines.append('')
+            result_lines.extend(comment.splitlines())
+            result_lines.append('')
 
     result_text = '\n'.join(result_lines)
 
