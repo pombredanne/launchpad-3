5fb9a1a0f572ad5e217e716d3febc1822b0a3ba6
William Grant <william.grant@canonical.com>
2014-06-17 22:17:14 +1000

    Add clean_status default and introduce date_clean_status_changed.

--- database/schema/patch-2209-57-0.sql
+++ database/schema/patch-2209-57-0.sql
@@ -4,7 +4,9 @@
 SET client_min_messages=ERROR;
 
 ALTER TABLE builder
-    ADD COLUMN clean_status integer,
-    ADD COLUMN vm_reset_protocol integer;
+    ADD COLUMN clean_status integer NOT NULL DEFAULT 1,
+    ADD COLUMN vm_reset_protocol integer,
+    ADD COLUMN date_clean_status_changed timestamp without time zone
+        NOT NULL DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC');
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2209, 57, 0);
