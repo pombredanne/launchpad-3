0ff68add2fb38a85a25a02d5cdedfd7df890dd4f
William Grant <william.grant@canonical.com>
2014-07-04 21:35:36 +1000

    Packageset URLs now include the distro name, not just the distroseries.

--- lib/lp/soyuz/browser/packageset.py
+++ lib/lp/soyuz/browser/packageset.py
@@ -11,7 +11,6 @@ __all__ = [
 
 from zope.component import getUtility
 
-from lp.app.errors import NotFoundError
 from lp.registry.interfaces.distribution import IDistributionSet
 from lp.services.webapp import GetitemNavigation
 from lp.soyuz.interfaces.packageset import IPackagesetSet
@@ -21,27 +20,24 @@ class PackagesetSetNavigation(GetitemNavigation):
     """Navigation methods for PackagesetSet."""
     usedfor = IPackagesetSet
 
-    def traverse(self, distroseries_name):
+    def traverse(self, distribution_name):
         """Traverse package sets in distro series context.
 
         The URI fragment of interest is:
 
-            /package-sets/lucid/mozilla
+            /package-sets/ubuntu/lucid/mozilla
 
-        where 'lucid' is the distro series and 'mozilla' is the package set
-        *name* respectively.
+        where 'ubuntu' is the distro, 'lucid' is the distro series and
+        'mozilla' is the package set.
         """
-
-        distro = getUtility(IDistributionSet).getByName('ubuntu')
-        try:
-            distroseries = distro[distroseries_name]
-        except NotFoundError:
+        distro = getUtility(IDistributionSet).getByName(distribution_name)
+        if distro is None:
             return None
-
+        if self.request.stepstogo:
+            distroseries_name = self.request.stepstogo.consume()
+            distroseries = distro[distroseries_name]
         if self.request.stepstogo:
             # The package set name follows after the distro series.
             ps_name = self.request.stepstogo.consume()
             return self.context.getByName(distroseries, ps_name)
-
-        # Otherwise return None (to trigger a NotFound error).
         return None
