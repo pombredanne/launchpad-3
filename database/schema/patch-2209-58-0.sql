127f6724c4824a4b94a0cec73c08a15d3fb27b45
Stuart Bishop <stuart.bishop@canonical.com>
2014-07-25 00:17:01 +0700

    Need for speed & document subsequent steps for testing

--- database/schema/patch-2209-58-0.sql
+++ database/schema/patch-2209-58-0.sql
@@ -4,12 +4,8 @@
 SET client_min_messages=ERROR;
 
 
--- We could avoid a later step using bigserial as the type, but alas that
--- is implemented as a DEFAULT and causes the table to be rewritten.
-ALTER TABLE LibraryFileContent ADD COLUMN _id bigint UNIQUE;
-
-ALTER TABLE LibraryFileAlias ADD COLUMN _content bigint
-    REFERENCES LibraryFileContent(_id);
+ALTER TABLE LibraryFileContent ADD COLUMN _id bigint;
+ALTER TABLE LibraryFileAlias ADD COLUMN _content bigint;
 
 -- LibraryFileContent needs an INSERT trigger, ensuring that new
 -- records get a LFC._id matching LFC.id
@@ -41,5 +37,50 @@ $$;
 CREATE TRIGGER lfa_sync_content_t BEFORE INSERT OR UPDATE ON LibraryFileAlias
 FOR EACH ROW EXECUTE PROCEDURE lfa_sync_content_t();
 
+/* Subsequent statements, to be executed live and in subsequent patches
+   after timing and optimization. */
+
+
+/*
+-- STEP 2 -- Fill in new columns, not a DB patch.
+--DROP TRIGGER lfa_sync_content_t ON LibraryFileAlias;
+--DROP TRIGGER lfc_sync_id_t ON LibraryFileContent;
+UPDATE LibraryFileAlias SET _content = content;
+UPDATE LibraryFileContent SET _id = id;
+
+
+-- STEP 3 -- Indexes, to be built concurrently.
+CREATE UNIQUE INDEX libraryfilecontent_id_key ON LibraryFileContent(_id);
+CREATE INDEX libraryfilealias__content__idx ON LibraryFileAlias(_content);
+CREATE INDEX libraryfilealias__expires_has_content__idx
+    ON LibraryFileAlias(expires) WHERE content IS NOT NULL;
+
+DROP INDEX LibraryFileAlias__expires__idx; -- Confirm this index is unused.
+
+
+-- STEP 4 -- Constraints, swap into place, and the rest. May be split.
+DROP TRIGGER lfa_sync_content_t ON LibraryFileAlias;
+DROP TRIGGER lfc_sync_id_t ON LibraryFileContent;
+DROP FUNCTION lfa_sync_content_t();
+DROP FUNCTION lfc_sync_id_t();
+
+ALTER SEQUENCE libraryfilecontent_id_seq OWNED BY LibraryFileContent._id;
+
+ALTER TABLE LibraryFileAlias DROP COLUMN content;
+ALTER TABLE LibraryFileAlias RENAME _content TO content;
+ALTER TABLE LibraryFileContent DROP COLUMN id;
+ALTER TABLE LibraryFileContent RENAME _id TO id;
+
+ALTER INDEX libraryfilecontent_id_key RENAME TO libraryfilecontent_pkey;
+ALTER TABLE LibraryFileContent
+    ALTER COLUMN id SET DEFAULT nextval('libraryfilecontent_id_seq'),
+    ALTER COLUMN id SET NOT NULL,  -- Slow
+    ALTER COLUMN sha256 SET NOT NULL, -- Slow
+    ADD CONSTRAINT libraryfilecontent_pkey
+        PRIMARY KEY USING INDEX libraryfilecontent_pkey; -- Maybe slow.
+
+ALTER TABLE LibraryFileAlias ADD CONSTRAINT libraryfilealias__content__fkey
+    FOREIGN KEY (content) REFERENCES LibraryFileContent(id); -- Slow
+*/
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2209, 58, 0);
