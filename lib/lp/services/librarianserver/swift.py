f472f618b59e9b302e1cbff87d91b3568227e7c9
Stuart Bishop <stuart.bishop@canonical.com>
2014-10-02 19:53:08 +0700

    If librarian-gc is in progress, or was aborted, there may be Librarian files on disk with no corresponding db record. Cope.

--- lib/lp/services/librarianserver/swift.py
+++ lib/lp/services/librarianserver/swift.py
@@ -136,9 +136,15 @@ def to_swift(log, start_lfc_id=None, end_lfc_id=None, remove=False):
             except swiftclient.ClientException as x:
                 if x.http_status != 404:
                     raise
-                log.info('Putting {0} into Swift ({1}, {2})'.format(
-                    lfc, container, obj_name))
-                _put(log, swift_connection, lfc, container, obj_name, fs_path)
+                if ISlaveStore(LibraryFileContent).get(
+                        LibraryFileContent, lfc) is None:
+                    log.info("{0} exists on disk but not in the db".format(
+                        lfc))
+                else:
+                    log.info('Putting {0} into Swift ({1}, {2})'.format(
+                        lfc, container, obj_name))
+                    _put(log, swift_connection, lfc, container, obj_name,
+                         fs_path)
             if remove:
                 os.unlink(fs_path)
 
