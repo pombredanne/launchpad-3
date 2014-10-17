77e9002a38a260ad8eb1891d4c82130cb9a6a92f
Stuart Bishop <stuart.bishop@canonical.com>
2014-10-17 15:41:56 +0700

    Fix assert failing uploads

--- lib/lp/services/librarianserver/storage.py
+++ lib/lp/services/librarianserver/storage.py
@@ -293,6 +293,7 @@ def _relFileLocation(file_id):
     The relative location is obtained by converting file_id into a 8-digit hex
     and then splitting it across four path segments.
     """
+    file_id = int(file_id)
     assert file_id <= 4294967295, 'file id has exceeded filesystem db maximum'
-    h = "%08x" % int(file_id)
+    h = "%08x" % file_id
     return '%s/%s/%s/%s' % (h[:2], h[2:4], h[4:6], h[6:])
