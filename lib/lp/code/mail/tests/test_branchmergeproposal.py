4c350a073879636204ff1cbc223adf4ada5495a8
William Grant <william.grant@canonical.com>
2014-11-14 23:30:28 +1100

    Drop merge proposal URLs from email footers where they're already included in the body.

--- lib/lp/code/mail/tests/test_branchmergeproposal.py
+++ lib/lp/code/mail/tests/test_branchmergeproposal.py
@@ -114,7 +114,6 @@ class TestMergeProposalMailing(TestCaseWithFactory):
         For more details, see:
         %(bmp)s
         --\x20
-        %(bmp)s
         %(reason)s
         """) % {
             'source': bmp.source_branch.bzr_identity,
@@ -516,7 +515,6 @@ class TestMergeProposalMailing(TestCaseWithFactory):
             For more details, see:
             %(bmp)s
             --\x20
-            %(bmp)s
             You are the owner of lp://dev/~bob/super-product/fix-foo-for-bar.
             """) % {
                 'source': bmp.source_branch.bzr_identity,
@@ -644,7 +642,6 @@ class TestBranchMergeProposalRequestReview(TestCaseWithFactory):
             This branch is awesome.
 
             --\x20
-            %(bmp)s
             You are requested to review the proposed merge of %(source)s"""
             """ into %(target)s.
             """ % {
