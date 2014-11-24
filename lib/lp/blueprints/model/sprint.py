2c6af38b0ce4baa2f7a7e082eacda181c5bc97da
Colin Watson <cjwatson@canonical.com>
2014-11-24 13:05:12 +0000

    Turn SprintAttendance.is_physical into a property.

--- lib/lp/blueprints/model/sprint.py
+++ lib/lp/blueprints/model/sprint.py
@@ -255,7 +255,7 @@ class Sprint(SQLBase, HasDriversMixin, HasSpecificationsMixin):
             attendance = SprintAttendance(sprint=self, attendee=person)
         attendance.time_starts = time_starts
         attendance.time_ends = time_ends
-        attendance.is_physical = is_physical
+        attendance._is_physical = is_physical
         return attendance
 
     def removeAttendance(self, person):
