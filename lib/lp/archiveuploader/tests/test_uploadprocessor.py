97dc32d7b78def103f0d3ffb1766692193594bea
Colin Watson <cjwatson@canonical.com>
2015-01-23 17:57:59 +0000

    Update copyright; lint.

--- lib/lp/archiveuploader/tests/test_uploadprocessor.py
+++ lib/lp/archiveuploader/tests/test_uploadprocessor.py
@@ -1,4 +1,4 @@
-# Copyright 2009-2014 Canonical Ltd.  This software is licensed under the
+# Copyright 2009-2015 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
 """Functional tests for uploadprocessor.py."""
@@ -482,7 +482,6 @@ class TestUploadProcessor(TestUploadProcessorBase):
         try:
             # Create an upload and a target to move it to.
             upload = self._makeUpload(testdir)
-            upload_name = os.path.basename(upload)
 
             # Remove it
             self.options.base_fsroot = testdir
