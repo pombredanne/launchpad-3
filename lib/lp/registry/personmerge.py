3ac0c6fc142057a1a6069012e124592b0a682d12
Colin Watson <cjwatson@canonical.com>
2015-02-06 10:50:09 +0000

    Temporarily whitelist gitrepository.owner and gitshortcut.owner from personmerge.

--- lib/lp/registry/personmerge.py
+++ lib/lp/registry/personmerge.py
@@ -691,6 +691,10 @@ def merge_people(from_person, to_person, reviewer, delete=False):
         ('bugsummaryjournal', 'viewed_by'),
         ('latestpersonsourcepackagereleasecache', 'creator'),
         ('latestpersonsourcepackagereleasecache', 'maintainer'),
+        # These need handling before we deploy the git code, but can be
+        # ignored for the purpose of deploying the database tables.
+        ('gitrepository', 'owner'),
+        ('gitshortcut', 'owner'),
         ]
 
     references = list(postgresql.listReferences(cur, 'person', 'id'))
