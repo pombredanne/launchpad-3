d42cec83229b12a1a29c2cbbd53f023cc5b74026
William Grant <william.grant@canonical.com>
2015-05-22 17:11:45 +1000

    Basic webhook schema. Needs indices, perms.

---
+++ database/schema/patch-2209-66-0.sql
@@ -0,0 +1,29 @@
+-- Copyright 2015 Canonical Ltd.  This software is licensed under the
+-- GNU Affero General Public License version 3 (see the file LICENSE).
+
+SET client_min_messages=ERROR;
+
+CREATE TABLE WebHook (
+    id serial PRIMARY KEY,
+    git_repository integer REFERENCES GitRepository,
+    registrant integer REFERENCES Person,
+    date_created timestamp without time zone DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC') NOT NULL,
+    date_last_modified timestamp without time zone DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC') NOT NULL,
+    active boolean DEFAULT true NOT NULL,
+    endpoint_url text NOT NULL,
+    secret text,
+    json_data json NOT NULL,
+    CHECK (git_repository IS NOT NULL) -- To be expanded to other targets later.
+    );
+
+CREATE TABLE WebHookJob (
+    -- TODO: UUID PK?
+    job integer PRIMARY KEY REFERENCES Job ON DELETE CASCADE NOT NULL,
+    webhook integer REFERENCES WebHook NOT NULL,
+    job_type integer NOT NULL,
+    json_data text NOT NULL,
+    request integer REFERENCES LibraryFileAlias,
+    response integer REFERENCES LibraryFileAlias
+    );
+
+INSERT INTO LaunchpadDatabaseRevision VALUES (2209, 66, 0);
