603bda00c6bc6969567c2f441442352344786aad
Colin Watson <cjwatson@canonical.com>
2015-05-26 14:34:42 +0100

    Fix OOPS trying to run ReclaimGitRepositorySpaceJob under a real job runner.

--- lib/lp/code/model/gitjob.py
+++ lib/lp/code/model/gitjob.py
@@ -162,8 +162,9 @@ class GitJobDerived(BaseRunnableJob):
         oops_vars.extend([
             ('git_job_id', self.context.job.id),
             ('git_job_type', self.context.job_type.title),
-            ('git_repository_id', self.context.repository.id),
-            ('git_repository_name', self.context.repository.unique_name)])
+            ('git_repository_name', self.metadata["repository_name"])])
+        if self.context.repository is not None:
+            oops_vars.append(('git_repository_id', self.context.repository.id))
         return oops_vars
 
     def getErrorRecipients(self):
