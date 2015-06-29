0994aca776aa556434288f0261e76003952daec9
William Grant <william.grant@canonical.com>
2015-06-29 15:12:41 +1000

    Test that requests are sent as expected.

--- lib/lp/services/webhooks/tests/test_webhookjob.py
+++ lib/lp/services/webhooks/tests/test_webhookjob.py
@@ -9,6 +9,7 @@ from httmock import (
     urlmatch,
     HTTMock,
     )
+from testtools.matchers import MatchesStructure
 
 from lp.services.job.runner import JobRunner
 from lp.services.job.interfaces.job import JobStatus
@@ -62,8 +63,11 @@ class TestWebhookEventJob(TestCaseWithFactory):
     layer = LaunchpadZopelessLayer
 
     def makeAndRunJob(self, response_status=200):
+        requests = []
+
         @urlmatch(netloc='hookep.com')
         def endpoint_mock(url, request):
+            requests.append(request)
             return {'status_code': response_status, 'content': 'Content'}
 
         hook = self.factory.makeWebhook(endpoint_url=u'http://hookep.com/foo')
@@ -71,7 +75,7 @@ class TestWebhookEventJob(TestCaseWithFactory):
         with HTTMock(endpoint_mock):
             with dbuser("webhookrunner"):
                 JobRunner([job]).runAll()
-        return job
+        return job, requests
 
     def test_provides_interface(self):
         # `WebhookEventJob` objects provide `IWebhookEventJob`.
@@ -80,21 +84,28 @@ class TestWebhookEventJob(TestCaseWithFactory):
 
     def test_run(self):
         with CaptureOops() as oopses:
-            job = self.makeAndRunJob(response_status=200)
+            job, requests = self.makeAndRunJob(response_status=200)
         self.assertEqual(JobStatus.COMPLETED, job.status)
+        self.assertEqual(1, len(requests))
+        self.assertThat(
+            requests[0],
+            MatchesStructure.byEquality(
+                url=u'http://hookep.com/foo', method='GET', body=None))
         self.assertEqual([], oopses.oopses)
 
     def test_run_404(self):
         with CaptureOops() as oopses:
-            job = self.makeAndRunJob(response_status=404)
+            job, requests = self.makeAndRunJob(response_status=404)
         self.assertEqual(JobStatus.FAILED, job.status)
+        self.assertEqual(1, len(requests))
         self.assertEqual([], oopses.oopses)
 
     def test_run_no_proxy(self):
         self.pushConfig('webhooks', http_proxy=None)
         with CaptureOops() as oopses:
-            job = self.makeAndRunJob(response_status=200)
+            job, requests = self.makeAndRunJob(response_status=200)
         self.assertEqual(JobStatus.FAILED, job.status)
+        self.assertEqual([], requests)
         self.assertEqual(1, len(oopses.oopses))
         self.assertEqual(
             'No webhook proxy configured.', oopses.oopses[0]['value'])
