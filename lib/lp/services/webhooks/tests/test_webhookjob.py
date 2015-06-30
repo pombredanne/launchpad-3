6c0e4c73fd61baa0670aa4e5887b27cba42902c4
William Grant <william.grant@canonical.com>
2015-06-30 12:30:21 +1000

    Rework tests to mock the utility rather than requests.

--- lib/lp/services/webhooks/tests/test_webhookjob.py
+++ lib/lp/services/webhooks/tests/test_webhookjob.py
@@ -19,6 +19,7 @@ from lp.services.job.interfaces.job import JobStatus
 from lp.services.job.runner import JobRunner
 from lp.services.webhooks.client import WebhookClient
 from lp.services.webhooks.interfaces import (
+    IWebhookClient,
     IWebhookEventJob,
     IWebhookJob,
     )
@@ -30,7 +31,10 @@ from lp.services.webhooks.model import (
     )
 from lp.testing import TestCaseWithFactory
 from lp.testing.dbuser import dbuser
-from lp.testing.fixture import CaptureOops
+from lp.testing.fixture import (
+    CaptureOops,
+    ZopeUtilityFixture,
+    )
 from lp.testing.layers import (
     DatabaseFunctionalLayer,
     LaunchpadZopelessLayer,
@@ -65,11 +69,13 @@ class TestWebhookJobDerived(TestCaseWithFactory):
 class TestWebhookClient(TestCase):
     """Tests for `WebhookClient`."""
 
-    def sendToWebhook(self, response_status=200):
+    def sendToWebhook(self, response_status=200, raises=None):
         reqs = []
 
         @urlmatch(netloc='hookep.com')
         def endpoint_mock(url, request):
+            if raises:
+                raise raises
             reqs.append(request)
             return {'status_code': response_status, 'content': 'Content'}
 
@@ -79,7 +85,6 @@ class TestWebhookClient(TestCase):
                 {'http': 'http://squid.example.com:3128'},
                 {'foo': 'bar'})
 
-        self.assertEqual(1, len(reqs))
         return reqs, result
 
     def test_sends_request(self):
@@ -102,28 +107,52 @@ class TestWebhookClient(TestCase):
         self.assertEqual({}, result['response']['headers'])
         self.assertEqual('Content', result['response']['body'])
 
+    def test_connection_error(self):
+        # Attempts that fail to connect have a connection_error rather
+        # than a response.
+        reqs, result = self.sendToWebhook(
+            raises=requests.ConnectionError('Connection refused'))
+        self.assertNotIn('response', result)
+        self.assertEqual(
+            'Connection refused', result['connection_error'])
+        self.assertEqual([], reqs)
+
+
+class MockWebhookClient:
+
+    def __init__(self, response_status=200, raises=None):
+        self.response_status = response_status
+        self.raises = raises
+        self.requests = []
+
+    def sendEvent(self, url, proxy, payload):
+        result = {'request': {}}
+        if isinstance(self.raises, requests.ConnectionError):
+            result['connection_error'] = str(self.raises)
+        elif self.raises is not None:
+            raise self.raises
+        else:
+            self.requests.append(('POST', url))
+            result['response'] = {'status_code': self.response_status}
+        return result
+
 
 class TestWebhookEventJob(TestCaseWithFactory):
     """Tests for `WebhookEventJob`."""
 
     layer = LaunchpadZopelessLayer
 
-    def makeAndRunJob(self, response_status=200, raises=None):
-        reqs = []
-
-        @urlmatch(netloc='hookep.com')
-        def endpoint_mock(url, request):
-            reqs.append(request)
-            if raises:
-                raise raises
-            return {'status_code': response_status, 'content': 'Content'}
-
+    def makeAndRunJob(self, response_status=200, raises=None, mock=True):
         hook = self.factory.makeWebhook(endpoint_url=u'http://hookep.com/foo')
         job = WebhookEventJob.create(hook, payload={'foo': 'bar'})
-        with HTTMock(endpoint_mock):
-            with dbuser("webhookrunner"):
-                JobRunner([job]).runAll()
-        return job, reqs
+
+        client = MockWebhookClient(
+            response_status=response_status, raises=raises)
+        if mock:
+            self.useFixture(ZopeUtilityFixture(client, IWebhookClient))
+        with dbuser("webhookrunner"):
+            JobRunner([job]).runAll()
+        return job, client.requests
 
     def test_provides_interface(self):
         # `WebhookEventJob` objects provide `IWebhookEventJob`.
@@ -139,13 +168,7 @@ class TestWebhookEventJob(TestCaseWithFactory):
         self.assertEqual(1, len(reqs))
         self.assertEqual(
             200, job.json_data['result']['response']['status_code'])
-        self.assertThat(
-            reqs[0],
-            MatchesStructure.byEquality(
-                url=u'http://hookep.com/foo', method='POST'))
-        self.assertEqual(
-            'application/json', reqs[0].headers['Content-Type'])
-        self.assertEqual({'foo': 'bar'}, json.loads(reqs[0].body))
+        self.assertEqual([('POST', 'http://hookep.com/foo')], reqs)
         self.assertEqual([], oopses.oopses)
 
     def test_run_404(self):
@@ -154,9 +177,9 @@ class TestWebhookEventJob(TestCaseWithFactory):
         with CaptureOops() as oopses:
             job, reqs = self.makeAndRunJob(response_status=404)
         self.assertEqual(JobStatus.COMPLETED, job.status)
+        self.assertEqual(1, len(reqs))
         self.assertEqual(
             404, job.json_data['result']['response']['status_code'])
-        self.assertEqual(1, len(reqs))
         self.assertEqual([], oopses.oopses)
 
     def test_run_connection_error(self):
@@ -169,7 +192,7 @@ class TestWebhookEventJob(TestCaseWithFactory):
         self.assertNotIn('response', job.json_data['result'])
         self.assertEqual(
             'Connection refused', job.json_data['result']['connection_error'])
-        self.assertEqual(1, len(reqs))
+        self.assertEqual([], reqs)
         self.assertEqual([], oopses.oopses)
 
     def test_run_no_proxy(self):
@@ -179,7 +202,7 @@ class TestWebhookEventJob(TestCaseWithFactory):
         # the job crashes.
         self.pushConfig('webhooks', http_proxy=None)
         with CaptureOops() as oopses:
-            job, reqs = self.makeAndRunJob(response_status=200)
+            job, reqs = self.makeAndRunJob(response_status=200, mock=False)
         self.assertEqual(JobStatus.FAILED, job.status)
         self.assertEqual([], reqs)
         self.assertEqual(1, len(oopses.oopses))
