17bf8135cf0d5a290a03976cc2b11c2a6422118d
William Grant <william.grant@canonical.com>
2015-07-06 18:33:27 +1000

    Add indices.

--- database/schema/patch-2209-66-0.sql
+++ database/schema/patch-2209-66-0.sql
@@ -7,20 +7,28 @@ CREATE TABLE WebHook (
     id serial PRIMARY KEY,
     git_repository integer REFERENCES GitRepository,
     registrant integer REFERENCES Person,
-    date_created timestamp without time zone DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC') NOT NULL,
-    date_last_modified timestamp without time zone DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC') NOT NULL,
+    date_created timestamp without time zone
+        DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC') NOT NULL,
+    date_last_modified timestamp without time zone
+        DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC') NOT NULL,
     active boolean DEFAULT true NOT NULL,
     delivery_url text NOT NULL,
     secret text,
     json_data text NOT NULL,
-    CHECK (git_repository IS NOT NULL) -- To be expanded to other targets later.
+    CHECK (git_repository IS NOT NULL) -- To be expanded to other targets.
     );
 
 CREATE TABLE WebHookJob (
     job integer PRIMARY KEY REFERENCES Job ON DELETE CASCADE NOT NULL,
     webhook integer REFERENCES WebHook NOT NULL,
     job_type integer NOT NULL,
-    json_data text NOT NULL,
+    json_data text NOT NULL
     );
+
+CREATE INDEX webhook__git_repository__id__idx
+    ON webhook(git_repository, id) WHERE git_repository IS NOT NULL;
+
+CREATE INDEX webhookjob__webhook__job_type__job__idx
+    ON webhookjob(webhook, job_type, job);
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2209, 66, 0);
