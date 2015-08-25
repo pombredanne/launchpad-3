b49939e8b1c005da74c740f11b7f82336fedecf1
Colin Watson <cjwatson@canonical.com>
2015-08-25 17:10:09 +0100

    Only publish PPAs once their signing key has been generated.

--- lib/lp/soyuz/model/archive.py
+++ lib/lp/soyuz/model/archive.py
@@ -413,6 +413,14 @@ class Archive(SQLBase):
         return self.status == ArchiveStatus.ACTIVE
 
     @property
+    def can_be_published(self):
+        """See `IArchive`."""
+        # PPAs can only be published once their signing key has been generated.
+        return (
+            self.publish and
+            (not self.is_ppa or self.signing_key is not None))
+
+    @property
     def reference(self):
         template = ARCHIVE_REFERENCE_TEMPLATES.get(self.purpose)
         if template is None:
