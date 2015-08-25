fa47cb631f7e181368a1e360d338a900532f77ca
Colin Watson <cjwatson@canonical.com>
2015-08-25 17:24:06 +0100

    Make question notification rationales more consistent, including team annotations for subscribers.

---
+++ lib/lp/answers/mail/question.py
@@ -0,0 +1,46 @@
+# Copyright 2015 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+__metaclass__ = type
+__all__ = [
+    'QuestionRecipientReason',
+    ]
+
+from lp.services.mail.basemailer import RecipientReason
+
+
+class QuestionRecipientReason(RecipientReason):
+
+    @classmethod
+    def forSubscriber(cls, subscriber, recipient):
+        header = cls.makeRationale("Subscriber", subscriber)
+        reason = (
+            "You received this question notification because "
+            "%(lc_entity_is)s a direct subscriber of the question.")
+        return cls(subscriber, recipient, header, reason)
+
+    @classmethod
+    def forAsker(cls, asker, recipient):
+        header = cls.makeRationale("Asker", asker)
+        reason = (
+            "You received this question notification because you asked the "
+            "question.")
+        return cls(asker, recipient, header, reason)
+
+    @classmethod
+    def forAssignee(cls, assignee, recipient):
+        header = cls.makeRationale("Assignee", assignee)
+        reason = (
+            "You received this question notification because "
+            "%(lc_entity_is)s the assignee for this question.")
+        return cls(assignee, recipient, header, reason)
+
+    @classmethod
+    def forAnswerContact(cls, answer_contact, recipient, target_name,
+                         target_displayname):
+        header = cls.makeRationale(
+            "Answer Contact (%s)" % target_name, answer_contact)
+        reason = (
+            "You received this question notification because "
+            "%%(lc_entity_is)s an answer contact for %s." % target_displayname)
+        return cls(answer_contact, recipient, header, reason)
