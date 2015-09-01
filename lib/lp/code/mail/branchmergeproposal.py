d958927732afbaebde3568b93620033e8f5c7790
Colin Watson <cjwatson@canonical.com>
2015-09-01 18:10:46 +0100

    Send branch/repository modification notifications from jobs rather than directly from the webapp.

--- lib/lp/code/mail/branchmergeproposal.py
+++ lib/lp/code/mail/branchmergeproposal.py
@@ -10,7 +10,10 @@ from lp.code.enums import CodeReviewNotificationLevel
 from lp.code.mail.branch import BranchMailer
 from lp.services.config import config
 from lp.services.mail.basemailer import BaseMailer
-from lp.services.mail.sendmail import get_msgid
+from lp.services.mail.sendmail import (
+    format_address_for_person,
+    get_msgid,
+    )
 from lp.services.webapp import canonical_url
 
 
@@ -23,13 +26,13 @@ class BMPMailer(BranchMailer):
                  direct_email=False):
         BranchMailer.__init__(
             self, subject, template_name, recipients, from_address,
-            message_id=message_id, notification_type='code-review')
+            delta=delta, message_id=message_id,
+            notification_type='code-review')
         self.merge_proposal = merge_proposal
         if requested_reviews is None:
             requested_reviews = []
         self.requested_reviews = requested_reviews
         self.preview_diff = preview_diff
-        self.delta_text = delta
         self.template_params = self._generateTemplateParams()
         self.direct_email = direct_email
 
@@ -51,7 +54,7 @@ class BMPMailer(BranchMailer):
 
         assert from_user.preferredemail is not None, (
             'The sender must have an email address.')
-        from_address = cls._format_user_address(from_user)
+        from_address = format_address_for_person(from_user)
 
         return cls(
             '%(proposal_title)s',
@@ -73,7 +76,7 @@ class BMPMailer(BranchMailer):
         if from_user is not None:
             assert from_user.preferredemail is not None, (
                 'The sender must have an email address.')
-            from_address = cls._format_user_address(from_user)
+            from_address = format_address_for_person(from_user)
         else:
             from_address = config.canonical.noreply_from_address
         return cls(
@@ -85,7 +88,7 @@ class BMPMailer(BranchMailer):
     @classmethod
     def forReviewRequest(cls, reason, merge_proposal, from_user):
         """Return a mailer for a request to review a BranchMergeProposal."""
-        from_address = cls._format_user_address(from_user)
+        from_address = format_address_for_person(from_user)
         recipients = {reason.subscriber: reason}
         return cls(
             '%(proposal_title)s',
@@ -114,7 +117,7 @@ class BMPMailer(BranchMailer):
                 continue
             if vote.reviewer.hide_email_addresses:
                 continue
-            to_addrs.append(self._format_user_address(vote.reviewer))
+            to_addrs.append(format_address_for_person(vote.reviewer))
         return to_addrs
 
     def _getHeaders(self, email, recipient):
