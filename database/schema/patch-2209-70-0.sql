3243f934e37d6288eb9bbf04e5219fc8491c1acb
William Grant <william.grant@canonical.com>
2015-09-23 14:41:12 +1000

    Make XRef directional (to/from rather than 1/2).
    All current references are internal, so both directions exist, but that
    needn't always be the case.
    
    The motivation is to avoid complicating queries that can confuse the
    PostgreSQL planner. A bit of a write and size penalty, but it's not a
    large table and it's extremely read-heavy.

--- database/schema/patch-2209-70-0.sql
+++ database/schema/patch-2209-70-0.sql
@@ -4,24 +4,27 @@
 SET client_min_messages=ERROR;
 
 CREATE TABLE XRef (
-    type1 text NOT NULL,
-    id1 text NOT NULL,
-    id1_int integer,
-    type2 text NOT NULL,
-    id2 text NOT NULL,
-    id2_int integer,
+    from_type text NOT NULL,
+    from_id text NOT NULL,
+    from_id_int integer,
+    to_type text NOT NULL,
+    to_id text NOT NULL,
+    to_id_int integer,
     creator integer REFERENCES Person,
     date_created timestamp without time zone
         DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC') NOT NULL,
     metadata text,
-    CONSTRAINT objects_in_canonical_order CHECK ((type1, id1) < (type2, id2)),
-    PRIMARY KEY (type1, id1, type2, id2)
+    PRIMARY KEY (from_type, from_id, to_type, to_id)
 );
 
-CREATE UNIQUE INDEX xref__inverse__key ON XRef(type2, id2, type1, id1);
-CREATE UNIQUE INDEX xref__int__key ON XRef(type1, id1_int, type2, id2_int);
-CREATE UNIQUE INDEX xref__int_inverse__key ON XRef(type2, id2_int, type1, id1_int);
-CREATE INDEX xref__type1__type2__idx ON XRef(type1, type2);
+CREATE UNIQUE INDEX xref__inverse__key
+    ON XRef(to_type, to_id, from_type, from_id);
+CREATE UNIQUE INDEX xref__int__key
+    ON XRef(from_type, from_id_int, to_type, to_id_int);
+CREATE UNIQUE INDEX xref__int_inverse__key
+    ON XRef(to_type, to_id_int, from_type, from_id_int);
+CREATE INDEX xref__from_type__to_type__idx ON XRef(from_type, to_type);
+CREATE INDEX xref__to_type__from_type__idx ON XRef(to_type, from_type);
 
 CREATE INDEX xref__creator__idx ON XRef(creator);
 
