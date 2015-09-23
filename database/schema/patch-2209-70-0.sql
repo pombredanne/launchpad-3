51d06babd328fbff728273a2374bbeaeddfe8f95
William Grant <william.grant@canonical.com>
2015-09-23 10:00:19 +1000

    Use integers in XRef for types and IDs where possible.
    It doesn't significantly change performance of sane queries, but it lets
    PostgreSQL better optimise eg. searches for bugs without linked
    branches.

--- database/schema/patch-2209-70-0.sql
+++ database/schema/patch-2209-70-0.sql
@@ -4,18 +4,24 @@
 SET client_min_messages=ERROR;
 
 CREATE TABLE XRef (
-    object1_id text NOT NULL,
-    object2_id text NOT NULL,
+    type1 integer NOT NULL,
+    id1 text NOT NULL,
+    id1_int integer,
+    type2 integer NOT NULL,
+    id2 text NOT NULL,
+    id2_int integer,
     creator integer REFERENCES Person,
     date_created timestamp without time zone
         DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC') NOT NULL,
     metadata text,
-    CONSTRAINT objects_in_canonical_order CHECK (object1_id < object2_id),
-    PRIMARY KEY (object1_id, object2_id)
+    CONSTRAINT objects_in_canonical_order CHECK ((type1, id1) < (type2, id2)),
+    PRIMARY KEY (type1, id1, type2, id2)
 );
 
-CREATE UNIQUE INDEX xref__object2_id__object1_id__key
-    ON XRef(object2_id, object1_id);
+CREATE UNIQUE INDEX xref__inverse__key ON XRef(type2, id2, type1, id1);
+CREATE UNIQUE INDEX xref__int__key ON XRef(type1, id1_int, type2, id2_int);
+CREATE UNIQUE INDEX xref__int_inverse__key ON XRef(type2, id2_int, type1, id1_int);
+CREATE INDEX xref__type1__type2__idx ON XRef(type1, type2);
 
 CREATE INDEX xref__creator__idx ON XRef(creator);
 
