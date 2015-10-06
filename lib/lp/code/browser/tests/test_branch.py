a49ea2b1aae74f685785323ec0f461cae294c388
Colin Watson <cjwatson@canonical.com>
2015-10-06 12:31:15 +0100

    Fix TestBranchView.test_query_count_index_with_subscribers, which now has one fewer count query.

--- lib/lp/code/browser/tests/test_branch.py
+++ lib/lp/code/browser/tests/test_branch.py
@@ -586,7 +586,7 @@ class TestBranchView(BrowserTestCase):
         logout()
         with StormStatementRecorder() as recorder:
             browser.open(branch_url)
-        self.assertThat(recorder, HasQueryCount(Equals(29)))
+        self.assertThat(recorder, HasQueryCount(Equals(28)))
 
 
 class TestBranchViewPrivateArtifacts(BrowserTestCase):
