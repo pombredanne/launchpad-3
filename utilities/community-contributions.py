4f2cd5ceb738f906debaf237aefd1da34eb01fed
Colin Watson <cjwatson@canonical.com>
2016-05-03 13:25:09 +0100

    Update copyright.

--- utilities/community-contributions.py
+++ utilities/community-contributions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2009 Canonical Ltd.  This software is licensed under the
+# Copyright 2009-2016 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
 """Show what Launchpad community contributors have done.
