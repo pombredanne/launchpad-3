899643cde4cf3e0ded8495f1e081a41b4a30eda4
William Grant <william.grant@canonical.com>
2016-05-05 17:23:20 +1000

    Fix GitRepository {target,owner}_default indexes to be unique and partial.

--- database/schema/patch-2209-77-1.sql
+++ database/schema/patch-2209-77-1.sql
@@ -19,4 +19,24 @@ CREATE INDEX gitrepository__owner__project__id__idx
 CREATE INDEX gitrepository__owner__id__idx
     ON gitrepository(owner, id);
 
+-- Replace owner/target_default indexes with unique and partial ones.
+CREATE UNIQUE INDEX gitrepository__distribution__spn__target_default__key
+    ON gitrepository(distribution, sourcepackagename)
+    WHERE distribution IS NOT NULL AND target_default;
+DROP INDEX gitrepository__distribution__spn__target_default__idx;
+CREATE UNIQUE INDEX gitrepository__owner__distribution__spn__owner_default__key
+    ON gitrepository(owner, distribution, sourcepackagename)
+    WHERE distribution IS NOT NULL AND owner_default;
+DROP INDEX gitrepository__owner__distribution__spn__owner_default__idx;
+
+CREATE UNIQUE INDEX gitrepository__project__target_default__key
+    ON gitrepository(project)
+    WHERE project IS NOT NULL AND target_default;
+DROP INDEX gitrepository__project__target_default__idx;
+CREATE UNIQUE INDEX gitrepository__owner__project__owner_default__key
+    ON gitrepository(owner, project)
+    WHERE project IS NOT NULL AND owner_default;
+DROP INDEX gitrepository__owner__project__owner_default__idx;
+
+
 INSERT INTO LaunchpadDatabaseRevision VALUES (2209, 77, 1);
