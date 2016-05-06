07bd082921e4bb2abde472095084f2a8153544c8
Andy Whitcroft <apw@ubuntu.com>
2016-05-06 10:16:30 +0100

    Update copyright dates.
    Signed-off-by: Andy Whitcroft <apw@ubuntu.com>

--- lib/lp/archiveuploader/tests/test_uploadpolicy.py
+++ lib/lp/archiveuploader/tests/test_uploadpolicy.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-# Copyright 2010-2013 Canonical Ltd.  This software is licensed under the
+# Copyright 2010-2016 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
 from zope.component import getUtility
--- lib/lp/soyuz/browser/queue.py
+++ lib/lp/soyuz/browser/queue.py
@@ -1,4 +1,4 @@
-# Copyright 2009-2013 Canonical Ltd.  This software is licensed under the
+# Copyright 2009-2016 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
 """Browser views for package queue."""
