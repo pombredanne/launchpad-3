82c8cb1a42240f47a2dacf3cb6afad1dd3951ecb
Andy Whitcroft <apw@ubuntu.com>
2016-05-09 19:07:49 +0100

    Queue: clarify the visible name of the signing upload.
    Signed-off-by: Andy Whitcroft <apw@ubuntu.com>

--- lib/lp/soyuz/browser/queue.py
+++ lib/lp/soyuz/browser/queue.py
@@ -574,7 +574,7 @@ class CompletePackageUpload:
             (self.contains_installer, ("Installer", 'ubuntu-icon')),
             (self.contains_upgrader, ("Upgrader", 'ubuntu-icon')),
             (self.contains_ddtp, (ddtp, 'ubuntu-icon')),
-            (self.contains_signing, ("Signing", 'ubuntu-icon')),
+            (self.contains_signing, ("Objects for Signing", 'ubuntu-icon')),
             ]
         return [
             self.composeIcon(*details)
