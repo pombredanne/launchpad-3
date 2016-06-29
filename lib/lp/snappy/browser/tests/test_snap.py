e093f567b5bb707c3f5fa027fe9af3af23dc1b86
Maximiliano Bertacchini <maximiliano.bertacchini@canonical.com>
2016-06-29 18:44:48 -0300

    Add tests for snap creation with the processor field.

--- lib/lp/snappy/browser/tests/test_snap.py
+++ lib/lp/snappy/browser/tests/test_snap.py
@@ -160,6 +160,28 @@ class TestSnapAddView(BrowserTestCase):
             self.snappyseries = self.factory.makeSnappySeries(
                 usable_distro_series=[self.distroseries])
 
+    def setUpDistroSeries(self):
+        """Set up a distroseries with some available processors."""
+        distroseries = self.factory.makeUbuntuDistroSeries()
+        processor_names = ["386", "amd64", "hppa"]
+        for name in processor_names:
+            processor = getUtility(IProcessorSet).getByName(name)
+            self.factory.makeDistroArchSeries(
+                distroseries=distroseries, architecturetag=name,
+                processor=processor)
+        with admin_logged_in():
+            self.factory.makeSnappySeries(usable_distro_series=[distroseries])
+        return distroseries
+
+    def assertProcessorControls(self, processors_control, enabled, disabled):
+        matchers = [
+            MatchesStructure.byEquality(optionValue=name, disabled=False)
+            for name in enabled]
+        matchers.extend([
+            MatchesStructure.byEquality(optionValue=name, disabled=True)
+            for name in disabled])
+        self.assertThat(processors_control.controls, MatchesSetwise(*matchers))
+
     def test_initial_distroseries(self):
         # The initial distroseries is the newest that is current or in
         # development.
@@ -362,6 +384,47 @@ class TestSnapAddView(BrowserTestCase):
             "macaroon_caveat_id": ["dummy"],
             }
         self.assertEqual(expected_args, parse_qs(parsed_location[3]))
+
+    def test_create_new_snap_display_processors(self):
+        branch = self.factory.makeAnyBranch()
+        distroseries = self.setUpDistroSeries()
+        browser = self.getViewBrowser(
+            branch, view_name="+new-snap", user=self.person)
+        processors = browser.getControl(name="field.processors")
+        self.assertContentEqual(
+            ["Intel 386 (386)", "AMD 64bit (amd64)", "HPPA Processor (hppa)"],
+            [extract_text(option) for option in processors.displayOptions])
+        self.assertContentEqual(["386", "amd64", "hppa"], processors.options)
+        self.assertContentEqual([], processors.value)
+
+    def test_create_new_snap_display_restricted_processors(self):
+        # A restricted processor is shown disabled in the UI.
+        branch = self.factory.makeAnyBranch()
+        distroseries = self.setUpDistroSeries()
+        proc_armhf = self.factory.makeProcessor(
+            name="armhf", restricted=True, build_by_default=False)
+        self.factory.makeDistroArchSeries(
+            distroseries=distroseries, architecturetag="armhf",
+            processor=proc_armhf)
+        browser = self.getViewBrowser(
+            branch, view_name="+new-snap", user=self.person)
+        processors = browser.getControl(name="field.processors")
+        self.assertProcessorControls(
+            processors, ["386", "amd64", "hppa"], ["armhf"])
+
+    def test_create_new_snap_processors(self):
+        branch = self.factory.makeAnyBranch()
+        distroseries = self.setUpDistroSeries()
+        browser = self.getViewBrowser(
+            branch, view_name="+new-snap", user=self.person)
+        processors = browser.getControl(name="field.processors")
+        processors.value = ["386", "amd64"]
+        browser.getControl(name="field.name").value = "snap-name"
+        browser.getControl("Create snap package").click()
+        login_person(self.person)
+        snap = getUtility(ISnapSet).getByName(self.person, u"snap-name")
+        self.assertContentEqual(
+            ["386", "amd64"], [proc.name for proc in snap.processors])
 
     def test_initial_name_extraction_git(self):
         [git_ref] = self.factory.makeGitRefs()
