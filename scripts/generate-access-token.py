392fde928b49d2cdedb6965991ee663b270b8f4c
Kit Randel <kit.randel@canonical.com>
2016-07-05 09:39:04 +1200

    Add license header.

--- scripts/generate-access-token.py
+++ scripts/generate-access-token.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python -S
+#
+# Copyright 2016 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
 
-# Conveniently generates access token and outputs relevant settings.
-# You will most likely want to run this with
-# LP_DISABLE_SSL_CERTIFICATE_VALIDATION=1 locally.
+"""Conveniently generates access token and outputs relevant settings.
+
+    You will most likely want to run this with:
+    LP_DISABLE_SSL_CERTIFICATE_VALIDATION=1
+"""
 
 import _pythonpath
 
