2d6e0bec503d82da07cb045b0557e1d68eff2d32
Colin Watson <cjwatson@canonical.com>
2016-07-18 14:48:42 +0100

    Restore "Create snap package" link for contexts with no snap packages.

--- lib/lp/snappy/browser/tests/test_hassnaps.py
+++ lib/lp/snappy/browser/tests/test_hassnaps.py
@@ -5,16 +5,23 @@
 
 __metaclass__ = type
 
+import soupmatchers
 from testscenarios import (
     load_tests_apply_scenarios,
     WithScenarios,
     )
 
 from lp.code.interfaces.branch import IBranch
+from lp.code.interfaces.githosting import IGitHostingClient
 from lp.code.interfaces.gitrepository import IGitRepository
 from lp.services.webapp import canonical_url
 from lp.testing import TestCaseWithFactory
-from lp.testing.layers import DatabaseFunctionalLayer
+from lp.testing.fakemethod import FakeMethod
+from lp.testing.fixture import ZopeUtilityFixture
+from lp.testing.layers import (
+    DatabaseFunctionalLayer,
+    LaunchpadFunctionalLayer,
+    )
 from lp.testing.views import create_initialized_view
 
 
@@ -85,6 +92,58 @@ class TestHasSnapsView(WithScenarios, TestCaseWithFactory):
             '<a href="+snaps">2 snap packages</a> using this %s.' %
             self.context_type)
         self.assertEqual(expected_link, view.snaps_link)
+
+
+class TestHasSnapsMenu(WithScenarios, TestCaseWithFactory):
+
+    needs_hosting_client = False
+
+    scenarios = [
+        ("Branch", {
+            "layer": DatabaseFunctionalLayer,
+            "context_factory": make_branch,
+            }),
+        ("GitRef", {
+            "layer": LaunchpadFunctionalLayer,
+            "context_factory": make_git_ref,
+            "needs_hosting_client": True,
+            }),
+        ]
+
+    def setUp(self):
+        super(TestHasSnapsMenu, self).setUp()
+        if self.needs_hosting_client:
+            hosting_client = FakeMethod()
+            hosting_client.getLog = FakeMethod(result=[])
+            self.useFixture(
+                ZopeUtilityFixture(hosting_client, IGitHostingClient))
+
+    def makeSnap(self, context):
+        if IBranch.providedBy(context):
+            return self.factory.makeSnap(branch=context)
+        else:
+            return self.factory.makeSnap(git_ref=context)
+
+    def test_creation_link_no_snaps(self):
+        # An object with no snap packages shows a creation link.
+        context = self.context_factory(self)
+        view = create_initialized_view(context, "+index")
+        new_snap_url = canonical_url(context, view_name="+new-snap")
+        self.assertThat(view(), soupmatchers.HTMLContains(
+            soupmatchers.Tag(
+                "creation link", "a", attrs={"href": new_snap_url},
+                text="Create snap package")))
+
+    def test_creation_link_snaps(self):
+        # An object with snap packages shows a creation link.
+        context = self.context_factory(self)
+        self.makeSnap(context)
+        view = create_initialized_view(context, "+index")
+        new_snap_url = canonical_url(context, view_name="+new-snap")
+        self.assertThat(view(), soupmatchers.HTMLContains(
+            soupmatchers.Tag(
+                "creation link", "a", attrs={"href": new_snap_url},
+                text="Create snap package")))
 
 
 load_tests = load_tests_apply_scenarios
