d658049c0de06a0a42f1e27718c8747b66b90780
Thomi Richards <thomi.richards@canonical.com>
2016-07-25 12:20:12 +1200

    Update copyright header & docstring.

--- lib/lp/registry/tests/test_gpgkey.py
+++ lib/lp/registry/tests/test_gpgkey.py
@@ -1,7 +1,7 @@
-# Copyright 2010 Canonical Ltd.  This software is licensed under the
+# Copyright 2016 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
-"""Tests for GPG key on the web."""
+"""Tests for GPGKeySet model."""
 
 __metaclass__ = type
 
