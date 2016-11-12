0319b748ec85533a2022c82888504a7efd46fecd
Colin Watson <cjwatson@canonical.com>
2016-11-12 02:12:45 +0000

    Split the unique CodeImport(url) index into one for each target RCS type.

---
+++ database/schema/patch-2209-80-1.sql
@@ -0,0 +1,12 @@
+-- Copyright 2016 Canonical Ltd.  This software is licensed under the
+-- GNU Affero General Public License version 3 (see the file LICENSE).
+
+SET client_min_messages=ERROR;
+
+DROP INDEX codeimport__url__idx;
+CREATE UNIQUE INDEX codeimport__url__branch__idx
+    ON CodeImport(url) WHERE branch IS NOT NULL;
+CREATE UNIQUE INDEX codeimport__url__git_repository__idx
+    ON CodeImport(url) WHERE git_repository IS NOT NULL;
+
+INSERT INTO LaunchpadDatabaseRevision VALUES (2209, 80, 1);
