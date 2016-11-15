b07a649aed5559c3f04cd95902a73ae842c62062
Colin Watson <cjwatson@canonical.com>
2016-11-15 12:26:14 +0000

    Switch order of index creation/deletion.

--- database/schema/patch-2209-80-1.sql
+++ database/schema/patch-2209-80-1.sql
@@ -3,10 +3,10 @@
 
 SET client_min_messages=ERROR;
 
-DROP INDEX codeimport__url__idx;
 CREATE UNIQUE INDEX codeimport__url__branch__idx
     ON CodeImport(url) WHERE branch IS NOT NULL;
 CREATE UNIQUE INDEX codeimport__url__git_repository__idx
     ON CodeImport(url) WHERE git_repository IS NOT NULL;
+DROP INDEX codeimport__url__idx;
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2209, 80, 1);
