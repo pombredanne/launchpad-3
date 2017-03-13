60e294ba246eda5b26d15f3e3edfc347c6621abc
Stuart Bishop <stuart@stuartbishop.net>
2017-03-13 17:35:21 +0700

    No need to specify the OpenID identifier during normal bot creation

--- lib/lp/registry/scripts/createbotaccount.py
+++ lib/lp/registry/scripts/createbotaccount.py
@@ -24,38 +24,36 @@ class CreateBotAccountScript(LaunchpadScript):
 
     def add_my_options(self):
         self.parser.add_option(
-            '--name', metavar='NAME', action='store',
-            type='string', dest='name', default=None,
+            '-u', '--username', metavar='NAME', action='store',
+            type='string', dest='username', default='',
             help='Username for the bot')
         self.parser.add_option(
-            '--openid', metavar='OPENID_SUFFIX', action='store',
-            type='string', dest='openid', default=None,
-            help='OpenID identifier. Just the suffix, not the full URL')
+            '--openid', metavar='SUFFIX', action='store',
+            type='string', dest='openid', default='',
+            help=('OpenID identifier suffix. Normally unnecessary because '
+                  'SSO account creation handles it'))
         self.parser.add_option(
-            '--email', metavar='ADDR', action='store',
-            type='string', dest='email', default=None,
+            '-e', '--email', metavar='ADDR', action='store',
+            type='string', dest='email', default='',
             help='Email address. Defaults to webops+username@canonical.com')
         self.parser.add_option(
-            '--sshkey', metavar='TXT', action='store',
-            type='string', dest='sshkey', default=None,
+            '-k', '--sshkey', metavar='TXT', action='store',
+            type='string', dest='sshkey', default='',
             help='SSH public key. Defaults to no ssh key.')
         self.parser.add_option(
-            '--teams', metavar='TEAMS', action='store',
+            '-t', '--teams', metavar='TEAMS', action='store',
             type='string', dest='teams',
             default='canonical-is-devopsolution-bots',
             help='Add bot to this comma separated list of teams')
 
     def main(self):
-        username = unicode(self.options.name)
+        username = unicode(self.options.username)
         if not username:
-            raise LaunchpadScriptFailure('--name is a required option')
-
+            raise LaunchpadScriptFailure('--username is required')
         openid_suffix = unicode(self.options.openid)
-        if not openid_suffix:
-            raise LaunchpadScriptFailure('--openid is a required option')
         if '/' in openid_suffix:
             raise LaunchpadScriptFailure(
-                '{} is not a valid openid suffix'.format(openid_suffix))
+                'Invalid OpenID suffix {}'.format(openid_suffix))
 
         displayname = u'{} McBotface'.format(username)
 
@@ -75,9 +73,26 @@ class CreateBotAccountScript(LaunchpadScript):
 
         person_set = getUtility(IPersonSet)
 
+        if openid_suffix and person_set.getByName(username) is None:
+            # Normally the SSO has already called this method.
+            # This codepath is really only used for testing.
+            person_set.createPlaceholderPerson(openid_suffix, username)
+
+        person = person_set.getByName(username)
+        if person is None:
+            raise LaunchpadScriptFailure(
+                'Account {} does not exist'.format(username))
+        if person.account is None:
+            raise LaunchpadScriptFailure(
+                'Person {} has no Account'.format(username))
+        if person.account.openid_identifiers.count() != 1:
+            raise LaunchpadScriptFailure(
+                'Account {} has invalid OpenID identifiers'.format(username))
+        openid_identifier = person.account.openid_identifiers.one()
+
         # Create the IPerson
         person, _ = person_set.getOrCreateByOpenIDIdentifier(
-            unicode(openid_suffix),
+            openid_identifier.identifier,
             emailaddress,
             displayname,
             # If we want a specific rationale, we should also update
@@ -85,7 +100,7 @@ class CreateBotAccountScript(LaunchpadScript):
             PersonCreationRationale.OWNER_CREATED_LAUNCHPAD,
             comment="when the create-bot-account launchpad script was run")
 
-        person.name = username
+        # person.name = username
         person.selfgenerated_bugnotifications = True
         person.expanded_notification_footers = True
         person.description = 'Canonical IS created bot'
