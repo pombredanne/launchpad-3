1cd27f3a7f9f869ca678cc91093a469b0e31ec05
Colin Watson <cjwatson@canonical.com>
2017-04-29 16:24:32 +0100

    Speed up tests that use archive signing keys using an in-process keyserver fixture.
    This only works for tests that only talk to the keyserver asynchronously,
    but for those that do it's very much faster to start one up in-process
    rather than using a .tac file: it saves on the order of 10 seconds per
    affected test on my laptop.
    
    The tests for the fixture itself seem to hit some slightly buggy bit of
    Twisted and require a couple of extra spins through the reactor to clear up
    cancelled DelayedCalls, but fortunately testtools.deferredruntest already
    has machinery to tolerate that.

---
+++ lib/lp/testing/keyserver/tests/test_inprocess.py
@@ -0,0 +1,44 @@
+# Copyright 2017 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+"""In-process keyserver fixture tests."""
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+__metaclass__ = type
+
+from testtools.deferredruntest import (
+    AsynchronousDeferredRunTestForBrokenTwisted,
+    )
+from twisted.internet import defer
+from twisted.web.client import getPage
+
+from lp.services.config import config
+from lp.testing import TestCase
+from lp.testing.keyserver import InProcessKeyServerFixture
+from lp.testing.keyserver.web import GREETING
+
+
+class TestInProcessKeyServerFixture(TestCase):
+
+    run_tests_with = AsynchronousDeferredRunTestForBrokenTwisted.make_factory(
+        timeout=10)
+
+    @defer.inlineCallbacks
+    def test_url(self):
+        # The url is the one that gpghandler is configured to hit.
+        fixture = self.useFixture(InProcessKeyServerFixture())
+        yield fixture.start()
+        self.assertEqual(
+            ("http://%s:%d" % (
+                config.gpghandler.host,
+                config.gpghandler.port)).encode("UTF-8"),
+            fixture.url)
+
+    @defer.inlineCallbacks
+    def test_starts_properly(self):
+        # The fixture starts properly and we can load the page.
+        fixture = self.useFixture(InProcessKeyServerFixture())
+        yield fixture.start()
+        content = yield getPage(fixture.url)
+        self.assertEqual(GREETING, content)
