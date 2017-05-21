13ee2f8465e9b2a369ba73132ec06ce7f31be616
Colin Watson <cjwatson@canonical.com>
2017-05-21 22:37:03 +0100

    Remove extra internal slashes from URLs requested by the mirror prober.

--- lib/lp/registry/scripts/distributionmirror_prober.py
+++ lib/lp/registry/scripts/distributionmirror_prober.py
@@ -1,4 +1,4 @@
-# Copyright 2009-2010 Canonical Ltd.  This software is licensed under the
+# Copyright 2009-2017 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
 
 __metaclass__ = type
@@ -651,7 +651,7 @@ def get_expected_cdimage_paths():
     for line in _get_cdimage_file_list().readlines():
         flavour, seriesname, path, size = line.split('\t')
         paths = d.setdefault((flavour, seriesname), [])
-        paths.append(path)
+        paths.append(path.lstrip('/'))
 
     ubuntu = getUtility(ILaunchpadCelebrities).ubuntu
     paths = []
@@ -686,7 +686,7 @@ def probe_archive_mirror(mirror, logfile, unchecked_keys, logger):
     all_paths = itertools.chain(packages_paths, sources_paths)
     request_manager = RequestManager()
     for series, pocket, component, path in all_paths:
-        url = "%s/%s" % (mirror.base_url, path)
+        url = urlparse.urljoin(mirror.base_url, path)
         callbacks = ArchiveMirrorProberCallbacks(
             mirror, series, pocket, component, url, logfile)
         unchecked_keys.append(url)
@@ -730,7 +730,7 @@ def probe_cdimage_mirror(mirror, logfile, unchecked_keys, logger):
         deferredList = []
         request_manager = RequestManager()
         for path in paths:
-            url = '%s/%s' % (mirror.base_url, path)
+            url = urlparse.urljoin(mirror.base_url, path)
             # Use a RedirectAwareProberFactory because CD mirrors are allowed
             # to redirect, and we need to cope with that.
             prober = RedirectAwareProberFactory(url)
