cfbfc912af0ce2d07478e3729f8de1005a7c4b9d
Andy Whitcroft <apw@ubuntu.com>
2017-08-01 06:13:20 +0000

    tests: remove debugging print import
    Signed-off-by: Andy Whitcroft <apw@ubuntu.com>

--- lib/lp/archivepublisher/tests/test_signing.py
+++ lib/lp/archivepublisher/tests/test_signing.py
@@ -1,6 +1,5 @@
 # Copyright 2012-2017 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
-from __future__ import print_function
 
 """Test UEFI custom uploads."""
 
