4a9e8f8a960c247ec4c08e601453b9894aec918e
Maximiliano Bertacchini <maximiliano.bertacchini@canonical.com>
2018-03-28 16:14:10 +0000

    Assorted improvements on sitesearch tests.

--- lib/lp/services/sitesearch/tests/test_google.py
+++ lib/lp/services/sitesearch/tests/test_google.py
@@ -5,8 +5,8 @@
 
 __metaclass__ = type
 
-from contextlib import contextmanager
 
+from fixtures import MockPatch
 from requests.exceptions import (
     ConnectionError,
     HTTPError,
@@ -17,26 +17,6 @@ from lp.services.sitesearch.interfaces import SiteSearchResponseError
 from lp.services.timeout import TimeoutError
 from lp.testing import TestCase
 from lp.testing.layers import LaunchpadFunctionalLayer
-
-
-@contextmanager
-def urlfetch_exception(test_error, *args):
-    """Raise an error during the execution of urlfetch.
-
-    This function replaces urlfetch() with a function that
-    raises an error.
-    """
-
-    def raise_exception(url):
-        raise test_error(*args)
-
-    from lp.services import timeout
-    original_urlfetch = timeout.urlfetch
-    timeout.urlfetch = raise_exception
-    try:
-        yield
-    finally:
-        timeout.urlfetch = original_urlfetch
 
 
 class TestGoogleSearchService(TestCase):
@@ -51,35 +31,42 @@ class TestGoogleSearchService(TestCase):
     def test_search_converts_HTTPError(self):
         # The method converts HTTPError to SiteSearchResponseError.
         args = ('url', 500, 'oops', {}, None)
-        with urlfetch_exception(HTTPError, *args):
-            self.assertRaises(
-                SiteSearchResponseError, self.search_service.search, 'fnord')
+        self.useFixture(MockPatch(
+            'lp.services.sitesearch.urlfetch', side_effect=HTTPError(*args)))
+        self.assertRaises(
+            SiteSearchResponseError, self.search_service.search, 'fnord')
 
     def test_search_converts_ConnectionError(self):
         # The method converts ConnectionError to SiteSearchResponseError.
-        with urlfetch_exception(ConnectionError, 'oops'):
-            self.assertRaises(
-                SiteSearchResponseError, self.search_service.search, 'fnord')
+        self.useFixture(MockPatch(
+            'lp.services.sitesearch.urlfetch',
+            side_effect=ConnectionError('oops')))
+        self.assertRaises(
+            SiteSearchResponseError, self.search_service.search, 'fnord')
 
     def test_search_converts_TimeoutError(self):
         # The method converts TimeoutError to SiteSearchResponseError.
-        with urlfetch_exception(TimeoutError, 'oops'):
-            self.assertRaises(
-                SiteSearchResponseError, self.search_service.search, 'fnord')
+        self.useFixture(MockPatch(
+            'lp.services.sitesearch.urlfetch',
+            side_effect=TimeoutError('oops')))
+        self.assertRaises(
+            SiteSearchResponseError, self.search_service.search, 'fnord')
 
     def test___parse_google_search_protocol_SyntaxError(self):
         # The method converts SyntaxError to SiteSearchResponseError.
-        with urlfetch_exception(SyntaxError, 'oops'):
-            self.assertRaises(
-                SiteSearchResponseError,
-                self.search_service._parse_google_search_protocol, '')
+        self.useFixture(MockPatch(
+            'lp.services.timeout.urlfetch', side_effect=SyntaxError('oops')))
+        self.assertRaises(
+            SiteSearchResponseError,
+            self.search_service._parse_google_search_protocol, '')
 
     def test___parse_google_search_protocol_IndexError(self):
         # The method converts IndexError to SiteSearchResponseError.
-        with urlfetch_exception(IndexError, 'oops'):
-            data = (
-                '<?xml version="1.0" encoding="UTF-8" standalone="no"?>'
-                '<GSP VER="3.2"></GSP>')
-            self.assertRaises(
-                SiteSearchResponseError,
-                self.search_service._parse_google_search_protocol, data)
+        self.useFixture(MockPatch(
+            'lp.services.timeout.urlfetch', side_effect=IndexError('oops')))
+        data = (
+            '<?xml version="1.0" encoding="UTF-8" standalone="no"?>'
+            '<GSP VER="3.2"></GSP>')
+        self.assertRaises(
+            SiteSearchResponseError,
+            self.search_service._parse_google_search_protocol, data)
