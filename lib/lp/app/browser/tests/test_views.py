e23b3e410b23c9e2962b4c828a68d4b55d54ff5d
Maximiliano Bertacchini <maximiliano.bertacchini@canonical.com>
2018-03-28 19:11:16 +0000

    Move setting sitesearch feature flag fixture outside of doctests.

--- lib/lp/app/browser/tests/test_views.py
+++ lib/lp/app/browser/tests/test_views.py
@@ -7,11 +7,11 @@ Run the view tests.
 
 import logging
 import os
-import unittest
 
+from lp.services.features.testing import FeatureFixture
+from lp.services.testing import build_test_suite
 from lp.testing.layers import (
     BingLaunchpadFunctionalLayer,
-    DatabaseFunctionalLayer,
     GoogleLaunchpadFunctionalLayer,
     )
 from lp.testing.systemdocs import (
@@ -22,33 +22,46 @@ from lp.testing.systemdocs import (
 
 
 here = os.path.dirname(os.path.realpath(__file__))
+bing_flag = FeatureFixture({'sitesearch.engine.name': 'bing'})
+google_flag = FeatureFixture({'sitesearch.engine.name': 'google'})
+
+
+def setUp_bing(test):
+    setUp(test)
+    bing_flag.setUp()
+
+
+def setUp_google(test):
+    setUp(test)
+    google_flag.setUp()
+
+
+def tearDown_bing(test):
+    bing_flag.cleanUp()
+    tearDown(test)
+
+
+def tearDown_google(test):
+    google_flag.cleanUp()
+    tearDown(test)
+
 
 # The default layer of view tests is the DatabaseFunctionalLayer. Tests
 # that require something special like the librarian or mailman must run
 # on a layer that sets those services up.
-special_test_layer = {
-    'launchpad-search-pages-google.txt': GoogleLaunchpadFunctionalLayer,
-    'launchpad-search-pages-bing.txt': BingLaunchpadFunctionalLayer,
+special = {
+    'launchpad-search-pages-bing.txt': LayeredDocFileSuite(
+        '../doc/launchpad-search-pages-bing.txt',
+        setUp=setUp_bing, tearDown=tearDown_bing,
+        layer=BingLaunchpadFunctionalLayer,
+        stdout_logging_level=logging.WARNING),
+    'launchpad-search-pages-google.txt': LayeredDocFileSuite(
+        '../doc/launchpad-search-pages-google.txt',
+        setUp=setUp_google, tearDown=tearDown_google,
+        layer=GoogleLaunchpadFunctionalLayer,
+        stdout_logging_level=logging.WARNING),
     }
 
 
 def test_suite():
-    suite = unittest.TestSuite()
-    testsdir = os.path.abspath(here)
-
-    # Add tests using default setup/teardown
-    filenames = [filename
-                 for filename in os.listdir(testsdir)
-                 if filename.endswith('.txt')]
-    # Sort the list to give a predictable order.
-    filenames.sort()
-    for filename in filenames:
-        path = filename
-        layer = special_test_layer.get(path, DatabaseFunctionalLayer)
-        one_test = LayeredDocFileSuite(
-            path, setUp=setUp, tearDown=tearDown, layer=layer,
-            stdout_logging_level=logging.WARNING
-            )
-        suite.addTest(one_test)
-
-    return suite
+    return build_test_suite(here, special)
