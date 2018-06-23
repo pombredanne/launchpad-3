4ea29be9f762826164b1dd548c42798afc10f2c6
Colin Watson <cjwatson@canonical.com>
2018-06-23 13:35:33 +0100

    Use the configured proxy for site search rather than relying on the environment.

--- lib/lp/services/sitesearch/__init__.py
+++ lib/lp/services/sitesearch/__init__.py
@@ -212,7 +212,9 @@ class BingSearchService:
         timeline = get_request_timeline(request)
         action = timeline.start("bing-search-api", search_url)
         try:
-            response = urlfetch(search_url, headers=search_headers)
+            response = urlfetch(
+                search_url, headers=search_headers,
+                trust_env=False, use_proxy=True)
         except (TimeoutError, requests.RequestException) as error:
             raise SiteSearchResponseError(
                 "The response errored: %s" % str(error))
